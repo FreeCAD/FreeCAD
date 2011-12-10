23164bfa1add3f2c7533c01446477e578ab4dac8
wmayer <wmayer@e8eeb9e2-ec13-0410-a4a9-efa5cf37419d>
2011-12-10 21:56:16 +0000

    0000534: cmake error
    git-svn-id: https://free-cad.svn.sourceforge.net/svnroot/free-cad/trunk@5263 e8eeb9e2-ec13-0410-a4a9-efa5cf37419d

--- cMake/FindSpnav.cmake
+++ cMake/FindSpnav.cmake
@@ -1,29 +1,35 @@
-# Find the spnav library and header.
-#
-# Sets the usual variables expected for find_package scripts:
-#
-# SPNAV_INCLUDE_DIR - header location
-# SPNAV_LIBRARIES - library to link against
-# SPNAV_FOUND - true if pugixml was found.
-
 IF(UNIX)
 
-    FIND_PATH(SPNAV_INCLUDE_DIR spnav.h)
+    set(TEST_SPNAV_CMAKE TRUE)
+
+    FIND_PATH(SPNAV_INCLUDE_PATH spnav.h
+    )
 
     FIND_LIBRARY(SPNAV_LIBRARY
         NAMES
         spnav libspnav
     )
 
-# Support the REQUIRED and QUIET arguments, and set SPNAV_FOUND if found.
-include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Spnav DEFAULT_MSG SPNAV_LIBRARY
-                                  SPNAV_INCLUDE_DIR)
+    if(SPNAV_INCLUDE_PATH AND SPNAV_LIBRARY)
+        set(SPNAV_FOUND TRUE)
+        set(SPNAV_LIBRARIES ${SPNAV_LIBRARY})
+        set(SPNAV_INCLUDE_DIR ${SPNAV_INCLUDE_PATH})
+    endif(SPNAV_INCLUDE_PATH AND SPNAV_LIBRARY)
 
-if(SPNAV_FOUND)
-    set(SPNAV_LIBRARIES ${SPNAV_LIBRARY})
-endif()
 
-mark_as_advanced(SPANV_LIBRARY SPNAV_INCLUDE_DIR)
+
+    if(TEST_SPNAV_CMAKE)
+        if(SPNAV_INCLUDE_PATH)
+            MESSAGE("found spnav include path ${SPNAV_INCLUDE_PATH}")
+        else(SPNAV_INCLUDE_PATH)
+            MESSAGE("didn't find spnav include path")
+        endif(SPNAV_INCLUDE_PATH)
+
+        if(SPNAV_LIBRARY)
+            MESSAGE("found spnav library ${SPNAV_LIBRARY}")
+        else(SPNAV_LIBRARY)
+            MESSAGE("didn't find spnav library")
+        endif(SPNAV_LIBRARY)
+    endif(TEST_SPNAV_CMAKE)
 
 ENDIF(UNIX)
