4015b7ff605af9d9f4ea7e9a36638005d3f5589b
wmayer <wmayer@e8eeb9e2-ec13-0410-a4a9-efa5cf37419d>
2012-01-03 15:16:41 +0000

    + change license to LGPL
    git-svn-id: https://free-cad.svn.sourceforge.net/svnroot/free-cad/trunk@5385 e8eeb9e2-ec13-0410-a4a9-efa5cf37419d

--- src/Tools/SubWCRev.py
+++ src/Tools/SubWCRev.py
@@ -1,34 +1,13 @@
+#! python
+# -*- coding: utf-8 -*-
+# (c) 2006 Werner Mayer LGPL
+#
 # FreeCAD RevInfo script to get the revision information from Subversion.
-# (c) 2006 Werner Mayer
 #
 # Under Linux the Subversion tool SubWCRev shipped with TortoiseSVN isn't 
 # available which is provided by this script. 
 # 2011/02/05: The script was extended to support also Bazaar
 
-#***************************************************************************
-#*   Copyright (c) 2006 Werner Mayer <werner.wm.mayer@gmx.de>              *
-#*                                                                         *
-#*   This file is part of the FreeCAD CAx development system.              *
-#*                                                                         *
-#*   This program is free software; you can redistribute it and/or modify  *
-#*   it under the terms of the GNU Library General Public License (LGPL)   *
-#*   as published by the Free Software Foundation; either version 2 of     *
-#*   the License, or (at your option) any later version.                   *
-#*   for detail see the LICENCE text file.                                 *
-#*                                                                         *
-#*   FreeCAD is distributed in the hope that it will be useful,            *
-#*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
-#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
-#*   GNU Library General Public License for more details.                  *
-#*                                                                         *
-#*   You should have received a copy of the GNU Library General Public     *
-#*   License along with FreeCAD; if not, write to the Free Software        *
-#*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
-#*   USA                                                                   *
-#*                                                                         *
-#***************************************************************************
-
-#!/usr/bin/python
 import os,sys,string,re,time,getopt
 import xml.sax
 import xml.sax.handler
