ccf878ec167bee52ab86b7fe9cd0a04331691bff
Yorik van Havre <yorikvanhavre@users.sourceforge.net>
2012-01-04 12:06:02 -0200

    Various fixes to Draft and Arch
    + Renamed Draft Scale object to Clone
    + Removed BasePoint property from Draft Clone
    + Reenabled Draft SelectPlane tool
    + Added Draft context tools to Arch module
    + Made the Arch module load pycollada later

--- src/Mod/Arch/InitGui.py
+++ src/Mod/Arch/InitGui.py
@@ -22,78 +22,84 @@
 #***************************************************************************
 
 class ArchWorkbench(Workbench):
-	"Arch workbench object"
-	Icon = """
-                /* XPM */
-                static char * arch_xpm[] = {
-                "16 16 9 1",
-                " 	c None",
-                ".	c #543016",
-                "+	c #6D2F08",
-                "@	c #954109",
-                "#	c #874C24",
-                "$	c #AE6331",
-                "%	c #C86423",
-                "&	c #FD7C26",
-                "*	c #F5924F",
-                "                ",
-                "                ",
-                "       #        ",
-                "      ***$#     ",
-                "    .*******.   ",
-                "   *##$****#+   ",
-                " #**%&&##$#@@   ",
-                ".$**%&&&&+@@+   ",
-                "@&@#$$%&&@@+..  ",
-                "@&&&%#.#$#+..#$.",
-                " %&&&&+%#.$**$@+",
-                "   @%&+&&&$##@@+",
-                "     @.&&&&&@@@ ",
-                "        @%&&@@  ",
-                "           @+   ",
-                "                "};
-			"""
-	MenuText = "Arch"
-	ToolTip = "Architecture workbench"
+    "Arch workbench object"
+    Icon = """
+        /* XPM */
+        static char * arch_xpm[] = {
+        "16 16 9 1",
+        " 	c None",
+        ".	c #543016",
+        "+	c #6D2F08",
+        "@	c #954109",
+        "#	c #874C24",
+        "$	c #AE6331",
+        "%	c #C86423",
+        "&	c #FD7C26",
+        "*	c #F5924F",
+        "                ",
+        "                ",
+        "       #        ",
+        "      ***$#     ",
+        "    .*******.   ",
+        "   *##$****#+   ",
+        " #**%&&##$#@@   ",
+        ".$**%&&&&+@@+   ",
+        "@&@#$$%&&@@+..  ",
+        "@&&&%#.#$#+..#$.",
+        " %&&&&+%#.$**$@+",
+        "   @%&+&&&$##@@+",
+        "     @.&&&&&@@@ ",
+        "        @%&&@@  ",
+        "           @+   ",
+        "                "};
+        """
+    MenuText = "Arch"
+    ToolTip = "Architecture workbench"
 	
-	def Initialize(self):
-                import DraftTools,DraftGui,Arch_rc,Arch
-                archtools = ["Arch_Wall","Arch_Structure","Arch_Cell",
-                             "Arch_Floor","Arch_Building","Arch_Site",
-                             "Arch_Window","Arch_Axis",
-                             "Arch_SectionPlane","Arch_Add","Arch_Remove"]
-                drafttools = ["Draft_Line","Draft_Wire","Draft_Rectangle",
-                              "Draft_Polygon","Draft_Arc",
-                              "Draft_Circle","Draft_Dimension",
-                              "Draft_Move","Draft_Rotate",
-                              "Draft_Offset","Draft_Upgrade",
-                              "Draft_Downgrade","Draft_Trimex"]
-                meshtools = ["Arch_SplitMesh","Arch_MeshToShape",
-                             "Arch_SelectNonSolidMeshes","Arch_RemoveShape"]
-                self.appendToolbar(str(DraftTools.translate("arch","Arch tools")),archtools)
-                self.appendToolbar(str(DraftTools.translate("arch","Draft tools")),drafttools)
-                self.appendMenu([str(DraftTools.translate("arch","Architecture")),str(DraftTools.translate("arch","Tools"))],meshtools)
-                self.appendMenu(str(DraftTools.translate("arch","Architecture")),archtools)
-                self.appendMenu(str(DraftTools.translate("arch","Draft")),drafttools)
-                FreeCADGui.addIconPath(":/icons")
-                FreeCADGui.addLanguagePath(":/translations")
-                FreeCADGui.addPreferencePage(":/ui/archprefs-base.ui","Arch")
-                FreeCAD.addImportType("Industry Foundation Classes (*.ifc)","importIFC")
-                FreeCAD.addExportType("Wavefront OBJ - Arch module (*.obj)","importOBJ")
-                try:
-                        import collada
-                except:
-                        Log("pycollada not found, no collada support\n")
-                else:
-                        FreeCAD.addImportType("Collada (*.dae)","importDAE")
-                        FreeCAD.addExportType("Collada (*.dae)","importDAE")
-		Log ('Loading Arch module... done\n')
-	def Activated(self):
-                FreeCADGui.draftToolBar.Activated()
-		Msg("Arch workbench activated\n")
-	def Deactivated(self):
-                FreeCADGui.draftToolBar.Deactivated()
-		Msg("Arch workbench deactivated\n")
+    def Initialize(self):
+        import DraftTools,DraftGui,Arch_rc,Arch
+        self.archtools = ["Arch_Wall","Arch_Structure","Arch_Cell",
+                     "Arch_Floor","Arch_Building","Arch_Site",
+                     "Arch_Window","Arch_Axis",
+                     "Arch_SectionPlane","Arch_Add","Arch_Remove"]
+        self.drafttools = ["Draft_Line","Draft_Wire","Draft_Rectangle",
+                      "Draft_Polygon","Draft_Arc",
+                      "Draft_Circle","Draft_Dimension",
+                      "Draft_Move","Draft_Rotate",
+                      "Draft_Offset","Draft_Upgrade",
+                      "Draft_Downgrade","Draft_Trimex"]
+        self.draftcontexttools = ["Draft_ApplyStyle","Draft_ToggleDisplayMode",
+                             "Draft_AddToGroup","Draft_SelectGroup",
+                             "Draft_SelectPlane"]
+        self.meshtools = ["Arch_SplitMesh","Arch_MeshToShape",
+                     "Arch_SelectNonSolidMeshes","Arch_RemoveShape"]
+        self.appendToolbar(str(DraftTools.translate("arch","Arch tools")),self.archtools)
+        self.appendToolbar(str(DraftTools.translate("arch","Draft tools")),self.drafttools)
+        self.appendMenu([str(DraftTools.translate("arch","Architecture")),str(DraftTools.translate("arch","Tools"))],self.meshtools)
+        self.appendMenu(str(DraftTools.translate("arch","Architecture")),self.archtools)
+        self.appendMenu(str(DraftTools.translate("arch","Draft")),self.drafttools+self.draftcontexttools)
+        FreeCADGui.addIconPath(":/icons")
+        FreeCADGui.addLanguagePath(":/translations")
+        FreeCADGui.addPreferencePage(":/ui/archprefs-base.ui","Arch")
+        FreeCAD.addImportType("Industry Foundation Classes (*.ifc)","importIFC")
+        FreeCAD.addExportType("Wavefront OBJ - Arch module (*.obj)","importOBJ")
+        FreeCAD.addImportType("Collada (*.dae)","importDAE")
+        FreeCAD.addExportType("Collada (*.dae)","importDAE")
+        Log ('Loading Arch module... done\n')
+                
+    def Activated(self):
+        FreeCADGui.draftToolBar.Activated()
+        Msg("Arch workbench activated\n")
+                
+    def Deactivated(self):
+        FreeCADGui.draftToolBar.Deactivated()
+        Msg("Arch workbench deactivated\n")
+                
+    def ContextMenu(self, recipient):
+        self.appendContextMenu("Display tools",self.draftcontexttools)
+
+    def GetClassName(self): 
+        return "Gui::PythonWorkbench"
 
 FreeCADGui.addWorkbench(ArchWorkbench)
 
