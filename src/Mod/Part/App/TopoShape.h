0778aef53bf9c0d82f24d18384bdd11ee328f783
wmayer <wmayer@users.sourceforge.net>
2012-05-14 19:15:11 +0200

    Replace makeTube algorithm

--- src/Mod/Part/App/TopoShape.h
+++ src/Mod/Part/App/TopoShape.h
@@ -158,8 +158,7 @@ public:
     TopoDS_Shape makeThickSolid(const TopTools_ListOfShape& remFace,
         Standard_Real offset, Standard_Real tolerance) const;
     TopoDS_Shape makeSweep(const TopoDS_Shape& profile, double, int) const;
-    TopoDS_Shape makeTube(double radius, double tol) const;
-    TopoDS_Shape makeTube() const;
+    TopoDS_Shape makeTube(double radius, double tol, int cont, int maxdeg, int maxsegm) const;
     TopoDS_Shape makeHelix(Standard_Real pitch, Standard_Real height,
         Standard_Real radius, Standard_Real angle=0, Standard_Boolean left=Standard_False) const;
     TopoDS_Shape makeLoft(const TopTools_ListOfShape& profiles, Standard_Boolean isSolid,
