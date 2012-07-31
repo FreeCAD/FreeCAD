b3c5f6ae7ff552fcfd3022c43d80b6000d6b7601
Jose Luis Cercos Pita <jlcercos@gmail.com>
2012-07-31 18:37:10 +0200

    Added non OpenCL solver (will be developed first)

--- src/Mod/Ship/simRun/Sim/__init__.py
+++ src/Mod/Ship/simRun/Sim/__init__.py
@@ -21,4 +21,4 @@
 #*                                                                         *
 #***************************************************************************
 
-import initialization, Utils
+import initialization
