af79546ee1d3a5c500db66ca842886c495337092
Yorik van Havre <yorikvanhavre@users.sourceforge.net>
2012-08-15 12:20:19 -0300

    Arch: small optimizations

--- src/Mod/Arch/ArchStructure.py
+++ src/Mod/Arch/ArchStructure.py
@@ -222,14 +222,15 @@ class _Structure(ArchComponent.Component):
             # finalizing
             else:
                 if base:
-                    if base.isValid() and (not base.isNull()) and base.Solids:
-                        if base.Volume < 0:
-                            base.reverse()
-                        if base.Volume < 0:
-                            FreeCAD.Console.PrintError(str(translate("Arch","Couldn't compute the wall shape")))
-                            return
-                        base = base.removeSplitter()
-                        obj.Shape = base
+                    if not base.isNull():
+                        if base.isValid() and base.Solids:
+                            if base.Volume < 0:
+                                base.reverse()
+                            if base.Volume < 0:
+                                FreeCAD.Console.PrintError(str(translate("Arch","Couldn't compute the wall shape")))
+                                return
+                            base = base.removeSplitter()
+                            obj.Shape = base
                 if not DraftGeomUtils.isNull(pl):
                     obj.Placement = pl
     
