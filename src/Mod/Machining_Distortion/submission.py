4a6436e4c40393d3c0ac4976fa9a04bcd904cf8e
Joachim Zettler <dvdjimmy@users.sourceforge.net>
2012-08-22 20:54:21 +0200

    Implemented Thread based job submission

--- src/Mod/Machining_Distortion/submission.py
+++ src/Mod/Machining_Distortion/submission.py
@@ -9,50 +9,18 @@ from lsf_submission_gui import Ui_DialogSubmitwithLSF
 ##------------------------------------------------
 
 
+class ThreadWorker(QtCore.QThread):
+    updateProgressBar = QtCore.pyqtSignal(int)
+    finished_calcs = QtCore.pyqtSignal()
+    def __init__(self,current_working_directory):
+	super(ThreadWorker, self).__init__(parent=None)
+	self.working_directory = current_working_directory
 
-class Submission(QtGui.QDialog, Ui_DialogSubmitwithLSF):
-    
-    
-    def __init__(self,working_dir,parent=None):
-        QtGui.QDialog.__init__(self,parent)
-        self.setupUi(self)
-        self.working_directory = working_dir
-	self.number_of_jobs=0
-	self.jobFolders = []
-	self.setValues()
-        self.progressBar.setValue(0.0)
-	QtCore.QObject.connect(self, QtCore.SIGNAL("testSIGNAL"), self.progressBar.setValue)
+    def run(self):
+ 	print 'Worker Thread startet'
+	self.launch_calculation()
 	
-        QtCore.QObject.connect(self.start_calculation, QtCore.SIGNAL("clicked()"), self.launch_calculation)
-        QtCore.QObject.connect(self.close_dialog, QtCore.SIGNAL("clicked()"), self.quit_dialog)
-
-
-    def build_launch_script(self,current_dir):
-        #Now lets generate a LSF Job-File to be used by the Airbus Clusters
-        lsf_input = open (str(os.path.join(current_dir,"job.lsf")),"wb")
-        lsf_input.write("#!/bin/bash\n")
-        lsf_input.write("export CCX_NPROC=" + str(self.numbercpus.value() ) + "\n")
-        lsf_input.write("#BSUB -n "+ str(self.numbercpus.value()) + "\n")
-        lsf_input.write("#BSUB -W 10:00\n")
-        lsf_input.write("#BSUB -o %J.out\n")
-        lsf_input.write("#BSUB -e %J.err\n")
-        lsf_input.write("#BSUB -J calculix\n")
-        lsf_input.write("#BSUB -q loc_dev_par\n")
-        lsf_input.write(str("datadir=\"" + current_dir + "\""))
-        lsf_input.write("cd $datadir\n")
-        lsf_input.write("ccx -i geometry_fe_input\n")
-        lsf_input.close()
-        
-
-    def setValues(self):
-        #Get an Idea of how many jobs we have to calculate
-        for root,dirs,files in os.walk(str(self.working_directory)):
-            self.number_of_jobs += 1
-        #Subtract one job as the root itself is also printed 
-	self.number_of_jobs -=1
-        self.progressBar.setMinimum(0)
-        self.progressBar.setMaximum(self.number_of_jobs)
-
+	
     def launch_job(self,current_dir):
 	initial_directory = os.getcwd()
         #print initial_directory
@@ -85,9 +53,6 @@ class Submission(QtGui.QDialog, Ui_DialogSubmitwithLSF):
 	return running
 
     def launch_calculation(self):
-	#inactivate the buttons to avoid errors
-	self.start_calculation.setEnabled(False)
-	self.close_dialog.setEnabled(False)
         current_job_number = 0
         for root,dirs,files in os.walk(str(self.working_directory)):
             if 'geometry_fe_input.inp' in files:
@@ -102,12 +67,71 @@ class Submission(QtGui.QDialog, Ui_DialogSubmitwithLSF):
                         #Lets continue to the next job as this job seems to have problems
                         current_job_number += 1
 			print "No convergence found . Skipping over to next job"
-			self.emit(QtCore.SIGNAL("testSIGNAL"),current_job_number)
+			self.updateProgressBar.emit(current_job_number)
                         break
                 current_job_number += 1
-                self.emit(QtCore.SIGNAL("testSIGNAL"),current_job_number)
-		
+                self.updateProgressBar.emit(current_job_number)
+	
+	self.finished_calcs.emit()
+	
+
+
+
+class Submission(QtGui.QDialog, Ui_DialogSubmitwithLSF):
+
+    def __init__(self,working_dir,parent=None):
+	super(Submission, self).__init__(parent)
+        QtGui.QDialog.__init__(self,parent)
+        self.setupUi(self)
+        self.working_directory = working_dir
+	self.number_of_jobs=0
+	self.setValues()
+        self.progressBar.setValue(0.0)
+	self.thread = ThreadWorker(working_dir)
+	self.thread.updateProgressBar.connect(self.progressBar.setValue)
+	self.thread.finished_calcs.connect(self.setValuesAfterCalc)
+        self.start_calculation.clicked.connect(self.launchWorkingThread)
+        self.close_dialog.clicked.connect(self.quit_dialog)
+
+
+    def build_launch_script(self,current_dir):
+        #Now lets generate a LSF Job-File to be used by the Airbus Clusters
+        lsf_input = open (str(os.path.join(current_dir,"job.lsf")),"wb")
+        lsf_input.write("#!/bin/bash\n")
+        lsf_input.write("export CCX_NPROC=" + str(self.numbercpus.value() ) + "\n")
+        lsf_input.write("#BSUB -n "+ str(self.numbercpus.value()) + "\n")
+        lsf_input.write("#BSUB -W 10:00\n")
+        lsf_input.write("#BSUB -o %J.out\n")
+        lsf_input.write("#BSUB -e %J.err\n")
+        lsf_input.write("#BSUB -J calculix\n")
+        lsf_input.write("#BSUB -q loc_dev_par\n")
+        lsf_input.write(str("datadir=\"" + current_dir + "\""))
+        lsf_input.write("cd $datadir\n")
+        lsf_input.write("ccx -i geometry_fe_input\n")
+        lsf_input.close()
+        
+
+    def setValues(self):
+        #Get an Idea of how many jobs we have to calculate
+        for root,dirs,files in os.walk(str(self.working_directory)):
+            self.number_of_jobs += 1
+        #Subtract one job as the root itself is also printed 
+	self.number_of_jobs -=1
+        self.progressBar.setMinimum(0)
+        self.progressBar.setMaximum(self.number_of_jobs)
+
+    
+
+    def launchWorkingThread(self):
+	#inactivate the buttons to avoid errors
+	self.start_calculation.setEnabled(False)
+	self.close_dialog.setEnabled(False)
+	self.thread.start()
+
+	
+    def setValuesAfterCalc(self):
 	self.close_dialog.setEnabled(True)
+	
                 
     def quit_dialog(self):
         self.close()
