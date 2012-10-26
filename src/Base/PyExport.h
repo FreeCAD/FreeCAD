15f38e1a622e4253c20e03bd30a3a3cb127be05e
wmayer <wmayer@users.sourceforge.net>
2012-10-26 12:59:43 +0200

    Fixes for MacOSX

--- src/Base/PyExport.h
+++ src/Base/PyExport.h
@@ -40,6 +40,15 @@
 #endif
 
 #include <Python.h>
+#ifdef FC_OS_MACOSX
+#undef toupper
+#undef tolower
+#undef isupper
+#undef islower
+#undef isspace
+#undef isalpha
+#undef isalnum
+#endif
 
 namespace Base
 {
