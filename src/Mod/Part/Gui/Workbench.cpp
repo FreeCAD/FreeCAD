b8ca20504af56092ea2c5d43aa67029c5b7e2d95
wmayer <wmayer@users.sourceforge.net>
2012-11-24 22:46:16 +0100

    Offset function

--- src/Mod/Part/Gui/Workbench.cpp
+++ src/Mod/Part/Gui/Workbench.cpp
@@ -72,7 +72,7 @@ Gui::MenuItem* Workbench::setupMenuBar() const
           << "Part_SimpleCopy" << "Part_RefineShape" << "Part_CheckGeometry" << "Separator"
           << "Part_Boolean" << "Part_CrossSections" << "Part_Extrude"
           << "Part_Revolve" << "Part_Mirror" << "Part_Fillet" << "Part_Chamfer"
-          << "Part_RuledSurface" << "Part_Loft" << "Part_Sweep";
+          << "Part_RuledSurface" << "Part_Loft" << "Part_Sweep" << "Part_Offset";
 
     return root;
 }
