5f43ef1a2a6ef57df468e46cce142ac992bce05b
jriegel <FreeCAD@juergen-riegel.net>
2013-02-24 21:57:35 +0100

    Add PropertyIntegerSet and some classes to FEM

--- src/Mod/Fem/Gui/Command.cpp
+++ src/Mod/Fem/Gui/Command.cpp
@@ -72,6 +72,103 @@ bool CmdFemCreateFromShape::isActive(void)
 }
 
 
+
+
+
+//DEF_STD_CMD_A(CmdFemDefineNodesSet);
+//
+//
+//void DefineNodesCallback(void * ud, SoEventCallback * n)
+//{
+//    // show the wait cursor because this could take quite some time
+//    Gui::WaitCursor wc;
+//
+//    // When this callback function is invoked we must in either case leave the edit mode
+//    Gui::View3DInventorViewer* view  = reinterpret_cast<Gui::View3DInventorViewer*>(n->getUserData());
+//    view->setEditing(false);
+//    view->removeEventCallback(SoMouseButtonEvent::getClassTypeId(), clipMeshCallback,ud);
+//    n->setHandled();
+//
+//    SbBool clip_inner;
+//    std::vector<SbVec2f> clPoly = view->getGLPolygon(&clip_inner);
+//    if (clPoly.size() < 3)
+//        return;
+//    if (clPoly.front() != clPoly.back())
+//        clPoly.push_back(clPoly.front());
+//
+//    std::vector<Gui::ViewProvider*> views = view->getViewProvidersOfType(ViewProviderMesh::getClassTypeId());
+//    if (!views.empty()) {
+//        Gui::Application::Instance->activeDocument()->openCommand("Cut");
+//        for (std::vector<Gui::ViewProvider*>::iterator it = views.begin(); it != views.end(); ++it) {
+//            ViewProviderMesh* that = static_cast<ViewProviderMesh*>(*it);
+//            if (that->getEditingMode() > -1) {
+//                that->finishEditing();
+//                that->cutMesh(clPoly, *view, clip_inner);
+//            }
+//        }
+//
+//        Gui::Application::Instance->activeDocument()->commitCommand();
+//
+//        view->render();
+//    }
+//}
+//
+//
+//
+//CmdMeshPolyCut::CmdMeshPolyCut()
+//  : Command("Mesh_PolyCut")
+//{
+//    sAppModule    = "Mesh";
+//    sGroup        = QT_TR_NOOP("Mesh");
+//    sMenuText     = QT_TR_NOOP("Cut mesh");
+//    sToolTipText  = QT_TR_NOOP("Cuts a mesh with a picked polygon");
+//    sWhatsThis    = "Mesh_PolyCut";
+//    sStatusTip    = QT_TR_NOOP("Cuts a mesh with a picked polygon");
+//    sPixmap       = "mesh_cut";
+//}
+//
+//void CmdMeshPolyCut::activated(int iMsg)
+//{
+//    std::vector<App::DocumentObject*> docObj = Gui::Selection().getObjectsOfType(Mesh::Feature::getClassTypeId());
+//    for (std::vector<App::DocumentObject*>::iterator it = docObj.begin(); it != docObj.end(); ++it) {
+//        if (it == docObj.begin()) {
+//            Gui::Document* doc = getActiveGuiDocument();
+//            Gui::MDIView* view = doc->getActiveView();
+//            if (view->getTypeId().isDerivedFrom(Gui::View3DInventor::getClassTypeId())) {
+//                Gui::View3DInventorViewer* viewer = ((Gui::View3DInventor*)view)->getViewer();
+//                viewer->setEditing(true);
+//                viewer->startSelection(Gui::View3DInventorViewer::Clip);
+//                viewer->addEventCallback(SoMouseButtonEvent::getClassTypeId(), MeshGui::ViewProviderMeshFaceSet::clipMeshCallback);
+//            }
+//            else {
+//                return;
+//            }
+//        }
+//
+//        Gui::ViewProvider* pVP = getActiveGuiDocument()->getViewProvider(*it);
+//        if (pVP->isVisible())
+//            pVP->startEditing();
+//    }
+//}
+//
+//bool CmdMeshPolyCut::isActive(void)
+//{
+//    // Check for the selected mesh feature (all Mesh types)
+//    if (getSelection().countObjectsOfType(Mesh::Feature::getClassTypeId()) == 0)
+//        return false;
+//
+//    Gui::MDIView* view = Gui::getMainWindow()->activeWindow();
+//    if (view && view->isDerivedFrom(Gui::View3DInventor::getClassTypeId())) {
+//        Gui::View3DInventorViewer* viewer = static_cast<Gui::View3DInventor*>(view)->getViewer();
+//        return !viewer->isEditing();
+//    }
+//
+//    return false;
+//}
+
+//--------------------------------------------------------------------------------------
+
+
 void CreateFemCommands(void)
 {
     Gui::CommandManager &rcCmdMgr = Gui::Application::Instance->commandManager();
