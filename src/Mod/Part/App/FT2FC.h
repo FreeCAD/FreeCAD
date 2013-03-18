c3da34736f5cbfd58670e302eac660880105ce97
WandererFan <wandererfan@gmail.com>
2013-03-18 18:05:38 -0400

    Handle Py_UNICODE objects & ASCII Py_Strings

--- src/Mod/Part/App/FT2FC.h
+++ src/Mod/Part/App/FT2FC.h
@@ -2,9 +2,16 @@
 
 #ifndef FT2FC_H
 #define FT2FC_H
-// public function
-std::vector <std::vector <TopoDS_Wire> > FT2FC(const std::string shapestring,
+// public functions
+std::vector <std::vector <TopoDS_Wire> > FT2FCc(const char *cstring,
                           const std::string FontPath, 
+                          const std::string FontName,
+                          const float stringheight,
+                          const int tracking);
+std::vector <std::vector <TopoDS_Wire> > FT2FCpu(const Py_UNICODE *unichars,
+//std::vector <std::vector <TopoDS_Wire> > FT2FCpu(const char16_t *unichars,
+                          const size_t length,
+                          const std::string FontPath,
                           const std::string FontName,
                           const float stringheight,
                           const int tracking);
