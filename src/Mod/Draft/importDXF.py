367a8f9aee36d6d76a8e9396774e9a378ba9a089
Yorik van Havre <yorik@uncreated.net>
2013-06-20 21:32:41 -0300

    Draft: Fixed license blocks

--- src/Mod/Draft/importDXF.py
+++ src/Mod/Draft/importDXF.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf8 -*-
 
 #***************************************************************************
@@ -6,7 +5,7 @@
 #*   Copyright (c) 2009 Yorik van Havre <yorik@uncreated.net>              *
 #*                                                                         *
 #*   This program is free software; you can redistribute it and/or modify  *
-#*   it under the terms of the GNU Lesser General Public License (GPL)     *
+#*   it under the terms of the GNU Lesser General Public License (LGPL)    *
 #*   as published by the Free Software Foundation; either version 2 of     *
 #*   the License, or (at your option) any later version.                   *
 #*   for detail see the LICENCE text file.                                 *
