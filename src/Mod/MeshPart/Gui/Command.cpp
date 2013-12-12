1ec66ed12aa3b155a60ee9a146d4eba84c6aabc3
jmaustpc <jmaustpc@users.sourceforge.net>
2013-12-13 00:17:35 +1100

    Add icons to menus for mesh to shape and shape to mesh
    Mesh to shape icon for the Part workbench Part menu
    Shape to mesh icon for the Mesh design Workbench Meshes menu

--- src/Mod/MeshPart/Gui/Command.cpp
+++ src/Mod/MeshPart/Gui/Command.cpp
@@ -48,7 +48,7 @@ CmdMeshPartMesher::CmdMeshPartMesher()
     sToolTipText  = QT_TR_NOOP("Tessellate shape");
     sWhatsThis    = sToolTipText;
     sStatusTip    = sToolTipText;
-  //sPixmap       = "MeshPart_Mesher";
+    sPixmap       = "Mesh_Mesh_from_Shape.svg";
 }
 
 void CmdMeshPartMesher::activated(int iMsg)
