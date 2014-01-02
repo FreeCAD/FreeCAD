8ca008d992d8ba40fb1ba14732e1df2a9ac4884b
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2014-01-02 17:01:28 +0100

    Created an svg icon for the examples loader

--- src/Mod/Ship/ShipGui.py
+++ src/Mod/Ship/ShipGui.py
@@ -37,7 +37,7 @@ class LoadExample:
     def GetResources(self):
         MenuText = QtCore.QT_TRANSLATE_NOOP('Ship_LoadExample', 'Load an example ship geometry')
         ToolTip  = QtCore.QT_TRANSLATE_NOOP('Ship_LoadExample', 'Load an example ship geometry able to be converted into a ship.')
-        return {'Pixmap' : 'LoadIco', 'MenuText': MenuText, 'ToolTip': ToolTip} 
+        return {'Pixmap' : 'Ship_Load', 'MenuText': MenuText, 'ToolTip': ToolTip} 
 
 class CreateShip: 
     def Activated(self):
