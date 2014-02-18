6edfd09dcb34a909310d7c18547b2cae9bb7239c
Cercos-Pita J.L <jlcercos@gmail.com>
2014-02-18 10:13:13 +0100

    Cleared unused icons

--- src/Mod/Ship/Ship_rc.py
+++ src/Mod/Ship/Ship_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: dom feb 16 13:42:12 2014
+# Created: mar feb 18 10:12:13 2014
 #      by: The Resource Compiler for PyQt (Qt v4.8.4)
 #
 # WARNING! All changes made in this file will be lost!
