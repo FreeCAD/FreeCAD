e6bd7e8de3527366c35254739c25a05781f92ab0
wmayer <wmayer@users.sourceforge.net>
2014-03-15 17:11:44 +0100

    + fixes #0001188: Change the word 'datum' as the label for the entry field for arcs and circles.

--- src/Mod/Sketcher/Gui/EditDatumDialog.cpp
+++ src/Mod/Sketcher/Gui/EditDatumDialog.cpp
@@ -45,6 +45,8 @@
 
 using namespace SketcherGui;
 
+/* TRANSLATOR SketcherGui::EditDatumDialog */
+
 EditDatumDialog::EditDatumDialog(ViewProviderSketch* vp, int ConstrNbr) : ConstrNbr(ConstrNbr)
 {
     sketch = vp->getSketchObject();
@@ -91,12 +93,23 @@ void EditDatumDialog::exec(bool atCursor)
         double datum = Constr->Value;
         Base::Quantity init_val;
 
-        if (Constr->Type == Sketcher::Angle){
+        if (Constr->Type == Sketcher::Angle) {
             datum = Base::toDegrees<double>(datum);
+            dlg.setWindowTitle(tr("Insert angle"));
             init_val.setUnit(Base::Unit::Angle);
+            ui_ins_datum.label->setText(tr("Angle:"));
             ui_ins_datum.labelEdit->setParamGrpPath(QByteArray("User parameter:BaseApp/History/SketcherAngle"));
-        }else{
+        }
+        else if (Constr->Type == Sketcher::Radius) {
+            dlg.setWindowTitle(tr("Insert radius"));
             init_val.setUnit(Base::Unit::Length);
+            ui_ins_datum.label->setText(tr("Radius:"));
+            ui_ins_datum.labelEdit->setParamGrpPath(QByteArray("User parameter:BaseApp/History/SketcherLength"));
+        }
+        else {
+            dlg.setWindowTitle(tr("Insert length"));
+            init_val.setUnit(Base::Unit::Length);
+            ui_ins_datum.label->setText(tr("Length:"));
             ui_ins_datum.labelEdit->setParamGrpPath(QByteArray("User parameter:BaseApp/History/SketcherLength"));
         }
 
