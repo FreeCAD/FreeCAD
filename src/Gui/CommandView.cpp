df50c8c9f3be925d9e6f0da318818c01d3608303
Ian Rees <ian.rees@gmail.com>
2014-06-30 23:02:50 +1200

    Added a cursor that gets applied when the user has the "Measure distance" tool selected.

--- src/Gui/CommandView.cpp
+++ src/Gui/CommandView.cpp
@@ -2221,6 +2221,45 @@ StdCmdMeasureDistance::StdCmdMeasureDistance()
     eType         = Alter3DView;
 }
 
+// Yay for cheezy drawings!
+/* XPM */
+static const char * cursor_ruler[] = {
+"32 32 3 1",
+" 	c None",
+".	c #FFFFFF",
+"+	c #FF0000",
+"      .                         ",
+"      .                         ",
+"      .                         ",
+"      .                         ",
+"      .                         ",
+"                                ",
+".....   .....                   ",
+"                                ",
+"      .                         ",
+"      .                         ",
+"      .        ++               ",
+"      .       +  +              ",
+"      .      +   ++             ",
+"            +   +  +            ",
+"           +   +    +           ",
+"          +   +     ++          ",
+"          +        +  +         ",
+"           +           +        ",
+"            +         + +       ",
+"             +       +   +      ",
+"              +           +     ",
+"               +         + +    ",
+"                +       +   +   ",
+"                 +           +  ",
+"                  +         + + ",
+"                   +       +  ++",
+"                    +     +   + ",
+"                     +       +  ",
+"                      +     +   ",
+"                       +   +    ",
+"                        + +     ",
+"                         +      "};
 void StdCmdMeasureDistance::activated(int iMsg)
 {
     Gui::Document* doc = Gui::Application::Instance->activeDocument();
@@ -2228,6 +2267,10 @@ void StdCmdMeasureDistance::activated(int iMsg)
     if (view) {
         Gui::View3DInventorViewer* viewer = view->getViewer();
         viewer->setEditing(true);
+        viewer->setEditingCursor(QCursor(QPixmap(cursor_ruler), 7, 7));
+	
+	// Derives from QObject and we have a parent object, so we don't
+	// require a delete.
         PointMarker* marker = new PointMarker(viewer);
         viewer->addEventCallback(SoMouseButtonEvent::getClassTypeId(),
             ViewProviderMeasureDistance::measureDistanceCallback, marker);
