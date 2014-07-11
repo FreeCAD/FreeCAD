e5015f83881b2fc8407b7801fa77989bbd9c4e91
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2014-07-11 14:58:13 +0200

    Added helpers for mass, time and angle units

--- src/Mod/Ship/shipUtils/Units.py
+++ src/Mod/Ship/shipUtils/Units.py
@@ -27,6 +27,9 @@ import Units
 
 # Systems of length units
 LENGTH_UNITS = ('mm', 'm', 'in', 'in')
+MASS_UNITS = ('kg', 'kg', 'lb', 'lb')
+TIME_UNITS = ('s', 's', 's', 's')
+ANGLE_UNITS = ('deg', 'deg', 'deg', 'deg')
 
 
 def getLengthUnits():
@@ -40,3 +43,42 @@ def getLengthFormat():
     decimals = param.GetInt("Decimals", 2)
     units_id = param.GetInt('UserSchema', 0)
     return '{0:.' + str(decimals) + 'f} ' + LENGTH_UNITS[units_id]
+
+
+def getMassUnits():
+    param = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Units")
+    units_id = param.GetInt('UserSchema', 0)
+    return MASS_UNITS[units_id]
+
+
+def getMassFormat():
+    param = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Units")
+    decimals = param.GetInt("Decimals", 2)
+    units_id = param.GetInt('UserSchema', 0)
+    return '{0:.' + str(decimals) + 'f} ' + MASS_UNITS[units_id]
+
+
+def getTimeUnits():
+    param = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Units")
+    units_id = param.GetInt('UserSchema', 0)
+    return TIME_UNITS[units_id]
+
+
+def getTimeFormat():
+    param = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Units")
+    decimals = param.GetInt("Decimals", 2)
+    units_id = param.GetInt('UserSchema', 0)
+    return '{0:.' + str(decimals) + 'f} ' + TIME_UNITS[units_id]
+
+
+def getAngleUnits():
+    param = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Units")
+    units_id = param.GetInt('UserSchema', 0)
+    return ANGLE_UNITS[units_id]
+
+
+def getAngleFormat():
+    param = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Units")
+    decimals = param.GetInt("Decimals", 2)
+    units_id = param.GetInt('UserSchema', 0)
+    return '{0:.' + str(decimals) + 'f} ' + ANGLE_UNITS[units_id]
