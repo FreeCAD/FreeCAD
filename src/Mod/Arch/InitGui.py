b17f401c5c9de9e560a311a76c50ec1c13b7b92a
wood galaxy <wood.galaxy@gmail.com>
2014-11-11 19:49:05 +0100

    Add arch cutComponent feature

--- src/Mod/Arch/InitGui.py
+++ src/Mod/Arch/InitGui.py
@@ -74,7 +74,7 @@ class ArchWorkbench(Workbench):
                      "Arch_Window","Arch_Roof","Arch_Axis",
                      "Arch_SectionPlane","Arch_Space","Arch_Stairs",
                      "Arch_Panel","Arch_Equipment",
-                     "Arch_Frame","Arch_Add","Arch_Remove","Arch_Survey"]
+                     "Arch_Frame","Arch_CutPlane","Arch_Add","Arch_Remove","Arch_Survey"]
         self.utilities = ["Arch_SplitMesh","Arch_MeshToShape",
                      "Arch_SelectNonSolidMeshes","Arch_RemoveShape",
                      "Arch_CloseHoles","Arch_MergeWalls","Arch_Check",
