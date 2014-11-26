e3e2667252c7f079d8e842a54814457421d877d2
wmayer <wmayer@users.sourceforge.net>
2014-11-26 13:03:59 +0100

    + remove some more autotools files

--- build.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-# We almost need to specify the switches for the OpenCascade library to make
-# configure running successfully. 
-# CASROOT=${CASROOT:-/opt/OpenCASCADE6.2.0/ros}
-#./configure CXXFLAGS="-fno-strict-aliasing" LDFLAGS="-Wl,-z,defs" --with-occ-include=$CASROOT/inc --with-occ-lib=$CASROOT/Linux/lib
-./configure CXXFLAGS="-fno-strict-aliasing -Wno-write-strings" LDFLAGS="-Wl,-z,defs"
-
