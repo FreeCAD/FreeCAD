8f1a0c3eb37ec3226b581b36ba17bf0938cdd701
balazs-bamer <kolaloka@freemail.hu>
2015-01-13 14:49:55 +0100

    fill type radio buttons - incomplete
    I tried to implement a dialog with fill type radio buttons, but no
    success yet.

---
+++ src/Mod/Surface/Gui/BSurf.h
@@ -0,0 +1,97 @@
+/***************************************************************************
+ *   Copyright (c) 2015 Balázs Bámer                                       *
+ *                                                                         *
+ *   This file is part of the FreeCAD CAx development system.              *
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Library General Public           *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2 of the License, or (at your option) any later version.      *
+ *                                                                         *
+ *   This library  is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU Library General Public License for more details.                  *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this library; see the file COPYING.LIB. If not,    *
+ *   write to the Free Software Foundation, Inc., 59 Temple Place,         *
+ *   Suite 330, Boston, MA  02111-1307, USA                                *
+ *                                                                         *
+ ***************************************************************************/
+
+// Part::CrossSections.*
+
+#ifndef SURFACE_GUI_BSURF_H
+#define SURFACE_GUI_BSURF_H
+
+#include <Gui/TaskView/TaskDialog.h>
+#include <Gui/TaskView/TaskView.h>
+#include <Base/BoundBox.h>
+#include <QDialog>
+#include <QPointer>
+#include "../FillType.h"
+#include "ui_BSurf.h"
+
+namespace SurfaceGui
+{
+    class ViewProviderBSurf;
+
+    class BSurf : public QDialog
+    {
+        Q_OBJECT
+    protected:
+        filltype_t fillType;
+
+    public:
+        BSurf(const Base::BoundBox3d& bb, QWidget* parent = 0, Qt::WFlags fl = 0);
+        ~BSurf();
+        void accept();
+        void apply();
+
+    protected:
+        void changeEvent(QEvent *e);
+
+    private Q_SLOTS:
+        void on_fillType_stretch_clicked();
+        void on_fillType_coons_clicked();
+        void on_fillType_curved_clicked();
+        filltype_t getFillType() const;
+
+/*    private:
+        std::vector<double> getPlanes() const;
+        void calcPlane(Plane, double);
+        void calcPlanes(Plane);
+        void makePlanes(Plane, const std::vector<double>&, double[4]);
+        Plane plane() const;*/
+
+    private:
+        Ui_DlgBSurf* ui;
+        Base::BoundBox3d bbox;
+        ViewProviderBSurf* vp;
+//        QPointer<Gui::View3DInventor> view;
+    };
+
+    class TaskBSurf : public Gui::TaskView::TaskDialog
+    {
+        Q_OBJECT
+
+    public:
+        TaskBSurf(const Base::BoundBox3d& bb);
+        ~TaskBSurf();
+
+    public:
+        bool accept();
+        void clicked(int id);
+
+        virtual QDialogButtonBox::StandardButtons getStandardButtons() const
+        { return QDialogButtonBox::Ok | QDialogButtonBox::Apply | QDialogButtonBox::Cancel; }
+
+    private:
+        BSurf* widget;
+        Gui::TaskView::TaskBox* taskbox;
+    };
+
+} //namespace Surface
+
+#endif // SURFACE_GUI_BSURF_H
