75dab66916f277b950554dc694c7f08a8445cc15
Eivind Kvedalen <eivind@kvedalen.name>
2015-02-03 00:32:00 +0100

    Added InterpreterSingleton::getValue(...) function.

--- src/Base/Interpreter.cpp
+++ src/Base/Interpreter.cpp
@@ -496,6 +496,28 @@ void InterpreterSingleton::runMethod(PyObject *pobject, const char *method,
     }
 }
 
+PyObject * InterpreterSingleton::getValue(const char * key, const char * result_var)
+{
+    PyObject *module, *dict, *presult;          /* "exec code in d, d" */
+
+    PyGILStateLocker locker;
+    module = PP_Load_Module("__main__");         /* get module, init python */
+    if (module == NULL)
+        throw PyException();                         /* not incref'd */
+    dict = PyModule_GetDict(module);            /* get dict namespace */
+    if (dict == NULL)
+        throw PyException();                           /* not incref'd */
+
+
+    presult = PyRun_String(key, Py_file_input, dict, dict); /* eval direct */
+    if (!presult) {
+        throw PyException();
+    }
+    Py_DECREF(presult);
+
+    return PyObject_GetAttrString(module, result_var);
+}
+
 void InterpreterSingleton::dbgObserveFile(const char* sFileName)
 {
     if (sFileName)
