ceef22330972492fcd3bf69fb67f15365fb7f087
wmayer <wmayer@users.sourceforge.net>
2015-02-28 12:42:27 +0100

    + fix linker error with MSVC 2012

--- src/Mod/Drawing/Gui/TaskOrthoViews.cpp
+++ src/Mod/Drawing/Gui/TaskOrthoViews.cpp
@@ -48,7 +48,7 @@ using namespace std;
 #endif
 
 
-#if _MSC_VER <= 1500
+#if _MSC_VER <= 1700
 // maybe in the c++ standard later, older compiler don't have round()
 double round(double r) {
     return (r > 0.0) ? floor(r + 0.5) : ceil(r - 0.5);
