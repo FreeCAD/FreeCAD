b82a28cd8939aacf892ac36dc3a204131f6b4a22
wmayer <wmayer@users.sourceforge.net>
2015-03-23 19:56:07 +0100

    + re-enable check for active window for spaceball events

--- src/Gui/GuiApplicationNativeEventAware.cpp
+++ src/Gui/GuiApplicationNativeEventAware.cpp
@@ -100,9 +100,8 @@ void Gui::GUIApplicationNativeEventAware::initSpaceball(QMainWindow *window)
 
 bool Gui::GUIApplicationNativeEventAware::processSpaceballEvent(QObject *object, QEvent *event)
 {
-    // Maybe this causes the regression reported in #0001970. So to test it it will be commented out for now
-    //if (!activeWindow())
-    //    return true;
+    if (!activeWindow())
+        return true;
 
     QApplication::notify(object, event);
     if (event->type() == Spaceball::MotionEvent::MotionEventType)
