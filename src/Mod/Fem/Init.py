d474e950ca91932fdfc8a0e24c72eed5889764fa
Przemo Firszt <przemo@firszt.eu>
2015-04-23 22:13:14 +0100

    FEM: Rename CalculixLib to ccxFrdReader
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- src/Mod/Fem/Init.py
+++ src/Mod/Fem/Init.py
@@ -29,5 +29,5 @@
 FreeCAD.addExportType("TetGen file (*.poly)","convert2TetGen") 
 FreeCAD.addImportType("FEM formats (*.unv *.med *.dat *.bdf)","Fem")
 FreeCAD.addExportType("FEM formats (*.unv *.med *.dat *.inp)","Fem")
-FreeCAD.addImportType("CalculiX result (*.frd)","CalculixLib")
+FreeCAD.addImportType("CalculiX result (*.frd)","ccxFrdReader")
 FreeCAD.addImportType("Abaqus file (*.inp)","FemGui")
