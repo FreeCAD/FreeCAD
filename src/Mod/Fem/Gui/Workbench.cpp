749e772d85d6aa081167c80063a6084434ef3532
Przemo Firszt <przemo@firszt.eu>
2015-04-30 22:13:42 +0100

    FEM: Add Purge FEM results button
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- src/Mod/Fem/Gui/Workbench.cpp
+++ src/Mod/Fem/Gui/Workbench.cpp
@@ -69,6 +69,7 @@ Gui::ToolBarItem* Workbench::setupToolBars() const
          << "Fem_ConstraintPulley"
          << "Separator"
          << "Fem_MechanicalJobControl"
+         << "Fem_PurgeResults"
          << "Fem_ShowResult";
     return root;
 }
@@ -94,6 +95,7 @@ Gui::MenuItem* Workbench::setupMenuBar() const
          << "Fem_ConstraintPulley"
          << "Separator"
          << "Fem_MechanicalJobControl"
+         << "Fem_PurgeResults"
          << "Fem_ShowResult";
 
     return root;
