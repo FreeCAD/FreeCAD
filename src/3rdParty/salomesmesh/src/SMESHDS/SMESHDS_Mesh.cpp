9a7b5d1607e96fb9acdca4560ddeac1266b6a64a
Alexander Golubev <fatzer2@gmail.com>
2015-05-17 17:28:00 +0300

    salomesmesh: suppress several warnings
    Warning was like:
    src/3rdParty/salomesmesh/src/SMESHDS/SMESHDS_Mesh.cpp|1028 col 5| warning: add explicit braces to avoid dangling else [-Wdangling-else]
    src/3rdParty/salomesmesh/src/SMESH/SMESH_MeshEditor.cpp|7377 col 27| warning: '&&' within '||' [-Wlogical-op-parentheses]

--- src/3rdParty/salomesmesh/src/SMESHDS/SMESHDS_Mesh.cpp
+++ src/3rdParty/salomesmesh/src/SMESHDS/SMESHDS_Mesh.cpp
@@ -1022,11 +1022,12 @@ void SMESHDS_Mesh::UnSetMeshElementOnShape(const SMDS_MeshElement * elem,
   int Index = myIndexToShape.FindIndex(S);
 
   map<int,SMESHDS_SubMesh*>::iterator it = myShapeIndexToSubMesh.find( Index );
-  if ( it != myShapeIndexToSubMesh.end() )
+  if ( it != myShapeIndexToSubMesh.end() ) {
     if ( elem->GetType() == SMDSAbs_Node )
       it->second->RemoveNode( static_cast<const SMDS_MeshNode* >( elem ), /*deleted=*/false );
     else
       it->second->RemoveElement( elem, /*deleted=*/false );
+  }
 }
 
 //=======================================================================
