eef340f19cbb0469b7091c9de817b083a93d8596
wmayer <wmayer@users.sourceforge.net>
2015-05-30 20:30:12 +0200

    + fix whitespaces

--- src/Mod/Sketcher/Gui/CommandAlterGeometry.cpp
+++ src/Mod/Sketcher/Gui/CommandAlterGeometry.cpp
@@ -46,7 +46,7 @@ using namespace SketcherGui;
 using namespace Sketcher;
 
 bool isAlterGeoActive(Gui::Document *doc)
-{   
+{
    if (doc) {
         // checks if a Sketch Viewprovider is in Edit
         if (doc->getInEdit() && doc->getInEdit()->isDerivedFrom
@@ -54,8 +54,8 @@ bool isAlterGeoActive(Gui::Document *doc)
                 return true;
         }
     }
-    return false;
 
+    return false;
 }
 
 namespace SketcherGui {
@@ -85,18 +85,18 @@ void CmdSketcherToggleConstruction::activated(int iMsg)
     if(Gui::Selection().countObjectsOfType(Sketcher::SketchObject::getClassTypeId()) == 0){
 
         Gui::CommandManager &rcCmdMgr = Gui::Application::Instance->commandManager();
-        
+
         if(geometryCreationMode==Construction) {
             geometryCreationMode=Normal;
-            
+
             rcCmdMgr.getCommandByName("Sketcher_CreateLine")->getAction()->setIcon(
                 Gui::BitmapFactory().pixmap("Sketcher_CreateLine"));
             rcCmdMgr.getCommandByName("Sketcher_CreateRectangle")->getAction()->setIcon(
                 Gui::BitmapFactory().pixmap("Sketcher_CreateRectangle"));
             rcCmdMgr.getCommandByName("Sketcher_CreatePolyline")->getAction()->setIcon(
-                Gui::BitmapFactory().pixmap("Sketcher_CreatePolyline"));     
+                Gui::BitmapFactory().pixmap("Sketcher_CreatePolyline"));
             rcCmdMgr.getCommandByName("Sketcher_CreateSlot")->getAction()->setIcon(
-                Gui::BitmapFactory().pixmap("Sketcher_CreateSlot")); 
+                Gui::BitmapFactory().pixmap("Sketcher_CreateSlot"));
             // Comp commands require a distinctive treatment
             Gui::ActionGroup* pcAction = qobject_cast<Gui::ActionGroup*>(rcCmdMgr.getCommandByName("Sketcher_CompCreateArc")->getAction());
             QList<QAction*> a = pcAction->actions();
@@ -112,7 +112,7 @@ void CmdSketcherToggleConstruction::activated(int iMsg)
             index = pcAction->property("defaultAction").toInt();
             a[0]->setIcon(Gui::BitmapFactory().pixmap("Sketcher_CreateEllipse"));
             a[1]->setIcon(Gui::BitmapFactory().pixmap("Sketcher_CreateEllipse_3points"));
-            a[2]->setIcon(Gui::BitmapFactory().pixmap("Sketcher_Elliptical_Arc"));        
+            a[2]->setIcon(Gui::BitmapFactory().pixmap("Sketcher_Elliptical_Arc"));
             rcCmdMgr.getCommandByName("Sketcher_CompCreateConic")->getAction()->setIcon(
                 index==0?Gui::BitmapFactory().pixmap("Sketcher_CreateEllipse"):
                 index==1?Gui::BitmapFactory().pixmap("Sketcher_CreateEllipse_3points"):
@@ -135,17 +135,17 @@ void CmdSketcherToggleConstruction::activated(int iMsg)
             a[2]->setIcon(Gui::BitmapFactory().pixmap("Sketcher_CreatePentagon"));
             a[3]->setIcon(Gui::BitmapFactory().pixmap("Sketcher_CreateHexagon"));
             a[4]->setIcon(Gui::BitmapFactory().pixmap("Sketcher_CreateHeptagon"));
-            a[5]->setIcon(Gui::BitmapFactory().pixmap("Sketcher_CreateOctagon"));        
+            a[5]->setIcon(Gui::BitmapFactory().pixmap("Sketcher_CreateOctagon"));
             rcCmdMgr.getCommandByName("Sketcher_CompCreateRegularPolygon")->getAction()->setIcon(
                 index==0?Gui::BitmapFactory().pixmap("Sketcher_CreateTriangle"):
                 index==1?Gui::BitmapFactory().pixmap("Sketcher_CreateSquare"):
                 index==2?Gui::BitmapFactory().pixmap("Sketcher_CreatePentagon"):
                 index==3?Gui::BitmapFactory().pixmap("Sketcher_CreateHexagon"):
-                index==4?Gui::BitmapFactory().pixmap("Sketcher_CreateHeptagon"):          
-                Gui::BitmapFactory().pixmap("Sketcher_CreateOctagon"));      
+                index==4?Gui::BitmapFactory().pixmap("Sketcher_CreateHeptagon"):
+                Gui::BitmapFactory().pixmap("Sketcher_CreateOctagon"));
         }
         else {
-            geometryCreationMode=Construction;       
+            geometryCreationMode=Construction;
             
             rcCmdMgr.getCommandByName("Sketcher_CreateLine")->getAction()->setIcon(
                 Gui::BitmapFactory().pixmap("Sketcher_CreateLine_Constr"));
@@ -194,17 +194,15 @@ void CmdSketcherToggleConstruction::activated(int iMsg)
             a[2]->setIcon(Gui::BitmapFactory().pixmap("Sketcher_CreatePentagon_Constr"));
             a[3]->setIcon(Gui::BitmapFactory().pixmap("Sketcher_CreateHexagon_Constr"));
             a[4]->setIcon(Gui::BitmapFactory().pixmap("Sketcher_CreateHeptagon_Constr"));
-            a[5]->setIcon(Gui::BitmapFactory().pixmap("Sketcher_CreateOctagon_Constr"));        
+            a[5]->setIcon(Gui::BitmapFactory().pixmap("Sketcher_CreateOctagon_Constr"));
             rcCmdMgr.getCommandByName("Sketcher_CompCreateRegularPolygon")->getAction()->setIcon(
                 index==0?Gui::BitmapFactory().pixmap("Sketcher_CreateTriangle_Constr"):
                 index==1?Gui::BitmapFactory().pixmap("Sketcher_CreateSquare_Constr"):
                 index==2?Gui::BitmapFactory().pixmap("Sketcher_CreatePentagon_Constr"):
                 index==3?Gui::BitmapFactory().pixmap("Sketcher_CreateHexagon_Constr"):
-                index==4?Gui::BitmapFactory().pixmap("Sketcher_CreateHeptagon_Constr"):          
+                index==4?Gui::BitmapFactory().pixmap("Sketcher_CreateHeptagon_Constr"):
                 Gui::BitmapFactory().pixmap("Sketcher_CreateOctagon_Constr"));
-                
-        }    
-    
+        }
     }
     else // there was a selection, so operate in toggle mode.
     {
