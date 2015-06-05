99d07fbd9a7585921d42df786b8ba831b601d7bc
Przemo Firszt <przemo@firszt.eu>
2015-06-05 12:39:52 +0100

    FEM: rename obj to move descriptive analysis_obj
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- src/Mod/Fem/ccxInpWriter.py
+++ src/Mod/Fem/ccxInpWriter.py
@@ -6,13 +6,13 @@ import sys
 
 
 class inp_writer:
-    def __init__(self, obj, mesh_obj, mat_obj, fixed_obj, force_obj, pressure_obj):
+    def __init__(self, analysis_obj, mesh_obj, mat_obj, fixed_obj, force_obj, pressure_obj):
         self.mesh_object = mesh_obj
         self.material_objects = mat_obj
         self.fixed_objects = fixed_obj
         self.force_objects = force_obj
         self.pressure_objects = pressure_obj
-        self.dir_name = FreeCAD.ActiveDocument.TransientDir.replace('\\', '/') + '/FemAnl_' + obj.Uid[-4:]
+        self.dir_name = FreeCAD.ActiveDocument.TransientDir.replace('\\', '/') + '/FemAnl_' + analysis_obj.Uid[-4:]
         if not os.path.isdir(self.dir_name):
             os.mkdir(self.dir_name)
         self.base_name = self.dir_name + '/' + self.mesh_object.Name
