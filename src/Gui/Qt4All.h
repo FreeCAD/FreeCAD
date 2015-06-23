7653d2d48f32d330ec0b287591d807d5666116e7
DeepSOIC <vv.titov@gmail.com>
2015-06-23 22:01:16 +0300

    Fix build on Windows

--- src/Gui/Qt4All.h
+++ src/Gui/Qt4All.h
@@ -77,8 +77,10 @@
 #include <QFileIconProvider>
 #include <qfontdatabase.h>
 #include <qfontdialog.h>
+#include <QGraphicsProxyWidget>
 #include <QGraphicsRectItem>
 #include <QGraphicsSvgItem>
+#include <QGraphicsSceneContextMenuEvent>
 #include <QGroupBox>
 #include <QHeaderView>
 #include <qimage.h>
