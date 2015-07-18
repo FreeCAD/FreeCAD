202a818cc675ddea3368ae6e2e1d7a227ab7cbdd
Dan Falck <ddfalck@gmail.com>
2015-07-18 07:27:33 -0700

    added BSD license, removed shared lib for C++

--- src/Mod/Path/libarea/kurve/kurve.cpp
+++ src/Mod/Path/libarea/kurve/kurve.cpp
@@ -1,7 +1,32 @@
-// written by g.j.hawkesford 2006 for Camtek Gmbh
-//
-// This program is released under the BSD license. See the file COPYING for details.
-//
+////////////////////////////////////////////////////////////////////////////////////////////////
+// kurve
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*==============================
+Copyright (c) 2006 g.j.hawkesford 
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the author may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+==============================*/
 
 #include "geometry.h"
 using namespace geoff_geometry;
@@ -9,11 +34,6 @@ using namespace geoff_geometry;
 #ifdef PEPSPOST
 	#include "postoutput.h"
 #endif
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////
-// kurve
-////////////////////////////////////////////////////////////////////////////////////////////////
 
 namespace geoff_geometry {
 
