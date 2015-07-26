013381ccb6a2a19eae9fcbd18633ec9d2ec0a2a7
Alexander Golubev <fatzer2@gmail.com>
2015-07-26 07:02:27 +0300

    PartDesign/Gui: share code for revolution and groove view providers

--- src/Mod/PartDesign/Gui/ViewProviderGroove.cpp
+++ src/Mod/PartDesign/Gui/ViewProviderGroove.cpp
@@ -26,21 +26,15 @@
 #ifndef _PreComp_
 # include <QAction>
 # include <QMenu>
-# include <QMessageBox>
 #endif
 
-#include <Mod/PartDesign/App/FeatureGroove.h>
-#include <Mod/Sketcher/App/SketchObject.h>
-#include <Gui/Control.h>
-#include <Gui/Command.h>
-#include <Gui/Application.h>
+#include "TaskRevolutionParameters.h"
 
 #include "ViewProviderGroove.h"
-#include "TaskRevolutionParameters.h"
 
 using namespace PartDesignGui;
 
-PROPERTY_SOURCE(PartDesignGui::ViewProviderGroove,PartDesignGui::ViewProvider)
+PROPERTY_SOURCE(PartDesignGui::ViewProviderGroove,PartDesignGui::ViewProviderSketchBased)
 
 ViewProviderGroove::ViewProviderGroove()
 {
@@ -51,89 +45,15 @@ ViewProviderGroove::~ViewProviderGroove()
 {
 }
 
-std::vector<App::DocumentObject*> ViewProviderGroove::claimChildren(void)const
-{
-    std::vector<App::DocumentObject*> temp;
-    App::DocumentObject* sketch = static_cast<PartDesign::Groove*>(getObject())->Sketch.getValue();
-    if (sketch != NULL)
-        temp.push_back(sketch);
-
-    return temp;
-}
-
 void ViewProviderGroove::setupContextMenu(QMenu* menu, QObject* receiver, const char* member)
 {
     QAction* act;
-    act = menu->addAction(QObject::tr("Edit Groove"), receiver, member);
+    act = menu->addAction(QObject::tr("Edit groove"), receiver, member);
     act->setData(QVariant((int)ViewProvider::Default));
     PartGui::ViewProviderPart::setupContextMenu(menu, receiver, member);
 }
 
-bool ViewProviderGroove::setEdit(int ModNum)
+TaskDlgFeatureParameters *ViewProviderGroove::getEditDialog()
 {
-    if (ModNum == ViewProvider::Default ) {
-        PartDesign::Groove* pcGroove = static_cast<PartDesign::Groove*>(getObject());
-        if (pcGroove->getSketchAxisCount() < 0) {
-            QMessageBox msgBox;
-            msgBox.setIcon(QMessageBox::Critical);
-            msgBox.setWindowTitle(QObject::tr("Lost link to base sketch"));
-            msgBox.setText(QObject::tr("The object can't be edited because the link to the the base sketch is lost."));
-            msgBox.setStandardButtons(QMessageBox::Ok);
-            msgBox.exec();
-            return false;
-        }
-        // When double-clicking on the item for this pad the
-        // object unsets and sets its edit mode without closing
-        // the task panel
-        Gui::TaskView::TaskDialog *dlg = Gui::Control().activeDialog();
-        TaskDlgRevolutionParameters *padDlg = qobject_cast<TaskDlgRevolutionParameters *>(dlg);
-        if (padDlg && padDlg->getRevolutionView() != this)
-            padDlg = 0; // another pad left open its task panel
-        if (dlg && !padDlg) {
-            QMessageBox msgBox;
-            msgBox.setText(QObject::tr("A dialog is already open in the task panel"));
-            msgBox.setInformativeText(QObject::tr("Do you want to close this dialog?"));
-            msgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
-            msgBox.setDefaultButton(QMessageBox::Yes);
-            int ret = msgBox.exec();
-            if (ret == QMessageBox::Yes)
-                Gui::Control().reject();
-            else
-                return false;
-        }
-
-        // clear the selection (convenience)
-        Gui::Selection().clearSelection();
-
-        // always change to PartDesign WB, remember where we come from
-        oldWb = Gui::Command::assureWorkbench("PartDesignWorkbench");
-
-        // start the edit dialog
-        if (padDlg)
-            Gui::Control().showDialog(padDlg);
-        else
-            Gui::Control().showDialog(new TaskDlgRevolutionParameters(this));
-
-        return true;
-    }
-    else {
-        return PartGui::ViewProviderPart::setEdit(ModNum);
-    }
+    return new TaskDlgRevolutionParameters( this );
 }
-
-bool ViewProviderGroove::onDelete(const std::vector<std::string> &s)
-{
-    // get the Sketch
-    PartDesign::Groove* pcGroove = static_cast<PartDesign::Groove*>(getObject());
-    Sketcher::SketchObject *pcSketch = 0;
-    if (pcGroove->Sketch.getValue())
-        pcSketch = static_cast<Sketcher::SketchObject*>(pcGroove->Sketch.getValue());
-
-    // if abort command deleted the object the Sketch is visible again
-    if (pcSketch && Gui::Application::Instance->getViewProvider(pcSketch))
-        Gui::Application::Instance->getViewProvider(pcSketch)->show();
-
-    return ViewProvider::onDelete(s);
-}
-
-
