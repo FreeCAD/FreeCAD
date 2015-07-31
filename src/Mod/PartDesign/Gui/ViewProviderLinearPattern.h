1b1bd5f17e7f3097727e752e92bae42a8478609d
Alexander Golubev <fatzer2@gmail.com>
2015-07-31 14:00:14 +0300

    PartDesign/Gui: make Transformed ViewProviders use common code with other

--- src/Mod/PartDesign/Gui/ViewProviderLinearPattern.h
+++ src/Mod/PartDesign/Gui/ViewProviderLinearPattern.h
@@ -37,7 +37,8 @@ public:
           sPixmap = "PartDesign_LinearPattern.svg"; }
 
 protected:
-    virtual bool setEdit(int ModNum);
+    /// Returns a newly create dialog for the part to be placed in the task view
+    virtual TaskDlgFeatureParameters *getEditDialog();
 
 };
 
