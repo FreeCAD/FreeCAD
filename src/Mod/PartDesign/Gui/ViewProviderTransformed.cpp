94a51aa017f96d537f22e0bca28d7f92f533d232
wmayer <wmayer@users.sourceforge.net>
2015-08-03 10:53:36 +0200

    + re-insert removed line

--- src/Mod/PartDesign/Gui/ViewProviderTransformed.cpp
+++ src/Mod/PartDesign/Gui/ViewProviderTransformed.cpp
@@ -28,6 +28,7 @@
 # include <BRep_Tool.hxx>
 # include <BRepBndLib.hxx>
 # include <BRepMesh_IncrementalMesh.hxx>
+# include <Standard_Version.hxx>
 # include <TopExp_Explorer.hxx>
 # include <TopoDS.hxx>
 # include <Poly_Triangulation.hxx>
@@ -253,6 +254,7 @@ void ViewProviderTransformed::recomputeFeature(void)
 
             // create or use the mesh on the data structure
 #if OCC_VERSION_HEX >= 0x060600
+            Standard_Real AngDeflectionRads = AngularDeflection.getValue() / 180.0 * M_PI;
             BRepMesh_IncrementalMesh(cShape,deflection,Standard_False,
                                         AngDeflectionRads,Standard_True);
 #else
