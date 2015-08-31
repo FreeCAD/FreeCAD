b4ee6b9be6a00860589875a2fcc5308559b3d981
Eivind Kvedalen <eivind@kvedalen.name>
2015-08-31 23:49:09 +0200

    Mod/Spreadsheet: Removed unused method.

--- src/Mod/Spreadsheet/App/Expression.h
+++ src/Mod/Spreadsheet/App/Expression.h
@@ -127,7 +127,6 @@ public:
     const std::string & getPropertyName() const { return components[propertyIndex].component; }
 
     const Component & getPropertyComponent(std::size_t i) const { assert(i < components.size()); return components[propertyIndex + i]; }
-    const std::string & getSubComponent(int i) const { assert(i >= 1); return components[propertyIndex - 1].component; }
 
     std::string getSubPathStr() const;
 
