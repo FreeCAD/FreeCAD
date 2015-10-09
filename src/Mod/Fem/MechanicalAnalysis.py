6b8abfe425a83f3707c04d5d508be6ed4facdf2d
Przemo Firszt <przemo@firszt.eu>
2015-10-09 20:05:56 +0100

    FEM: Move _CommandMechanicalJobControl class to separate file
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- src/Mod/Fem/MechanicalAnalysis.py
+++ src/Mod/Fem/MechanicalAnalysis.py
@@ -104,26 +104,6 @@ class _CommandFemFromShape:
         return False
 
 
-class _CommandMechanicalJobControl:
-    "the Fem JobControl command definition"
-    def GetResources(self):
-        return {'Pixmap': 'fem-new-analysis',
-                'MenuText': QtCore.QT_TRANSLATE_NOOP("Fem_JobControl", "Start calculation"),
-                'Accel': "S, C",
-                'ToolTip': QtCore.QT_TRANSLATE_NOOP("Fem_JobControl", "Dialog to start the calculation of the mechanical anlysis")}
-
-    def Activated(self):
-        import _JobControlTaskPanel
-        taskd = _JobControlTaskPanel._JobControlTaskPanel(FemGui.getActiveAnalysis())
-        #taskd.obj = vobj.Object
-        taskd.update()
-        FreeCADGui.Control.showDialog(taskd)
-
-    def IsActive(self):
-        return FreeCADGui.ActiveDocument is not None and FemGui.getActiveAnalysis() is not None
-
-
 if FreeCAD.GuiUp:
     FreeCADGui.addCommand('Fem_NewMechanicalAnalysis', _CommandNewMechanicalAnalysis())
     FreeCADGui.addCommand('Fem_CreateFromShape', _CommandFemFromShape())
-    FreeCADGui.addCommand('Fem_MechanicalJobControl', _CommandMechanicalJobControl())
