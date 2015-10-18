1a3d4249a45201f0655beeae673412dedb4cda8a
wmayer <wmayer@users.sourceforge.net>
2015-10-18 19:50:47 +0200

    + make Python API of Selection consistent to C++ API

--- src/Gui/Selection.h
+++ src/Gui/Selection.h
@@ -311,6 +311,7 @@ protected:
     static PyObject *sIsSelected          (PyObject *self,PyObject *args,PyObject *kwd);
     static PyObject *sCountObjectsOfType  (PyObject *self,PyObject *args,PyObject *kwd);
     static PyObject *sGetSelection        (PyObject *self,PyObject *args,PyObject *kwd);
+    static PyObject *sGetCompleteSelection(PyObject *self,PyObject *args,PyObject *kwd);
     static PyObject *sGetSelectionEx      (PyObject *self,PyObject *args,PyObject *kwd);
     static PyObject *sGetSelectionObject  (PyObject *self,PyObject *args,PyObject *kwd);
     static PyObject *sAddSelObserver      (PyObject *self,PyObject *args,PyObject *kwd);
