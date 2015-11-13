bf0d8d68bbb90018652f7c845fc16cb6d51df8f3
wmayer <wmayer@users.sourceforge.net>
2015-11-13 13:38:24 +0100

    + Add selected sketches to one Face, implement specialized view provider for Face

--- src/Mod/Part/App/FeatureFace.h
+++ src/Mod/Part/App/FeatureFace.h
@@ -43,6 +43,10 @@ public:
     /// recalculate the feature
     App::DocumentObjectExecReturn *execute(void);
     short mustExecute() const;
+    /// returns the type name of the ViewProvider
+    const char* getViewProviderName(void) const {
+        return "PartGui::ViewProviderFace";
+    }
     //@}
 
 protected:
