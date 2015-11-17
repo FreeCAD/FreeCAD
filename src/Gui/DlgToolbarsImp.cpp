3268acf9a24e167feddc1c2d6396bce4e3591145
wmayer <wmayer@users.sourceforge.net>
2015-11-17 18:39:23 +0100

    + set object name to tool buttons with popup menu

--- src/Gui/DlgToolbarsImp.cpp
+++ src/Gui/DlgToolbarsImp.cpp
@@ -724,6 +724,7 @@ void DlgCustomToolbarsImp::setActionGroup(QAction* action, const QList<QAction*>
             QMenu* menu = tb->menu();
             if (!menu) {
                 tb->setPopupMode(QToolButton::MenuButtonPopup);
+                tb->setObjectName(QString::fromLatin1("qt_toolbutton_menubutton"));
                 QMenu* menu = new QMenu(tb);
                 menu->addActions(group);
                 tb->setMenu(menu);
