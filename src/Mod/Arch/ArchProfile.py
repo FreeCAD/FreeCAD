fa3d8c4824618859898d811d2d0c31aeced509e1
Yorik van Havre <yorik@uncreated.net>
2016-01-09 16:13:05 -0200

    Arch: updated license block

--- src/Mod/Arch/ArchProfile.py
+++ src/Mod/Arch/ArchProfile.py
@@ -1,3 +1,26 @@
+#***************************************************************************
+#*                                                                         *
+#*   Copyright (c) 2011                                                    *
+#*   Yorik van Havre <yorik@uncreated.net>                                 *
+#*                                                                         *
+#*   This program is free software; you can redistribute it and/or modify  *
+#*   it under the terms of the GNU Lesser General Public License (LGPL)    *
+#*   as published by the Free Software Foundation; either version 2 of     *
+#*   the License, or (at your option) any later version.                   *
+#*   for detail see the LICENCE text file.                                 *
+#*                                                                         *
+#*   This program is distributed in the hope that it will be useful,       *
+#*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+#*   GNU Library General Public License for more details.                  *
+#*                                                                         *
+#*   You should have received a copy of the GNU Library General Public     *
+#*   License along with this program; if not, write to the Free Software   *
+#*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+#*   USA                                                                   *
+#*                                                                         *
+#***************************************************************************
+
 import FreeCAD, Draft, Part, os
 from FreeCAD import Vector
 import csv
@@ -11,8 +34,8 @@ else:
         return txt
 
 __title__="FreeCAD Profile"
-__author__ = "XX"
-__url__ = "XX"
+__author__ = "Yorik van Havre"
+__url__ = "http://www.freecadweb.org"
 
 # Presets in the form: Class, Name, Profile type, [profile data]
 # Loaded from Mod/Arch/Data/beams.csv
