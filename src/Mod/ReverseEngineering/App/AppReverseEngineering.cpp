707f438abd3c8e26837ec631ca37072e66d005c1
wmayer <wmayer@users.sourceforge.net>
2016-01-23 18:16:14 +0100

    py3: ported ReverseEngineering to python3

--- src/Mod/ReverseEngineering/App/AppReverseEngineering.cpp
+++ src/Mod/ReverseEngineering/App/AppReverseEngineering.cpp
@@ -735,6 +735,12 @@ Mesh.show(m)
     }
 #endif
 };
+
+PyObject* initModule()
+{
+    return (new Module)->module().ptr();
+}
+
 } // namespace Reen
 
 
@@ -748,9 +754,10 @@ PyMODINIT_FUNC initReverseEngineering()
     }
     catch(const Base::Exception& e) {
         PyErr_SetString(PyExc_ImportError, e.what());
-        return;
+        PyMOD_Return(0);
     }
 
-    new Reen::Module();
+    PyObject* mod = Reen::initModule();
     Base::Console().Log("Loading ReverseEngineering module... done\n");
+    PyMOD_Return(mod);
 }
