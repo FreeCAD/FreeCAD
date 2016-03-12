0730c01fe81e1cfefefb30f43bd569788e01f1af
Stefan Tröger <stefantroeger@gmx.net>
2016-03-12 10:09:56 +0100

    PartDesign: prevent crash on delete and add body

--- src/Mod/PartDesign/Gui/ViewProviderBody.cpp
+++ src/Mod/PartDesign/Gui/ViewProviderBody.cpp
@@ -267,7 +267,9 @@ void ViewProviderBody::updateData(const App::Property* prop)
 
 
 void ViewProviderBody::slotChangedObjectApp ( const App::DocumentObject& obj, const App::Property& prop ) {
-    if (!obj.isDerivedFrom ( Part::Feature::getClassTypeId () ) ) { // we are intrested only in Part::Features
+    
+    if (!obj.isDerivedFrom ( Part::Feature::getClassTypeId () ) ||
+        obj.isDerivedFrom ( Part::BodyBase::getClassTypeId () )    ) { // we are intrested only in Part::Features and not in bodies
         return;
     }
 
