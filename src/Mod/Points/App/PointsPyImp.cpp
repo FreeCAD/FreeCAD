a1e87c7990eba0630b39f107f14d448918430abf
lorenz_l <sppedflyer@gmail.com>
2016-04-14 03:22:34 +0200

    py3: using long instead of int in points loop

--- src/Mod/Points/App/PointsPyImp.cpp
+++ src/Mod/Points/App/PointsPyImp.cpp
@@ -188,7 +188,11 @@ PyObject* PointsPy::fromSegment(PyObject * args)
         pts->reserve(list.size());
         int numPoints = static_cast<int>(points->size());
         for (Py::Sequence::iterator it = list.begin(); it != list.end(); ++it) {
+#if PY_MAJOR_VERSION < 3
             int index = static_cast<int>(Py::Int(*it));
+#else
+            long index = static_cast<long>(Py::Long(*it));
+#endif
             if (index >= 0 && index < numPoints)
                 pts->push_back(points->getPoint(index));
         }
