d32ee551794641bbd95fd76cbdf30286d80b33aa
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2016-05-07 13:40:35 -0400

    Bug fix for ID#0002453
    Note that the version of Bison and Flex used to generate the two .c files are newer than
    in the last commit from 2010. Also,for anyine working on tbis in the future, dont forget
    to run flex and bison on the .l and .y files respectively.

---
+++ src/Base/QuantityParser.h
@@ -0,0 +1,83 @@
+/* A Bison parser, made by GNU Bison 3.0.4.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_QUANTITYPARSER_H_INCLUDED
+# define YY_YY_QUANTITYPARSER_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    UNIT = 258,
+    NUM = 259,
+    MINUSSIGN = 260,
+    ACOS = 261,
+    ASIN = 262,
+    ATAN = 263,
+    ATAN2 = 264,
+    COS = 265,
+    EXP = 266,
+    ABS = 267,
+    MOD = 268,
+    LOG = 269,
+    LOG10 = 270,
+    POW = 271,
+    SIN = 272,
+    SINH = 273,
+    TAN = 274,
+    TANH = 275,
+    SQRT = 276,
+    NEG = 277
+  };
+#endif
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+int yyparse (void);
+
+#endif /* !YY_YY_QUANTITYPARSER_H_INCLUDED  */
