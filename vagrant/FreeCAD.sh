790699c4f7efb06ee780594ff4f712a01cce8070
Jean-Marie Verdun <vejmarie@ruggedpod.qyshare.com>
2016-05-20 22:11:45 +0200

    Update linux package generation

--- vagrant/FreeCAD.sh
+++ vagrant/FreeCAD.sh
@@ -4,14 +4,43 @@
 # Released under GPL v2.0
 # Provided without any warranty
 # Warning: compilation time is long quite long
-
-sudo apt-get install -y xfce4 xfce4-goodies
-sudo apt-get install -y lightdm
-sudo cat /etc/lightdm/lightdm.conf | sed 's/\#autologin-user=/autologin-user=vagrant/' > /tmp/lightdm.conf
-sudo cp /tmp/lightdm.conf /etc/lightdm/lightdm.conf
-sudo /etc/init.d/lightdm start
 # Must add the autlogin script
 # Must add a lightdm start / reboot ?
+
+function create_deb {
+rm -rf /tmp/$1-$2
+mkdir /tmp/$1-$2
+mkdir /tmp/$1-$2/DEBIAN
+echo "Package:$1" >> /tmp/$1-$2/DEBIAN/control
+echo "Version:$2" >> /tmp/$1-$2/DEBIAN/control
+echo "Section:base" >> /tmp/$1-$2/DEBIAN/control
+echo "Priority:optional" >> /tmp/$1-$2/DEBIAN/control
+echo "Architecture:amd64" >> /tmp/$1-$2/DEBIAN/control
+echo "Depends:"$3 >> /tmp/$1-$2/DEBIAN/control
+echo "Maintainer:vejmarie@ruggedpod.qyshare.com" >> /tmp/$1-$2/DEBIAN/control
+echo "Homepage:http://ruggedpod.qyshare.com" >> /tmp/$1-$2/DEBIAN/control
+echo "Description:TEST PACKAGE" >> /tmp/$1-$2/DEBIAN/control
+file_list=`ls -ltd $(find /opt/local/FreeCAD-0.17) | awk '{ print $9}'`
+for file in $file_list
+do
+  is_done=`cat /tmp/stage | grep $file`
+if [ "$is_done" == "" ]
+then
+# The file must be integrated into the new deb
+    cp --parents $file /tmp/$1-$2
+    echo $file >> /tmp/stage
+fi    
+done
+current_dir=`pwd`
+cd /tmp
+dpkg-deb --build $1-$2
+mv $1-$2.deb $1-$2_trusty-amd64.deb
+cd $current_dir
+}
+
+
+
+sudo apt-get install -y dictionaries-common
 sudo apt-get install -y doxygen                          \
                                libboost1.55-dev                 \
                                libboost-filesystem1.55-dev      \
@@ -48,9 +77,50 @@ sudo apt-get install -y g++
 sudo apt-get install -y libfreetype6-dev
 sudo apt-get install -y tcl8.5-dev tk8.5-dev
 sudo apt-get install -y libtogl-dev
+sudo apt-get install -y libhdf5-dev
+sudo apt-get install -y xfce4 xfce4-goodies
+sudo apt-get install -y xubuntu-default-settings
+sudo apt-get install -y lightdm
+sudo apt-get install -y automake
+myversion=`lsb_release -a | grep -i Distributor | awk '{ print $3}'`
+if [ "$myversion" != "Ubuntu" ]
+then
+sudo cat /etc/lightdm/lightdm.conf | sed 's/\#autologin-user=/autologin-user=vagrant/' > /tmp/lightdm.conf
+sudo cp /tmp/lightdm.conf /etc/lightdm/lightdm.conf
+sudo /etc/init.d/lightdm start
+sudo apt-get install libmed
+sudo apt-get install libmedc
 sudo apt-get install -y libmed-dev
 sudo apt-get install -y libmedc-dev
-sudo apt-get install -y libhdf5-dev
+else
+cat <<EOF >& /tmp/lightdm.conf
+[SeatDefaults]
+user-session=xfce
+autologin-session=xfce
+autologin-user=vagrant
+autologin-user-timeout=0
+greeter-session=xfce
+pam-service=lightdm-autologin
+EOF
+sudo cp /tmp/lightdm.conf /etc/lightdm/
+sudo rm /etc/lightdm/lightdm-gtk-greeter.conf
+sudo /etc/init.d/lightdm start &
+
+# Building MED
+
+git clone https://github.com/vejmarie/libMED.git
+cd libMED
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/opt/local/FreeCAD-0.17 ..
+make -j 2
+sudo make install
+cd ../..
+fi
+
+# I must create the package
+
+create_deb MED 3.10 ""
 
 # Building VTK
 
@@ -62,9 +132,11 @@ cd VTK-7.0.0
 mkdir build
 cd build
 # cmake .. -DVTK_RENDERING_BACKEND=OpenGL
-cmake .. -DVTK_Group_Rendering:BOOL=OFF -DVTK_Group_StandAlone:BOOL=ON -DVTK_RENDERING_BACKEND=None
+cmake .. -DCMAKE_INSTALL_PREFIX:PATH=/opt/local/FreeCAD-0.17 -DVTK_Group_Rendering:BOOL=OFF -DVTK_Group_StandAlone:BOOL=ON -DVTK_RENDERING_BACKEND=None
 make -j 2
 sudo make install
+
+create_deb VTK 7.0 ""
 
 # Building OCCT
 
@@ -82,29 +154,42 @@ grep -v vtkRenderingFreeTypeOpenGL src/TKIVtkDraw/EXTERNLIB >& /tmp/EXTERNLIB
 mkdir build
 cd build
 # cmake .. -DUSE_VTK:BOOL=ON
-cmake .. -DUSE_VTK:BOOL=OFF
+cmake .. -DCMAKE_INSTALL_PREFIX:PATH=/opt/local/FreeCAD-0.17 -DUSE_VTK:BOOL=OFF
 sudo make -j 2
 sudo make install
+
+create_deb OCCT 7.0 "vtk (>= 7.0), med (>= 3.10)"
 
 # Building Netgen
 
 cd ../..
 git clone https://github.com/vejmarie/Netgen
 cd Netgen/netgen-5.3.1
-./configure --with-tcl=/usr/lib/tcl8.5 --with-tk=/usr/lib/tk8.5  --enable-occ  --enable-shared --enable-nglib CXXFLAGS="-DNGLIB_EXPORTS -std=gnu++11"
+./configure --prefix=/opt/local/FreeCAD-0.17 --with-tcl=/usr/lib/tcl8.5 --with-tk=/usr/lib/tk8.5  --enable-occ --with-occ=/opt/local/FreeCAD-0.17 --enable-shared --enable-nglib CXXFLAGS="-DNGLIB_EXPORTS -std=gnu++11"
 make -j 2
 sudo make install
 cd ../..
 sudo cp -rf Netgen/netgen-5.3.1 /usr/share/netgen
+
+create_deb Netgen 5.3.1 "occt (>= 7.0)"
 
 #building FreeCAD
 
 git clone https://github.com/vejmarie/FreeCAD
 cd FreeCAD
 git checkout occt7
+cat cMake/FindOpenCasCade.cmake | sed 's/\/usr\/local\/share\/cmake\//\/opt\/local\/FreeCAD-0.17\/lib\/cmake/' > /tmp/FindOpenCasCade.cmake
+cp /tmp/FindOpenCasCade.cmake cMake/FindOpenCasCade.cmake
+cp cMake/FindOpenCasCade.cmake cMake/FindOPENCASCADE.cmake
 cd ..
 mkdir build
 cd build
-cmake ../FreeCAD -DBUILD_FEM=1 -DBUILD_FEM_NETGEN=1 -DCMAKE_CXX_FLAGS="-DNETGEN_V5"
+cmake ../FreeCAD -DCMAKE_INSTALL_PREFIX:PATH=/opt/local/FreeCAD-0.17 -DBUILD_FEM=1 -DBUILD_FEM_NETGEN=1 -DCMAKE_CXX_FLAGS="-DNETGEN_V5"
 make -j 2
-
+make install
+create_deb FreeCAD 0.17 "netgen (>= 5.3.1), occt (>= 7.0), med (>= 3.10)"
+cd /tmp
+mkdir deb
+mv *.deb deb
+cd deb
+dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
