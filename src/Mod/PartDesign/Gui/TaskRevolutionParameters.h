d9edef9fd51a2ce0a8de8642ba9168738c0dcd9f
Ian Rees <ian.rees@gmail.com>
2016-05-29 15:56:36 +1200

    Clean up warnings due to missing "override".

--- src/Mod/PartDesign/Gui/TaskRevolutionParameters.h
+++ src/Mod/PartDesign/Gui/TaskRevolutionParameters.h
@@ -71,8 +71,8 @@ private Q_SLOTS:
     void onReversed(bool);
 
 protected:
-    void onSelectionChanged(const Gui::SelectionChanges& msg);
-    void changeEvent(QEvent *e);
+    void onSelectionChanged(const Gui::SelectionChanges& msg) override;
+    void changeEvent(QEvent *e) override;
     const bool updateView() const;
     void getReferenceAxis(App::DocumentObject *&obj, std::vector<std::string> &sub) const;
     double getAngle(void) const;
@@ -89,7 +89,6 @@ protected:
 private:
     void updateUI();
 
-private:
     QWidget* proxy;
     Ui_TaskRevolutionParameters* ui;    
 
