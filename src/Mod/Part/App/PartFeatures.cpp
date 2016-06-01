FreeCAD/FreeCAD

commit c3f35d9f05b28300187868ef3749808e12ebbb4c

add part design thickness

src/Mod/Part/App/PartFeatures.cpp
@@ -542,14 +542,12 @@ PROPERTY_SOURCE(Part::Thickness, Part::Feature)
 
 Thickness::Thickness()
 {
-    ADD_PROPERTY_TYPE(Faces,(0),"Thickness",App::Prop_None,"Source shape");
+    ADD_PROPERTY_TYPE(Faces,(0),"Thickness",App::Prop_None,"Faces to be removed");
     ADD_PROPERTY_TYPE(Value,(1.0),"Thickness",App::Prop_None,"Thickness value");
     ADD_PROPERTY_TYPE(Mode,(long(0)),"Thickness",App::Prop_None,"Mode");
     Mode.setEnums(ModeEnums);
     ADD_PROPERTY_TYPE(Join,(long(0)),"Thickness",App::Prop_None,"Join type");
     Join.setEnums(JoinEnums);
-    ADD_PROPERTY_TYPE(Intersection,(false),"Thickness",App::Prop_None,"Intersection");
-    ADD_PROPERTY_TYPE(SelfIntersection,(false),"Thickness",App::Prop_None,"Self Intersection");
 }
 
 short Thickness::mustExecute() const

src/Mod/PartDesign/App/AppPartDesign.cpp
@@ -53,6 +53,7 @@
 #include "FeatureBoolean.h"
 #include "FeaturePrimitive.h"
 #include "DatumCS.h"
+#include "FeatureThickness.h"
 
 namespace PartDesign {
 extern PyObject* initModule();
@@ -99,6 +100,7 @@ PyMODINIT_FUNC init_PartDesign()
     PartDesign::Groove             ::init();
     PartDesign::Chamfer            ::init();
     PartDesign::Draft              ::init();
+    PartDesign::Thickness          ::init();
     PartDesign::Plane              ::init();
     PartDesign::Line               ::init();
     PartDesign::Point              ::init();
@@ -114,21 +116,21 @@ PyMODINIT_FUNC init_PartDesign()
     PartDesign::Sphere             ::init();
     PartDesign::AdditiveSphere     ::init();
     PartDesign::SubtractiveSphere  ::init();
-    PartDesign::Cone             ::init();
-    PartDesign::AdditiveCone     ::init();
-    PartDesign::SubtractiveCone  ::init();
-    PartDesign::Ellipsoid             ::init();
-    PartDesign::AdditiveEllipsoid     ::init();
+    PartDesign::Cone               ::init();
+    PartDesign::AdditiveCone       ::init();
+    PartDesign::SubtractiveCone    ::init();
+    PartDesign::Ellipsoid          ::init();
+    PartDesign::AdditiveEllipsoid  ::init();
     PartDesign::SubtractiveEllipsoid  ::init();
-    PartDesign::Torus             ::init();
-    PartDesign::AdditiveTorus     ::init();
-    PartDesign::SubtractiveTorus  ::init();
-    PartDesign::Prism             ::init();
-    PartDesign::AdditivePrism     ::init();
-    PartDesign::SubtractivePrism  ::init();
-    PartDesign::Wedge             ::init();
-    PartDesign::AdditiveWedge     ::init();
-    PartDesign::SubtractiveWedge  ::init();
+    PartDesign::Torus              ::init();
+    PartDesign::AdditiveTorus      ::init();
+    PartDesign::SubtractiveTorus   ::init();
+    PartDesign::Prism              ::init();
+    PartDesign::AdditivePrism      ::init();
+    PartDesign::SubtractivePrism   ::init();
+    PartDesign::Wedge              ::init();
+    PartDesign::AdditiveWedge      ::init();
+    PartDesign::SubtractiveWedge   ::init();
     
     PartDesign::Point              ::initHints();
     PartDesign::Line               ::initHints();

src/Mod/PartDesign/App/CMakeLists.txt
@@ -76,6 +76,8 @@ SET(FeaturesDressUp_SRCS
     FeatureChamfer.h
     FeatureDraft.cpp
     FeatureDraft.h
+    FeatureThickness.cpp
+    FeatureThickness.h
 )
 SOURCE_GROUP("DressUpFeatures" FILES ${FeaturesDressUp_SRCS})
 

src/Mod/PartDesign/App/FeatureThickness.cpp
@@ -1,0 +1,96 @@
+/***************************************************************************
+ *   Copyright (c) 2015 Stefan Tröger <stefantroeger@gmx.net>              *
+ *                                                                         *
+ *   This file is part of the FreeCAD CAx development system.              *
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Library General Public           *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2 of the License, or (at your option) any later version.      *
+ *                                                                         *
+ *   This library  is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU Library General Public License for more details.                  *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this library; see the file COPYING.LIB. If not,    *
+ *   write to the Free Software Foundation, Inc., 59 Temple Place,         *
+ *   Suite 330, Boston, MA  02111-1307, USA                                *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#include "PreCompiled.h"
+#ifndef _PreComp_
+
+#endif
+
+#include "FeatureThickness.h"
+#include <Base/Exception.h>
+#include <TopoDS.hxx>
+#include <Precision.hxx>
+
+using namespace PartDesign;
+
+const char* PartDesign::Thickness::ModeEnums[] = {"Skin","Pipe", "RectoVerso",NULL};
+const char* PartDesign::Thickness::JoinEnums[] = {"Arc", "Intersection",NULL};
+
+namespace PartDesign {
+    const App::PropertyQuantityConstraint::Constraints quantityRange  = {0.0,FLT_MAX,0.1};
+}
+
+PROPERTY_SOURCE(PartDesign::Thickness, PartDesign::DressUp)
+
+Thickness::Thickness()
+{
+    ADD_PROPERTY_TYPE(Value,(1.0),"Thickness",App::Prop_None,"Thickness value");
+    ADD_PROPERTY_TYPE(Mode,(long(0)),"Thickness",App::Prop_None,"Mode");
+    Mode.setEnums(ModeEnums);
+    ADD_PROPERTY_TYPE(Join,(long(0)),"Thickness",App::Prop_None,"Join type");
+    Join.setEnums(JoinEnums);
+    ADD_PROPERTY_TYPE(Reversed,(false),"Thickness",App::Prop_None,"Apply the thickness towards the solids interior");
+}
+
+short Thickness::mustExecute() const
+{
+    if (Placement.isTouched() ||
+        Value.isTouched() ||
+        Mode.isTouched() ||
+        Join.isTouched())
+        return 1;
+    return DressUp::mustExecute();
+}
+
+App::DocumentObjectExecReturn *Thickness::execute(void)
+{
+    // Base shape
+    Part::TopoShape TopShape;
+    try {
+        TopShape = getBaseShape();
+    } catch (Base::Exception& e) {
+        return new App::DocumentObjectExecReturn(e.what());
+    }
+
+    TopTools_ListOfShape closingFaces;
+    const std::vector<std::string>& subStrings = Base.getSubValues();
+    for (std::vector<std::string>::const_iterator it = subStrings.begin(); it != subStrings.end(); ++it) {
+        TopoDS_Face face = TopoDS::Face(TopShape.getSubShape(it->c_str()));
+        closingFaces.Append(face);
+    }
+
+    bool reversed = Reversed.getValue();
+    double thickness =  (reversed ? -1. : 1. )*Value.getValue();
+    double tol = Precision::Confusion();
+    short mode = (short)Mode.getValue();
+    short join = (short)Join.getValue();
+    //we do not offer tangent join type
+    if(join == 1)
+        join = 2;
+
+    if (fabs(thickness) > 2*tol)
+        this->Shape.setValue(TopShape.makeThickSolid(closingFaces, thickness, tol, false, false, mode, join));
+    else
+        this->Shape.setValue(TopShape._Shape);
+    return App::DocumentObject::StdReturn;
+}

src/Mod/PartDesign/App/FeatureThickness.h
@@ -1,0 +1,65 @@
+/***************************************************************************
+ *   Copyright (c) 2015 Stefan Tröger <stefantroeger@gmx.net>              *
+ *                                                                         *
+ *   This file is part of the FreeCAD CAx development system.              *
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Library General Public           *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2 of the License, or (at your option) any later version.      *
+ *                                                                         *
+ *   This library  is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU Library General Public License for more details.                  *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this library; see the file COPYING.LIB. If not,    *
+ *   write to the Free Software Foundation, Inc., 59 Temple Place,         *
+ *   Suite 330, Boston, MA  02111-1307, USA                                *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#ifndef PARTDESIGN_FEATURETHICKNESS_H
+#define PARTDESIGN_FEATURETHICKNESS_H
+
+#include <App/PropertyStandard.h>
+#include <App/PropertyLinks.h>
+#include <App/PropertyUnits.h>
+#include "FeatureDressUp.h"
+
+namespace PartDesign
+{
+
+class PartDesignExport Thickness : public DressUp
+{
+    PROPERTY_HEADER(PartDesign::Thickness);
+
+public:
+    Thickness();
+
+    App::PropertyLength         Value;
+    App::PropertyBool           Reversed;
+    App::PropertyEnumeration    Mode;
+    App::PropertyEnumeration    Join;      
+
+    /** @name methods override feature */
+    //@{
+    /// recalculate the feature
+    App::DocumentObjectExecReturn *execute(void);
+    short mustExecute() const;
+    /// returns the type name of the view provider
+    const char* getViewProviderName(void) const {
+        return "PartDesignGui::ViewProviderThickness";
+    }
+    //@}
+private:
+    static const char* ModeEnums[];
+    static const char* JoinEnums[];
+};
+
+} //namespace PartDesign
+
+
+#endif // PARTDESIGN_FEATURETHICKNESS_H

src/Mod/PartDesign/Gui/AppPartDesignGui.cpp
@@ -56,6 +56,7 @@
 #include "ViewProviderBoolean.h"
 #include "ViewProviderPrimitive.h"
 #include "ViewProviderDatumCS.h"
+#include "ViewProviderThickness.h"
 
 // use a different name to CreateCommand()
 void CreatePartDesignCommands(void);
@@ -125,6 +126,7 @@ PyMODINIT_FUNC initPartDesignGui()
     PartDesignGui::ViewProviderChamfer       ::init();
     PartDesignGui::ViewProviderFillet        ::init();
     PartDesignGui::ViewProviderDraft         ::init();
+    PartDesignGui::ViewProviderThickness     ::init();
     PartDesignGui::ViewProviderTransformed   ::init();
     PartDesignGui::ViewProviderMirrored      ::init();
     PartDesignGui::ViewProviderLinearPattern ::init();

src/Mod/PartDesign/Gui/CMakeLists.txt
@@ -35,6 +35,7 @@ set(PartDesignGui_MOC_HDRS
     TaskChamferParameters.h
     TaskFilletParameters.h
     TaskDraftParameters.h
+    TaskThicknessParameters.h
     TaskDressUpParameters.h
     TaskHoleParameters.h
     TaskRevolutionParameters.h
@@ -62,6 +63,7 @@ set(PartDesignGui_UIC_SRCS
     TaskChamferParameters.ui
     TaskFilletParameters.ui
     TaskDraftParameters.ui
+    TaskThicknessParameters.ui
     TaskBooleanParameters.ui
     TaskHoleParameters.ui
     TaskRevolutionParameters.ui
@@ -94,6 +96,8 @@ SET(PartDesignGuiViewProvider_SRCS CommandPrimitive.cpp
     ViewProviderFillet.h
     ViewProviderDraft.cpp
     ViewProviderDraft.h
+    ViewProviderThickness.cpp
+    ViewProviderThickness.h
     ViewProviderDressUp.cpp
     ViewProviderDressUp.h
     ViewProviderRevolution.cpp
@@ -152,6 +156,9 @@ SET(PartDesignGuiTaskDlgs_SRCS
     TaskDraftParameters.ui
     TaskDraftParameters.cpp
     TaskDraftParameters.h
+    TaskThicknessParameters.ui
+    TaskThicknessParameters.cpp
+    TaskThicknessParameters.h
     TaskDressUpParameters.cpp
     TaskDressUpParameters.h
     TaskRevolutionParameters.ui

src/Mod/PartDesign/Gui/Command.cpp
@@ -1468,6 +1468,108 @@ bool CmdPartDesignDraft::isActive(void)
     return hasActiveDocument();
 }
 
+
+//===========================================================================
+// PartDesign_Thickness
+//===========================================================================
+DEF_STD_CMD_A(CmdPartDesignThickness);
+
+CmdPartDesignThickness::CmdPartDesignThickness()
+  :Command("PartDesign_Thickness")
+{
+    sAppModule    = "PartDesign";
+    sGroup        = QT_TR_NOOP("PartDesign");
+    sMenuText     = QT_TR_NOOP("Thickness");
+    sToolTipText  = QT_TR_NOOP("Make a thick solid");
+    sWhatsThis    = "PartDesign_Thickness";
+    sStatusTip    = sToolTipText;
+    sPixmap       = "PartDesign_Thickness";
+}
+
+void CmdPartDesignThickness::activated(int iMsg)
+{
+    PartDesign::Body *pcActiveBody = PartDesignGui::getBody(/*messageIfNot = */true);
+    if (!pcActiveBody) return;
+
+    std::vector<Gui::SelectionObject> selection = getSelection().getSelectionEx();
+
+    if (selection.size() < 1) {
+        QMessageBox::warning(Gui::getMainWindow(), QObject::tr("Wrong selection"),
+            QObject::tr("Select one or more faces."));
+        return;
+    }
+
+    if (!selection[0].isObjectTypeOf(Part::Feature::getClassTypeId())){
+        QMessageBox::warning(Gui::getMainWindow(), QObject::tr("Wrong object type"),
+            QObject::tr("Thickness works only on parts"));
+        return;
+    }
+
+    Part::Feature *base = static_cast<Part::Feature*>(selection[0].getObject());
+
+    if (base != pcActiveBody->getPrevSolidFeature(NULL, true)) {
+        QMessageBox::warning(Gui::getMainWindow(), QObject::tr("Wrong base feature"),
+            QObject::tr("Only the current Tip of the active Body can be selected as the base feature"));
+        return;
+    }
+
+    const Part::TopoShape& TopShape = base->Shape.getShape();
+    if (TopShape._Shape.IsNull()){
+        QMessageBox::warning(Gui::getMainWindow(), QObject::tr("Wrong selection"),
+            QObject::tr("Shape of selected Part is empty"));
+        return;
+    }
+
+    std::vector<std::string> SubNames = std::vector<std::string>(selection[0].getSubNames());
+    unsigned int i = 0;
+
+    while(i < SubNames.size())
+    {
+        std::string aSubName = static_cast<std::string>(SubNames.at(i));
+
+        if(aSubName.size() > 4 && aSubName.substr(0,4) != "Face") {
+            // empty name or any other sub-element
+            SubNames.erase(SubNames.begin()+i);
+        }
+        i++;
+    }
+
+    if (SubNames.size() == 0) {
+        QMessageBox::warning(Gui::getMainWindow(), QObject::tr("Wrong selection"),
+        QObject::tr("No thickness possible with selected faces"));
+        return;
+    }
+
+    std::string SelString;
+    SelString += "(App.";
+    SelString += "ActiveDocument";
+    SelString += ".";
+    SelString += selection[0].getFeatName();
+    SelString += ",[";
+    for(std::vector<std::string>::const_iterator it = SubNames.begin();it!=SubNames.end();++it){
+        SelString += "\"";
+        SelString += *it;
+        SelString += "\"";
+        if(it != --SubNames.end())
+            SelString += ",";
+    }
+    SelString += "])";
+
+    std::string FeatName = getUniqueObjectName("Thickness");
+
+    openCommand("Make Thickness");
+    doCommand(Doc,"App.activeDocument().addObject(\"PartDesign::Thickness\",\"%s\")",FeatName.c_str());
+    doCommand(Doc,"App.activeDocument().%s.Base = %s",FeatName.c_str(),SelString.c_str());
+    doCommand(Doc,"App.activeDocument().%s.Value = %f",FeatName.c_str(), 1.);
+
+    finishFeature(this, FeatName);
+}
+
+bool CmdPartDesignThickness::isActive(void)
+{
+    return hasActiveDocument();
+}
+
 //===========================================================================
 // Common functions for all Transformed features
 //===========================================================================
@@ -1930,6 +2032,7 @@ void CreatePartDesignCommands(void)
     rcCmdMgr.addCommand(new CmdPartDesignFillet());
     rcCmdMgr.addCommand(new CmdPartDesignDraft());    
     rcCmdMgr.addCommand(new CmdPartDesignChamfer());
+    rcCmdMgr.addCommand(new CmdPartDesignThickness());    
 
     rcCmdMgr.addCommand(new CmdPartDesignMirrored());
     rcCmdMgr.addCommand(new CmdPartDesignLinearPattern());

commit 989c007186927d3cc2222973402d5ba14a7b7b36

+ revert regression in commit c3f35d9f affecting Part Thickness tool

src/Mod/Part/App/PartFeatures.cpp
@@ -548,6 +548,8 @@ Thickness::Thickness()
     Mode.setEnums(ModeEnums);
     ADD_PROPERTY_TYPE(Join,(long(0)),"Thickness",App::Prop_None,"Join type");
     Join.setEnums(JoinEnums);
+    ADD_PROPERTY_TYPE(Intersection,(false),"Thickness",App::Prop_None,"Intersection");
+    ADD_PROPERTY_TYPE(SelfIntersection,(false),"Thickness",App::Prop_None,"Self Intersection");
 }
 
 short Thickness::mustExecute() const