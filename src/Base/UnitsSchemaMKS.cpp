f465521d6098055d4df8d8178f8b3e859dbbab4b
vdwalts <vdwalts@eskom.co.za>
2016-08-01 21:56:08 +0100

    Base: Units: add units for thermomechanical analysis

--- src/Base/UnitsSchemaMKS.cpp
+++ src/Base/UnitsSchemaMKS.cpp
@@ -109,6 +109,28 @@ QString UnitsSchemaMKS::schemaTranslate(Base::Quantity quant,double &factor,QStr
             unitString = QString::fromLatin1("Pa");
             factor = 1.0;
         }
+    }else if (unit == Unit::ThermalConductivity){
+    	if (UnitValue < 1000){    	
+    	unitString = QString::fromLatin1("W/mm/K");
+    	factor = 1.0;
+    	}else{    	
+        unitString = QString::fromLatin1("W/m/K");
+        factor = 1000.0;
+    	}
+    }else if (unit == Unit::ThermalExpansionCoefficient){
+        if(UnitValue < 0.001){
+            unitString = QString::fromLatin1("um/m/K");
+            factor = 0.000001;
+        }else{
+            unitString = QString::fromLatin1("m/m/K");
+            factor = 1.0;
+        }
+    }else if (unit == Unit::SpecificHeat){
+        unitString = QString::fromLatin1("J/kg/K");
+        factor = 1000000.0;
+	}else if (unit == Unit::ThermalTransferCoefficient){
+        unitString = QString::fromLatin1("W/m^2/K");
+        factor = 1.0;
     }else{
         // default action for all cases without special treatment:
         unitString = quant.getUnit().getString();
