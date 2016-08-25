94eeab03ec8a479535cd2bb9380e45aeba396860
DeepSOIC <vv.titov@gmail.com>
2016-08-25 21:17:14 +0300

    Part: Extrude: improvements
    * split Dir into Dir+Length; added second Length
    * support for parametric linked direction: to arbitrary edge, and to
    normal
    * options: reverse, symmetric
    * second taper angle
    * changed offset join mode for tapered extrusion on OCCT7

--- src/Mod/Part/App/FeatureExtrusion.h
+++ src/Mod/Part/App/FeatureExtrusion.h
@@ -32,7 +32,7 @@
 namespace Part
 {
 
-class Extrusion : public Part::Feature
+class PartExport Extrusion : public Part::Feature
 {
     PROPERTY_HEADER(Part::Extrusion);
 
@@ -41,8 +41,31 @@ public:
 
     App::PropertyLink Base;
     App::PropertyVector Dir;
+    App::PropertyEnumeration DirMode;
+    App::PropertyLinkSub DirLink;
+    App::PropertyDistance LengthFwd;
+    App::PropertyDistance LengthRev;
     App::PropertyBool Solid;
+    App::PropertyBool Reversed;
+    App::PropertyBool Symmetric;
     App::PropertyAngle TaperAngle;
+    App::PropertyAngle TaperAngleRev;
+
+
+    /**
+     * @brief The ExtrusionParameters struct is supposed to be filled with final
+     * extrusion parameters, after resolving links, applying mode logic,
+     * reversing, etc., and be passed to extrudeShape.
+     */
+    struct ExtrusionParameters {
+        gp_Dir dir;
+        double lengthFwd;
+        double lengthRev;
+        bool solid;
+        double taperAngleFwd; //in radians
+        double taperAngleRev;
+        ExtrusionParameters(): lengthFwd(0), lengthRev(0), solid(false), taperAngleFwd(0), taperAngleRev(0) {}// constructor to keep garbage out
+    };
 
     /** @name methods override feature */
     //@{
@@ -55,12 +78,51 @@ public:
     }
     //@}
 
+    /**
+     * @brief extrudeShape powers the extrusion feature.
+     * @param source: the shape to be extruded
+     * @param params: extrusion parameters
+     * @return result of extrusion
+     */
+    static TopoShape extrudeShape(TopoShape source, ExtrusionParameters params);
+
+    /**
+     * @brief fetchAxisLink: read AxisLink to obtain the direction and
+     * length. Note: this routine is re-used in Extrude dialog, hence it
+     * is static.
+     * @param axisLink (input): the link
+     * @param basepoint (output): starting point of edge. Not used by extrude as of now.
+     * @param dir (output): direction of axis, with magnitude (length)
+     * @return true if link was fetched. false if link was empty. Throws if the
+     * link is wrong.
+     */
+    static bool fetchAxisLink(const App::PropertyLinkSub& axisLink,
+                              Base::Vector3d &basepoint,
+                              Base::Vector3d &dir);
+
+    /**
+     * @brief computeFinalParameters: applies mode logic and fetches links, to
+     * compute the actual parameters of extrusion. Dir property is updated in
+     * the process, hence the function is non-const.
+     */
+    ExtrusionParameters computeFinalParameters();
+
+    static Base::Vector3d calculateShapeNormal(const App::PropertyLink &shapeLink);
+
+public: //mode enumerations
+    enum eDirMode{
+        dmCustom,
+        dmEdge,
+        dmNormal
+    };
+    static const char* eDirModeStrings[];
+
 private:
-    bool isInside(const TopoDS_Wire&, const TopoDS_Wire&) const;
-    TopoDS_Face validateFace(const TopoDS_Face&) const;
-    TopoDS_Shape makeFace(const std::vector<TopoDS_Wire>&) const;
-    TopoDS_Shape makeFace(std::list<TopoDS_Wire>&) const; // for internal use only
-    void makeDraft(double, const gp_Vec&, bool, const TopoDS_Shape&, std::list<TopoDS_Shape>&) const;
+    static bool isInside(const TopoDS_Wire&, const TopoDS_Wire&);
+    static TopoDS_Face validateFace(const TopoDS_Face&);
+    static TopoDS_Shape makeFace(const std::vector<TopoDS_Wire>&);
+    static TopoDS_Shape makeFace(std::list<TopoDS_Wire>&); // for internal use only
+    static void makeDraft(ExtrusionParameters params, const TopoDS_Shape&, std::list<TopoDS_Shape>&);
 
 private:
     class Wire_Compare;
