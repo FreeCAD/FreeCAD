c0428a940bcf7b3490118e65118b166d9da70245
WandererFan <wandererfan@gmail.com>
2016-09-01 11:38:20 -0400

    minor fixes SectionFace zOrder, orphan graphics items

--- src/Mod/TechDraw/Gui/ZVALUE.h
+++ src/Mod/TechDraw/Gui/ZVALUE.h
@@ -5,12 +5,13 @@ namespace ZVALUE {
     const int TEMPLATE = -1000;
     const int SVGTEMPLATE = -500;
     const int FACE = 10;
-    const int SECTIONFACE = 20;
     const int HATCH = 30;
     const int HIDEDGE = 40;
     const int EDGE = 50;
     const int VERTEX = 60;
+    const int SECTIONFACE = 65;
+    const int SECTIONHATCH = 66;
     const int DIMENSION = 70;
-    const int SECTIONLINE = 80;
+    const int SECTIONLINE = 80;    //TODO: change to "DECORATION"? section lines, symmetry lines, etc?
 }
 #endif
