61d3f23e7ebdf7a354ac3b6b2c66901ab7e43513
wmayer <wmayer@users.sourceforge.net>
2016-11-09 10:35:49 +0100

    dynamic properties: + move addProperty, removeProperty and supportedProperties from FeaturePythonPyT to DocumentObjectPy

--- src/App/FeaturePythonPyImp.h
+++ src/App/FeaturePythonPyImp.h
@@ -48,18 +48,6 @@ public:
     /** @name callbacks and implementers for the python object methods */
     //@{
     static  int __setattr(PyObject *PyObj, char *attr, PyObject *value);
-    /// callback for the addProperty() method
-    static PyObject * staticCallback_addProperty (PyObject *self, PyObject *args);
-    /// implementer for the addProperty() method
-    PyObject*  addProperty(PyObject *args);
-    /// callback for the removeProperty() method
-    static PyObject * staticCallback_removeProperty (PyObject *self, PyObject *args);
-    /// implementer for the removeProperty() method
-    PyObject*  removeProperty(PyObject *args);
-    /// callback for the supportedProperties() method
-    static PyObject * staticCallback_supportedProperties (PyObject *self, PyObject *args);
-    /// implementer for the supportedProperties() method
-    PyObject*  supportedProperties(PyObject *args);
     //@}
 
     /// getter method for special attributes (e.g. dynamic ones)
