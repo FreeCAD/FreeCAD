99c902ba0969438f16b8ffbc6b874df309482384
Bernd Hahnebach <bernd@bimstatik.org>
2016-11-22 19:48:05 +0100

    FEM: GMSH mesh tool, add a pref page for gmsh binary path

--- src/Mod/Fem/Gui/AppFemGui.cpp
+++ src/Mod/Fem/Gui/AppFemGui.cpp
@@ -35,6 +35,7 @@
 #include "PropertyFemMeshItem.h"
 #include "DlgSettingsFemGeneralImp.h"
 #include "DlgSettingsFemCcxImp.h"
+#include "DlgSettingsFemGmshImp.h"
 #include "DlgSettingsFemZ88Imp.h"
 #include "ViewProviderFemMesh.h"
 #include "ViewProviderFemMeshShape.h"
@@ -154,6 +155,7 @@ PyMODINIT_FUNC initFemGui()
     // register preferences pages
     new Gui::PrefPageProducer<FemGui::DlgSettingsFemGeneralImp> (QT_TRANSLATE_NOOP("QObject","FEM"));
     new Gui::PrefPageProducer<FemGui::DlgSettingsFemCcxImp> (QT_TRANSLATE_NOOP("QObject","FEM"));
+    new Gui::PrefPageProducer<FemGui::DlgSettingsFemGmshImp> (QT_TRANSLATE_NOOP("QObject","FEM"));
     new Gui::PrefPageProducer<FemGui::DlgSettingsFemZ88Imp> (QT_TRANSLATE_NOOP("QObject","FEM"));
 
      // add resources and reloads the translators
