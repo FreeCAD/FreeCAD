d54906d52cd3622be8fdfd7fcbbad297e4b6e980
Bernd Hahnebach <bernd@bimstatik.org>
2016-11-22 19:47:07 +0100

    FEM: do not longer open mesh at analyse creation better make mesh last as before the constraints because of group meshing

--- src/Mod/Fem/_CommandAnalysis.py
+++ src/Mod/Fem/_CommandAnalysis.py
@@ -58,11 +58,6 @@ class _CommandAnalysis(FemCommands):
         if (len(sel) == 1):
             if(sel[0].isDerivedFrom("Fem::FemMeshObject")):
                 FreeCADGui.doCommand("FemGui.getActiveAnalysis().Member = FemGui.getActiveAnalysis().Member + [App.activeDocument()." + sel[0].Name + "]")
-            if(sel[0].isDerivedFrom("Part::Feature")):
-                FreeCADGui.doCommand("App.activeDocument().addObject('Fem::FemMeshShapeNetgenObject', '" + sel[0].Name + "_Mesh')")
-                FreeCADGui.doCommand("App.activeDocument().ActiveObject.Shape = App.activeDocument()." + sel[0].Name)
-                FreeCADGui.doCommand("FemGui.getActiveAnalysis().Member = FemGui.getActiveAnalysis().Member + [App.activeDocument().ActiveObject]")
-                FreeCADGui.doCommand("Gui.activeDocument().setEdit(App.ActiveDocument.ActiveObject.Name)")
         FreeCADGui.Selection.clearSelection()
 
 if FreeCAD.GuiUp:
