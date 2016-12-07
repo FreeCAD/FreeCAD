b758df71939b6ebc847c143c6ae0f7b73913cb8a
Stefan Tröger <stefantroeger@gmx.net>
2016-12-07 06:23:54 +0100

    Port attachment UI to Part for general use
    Furthermore change PartDesign primitives to use the new task dialog

--- src/Mod/PartDesign/Gui/TaskDatumParameters.h
+++ src/Mod/PartDesign/Gui/TaskDatumParameters.h
@@ -27,7 +27,7 @@
 #include <Gui/TaskView/TaskView.h>
 #include <Gui/Selection.h>
 #include <Gui/TaskView/TaskDialog.h>
-#include <Mod/Part/App/Attacher.h>
+#include <Mod/Part/Gui/TaskAttacher.h>
 
 #include "ViewProviderDatum.h"
 
@@ -46,126 +46,25 @@ namespace PartDesignGui {
 
 
 
-class TaskDatumParameters : public Gui::TaskView::TaskBox, public Gui::SelectionObserver
+class TaskDatumParameters : public PartGui::TaskAttacher
 {
     Q_OBJECT
 
 public:
     TaskDatumParameters(ViewProviderDatum *DatumView,QWidget *parent = 0);
-    ~TaskDatumParameters();
-
-    bool   getFlip(void) const;
-
-    /**
-     * @brief getActiveMapMode returns either the default mode for selected
-     * references, or the mode that was selected by the user in the list. If
-     * no modes fit current set of references, mmDeactivated is returned.
-     */
-    Attacher::eMapMode getActiveMapMode();
-
-    bool isCompleted() const { return completed; }
-
-private Q_SLOTS:
-    void onSuperplacementChanged(double, int idx);
-    void onSuperplacementXChanged(double);
-    void onSuperplacementYChanged(double);
-    void onSuperplacementZChanged(double);
-    void onSuperplacementYawChanged(double);
-    void onSuperplacementPitchChanged(double);
-    void onSuperplacementRollChanged(double);
-    void onCheckFlip(bool);
-    void onRefName1(const QString& text);
-    void onRefName2(const QString& text);
-    void onRefName3(const QString& text);
-    void onRefName4(const QString& text);
-    void onButtonRef1(const bool checked = true);
-    void onButtonRef2(const bool checked = true);
-    void onButtonRef3(const bool checked = true);
-    void onButtonRef4(const bool checked = true);
-    void onModeSelect(void);
-    void visibilityAutomation(bool opening_not_closing);
-
-protected:
-    void changeEvent(QEvent *e);
-
-private:
-    void resetViewMode();
-    void objectDeleted(const Gui::ViewProviderDocumentObject&);
-    void onSelectionChanged(const Gui::SelectionChanges& msg) override;
-    void updateReferencesUI();
-
-    /**
-     * @brief updatePreview: calculate attachment, update 3d view, update status message
-     * @return true if attachment calculation was successful, false otherwise
-     */
-    bool updatePreview();
-
-    void makeRefStrings(std::vector<QString>& refstrings, std::vector<std::string>& refnames);
-    QLineEdit* getLine(unsigned idx);
-    void onButtonRef(const bool checked, unsigned idx);
-    void onRefName(const QString& text, unsigned idx);
-    void updateRefButton(int idx);
-    void updateSuperplacementUI();
-
-    /**
-     * @brief updateListOfModes Fills the mode list with modes that apply to
-     * current set of references.
-     * @param curMode the mode to select in the list. If the mode isn't
-     * contained in the list, nothing is selected. If mmDeactivated is passed,
-     * currently selected mode is kept.
-     */
-    void updateListOfModes(Attacher::eMapMode curMode = Attacher::mmDeactivated);
-
-private:
-    QWidget* proxy;
-    Ui_TaskDatumParameters* ui;
-    ViewProviderDatum *DatumView;
-
-    // TODO fix documentation here (2015-11-10, Fat-Zer)
-    int iActiveRef; //what reference is being picked in 3d view now? -1 means no one, 0-3 means a reference is being picked.
-    bool autoNext;//if we should automatically switch to next reference (true after dialog launch, false afterwards)
-    std::vector<Attacher::eMapMode> modesInList; //this list is synchronous to what is populated into listOfModes widget.
-    Attacher::SuggestResult lastSuggestResult;
-    bool completed;
-
-    typedef boost::BOOST_SIGNALS_NAMESPACE::connection Connection;
-    Connection connectDelObject;
+    ~TaskDatumParameters(); 
 };
 
 /// simulation dialog for the TaskView
-class TaskDlgDatumParameters : public Gui::TaskView::TaskDialog
+class TaskDlgDatumParameters : public PartGui::TaskDlgAttacher
 {
     Q_OBJECT
 
 public:
     TaskDlgDatumParameters(ViewProviderDatum *DatumView);
     ~TaskDlgDatumParameters();
-
-    ViewProviderDatum* getDatumView() const
-    { return DatumView; }
-
-
-public:
-    /// is called the TaskView when the dialog is opened
-    virtual void open();
-    /// is called by the framework if an button is clicked which has no accept or reject role
-    virtual void clicked(int);
-    /// is called by the framework if the dialog is accepted (Ok)
+    
     virtual bool accept();
-    /// is called by the framework if the dialog is rejected (Cancel)
-    virtual bool reject();
-    /// is called by the framework if the user presses the help button 
-    virtual bool isAllowedAlterDocument(void) const
-    { return false; }
-
-    /// returns for Close and Help button 
-    virtual QDialogButtonBox::StandardButtons getStandardButtons(void) const
-    { return QDialogButtonBox::Ok|QDialogButtonBox::Cancel; }
-
-protected:
-    ViewProviderDatum   *DatumView;
-
-    TaskDatumParameters  *parameter;
 };
 
 } //namespace PartDesignGui
