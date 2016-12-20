69e733a34f722a483afd3cf1e44ab73c126fa912
Bernd Hahnebach <bernd@bimstatik.org>
2016-12-20 18:11:51 +0100

    FEM: beam section properties, use diameter instead of radius

--- src/Mod/Fem/_FemBeamSection.py
+++ src/Mod/Fem/_FemBeamSection.py
@@ -36,9 +36,9 @@ class _FemBeamSection:
     def __init__(self, obj):
         obj.addProperty("App::PropertyLength", "RectWidth", "RectBeamSection", "set width of the rectangular beam elements")
         obj.addProperty("App::PropertyLength", "RectHeight", "RectBeamSection", "set height of therectangular beam elements")
-        obj.addProperty("App::PropertyLength", "CircRadius", "CircBeamSection", "set radius of the circular beam elements")
-        obj.addProperty("App::PropertyLength", "PipeRadius", "PipeBeamSection", "set height of the pipe beam elements")
-        obj.addProperty("App::PropertyLength", "PipeThickness", "PipeBeamSection", "set height of the pipe beam elements")
+        obj.addProperty("App::PropertyLength", "CircDiameter", "CircBeamSection", "set diameter of the circular beam elements")
+        obj.addProperty("App::PropertyLength", "PipeDiameter", "PipeBeamSection", "set outer diameter of the pipe beam elements")
+        obj.addProperty("App::PropertyLength", "PipeThickness", "PipeBeamSection", "set thickness of the pipe beam elements")
         obj.addProperty("App::PropertyEnumeration", "SectionType", "BeamSection", "select beam section type")
         obj.addProperty("App::PropertyLinkSubList", "References", "BeamSection", "List of beam section shapes")
         obj.SectionType = _FemBeamSection.known_beam_types
