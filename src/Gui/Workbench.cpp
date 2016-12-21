612e8aeae7b07490edbfd94ffad710d2e2bb19f1
wmayer <wmayer@users.sourceforge.net>
2016-12-21 15:21:18 +0100

    add command to select visible objects

--- src/Gui/Workbench.cpp
+++ src/Gui/Workbench.cpp
@@ -531,6 +531,7 @@ MenuItem* StdWorkbench::setupMenuBar() const
     MenuItem* visu = new MenuItem;
     visu->setCommand("Visibility");
     *visu << "Std_ToggleVisibility" << "Std_ShowSelection" << "Std_HideSelection"
+          << "Std_SelectVisibleObjects"
           << "Separator" << "Std_ToggleObjects" << "Std_ShowObjects" << "Std_HideObjects" 
           << "Separator" << "Std_ToggleSelectability"
           << "Separator" << "View_Measure_Toggle_All" << "View_Measure_Clear_All";
