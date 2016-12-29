6b8976b492b53e0978f08bfdba78159cbf14d149
wmayer <wmayer@users.sourceforge.net>
2016-12-29 15:01:57 +0100

    fix build failure

--- src/Mod/TechDraw/App/DrawProjectSplit.cpp
+++ src/Mod/TechDraw/App/DrawProjectSplit.cpp
@@ -125,7 +125,7 @@ std::vector<TopoDS_Edge> DrawProjectSplit::getEdgesForWalker(TopoDS_Shape shape,
 
 
 TechDrawGeometry::GeometryObject* DrawProjectSplit::buildGeometryObject(TopoDS_Shape shape,
-                                                                        gp_Ax2 viewAxis)
+                                                                        const gp_Ax2& viewAxis)
 {
     TechDrawGeometry::GeometryObject* geometryObject = new TechDrawGeometry::GeometryObject("DrawProjectSplit");
 
