c0a7d5d161850ab4490a8cdb1780f382e12d72c3
wmayer <wmayer@users.sourceforge.net>
2016-12-30 14:38:49 +0100

    comment out unused global functions, fix -Wconstant-logical-operand

--- src/Gui/WidgetFactory.cpp
+++ src/Gui/WidgetFactory.cpp
@@ -44,6 +44,7 @@
 #if defined(__clang__)
 # pragma clang diagnostic push
 # pragma clang diagnostic ignored "-Wmismatched-tags"
+# pragma clang diagnostic ignored "-Wunused-parameter"
 #elif defined (__GNUC__)
 # pragma GCC diagnostic push
 # pragma GCC diagnostic ignored "-Wunused-parameter"
