80adf30da373daf648e047a8d78b45ad5507ff25
Abdullah Tahiri <abdullah.tahiri.yo@gmail.com>
2017-01-04 01:19:18 +0100

    Sketcher: BSpline FreeGCS geometry definition
    =============================================
    
    multiplicities, degree and periodic are left as non-parameters of the solver, while still allowing certain manipulations to be effected from the solver
    in certain situations (for example modifying the multiplicity of start/end nodes when applying G1,G2,G3 constraints between BSplines).

--- src/Mod/Sketcher/App/planegcs/Geo.h
+++ src/Mod/Sketcher/App/planegcs/Geo.h
@@ -36,6 +36,8 @@ namespace GCS
         double *y;
     };
 
+    typedef std::vector<Point> VEC_P;
+    
     ///Class DeriVector2 holds a vector value and its derivative on the
     ///parameter that the derivatives are being calculated for now. x,y is the
     ///actual vector (v). dx,dy is a derivative of the vector by a parameter
@@ -270,6 +272,32 @@ namespace GCS
         virtual ArcOfParabola* Copy();
     };
 
+    class BSpline: public Curve
+    {
+    public:
+        BSpline(){periodic=false;degree=2;}
+        virtual ~BSpline(){}
+	// parameters
+	VEC_P poles;
+	VEC_pD weights;
+	VEC_pD knots;
+	// dependent parameters (depends on previous parameters, 
+	// but an "arcrules" constraint alike would be required to gain the commodity of simple coincident
+	// with endpoint constraints)
+        Point start;
+        Point end;
+	// not solver parameters
+	VEC_I mult;
+	int degree;
+	bool periodic;	
+	// interface helpers
+	DeriVector2 CalculateNormal(Point &p, double* derivparam = 0);
+        virtual DeriVector2 Value(double u, double du, double* derivparam = 0);
+        virtual int PushOwnParams(VEC_pD &pvec);
+        virtual void ReconstructOnNewPvec (VEC_pD &pvec, int &cnt);
+        virtual BSpline* Copy();
+    };
+    
 } //namespace GCS
 
 #endif // PLANEGCS_GEO_H
