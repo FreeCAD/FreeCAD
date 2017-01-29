fddb028cbdb2a1ab2b24f958b270e212523251d1
qingfengxia <qingfeng.xia@gmail.com>
2017-01-29 22:37:28 +0000

    Fem: add FemResult export to VTK files for visualization externally

--- src/Mod/Fem/Init.py
+++ src/Mod/Fem/Init.py
@@ -30,9 +30,10 @@ import FreeCAD
 FreeCAD.addExportType("TetGen file (*.poly)", "convert2TetGen")
 FreeCAD.addImportType("FEM formats (*.unv *.med *.dat *.bdf)", "Fem")
 if("BUILD_FEM_VTK" in FreeCAD.__cmake__):
-    FreeCAD.addImportType("FEM results (*.vtk *.vtp *.vts *.vtr *.vtu *.vti)", "Fem")
+    #FreeCAD.addImportType("FEM results (*.vtk *.vtp *.vts *.vtr *.vtu *.vti)", "FemResultVTK")  # not implemented yet
     FreeCAD.addImportType("FEM CFD Unstructure Mesh (*.vtk *.vtu)", "Fem")
     FreeCAD.addExportType("FEM CFD Unstructure Mesh (*.vtk *.vtu)", "Fem")
+    FreeCAD.addExportType("FEM CFD Result in VTK format (*.vtk *.vtu)", "FemResultVTK")
 
 FreeCAD.addExportType("FEM formats (*.unv *.med *.dat *.inp)", "Fem")
 FreeCAD.addImportType("CalculiX result (*.frd)", "ccxFrdReader")
