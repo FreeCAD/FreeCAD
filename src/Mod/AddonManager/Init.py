6b9e6539eb16af5cc93b6306383531c7efa860ed
Kurt Kremitzki <kkremitzki@gmail.com>
2017-02-01 20:20:38 -0600

    Add Addon Manager to Tools menu

---
+++ src/Mod/AddonManager/Init.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# FreeCAD init script of the AddonManager module
+# (c) 2001 Juergen Riegel
+# License LGPL
---
+++ src/Mod/AddonManager/InitGui.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# AddonManager gui init module
+# (c) 2001 Juergen Riegel
+# License LGPL
+from AddonManagerGui import CmdAddonMgr
+
+FreeCADGui.addCommand('Std_AddonMgr', CmdAddonMgr())
