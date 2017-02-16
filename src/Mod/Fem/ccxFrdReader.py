7a07ae8859fe797917967c83c99838f43ac96bd0
Bernd Hahnebach <bernd@bimstatik.org>
2017-02-16 07:54:37 +0100

    FEM: result object refactor, fix frd, vtk, z88 result file import

--- src/Mod/Fem/ccxFrdReader.py
+++ src/Mod/Fem/ccxFrdReader.py
@@ -443,7 +443,9 @@ def importFrd(filename, analysis=None, result_name_prefix=None):
                 results_name = result_name_prefix + 'time_' + str(step_time) + '_results'
             else:
                 results_name = result_name_prefix + 'results'
-            results = FreeCAD.ActiveDocument.addObject('Fem::FemResultObject', results_name)
+            import FemResult
+            results = FemResult.makeFemResult(results_name)
+            #results = FreeCAD.ActiveDocument.addObject('Fem::FemResultObject', results_name)
             for m in analysis_object.Member:
                 if m.isDerivedFrom("Fem::FemMeshObject"):
                     results.Mesh = m
