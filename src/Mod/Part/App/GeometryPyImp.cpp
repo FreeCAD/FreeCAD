91b5788a42bc7fe90858b8d4387c63709700bc75
wmayer <wmayer@users.sourceforge.net>
2017-04-08 16:56:59 +0200

    expose Geometry::getTag to Python

--- src/Mod/Part/App/GeometryPyImp.cpp
+++ src/Mod/Part/App/GeometryPyImp.cpp
@@ -35,6 +35,8 @@
 # include <Standard_Failure.hxx>
 #endif
 
+#include <boost/uuid/uuid_io.hpp>
+
 #include <Base/GeometryPyCXX.h>
 #include <Base/Matrix.h>
 #include <Base/MatrixPy.h>
@@ -219,6 +221,12 @@ void  GeometryPy::setConstruction(Py::Boolean arg)
 {
     if(getGeometryPtr()->getClassTypeId() != Part::GeomPoint::getClassTypeId())
         getGeometryPtr()->Construction = arg;
+}
+
+Py::String GeometryPy::getTag(void) const
+{
+    std::string tmp = boost::uuids::to_string(getGeometryPtr()->getTag());
+    return Py::String(tmp);
 }
 
 PyObject *GeometryPy::getCustomAttributes(const char* /*attr*/) const
