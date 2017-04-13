a5452e936300232f91b16750574223d3203d37a7
wmayer <wmayer@users.sourceforge.net>
2017-04-13 12:29:35 +0200

    polish sewing feature

--- src/Mod/Surface/App/FeatureSewing.h
+++ src/Mod/Surface/App/FeatureSewing.h
@@ -49,11 +49,6 @@ public:
     // recalculate the feature
     App::DocumentObjectExecReturn *execute(void);
     short mustExecute() const;
-    /// returns the type name of the view provider
-//    const char* getViewProviderName(void) const {
-//        return "PartGui::ViewProviderSewing";
-//    }
-
 };
 
 }//Namespace Surface
