0ce4d8196d96bad60675b01f2c17dab50ca0c60e
DeepSOIC <vv.titov@gmail.com>
2017-05-03 03:24:45 +0300

    App: Container: disable objects available as attributes
    Because it didn't work very well.
    The main problem was that built-in attributes were not in __dict__, so:
    a) accessing them triggered object lookup every time, which is a bit
    expensive;
    b) objects named same as attributes will screen out the attributes.
    
    Same problems exist with DocumentPy, BTW.

--- src/App/Containers/ContainerBasePyImp.cpp
+++ src/App/Containers/ContainerBasePyImp.cpp
@@ -252,24 +252,7 @@ PyObject* ContainerBasePy::isADocumentObject(PyObject* args)
 
 PyObject* ContainerBasePy::getCustomAttributes(const char* attr) const
 {
-    //objects accessible as attributes of Container...
-
-    if (this->ob_type->tp_dict == NULL) {
-        if (PyType_Ready(this->ob_type) < 0)
-            return 0;
-    }
-    PyObject* item = PyDict_GetItemString(this->ob_type->tp_dict, attr);
-    if (item) return 0; //don't replace an existing attribute if object name happens to match it...
-
-    if (getContainerBasePtr()->isNull())
-        return nullptr;
-    // search for an object with this name
-    try {
-        DocumentObject* obj = getContainerBasePtr()->getObject(attr);
-        return obj->getPyObject();
-    } catch (ObjectNotFoundError&){
-        return nullptr;
-    }
+    return 0;
 }
 
 int ContainerBasePy::setCustomAttributes(const char*,PyObject*)
