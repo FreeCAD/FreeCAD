3dcf71aee41cf9d83c91a406d2c392d5f4dd7ae5
Stefan Tröger <stefantroeger@gmx.net>
2017-07-21 08:34:10 +0200

    PartDesign: ShapeBinder can link globally

--- src/Mod/PartDesign/App/ShapeBinder.h
+++ src/Mod/PartDesign/App/ShapeBinder.h
@@ -47,7 +47,7 @@ public:
     ShapeBinder();
     virtual ~ShapeBinder();
 
-    App::PropertyLinkSubList    Support;
+    App::PropertyLinkSubListGlobal    Support;
 
     static void getFilteredReferences(App::PropertyLinkSubList* prop, Part::Feature*& object, std::vector< std::string >& subobjects);
     static Part::TopoShape buildShapeFromReferences(Feature* obj, std::vector< std::string > subs);
