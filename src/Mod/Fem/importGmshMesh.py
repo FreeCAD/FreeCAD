8d2853cb99d1b8ead9fd777aea10bed7da0d9ca3
qingfengxia <qingfeng.xia@gmail.com>
2017-08-15 00:01:11 +0100

    FEM: bugfix mixed str and unicode in commandlist for subprocess.Popen()

--- src/Mod/Fem/importGmshMesh.py
+++ src/Mod/Fem/importGmshMesh.py
@@ -67,39 +67,45 @@ def export(objectslist, fileString):
 
 
 def _run_command(comandlist):
-    # print(comandlist)
+    print("Run command: " + ' '.join(comandlist))
     error = None
     try:
         p = subprocess.Popen(comandlist, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output, error = p.communicate()
-        # print(output)  # stdout is still cut at some point but the warnings are in stderr and thus printed :-)
-        # print(error)
-    except:
-        error = 'Error executing: {}\n'.format(' '.joint(comandlist))
+        #print(output)  # stdout is still cut at some point but the warnings are in stderr and thus printed :-)
+        #print(error)
+    except Exception as e:
+        error = 'Error executing: {}\n'.format(' '.join(comandlist))
+        print(e)
         FreeCAD.Console.PrintError(error)
     return error
+
 
 def export_fenics_mesh(obj, meshfileString):
     if not meshfileString[-4:] == ".xml":
         error = "Error: only xml mesh is supported by gmsh conversion"
         FreeCAD.Console.PrintError(error)
         return error
-    meshfileStem = meshfileString[:-4]
+    meshfileStem = meshfileString[:-4]  #.encode('ascii')  # FIXME
 
     gmsh = FemGmshTools.FemGmshTools(obj, FemGui.getActiveAnalysis())
-    meshfile = gmsh.export_mesh("Gmsh MSH", meshfileStem + ".msh")
-    if not meshfile:
-        error = "Mesh is not written to `{}` by Gmsh\n".format(meshfile)
+    meshfile = gmsh.export_mesh(u"Gmsh MSH", meshfileStem + ".msh")
+    if meshfile:
+        msg = "Info: Mesh has been written to `{}` by Gmsh\n".format(meshfile)
+        FreeCAD.Console.PrintMessage(msg)
+        # once Fenics is installed, dolfin-convert should be in path
+        comandlist = [u'dolfin-convert', u'-i gmsh', meshfileStem + u".msh", meshfileStem + u".xml"]
+        # mixed str and unicode cause error in comandlist
+        error = _run_command(comandlist)
+        if not os.path.exists(meshfileStem+"_physical_region.xml"):
+            FreeCAD.Console.PrintWarning("Mesh  boundary file `{}` not generated\n".format(meshfileStem+"_physical_region.xml"))
+        if error:
+            return error
+    else:
+        error = "Failed to write mesh file `{}` by Gmsh\n".format(meshfileString)
         FreeCAD.Console.PrintError(errror)
         return error
 
-    comandlist = ['dolfin-convert', '-i gmsh', meshfileStem + ".msh", meshfileStem + ".xml"]
-    error = _run_command(comandlist)
-    if error:
-        return error
-
-    if not os.path.exists(meshfileStem+"_physical_region.xml"):
-        FreeCAD.Console.PrintWarning("Warning: Mesh  boundary file `{}` is not exported by Gmsh\n".format(meshfileStem+"_physical_region.xml"))
 
 def show_fenics_mesh(fname):
     # boundary layer, quad element is not supported
@@ -115,14 +121,17 @@ def show_fenics_mesh(fname):
     plot(mesh)
     interactive()
 
-def export_foam_mesh(obj, foamCaseFolder):
-    # support only 3D only
+
+def export_foam_mesh(obj, meshfileString):
+    # support only 3D
     gmsh = FemGmshTools.FemGmshTools(obj, FemGui.getActiveAnalysis())
-    meshfile = gmsh.export_mesh("Gmsh MSH")
-    if not meshfile:
-        error = "Mesh is NOT written to `{}` by Gmsh\n".format(ret)
+    meshfile = gmsh.export_mesh(u"Gmsh MSH", meshfileString)
+    if meshfile:
+        msg = "Info: Mesh is not written to `{}` by Gmsh\n".format(meshfile)
+        FreeCAD.Console.PrintMessage(msg)
+        comandlist = ['gmshToFoam', '-case', foamCaseFolder, meshfileStem + ".msh"]
+        return _run_command(comandlist)
+    else:
+        error = "Mesh is NOT written to `{}` by Gmsh\n".format(meshfileString)
         FreeCAD.Console.PrintError(errror)
         return error
-
-    comandlist = ['gmshToFoam', '-case', foamCaseFolder, meshfileStem + ".msh"]
-    return _run_command(comandlist)
