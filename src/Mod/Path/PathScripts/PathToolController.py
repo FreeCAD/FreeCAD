0e81583e3f44761e2a26b6bbd9106c79e730157f
Markus Lampert <markus@bibi.ca>
2017-08-20 17:20:55 -0700

    Set object's Restore flag while restoring the Path property.

--- src/Mod/Path/PathScripts/PathToolController.py
+++ src/Mod/Path/PathScripts/PathToolController.py
@@ -134,8 +134,8 @@ class ToolController:
     def onChanged(self, obj, prop):
         PathLog.track('prop: {}  state: {}'.format(prop, obj.State))
 
-
-        if 'Restore' not in obj.State:
+        if 'Path' == prop and 'Restore' not in obj.State:
+            PathLog.debug("--- dirty deeds")
             job = PathScripts.PathUtils.findParentJob(obj)
             if job is not None:
                 for g in job.Group:
