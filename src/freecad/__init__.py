6cf54fa6765a6a84ddfb1c73f5936e1a8c4f888e
looooo <sppedflyer@gmail.com>
2017-08-22 23:21:58 +0200

    adding the possebility to import from "freecad" 1. any module having problems with nameclashes can use this syntax (from freecad import module) 2. old style import should still work, structure of modules won't change) 3. the new directory "freecad" can be imported from python directly, as it will be installed in std. path of python (eg.: site-packages)

---
+++ src/freecad/__init__.py
@@ -0,0 +1,13 @@
+try:
+    # if FreeCADInit.py was called allready, we can import FreeCAD directly
+    # as the sys.path is setup
+    import FreeCAD as App
+except ImportError:
+    # if we want to import from freecad without starting the gui,
+    # we first have to find the FreeCAD (shared-library)
+    # the location of this file was setup with cmake.
+    import sys
+    __path__.append("@CMAKE_INSTALL_LIBDIR@")
+    from .import FreeCAD as App
+
+__path__.append(App.getHomePath() + 'Mod')
