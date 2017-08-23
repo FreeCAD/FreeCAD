9c89b0aaf0301d27097b121b0628cbbeb4bbe0f4
looooo <sppedflyer@gmail.com>
2017-08-23 19:22:03 +0200

    new-style modules: consistency and import warnings - switch back to freecad.App and freecad.Gui (consistency with other modules) - add a warning if modules (Part/ PartDesign) are imported directly

--- src/Mod/PartDesign/__init__.py
+++ src/Mod/PartDesign/__init__.py
@@ -1,2 +1,7 @@
 import _PartDesign   # TODO: where is this needed?
 makeFilletArc = _PartDesign.makeFilletArc
+
+
+if not "freecad" in __package__:
+    import warnings
+    warnings.warn("use 'from freecad import PartDesign'", FutureWarning, stacklevel=2)
--- src/freecad/__init__.py
+++ src/freecad/__init__.py
@@ -1,13 +1,15 @@
 try:
     # if FreeCADInit.py was called allready, we can import FreeCAD directly
     # as the sys.path is setup
-    import FreeCAD as app
+    import FreeCAD as App
+    import FreeCADGui as Gui
 except ImportError:
     # if we want to import from freecad without starting the gui,
     # we first have to find the FreeCAD (shared-library)
     # the location of this file was setup with cmake.
     import sys
     __path__.append("@CMAKE_INSTALL_LIBDIR@")
-    from .import FreeCAD as app
+    from .import FreeCAD as App
+    from .import FreeCADGui as Gui
 
-__path__.append(app.getHomePath() + 'Mod')
+__path__.append(App.getHomePath() + 'Mod')
