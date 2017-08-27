467ef5578879eda3f691817971615c33679174dc
luzpaz <luzpaz@@users.noreply.github.com>
2017-08-27 13:40:54 -0400

    Misc. typos discovered via crowdin
    should close several crowdin issues.

--- src/Mod/Arch/Resources/translations/Arch.ts
+++ src/Mod/Arch/Resources/translations/Arch.ts
@@ -2488,7 +2488,7 @@ Site creation aborted.</source>
     </message>
     <message>
         <location filename="ArchMaterial.ui" line="38"/>
-        <source>Copy values from an existing mateiral in the document</source>
+        <source>Copy values from an existing material in the document</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -3559,7 +3559,7 @@ Site creation aborted.</source>
     </message>
     <message>
         <location filename="preferences-arch.ui" line="223"/>
-        <source>Tolerance value to use when checking if 2 adjacent faces as planar</source>
+        <source>Tolerance value to use when checking if 2 adjacent faces are planar</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
