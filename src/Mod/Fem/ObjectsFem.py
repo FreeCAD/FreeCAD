b11e4616c5cf096e143365da3ec460022b556c70
Bernd Hahnebach <bernd@bimstatik.org>
2017-08-28 07:41:45 +0200

    FEM: make objects module, fix name for new ccx solver

--- src/Mod/Fem/ObjectsFem.py
+++ src/Mod/Fem/ObjectsFem.py
@@ -343,7 +343,7 @@ def makeSolverCalculixOld(doc, name="CalculiX"):
     return obj
 
 
-def makeSolverCalculix(doc, name="CalculiX"):
+def makeSolverCalculix(doc, name="SolverCalculiX"):
     '''makeSolverCalculix(document, name): makes a Calculix solver object'''
     import FemCalculix.SolverObject
     obj = FemCalculix.SolverObject.create(doc, name)
