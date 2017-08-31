9ce734eeba19561b807a4672d78d45fab3ce9945
wmayer <wmayer@users.sourceforge.net>
2017-09-01 01:18:44 +0200

    port smesh to occ7.2

--- src/3rdParty/salomesmesh/src/Controls/SMESH_Controls.cpp
+++ src/3rdParty/salomesmesh/src/Controls/SMESH_Controls.cpp
@@ -4293,7 +4293,7 @@ bool ElementsOnShape::TClassifier::isOutOfFace  (const gp_Pnt& p)
   if ( myProjFace.IsDone() && myProjFace.LowerDistance() <= myTol )
   {
     // check relatively to the face
-    Quantity_Parameter u, v;
+    Standard_Real u, v;
     myProjFace.LowerDistanceParameters(u, v);
     gp_Pnt2d aProjPnt (u, v);
     BRepClass_FaceClassifier aClsf ( TopoDS::Face( myShape ), aProjPnt, myTol );
