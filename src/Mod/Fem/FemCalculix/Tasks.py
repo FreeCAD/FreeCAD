4af293b8428ebcc4fef3a180c29ff9153ace055a
Bernd Hahnebach <bernd@bimstatik.org>
2017-09-10 14:00:17 +0200

    FEM: py3 even more import fixes

--- src/Mod/Fem/FemCalculix/Tasks.py
+++ src/Mod/Fem/FemCalculix/Tasks.py
@@ -57,7 +57,7 @@ class Prepare(FemRun.Prepare):
         global _inputFileName
         self.pushStatus("Preparing input files...\n")
         c = _Container(self.analysis)
-        writer = Writer.FemInputWriterCcx(
+        writer = FemCalculix.Writer.FemInputWriterCcx(
             self.analysis, self.solver, c.mesh, c.materials_linear,
             c.materials_nonlinear, c.fixed_constraints,
             c.displacement_constraints, c.contact_constraints,
