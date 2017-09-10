336bd47c91ae1c867ff1651ff80f0a7bfe7c10cf
wmayer <wmayer@users.sourceforge.net>
2017-09-10 17:08:23 +0200

    load/save setting in techdraw panel

--- src/Mod/TechDraw/Gui/DlgPrefsTechDrawImp.cpp
+++ src/Mod/TechDraw/Gui/DlgPrefsTechDrawImp.cpp
@@ -47,6 +47,7 @@ void DlgPrefsTechDrawImp::saveSettings()
     cb_Angle->onSave();
     cb_Faces->onSave();
     cb_SectionEdges->onSave();
+    cb_PageUpdate->onSave();
     dsb_TemplateDot->onSave();
 
     pcb_Normal->onSave();
@@ -73,6 +74,7 @@ void DlgPrefsTechDrawImp::loadSettings()
     cb_Angle->onRestore();
     cb_Faces->onRestore();
     cb_SectionEdges->onRestore();
+    cb_PageUpdate->onRestore();
     dsb_TemplateDot->onRestore();
 
     pcb_Normal->onRestore();
