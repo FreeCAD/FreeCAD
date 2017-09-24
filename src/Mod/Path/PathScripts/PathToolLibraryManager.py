b365e60c1785e3cc897a8d27c2a9f587919239e7
Markus Lampert <markus@bibi.ca>
2017-09-24 16:15:24 -0700

    Fixed class name typo - thanks pylint.

--- src/Mod/Path/PathScripts/PathToolLibraryManager.py
+++ src/Mod/Path/PathScripts/PathToolLibraryManager.py
@@ -264,7 +264,7 @@ class ToolLibraryManager():
             if fileExtension == '.tooltable':
                 xmlHandler = HeeksTooltableHandler()
             if fileExtension == '.xml':
-                xmlHandler = FeeCADTooltableHandler()
+                xmlHandler = FreeCADTooltableHandler()
 
             if xmlHandler:
                 parser = xml.sax.make_parser()
