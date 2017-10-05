86b925744e0677f17a56fa78aaf69bb95ec83551
Bernd Hahnebach <bernd@bimstatik.org>
2017-10-05 05:58:54 +0100

    FEM: objects fem module, fix comments

--- src/Mod/Fem/ObjectsFem.py
+++ src/Mod/Fem/ObjectsFem.py
@@ -32,7 +32,7 @@ import FreeCAD
 
 ########## analysis objects ##########
 def makeAnalysis(doc, name="Analysis"):
-    '''makeAnalysis(name): makes a Fem Analysis object'''
+    '''makeAnalysis(document, [name]): makes a Fem Analysis object'''
     obj = doc.addObject("Fem::FemAnalysisPython", name)
     if FreeCAD.GuiUp:
         obj.ViewObject.Proxy=0
@@ -41,79 +41,79 @@ def makeAnalysis(doc, name="Analysis"):
 
 ########## constraint objects ##########
 def makeConstraintBearing(doc, name="ConstraintBearing"):
-    '''makeConstraintBearing(name): makes a Fem ConstraintBearing object'''
+    '''makeConstraintBearing(document, [name]): makes a Fem ConstraintBearing object'''
     obj = doc.addObject("Fem::ConstraintBearing", name)
     return obj
 
 
 def makeConstraintContact(doc, name="ConstraintContact"):
-    '''makeConstraintContact(name): makes a Fem ConstraintContact object'''
+    '''makeConstraintContact(document, [name]): makes a Fem ConstraintContact object'''
     obj = doc.addObject("Fem::ConstraintContact", name)
     return obj
 
 
 def makeConstraintDisplacement(doc, name="ConstraintDisplacement"):
-    '''makeConstraintDisplacement(name): makes a Fem ConstraintDisplacement object'''
+    '''makeConstraintDisplacement(document, [name]): makes a Fem ConstraintDisplacement object'''
     obj = doc.addObject("Fem::ConstraintDisplacement", name)
     return obj
 
 
 def makeConstraintFixed(doc, name="ConstraintFixed"):
-    '''makeConstraintFixed(name): makes a Fem ConstraintFixed object'''
+    '''makeConstraintFixed(document, [name]): makes a Fem ConstraintFixed object'''
     obj = doc.addObject("Fem::ConstraintFixed", name)
     return obj
 
 
 def makeConstraintFluidBoundary(doc, name="ConstraintFluidBoundary"):
-    '''makeConstraintFluidBoundary(name): makes a Fem ConstraintForce object'''
+    '''makeConstraintFluidBoundary(document, name): makes a Fem ConstraintForce object'''
     obj = doc.addObject("Fem::ConstraintForce", name)
     return obj
 
 
 def makeConstraintForce(doc, name="ConstraintForce"):
-    '''makeConstraintForce(name): makes a Fem ConstraintForce object'''
+    '''makeConstraintForce(document, [name]): makes a Fem ConstraintForce object'''
     obj = doc.addObject("Fem::ConstraintForce", name)
     return obj
 
 
 def makeConstraintGear(doc, name="ConstraintGear"):
-    '''makeConstraintGear(name): makes a Fem ConstraintGear object'''
+    '''makeConstraintGear(document, [name]): makes a Fem ConstraintGear object'''
     obj = doc.addObject("Fem::ConstraintGear", name)
     return obj
 
 
 def makeConstraintHeatflux(doc, name="ConstraintHeatflux"):
-    '''makeConstraintHeatflux(name): makes a Fem ConstraintHeatflux object'''
+    '''makeConstraintHeatflux(document, [name]): makes a Fem ConstraintHeatflux object'''
     obj = doc.addObject("Fem::ConstraintHeatflux", name)
     return obj
 
 
 def makeConstraintInitialTemperature(doc, name="ConstraintInitialTemperature"):
-    '''makeConstraintInitialTemperature(name): makes a Fem ConstraintInitialTemperature object'''
+    '''makeConstraintInitialTemperature(document, name): makes a Fem ConstraintInitialTemperature object'''
     obj = doc.addObject("Fem::ConstraintInitialTemperature", name)
     return obj
 
 
 def makeConstraintPlaneRotation(doc, name="ConstraintPlaneRotation"):
-    '''makeConstraintPlaneRotation(name): makes a Fem ConstraintPlaneRotation object'''
+    '''makeConstraintPlaneRotation(document, [name]): makes a Fem ConstraintPlaneRotation object'''
     obj = doc.addObject("Fem::ConstraintPlaneRotation", name)
     return obj
 
 
 def makeConstraintPressure(doc, name="ConstraintPressure"):
-    '''makeConstraintPressure(name): makes a Fem ConstraintPressure object'''
+    '''makeConstraintPressure(document, [name]): makes a Fem ConstraintPressure object'''
     obj = doc.addObject("Fem::ConstraintPressure", name)
     return obj
 
 
 def makeConstraintPulley(doc, name="ConstraintPulley"):
-    '''makeConstraintPulley(name): makes a Fem ConstraintPulley object'''
+    '''makeConstraintPulley(document, [name]): makes a Fem ConstraintPulley object'''
     obj = doc.addObject("Fem::ConstraintPulley", name)
     return obj
 
 
 def makeConstraintSelfWeight(doc, name="ConstraintSelfWeight"):
-    '''makeConstraintSelfWeight([name]): creates an self weight object to define a gravity load'''
+    '''makeConstraintSelfWeight(document, [name]): creates an self weight object to define a gravity load'''
     obj = doc.addObject("Fem::ConstraintPython", name)
     import PyObjects._FemConstraintSelfWeight
     PyObjects._FemConstraintSelfWeight._FemConstraintSelfWeight(obj)
@@ -124,20 +124,20 @@ def makeConstraintSelfWeight(doc, name="ConstraintSelfWeight"):
 
 
 def makeConstraintTemperature(doc, name="ConstraintTemperature"):
-    '''makeConstraintTemperature(name): makes a Fem ConstraintTemperature object'''
+    '''makeConstraintTemperature(document, [name]): makes a Fem ConstraintTemperature object'''
     obj = doc.addObject("Fem::ConstraintTemperature", name)
     return obj
 
 
 def makeConstraintTransform(doc, name="ConstraintTransform"):
-    '''makeConstraintTransform(name): makes a Fem ConstraintTransform object'''
+    '''makeConstraintTransform(document, [name]): makes a Fem ConstraintTransform object'''
     obj = doc.addObject("Fem::ConstraintTransform", name)
     return obj
 
 
 ########## element definition objects ##########
 def makeElementFluid1D(doc, name="ElementFluid1D"):
-    '''makeElementFluid1D([name]): creates an 1D fluid element object to define 1D flow'''
+    '''makeElementFluid1D(document, [name]): creates an 1D fluid element object to define 1D flow'''
     obj = doc.addObject("Fem::FeaturePython", name)
     import PyObjects._FemElementFluid1D
     PyObjects._FemElementFluid1D._FemElementFluid1D(obj)
@@ -148,7 +148,7 @@ def makeElementFluid1D(doc, name="ElementFluid1D"):
 
 
 def makeElementGeometry1D(doc, sectiontype='Rectangular', width=10.0, height=25.0, name="ElementGeometry1D"):
-    '''makeElementGeometry1D([width], [height], [name]): creates an 1D geometry element object to define a cross section'''
+    '''makeElementGeometry1D(document, [width], [height], [name]): creates an 1D geometry element object to define a cross section'''
     obj = doc.addObject("Fem::FeaturePython", name)
     import PyObjects._FemElementGeometry1D
     PyObjects._FemElementGeometry1D._FemElementGeometry1D(obj)
@@ -170,7 +170,7 @@ def makeElementGeometry1D(doc, sectiontype='Rectangular', width=10.0, height=25.
 
 
 def makeElementGeometry2D(doc, thickness=20.0, name="ElementGeometry2D"):
-    '''makeElementGeometry2D([thickness], [name]): creates an 2D geometry element object to define a plate thickness'''
+    '''makeElementGeometry2D(document, [thickness], [name]): creates an 2D geometry element object to define a plate thickness'''
     obj = doc.addObject("Fem::FeaturePython", name)
     import PyObjects._FemElementGeometry2D
     PyObjects._FemElementGeometry2D._FemElementGeometry2D(obj)
@@ -183,7 +183,7 @@ def makeElementGeometry2D(doc, thickness=20.0, name="ElementGeometry2D"):
 
 ########## material objects ##########
 def makeMaterialFluid(doc, name="FluidMaterial"):
-    '''makeMaterialFluid(name): makes an FEM Material for fluid'''
+    '''makeMaterialFluid(document, [name]): makes an FEM Material for fluid'''
     obj = doc.addObject("App::MaterialObjectPython", name)
     import PyObjects._FemMaterial
     PyObjects._FemMaterial._FemMaterial(obj)
@@ -196,7 +196,7 @@ def makeMaterialFluid(doc, name="FluidMaterial"):
 
 
 def makeMaterialMechanicalNonlinear(doc, base_material, name="MechanicalMaterialNonlinear"):
-    '''makeMaterialMechanicalNonlinear(base_material, [name]): creates an nonlinear material object'''
+    '''makeMaterialMechanicalNonlinear(document, base_material, [name]): creates an nonlinear material object'''
     obj = doc.addObject("Fem::FeaturePython", name)
     import PyObjects._FemMaterialMechanicalNonlinear
     PyObjects._FemMaterialMechanicalNonlinear._FemMaterialMechanicalNonlinear(obj)
@@ -208,7 +208,7 @@ def makeMaterialMechanicalNonlinear(doc, base_material, name="MechanicalMaterial
 
 
 def makeMaterialSolid(doc, name="MechanicalSolidMaterial"):
-    '''makeMaterialSolid(name): makes an FEM Material for solid'''
+    '''makeMaterialSolid(document, [name]): makes an FEM Material for solid'''
     obj = doc.addObject("App::MaterialObjectPython", name)
     import PyObjects._FemMaterial
     PyObjects._FemMaterial._FemMaterial(obj)
@@ -222,11 +222,10 @@ def makeMaterialSolid(doc, name="MechanicalSolidMaterial"):
 
 ########## mesh objects ##########
 def makeMeshBoundaryLayer(doc, base_mesh, name="MeshBoundaryLayer"):
-    '''makeMeshBoundaryLayer([length], [name]): creates a  FEM mesh BoundaryLayer object to define boundary layer properties'''
+    '''makeMeshBoundaryLayer(document, base_mesh, [name]): creates a  FEM mesh BoundaryLayer object to define boundary layer properties'''
     obj = doc.addObject("Fem::FeaturePython", name)
     import PyObjects._FemMeshBoundaryLayer
     PyObjects._FemMeshBoundaryLayer._FemMeshBoundaryLayer(obj)
-
     # obj.BaseMesh = base_mesh
     # App::PropertyLinkList does not support append, we will use a temporary list to append the mesh BoundaryLayer obj. to the list
     tmplist = base_mesh.MeshBoundaryLayerList
@@ -239,7 +238,7 @@ def makeMeshBoundaryLayer(doc, base_mesh, name="MeshBoundaryLayer"):
 
 
 def makeMeshGmsh(doc, name="FEMMeshGMSH"):
-    '''makeMeshGmsh(name): makes a GMSH FEM mesh object'''
+    '''makeMeshGmsh(document, [name]): makes a GMSH FEM mesh object'''
     obj = doc.addObject("Fem::FemMeshObjectPython", name)
     import PyObjects._FemMeshGmsh
     PyObjects._FemMeshGmsh._FemMeshGmsh(obj)
@@ -250,7 +249,7 @@ def makeMeshGmsh(doc, name="FEMMeshGMSH"):
 
 
 def makeMeshGroup(doc, base_mesh, use_label=False, name="FEMMeshGroup"):
-    '''makeMeshGroup([length], [name]): creates a  FEM mesh region object to define properties for a regon of a FEM mesh'''
+    '''makeMeshGroup(document, base_mesh, [use_label], [name]): creates a  FEM mesh region object to define properties for a regon of a FEM mesh'''
     obj = doc.addObject("Fem::FeaturePython", name)
     import PyObjects._FemMeshGroup
     PyObjects._FemMeshGroup._FemMeshGroup(obj)
@@ -267,13 +266,13 @@ def makeMeshGroup(doc, base_mesh, use_label=False, name="FEMMeshGroup"):
 
 
 def makeMeshNetgen(doc, name="FEMMeshNetgen"):
-    '''makeMeshNetgen(name): makes a Fem MeshShapeNetgenObject object'''
+    '''makeMeshNetgen(document, [name]): makes a Fem MeshShapeNetgenObject object'''
     obj = doc.addObject("Fem::FemMeshShapeNetgenObject", name)
     return obj
 
 
 def makeMeshRegion(doc, base_mesh, element_length=0.0, name="FEMMeshRegion"):
-    '''makeMeshRegion([length], [name]): creates a  FEM mesh region object to define properties for a regon of a FEM mesh'''
+    '''makeMeshRegion(document, base_mesh, [element_length] [name]): creates a  FEM mesh region object to define properties for a regon of a FEM mesh'''
     obj = doc.addObject("Fem::FeaturePython", name)
     import PyObjects._FemMeshRegion
     PyObjects._FemMeshRegion._FemMeshRegion(obj)
@@ -290,7 +289,7 @@ def makeMeshRegion(doc, base_mesh, element_length=0.0, name="FEMMeshRegion"):
 
 
 def makeMeshResult(doc, name="FEMMeshResult"):
-    '''(name): makes a Fem MeshResult object'''
+    '''makeMeshResult(document, name): makes a Fem MeshResult object'''
     obj = doc.addObject("Fem::FemMeshObjectPython", name)
     import PyObjects._FemMeshResult
     PyObjects._FemMeshResult._FemMeshResult(obj)
@@ -302,7 +301,7 @@ def makeMeshResult(doc, name="FEMMeshResult"):
 
 ########## result objects ##########
 def makeResultMechanical(doc, name="MechanicalResult"):
-    '''makeResultMechanical(name): creates an mechanical result object to hold FEM results'''
+    '''makeResultMechanical(document, [name]): creates an mechanical result object to hold FEM results'''
     obj = doc.addObject('Fem::FemResultObjectPython', name)
     import PyObjects._FemResultMechanical
     PyObjects._FemResultMechanical._FemResultMechanical(obj)
@@ -314,7 +313,7 @@ def makeResultMechanical(doc, name="MechanicalResult"):
 
 ########## solver objects ##########
 def makeSolverCalculix(doc, name="CalculiX"):
-    '''makeSolverCalculix(name): makes a Calculix solver object'''
+    '''makeSolverCalculix(document, [name]): makes a Calculix solver object'''
     obj = doc.addObject("Fem::FemSolverObjectPython", name)
     import PyObjects._FemSolverCalculix
     PyObjects._FemSolverCalculix._FemSolverCalculix(obj)
@@ -325,7 +324,7 @@ def makeSolverCalculix(doc, name="CalculiX"):
 
 
 def makeSolverZ88(doc, name="Z88"):
-    '''makeSolverZ88(name): makes a Z88 solver object'''
+    '''makeSolverZ88(document, [name]): makes a Z88 solver object'''
     obj = doc.addObject("Fem::FemSolverObjectPython", name)
     import PyObjects._FemSolverZ88
     PyObjects._FemSolverZ88._FemSolverZ88(obj)
