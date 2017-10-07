ad5b78ed113d2fd141cb6579d2b7bd982915be59
Bernd Hahnebach <bernd@bimstatik.org>
2017-10-07 21:18:50 +0200

    FEM: set back some rebase changes

--- src/Mod/Fem/PyGui/_CommandFemAnalysis.py
+++ src/Mod/Fem/PyGui/_CommandFemAnalysis.py
@@ -49,8 +49,8 @@ class _CommandFemAnalysis(FemCommands):
         FreeCADGui.addModule("ObjectsFem")
         FreeCADGui.doCommand("ObjectsFem.makeAnalysis(FreeCAD.ActiveDocument, 'Analysis')")
         FreeCADGui.doCommand("FemGui.setActiveAnalysis(FreeCAD.ActiveDocument.ActiveObject)")
-
         ccx_prefs = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Mod/Fem/Ccx")
+        use_old_solver_frame_work = ccx_prefs.GetBool("useOldSolverFrameWork", False)
         use_new_solver_frame_work = ccx_prefs.GetBool("useNewSolverFrameWork", True)
         if use_old_solver_frame_work and not use_new_solver_frame_work:
             FreeCADGui.doCommand("ObjectsFem.makeSolverCalculixOld(FreeCAD.ActiveDocument)")
