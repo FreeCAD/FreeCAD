fd092ef080d08b50a24661413b15dc917efb4c7e
Unknown <kunda@scribus.net>
2017-10-16 13:52:18 -0400

    changed bash formatting to get token to work
    according to @bblacey on gitter.im
    + added a brief intro when bot finds typos

--- spellcheck_code.sh
+++ spellcheck_code.sh
@@ -6,23 +6,28 @@
 # 2. Needs a repo_public token as an ENV variable that is hidden
 # 3. requires most up to date codespell: pip install --user --upgrade git+https://github.com/lucasdemarchi/codespell.git
 
+INTRO="Hi! I'm an automated TravisCI build script bot that reports PR typos. 
+If your PR has typos the build will continue neverthless, *but* 
+we ask you to please provide a follow-up fix. FYI sometimes I report false positives.
+"
+
 # For colorizing output. When using them, we need to terminate with $NC to reset back to default 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 BLUE='\033[0;36m'
 NC='\033[0m' # No Color
 
-# Categorized files/directories that we want codespell to skip 
+# Categorized FreeCAD files/dirs that we want codespell to skip 
 TRANSLATION_FILES="*.po,*.ts"
 THIRDRDPARTY_FILES="./src/3rdParty,./src/zipios++,./src/CXX"
 MISC_FILES="./git,./src/Mod/Assembly/App/opendcm,./src/Mod/Cam/App"
 
-# Output what files/directories we will be skipping
+# Output what FreeCAD files/dirs we will be skipping
 echo -e "$BLUE>> Skipping all translation files: $TRANSLATION_FILES $NC\n"
 echo -e "$BLUE>> Skipping all 3 party directories: $THIRDPARTY_FILES $NC\n"
 echo -e "$BLUE>> Skipping all misc. directories: $MISC_FILES $NC\n"
 
-# consolidate skipped file/directories in to 1 variable
+# consolidate skipped file/dirs all in to 1 variable
 CODESPELL_SKIPS="$TRANSLATION_FILES,$THIRDPARTY_FILES,$MISC_FILES"
 
 # Round up all changed content
@@ -51,9 +56,9 @@ CODESPELL_RESULTS=`cat codespell_results.txt`
 
 # Send results back to the PR comment
 echo -e "$BLUE>> Sending results in a comment on the Github pull request #$TRAVIS_PULL_REQUEST:$NC"
-curl -i -H "Authorization: token $GH_TOKEN" \
+curl -i -H "Authorization: token ${GH_TOKEN}" \
     -H "Content-Type: application/json" \
-    -X POST -d "{\"body\":\"$CODESPELL_RESULTS\"}" \
+    -X POST -d "{\"body\":\"$INTRO\"\"$CODESPELL_RESULTS\"}" \
     https://api.github.com/repos/FreeCAD/FreeCAD/issues/$TRAVIS_PULL_REQUEST/comments
 
 # Clean up
