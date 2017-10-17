2b03cc7866edadc04faa22df891636cc2497d9c2
wmayer <wmayer@users.sourceforge.net>
2017-10-17 19:13:09 +0200

    fixes #0003169: Mesh Design WB tools does not work on translated object

--- src/Mod/Mesh/Gui/MeshSelection.cpp
+++ src/Mod/Mesh/Gui/MeshSelection.cpp
@@ -475,7 +475,11 @@ void MeshSelection::selectGLCallback(void * ud, SoEventCallback * n)
         SoCamera* cam = view->getSoRenderManager()->getCamera();
         SbViewVolume vv = cam->getViewVolume();
         Gui::ViewVolumeProjection proj(vv);
+
+        Base::Placement plm = static_cast<Mesh::Feature*>(vp->getObject())->Placement.getValue();
+        proj.setTransform(plm.toMatrix());
         vp->getFacetsFromPolygon(polygon, proj, true, faces);
+
         if (self->onlyVisibleTriangles) {
             const SbVec2s& sz = view->getSoRenderManager()->getViewportRegion().getWindowSize();
             short width,height; sz.getValue(width,height);
