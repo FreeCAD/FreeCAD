aee970cf1de0a6340620a9744637603c51c125ad
wmayer <wmayer@users.sourceforge.net>
2017-11-27 22:11:13 +0100

    set global link scope of surface features

--- src/Mod/Surface/App/FeatureSewing.cpp
+++ src/Mod/Surface/App/FeatureSewing.cpp
@@ -49,6 +49,8 @@ Sewing::Sewing()
     ADD_PROPERTY_TYPE(DegenerateShape,(true), "Sewing", App::Prop_None, "Analysis of degenerated shapes");
     ADD_PROPERTY_TYPE(CutFreeEdges,(true), "Sewing", App::Prop_None, "Cutting of free edges");
     ADD_PROPERTY_TYPE(Nonmanifold,(false), "Sewing", App::Prop_None, "Non-manifold processing");
+
+    ShapeList.setScope(App::LinkScope::Global);
 }
 
 short Sewing::mustExecute() const
