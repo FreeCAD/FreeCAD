65e7932930ce0eb7e44c3fa8d78dcfac206ba950
wmayer <wmayer@users.sourceforge.net>
2018-01-01 20:51:12 +0100

    improve whitespaces

--- src/Mod/Sketcher/Gui/ViewProviderSketch.cpp
+++ src/Mod/Sketcher/Gui/ViewProviderSketch.cpp
@@ -4708,20 +4708,20 @@ Restart:
                                         p0 = SbVec3f(x,y,0);
                                     }
                                 }
-                                
-				range = Constr->getValue(); // WYSIWYG
-				startangle = atan2(dir1.y,dir1.x);
-                                
-                            } else {//angle-via-point
+
+                                range = Constr->getValue(); // WYSIWYG
+                                startangle = atan2(dir1.y,dir1.x);
+                            }
+                            else {//angle-via-point
                                 Base::Vector3d p = getSketchObject()->getSolvedSketch().getPoint(Constr->Third, Constr->ThirdPos);
                                 p0 = SbVec3f(p.x, p.y, 0);
                                 dir1 = getSketchObject()->getSolvedSketch().calculateNormalAtPoint(Constr->First, p.x, p.y);
                                 dir1.RotateZ(-M_PI/2);//convert to vector of tangency by rotating
                                 dir2 = getSketchObject()->getSolvedSketch().calculateNormalAtPoint(Constr->Second, p.x, p.y);
                                 dir2.RotateZ(-M_PI/2);
-				
-				startangle = atan2(dir1.y,dir1.x);
-				range = atan2(dir1.x*dir2.y-dir1.y*dir2.x,
+
+                                startangle = atan2(dir1.y,dir1.x);
+                                range = atan2(dir1.x*dir2.y-dir1.y*dir2.x,
                                           dir1.x*dir2.x+dir1.y*dir2.y);
                             }
 
