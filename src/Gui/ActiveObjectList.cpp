ccaa87e12aaadbd3106607fc503542a863b742da
Zheng, Lei <realthunder.dev@gmail.com>
2018-02-01 16:33:27 +0800

    ActiveObjectList: allow internal links as active object
    Previous patch (0b10c1a8) only allow setting external links as active
    object. This patch enable internal links as well.
    
    The working mechanism is the same. When some one calls
    View.setActiveObject(obj), ActiveObjectList will check for
    Gui.Selection to see if the current selection is actually a link object
    that links to 'obj'. If so, the link becomes the active object instead
    of 'obj'. When other code calls View.getActiveObject(), the default
    behavior is to resolve the link automatically.

--- src/Gui/ActiveObjectList.cpp
+++ src/Gui/ActiveObjectList.cpp
@@ -70,7 +70,7 @@ Gui::ActiveObjectList::ObjectInfo Gui::ActiveObjectList::getObjectInfo(
     info.obj = 0;
     if(!obj || !obj->getNameInDocument())
         return info;
-    if(subname || obj->getDocument()==_Doc->getDocument()) {
+    if(subname) {
         info.obj = obj;
         if(subname) info.subname = subname;
     }else{
@@ -96,6 +96,8 @@ Gui::ActiveObjectList::ObjectInfo Gui::ActiveObjectList::getObjectInfo(
             }
             if(info.obj) break;
         }
+        if(!info.obj && obj->getDocument()==_Doc->getDocument())
+            info.obj = obj;
     }
     return info;
 }
