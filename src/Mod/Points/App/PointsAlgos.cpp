880a200c700009870244bb2bd819096baf7687ff
wmayer <wmayer@users.sourceforge.net>
2018-02-13 00:27:18 +0100

    improve point import and export

--- src/Mod/Points/App/PointsAlgos.cpp
+++ src/Mod/Points/App/PointsAlgos.cpp
@@ -55,7 +55,7 @@ void PointsAlgos::Load(PointKernel &points, const char *FileName)
     if (!File.isReadable())
         throw Base::FileException("File to load not existing or not readable", FileName);
 
-    if (File.extension() == "asc" ||File.extension() == "ASC")
+    if (File.hasExtension("asc"))
         LoadAscii(points,FileName);
     else
         throw Base::RuntimeError("Unknown ending");
