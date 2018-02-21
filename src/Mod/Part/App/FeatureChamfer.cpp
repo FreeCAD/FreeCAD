727d219bbfe918953f40b0bf02c2456f6898ab1e
Zheng, Lei <realthunder.dev@gmail.com>
2018-02-21 18:32:57 +0800

    Part:: move buildHistory to ShapeHistory class
    This is to prepare for building stable topo name reference in
    TopoShape. The ShapeHistory class should probably moved to TopoShape.h
    later.

--- src/Mod/Part/App/FeatureChamfer.cpp
+++ src/Mod/Part/App/FeatureChamfer.cpp
@@ -73,7 +73,7 @@ App::DocumentObjectExecReturn *Chamfer::execute(void)
         TopoDS_Shape shape = mkChamfer.Shape();
         if (shape.IsNull())
             return new App::DocumentObjectExecReturn("Resulting shape is null");
-        ShapeHistory history = buildHistory(mkChamfer, TopAbs_FACE, shape, baseShape);
+        ShapeHistory history(mkChamfer, TopAbs_FACE, shape, baseShape);
         this->Shape.setValue(shape);
 
         // make sure the 'PropertyShapeHistory' is not safed in undo/redo (#0001889)
