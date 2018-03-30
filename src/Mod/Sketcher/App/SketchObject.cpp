1a8ca1982cdf6dccf8d0b352cb2fd264dd745557
Abdullah Tahiri <abdullah.tahiri.yo@gmail.com>
2018-03-30 16:45:19 +0200

    SketchObject: Backwards compatibility with old property for external links

--- src/Mod/Sketcher/App/SketchObject.cpp
+++ src/Mod/Sketcher/App/SketchObject.cpp
@@ -6138,6 +6138,18 @@ void SketchObject::setExpression(const App::ObjectIdentifier &path, boost::share
         solve();
 }
 
+void SketchObject::handleChangedPropertyType( Base::XMLReader &reader,
+                                              const char * TypeName,
+                                              App::Property * prop)
+{
+    if (prop == &ExternalGeometry && strcmp(TypeName, "App::PropertyLinkSubList") == 0) {
+        App::PropertyLinkSubList v;
+        v.setContainer(this);
+        v.Restore(reader);
+
+        ExternalGeometry.setValues(v.getValues(),v.getSubValues());
+    }
+}
 
 // Python Sketcher feature ---------------------------------------------------------
 
