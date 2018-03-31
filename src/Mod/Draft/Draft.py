121ceaf016cb4d636444fcea2f63c064ae7d0739
Yorik van Havre <yorik@uncreated.net>
2018-03-31 17:21:18 -0300

    Draft: small bugfix in WPProxy

--- src/Mod/Draft/Draft.py
+++ src/Mod/Draft/Draft.py
@@ -3709,6 +3709,7 @@ def makeWorkingPlaneProxy(placement):
             obj.ViewObject.Proxy.writeCamera()
             obj.ViewObject.Proxy.writeState()
         obj.Placement = placement
+        return obj
 
 
 #---------------------------------------------------------------------------
