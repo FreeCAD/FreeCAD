8e8d0d0cba381682a9a4ffd60a36b6dece9e9791
looooo <sppedflyer@gmail.com>
2018-05-18 16:01:57 +0200

    py3: Import: 2to3

--- src/Mod/Import/App/SCL/essa_par.py
+++ src/Mod/Import/App/SCL/essa_par.py
@@ -63,12 +63,12 @@ def process_nested_parent_str2(attr_str,idx=0):
 #print process_nested_parent_str2('1,2,3,4,5,6')
 #idx=0
 #print process_nested_parent_str2("'A','B','C'")
-print process_nested_parent_str2("'A'")[0]
-print process_nested_parent_str2("30.0,0.0,5.0")[0]
-print process_nested_parent_str2("(Thomas)")[0]
-print process_nested_parent_str2("Thomas, Paviot, ouais")[0]
-print process_nested_parent_str2("1,2,(3,4,5),6,7,8")[0]
-print process_nested_parent_str2("(#9149,#9166),#9142,.T.")[0]
+print(process_nested_parent_str2("'A'")[0])
+print(process_nested_parent_str2("30.0,0.0,5.0")[0])
+print(process_nested_parent_str2("(Thomas)")[0])
+print(process_nested_parent_str2("Thomas, Paviot, ouais")[0])
+print(process_nested_parent_str2("1,2,(3,4,5),6,7,8")[0])
+print(process_nested_parent_str2("(#9149,#9166),#9142,.T.")[0])
 
 
 
