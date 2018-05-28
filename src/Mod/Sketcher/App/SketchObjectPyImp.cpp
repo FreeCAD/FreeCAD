bf03c7bcb11205885999b23874e88af7998ffd92
Abdullah Tahiri <abdullah.tahiri.yo@gmail.com>
2018-05-28 23:35:54 +0200

    Sketcher: Empty list means all geometry for Python addCopy addRectangularArray and addMove

--- src/Mod/Sketcher/App/SketchObjectPyImp.cpp
+++ src/Mod/Sketcher/App/SketchObjectPyImp.cpp
@@ -1025,16 +1025,16 @@ PyObject* SketchObjectPy::addMove(PyObject *args)
     if (PyObject_TypeCheck(pcObj, &(PyList_Type)) ||
         PyObject_TypeCheck(pcObj, &(PyTuple_Type))) {
         std::vector<int> geoIdList;
-    Py::Sequence list(pcObj);
-    for (Py::Sequence::iterator it = list.begin(); it != list.end(); ++it) {
-        #if PY_MAJOR_VERSION >= 3
-        if (PyLong_Check((*it).ptr()))
-            geoIdList.push_back(PyLong_AsLong((*it).ptr()));
-        #else
-        if (PyInt_Check((*it).ptr()))
-            geoIdList.push_back(PyInt_AsLong((*it).ptr()));
-        #endif
-    }
+        Py::Sequence list(pcObj);
+        for (Py::Sequence::iterator it = list.begin(); it != list.end(); ++it) {
+            #if PY_MAJOR_VERSION >= 3
+            if (PyLong_Check((*it).ptr()))
+                geoIdList.push_back(PyLong_AsLong((*it).ptr()));
+            #else
+            if (PyInt_Check((*it).ptr()))
+                geoIdList.push_back(PyInt_AsLong((*it).ptr()));
+            #endif
+        }
 
     this->getSketchObjectPtr()->addCopy(geoIdList, vect, true);
     
