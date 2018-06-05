70c247287fc6119cab9f6d313d334f8a242c50c5
Bernd Hahnebach <bernd@bimstatik.org>
2018-06-05 07:31:25 +0200

    FEM: prefs, move material prefs from general tab into an own tab in FEM

--- src/Mod/Fem/Gui/DlgSettingsFemMaterialImp.cpp
+++ src/Mod/Fem/Gui/DlgSettingsFemMaterialImp.cpp
@@ -1,7 +1,7 @@
 /***************************************************************************
- *   Copyright (c) 2015 FreeCAD Developers                                 *
- *   Author: Przemo Firszt <przemo@firszt.eu>                              *
- *   Based on src/Mod/Raytracing/Gui/DlgSettingsRayImp.cpp                 *
+ *   Copyright (c) 2018 FreeCAD Developers                                 *
+ *   Author: Bernd Hahnebach <bernd@bimstatik.ch>                          *
+ *   Based on src/Mod/Fem/Gui/DlgSettingsFemElmerImp.cpp                   *
  *                                                                         *
  *   This file is part of the FreeCAD CAx development system.              *
  *                                                                         *
@@ -26,58 +26,42 @@
 #include "PreCompiled.h"
 
 #include "Gui/Application.h"
-#include "DlgSettingsFemGeneralImp.h"
+#include "DlgSettingsFemMaterialImp.h"
 #include <Gui/PrefWidgets.h>
 
 using namespace FemGui;
 
-DlgSettingsFemGeneralImp::DlgSettingsFemGeneralImp( QWidget* parent )
+DlgSettingsFemMaterialImp::DlgSettingsFemMaterialImp( QWidget* parent )
   : PreferencePage( parent )
 {
     this->setupUi(this);
 }
 
-DlgSettingsFemGeneralImp::~DlgSettingsFemGeneralImp()
+DlgSettingsFemMaterialImp::~DlgSettingsFemMaterialImp()
 {
     // no need to delete child widgets, Qt does it all for us
 }
 
-void DlgSettingsFemGeneralImp::saveSettings()
+void DlgSettingsFemMaterialImp::saveSettings()
 {
-    fc_analysis_working_directory->onSave();
-
-    cb_analysis_group_meshing->onSave();
-
-    cb_restore_result_dialog->onSave();
-    cb_keep_results_on_rerun->onSave();
-    cb_hide_constraint->onSave();
-
-    cb_wd_temp->onSave();
-    cb_wd_beside->onSave();
-    cb_wd_custom->onSave();
-    le_wd_custom->onSave();
+    cb_use_built_in_materials->onSave();
+    cb_use_mat_from_config_dir->onSave();
+    cb_use_mat_from_custom_dir->onSave();
+    fc_custom_mat_dir->onSave();
 }
 
-void DlgSettingsFemGeneralImp::loadSettings()
+void DlgSettingsFemMaterialImp::loadSettings()
 {
-    fc_analysis_working_directory->onRestore();
-
-    cb_analysis_group_meshing->onRestore();
-
-    cb_restore_result_dialog->onRestore();
-    cb_keep_results_on_rerun->onRestore();
-    cb_hide_constraint->onRestore();
-
-    cb_wd_temp->onRestore();
-    cb_wd_beside->onRestore();
-    cb_wd_custom->onRestore();
-    le_wd_custom->onRestore();
+    cb_use_built_in_materials->onRestore();
+    cb_use_mat_from_config_dir->onRestore();
+    cb_use_mat_from_custom_dir->onRestore();
+    fc_custom_mat_dir->onRestore();
 }
 
 /**
  * Sets the strings of the subwidgets using the current language.
  */
-void DlgSettingsFemGeneralImp::changeEvent(QEvent *e)
+void DlgSettingsFemMaterialImp::changeEvent(QEvent *e)
 {
     if (e->type() == QEvent::LanguageChange) {
     }
@@ -86,4 +70,4 @@ void DlgSettingsFemGeneralImp::changeEvent(QEvent *e)
     }
 }
 
-#include "moc_DlgSettingsFemGeneralImp.cpp"
+#include "moc_DlgSettingsFemMaterialImp.cpp"
