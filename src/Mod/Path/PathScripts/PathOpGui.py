d023ce6248c3ba85196587a26c05225194ba1b2c
Markus Lampert <markus@bibi.ca>
2018-06-19 21:53:41 -0700

    Removed PathPreferences class and moved functionality into the module interface.

--- src/Mod/Path/PathScripts/PathOpGui.py
+++ src/Mod/Path/PathScripts/PathOpGui.py
@@ -28,14 +28,14 @@ import PathScripts.PathGeom as PathGeom
 import PathScripts.PathGetPoint as PathGetPoint
 import PathScripts.PathGui as PathGui
 import PathScripts.PathLog as PathLog
-import PathScripts.PathSelection as PathSelection
 import PathScripts.PathOp as PathOp
+import PathScripts.PathPreferences as PathPreferences
+import PathScripts.PathSelection as PathSelection
 import PathScripts.PathUtil as PathUtil
 import PathScripts.PathUtils as PathUtils
 import importlib
 
 from PySide import QtCore, QtGui
-from PathScripts.PathPreferences import PathPreferences
 
 __title__ = "Path Operation UI base classes"
 __author__ = "sliptonic (Brad Collette)"
