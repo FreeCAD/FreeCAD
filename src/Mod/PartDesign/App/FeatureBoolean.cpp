7e781bcfd0e4406376ccce4c2fdba66b440dbdca
wandererfan <wandererfan@gmail.com>
2018-07-04 19:17:50 -0400

    Revise multiple solids message

--- src/Mod/PartDesign/App/FeatureBoolean.cpp
+++ src/Mod/PartDesign/App/FeatureBoolean.cpp
@@ -144,7 +144,7 @@ App::DocumentObjectExecReturn *Boolean::execute(void)
 
     int solidCount = countSolids(result);
     if (solidCount > 1) {
-        return new App::DocumentObjectExecReturn("Boolean: Result has multiple solids. Check parameters.");
+        return new App::DocumentObjectExecReturn("Boolean: Result has multiple solids. This is not supported at this time.");
     }
 
     this->Shape.setValue(getSolid(result));
