039e972a4b1735bad63bdd458e182d2caec3fccf
wmayer <wmayer@users.sourceforge.net>
2018-07-28 11:08:03 +0200

    implement fast mesh rendering for Qt5 port (7x faster)

--- src/Mod/Mesh/Gui/SoFCIndexedFaceSet.h
+++ src/Mod/Mesh/Gui/SoFCIndexedFaceSet.h
@@ -24,6 +24,10 @@
 #define MESHGUI_SOFCINDEXEDFACESET_H
 
 #include <Inventor/nodes/SoIndexedFaceSet.h>
+#include <Inventor/elements/SoMaterialBindingElement.h>
+#include <Inventor/engines/SoSubEngine.h>
+#include <Inventor/fields/SoSFBool.h>
+#include <Inventor/fields/SoMFColor.h>
 
 class SoGLCoordinateElement;
 class SoTextureCoordinateBundle;
@@ -33,6 +37,47 @@ typedef int GLint;
 typedef float GLfloat;
 
 namespace MeshGui {
+
+class MeshRenderer
+{
+public:
+    MeshRenderer();
+    ~MeshRenderer();
+    void generateGLArrays(SoState*, SoMaterialBindingElement::Binding binding,
+        std::vector<float>& vertex, std::vector<int32_t>& index);
+    void renderFacesGLArray(SoGLRenderAction *action);
+    void renderCoordsGLArray(SoGLRenderAction *action);
+    bool matchMaterial(SoState*) const;
+    static bool shouldRenderDirectly(bool);
+
+private:
+    class Private;
+    Private* p;
+};
+
+/**
+ * class SoFCMaterialEngine
+ * \brief The SoFCMaterialEngine class is used to notify an
+ * SoFCIndexedFaceSet node about material changes.
+ *
+ * @author Werner Mayer
+ */
+class MeshGuiExport SoFCMaterialEngine : public SoEngine
+{
+    SO_ENGINE_HEADER(SoFCMaterialEngine);
+
+public:
+    SoFCMaterialEngine();
+    static void initClass();
+
+    SoMFColor diffuseColor;
+    SoEngineOutput trigger;
+
+private:
+    virtual ~SoFCMaterialEngine();
+    virtual void evaluate();
+    virtual void inputChanged(SoField *);
+};
 
 /**
  * class SoFCIndexedFaceSet
@@ -50,6 +95,7 @@ public:
     static void initClass();
     SoFCIndexedFaceSet();
 
+    SoSFBool updateGLArray;
     unsigned int renderTriangleLimit;
 
     void invalidate();
@@ -80,13 +126,9 @@ private:
     void renderVisibleFaces(const SbVec3f *);
 
     void generateGLArrays(SoState * state);
-    void renderFacesGLArray(SoGLRenderAction *action);
-    void renderCoordsGLArray(SoGLRenderAction *action);
 
 private:
-    std::vector<int32_t> index_array;
-    std::vector<float> vertex_array;
-    SbBool updateGLArray;
+    MeshRenderer render;
     GLuint *selectBuf;
 };
 
