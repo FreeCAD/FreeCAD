0e06c666bb06e2f57c3235f7d40a35ba3217638f
Sammel Lothar LTS <SammelLothar@gmx.de>
2018-08-02 19:10:40 +0200

    PATH PATH-DressupLeadInOut-Arc-Fix-pep8-conform

--- src/Mod/Path/PathScripts/PathDressupLeadInOut.py
+++ src/Mod/Path/PathScripts/PathDressupLeadInOut.py
@@ -2,7 +2,7 @@
 
 # ***************************************************************************
 # *                                                                         *
-# *   Copyright (c) 2017 LTS <SammelLothar@gmx.de> under LGPL               *
+# *   Copyright (c) 2018 LTS <SammelLothar@gmx.de> under LGPL               *
 # *                                                                         *
 # *   This program is free software; you can redistribute it and/or modify  *
 # *   it under the terms of the GNU Lesser General Public License (LGPL)    *
