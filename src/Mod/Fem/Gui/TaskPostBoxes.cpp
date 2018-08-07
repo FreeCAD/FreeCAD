d9ed75d95467d01244995274f5345ba540a649be
Bernd Hahnebach <bernd@bimstatik.org>
2018-08-07 08:18:55 +0200

    FEM: icons, rename data along line icon

--- src/Mod/Fem/Gui/TaskPostBoxes.cpp
+++ src/Mod/Fem/Gui/TaskPostBoxes.cpp
@@ -534,7 +534,7 @@ void TaskPostClip::on_InsideOut_toggled(bool val) {
 //############################################################################################
 // data along a line
 TaskPostDataAlongLine::TaskPostDataAlongLine(ViewProviderDocumentObject* view, QWidget* parent)
-    : TaskPostBox(view,Gui::BitmapFactory().pixmap("fem-DataAlongLine"), tr("Data along a line options"), parent) {
+    : TaskPostBox(view,Gui::BitmapFactory().pixmap("fem-post-filter-data-along-line"), tr("Data along a line options"), parent) {
 
     assert(view->isDerivedFrom(ViewProviderFemPostDataAlongLine::getClassTypeId()));
 
