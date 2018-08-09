90ad845396ed2f4d273fa3f4b788efafcbe69dc0
wmayer <wmayer@users.sourceforge.net>
2018-08-09 18:57:21 +0200

    for Qt5 no explict event processing after creating a 3d view

--- src/Gui/Application.cpp
+++ src/Gui/Application.cpp
@@ -678,7 +678,11 @@ void Application::slotNewDocument(const App::Document& Doc)
 
     signalNewDocument(*pDoc);
     pDoc->createView(View3DInventor::getClassTypeId());
+    // FIXME: Do we really need this further? Calling processEvents() mixes up order of execution in an
+    // unpredicatable way. At least it seems that with Qt5 we don't need this any more.
+#if QT_VERSION < 0x050000
     qApp->processEvents(); // make sure to show the window stuff on the right place
+#endif
 }
 
 void Application::slotDeleteDocument(const App::Document& Doc)
