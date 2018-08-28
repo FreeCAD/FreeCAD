835a861816b29adfe4be0c6e709d1b2a0bb6209c
Zheng, Lei <realthunder.dev@gmail.com>
2018-08-28 18:21:33 +0800

    MeshGui::SoFCIndexedFaceSet: fix missing normal cache unlock

--- src/Mod/Mesh/Gui/SoFCIndexedFaceSet.cpp
+++ src/Mod/Mesh/Gui/SoFCIndexedFaceSet.cpp
@@ -576,6 +576,9 @@ void SoFCIndexedFaceSet::drawFaces(SoGLRenderAction *action)
 
         drawCoords(static_cast<const SoGLCoordinateElement*>(coords), cindices, numindices,
                    normals, nindices, &mb, mindices, binding, &tb, tindices);
+
+        if(normalCacheUsed)
+            this->readUnlockNormalCache();
         // Disable caching for this node
         SoGLCacheContextElement::shouldAutoCache(state, SoGLCacheContextElement::DONT_AUTO_CACHE);
 #endif
@@ -847,6 +850,9 @@ void SoFCIndexedFaceSet::generateGLArrays(SoGLRenderAction * action)
     }
 
     render.generateGLArrays(action, matbind, face_vertices, face_indices);
+
+    if(normalCacheUsed)
+        this->readUnlockNormalCache();
 }
 
 void SoFCIndexedFaceSet::doAction(SoAction * action)
