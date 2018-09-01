0550480ea55a829c15b083f551f6799837a8268a
Zheng, Lei <realthunder.dev@gmail.com>
2018-09-01 13:28:37 +0800

    Gui::Document: fix setEdit()

--- src/Gui/Document.cpp
+++ src/Gui/Document.cpp
@@ -362,7 +362,7 @@ bool Document::setEdit(Gui::ViewProvider* p, int ModNum, const char *subname)
     View3DInventor *activeView = dynamic_cast<View3DInventor *>(getActiveView());
     // if the currently active view is not the 3d view search for it and activate it
     if (!activeView) {
-        activeView = dynamic_cast<View3DInventor *>(getViewOfViewProvider(p));
+        activeView = dynamic_cast<View3DInventor *>(getViewOfViewProvider(vp));
         if(!activeView){
             FC_ERR("cannot edit without active view");
             return false;
