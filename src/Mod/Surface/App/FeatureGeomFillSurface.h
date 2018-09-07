a624fee2c27bbbb869df6a8ce40438953a282a8c
wmayer <wmayer@users.sourceforge.net>
2018-09-07 23:03:07 +0200

    add GUI functions to flip orientation of input curves

--- src/Mod/Surface/App/FeatureGeomFillSurface.h
+++ src/Mod/Surface/App/FeatureGeomFillSurface.h
@@ -64,7 +64,7 @@ class GeomFillSurface : public Part::Spline
 public:
     GeomFillSurface();
     App::PropertyLinkSubList BoundaryList;  // Curves to be turned into a face (2-4 curves allowed).
-    App::PropertyBoolList ForwardList;      // Booleans to handle orientation of the curves
+    App::PropertyBoolList ReversedList;     // Booleans to handle orientation of the curves
     App::PropertyEnumeration FillType;      // Fill method (1, 2, or 3 for Stretch, Coons, and Curved)
 
     short mustExecute() const;
