6b9c1e89d1975577e1b67e62673f8678a4001730
wandererfan <wandererfan@gmail.com>
2018-09-15 14:13:37 -0400

    Show full width Edges around Section Face
    - Section Face was hidding 1/2 of Edge if "Show Section
      Edges" was set to false.

--- src/Mod/TechDraw/Gui/ZVALUE.h
+++ src/Mod/TechDraw/Gui/ZVALUE.h
@@ -8,10 +8,10 @@ namespace ZVALUE {
     const int FACE = 10;
     const int HATCH = 30;
     const int HIDEDGE = 40;
+    const int SECTIONFACE = 45;
+    const int SECTIONHATCH = 46;
     const int EDGE = 50;
     const int VERTEX = 60;
-    const int SECTIONFACE = 65;
-    const int SECTIONHATCH = 66;
     const int DIMENSION = 110;
     const int SECTIONLINE = 80;    //TODO: change to "DECORATION"? section lines, symmetry lines, etc?
     const int HIGHLIGHT = 80;
