f9ca7d62c250e4a0f19df964f003604da3eb342f
wmayer <wmayer@users.sourceforge.net>
2018-09-17 10:20:03 +0200

    fix compiler warning, fix build failure with older OCCT versions

--- src/Mod/Part/App/BSplineCurvePyImp.cpp
+++ src/Mod/Part/App/BSplineCurvePyImp.cpp
@@ -37,10 +37,10 @@
 # include <TColStd_HArray1OfReal.hxx>
 # include <TColStd_Array1OfReal.hxx>
 # include <TColStd_HArray1OfBoolean.hxx>
-
 # include <Precision.hxx>
 #endif
 
+#include <cmath>
 #include <Base/VectorPy.h>
 #include <Base/GeometryPyCXX.h>
 
@@ -298,9 +298,11 @@ PyObject* BSplineCurvePy::segment(PyObject * args)
     try {
         Handle(Geom_BSplineCurve) curve = Handle(Geom_BSplineCurve)::DownCast
             (getGeometryPtr()->handle());
-        Handle(Geom_BSplineCurve) tempCurve = new Geom_BSplineCurve(*curve);
+        Handle(Geom_BSplineCurve) tempCurve = Handle(Geom_BSplineCurve)::DownCast
+            (curve->Copy());
         tempCurve->Segment(u1,u2);
-        if (abs(tempCurve->FirstParameter()-u1)>Precision::Approximation() || abs(tempCurve->LastParameter()-u2)>Precision::Approximation()) {
+        if (std::abs(tempCurve->FirstParameter()-u1) > Precision::Approximation() ||
+            std::abs(tempCurve->LastParameter()-u2) > Precision::Approximation()) {
             Standard_Failure::Raise("Failed to segment BSpline curve");
             return 0;
         }
