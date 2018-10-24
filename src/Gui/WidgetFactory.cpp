2004653fe4ff6bc56bc29cfec84abdd4f6adf2c0
wmayer <wmayer@users.sourceforge.net>
2018-10-24 23:12:59 +0200

    check for existence of PySide stuff when defining convertWrapperToQuantity

--- src/Gui/WidgetFactory.cpp
+++ src/Gui/WidgetFactory.cpp
@@ -161,7 +161,7 @@ PythonToCppFunc isBaseQuantity_PythonToCpp_QVariantConvertible(PyObject* obj)
     return 0;
 }
 
-#if QT_VERSION >= 0x050200
+#if defined (HAVE_PYSIDE) && QT_VERSION >= 0x050200
 Base::Quantity convertWrapperToQuantity(const PySide::PyObjectWrapper &w)
 {
     PyObject* pyIn = static_cast<PyObject*>(w);
@@ -191,7 +191,7 @@ void registerTypes()
                                                              isBaseQuantity_PythonToCpp_QVariantConvertible);
     }
 
-#if QT_VERSION >= 0x050200
+#if defined (HAVE_PYSIDE) && QT_VERSION >= 0x050200
     QMetaType::registerConverter<PySide::PyObjectWrapper, Base::Quantity>(&convertWrapperToQuantity);
 #endif
 }
