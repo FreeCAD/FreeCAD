f9ba16b7db3db1be4605e905a1fd14adabbc3331
Markus Lampert <markus@bibi.ca>
2018-11-03 15:55:39 -0700

    Also restore position of base op when dressup is deleted.

--- src/Mod/Path/PathScripts/PathDressupRampEntry.py
+++ src/Mod/Path/PathScripts/PathDressupRampEntry.py
@@ -642,7 +642,7 @@ class ViewProviderDressup:
         '''this makes sure that the base operation is added back to the project and visible'''
         FreeCADGui.ActiveDocument.getObject(arg1.Object.Base.Name).Visibility = True
         job = PathUtils.findParentJob(self.obj)
-        job.Proxy.addOperation(arg1.Object.Base)
+        job.Proxy.addOperation(arg1.Object.Base, arg1.Object)
         arg1.Object.Base = None
         return True
 
