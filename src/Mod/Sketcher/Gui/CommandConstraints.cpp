13f589a0a6e0204f6a3487912b2c57f727821411
Abdullah Tahiri <abdullah.tahiri.yo@gmail.com>
2018-11-03 16:41:04 +0100

    Sketcher: Allow driven constraints when autoredundancy removal and autorecomputes active
    ==========================================================================================
    
    https://forum.freecadweb.org/viewtopic.php?f=3&t=31998#p266626

--- src/Mod/Sketcher/Gui/CommandConstraints.cpp
+++ src/Mod/Sketcher/Gui/CommandConstraints.cpp
@@ -669,6 +669,11 @@ bool SketcherGui::tryAutoRecompute(Sketcher::SketchObject* obj, bool &autoremove
     bool autoRecompute = hGrp->GetBool("AutoRecompute",false);
     bool autoRemoveRedundants = hGrp->GetBool("AutoRemoveRedundants",false);
 
+    // We need to make sure the solver has right redundancy information before trying to remove the redundants.
+    // for example if a non-driving constraint has been added.
+    if(autoRemoveRedundants && autoRecompute) 
+        obj->solve();
+        
     if(autoRemoveRedundants)
         obj->autoRemoveRedundants();
     
