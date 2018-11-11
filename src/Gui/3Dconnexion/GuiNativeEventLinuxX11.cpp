432cdaf272ab39df744840d44e26d898cec27588
Torsten Sadowski <tsadowski@gmx.net>
2018-11-11 23:27:07 +0100

    Copyright notices added

--- src/Gui/3Dconnexion/GuiNativeEventLinuxX11.cpp
+++ src/Gui/3Dconnexion/GuiNativeEventLinuxX11.cpp
@@ -1,6 +1,24 @@
- /*
-Implementation by Torsten Sadowski 2018
- */
+/***************************************************************************
+ *   Copyright (c) 2018 Torsten Sadowski <tsadowski[at]gmx.net>            *
+ *                                                                         *
+ *   This file is part of the FreeCAD CAx development system.              *
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Library General Public           *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2 of the License, or (at your option) any later version.      *
+ *                                                                         *
+ *   This library  is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU Library General Public License for more details.                  *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this library; see the file COPYING.LIB. If not,    *
+ *   write to the Free Software Foundation, Inc., 59 Temple Place,         *
+ *   Suite 330, Boston, MA  02111-1307, USA                                *
+ *                                                                         *
+ ***************************************************************************/
 
 #include "SpaceballEvent.h"
 #include <QMainWindow>
@@ -57,16 +75,6 @@ void Gui::GuiNativeEvent::initSpaceball(QMainWindow *window)
 		mainApp->setSpaceballPresent(true);
 
     #if QT_VERSION >= 0x050000
-        //static auto evFilter( [](void *msg, long *result){
-        //        Q_UNUSED(result);
-        //        auto inst(dynamic_cast<Gui::GUIApplicationNativeEventAware *>(QApplication::instance()));
-        //        if (inst) {
-        //            return inst->nativeEvent->xcbEventFilter(static_cast<const xcb_client_message_event_t *>(msg));
-        //        } else {
-        //            return false;
-        //        }
-        //    } );
-        //mainApp->installNativeEventFilter(new Gui::RawInputEventFilter(evFilter));
         mainApp->installNativeEventFilter(new Gui::RawInputEventFilter(&xcbEventFilter));
     #endif // #if QT_VERSION >= 0x050000
     }
--- src/Gui/3Dconnexion/GuiNativeEventMac.cpp
+++ src/Gui/3Dconnexion/GuiNativeEventMac.cpp
@@ -1,11 +1,32 @@
+/***************************************************************************
+ *   Copyright (c) 2018 Torsten Sadowski <tsadowski[at]gmx.net>            *
+ *                                                                         *
+ *   This file is part of the FreeCAD CAx development system.              *
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Library General Public           *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2 of the License, or (at your option) any later version.      *
+ *                                                                         *
+ *   This library  is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU Library General Public License for more details.                  *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this library; see the file COPYING.LIB. If not,    *
+ *   write to the Free Software Foundation, Inc., 59 Temple Place,         *
+ *   Suite 330, Boston, MA  02111-1307, USA                                *
+ *                                                                         *
+ ***************************************************************************/
+
 /*
 Development tools and related technology provided under license from 3Dconnexion.
 (c) 1992 - 2012 3Dconnexion. All rights reserved
 */
 
  /*
-Implementation by Torsten Sadowski 2015
-with special thanks to marcxs for making the first steps
+With special thanks to marcxs for making the first steps
  */
 
 #include "GuiNativeEventMac.h"
