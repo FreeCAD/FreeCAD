5882e64d7d73ce98aeca8ae504fca42e36b78add
luz.paz <luzpaz@users.noreply.github.com>
2018-11-20 23:25:37 +0100

    FEM: solver, make uniform copyright and developer info
    .

--- src/Mod/Fem/femsolver/fenics/fenics_tools.py
+++ src/Mod/Fem/femsolver/fenics/fenics_tools.py
@@ -1,6 +1,5 @@
 # ***************************************************************************
-# *                                                                         *
-# *   Copyright (c) 2017 - Johannes Hartung <j.hartung@gmx.net>             *
+# *   Copyright (c) 2017 Johannes Hartung <j.hartung@gmx.net>               *
 # *                                                                         *
 # *   This program is free software; you can redistribute it and/or modify  *
 # *   it under the terms of the GNU Lesser General Public License (LGPL)    *
