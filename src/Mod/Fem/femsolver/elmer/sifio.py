bf779155d4401845dffd25a89fcfd194668a040e
luz.paz <luzpaz@users.noreply.github.com>
2018-11-20 23:25:37 +0100

    FEM: solver, make uniform copyright and developer info
    .

--- src/Mod/Fem/femsolver/elmer/sifio.py
+++ src/Mod/Fem/femsolver/elmer/sifio.py
@@ -1,6 +1,5 @@
 # ***************************************************************************
-# *                                                                         *
-# *   Copyright (c) 2016 - Markus Hovorka <m.hovorka@live.de>               *
+# *   Copyright (c) 2016 Markus Hovorka <m.hovorka@live.de>                 *
 # *                                                                         *
 # *   This program is free software; you can redistribute it and/or modify  *
 # *   it under the terms of the GNU Lesser General Public License (LGPL)    *
