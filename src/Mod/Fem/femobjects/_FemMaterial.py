771c61e89c9dd01a6ab746fa73602cd3e0c47479
Bernd Hahnebach <bernd@bimstatik.org>
2018-11-25 20:18:13 +0100

    FEM: special character, Python seams not to like the ü of Jürgen even it is an comment

--- src/Mod/Fem/femobjects/_FemMaterial.py
+++ src/Mod/Fem/femobjects/_FemMaterial.py
@@ -1,6 +1,6 @@
 # ***************************************************************************
 # *                                                                         *
-# *   Copyright (c) 2013 Jürgen Riegel <FreeCAD@juergen-riegel.net>         *
+# *   Copyright (c) 2013 Juergen Riegel <FreeCAD@juergen-riegel.net>         *
 # *                                                                         *
 # *   This program is free software; you can redistribute it and/or modify  *
 # *   it under the terms of the GNU Lesser General Public License (LGPL)    *
