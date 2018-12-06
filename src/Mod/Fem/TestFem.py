5ccd715e3ff4d87e47dc3bce85f6e4a54da5d335
Bernd Hahnebach <bernd@bimstatik.org>
2018-12-06 10:15:45 +0100

    FEM: unit tests, rename common class

--- src/Mod/Fem/TestFem.py
+++ src/Mod/Fem/TestFem.py
@@ -27,7 +27,7 @@
 # Unit test for the FEM module
 # the order should be as follows:
 # common-, object-, mesh-, inout-, ccxtools-, solverframworktests
-from femtest.testcommon import FemTest
+from femtest.testcommon import FemCommon
 from femtest.testmesh import FemMeshTest
 from femtest.testccxtools import FemCcxAnalysisTest
 from femtest.testsolverframework import SolverFrameWorkTest
