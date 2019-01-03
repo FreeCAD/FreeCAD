d3074d860769fcfb75810a900437d18d3014ab88
wmayer <wmayer@users.sourceforge.net>
2019-01-03 15:37:02 +0100

    Update copyright notice

--- src/Main/MainCmd.cpp
+++ src/Main/MainCmd.cpp
@@ -53,7 +53,7 @@
 using Base::Console;
 using App::Application;
 
-const char sBanner[] = "(c) Juergen Riegel, Werner Mayer, Yorik van Havre 2001-2018\n"\
+const char sBanner[] = "(c) Juergen Riegel, Werner Mayer, Yorik van Havre 2001-2019\n"\
                        "  #####                 ####  ###   ####  \n" \
                        "  #                    #      # #   #   # \n" \
                        "  #     ##  #### ####  #     #   #  #   # \n" \
