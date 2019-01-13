632f9cf250bb705dfcee0f5af2d66f5d5cee3136
wmayer <wmayer@users.sourceforge.net>
2019-01-14 00:21:56 +0100

    fixes 0003515: Changing Name of Spreadsheet in History Tree Does Not Trigger Name Update in Tab at Bottom of 3D Window

--- src/Mod/Spreadsheet/Gui/SpreadsheetView.cpp
+++ src/Mod/Spreadsheet/Gui/SpreadsheetView.cpp
@@ -327,6 +327,10 @@ void SheetView::currentChanged ( const QModelIndex & current, const QModelIndex 
 void SheetView::updateCell(const App::Property *prop)
 {
     try {
+        if (sheet && prop == &sheet->Label) {
+            QString cap = QString::fromUtf8(sheet->Label.getValue());
+            setWindowTitle(cap);
+        }
         CellAddress address;
 
         sheet->getCellAddress(prop, address);
