60fa6eea458983e10e2a072cd17cc3e071e9641f
Bernd Hahnebach <bernd@bimstatik.org>
2019-01-16 22:22:18 +0100

    FEM: code formating, space in block comment

--- src/Mod/Fem/InitGui.py
+++ src/Mod/Fem/InitGui.py
@@ -5,29 +5,29 @@
 # This is the second one of three init scripts, the third one
 # runs when the gui is up
 
-#***************************************************************************
-#*   (c) Juergen Riegel (juergen.riegel@web.de) 2009                       *
-#*                                                                         *
-#*   This file is part of the FreeCAD CAx development system.              *
-#*                                                                         *
-#*   This program is free software; you can redistribute it and/or modify  *
-#*   it under the terms of the GNU Lesser General Public License (LGPL)    *
-#*   as published by the Free Software Foundation; either version 2 of     *
-#*   the License, or (at your option) any later version.                   *
-#*   for detail see the LICENCE text file.                                 *
-#*                                                                         *
-#*   FreeCAD is distributed in the hope that it will be useful,            *
-#*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
-#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
-#*   GNU Lesser General Public License for more details.                   *
-#*                                                                         *
-#*   You should have received a copy of the GNU Library General Public     *
-#*   License along with FreeCAD; if not, write to the Free Software        *
-#*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
-#*   USA                                                                   *
-#*                                                                         *
-#*   Juergen Riegel 2002                                                   *
-#***************************************************************************/
+# ***************************************************************************
+# *   (c) Juergen Riegel (juergen.riegel@web.de) 2009                       *
+# *                                                                         *
+# *   This file is part of the FreeCAD CAx development system.              *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENCE text file.                                 *
+# *                                                                         *
+# *   FreeCAD is distributed in the hope that it will be useful,            *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Lesser General Public License for more details.                   *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with FreeCAD; if not, write to the Free Software        *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# *   Juergen Riegel 2002                                                   *
+# ***************************************************************************/
 
 import FreeCAD
 import FreeCADGui
@@ -48,5 +48,6 @@ class FemWorkbench (Workbench):
 
     def GetClassName(self):
         return "FemGui::Workbench"
+
 
 FreeCADGui.addWorkbench(FemWorkbench())
