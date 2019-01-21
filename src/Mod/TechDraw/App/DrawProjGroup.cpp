3356b58686a5fc0e2a87ee71d99d0c885d45dc18
wandererfan <wandererfan@gmail.com>
2019-01-21 13:06:02 -0500

    Fix out of scope warnings
    - Links within TD were not set to Global scope. This caused
      problems when a Page was in a container, but Views were not
      in the container.

--- src/Mod/TechDraw/App/DrawProjGroup.cpp
+++ src/Mod/TechDraw/App/DrawProjGroup.cpp
@@ -70,6 +70,7 @@ DrawProjGroup::DrawProjGroup(void)
     ADD_PROPERTY_TYPE(Source    ,(0), group, App::Prop_None,"Shape to view");
     Source.setScope(App::LinkScope::Global);
     ADD_PROPERTY_TYPE(Anchor, (0), group, App::Prop_None, "The root view to align projections with");
+    Anchor.setScope(App::LinkScope::Global);
     ProjectionType.setEnums(ProjectionTypeEnums);
     ADD_PROPERTY(ProjectionType, ((long)0));
 
