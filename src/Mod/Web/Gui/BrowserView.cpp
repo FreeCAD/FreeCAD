b3b63c4a53371b2b90a27463ce82eebe11741261
Fredrik Johansson <fredrik@mumme.se>
2019-01-28 20:28:41 +0100

    Enhance url addressbar, toggle hide from action

--- src/Mod/Web/Gui/BrowserView.cpp
+++ src/Mod/Web/Gui/BrowserView.cpp
@@ -162,6 +162,7 @@ void UrlWidget::keyPressEvent(QKeyEvent *keyEvt)
 void UrlWidget::display()
 {
     setFixedWidth(m_view->size().width());
+    setText(m_view->url().toString());
     show();
     setFocus(Qt::ActiveWindowFocusReason);
 }
@@ -644,6 +645,11 @@ void BrowserView::stop(void)
     view->stop();
 }
 
+QUrl BrowserView::url() const
+{
+    return view->url();
+}
+
 void BrowserView::onLoadStarted()
 {
     QProgressBar* bar = Gui::Sequencer::instance()->getProgressBar();
@@ -717,7 +723,10 @@ bool BrowserView::onMsg(const char* pMsg,const char** )
         view->setZoomFactor(factor - 0.2);
         return true;
     } else if (strcmp(pMsg,"SetURL")==0){
-        urlWgt->display();
+        if (urlWgt->isVisible())
+            urlWgt->hide();
+        else
+            urlWgt->display();
         return true;
     }
 
