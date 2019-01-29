0b7c0b93dc2c9a59e1b94749f3b803f8174c9c94
markus <markus@bibi.ca>
2019-01-28 16:00:00 -0800

    Use regular 'w' mode when writing text files for py3 compatibility.

--- src/Mod/Path/PathScripts/post/fablin_post.py
+++ src/Mod/Path/PathScripts/post/fablin_post.py
@@ -197,7 +197,7 @@ def export(objectslist,filename,argstring):
 
     print ("done postprocessing.")
 
-    gfile = pythonopen(filename,"wb")
+    gfile = pythonopen(filename,"w")
     gfile.write(gcode)
     gfile.close()
 
