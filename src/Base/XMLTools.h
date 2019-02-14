1a68b75a4b3ffeb9632f39afc463ed739551ef56
wmayer <wmayer@users.sourceforge.net>
2019-02-14 11:06:30 +0100

    fix possible infinite loop when transcoding from/to UTF-8

--- src/Base/XMLTools.h
+++ src/Base/XMLTools.h
@@ -157,6 +157,10 @@ inline StrXUTF8::StrXUTF8(const XMLCh* const toTranscode)
         str.append((const char*)outBuff, outputLength);
         offset += eaten;
         inputLength -= eaten;
+
+        //  Bail out if nothing more was produced
+        if (outputLength == 0)
+            break;
     }
 }
 
@@ -274,6 +278,10 @@ inline XUTF8Str::XUTF8Str(const char* const fromTranscode)
         str.append(outBuff, outputLength);
         offset += eaten;
         inputLength -= eaten;
+
+        //  Bail out if nothing more was produced
+        if (outputLength == 0)
+            break;
     }
 
     delete[] charSizes;
