8e15118bb7708b18e7998e381b7819c6bb6a46ba
wmayer <wmayer@users.sourceforge.net>
2019-02-17 20:12:56 +0100

    PVS: V779 Unreachable code detected. It is possible that an error is present.

--- src/App/Application.cpp
+++ src/App/Application.cpp
@@ -1214,8 +1214,7 @@ void segmentation_fault_handler(int sig)
     std::cerr << "Program received signal SIGSEGV, Segmentation fault.\n";
     printBacktrace(2);
     exit(1);
-#endif
-
+#else
     switch (sig) {
         case SIGSEGV:
             std::cerr << "Illegal storage access..." << std::endl;
@@ -1233,6 +1232,7 @@ void segmentation_fault_handler(int sig)
             std::cerr << "Unknown error occurred..." << std::endl;
             break;
     }
+#endif // FC_OS_LINUX
 }
 
 void unhandled_exception_handler()
