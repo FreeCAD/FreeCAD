001abca4bb4daa1eb272b535bb80482189fd2013
wmayer <wmayer@users.sourceforge.net>
2019-02-23 14:58:41 +0100

    avoid using hard-code precision when writing double to streams

--- src/Gui/DlgSettingsUnitsImp.cpp
+++ src/Gui/DlgSettingsUnitsImp.cpp
@@ -27,6 +27,8 @@
 # include <QRegExp>
 #endif
 
+#include <limits>
+
 #include "ui_DlgSettingsUnits.h"
 #include "DlgSettingsUnitsImp.h"
 #include "NavigationStyle.h"
@@ -49,6 +51,7 @@ DlgSettingsUnitsImp::DlgSettingsUnitsImp(QWidget* parent)
     : PreferencePage( parent ), ui(new Ui_DlgSettingsUnits)
 {
     ui->setupUi(this);
+    ui->spinBoxDecimals->setMaximum(std::numeric_limits<double>::digits10 + 1);
 
     //fillUpListBox();
     ui->tableWidget->setVisible(false);
