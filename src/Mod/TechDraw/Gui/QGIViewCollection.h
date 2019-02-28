fcd3fe0a464a4e94897ccfc81c7ef6730a60a5b4
wmayer <wmayer@users.sourceforge.net>
2019-02-28 23:17:20 +0100

    fix -Winconsistent-missing-override

--- src/Mod/TechDraw/Gui/QGIViewCollection.h
+++ src/Mod/TechDraw/Gui/QGIViewCollection.h
@@ -43,13 +43,13 @@ public:
     ~QGIViewCollection() = default;
 
     enum {Type = QGraphicsItem::UserType + 110};
-    int type() const { return Type;}
+    int type() const override { return Type;}
 
-    virtual void updateView(bool update = false);
+    virtual void updateView(bool update = false) override;
     virtual void mouseReleaseEvent(QGraphicsSceneMouseEvent * event);
 
 protected:
-  virtual QVariant itemChange(GraphicsItemChange change, const QVariant &value);
+    virtual QVariant itemChange(GraphicsItemChange change, const QVariant &value) override;
 
 };
 
