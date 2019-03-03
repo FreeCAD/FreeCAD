9822b0f0d6fd859aa1e58aaa1089e062ea455d73
wmayer <wmayer@users.sourceforge.net>
2019-03-03 17:14:20 +0100

    PVS: V530 The return value of function 'freopen' is required to be utilized

--- src/Main/MainGui.cpp
+++ src/Main/MainGui.cpp
@@ -78,10 +78,13 @@ public:
         : fi(Base::FileInfo::getTempFileName()), file(f)
     {
 #ifdef WIN32
-        _wfreopen(fi.toStdWString().c_str(),L"w",file);
+        FILE* ptr = _wfreopen(fi.toStdWString().c_str(),L"w",file);
 #else
-        freopen(fi.filePath().c_str(),"w",file);
+        FILE* ptr = freopen(fi.filePath().c_str(),"w",file);
 #endif
+        if (!ptr) {
+            std::cerr << "Failed to reopen file" << std::endl;
+        }
     }
     ~Redirection()
     {
