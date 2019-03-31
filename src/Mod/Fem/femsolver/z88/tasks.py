e57241e91fdbc9f70186a423ddbba67e4cfe95c9
Bernd Hahnebach <bernd@bimstatik.org>
2019-03-31 15:07:38 +0200

    FEM: solver writer and tasks, code formating

--- src/Mod/Fem/femsolver/z88/tasks.py
+++ src/Mod/Fem/femsolver/z88/tasks.py
@@ -75,7 +75,8 @@ class Prepare(run.Prepare):
             c.beam_rotations,
             c.shell_thicknesses,
             c.fluid_sections,
-            self.directory)
+            self.directory
+        )
         path = w.write_z88_input()
         # report to user if task succeeded
         if path is not None:
