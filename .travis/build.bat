eb9a75a46e89643281b07ae1a3dce76a88c6aef3
Abdullah Tahiri <abdullah.tahiri.yo@gmail.com>
2019-05-08 13:14:40 +0200

    Add testing to the batch file

--- .travis/build.bat
+++ .travis/build.bat
@@ -4,3 +4,8 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliar
 
 MSBuild.exe FreeCAD_Trunk.sln /p:CLToolExe=clcache.exe /p:TrackFileAccess=false /p:CLToolPath=C:\Users\travis\build\FreeCAD\FreeCAD /m:2 /nologo /verbosity:minimal /p:Configuration=Release /p:Platform=x64
 
+echo on
+
+xcopy C:\Users\travis\build\FreeCAD\FreeCAD\FreeCADLibs\bin C:\Users\travis\build\FreeCAD\FreeCAD\build\bin /E /Q
+
+C:\Users\travis\build\FreeCAD\FreeCAD\build\bin\FreeCADCmd.exe --run-test 0
