598a465df0b0ecdc1f6251b6534ddae344aec02e
Abdullah Tahiri <abdullah.tahiri.yo@gmail.com>
2019-05-14 20:36:19 +0200

    Enable PD Gui

--- src/Mod/PartDesign/TestPartDesignApp.py
+++ src/Mod/PartDesign/TestPartDesignApp.py
@@ -24,17 +24,6 @@
 #---------------------------------------------------------------------------
 
 
-#import FreeCAD, os, unittest, tempfile, math
-
-# dummy text
-#class RandomPartDesignTest(unittest.TestCase):
-#    def setUp(self):
-#        self.count = 0
-#
-#    def testPrint(self):
-#        FreeCAD.Console.PrintMessage("   Oh great spirit of FreeCAD, let PartDesign flow\n")
-
-
 # datum tools
 from PartDesignTests.TestDatum import TestDatumPoint, TestDatumLine, TestDatumPlane
 from PartDesignTests.TestShapeBinder import TestShapeBinder
