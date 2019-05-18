7a8e0c959447d1b27ca7f67b03ab26b9bfad5d4b
Abdullah Tahiri <abdullah.tahiri.yo@gmail.com>
2019-05-18 13:34:59 +0200

    Test Path kindofbinarysearch 2

--- src/Mod/Path/TestPathApp.py
+++ src/Mod/Path/TestPathApp.py
@@ -27,10 +27,10 @@ import TestApp
 from PathTests.TestPathLog   import TestPathLog
 from PathTests.TestPathCore  import TestPathCore
 from PathTests.TestPathPost  import PathPostTestCases
-from PathTests.TestPathGeom  import TestPathGeom
-from PathTests.TestPathOpTools  import TestPathOpTools
-from PathTests.TestPathUtil  import TestPathUtil
-from PathTests.TestPathDepthParams        import depthTestCases
+#from PathTests.TestPathGeom  import TestPathGeom
+#from PathTests.TestPathOpTools  import TestPathOpTools
+#from PathTests.TestPathUtil  import TestPathUtil
+#from PathTests.TestPathDepthParams        import depthTestCases
 #from PathTests.TestPathDressupHoldingTags import TestHoldingTags
 #from PathTests.TestPathDressupDogbone import TestDressupDogbone
 #from PathTests.TestPathStock import TestPathStock
