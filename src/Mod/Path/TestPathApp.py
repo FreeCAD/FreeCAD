100e8cdc6d8d797550396bbe7b4072e0da1c03be
Abdullah Tahiri <abdullah.tahiri.yo@gmail.com>
2019-05-19 12:32:27 +0200

    New binary search - second leg 2

--- src/Mod/Path/TestPathApp.py
+++ src/Mod/Path/TestPathApp.py
@@ -34,8 +34,9 @@ from PathTests.TestPathDepthParams        import depthTestCases
 
 from PathTests.TestPathDressupHoldingTags import TestHoldingTags
 from PathTests.TestPathDressupDogbone import TestDressupDogbone
-from PathTests.TestPathStock import TestPathStock
-from PathTests.TestPathTool import TestPathTool
+#from PathTests.TestPathStock import TestPathStock
+#from PathTests.TestPathTool import TestPathTool
+
 #from PathTests.TestPathTooltable import TestPathTooltable
 #from PathTests.TestPathToolController import TestPathToolController
 #from PathTests.TestPathSetupSheet import TestPathSetupSheet
