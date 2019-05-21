8f38d9da81a9672dab20b362546384121357e2a8
Abdullah Tahiri <abdullah.tahiri.yo@gmail.com>
2019-05-21 13:25:29 +0200

    New TestPathDressupHoldingTags

--- src/Mod/Path/TestPathApp.py
+++ src/Mod/Path/TestPathApp.py
@@ -32,9 +32,9 @@ from PathTests.TestPathOpTools  import TestPathOpTools
 from PathTests.TestPathUtil  import TestPathUtil
 from PathTests.TestPathDepthParams        import depthTestCases
 
-#from PathTests.TestPathDressupHoldingTags import TestHoldingTags
+from PathTests.TestPathDressupHoldingTags import TestHoldingTags
 
-from PathTests.TestPathDressupDogbone import TestDressupDogbone
+#from PathTests.TestPathDressupDogbone import TestDressupDogbone
 #from PathTests.TestPathStock import TestPathStock
 #from PathTests.TestPathTool import TestPathTool
 
