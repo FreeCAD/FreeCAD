79d179d567379cc28a6e9c2508a53fdd4d067456
Abdullah Tahiri <abdullah.tahiri.yo@gmail.com>
2019-05-21 13:19:04 +0200

    Revert "Investigate failed test - 1"
    This reverts commit f90fbf09accf6cd8422573e817f96ceabf2bace9.

--- src/Mod/Path/TestPathApp.py
+++ src/Mod/Path/TestPathApp.py
@@ -32,10 +32,9 @@ from PathTests.TestPathOpTools  import TestPathOpTools
 from PathTests.TestPathUtil  import TestPathUtil
 from PathTests.TestPathDepthParams        import depthTestCases
 
-# Testing this one that fails:
-from PathTests.TestPathDressupHoldingTags import TestHoldingTags
+#from PathTests.TestPathDressupHoldingTags import TestHoldingTags
 
-#from PathTests.TestPathDressupDogbone import TestDressupDogbone
+from PathTests.TestPathDressupDogbone import TestDressupDogbone
 #from PathTests.TestPathStock import TestPathStock
 #from PathTests.TestPathTool import TestPathTool
 
