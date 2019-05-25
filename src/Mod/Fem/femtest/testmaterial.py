a96a35eb5bf767be40669a6fab4c230e30f094c8
Bernd Hahnebach <bernd@bimstatik.org>
2019-05-25 12:04:51 +0200

    FEM: code formating, max line length < 100, fem test modules

--- src/Mod/Fem/femtest/testmaterial.py
+++ src/Mod/Fem/femtest/testmaterial.py
@@ -33,7 +33,10 @@ from os.path import join
 class TestMaterialUnits(unittest.TestCase):
     fcc_print('import TestMaterialUnits')
 
-    def setUp(self):
+    # ********************************************************************************************
+    def setUp(
+        self
+    ):
         # init, is executed before every test
         self.doc_name = "TestMaterialUnits"
         try:
@@ -44,7 +47,10 @@ class TestMaterialUnits(unittest.TestCase):
             FreeCAD.setActiveDocument(self.doc_name)
         self.active_doc = FreeCAD.ActiveDocument
 
-    def test_known_quantity_units(self):
+    # ********************************************************************************************
+    def test_known_quantity_units(
+        self
+    ):
         from materialtools.cardutils import get_known_material_quantity_parameter as knownquant
         known_quantity_parameter = knownquant()
         from materialtools.cardutils import check_parm_unit as checkparamunit
@@ -52,17 +58,27 @@ class TestMaterialUnits(unittest.TestCase):
             fcc_print('{}'.format(param))
             self.assertTrue(
                 checkparamunit(param),
-                'Unit of quantity material parameter {} is not known by FreeCAD unit system.'
+                'Unit of quantity material parameter {} '
+                'is not known by FreeCAD unit system.'
                 .format(param)
             )
 
-    def test_material_card_quantities(self):
-        # test the value and unit of known quantity parameter from solid build in material cards
+    # ********************************************************************************************
+    def test_material_card_quantities(
+        self
+    ):
+        # test the value and unit of known quantity parameter
+        # from solid build in material cards
         # keep in mind only if FreeCAD is installed all materials are copied
         # TODO Fluid materials (are they installed?)
 
         # get build in materials
-        builtin_solid_mat_dir = join(FreeCAD.getResourceDir(), "Mod", "Material", "StandardMaterial")
+        builtin_solid_mat_dir = join(
+            FreeCAD.getResourceDir(),
+            "Mod",
+            "Material",
+            "StandardMaterial"
+        )
         fcc_print('{}'.format(builtin_solid_mat_dir))
         from materialtools.cardutils import add_cards_from_a_dir as addmats
         materials, cards, icons = addmats({}, {}, {}, builtin_solid_mat_dir, '')
@@ -84,7 +100,10 @@ class TestMaterialUnits(unittest.TestCase):
                         .format(value, param)
                     )
 
-    def tearDown(self):
+    # ********************************************************************************************
+    def tearDown(
+        self
+    ):
         # clearance, is executed after every test
         FreeCAD.closeDocument(self.doc_name)
         pass
