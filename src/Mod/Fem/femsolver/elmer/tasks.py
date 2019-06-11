627ae70a5307ac834f8d02af1dc0eb93ef59a7fe
Bernd Hahnebach <bernd@bimstatik.org>
2019-06-11 09:59:32 +0200

    FEM: solver elmer tasks, Py3 decode fix

--- src/Mod/Fem/femsolver/elmer/tasks.py
+++ src/Mod/Fem/femsolver/elmer/tasks.py
@@ -28,6 +28,8 @@ __url__ = "http://www.freecadweb.org"
 
 import subprocess
 import os.path
+import sys
+
 import femtools.femutils as femutils
 
 from .. import run
@@ -112,7 +114,10 @@ class Solve(run.Solve):
     def _updateOutput(self, output):
         if self.solver.ElmerOutput is None:
             self._createOutput()
-        self.solver.ElmerOutput.Text = output.decode("utf-8")
+        if sys.version_info.major >= 3:
+            self.solver.ElmerOutput.Text = output
+        else:
+            self.solver.ElmerOutput.Text = output.decode("utf-8")
 
     def _createOutput(self):
         self.solver.ElmerOutput = self.analysis.Document.addObject(
