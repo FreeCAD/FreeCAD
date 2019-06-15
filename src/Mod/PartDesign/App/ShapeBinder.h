18183bc79bb22adbde2e0dd6b36f03da75e16a28
wmayer <wmayer@users.sourceforge.net>
2019-06-15 15:07:52 +0200

    extend ShapeBinder to also handle Origin features

--- src/Mod/PartDesign/App/ShapeBinder.h
+++ src/Mod/PartDesign/App/ShapeBinder.h
@@ -51,8 +51,7 @@ public:
     App::PropertyLinkSubListGlobal    Support;
     App::PropertyBool TraceSupport;
 
-    static void getFilteredReferences(App::PropertyLinkSubList* prop, Part::Feature*& object, std::vector< std::string >& subobjects);
-    static Part::TopoShape buildShapeFromReferences(Feature* obj, std::vector< std::string > subs);
+    static void getFilteredReferences(App::PropertyLinkSubList* prop, App::GeoFeature*& object, std::vector< std::string >& subobjects);
 
     const char* getViewProviderName(void) const {
         return "PartDesignGui::ViewProviderShapeBinder";
@@ -66,6 +65,7 @@ protected:
 private:
     void slotChangedObject(const App::DocumentObject& Obj, const App::Property& Prop);
     virtual void onSettingDocument();
+    static Part::TopoShape buildShapeFromReferences(App::GeoFeature* obj, std::vector< std::string > subs);
 
     typedef boost::signals2::connection Connection;
     Connection connectDocumentChangedObject;
