9453e68e234a8a507b64af5880f60f773c1ad60d
luz.paz <luzpaz@users.noreply.github.com>
2019-06-16 10:03:30 -0400

    Misc. source comment typo and whitespace fixes
    Found via codespell

--- src/Mod/Fem/femtest/testcommon.py
+++ src/Mod/Fem/femtest/testcommon.py
@@ -59,9 +59,9 @@ class TestFemCommon(unittest.TestCase):
         slab.Width = 500.00
         cf = ObjectsFem.makeConstraintFixed(doc)
         ref_eles = []
-        # FreeCAD list property seam not to support append,
-        # thus we need some workaround
-        # which is on many elements even much faster
+        # FreeCAD list property doesn't seem to support append,
+        # thus we need a workaround
+        # which on many elements is even much faster
         for i, face in enumerate(slab.Shape.Edges):
             ref_eles.append("Edge%d" % (i + 1))
         cf.References = [(slab, ref_eles)]
@@ -77,11 +77,11 @@ class TestFemCommon(unittest.TestCase):
     def test_pyimport_all_FEM_modules(
         self
     ):
-        # we're going to try to import all python modules from FreeCAD Fem
+        # we're going to try to import all python modules from FreeCAD FEM
         pymodules = []
 
-        # collect all Python modules in Fem
-        pymodules += testtools.collect_python_modules('')  # Fem main dir
+        # collect all Python modules in FEM
+        pymodules += testtools.collect_python_modules('') # FEM main dir
         pymodules += testtools.collect_python_modules('femexamples')
         pymodules += testtools.collect_python_modules('feminout')
         pymodules += testtools.collect_python_modules('femmesh')
@@ -90,8 +90,8 @@ class TestFemCommon(unittest.TestCase):
         pymodules += testtools.collect_python_modules('femtest')
         pymodules += testtools.collect_python_modules('femtools')
         pymodules += testtools.collect_python_modules('femsolver')
-        # TODO test with join on windows, the use of os.path.join
-        # in following code seams to make problems on windws os
+        # TODO test with join on Windows, the use of os.path.join
+        # in the following code seems to create problems on Windows OS
         pymodules += testtools.collect_python_modules('femsolver/elmer')
         pymodules += testtools.collect_python_modules('femsolver/elmer/equations')
         pymodules += testtools.collect_python_modules('femsolver/z88')
