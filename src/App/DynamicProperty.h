3b2be8ea52f365cfd5c9869d890746a7bc85b74b
Zheng, Lei <realthunder.dev@gmail.com>
2019-07-04 18:27:16 +0800

    CStringHasher: improve rouburstness

--- src/App/DynamicProperty.h
+++ src/App/DynamicProperty.h
@@ -51,9 +51,12 @@ namespace bmi = boost::multi_index;
 
 struct CStringHasher {
     inline std::size_t operator()(const char *s) const {
+        if(!s) return 0;
         return boost::hash_range(s,s+std::strlen(s));
     }
     inline bool operator()(const char *a, const char *b) const {
+        if(!a) return !b;
+        if(!b) return false;
         return std::strcmp(a,b)==0;
     }
 };
