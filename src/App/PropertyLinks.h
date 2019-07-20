4aba7e2f5ac7e866118be101be2480ed5bbd7049
luz.paz <luzpaz@users.noreply.github.com>
2019-07-19 22:42:23 -0400

    Fix more typos

--- src/App/PropertyLinks.h
+++ src/App/PropertyLinks.h
@@ -294,7 +294,7 @@ public:
      * tracking of external objects, therefore the link will not by auto broken
      * when external document is closed. Only use this for temporary case, or
      * if you handle signalDeleteDocument yourself, or use one of the
-     * ProeprtyXLink related property.
+     * PropertyXLink related property.
      */
     void setAllowExternal(bool allow);
 
