80f70ff631d831c218afa7974f74e19f4faecbd4
Zheng, Lei <realthunder.dev@gmail.com>
2019-08-20 10:58:07 +0800

    Gui: try to fix property editor crash
    Crash stack trace
    https://forum.freecadweb.org/viewtopic.php?f=8&t=37757&e=1&view=unread#p327901
    
    It appears to by caused by handling QAbstractItemDelegate::closeEditor
    signal twice. Once inside PropertyItemDelegate::editorClosed via slot
    connection where the editor is closed/destroyed, the other in
    PropertyEditor::closeEditor() which tries to access the destroyed
    editor.
    
    This patch removes handling of closeEditor signal in
    PropertyItemDelegate.

--- src/Gui/propertyeditor/PropertyEditor.cpp
+++ src/Gui/propertyeditor/PropertyEditor.cpp
@@ -154,7 +154,6 @@ void PropertyEditor::commitData (QWidget * editor)
 
 void PropertyEditor::editorDestroyed (QObject * editor)
 {
-    delegate->editorClosed(0,QAbstractItemDelegate::NoHint);
     QTreeView::editorDestroyed(editor);
 }
 
--- src/Gui/propertyeditor/PropertyItemDelegate.h
+++ src/Gui/propertyeditor/PropertyItemDelegate.h
@@ -47,7 +47,6 @@ public:
 
 public Q_SLOTS:
     void valueChanged();
-    void editorClosed (QWidget * editor, QAbstractItemDelegate::EndEditHint hint);
 
 private:
     mutable QWidget *expressionEditor;
