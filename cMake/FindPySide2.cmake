114499dd40d4641e0d805c413a7bdcab286526b9
ezzieyguywuf <WolfgangESanyer@gmail.com>
2019-08-19 23:22:41 -0400

    Updated some variable names to make more sense.

--- cMake/FindPySide2.cmake
+++ cMake/FindPySide2.cmake
@@ -1,22 +1,25 @@
 # - Find PySide2 installation
 #
 # The following variable are set:
+#   PySide2_FOUND   - PySide2 was found
 #   PYSIDE2_FOUND   - PySide2 was found
 #   PYSIDE2_LIBRARY - The PySide2 libraries
 execute_process(
-    COMMAND ${PYTHON_EXECUTABLE} -c "import PySide2;import os;print(os.path.dirname(PySide2.__file__))"
-    RESULT_VARIABLE SUCCESS
+    COMMAND ${PYTHON_EXECUTABLE} -c "import PySide2;import os;print(os.path.dirname(PySide2.__file__), end='')"
+    RESULT_VARIABLE FAILURE
     OUTPUT_VARIABLE PRINT_OUTPUT
 )
 
 set(PYSIDE2_FOUND FALSE)
-if(NOT SUCCESS)
+if(NOT FAILURE)
     set(PYSIDE2_FOUND TRUE)
     set(PYSIDE2_LIBRARY ${PRINT_OUTPUT} CACHE PATH "The location of the PySide2 library")
-endif(NOT SUCCESS)
+endif(NOT FAILURE)
 
 if(NOT PYSIDE2_FOUND)
     MESSAGE("==================\n"
             "PySide2 not found.\n"
             "==================\n")
 endif(NOT PYSIDE2_FOUND)
+
+set(PySide2_FOUND ${PYSIDE2_FOUND})
--- cMake/FindShiboken2.cmake
+++ cMake/FindShiboken2.cmake
@@ -1,24 +1,29 @@
 # - Find Shiboken2 installation
 #
 # The following variable are set:
+#   Shiboken2_FOUND   - Shiboken2 was found
 #   SHIBOKEN2_FOUND   - Shiboken2 was found
+#   Shiboken2_DIR     - The Shiboken2 libraries dir
 #   SHIBOKEN2_LIBRARY - The Shiboken2 libraries
 
 
 execute_process(
-    COMMAND ${PYTHON_EXECUTABLE} -c "import shiboken2;import os;print(os.path.dirname(shiboken2.__file__))"
-    RESULT_VARIABLE SUCCESS
+    COMMAND ${PYTHON_EXECUTABLE} -c "import shiboken2;import os;print(os.path.dirname(shiboken2.__file__), end='')"
+    RESULT_VARIABLE FAILURE
     OUTPUT_VARIABLE PRINT_OUTPUT
 )
 
 set(SHIBOKEN2_FOUND FALSE)
-if(NOT SUCCESS)
+if(NOT FAILURE)
     set(SHIBOKEN2_FOUND TRUE)
     set(SHIBOKEN2_LIBRARY ${PRINT_OUTPUT} CACHE PATH "The location of the Shiboken2 library")
-endif(NOT SUCCESS)
+endif(NOT FAILURE)
 
 if(NOT SHIBOKEN2_FOUND)
     MESSAGE("====================\n"
             "shiboken2 not found.\n"
             "====================\n")
 endif()
+
+set(Shiboken2_FOUND ${SHIBOKEN2_FOUND})
+set(Shiboken2_DIR ${SHIBOKEN2_LIBRARY})
