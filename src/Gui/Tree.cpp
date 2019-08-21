8f32b2dc858c3b470873434bd6b1a15b9f2dc590
Zheng, Lei <realthunder.dev@gmail.com>
2019-08-21 09:57:09 +0800

    Gui: expose MainWindow:showStatus()
    Tree view now uses this to show error on hovering over object in error.

--- src/Gui/Tree.cpp
+++ src/Gui/Tree.cpp
@@ -4585,9 +4585,11 @@ void DocumentObjectItem::displayStatusInfo()
     QString status = TreeWidget::tr("%1, Internal name: %2")
             .arg(info,
                  QString::fromLatin1(Obj->getNameInDocument()));
-    getMainWindow()->showMessage(status);
 
-    if (Obj->isError()) {
+    if (!Obj->isError())
+        getMainWindow()->showMessage(status);
+    else {
+        getMainWindow()->showStatus(MainWindow::Err,status);
         QTreeWidget* tree = this->treeWidget();
         QPoint pos = tree->visualItemRect(this).topRight();
         QToolTip::showText(tree->mapToGlobal(pos), info);
