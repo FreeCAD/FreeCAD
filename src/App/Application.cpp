a1417c5ffa52ea1c55bda08f3534a098dae23e07
Zheng, Lei <realthunder.dev@gmail.com>
2019-08-29 18:53:51 +0800

    App: extend Expression syntax
    * Support sub-object reference syntax using the following syntax,
        Part.<<Box.>>.Placement
      or, with sub-object label referencing
        Part.<<$Cube.>>.Placement
    
    * Extend indexing support, including range based indexing, e.g.
        A1[B2+1][C3][D4:-1]
    
    * Add new constants, None, True, true, False, false.

--- src/App/Application.cpp
+++ src/App/Application.cpp
@@ -1750,7 +1750,6 @@ void Application::initTypes(void)
     App ::ConditionalExpression     ::init();
     App ::StringExpression          ::init();
     App ::FunctionExpression        ::init();
-    App ::BooleanExpression         ::init();
     App ::RangeExpression           ::init();
     App ::PyObjectExpression        ::init();
 
