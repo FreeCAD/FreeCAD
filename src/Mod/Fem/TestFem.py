70ade29afae8a8fafca437a9b9cf5031dbaaed6f
Bernd Hahnebach <bernd@bimstatik.org>
2019-09-02 19:45:37 +0200

    FEM: gmsh tools, fix writing groups and reactivate unit test for solve frame work

--- src/Mod/Fem/TestFem.py
+++ src/Mod/Fem/TestFem.py
@@ -35,7 +35,7 @@ from femtest.testmesh import TestMeshCommon
 from femtest.testmesh import TestMeshEleTetra10
 from femtest.testresult import TestResult
 from femtest.testccxtools import TestCcxTools
-#from femtest.testsolverframework import TestSolverFrameWork
+from femtest.testsolverframework import TestSolverFrameWork
 
 # dummy usage to get flake8 and lgtm quiet
 False if TestFemCommon.__name__ else True
@@ -47,7 +47,7 @@ False if TestMeshEleTetra10.__name__ else True
 False if TestMeshEleTetra10.__name__ else True
 False if TestResult.__name__ else True
 False if TestCcxTools.__name__ else True
-#False if TestSolverFrameWork.__name__ else True
+False if TestSolverFrameWork.__name__ else True
 
 
 # For more information on how to run a specific test class or a test method see
