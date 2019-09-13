909f533e292ed26691bd416548882edce327cb3e
Bernd Hahnebach <bernd@bimstatik.org>
2019-09-13 08:58:55 +0200

    FEM: in out modules, use double quotes instead of single ones

--- src/Mod/Fem/feminout/importZ88O2Results.py
+++ src/Mod/Fem/feminout/importZ88O2Results.py
@@ -33,10 +33,10 @@ import os
 
 
 # ********* generic FreeCAD import and export methods *********
-if open.__module__ == '__builtin__':
+if open.__module__ == "__builtin__":
     # because we'll redefine open below (Python2)
     pyopen = open
-elif open.__module__ == 'io':
+elif open.__module__ == "io":
     # because we'll redefine open below (Python3)
     pyopen = open
 
@@ -68,42 +68,42 @@ def import_z88_disp(
     analysis=None,
     result_name_prefix=None
 ):
-    '''insert a FreeCAD FEM mechanical result object in the ActiveDocument
+    """insert a FreeCAD FEM mechanical result object in the ActiveDocument
     pure usage:
     import feminout.importZ88O2Results as importZ88O2Results
-    disp_file = '/pathtofile/z88o2.txt'
+    disp_file = "/pathtofile/z88o2.txt"
     importZ88O2Results.import_z88_disp(disp_file)
 
     the z888i1.txt FEMMesh file needs to be in the same directory as z88o2.txt
     # ahh, make a new document first ;-)
-    '''
+    """
     from . import importZ88Mesh
     from . import importToolsFem
     import ObjectsFem
     if result_name_prefix is None:
-        result_name_prefix = ''
+        result_name_prefix = ""
     disp_read = read_z88_disp(filename)
     result_mesh_object = None
-    if len(disp_read['Nodes']) > 0:
+    if len(disp_read["Nodes"]) > 0:
         if analysis:
             analysis_object = analysis
 
         # read result mesh
-        if filename.endswith('z88o2.txt'):
-            mesh_file = filename.replace('o2', 'i1')
+        if filename.endswith("z88o2.txt"):
+            mesh_file = filename.replace("o2", "i1")
             mesh_data = importZ88Mesh.read_z88_mesh(mesh_file)
             femmesh = importToolsFem.make_femmesh(mesh_data)
             result_mesh_object = ObjectsFem.makeMeshResult(
                 FreeCAD.ActiveDocument,
-                'Result_mesh'
+                "Result_mesh"
             )
             result_mesh_object.FemMesh = femmesh
         else:
-            FreeCAD.Console.PrintError('Z88 mesh file z88i1.txt not found!')
+            FreeCAD.Console.PrintError("Z88 mesh file z88i1.txt not found!")
 
         # create result obj
-        for result_set in disp_read['Results']:
-            results_name = result_name_prefix + 'results'
+        for result_set in disp_read["Results"]:
+            results_name = result_name_prefix + "results"
 
             res_obj = ObjectsFem.makeResultMechanical(FreeCAD.ActiveDocument, results_name)
             res_obj.Mesh = result_mesh_object
@@ -119,7 +119,7 @@ def import_z88_disp(
 
     else:
         FreeCAD.Console.PrintError(
-            'Problem on Z88 result file import. No nodes found in Z88 result file.\n'
+            "Problem on Z88 result file import. No nodes found in Z88 result file.\n"
         )
     return res_obj
 
@@ -127,11 +127,11 @@ def import_z88_disp(
 def read_z88_disp(
     z88_disp_input
 ):
-    '''
+    """
     read a z88 disp file and extract the nodes and elements
     z88 Displacement output file is z88o2.txt
     works with Z88OS14
-    '''
+    """
     nodes = {}
     mode_disp = {}
     mode_results = {}
@@ -156,11 +156,11 @@ def read_z88_disp(
             mode_disp[node_no] = FreeCAD.Vector(mode_disp_x, mode_disp_y, mode_disp_z)
             nodes[node_no] = node_no
 
-    mode_results['disp'] = mode_disp
+    mode_results["disp"] = mode_disp
     results.append(mode_results)
 
-    for r in results[0]['disp']:
-        FreeCAD.Console.PrintLog("{} --> {}\n".format(r, results[0]['disp'][r]))
+    for r in results[0]["disp"]:
+        FreeCAD.Console.PrintLog("{} --> {}\n".format(r, results[0]["disp"][r]))
 
     z88_disp_file.close()
-    return {'Nodes': nodes, 'Results': results}
+    return {"Nodes": nodes, "Results": results}
