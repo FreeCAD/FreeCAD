2880312c4b51a3765cadef936db7471ba5cf6250
Zheng, Lei <realthunder.dev@gmail.com>
2019-09-17 11:23:08 +0800

    App: migrate old SubElements in Link

--- src/App/Link.h
+++ src/App/Link.h
@@ -204,7 +204,7 @@ public:
         return static_cast<LINK_PPTYPE(_param) *>(prop);\
     }\
 
-    // defines get##Name() and get##Name##Property() accessor
+    // defines get##Name##Property() and get##Name##Value() accessor
     BOOST_PP_SEQ_FOR_EACH(LINK_PROP_GET,_,LINK_PARAMS)
 
     PropertyLinkList *_getElementListProperty() const;
@@ -284,6 +284,8 @@ public:
     void cacheChildLabel(int enable=-1) const;
 
 protected:
+    void _handleChangedPropertyName(Base::XMLReader &reader, 
+            const char * TypeName, const char *PropName);
     void parseSubName() const;
     void update(App::DocumentObject *parent, const Property *prop);
     void syncElementList();
@@ -302,10 +304,12 @@ protected:
     std::unordered_map<const App::DocumentObject*, 
         boost::signals2::scoped_connection> plainGroupConns;
 
+    long myOwner;
+
     mutable std::unordered_map<std::string,int> myLabelCache; // for label based subname lookup
     mutable bool enableLabelCache;
 
-    long myOwner;
+    bool hasOldSubElement;
 };
 
 ///////////////////////////////////////////////////////////////////////////
@@ -452,6 +456,12 @@ public:
         inherited::onDocumentRestored();
     }
 
+    void handleChangedPropertyName(Base::XMLReader &reader, 
+            const char * TypeName, const char *PropName) override
+    {
+        _handleChangedPropertyName(reader,TypeName,PropName);
+    }
+
     bool canLinkProperties() const override;
 };
 
@@ -485,6 +495,12 @@ public:
     }
 
     bool canDelete() const;
+
+    void handleChangedPropertyName(Base::XMLReader &reader, 
+            const char * TypeName, const char *PropName) override
+    {
+        _handleChangedPropertyName(reader,TypeName,PropName);
+    }
 };
 
 typedef App::FeaturePythonT<LinkElement> LinkElementPython;
