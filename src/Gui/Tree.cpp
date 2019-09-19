412e316f53ea2d1decdb412621344d9c924f1d9a
Zheng, Lei <realthunder.dev@gmail.com>
2019-09-19 12:25:26 +0800

    Gui: fix missing tree view sync after drag and drop

--- src/Gui/Tree.cpp
+++ src/Gui/Tree.cpp
@@ -2120,6 +2120,12 @@ void TreeWidget::dropEvent(QDropEvent *event)
 
     if(touched && FC_TREEPARAM(RecomputeOnDrop))
         thisDoc->recompute();
+
+    if(touched && TreeParams::Instance()->SyncView()) {
+        auto gdoc = Application::Instance->getDocument(thisDoc);
+        if(gdoc)
+            gdoc->setActiveView();
+    }
 }
 
 void TreeWidget::drawRow(QPainter *painter, const QStyleOptionViewItem &options, const QModelIndex &index) const
