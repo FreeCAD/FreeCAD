641f615bf501a63a2c152ab74a24108ad295ca02
Markus Lampert <markus@bibi.ca>
2019-10-06 22:13:07 -0700

    Restrict loading of ocl and PathSurface to enabling experimental features.

--- src/Mod/Path/PathScripts/PathGuiInit.py
+++ src/Mod/Path/PathScripts/PathGuiInit.py
@@ -68,12 +68,6 @@ def Startup():
         from PathScripts import PathSimpleCopy
         from PathScripts import PathSimulatorGui
         from PathScripts import PathStop
-        try:
-            import ocl
-            from PathScripts import PathSurfaceGui
-        except ImportError:
-            import FreeCAD
-            FreeCAD.Console.PrintError("OpenCamLib is not working!\n")
         from PathScripts import PathToolController
         from PathScripts import PathToolControllerGui
         from PathScripts import PathToolLibraryManager
