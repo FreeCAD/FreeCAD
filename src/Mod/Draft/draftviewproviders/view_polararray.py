22eacde71bd78167bd0244c4372e9960b69911f9
vocx-fc <vocx-fc@users.noreply.github.com>
2019-10-08 22:27:40 -0500

    Draft: new command for Draft PolarArray
    Added object, viewprovider, icon; gui command definition, task panel,
    add command to InitGui; add callbacks, and delay system.

---
+++ src/Mod/Draft/draftviewproviders/view_polararray.py
@@ -0,0 +1,44 @@
+"""This module provides the view provider code for Draft PolarArray.
+"""
+## @package polararray
+# \ingroup DRAFT
+# \brief This module provides the view provider code for Draft PolarArray.
+
+# ***************************************************************************
+# *   (c) 2019 Eliud Cabrera Castillo <e.cabrera-castillo@tum.de>           *
+# *                                                                         *
+# *   This file is part of the FreeCAD CAx development system.              *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENCE text file.                                 *
+# *                                                                         *
+# *   FreeCAD is distributed in the hope that it will be useful,            *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Library General Public License for more details.                  *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with FreeCAD; if not, write to the Free Software        *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# ***************************************************************************
+
+import Draft
+import Draft_rc
+ViewProviderDraftArray = Draft._ViewProviderDraftArray
+
+# So the resource file doesn't trigger errors from code checkers (flake8)
+True if Draft_rc.__name__ else False
+
+
+class ViewProviderPolarArray(ViewProviderDraftArray):
+
+    def __init__(self, vobj):
+        super().__init__(self, vobj)
+
+    def getIcon(self):
+        return ":/icons/Draft_PolarArray"
