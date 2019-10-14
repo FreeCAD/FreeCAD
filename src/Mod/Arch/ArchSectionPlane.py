1d0bf0e5bced1f9a103add61e9af4a46d8b31266
carlopav <carlopav@gmail.com>
2019-10-14 22:14:02 +0200

    [Arch] Add Section plane context menu for CutView
    Added Section plane context menu to toggle cutview property

--- src/Mod/Arch/ArchSectionPlane.py
+++ src/Mod/Arch/ArchSectionPlane.py
@@ -1133,6 +1133,19 @@ class _ViewProviderSectionPlane:
 
         self.setEdit(vobj,None)
 
+    def setupContextMenu(self,vobj,menu):
+        """CONTEXT MENU setup"""
+        from PySide import QtCore,QtGui
+        action1 = QtGui.QAction(QtGui.QIcon(":/icons/Draft_Edit.svg"),"Toggle Cutview",menu)
+        action1.triggered.connect(lambda f=self.contextCutview, arg=vobj:f(arg))
+        menu.addAction(action1)
+    
+    def contextCutview(self,vobj):
+        """CONTEXT MENU command to toggle CutView property on and off"""
+        if vobj.CutView:
+            vobj.CutView = False
+        else: vobj.CutView = True        
+
 
 class _ArchDrawingView:
 
