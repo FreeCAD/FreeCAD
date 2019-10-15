89f451c8811f49b7679add63b2332849993f06cb
Jean-Marie Verdun <jmverdun3@gmail.com>
2019-10-15 21:03:44 +0200

    Refactor code to prepare Amazon s3 v4 API support We do support currently only v2 which will be EOL in june 2020 according to Amazon

--- src/Mod/Cloud/App/AppCloud.h
+++ src/Mod/Cloud/App/AppCloud.h
@@ -47,8 +47,17 @@ XERCES_CPP_NAMESPACE_END
 
 namespace Cloud {
 
+struct AmzData {
+	std::string digest;
+	char dateFormatted[256];
+	char ContentType[256];
+	char Host[256];
+};
+
 void eraseSubStr(std::string & Str, const std::string & toErase);
 size_t CurlWrite_CallbackFunc_StdString(void *contents, size_t size, size_t nmemb, std::string *s);
+struct AmzData *ComputeDigestAmzS3v2(char *operation, char *data_type, const char *target, const char *Secret);
+struct curl_slist *BuildHeaderAmzS3v2(const char *Url, const char *TcpPort, const char *PublicKey, struct AmzData *Data);
 
 class CloudAppExport CloudReader
 {
