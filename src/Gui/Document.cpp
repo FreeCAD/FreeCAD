820d9bcd01a2cd53d89aa7dbcbbf8519afaaa7ae
Zheng, Lei <realthunder.dev@gmail.com>
2019-10-16 15:23:41 +0800

    Gui: clear selection before undo/redo

--- src/Gui/Document.cpp
+++ src/Gui/Document.cpp
@@ -2144,6 +2144,8 @@ void Document::undo(int iSteps)
     if(!checkTransactionID(true,iSteps))
         return;
 
+    Gui::Selection().clearCompleteSelection();
+
     for (int i=0;i<iSteps;i++) {
         getDocument()->undo();
     }
@@ -2157,6 +2159,8 @@ void Document::redo(int iSteps)
 
     if(!checkTransactionID(false,iSteps))
         return;
+
+    Gui::Selection().clearCompleteSelection();
 
     for (int i=0;i<iSteps;i++) {
         getDocument()->redo();
