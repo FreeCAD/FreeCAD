6242118209116319c00375ef81a2e2809047b471
Zheng, Lei <realthunder.dev@gmail.com>
2019-11-17 10:39:48 +0800

    PartDesign: disable replaceObject() in Body

--- src/Mod/PartDesign/Gui/ViewProviderBody.cpp
+++ src/Mod/PartDesign/Gui/ViewProviderBody.cpp
@@ -517,3 +517,7 @@ void ViewProviderBody::dropObject(App::DocumentObject* obj)
         }
     }
 }
+
+int ViewProviderBody::replaceObject(App::DocumentObject *, App::DocumentObject *) {
+    return 0;
+}
