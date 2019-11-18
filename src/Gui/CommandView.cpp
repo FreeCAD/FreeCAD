87b3cf1944dc3b799f5328a2fcb55b0d9966fdee
Zheng, Lei <realthunder.dev@gmail.com>
2019-11-18 16:53:58 +0800

    Gui: improve selection stack

--- src/Gui/CommandView.cpp
+++ src/Gui/CommandView.cpp
@@ -3180,7 +3180,7 @@ void StdCmdSelBack::activated(int iMsg)
 
 bool StdCmdSelBack::isActive(void)
 {
-  return Selection().selStackBackSize()>1;
+    return Selection().selStackBackSize()>0;
 }
 
 //===========================================================================
--- src/Gui/Selection.cpp
+++ src/Gui/Selection.cpp
@@ -1113,7 +1113,8 @@ void SelectionSingleton::selStackGoBack(int count) {
     if(_SelList.size()) {
         selStackPush(false,true);
         clearCompleteSelection();
-    }
+    } else 
+        --count;
     for(int i=0;i<count;++i) {
         _SelStackForward.push_front(_SelStackBack.back());
         _SelStackBack.pop_back();
