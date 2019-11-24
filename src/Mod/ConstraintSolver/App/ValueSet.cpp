1c0d0f0c1d8e975b9ac04db2f339d67dc1b5ef42
DeepSOIC <vv.titov@gmail.com>
2019-11-24 19:35:35 +0300

    ValueSet: operator support, more methods

--- src/Mod/ConstraintSolver/App/ValueSet.cpp
+++ src/Mod/ConstraintSolver/App/ValueSet.cpp
@@ -9,7 +9,52 @@ using DualNumer = Base::DualNumber;
 FCS::ValueSet::ValueSet(FCS::HParameterSubset subset)
     : _subset(subset)
 {
+    init();
     reset();
+}
+
+void ValueSet::init(bool skip_values)
+{
+    _values.resize(size());
+    _duals.resize(size(), 0.0);
+    _scales.resize(size(), 0.0);
+    for (int i = 0; i < size(); ++i) {
+        _scales[i] = subset()[i].masterScale();
+    }
+}
+
+void ValueSet::checkSameSize(int sz)
+{
+    if (sz != size()){
+        std::stringstream ss;
+        ss << "Length of argument vector (" << sz
+           << ") doesn't match the number of parameters in ValueSet (" << size()
+           << "). Use .paste or .initFrom methods instead.";
+        throw Py::ValueError(ss.str());
+    }
+}
+
+void ValueSet::checkSameSet(const ValueSet& other)
+{
+    if (! other._subset.is(_subset))
+        throw Py::ValueError("Parameter sets of these value sets are not same. Use .paste or .initFrom methods to assign values.");
+}
+
+ValueSet::ValueSet(HParameterSubset subset, const Eigen::VectorXd& vals, bool no_size_check)
+    : _subset(subset)
+{
+    if (!no_size_check){
+        checkSameSize(vals.size());
+    }
+    init(/*skip_values = */true);
+    _values = vals;
+}
+
+ValueSet::ValueSet(HParameterSubset subset, const ValueSet& other)
+    : _subset(subset)
+{
+    init();
+    initFrom(other);
 }
 
 HValueSet ValueSet::make(HParameterSubset subset)
@@ -20,10 +65,26 @@ HValueSet ValueSet::make(HParameterSubset subset)
     return HValueSet(pyobj, /*new_reference=*/true);
 }
 
+HValueSet ValueSet::make(HParameterSubset subset, const Eigen::VectorXd& vals, bool no_size_check)
+{
+    ValueSet* obj = new ValueSet(subset, vals, no_size_check);
+    PyObject* pyobj = new ValueSetPy(obj);
+    obj->_twin = pyobj;
+    return HValueSet(pyobj, /*new_reference=*/true);
+}
+
 HValueSet ValueSet::makeTrivial(HParameterStore store)
 {
     HParameterSubset s = ParameterSubset::make(store);
     return make(s);
+}
+
+HValueSet ValueSet::makeFrom(HParameterSubset subset, const ValueSet& other)
+{
+    ValueSet* obj = new ValueSet(subset, other);
+    PyObject* pyobj = new ValueSetPy(obj);
+    obj->_twin = pyobj;
+    return HValueSet(pyobj, /*new_reference=*/true);
 }
 
 HValueSet ValueSet::copy() const
@@ -55,14 +116,23 @@ Eigen::VectorXd ValueSet::savedValues() const
     return ret;
 }
 
+Eigen::VectorXd ValueSet::getSubvector(const ParameterSubset& params) const
+{
+    Eigen::VectorXd ret(params.size());
+    for (int ito = 0; ito < size(); ++ito) {
+        const ParameterRef& param = params[ito];
+        int ifrom = subset().indexOf(param);
+        if (ifrom == -1)
+            ret[ito] = param.savedValue() / param.masterScale();
+        else
+            ret[ito] = _values[ifrom];
+    }
+    return ret;
+}
+
+
 void ValueSet::reset()
 {
-    _values.resize(size());
-    _duals.resize(size(), 0.0);
-    _scales.resize(size(), 0.0);
-    for (int i = 0; i < size(); ++i) {
-        _scales[i] = subset()[i].masterScale();
-    }
     _values = savedValues();
 }
 
@@ -71,6 +141,35 @@ void ValueSet::apply() const
     for (int i = 0; i < size(); ++i) {
         ParameterRef param = subset()[i];
         param.savedValue() = _values[i] * _scales[i];
+    }
+}
+
+void ValueSet::paste(const ValueSet& from)
+{
+
+    for(int ifrom = 0; ifrom < from.size(); ++ifrom){
+        int ito = _subset->indexOf(from.subset()[ifrom]);
+        if (ito != -1){
+            _values[ito] = from._values[ifrom];
+            _duals[ito] = from._duals[ifrom];
+        }
+    }
+}
+
+void ValueSet::paste(const HValueSet from)
+{
+    paste(*from);
+}
+
+void ValueSet::initFrom(const ValueSet& from)
+{
+
+    for(int ito = 0; ito < size(); ++ito){
+        int ifrom = from.subset().indexOf((*_subset)[ito]);
+        if (ito != -1){
+            _values[ito] = from._values[ifrom];
+            _duals[ito] = from._duals[ifrom];
+        }
     }
 }
 
@@ -97,6 +196,16 @@ void ValueSet::setForDerivative(const Eigen::VectorXd& dir)
     }
     for(int i = 0; i < size(); ++i)
         _duals[i] = dir[i];
+}
+
+void ValueSet::resetDerivatives()
+{
+    _duals.assign(size(), 0.0);
+}
+
+void ValueSet::setDual(const ParameterRef& param, double val)
+{
+    _duals[subset().indexOf(param)] = val;
 }
 
 Base::DualNumber ValueSet::operator[](const ParameterRef& param) const
@@ -127,4 +236,97 @@ Base::DualNumber ValueSet::get(const ParameterRef& param) const
 HValueSet ValueSet::self() const
 {
     return HValueSet(_twin, false);
+}
+
+void ValueSet::operator=(const ValueSet& other)
+{
+    checkSameSet(other);
+    _values = other._values;
+    _duals = other._duals;
+}
+
+void ValueSet::operator=(const HValueSet& other)
+{
+    operator=(*other);
+}
+
+void ValueSet::operator=(const Eigen::VectorXd& vals)
+{
+    checkSameSize(vals.size());
+    _values = vals;
+}
+
+void ValueSet::operator+=(const ValueSet& other)
+{
+    checkSameSet(other);
+    _values += other._values;
+}
+
+void ValueSet::operator*=(double mult)
+{
+    _values *= mult;
+}
+
+HValueSet FCS::operator+(const HValueSet& a, const HValueSet& b)
+{
+    a->checkSameSet(*b);
+    HValueSet hret = ValueSet::make(a->_subset, Eigen::VectorXd(0), true);
+    hret->_values = a->_values + b->_values;
+    return hret;
+}
+
+HValueSet FCS::operator+(const HValueSet &a, const Eigen::VectorXd &b)
+{
+    a->checkSameSize(b.size());
+    HValueSet hret = ValueSet::make(a->_subset, Eigen::VectorXd(0), true);
+    hret->_values = a->_values + b;
+    return hret;
+}
+
+HValueSet FCS::operator+(const Eigen::VectorXd &a, const HValueSet &b)
+{
+    return b+a;
+}
+
+HValueSet FCS::operator-(const HValueSet &a, const HValueSet &b)
+{
+    a->checkSameSet(*b);
+    HValueSet hret = ValueSet::make(a->_subset, Eigen::VectorXd(0), true);
+    hret->_values = a->_values - b->_values;
+    return hret;
+}
+
+HValueSet FCS::operator-(const HValueSet &a, const Eigen::VectorXd &b)
+{
+    a->checkSameSize(b.size());
+    HValueSet hret = ValueSet::make(a->_subset, Eigen::VectorXd(0), true);
+    hret->_values = a->_values - b;
+    return hret;
+}
+
+HValueSet FCS::operator-(const Eigen::VectorXd &b, const HValueSet &a)
+{
+    a->checkSameSize(b.size());
+    HValueSet hret = ValueSet::make(a->_subset, Eigen::VectorXd(0), true);
+    hret->_values = b - a->_values;
+    return hret;
+}
+
+HValueSet FCS::operator*(const HValueSet &a, double b)
+{
+    HValueSet hret = ValueSet::make(a->_subset, Eigen::VectorXd(0), true);
+    hret->_values = a->_values * b;
+    return hret;
+}
+
+HValueSet FCS::operator*(double b, const HValueSet &a)
+{
+    return a*b;
+}
+
+HValueSet FCS::operator/(const HValueSet &a, double b)
+{
+    HValueSet hret = ValueSet::make(a->_subset, Eigen::VectorXd(0), true);
+    hret->_values = a->_values / b;
+    return hret;
 }
--- src/Mod/ConstraintSolver/App/ValueSet.h
+++ src/Mod/ConstraintSolver/App/ValueSet.h
@@ -34,8 +34,7 @@ namespace FCS {
 
 class ValueSet;
 
-typedef UnsafePyHandle<ValueSet> HValueSet;
-
+typedef UnsafePyHandle<ValueSet> HValueSet ;
 /**
  * @brief ValueSet stores overrides for parameter values. Use [] operator (with
  * ParameterRef as an index) to obtain the overridden value of a parameter.
@@ -52,13 +51,21 @@ protected://data
     PyObject* _twin;
 protected://methods
     ValueSet(HParameterSubset subset);
+    ValueSet(HParameterSubset subset, const Eigen::VectorXd& vals, bool no_size_check);
+    ValueSet(HParameterSubset subset, const ValueSet& other);
     ValueSet(const ValueSet& other) = delete; //handle-only, so no copy constructor
 
+    void init(bool skip_values = false);
+    void checkSameSize(int sz);
+    void checkSameSet(const ValueSet& other);
 public:
     //constructors
     static HValueSet make(HParameterSubset subset);
+    static HValueSet make(HParameterSubset subset, const Eigen::VectorXd& vals, bool no_size_check = false);
     ///makes a new empty ValueSet, for evaluation of geometry and constraints outside of solver.
     static HValueSet makeTrivial(HParameterStore store);
+    ///makes a valueset and inits values from another valueset
+    static HValueSet makeFrom(HParameterSubset subset, const ValueSet& other);
     HValueSet copy() const;
 
     int size() const;
@@ -71,26 +78,70 @@ public:
 
     ///returns values as saved in parameters (doesn't change this)
     Eigen::VectorXd savedValues() const;
+    ///returns a vector of values fetched from another valueSet (not on same parameterset)
+    Eigen::VectorXd getSubvector(const ParameterSubset& params) const;
     ///reset values to saved values
     void reset();
+    void initFrom(const ValueSet& from);
     ///save values to parameters
     void apply() const;
+    ///write only overridden values of "from" into this valueset.
+    void paste(const ValueSet& from);
+    void paste(const HValueSet from);
 
     ///set up duals for computing derivatives by a parameter. Returns true if assigned, false if the parameter is not in the set.
     bool setForDerivative(ParameterRef param);
     ///set up duals for computing derivatives along a direction
     void setForDerivative(const Eigen::VectorXd& dir);
+    void resetDerivatives();
+    ///assumes the parameter is in the set!
+    void setDual(const ParameterRef& param, double val);
 
     ///accepts any parameter from store.
     Base::DualNumber operator[](const ParameterRef& param) const;
     Base::DualNumber operator[](int index) const;
     ///with checks against parameters from different stores
     Base::DualNumber get(const ParameterRef& param) const;
-    operator Eigen::VectorXd&() {return _values;}
-    operator const Eigen::VectorXd&() const {return _values;}
+
 
     HValueSet self() const;
+public://operators
+    operator Eigen::VectorXd&() {return _values;}
+    operator const Eigen::VectorXd&() const {return _values;}
+    void operator=(const ValueSet& other);
+    void operator=(const HValueSet& other);
+    void operator=(const Eigen::VectorXd& vals);
+    void operator+=(const ValueSet& other);
+    void operator*=(double mult);
+    //define all on-handle operators. They are convenient: we have to return a handle. Having them avoids extra dereference when chaining operations
+    friend HValueSet operator+(const HValueSet &a, const HValueSet &b);
+    friend HValueSet operator+(const HValueSet &a, const Eigen::VectorXd &b);
+    friend HValueSet operator+(const Eigen::VectorXd &a, const HValueSet &b);
+    friend HValueSet operator-(const HValueSet &a, const HValueSet &b);
+    friend HValueSet operator-(const HValueSet &a, const Eigen::VectorXd &b);
+    friend HValueSet operator-(const Eigen::VectorXd &a, const HValueSet &b);
+    friend HValueSet operator*(const HValueSet &a, double b);
+    friend HValueSet operator*(double b, const HValueSet &a);
+    friend HValueSet operator/(const HValueSet &a, double b);
 };
+
+//define further operators between combinations of hanlde, reference, and eigen-vector
+inline HValueSet operator+(const ValueSet& a, const ValueSet& b)        {return a.self() + b.self();}
+inline HValueSet operator+(const HValueSet& a, const ValueSet& b)       {return a        + b.self();}
+inline HValueSet operator+(const ValueSet& a, const HValueSet& b)       {return a.self() + b;}
+inline HValueSet operator+(const ValueSet& a, const Eigen::VectorXd& b) {return a.self() + b;}
+inline HValueSet operator+(const Eigen::VectorXd& a, const ValueSet& b) {return a        + b.self();}
+
+inline HValueSet operator-(const ValueSet& a, const ValueSet& b)        {return a.self() - b.self();}
+inline HValueSet operator-(const HValueSet& a, const ValueSet& b)       {return a        - b.self();}
+inline HValueSet operator-(const ValueSet& a, const HValueSet& b)       {return a.self() - b;}
+inline HValueSet operator-(const ValueSet& a, const Eigen::VectorXd& b) {return a.self() - b;}
+inline HValueSet operator-(const Eigen::VectorXd& a, const ValueSet& b) {return a        - b.self();}
+
+inline HValueSet operator*(const ValueSet& a, double b){return a.self() * b;}
+inline HValueSet operator*(double b, const ValueSet& a){return a.self() * b;}
+inline HValueSet operator/(const ValueSet& a, double b){return a.self() / b;}
+
 
 } //namespace
 
