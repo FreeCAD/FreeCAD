e66d053637382d7a6b235b52956235e90112374b
Zheng, Lei <realthunder.dev@gmail.com>
2019-11-26 06:50:51 +0800

    Part: fix Compound2 placement lost after restore

--- src/Mod/Part/App/FeatureCompound.cpp
+++ src/Mod/Part/App/FeatureCompound.cpp
@@ -115,6 +115,8 @@ Compound2::Compound2() {
 }
 
 void Compound2::onDocumentRestored() {
+    Base::Placement pla = Placement.getValue();
     auto res = execute();
     delete res;
+    Placement.setValue(pla);
 }
