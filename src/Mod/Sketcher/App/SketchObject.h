7f8146c10a6b386d20c33159c4f28f644c8f281e
luz.paz <luzpaz@users.noreply.github.com>
2019-12-21 17:36:10 -0500

    Sketcher: [skip ci] fix header uniformity
    This PR fixes header uniformity across all Sketcher WB files

--- src/Mod/Sketcher/App/SketchObject.h
+++ src/Mod/Sketcher/App/SketchObject.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (c) Juergen Riegel          (juergen.riegel@web.de) 2008    *
+ *   Copyright (c) 2008 Jürgen Riegel <juergen.riegel@web.de>              *
  *                                                                         *
  *   This file is part of the FreeCAD CAx development system.              *
  *                                                                         *
