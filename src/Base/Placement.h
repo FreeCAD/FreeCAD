6f11ed298a9896c2b693cb2a5c6d540467496ca0
luz.paz <luzpaz@users.noreply.github.com>
2019-12-23 16:28:26 -0500

    src/Base: [skip ci] fix header uniformity
    This PR fixes header uniformity across all `src/Base` files

--- src/Base/Placement.h
+++ src/Base/Placement.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (c) 2006 Juergen Riegel                                     *
+ *   Copyright (c) 2006 Jürgen Riegel <juergen.riegel@web.de>              *
  *                                                                         *
  *   This file is part of the FreeCAD CAx development system.              *
  *                                                                         *
