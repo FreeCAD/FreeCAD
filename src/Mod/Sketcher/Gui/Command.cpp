c709856b2ae151f0166658a78f7c2f44e102686f
Rajendra Pardeshi <rajendrasp79@gmail.com>
2019-12-28 16:08:42 +0530

    fixed #0004230 incremental change - adding spaces instead of tabs for indentation

--- src/Mod/Sketcher/Gui/Command.cpp
+++ src/Mod/Sketcher/Gui/Command.cpp
@@ -723,7 +723,7 @@ void CmdSketcherMirrorSketch::activated(int iMsg)
         std::vector<Part::Geometry *> tempgeo = tempsketch->getInternalGeometry();
         std::vector<Sketcher::Constraint *> tempconstr = tempsketch->Constraints.getValues();
 
-		// If value of addedGeometries or addedConstraints is -1, it gets added to vector begin iterator and that is invlid
+        // If value of addedGeometries or addedConstraints is -1, it gets added to vector begin iterator and that is invlid
         std::vector<Part::Geometry *> mirrorgeo(tempgeo.begin() + (addedGeometries + 1), tempgeo.end());
         std::vector<Sketcher::Constraint *> mirrorconstr(tempconstr.begin() + (addedConstraints + 1), tempconstr.end());
 
