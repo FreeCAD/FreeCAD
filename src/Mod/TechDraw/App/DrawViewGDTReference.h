6185ac849dbee6345d2bcbbbccc2e541dcbdea9a
ludovic leau-mercier (aka lidiriel) <ludovic@scilink.net>
2020-01-02 14:58:09 +0100

    correct provider name

--- src/Mod/TechDraw/App/DrawViewGDTReference.h
+++ src/Mod/TechDraw/App/DrawViewGDTReference.h
@@ -74,7 +74,7 @@ public:
 
     /// returns the type name of the ViewProvider
     virtual const char* getViewProviderName(void) const {
-        return "TechDrawGui::ViewProviderReference";
+        return "TechDrawGui::ViewProviderGDTReference";
     }
 
 protected:
