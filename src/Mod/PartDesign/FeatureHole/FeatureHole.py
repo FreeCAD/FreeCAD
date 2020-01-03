2830b6847fd5a801aef32c93c5baa84c08a519ad
Zheng, Lei <realthunder.dev@gmail.com>
2020-01-03 20:33:53 +0800

    PartDesign: add Python source code encoding

--- src/Mod/PartDesign/FeatureHole/FeatureHole.py
+++ src/Mod/PartDesign/FeatureHole/FeatureHole.py
@@ -1,3 +1,4 @@
+# -*- coding: utf8 -*-
 #/******************************************************************************
 # *   Copyright (c) 2012 Jan Rheinländer <jrheinlaender@users.sourceforge.net> *
 # *                                                                            *
