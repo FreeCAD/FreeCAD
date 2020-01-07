6f52e8425e73693d1242d02dc99a23b63ae36975
Bernd Hahnebach <bernd@bimstatik.org>
2020-01-07 14:56:52 +0100

    Part: compound filter, fix gui object delete if obj has no Base anymore

--- src/Mod/Part/CompoundTools/CompoundFilter.py
+++ src/Mod/Part/CompoundTools/CompoundFilter.py
@@ -206,11 +206,18 @@ class _ViewProviderCompoundFilter:
     def onDelete(self, feature, subelements):  # subelements is a tuple of strings
         if not self.ViewObject.DontUnhideOnDelete:
             try:
-                self.Object.Base.ViewObject.show()
+                if self.Object.Base:
+                    # the base object migt be deleted be the user
+                    # https://forum.freecadweb.org/viewtopic.php?f=3&t=42242
+                    self.Object.Base.ViewObject.show()
                 if self.Object.Stencil:
                     self.Object.Stencil.ViewObject.show()
             except Exception as err:
-                FreeCAD.Console.PrintError("Error in onDelete: " + err.message)
+                if hasattr(err, "message"):
+                    error_string = err.message
+                else:
+                    error_string = err
+                FreeCAD.Console.PrintError("Error in onDelete: {}\n".format(error_string))
         return True
 
 
