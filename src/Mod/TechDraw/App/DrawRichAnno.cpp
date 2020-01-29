10fbb6a7cc41cc5fc621dd5086e360450eda3725
donovaly <uwestoehr@lyx.org>
2020-01-29 23:25:58 +0100

    [TD] set default text size for RichAnno
    see: https://forum.freecadweb.org/viewtopic.php?f=35&t=42414#p361357
    - also improve a tooltip

--- src/Mod/TechDraw/App/DrawRichAnno.cpp
+++ src/Mod/TechDraw/App/DrawRichAnno.cpp
@@ -49,7 +49,7 @@ DrawRichAnno::DrawRichAnno(void)
 
     ADD_PROPERTY_TYPE(AnnoParent,(0),group,(App::PropertyType)(App::Prop_None),
                       "Object to which this annontation is attached");
-    ADD_PROPERTY_TYPE(AnnoText, (""), group, App::Prop_None, "Anno text");
+    ADD_PROPERTY_TYPE(AnnoText, (""), group, App::Prop_None, "Annotation text");
     ADD_PROPERTY_TYPE(ShowFrame, (true), group, App::Prop_None, "Outline rectangle on/off");
     ADD_PROPERTY_TYPE(MaxWidth, (-1.0), group, App::Prop_None, "Width limit before auto wrap");
     Caption.setStatus(App::Property::Hidden,true);
