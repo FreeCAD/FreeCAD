ae3f750044c8b7a1b495f1b951cac6d3f4ddf455
Bernd Hahnebach <bernd@bimstatik.org>
2020-02-01 13:26:28 +0100

    FEM: add constraint contact solid solid example

--- src/Mod/Fem/femexamples/manager.py
+++ src/Mod/Fem/femexamples/manager.py
@@ -34,6 +34,7 @@ doc = run_ccx_cantileverfaceload()
 doc = run_ccx_cantilevernodeload()
 doc = run_ccx_cantileverprescribeddisplacement()
 doc = run_contact_shell_shell()
+doc = run_constraint_contact_solid_solid()
 doc = run_material_nl_platewithhole()
 doc = run_material_multiple_twoboxes()
 doc = run_rcwall2d()
@@ -181,6 +182,21 @@ def run_contact_shell_shell(solver=None, base_name=None):
 
     if base_name is None:
         base_name = "Contact_Shell_Shell"
+        if solver is not None:
+            base_name += "_" + solver
+    run_analysis(doc, base_name)
+    doc.recompute()
+
+    return doc
+
+
+def run_constraint_contact_solid_solid(solver=None, base_name=None):
+
+    from .constraint_contact_solid_solid import setup
+    doc = setup()
+
+    if base_name is None:
+        base_name = "Constraint_Contact_Solid_Solid"
         if solver is not None:
             base_name += "_" + solver
     run_analysis(doc, base_name)
