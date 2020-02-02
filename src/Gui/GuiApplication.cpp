96a4d7013677c954e2ca3caba84cf195f145826a
Zheng, Lei <realthunder.dev@gmail.com>
2020-02-02 15:40:50 +0800

    Gui: catch Py::Exception
    The Python exception is supposed to be captured by application code. If
    it is somehow leaked, it may cause system instability.

--- src/Gui/GuiApplication.cpp
+++ src/Gui/GuiApplication.cpp
@@ -96,8 +96,14 @@ bool GUIApplication::notify (QObject * receiver, QEvent * event)
         return true;
     }
     catch (const Base::Exception& e) {
-        Base::Console().Error("Unhandled Base::Exception caught in GUIApplication::notify.\n"
-                              "The error message is: %s\n", e.what());
+        e.ReportException();
+        Base::Console().Error("Unhandled Base::Exception caught in GUIApplication::notify\n");
+    }
+    catch (Py::Exception &) {
+        Base::PyGILStateLocker lock;
+        Base::PyException e;
+        e.ReportException();
+        Base::Console().Error("Unhandled Python exception caught in GUIApplication::notify\n");
     }
     catch (const std::exception& e) {
         Base::Console().Error("Unhandled std::exception caught in GUIApplication::notify.\n"
