7308db63312c67d8219d4fa9ef4623be6810abe0
wmayer <wmayer@users.sourceforge.net>
2020-02-08 14:04:53 +0100

    Gui: [skip ci] fix naming inconsistency and rename 'combi' to 'combo'

--- src/Gui/Language/FreeCAD_af.ts
+++ src/Gui/Language/FreeCAD_af.ts
@@ -3628,10 +3628,10 @@ The 'Status' column shows whether the document could be recovered.</translation>
     </message>
   </context>
   <context>
-    <name>Gui::DockWnd::CombiView</name>
+    <name>Gui::DockWnd::ComboView</name>
     <message>
-      <source>CombiView</source>
-      <translation>SaamgesteldeVoorkoms</translation>
+      <source>Combo View</source>
+      <translation>Saamgestelde Voorkoms</translation>
     </message>
     <message>
       <source>Tasks</source>
