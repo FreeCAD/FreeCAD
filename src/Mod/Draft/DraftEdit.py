79b2ae5f19fe54149dfdeb7887f7138a534473ad
carlopav <carlopav@gmail.com>
2020-02-12 23:05:12 +0100

    [Draft] Remove AddPoint and DelPoint commands from UI
    [Draft] Edit: Remove Bezcurve interface from Edit Ui
    
    
    [Draft] Edit: cleanup
    
    
    small improvement to tooltip

--- src/Mod/Draft/DraftEdit.py
+++ src/Mod/Draft/DraftEdit.py
@@ -253,14 +253,17 @@ class Edit():
                                 "Part", "Part::Line", "Part::Box"]
 
     def GetResources(self):
+        
+        tooltip = ("Edits the active object.\n"
+                   "Press E or ALT+LeftClick to display context menu\n"
+                   "on supported nodes and on supported objects.")
+                  
         return {'Pixmap': 'Draft_Edit',
                 'Accel': "D, E",
                 'MenuText': QtCore.QT_TRANSLATE_NOOP("Draft_Edit", "Edit"),
-                'ToolTip': QtCore.QT_TRANSLATE_NOOP("Draft_Edit", 
-                                                    "Edits the active object.\n"
-                                                    "Press E or ALT+LeftClick to display context menu\n"
-                                                    "on supported nodes and on supported objects.")
+                'ToolTip': QtCore.QT_TRANSLATE_NOOP("Draft_Edit", tooltip)
                 }
+
 
     #---------------------------------------------------------------------------
     # MAIN FUNCTIONS
@@ -443,18 +446,6 @@ class Edit():
             event.getButton() == event.BUTTON1
             ):#left click
             if not event.wasAltDown():
-                if Draft.getType(self.obj) == "BezCurve" and (self.ui.sharpButton.isChecked()
-                                        or self.ui.tangentButton.isChecked() or
-                                        self.ui.symmetricButton.isChecked()):
-                    pos = event.getPosition()
-                    node = self.getEditNode(pos)
-                    ep = self.getEditNodeIndex(node)
-                    if ep is None:
-                        return
-                    doc = App.getDocument(str(node.documentName.getValue()))
-                    self.obj = doc.getObject(str(node.objectName.getValue()))
-                    if self.obj is None:
-                        return
                 if self.editing is None:
                     self.startEditing(event)
                 else:
