84229b430f5326b60e696d5c1cfc7d32914c8723
wmayer <wmayer@users.sourceforge.net>
2020-02-18 20:05:20 +0100

    Gui: [skip ci] correctly set size when selection resolution of 10000 x 10000

--- src/Gui/DlgSettingsImageImp.cpp
+++ src/Gui/DlgSettingsImageImp.cpp
@@ -219,7 +219,7 @@ void DlgSettingsImageImp::on_standardSizeBox_activated(int index)
     else {
         // try to extract from the string
         QString text = ui->standardSizeBox->itemText(index);
-        QRegExp rx(QLatin1String("\\b\\d{2,4}\\b"));
+        QRegExp rx(QLatin1String("\\b\\d{2,5}\\b"));
         int pos = 0;
         pos = rx.indexIn(text, pos);
         QString w = text.mid(pos, rx.matchedLength());
