361acd5a8405139aed3fbf7036bd76d2ca273cd0
donovaly <uwestoehr@lyx.org>
2020-02-21 03:14:28 +0100

    [PD] enable selection of fillets etc. using arrow keys in Dlg
    see https://forum.freecadweb.org/viewtopic.php?f=19&t=43383&p=370758#p370758

--- src/Mod/PartDesign/Gui/TaskFilletParameters.cpp
+++ src/Mod/PartDesign/Gui/TaskFilletParameters.cpp
@@ -90,6 +90,8 @@ TaskFilletParameters::TaskFilletParameters(ViewProviderDressUp *DressUpView, QWi
     createDeleteAction(ui->listWidgetReferences, ui->buttonRefRemove);
     connect(deleteAction, SIGNAL(triggered()), this, SLOT(onRefDeleted()));
 
+    connect(ui->listWidgetReferences, SIGNAL(currentItemChanged(QListWidgetItem*, QListWidgetItem*)),
+        this, SLOT(setSelection(QListWidgetItem*)));
     connect(ui->listWidgetReferences, SIGNAL(itemClicked(QListWidgetItem*)),
         this, SLOT(setSelection(QListWidgetItem*)));
     connect(ui->listWidgetReferences, SIGNAL(itemDoubleClicked(QListWidgetItem*)),
