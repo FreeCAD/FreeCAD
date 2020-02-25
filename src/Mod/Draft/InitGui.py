76d35e8bfd39ca788e4af3d234f57bbd8bead4c9
vocx-fc <vocx-fc@users.noreply.github.com>
2020-02-24 23:46:42 -0600

    Draft: unit tests registered in Init.py
    This means that now the unit tests will run from
    the console mode when using
    ```
    FreeCADCmd -t 0
    FreeCAD --console -t 0
    ```
    
    This will allow us to catch errors more easily,
    as we separate better the behavior of non-GUI
    and GUI-required modules.
    
    Also small spacing fixes and position of the license.

--- src/Mod/Draft/InitGui.py
+++ src/Mod/Draft/InitGui.py
@@ -174,5 +174,3 @@ FreeCADGui.addPreferencePage(":/ui/preferences-dxf.ui", QT_TRANSLATE_NOOP("Draft
 FreeCADGui.addPreferencePage(":/ui/preferences-dwg.ui", QT_TRANSLATE_NOOP("Draft", "Import-Export"))
 FreeCADGui.addPreferencePage(":/ui/preferences-svg.ui", QT_TRANSLATE_NOOP("Draft", "Import-Export"))
 FreeCADGui.addPreferencePage(":/ui/preferences-oca.ui", QT_TRANSLATE_NOOP("Draft", "Import-Export"))
-
-FreeCAD.__unit_test__ += ["TestDraft"]
