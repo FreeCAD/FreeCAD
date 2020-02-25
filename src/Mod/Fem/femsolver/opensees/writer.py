97c0fde49b9c4f9d0b68b67c89f9b026d5859ab5
Bernd Hahnebach <bernd@bimstatik.org>
2020-02-25 21:01:18 +0100

    FEM: OpenSees tasks and writer, add modules: - the solver runs :-) with a dummy input file - a string in writer is used as dummy input file

--- src/Mod/Fem/femsolver/opensees/writer.py
+++ src/Mod/Fem/femsolver/opensees/writer.py
@@ -51,7 +51,7 @@ class FemInputWriterOpenSees(FemInputWriter.FemInputWriter):
         # working dir and input file
         from os.path import join
         # self.main_file_name = self.mesh_object.Name + ".in"
-        self.main_file_name = "2DPlaneStress.in"
+        self.main_file_name = "opensees_example.tcl"
         self.file_name = join(self.dir_name, self.main_file_name)
         FreeCAD.Console.PrintLog(
             "FemInputWriterCcx --> self.dir_name  -->  " + self.dir_name + "\n"
@@ -79,35 +79,168 @@ class FemInputWriterOpenSees(FemInputWriter.FemInputWriter):
         return self.file_name
 
 
-example_input_file = """2DPlaneStress.out
-Patch test of PlaneStress2d elements -> pure compression
-LinearStatic nsteps 1 nmodules 1
-vtkxml tstep_all domain_all  primvars 1 1 vars 5 1 2 4 5 27 stype 2
-domain 2dPlaneStress
-OutputManager tstep_all dofman_all element_all
-ndofman 8 nelem 5 ncrosssect 1 nmat 1 nbc 3 nic 0 nltf 1 nset 3
-node 1 coords 3  0.0   0.0   0.0
-node 2 coords 3  0.0   4.0   0.0
-node 3 coords 3  2.0   2.0   0.0
-node 4 coords 3  3.0   1.0   0.0
-node 5 coords 3  8.0   0.8   0.0
-node 6 coords 3  7.0   3.0   0.0
-node 7 coords 3  9.0   0.0   0.0
-node 8 coords 3  9.0   4.0   0.0
-PlaneStress2d 1 nodes 4 1 4 3 2  NIP 1
-PlaneStress2d 2 nodes 4 1 7 5 4  NIP 1
-PlaneStress2d 3 nodes 4 4 5 6 3  NIP 1
-PlaneStress2d 4 nodes 4 3 6 8 2  NIP 1
-PlaneStress2d 5 nodes 4 5 7 8 6  NIP 1
-Set 1 elementranges {(1 5)}
-Set 2 nodes 2 1 2
-Set 3 nodes 2 7 8
-SimpleCS 1 thick 1.0 width 1.0 material 1 set 1
-IsoLE 1 d 0. E 15.0 n 0.25 talpha 1.0
-BoundaryCondition 1 loadTimeFunction 1 dofs 2 1 2 values 1 0.0 set 2
-BoundaryCondition 2 loadTimeFunction 1 dofs 1 2 values 1 0.0 set 3
-NodalLoad 3 loadTimeFunction 1 dofs 2 1 2 components 2 2.5 0.0 set 3
-ConstantFunction 1 f(t) 1.0
+example_input_file = """# ----------------------------
+# Start of model generation
+# ----------------------------
+# kgf-m
+# Create ModelBuilder with 3 dimensions and 6 DOF/node
+model basic -ndm 3 -ndf 3
+
+#Basic units
+set m   1.0; # meter for length
+set sec 1.0; # second for time
+set kg  1.0; # Kilogram for mass
+
+#Other units
+# angle
+#set rad 1.0;
+#set deg [expr $PI/180.0*$rad];
+
+# length
+set cm  0.01;
+set in  0.0254;
+set ft [expr 12.0*$in];
+
+# mass
+set lbs 0.4536;
+set kip [expr 1000.0*$lbs];
+set ton [expr 1000.0*$kg]
+
+# force
+set N   [expr 0.1 * $kg * $m / ($sec * $sec)] ;
+set KN [expr 1000.0*$N];
+set MN [expr 1000.0*$KN];
+
+# pressure
+set Pa  [expr 1.0*$N/pow($m, 2)];
+set KPa [expr 1000.0*$Pa];
+set GPa [expr 1000.0*$KPa];
+set pcf [expr $lbs/pow($ft,3)];	# pcf = #/cubic foot
+set ksi [expr $kip/pow($in,2)];
+set psi [expr $ksi/1000.];
+
+
+# create the material
+nDMaterial ElasticIsotropic   1   [expr 210*$GPa]   0.3  7900
+
+# Define geometry
+# ---------------
+
+# define some  parameters
+set eleArgs "1"
+
+#set element stdBrick
+set element bbarBrick
+
+set nz 2
+set nx 6
+set ny 2
+
+set nn [expr ($nz+1)*($nx+1)*($ny+1) ]
+
+# mesh generation
+block3D $nx $ny $nz   8 1  $element  $eleArgs {
+
+    1   4 1 0
+    2   -4 1 0
+    3   -4 0 0
+    4   4 0 0
+    5   4 1 1
+    6   -4 1 1
+    7   -4 0 1
+    8   4 0 1
+}
+
+
+set load [expr 900*$N]
+
+# Constant point load
+pattern Plain 1 Linear {
+   load $nn  0  0 $load
+}
+
+# boundary conditions
+fixX 4   1 1 1  1 1 1
+
+# --------------------------------------------------------------------
+# Start of static analysis (creation of the analysis & analysis itself)
+# --------------------------------------------------------------------
+
+# Load control with variable load steps
+#                       init   Jd  min   max
+integrator LoadControl  1.0  1
+
+# Convergence test
+#                  tolerance maxIter displayCode
+test NormUnbalance     1.0e-10    20     0
+
+# Solution algorithm
+algorithm Newton
+
+# DOF numberer
+numberer RCM
+
+# Cosntraint handler
+constraints Plain
+
+# System of equations solver
+system ProfileSPD
+
+# Analysis for gravity load
+analysis Static
+
+# Perform the analysis
+analyze 5
+
+
+# --------------------------
+# End of static analysis
+# --------------------------
+
+# ----------------------------
+# Start of recorder generation
+# ----------------------------
+
+recorder Node -file Node.out -time -node $nn -dof 1 2 3 disp
+#recorder plot Node.out CenterNodeDisp 625 10 625 450 -columns 1 2
+
+recorder display ShakingBeam 0 0 300 300 -wipe
+prp -100 100 120.5
+vup 0 1 0
+display 1 4 1
+
+# --------------------------
+# End of recorder generation
+# --------------------------
+
+
+# ---------------------------------------
+# Create and Perform the dynamic analysis
+# ---------------------------------------
+
+# Remove the static analysis & reset the time to 0.0
+wipeAnalysis
+setTime 0.0
+
+# Now remove the loads and let the beam vibrate
+remove loadPattern 1
+
+# add some mass proportional damping
+rayleigh 0.01 0.0 0.0 0.0
+
+# Create the transient analysis
+test EnergyIncr     1.0e-10    20   0
+algorithm Newton
+numberer RCM
+constraints Plain
+integrator Newmark 0.5 0.25
+#integrator GeneralizedMidpoint 0.50
+analysis Transient
+
+
+# Perform the transient analysis (20 sec)
+#       numSteps  dt
+analyze 100 2.0
 """
 
 ##  @}
