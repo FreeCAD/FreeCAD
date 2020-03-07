3bba14ac54a7f9a69378f2aa715057034c47309d
Zheng, Lei <realthunder.dev@gmail.com>
2020-03-07 15:48:39 +0800

    Gui: fix build warning

--- src/Gui/ExpressionCompleter.cpp
+++ src/Gui/ExpressionCompleter.cpp
@@ -415,6 +415,8 @@ public:
             assert(getModel());
         }
 
+        virtual ~ModelData() {}
+
         const ExpressionCompleterModel *getModel() const {
             return static_cast<const ExpressionCompleterModel*>(mindex.model());
         }
