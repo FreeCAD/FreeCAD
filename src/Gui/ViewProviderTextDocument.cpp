53b01f08f971429437969c9ff22034759008f04c
wmayer <wmayer@users.sourceforge.net>
2020-03-17 11:36:03 +0100

    Gui: improve usability of text document object

--- src/Gui/ViewProviderTextDocument.cpp
+++ src/Gui/ViewProviderTextDocument.cpp
@@ -122,6 +122,19 @@ void ViewProviderTextDocument::onChanged(const App::Property* prop)
     ViewProviderDocumentObject::onChanged(prop);
 }
 
+MDIView* ViewProviderTextDocument::getMDIView() const
+{
+    auto views = getDocument()->getMDIViewsOfType(
+            TextDocumentEditorView::getClassTypeId());
+    for (auto v : views) {
+        auto textView = static_cast<TextDocumentEditorView *>(v);
+        if (textView->getTextObject() == getObject()) {
+            return textView;
+        }
+    }
+    return nullptr;
+}
+
 bool ViewProviderTextDocument::activateView() const
 {
     auto views = getDocument()->getMDIViewsOfType(
