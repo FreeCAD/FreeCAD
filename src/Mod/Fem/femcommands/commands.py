de0e5fac2c208f0b0d7df61ec28cf262163e9601
Bernd Hahnebach <bernd@bimstatik.org>
2020-03-18 23:43:27 +0100

    FEM: solver z88, add icon and make use of it

--- src/Mod/Fem/femcommands/commands.py
+++ src/Mod/Fem/femcommands/commands.py
@@ -744,7 +744,7 @@ class _SolverZ88(CommandManager):
 
     def __init__(self):
         super(_SolverZ88, self).__init__()
-        self.pixmap = "FEM_SolverStandard"
+        self.pixmap = "FEM_SolverZ88.ico"
         self.menuetext = "Solver Z88"
         self.accel = "S, Z"
         self.tooltip = "Creates a FEM solver Z88"
