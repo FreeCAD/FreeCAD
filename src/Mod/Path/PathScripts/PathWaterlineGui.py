0b084d935ad75e0e31b1bed91f35ee732667ff27
Russell Johnson <47639332+Russ4262@users.noreply.github.com>
2020-03-23 11:31:49 -0500

    Path: Update `Waterline` task panel and GUI code

--- src/Mod/Path/PathScripts/PathWaterlineGui.py
+++ src/Mod/Path/PathScripts/PathWaterlineGui.py
@@ -2,7 +2,8 @@
 
 # ***************************************************************************
 # *                                                                         *
-# *   Copyright (c) 2017 sliptonic <shopinthewoods@gmail.com>               *
+# *   Copyright (c) 2020 sliptonic <shopinthewoods@gmail.com>               *
+# *   Copyright (c) 2020 russ4262 <russ4262@gmail.com>                      *
 # *                                                                         *
 # *   This program is free software; you can redistribute it and/or modify  *
 # *   it under the terms of the GNU Lesser General Public License (LGPL)    *
@@ -31,13 +32,9 @@ import PathScripts.PathOpGui as PathOpGui
 from PySide import QtCore
 
 __title__ = "Path Waterline Operation UI"
-__author__ = "sliptonic (Brad Collette)"
+__author__ = "sliptonic (Brad Collette), russ4262 (Russell Johnson)"
 __url__ = "http://www.freecadweb.org"
 __doc__ = "Waterline operation page controller and command implementation."
-__contributors__ = "russ4262 (Russell Johnson)"
-__created__ = "2019"
-__scriptVersion__ = "3t Usable"
-__lastModified__ = "2019-05-18 21:18 CST"
 
 
 class TaskPanelOpPage(PathOpGui.TaskPanelPage):
@@ -49,25 +46,14 @@ class TaskPanelOpPage(PathOpGui.TaskPanelPage):
 
     def getFields(self, obj):
         '''getFields(obj) ... transfers values from UI to obj's proprties'''
-        # if obj.StartVertex != self.form.startVertex.value():
-        #     obj.StartVertex = self.form.startVertex.value()
-        PathGui.updateInputField(obj, 'DepthOffset', self.form.depthOffset)
+        PathGui.updateInputField(obj, 'BoundaryAdjustment', self.form.boundaryAdjustment)
         PathGui.updateInputField(obj, 'SampleInterval', self.form.sampleInterval)
 
         if obj.StepOver != self.form.stepOver.value():
             obj.StepOver = self.form.stepOver.value()
 
-        # if obj.Algorithm != str(self.form.algorithmSelect.currentText()):
-        #    obj.Algorithm = str(self.form.algorithmSelect.currentText())
-
         if obj.BoundBox != str(self.form.boundBoxSelect.currentText()):
             obj.BoundBox = str(self.form.boundBoxSelect.currentText())
-
-        # if obj.DropCutterDir != str(self.form.dropCutterDirSelect.currentText()):
-        #    obj.DropCutterDir = str(self.form.dropCutterDirSelect.currentText())
-
-        # obj.DropCutterExtraOffset.x = FreeCAD.Units.Quantity(self.form.boundBoxExtraOffsetX.text()).Value
-        # obj.DropCutterExtraOffset.y = FreeCAD.Units.Quantity(self.form.boundBoxExtraOffsetY.text()).Value
 
         if obj.OptimizeLinearPaths != self.form.optimizeEnabled.isChecked():
             obj.OptimizeLinearPaths = self.form.optimizeEnabled.isChecked()
@@ -76,70 +62,38 @@ class TaskPanelOpPage(PathOpGui.TaskPanelPage):
 
     def setFields(self, obj):
         '''setFields(obj) ... transfers obj's property values to UI'''
-        # self.form.startVertex.setValue(obj.StartVertex)
-        # self.selectInComboBox(obj.Algorithm, self.form.algorithmSelect)
+        self.setupToolController(obj, self.form.toolController)
         self.selectInComboBox(obj.BoundBox, self.form.boundBoxSelect)
-        # self.selectInComboBox(obj.DropCutterDir, self.form.dropCutterDirSelect)
-
-        # self.form.boundBoxExtraOffsetX.setText(str(obj.DropCutterExtraOffset.x))
-        # self.form.boundBoxExtraOffsetY.setText(str(obj.DropCutterExtraOffset.y))
-        # self.form.depthOffset.setText(FreeCAD.Units.Quantity(obj.DepthOffset.Value, FreeCAD.Units.Length).UserString)
-        self.form.sampleInterval.setText(str(obj.SampleInterval))
+        self.form.boundaryAdjustment.setText(FreeCAD.Units.Quantity(obj.BoundaryAdjustment.Value, FreeCAD.Units.Length).UserString)
         self.form.stepOver.setValue(obj.StepOver)
+        self.form.sampleInterval.setText(str(obj.SampleInterval))
 
         if obj.OptimizeLinearPaths:
             self.form.optimizeEnabled.setCheckState(QtCore.Qt.Checked)
         else:
             self.form.optimizeEnabled.setCheckState(QtCore.Qt.Unchecked)
 
-        self.setupToolController(obj, self.form.toolController)
-
     def getSignalsForUpdate(self, obj):
         '''getSignalsForUpdate(obj) ... return list of signals for updating obj'''
         signals = []
-        # signals.append(self.form.startVertex.editingFinished)
         signals.append(self.form.toolController.currentIndexChanged)
-        # signals.append(self.form.algorithmSelect.currentIndexChanged)
         signals.append(self.form.boundBoxSelect.currentIndexChanged)
-        # signals.append(self.form.dropCutterDirSelect.currentIndexChanged)
-        # signals.append(self.form.boundBoxExtraOffsetX.editingFinished)
-        # signals.append(self.form.boundBoxExtraOffsetY.editingFinished)
-        signals.append(self.form.sampleInterval.editingFinished)
+        signals.append(self.form.boundaryAdjustment.editingFinished)
         signals.append(self.form.stepOver.editingFinished)
-        # signals.append(self.form.depthOffset.editingFinished)
+        signals.append(self.form.sampleInterval.editingFinished)
         signals.append(self.form.optimizeEnabled.stateChanged)
 
         return signals
 
     def updateVisibility(self):
-        # self.form.boundBoxExtraOffsetX.setEnabled(True)
-        # self.form.boundBoxExtraOffsetY.setEnabled(True)
         self.form.boundBoxSelect.setEnabled(True) 
-        self.form.sampleInterval.setEnabled(True)
+        self.form.boundaryAdjustment.setEnabled(True) 
         self.form.stepOver.setEnabled(True)
-        '''
-        if self.form.algorithmSelect.currentText() == "OCL Dropcutter":
-            self.form.boundBoxExtraOffsetX.setEnabled(True)
-            self.form.boundBoxExtraOffsetY.setEnabled(True)
-            self.form.boundBoxSelect.setEnabled(True)
-            self.form.dropCutterDirSelect.setEnabled(True)
-            self.form.stepOver.setEnabled(True)
-        else:
-            self.form.boundBoxExtraOffsetX.setEnabled(False)
-            self.form.boundBoxExtraOffsetY.setEnabled(False)
-            self.form.boundBoxSelect.setEnabled(False)
-            self.form.dropCutterDirSelect.setEnabled(False)
-            self.form.stepOver.setEnabled(False)
-        '''
-        self.form.boundBoxExtraOffsetX.setEnabled(False)
-        self.form.boundBoxExtraOffsetY.setEnabled(False)
-        self.form.dropCutterDirSelect.setEnabled(False)
-        self.form.depthOffset.setEnabled(False)
-        # self.form.stepOver.setEnabled(False)
-        pass
+        self.form.sampleInterval.setEnabled(True)
+        self.form.optimizeEnabled.setEnabled(True)
 
     def registerSignalHandlers(self, obj):
-        # self.form.algorithmSelect.currentIndexChanged.connect(self.updateVisibility)
+        # self.form.clearLayers.currentIndexChanged.connect(self.updateVisibility)
         pass
 
 
