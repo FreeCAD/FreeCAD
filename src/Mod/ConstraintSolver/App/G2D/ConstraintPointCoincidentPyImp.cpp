63770f5707fb381c05b08b473dc997fe47d8957b
Abdullah Tahiri <abdullah.tahiri.yo@gmail.com>
2020-03-25 16:31:01 +0100

    self to getHandle for coherence

--- src/Mod/ConstraintSolver/App/G2D/ConstraintPointCoincidentPyImp.cpp
+++ src/Mod/ConstraintSolver/App/G2D/ConstraintPointCoincidentPyImp.cpp
@@ -19,8 +19,7 @@ PyObject *ConstraintPointCoincidentPy::PyMake(struct _typeobject *, PyObject* ar
             PyErr_SetString(PyExc_TypeError, "Only keyword arguments are supported");
             throw Py::Exception();
         }
-        /*HConstraintPointCoincident p = (new ConstraintPointCoincident)->self().downcast<ConstraintPointCoincident>();*/
-        HConstraintPointCoincident p = HConstraintPointCoincident(new ConstraintPointCoincident);
+        HConstraintPointCoincident p = (new ConstraintPointCoincident)->getHandle<ConstraintPointCoincident>();
         if (kwd && kwd != Py_None)
             p->initFromDict(Py::Dict(kwd));
         return p.getHandledObject();
