18b3734121c654564fc83a4197b7019b23b5789c
Adam Spontarelli <spontarelliam@gmail.com>
2020-03-26 14:00:20 -0400

    remove unit conversion and cleanup sprocket.py

--- src/Mod/PartDesign/fcsprocket/sprocket.py
+++ src/Mod/PartDesign/fcsprocket/sprocket.py
@@ -18,24 +18,16 @@ def CreateSprocket(w, P, N, Dr):
 
     Remaining variables can be found in Standard Handbook of Chains
     """
-    # P = mm_to_in(P)
-    # Dr = mm_to_in(Dr)
-
     Ds = 1.005 * Dr + 0.003
     R = Ds / 2
     alpha = 35 + 60/N
     beta = 18 - 56 / N
-    # ac = 0.8 * Dr
     M = 0.8 * Dr * cos(radians(35) + radians(60/N))
     T = 0.8 * Dr * sin(radians(35) + radians(60/N))
     E = 1.3025 * Dr + 0.0015
-    # yz = Dr * (1.4 * sin(radians(17) - radians(64)/N) - 0.8 * sin(radians(18) - radians(56) / N))
-    # ab = 1.4 * Dr
     W = 1.4 * Dr * cos(radians(180/N))
     V = 1.4 * Dr * sin(radians(180/N))
     F = Dr * (0.8 * cos(radians(18) - radians(56)/N) + 1.4 * cos(radians(17) - radians(64) / N) - 1.3025) - 0.0015
-    # H = sqrt(F**2 - (1.4 * Dr - P/2.) ** 2)
-    # S = P/2 * cos(radians(180)/N) + H * sin(radians(180)/N)
     PD = P / (sin(radians(180)/N))
 
     # The sprocket tooth gullet consists of four segments
@@ -128,15 +120,5 @@ def rotate(pt, rads):
     return (pt[0] * cosA - pt[1] * sinA,
             pt[0] * sinA + pt[1] * cosA)
 
-def mirror(pt, rads):
-    """
-    mirror pt about radians from vertical axis (CW positive)
-    """
-    sinA = sin(rads)
-    cosA = cos(rads)
-    return(-1 * pt[0] * sinA, pt[1] * cosA)
 
-def in_to_mm(inch):
-    return inch / 0.03937008
-def mm_to_in(mm):
-    return mm * 0.03937008
+
