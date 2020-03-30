39f12af5e90689af82b27a542c1812bd98f0524e
vocx-fc <vocx-fc@users.noreply.github.com>
2020-03-29 21:53:21 -0600

    Draft: move DraftTools utilities to another module
    These small functions and utilities are placed in another
    module, so they can be imported and called
    by the individual Gui Command classes as necessary.
    
    When these Gui Command classes are moved to their respective
    modules, they will be able to import this auxiliary module as well.

--- src/Mod/Draft/DraftTools.py
+++ src/Mod/Draft/DraftTools.py
@@ -114,144 +114,29 @@ elif defaultWP == 3: plane.alignToPointAndAxis(Vector(0,0,0), Vector(1,0,0), 0)
 # last snapped objects, for quick intersection calculation
 lastObj = [0,0]
 
-# set modifier keys
-MODS = ["shift","ctrl","alt"]
-MODCONSTRAIN = MODS[Draft.getParam("modconstrain",0)]
-MODSNAP = MODS[Draft.getParam("modsnap",1)]
-MODALT = MODS[Draft.getParam("modalt",2)]
+# Set modifier keys
+from draftguitools.gui_tool_utils import MODCONSTRAIN
+from draftguitools.gui_tool_utils import MODSNAP
+from draftguitools.gui_tool_utils import MODALT
 
 # ---------------------------------------------------------------------------
 # General functions
 # ---------------------------------------------------------------------------
-def formatUnit(exp,unit="mm"):
-    '''returns a formatting string to set a number to the correct unit'''
-    return FreeCAD.Units.Quantity(exp,FreeCAD.Units.Length).UserString
+from draftguitools.gui_tool_utils import formatUnit
 
-def selectObject(arg):
-    '''this is a scene even handler, to be called from the Draft tools
-    when they need to select an object'''
-    if (arg["Type"] == "SoKeyboardEvent"):
-        if (arg["Key"] == "ESCAPE"):
-            FreeCAD.activeDraftCommand.finish()
-            # TODO : this part raises a coin3D warning about scene traversal, to be fixed.
-    elif (arg["Type"] == "SoMouseButtonEvent"):
-        if (arg["State"] == "DOWN") and (arg["Button"] == "BUTTON1"):
-            cursor = arg["Position"]
-            snapped = Draft.get3DView().getObjectInfo((cursor[0],cursor[1]))
-            if snapped:
-                obj = FreeCAD.ActiveDocument.getObject(snapped['Object'])
-                FreeCADGui.Selection.addSelection(obj)
-                FreeCAD.activeDraftCommand.component=snapped['Component']
-                FreeCAD.activeDraftCommand.proceed()
+from draftguitools.gui_tool_utils import selectObject
 
-def getPoint(target,args,mobile=False,sym=False,workingplane=True,noTracker=False):
-    """Function used by the Draft Tools.
-    returns a constrained 3d point and its original point.
-    if mobile=True, the constraining occurs from the location of
-    mouse cursor when Shift is pressed, otherwise from last entered
-    point. If sym=True, x and y values stay always equal. If workingplane=False,
-    the point won't be projected on the Working Plane. if noTracker is True, the
-    tracking line will not be displayed
-    """
+from draftguitools.gui_tool_utils import getPoint
 
-    ui = FreeCADGui.draftToolBar
+from draftguitools.gui_tool_utils import getSupport
 
-    if target.node:
-        last = target.node[-1]
-    else:
-        last = None
+from draftguitools.gui_tool_utils import setWorkingPlaneToObjectUnderCursor
 
-    amod = hasMod(args, MODSNAP)
-    cmod = hasMod(args, MODCONSTRAIN)
-    point = None
+from draftguitools.gui_tool_utils import setWorkingPlaneToSelectedObject
 
-    if hasattr(FreeCADGui, "Snapper"):
-        point = FreeCADGui.Snapper.snap(args["Position"],lastpoint=last,active=amod,constrain=cmod,noTracker=noTracker)
-        info = FreeCADGui.Snapper.snapInfo
-        mask = FreeCADGui.Snapper.affinity
-    if not point:
-        p = FreeCADGui.ActiveDocument.ActiveView.getCursorPos()
-        point = FreeCADGui.ActiveDocument.ActiveView.getPoint(p)
-        info = FreeCADGui.ActiveDocument.ActiveView.getObjectInfo(p)
-        mask = None
+from draftguitools.gui_tool_utils import hasMod
 
-    ctrlPoint = Vector(point)
-    if target.node:
-        if target.featureName == "Rectangle":
-            ui.displayPoint(point, target.node[0], plane=plane, mask=mask)
-        else:
-            ui.displayPoint(point, target.node[-1], plane=plane, mask=mask)
-    else:
-        ui.displayPoint(point, plane=plane, mask=mask)
-    return point,ctrlPoint,info
-
-def getSupport(mouseEvent=None):
-    """returns the supporting object and sets the working plane"""
-    plane.save()
-    if mouseEvent:
-        return setWorkingPlaneToObjectUnderCursor(mouseEvent)
-    return setWorkingPlaneToSelectedObject()
-
-def setWorkingPlaneToObjectUnderCursor(mouseEvent):
-    objectUnderCursor = Draft.get3DView().getObjectInfo((
-        mouseEvent["Position"][0],
-        mouseEvent["Position"][1]))
-
-    if not objectUnderCursor:
-        return None
-
-    try:
-        componentUnderCursor = getattr(
-            FreeCAD.ActiveDocument.getObject(
-                objectUnderCursor['Object']
-                ).Shape,
-            objectUnderCursor["Component"])
-
-        if not plane.weak:
-            return None
-
-        if "Face" in objectUnderCursor["Component"]:
-            plane.alignToFace(componentUnderCursor)
-        else:
-            plane.alignToCurve(componentUnderCursor)
-        plane.weak = True
-        return objectUnderCursor
-    except:
-        pass
-
-    return None
-
-def setWorkingPlaneToSelectedObject():
-    sel = FreeCADGui.Selection.getSelectionEx()
-    if len(sel) != 1:
-        return None
-    sel = sel[0]
-    if sel.HasSubObjects \
-        and len(sel.SubElementNames) == 1 \
-        and "Face" in sel.SubElementNames[0]:
-        if plane.weak:
-            plane.alignToFace(sel.SubObjects[0])
-            plane.weak = True
-        return sel.Object
-    return None
-
-def hasMod(args,mod):
-    """checks if args has a specific modifier"""
-    if mod == "shift":
-        return args["ShiftDown"]
-    elif mod == "ctrl":
-        return args["CtrlDown"]
-    elif mod == "alt":
-        return args["AltDown"]
-
-def setMod(args,mod,state):
-    """sets a specific modifier state in args"""
-    if mod == "shift":
-        args["ShiftDown"] = state
-    elif mod == "ctrl":
-        args["CtrlDown"] = state
-    elif mod == "alt":
-        args["AltDown"] = state
+from draftguitools.gui_tool_utils import setMod
 
 
 # ---------------------------------------------------------------------------
@@ -367,12 +252,8 @@ class DraftTool:
 # ---------------------------------------------------------------------------
 # Geometry constructors
 # ---------------------------------------------------------------------------
-def redraw3DView():
-    """redraw3DView(): forces a redraw of 3d view."""
-    try:
-        FreeCADGui.ActiveDocument.ActiveView.redraw()
-    except AttributeError as err:
-        pass
+from draftguitools.gui_tool_utils import redraw3DView
+
 
 class Creator(DraftTool):
     """A generic Draft Creator Tool used by creation tools such as line or arc"""
