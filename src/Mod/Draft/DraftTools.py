8fd8b4d4167aba0edcba845fc5ea04d9cd0fddef
vocx-fc <vocx-fc@users.noreply.github.com>
2020-04-01 17:32:49 -0600

    Draft: move Ellipse GuiCommand to gui_ellipses module

--- src/Mod/Draft/DraftTools.py
+++ src/Mod/Draft/DraftTools.py
@@ -160,125 +160,7 @@ from draftguitools.gui_arcs import Arc
 from draftguitools.gui_arcs import Draft_Arc_3Points
 from draftguitools.gui_circles import Circle
 from draftguitools.gui_polygons import Polygon
-
-
-class Ellipse(Creator):
-    """the Draft_Ellipse FreeCAD command definition"""
-
-    def GetResources(self):
-        return {'Pixmap'  : 'Draft_Ellipse',
-                'Accel' : "E, L",
-                'MenuText': QtCore.QT_TRANSLATE_NOOP("Draft_Ellipse", "Ellipse"),
-                'ToolTip': QtCore.QT_TRANSLATE_NOOP("Draft_Ellipse", "Creates an ellipse. CTRL to snap")}
-
-    def Activated(self):
-        name = translate("draft","Ellipse")
-        Creator.Activated(self,name)
-        if self.ui:
-            self.refpoint = None
-            self.ui.pointUi(name)
-            self.ui.extUi()
-            self.call = self.view.addEventCallback("SoEvent",self.action)
-            self.rect = trackers.rectangleTracker()
-            FreeCAD.Console.PrintMessage(translate("draft", "Pick first point")+"\n")
-
-    def finish(self,closed=False,cont=False):
-        """terminates the operation and closes the poly if asked"""
-        Creator.finish(self)
-        if self.ui:
-            self.rect.off()
-            self.rect.finalize()
-        if self.ui:
-            if self.ui.continueMode:
-                self.Activated()
-
-    def createObject(self):
-        """creates the final object in the current doc"""
-        p1 = self.node[0]
-        p3 = self.node[-1]
-        diagonal = p3.sub(p1)
-        halfdiag = Vector(diagonal).multiply(0.5)
-        center = p1.add(halfdiag)
-        p2 = p1.add(DraftVecUtils.project(diagonal, plane.v))
-        p4 = p1.add(DraftVecUtils.project(diagonal, plane.u))
-        r1 = (p4.sub(p1).Length)/2
-        r2 = (p2.sub(p1).Length)/2
-        try:
-            # building command string
-            rot,sup,pts,fil = self.getStrings()
-            if r2 > r1:
-                r1,r2 = r2,r1
-                m = FreeCAD.Matrix()
-                m.rotateZ(math.pi/2)
-                rot1 = FreeCAD.Rotation()
-                rot1.Q = eval(rot)
-                rot2 = FreeCAD.Placement(m)
-                rot2 = rot2.Rotation
-                rot = str((rot1.multiply(rot2)).Q)
-            FreeCADGui.addModule("Draft")
-            if Draft.getParam("UsePartPrimitives",False):
-                # Use Part Primitive
-                self.commit(translate("draft","Create Ellipse"),
-                            ['import Part',
-                             'ellipse = FreeCAD.ActiveDocument.addObject("Part::Ellipse","Ellipse")',
-                             'ellipse.MajorRadius = '+str(r1),
-                             'ellipse.MinorRadius = '+str(r2),
-                             'pl = FreeCAD.Placement()',
-                             'pl.Rotation.Q='+rot,
-                             'pl.Base = '+DraftVecUtils.toString(center),
-                             'ellipse.Placement = pl',
-                             'Draft.autogroup(ellipse)',
-                             'FreeCAD.ActiveDocument.recompute()'])
-            else:
-                self.commit(translate("draft","Create Ellipse"),
-                            ['pl = FreeCAD.Placement()',
-                             'pl.Rotation.Q = '+rot,
-                             'pl.Base = '+DraftVecUtils.toString(center),
-                             'ellipse = Draft.makeEllipse('+str(r1)+','+str(r2)+',placement=pl,face='+fil+',support='+sup+')',
-                             'Draft.autogroup(ellipse)',
-                             'FreeCAD.ActiveDocument.recompute()'])
-        except:
-            print("Draft: Error: Unable to create object.")
-        self.finish(cont=True)
-
-    def action(self,arg):
-        """scene event handler"""
-        if arg["Type"] == "SoKeyboardEvent":
-            if arg["Key"] == "ESCAPE":
-                self.finish()
-        elif arg["Type"] == "SoLocation2Event": #mouse movement detection
-            self.point,ctrlPoint,info = getPoint(self,arg,mobile=True,noTracker=True)
-            self.rect.update(self.point)
-            redraw3DView()
-        elif arg["Type"] == "SoMouseButtonEvent":
-            if (arg["State"] == "DOWN") and (arg["Button"] == "BUTTON1"):
-                if (arg["Position"] == self.pos):
-                    self.finish()
-                else:
-                    if (not self.node) and (not self.support):
-                        getSupport(arg)
-                        self.point,ctrlPoint,info = getPoint(self,arg,mobile=True,noTracker=True)
-                    if self.point:
-                        self.ui.redraw()
-                        self.appendPoint(self.point)
-
-    def numericInput(self,numx,numy,numz):
-        """this function gets called by the toolbar when valid x, y, and z have been entered there"""
-        self.point = Vector(numx,numy,numz)
-        self.appendPoint(self.point)
-
-    def appendPoint(self,point):
-        self.node.append(point)
-        if (len(self.node) > 1):
-            self.rect.update(point)
-            self.createObject()
-        else:
-            FreeCAD.Console.PrintMessage(translate("draft", "Pick opposite point")+"\n")
-            self.ui.setRelative()
-            self.rect.setorigin(point)
-            self.rect.on()
-            if self.planetrack:
-                self.planetrack.set(point)
+from draftguitools.gui_ellipses import Ellipse
 
 
 class Text(Creator):
@@ -3553,7 +3435,6 @@ from draftguitools.gui_snaps import ShowSnapBar
 FreeCADGui.addCommand('Draft_Text',Text())
 FreeCADGui.addCommand('Draft_Dimension',Dimension())
 FreeCADGui.addCommand('Draft_Point',Point())
-FreeCADGui.addCommand('Draft_Ellipse',Ellipse())
 FreeCADGui.addCommand('Draft_ShapeString',ShapeString())
 FreeCADGui.addCommand('Draft_Facebinder',Draft_Facebinder())
 FreeCADGui.addCommand('Draft_Label',Draft_Label())
