7edef56d218f933eba3a04643cda0d3317584ef4
carlopav <carlopav@gmail.com>
2020-04-10 17:39:32 +0200

    [Draft] Removed annotation styles objects
    Removed Annotation styles current implementation.
    As pointed out by yorik, in https://forum.freecadweb.org/viewtopic.php?f=23&t=44051&p=385710#p385179, this feature will be added using document Meta property.

--- src/Mod/Draft/InitGui.py
+++ src/Mod/Draft/InitGui.py
@@ -86,7 +86,6 @@ class DraftWorkbench(FreeCADGui.Workbench):
             from draftguitools import gui_polararray
             from draftguitools import gui_orthoarray
             from draftguitools import gui_arrays
-            from draftguitools import gui_style_dimension
             FreeCADGui.addLanguagePath(":/translations")
             FreeCADGui.addIconPath(":/icons")
         except Exception as exc:
