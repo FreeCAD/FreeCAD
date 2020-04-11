f1ca3c945484f1637d4d4a1805d5f8fa6f20db90
vocx-fc <vocx-fc@users.noreply.github.com>
2020-04-11 18:09:04 -0500

    Draft: small changes to the taskpanels for arrays

--- src/Mod/Draft/drafttaskpanels/task_circulararray.py
+++ src/Mod/Draft/drafttaskpanels/task_circulararray.py
@@ -77,7 +77,7 @@ class TaskPanelCircularArray:
 
     def __init__(self):
         self.name = "Circular array"
-        _log(_tr("Task panel:") + "{}".format(_tr(self.name)))
+        _log(_tr("Task panel:") + " {}".format(_tr(self.name)))
 
         # The .ui file must be loaded into an attribute
         # called `self.form` so that it is displayed in the task panel.
