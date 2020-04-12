8643025d4936221ba9032a649a1c93ce71cbf2b5
vocx-fc <vocx-fc@users.noreply.github.com>
2020-04-11 21:17:18 -0500

    Draft: move WireToBSpline GuiCommand to gui_wire2spline module

--- src/Mod/Draft/DraftTools.py
+++ src/Mod/Draft/DraftTools.py
@@ -185,53 +185,7 @@ from draftguitools.gui_downgrade import Downgrade
 from draftguitools.gui_trimex import Trimex
 from draftguitools.gui_scale import Scale
 from draftguitools.gui_drawing import Drawing
-
-
-class WireToBSpline(Modifier):
-    """The Draft_Wire2BSpline FreeCAD command definition"""
-
-    def __init__(self):
-        self.running = False
-
-    def GetResources(self):
-        return {'Pixmap'  : 'Draft_WireToBSpline',
-                'MenuText': QtCore.QT_TRANSLATE_NOOP("Draft_WireToBSpline", "Wire to B-spline"),
-                'ToolTip': QtCore.QT_TRANSLATE_NOOP("Draft_WireToBSpline", "Converts between Wire and B-spline")}
-
-    def IsActive(self):
-        if FreeCADGui.Selection.getSelection():
-            return True
-        else:
-            return False
-
-    def Activated(self):
-        if self.running:
-            self.finish()
-        else:
-            selection = FreeCADGui.Selection.getSelection()
-            if selection:
-                if (Draft.getType(selection[0]) in ['Wire','BSpline']):
-                    Modifier.Activated(self,"Convert Curve Type")
-                    if self.doc:
-                        self.obj = FreeCADGui.Selection.getSelection()
-                        if self.obj:
-                            self.obj = self.obj[0]
-                            self.pl = None
-                            if "Placement" in self.obj.PropertiesList:
-                                self.pl = self.obj.Placement
-                            self.Points = self.obj.Points
-                            self.closed = self.obj.Closed
-                            n = None
-                            if (Draft.getType(self.obj) == 'Wire'):
-                                n = Draft.makeBSpline(self.Points, self.closed, self.pl)
-                            elif (Draft.getType(self.obj) == 'BSpline'):
-                                self.bs2wire = True
-                                n = Draft.makeWire(self.Points, self.closed, self.pl, None, None, self.bs2wire)
-                            if n:
-                                Draft.formatObject(n,self.obj)
-                                self.doc.recompute()
-                        else:
-                            self.finish()
+from draftguitools.gui_wire2spline import WireToBSpline
 
 
 class Shape2DView(Modifier):
@@ -671,7 +625,6 @@ from draftguitools.gui_snaps import ShowSnapBar
 # drawing commands
 
 # modification commands
-FreeCADGui.addCommand('Draft_WireToBSpline',WireToBSpline())
 FreeCADGui.addCommand('Draft_Draft2Sketch',Draft2Sketch())
 FreeCADGui.addCommand('Draft_Array',Array())
 FreeCADGui.addCommand('Draft_LinkArray',LinkArray())
