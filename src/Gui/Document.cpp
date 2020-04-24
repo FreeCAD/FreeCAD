715d67e2c3c6e90c8a465013e683a38bfa20f46a
Zheng, Lei <realthunder.dev@gmail.com>
2020-04-24 16:06:01 +0800

    Gui: do not prompt when closing view of temp document

--- src/Gui/Document.cpp
+++ src/Gui/Document.cpp
@@ -1989,6 +1989,9 @@ bool Document::canClose (bool checkModify, bool checkLink)
     if (checkLink && App::PropertyXLink::getDocumentInList(getDocument()).size())
         return true;
 
+    if (getDocument()->testStatus(App::Document::TempDoc))
+        return true;
+
     bool ok = true;
     if (checkModify && isModified() && !getDocument()->testStatus(App::Document::PartialDoc)) {
         const char *docName = getDocument()->Label.getValue();
