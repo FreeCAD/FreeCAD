036e7c9d1c566ad47565f0257adc20ea5e8cac4a
carlopav <carlopav@gmail.com>
2020-04-24 09:06:22 +0200

    Draft: split makeCopy from Draft.py
    .
    
    
    Draft: commented drawingview in make copy
    
    
    .

--- src/Mod/Draft/Draft.py
+++ src/Mod/Draft/Draft.py
@@ -350,6 +350,11 @@ from draftmake.make_sketch import make_sketch
 makeSketch = make_sketch
 
 #---------------------------------------------------------------------------
+
+from draftmake.make_copy import make_copy
+from draftmake.make_copy import makeCopy
+
+#---------------------------------------------------------------------------
 # Draft annotation objects
 #---------------------------------------------------------------------------
 
@@ -421,115 +426,6 @@ def convertDraftTexts(textslist=[]):
     for n in todelete:
         FreeCAD.ActiveDocument.removeObject(n)
 
-
-def makeCopy(obj,force=None,reparent=False):
-    """makeCopy(object): returns an exact copy of an object"""
-    if not FreeCAD.ActiveDocument:
-        FreeCAD.Console.PrintError("No active document. Aborting\n")
-        return
-    if (getType(obj) == "Rectangle") or (force == "Rectangle"):
-        newobj = FreeCAD.ActiveDocument.addObject(obj.TypeId,getRealName(obj.Name))
-        _Rectangle(newobj)
-        if gui:
-            _ViewProviderRectangle(newobj.ViewObject)
-    elif (getType(obj) == "Point") or (force == "Point"):
-        newobj = FreeCAD.ActiveDocument.addObject(obj.TypeId,getRealName(obj.Name))
-        _Point(newobj)
-        if gui:
-            _ViewProviderPoint(newobj.ViewObject)
-    elif (getType(obj) in ["Dimension","LinearDimension"]) or (force == "Dimension"):
-        newobj = FreeCAD.ActiveDocument.addObject(obj.TypeId,getRealName(obj.Name))
-        _Dimension(newobj)
-        if gui:
-            _ViewProviderDimension(newobj.ViewObject)
-    elif (getType(obj) == "Wire") or (force == "Wire"):
-        newobj = FreeCAD.ActiveDocument.addObject(obj.TypeId,getRealName(obj.Name))
-        _Wire(newobj)
-        if gui:
-            _ViewProviderWire(newobj.ViewObject)
-    elif (getType(obj) == "Circle") or (force == "Circle"):
-        newobj = FreeCAD.ActiveDocument.addObject(obj.TypeId,getRealName(obj.Name))
-        _Circle(newobj)
-        if gui:
-            _ViewProviderDraft(newobj.ViewObject)
-    elif (getType(obj) == "Polygon") or (force == "Polygon"):
-        newobj = FreeCAD.ActiveDocument.addObject(obj.TypeId,getRealName(obj.Name))
-        _Polygon(newobj)
-        if gui:
-            _ViewProviderDraft(newobj.ViewObject)
-    elif (getType(obj) == "BSpline") or (force == "BSpline"):
-        newobj = FreeCAD.ActiveDocument.addObject(obj.TypeId,getRealName(obj.Name))
-        _BSpline(newobj)
-        if gui:
-            _ViewProviderWire(newobj.ViewObject)
-    elif (getType(obj) == "Block") or (force == "BSpline"):
-        newobj = FreeCAD.ActiveDocument.addObject(obj.TypeId,getRealName(obj.Name))
-        _Block(newobj)
-        if gui:
-            _ViewProviderDraftPart(newobj.ViewObject)
-    elif (getType(obj) == "DrawingView") or (force == "DrawingView"):
-        newobj = FreeCAD.ActiveDocument.addObject(obj.TypeId,getRealName(obj.Name))
-        _DrawingView(newobj)
-    elif (getType(obj) == "Structure") or (force == "Structure"):
-        import ArchStructure
-        newobj = FreeCAD.ActiveDocument.addObject(obj.TypeId,getRealName(obj.Name))
-        ArchStructure._Structure(newobj)
-        if gui:
-            ArchStructure._ViewProviderStructure(newobj.ViewObject)
-    elif (getType(obj) == "Wall") or (force == "Wall"):
-        import ArchWall
-        newobj = FreeCAD.ActiveDocument.addObject(obj.TypeId,getRealName(obj.Name))
-        ArchWall._Wall(newobj)
-        if gui:
-            ArchWall._ViewProviderWall(newobj.ViewObject)
-    elif (getType(obj) == "Window") or (force == "Window"):
-        import ArchWindow
-        newobj = FreeCAD.ActiveDocument.addObject(obj.TypeId,getRealName(obj.Name))
-        ArchWindow._Window(newobj)
-        if gui:
-            ArchWindow._ViewProviderWindow(newobj.ViewObject)
-    elif (getType(obj) == "Panel") or (force == "Panel"):
-        import ArchPanel
-        newobj = FreeCAD.ActiveDocument.addObject(obj.TypeId,getRealName(obj.Name))
-        ArchPanel._Panel(newobj)
-        if gui:
-            ArchPanel._ViewProviderPanel(newobj.ViewObject)
-    elif (getType(obj) == "Sketch") or (force == "Sketch"):
-        newobj = FreeCAD.ActiveDocument.addObject("Sketcher::SketchObject",getRealName(obj.Name))
-        for geo in obj.Geometry:
-            newobj.addGeometry(geo)
-        for con in obj.Constraints:
-            newobj.addConstraint(con)
-    elif hasattr(obj, 'Shape'):
-        newobj = FreeCAD.ActiveDocument.addObject("Part::Feature",getRealName(obj.Name))
-        newobj.Shape = obj.Shape
-    else:
-        print("Error: Object type cannot be copied")
-        return None
-    for p in obj.PropertiesList:
-        if not p in ["Proxy","ExpressionEngine"]:
-            if p in newobj.PropertiesList:
-                if not "ReadOnly" in newobj.getEditorMode(p):
-                    try:
-                        setattr(newobj,p,obj.getPropertyByName(p))
-                    except AttributeError:
-                        try:
-                            setattr(newobj,p,obj.getPropertyByName(p).Value)
-                        except AttributeError:
-                            pass
-    if reparent:
-        parents = obj.InList
-        if parents:
-            for par in parents:
-                if par.isDerivedFrom("App::DocumentObjectGroup"):
-                    par.addObject(newobj)
-                else:
-                    for prop in par.PropertiesList:
-                        if getattr(par,prop) == obj:
-                            setattr(par,prop,newobj)
-
-    formatObject(newobj,obj)
-    return newobj
 
 
 def makeArray(baseobject,arg1,arg2,arg3,arg4=None,arg5=None,arg6=None,name="Array",use_link=False):
