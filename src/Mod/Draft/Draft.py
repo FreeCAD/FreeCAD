22d46141b5d7333de2b060e474341b3c26c15fc7
carlopav <carlopav@gmail.com>
2020-04-26 11:54:09 +0200

    Draft: split join functions from Draft.py

--- src/Mod/Draft/Draft.py
+++ src/Mod/Draft/Draft.py
@@ -182,6 +182,11 @@ from draftutils.gui_utils import load_texture
 from draftfunctions.move import move
 from draftfunctions.rotate import rotate
 
+from draftfunctions.join import join_wires
+from draftfunctions.join import join_wires as joinWires
+from draftfunctions.join import join_two_wires
+from draftfunctions.join import join_two_wires as joinTwoWires
+
 #---------------------------------------------------------------------------
 # Draft objects
 #---------------------------------------------------------------------------
@@ -491,54 +496,6 @@ def extrude(obj,vector,solid=False):
 
     return newobj
 
-def joinWires(wires, joinAttempts = 0):
-    """joinWires(objects): merges a set of wires where possible, if any of those
-    wires have a coincident start and end point"""
-    if joinAttempts > len(wires):
-        return
-    joinAttempts += 1
-    for wire1Index, wire1 in enumerate(wires):
-        for wire2Index, wire2 in enumerate(wires):
-            if wire2Index <= wire1Index:
-                continue
-            if joinTwoWires(wire1, wire2):
-                wires.pop(wire2Index)
-                break
-    joinWires(wires, joinAttempts)
-
-def joinTwoWires(wire1, wire2):
-    """joinTwoWires(object, object): joins two wires if they share a common
-    point as a start or an end.
-
-    BUG: it occasionally fails to join lines even if the lines
-    visually share a point.
-    This is a rounding error in the comparison of the shared point;
-    a small difference will result in the points being considered different
-    and thus the lines not joining.
-    Test properly using `DraftVecUtils.equals` because then it will consider
-    the precision set in the Draft preferences.
-    """
-    wire1AbsPoints = [wire1.Placement.multVec(point) for point in wire1.Points]
-    wire2AbsPoints = [wire2.Placement.multVec(point) for point in wire2.Points]
-    if (wire1AbsPoints[0] == wire2AbsPoints[-1] and wire1AbsPoints[-1] == wire2AbsPoints[0]) \
-        or (wire1AbsPoints[0] == wire2AbsPoints[0] and wire1AbsPoints[-1] == wire2AbsPoints[-1]):
-        wire2AbsPoints.pop()
-        wire1.Closed = True
-    elif wire1AbsPoints[0] == wire2AbsPoints[0]:
-        wire1AbsPoints = list(reversed(wire1AbsPoints))
-    elif wire1AbsPoints[0] == wire2AbsPoints[-1]:
-        wire1AbsPoints = list(reversed(wire1AbsPoints))
-        wire2AbsPoints = list(reversed(wire2AbsPoints))
-    elif wire1AbsPoints[-1] == wire2AbsPoints[-1]:
-        wire2AbsPoints = list(reversed(wire2AbsPoints))
-    elif wire1AbsPoints[-1] == wire2AbsPoints[0]:
-        pass
-    else:
-        return False
-    wire2AbsPoints.pop(0)
-    wire1.Points = [wire1.Placement.inverse().multVec(point) for point in wire1AbsPoints] + [wire1.Placement.inverse().multVec(point) for point in wire2AbsPoints]
-    FreeCAD.ActiveDocument.removeObject(wire2.Name)
-    return True
 
 def split(wire, newPoint, edgeIndex):
     if getType(wire) != "Wire":
