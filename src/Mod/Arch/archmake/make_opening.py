a6f542b8b2122b95459206ebf4777c8ec416ae63
carlopav <carlopav@gmail.com>
2020-05-11 11:47:13 +0200

    Arch: provide base modules fro Opening Object

---
+++ src/Mod/Arch/archmake/make_opening.py
@@ -0,0 +1,40 @@
+#***************************************************************************
+#*   Copyright (c) 2011 Yorik van Havre <yorik@uncreated.net>              *
+#*   Copyright (c) 2020 Carlo Pavan                                        *
+#*                                                                         *
+#*   This program is free software; you can redistribute it and/or modify  *
+#*   it under the terms of the GNU Lesser General Public License (LGPL)    *
+#*   as published by the Free Software Foundation; either version 2 of     *
+#*   the License, or (at your option) any later version.                   *
+#*   for detail see the LICENCE text file.                                 *
+#*                                                                         *
+#*   This program is distributed in the hope that it will be useful,       *
+#*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+#*   GNU Library General Public License for more details.                  *
+#*                                                                         *
+#*   You should have received a copy of the GNU Library General Public     *
+#*   License along with this program; if not, write to the Free Software   *
+#*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+#*   USA                                                                   *
+#*                                                                         *
+#***************************************************************************
+"""Provide the object code for Arch make_opening function."""
+## @package make_opening
+# \ingroup ARCH
+# \brief Provide the object code for Arch make_opening function.
+
+import FreeCAD as App
+
+from archobjects.opening import Opening 
+
+if App.GuiUp:
+    import FreeCADGui as Gui
+    from archviewproviders.view_opening import ViewProviderOpening
+
+
+def make_opening():
+    """
+    NOT IMPLEMENTED YET
+    """
+    pass
