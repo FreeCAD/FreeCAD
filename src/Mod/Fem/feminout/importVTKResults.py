a3a61c3348fbf82eeeb780950d48329872c55846
Bernd Hahnebach <bernd@bimstatik.org>
2020-05-22 18:52:55 +0200

    FEM: normalise license headers

--- src/Mod/Fem/feminout/importVTKResults.py
+++ src/Mod/Fem/feminout/importVTKResults.py
@@ -10,17 +10,17 @@
 # *   the License, or (at your option) any later version.                   *
 # *   for detail see the LICENCE text file.                                 *
 # *                                                                         *
-# *   FreeCAD is distributed in the hope that it will be useful,            *
+# *   This program is distributed in the hope that it will be useful,       *
 # *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
 # *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
-# *   GNU Lesser General Public License for more details.                   *
+# *   GNU Library General Public License for more details.                  *
 # *                                                                         *
 # *   You should have received a copy of the GNU Library General Public     *
-# *   License along with FreeCAD; if not, write to the Free Software        *
+# *   License along with this program; if not, write to the Free Software   *
 # *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
 # *   USA                                                                   *
 # *                                                                         *
-# ***************************************************************************/
+# ***************************************************************************
 
 __title__ = "FreeCAD Result import and export VTK file library"
 __author__ = "Qingfeng Xia, Bernd Hahnebach"
