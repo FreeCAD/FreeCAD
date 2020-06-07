ae641dc5278efafa22b168fc196875558b92e436
wmayer <wmayer@users.sourceforge.net>
2020-06-07 12:53:47 +0200

    Python: [skip ci] tp_print slot has been replaced with tp_vectorcall_offset in Py3.8

--- src/Base/swigpyrun_1.3.38.h
+++ src/Base/swigpyrun_1.3.38.h
@@ -1741,7 +1741,11 @@ _PySwigObject_type(void) {
 	sizeof(SwigPyObject),		    /* tp_basicsize */
 	0,			            /* tp_itemsize */
 	(destructor)SwigPyObject_dealloc,   /* tp_dealloc */
+#if PY_VERSION_HEX < 0x03080000
 	(printfunc)SwigPyObject_print,	    /* tp_print */
+#else
+	0, /*tp_vectorcall_offset*/
+#endif
 #if PY_VERSION_HEX < 0x02020000
 	(getattrfunc)SwigPyObject_getattr,  /* tp_getattr */ 
 #else
@@ -1919,7 +1923,11 @@ _PySwigPacked_type(void) {
 	sizeof(SwigPyPacked),		    /* tp_basicsize */	
 	0,				    /* tp_itemsize */	
 	(destructor)SwigPyPacked_dealloc,   /* tp_dealloc */	
+#if PY_VERSION_HEX < 0x03080000
 	(printfunc)SwigPyPacked_print,	    /* tp_print */   	
+#else
+	0, /*tp_vectorcall_offset*/
+#endif
 	(getattrfunc)0,			    /* tp_getattr */ 	
 	(setattrfunc)0,			    /* tp_setattr */ 	
 	(cmpfunc)SwigPyPacked_compare,	    /* tp_compare */ 	
--- src/CXX/Python3/cxx_extensions.cxx
+++ src/CXX/Python3/cxx_extensions.cxx
@@ -381,7 +381,11 @@ PythonType::PythonType( size_t basic_size, int itemsize, const char *default_nam
 
     // Methods to implement standard operations
     table->tp_dealloc = (destructor)standard_dealloc;
+#if PY_VERSION_HEX < 0x03080000
     table->tp_print = 0;
+#else
+    table->tp_vectorcall_offset = 0;
+#endif
     table->tp_getattr = 0;
     table->tp_setattr = 0;
     table->tp_repr = 0;
@@ -524,7 +528,9 @@ PythonType &PythonType::supportClass()
 #ifdef PYCXX_PYTHON_2TO3
 PythonType &PythonType::supportPrint()
 {
+#if PY_VERSION_HEX < 0x03080000
     table->tp_print = print_handler;
+#endif
     return *this;
 }
 #endif
