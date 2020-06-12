f9fc3a9b057986f33b38a3b8b4ef5197f759d13c
donovaly <uwestoehr@lyx.org>
2020-06-12 03:04:28 +0200

    [TD] add dialog for hatches
    as discussed in the forum, the hatch feature misses a dialog. So here it is. The code is more or less just the one of the existing GeomHatch dialog

--- src/Mod/TechDraw/App/DrawHatch.cpp
+++ src/Mod/TechDraw/App/DrawHatch.cpp
@@ -59,16 +59,16 @@ DrawHatch::DrawHatch(void)
 {
     static const char *vgroup = "Hatch";
 
-    ADD_PROPERTY_TYPE(DirProjection ,(0,0,1.0)    ,vgroup,App::Prop_None,"Projection direction when Hatch was defined");     //sb RO?
-    ADD_PROPERTY_TYPE(Source,(0),vgroup,(App::PropertyType)(App::Prop_None),"The View + Face to be hatched");
+    ADD_PROPERTY_TYPE(DirProjection, (0,0,1.0), vgroup, App::Prop_None, "Projection direction when Hatch was defined");     //sb RO?
+    ADD_PROPERTY_TYPE(Source, (0), vgroup, (App::PropertyType)(App::Prop_None), "The View + Face to be hatched");
     Source.setScope(App::LinkScope::Global);
-    ADD_PROPERTY_TYPE(HatchPattern ,(prefSvgHatch()),vgroup,App::Prop_None,"The hatch pattern file for this area");
+    ADD_PROPERTY_TYPE(HatchPattern, (prefSvgHatch()), vgroup, App::Prop_None, "The hatch pattern file for this area");
     ADD_PROPERTY_TYPE(SvgIncluded, (""), vgroup,App::Prop_None,
-                                            "Embedded Svg hatch file. System use only.");   // n/a to end users
+                                            "Embedded SVG hatch file. System use only.");   // n/a to end users
 
     DirProjection.setStatus(App::Property::ReadOnly,true);
 
-    std::string svgFilter("Svg files (*.svg *.SVG);;All files (*)");
+    std::string svgFilter("SVG files (*.svg *.SVG);;All files (*)");
     HatchPattern.setFilter(svgFilter);
 }
 
