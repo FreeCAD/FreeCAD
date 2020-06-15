02a4ba3bcadc184750a2d93c5cee9072e31c6202
wmayer <wmayer@users.sourceforge.net>
2020-06-15 19:16:19 +0200

    boost: [skip ci] prepare fix for boost < 1.68

---
+++ src/boost_bind_bind.hpp
@@ -0,0 +1,21 @@
+
+// Workaround for boost older than 1.60
+#ifndef BOOST_BIND_BIND_HPP_INCLUDED
+#include <boost/bind/bind.hpp>
+#endif
+
+#include <boost/version.hpp>
+#if (BOOST_VERSION < 106000)
+namespace boost { namespace placeholders {
+    using ::_1;
+    using ::_2;
+    using ::_3;
+    using ::_4;
+    using ::_5;
+    using ::_6;
+    using ::_7;
+    using ::_8;
+    using ::_9;
+}};
+#endif
+
