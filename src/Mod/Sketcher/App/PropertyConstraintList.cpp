7087d2647deb6b4fc5a26de96dc35f990134fea0
Zheng, Lei <realthunder.dev@gmail.com>
2020-07-15 07:43:00 +0000

    Sketcher: enable PropertyConstraintList::isSame()
    Because sketcher constrain is often used with expression. Enable
    isSame() can greatly reduce recomputation due to unrelated changes in
    spreadsheet, such as add/remove rows.
    
    Related realthunder/FreeCAD_Assembly3#306

--- src/Mod/Sketcher/App/PropertyConstraintList.cpp
+++ src/Mod/Sketcher/App/PropertyConstraintList.cpp
@@ -641,8 +641,9 @@ App::Property *PropertyConstraintList::copyBeforeChange() const
 {
     // This effectively disabled change detection in hasSetValue(). To enable
     // it (i.e. trigger onChange() only if the value actually changed), simply
-    // return Copy().
-    return nullptr;
+    //
+    return Copy();
+    // return nullptr;
 }
 
 std::vector<Constraint *> PropertyConstraintList::_emptyValueList(0);
