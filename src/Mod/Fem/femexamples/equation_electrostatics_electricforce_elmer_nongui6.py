48c053471a1178ff0e9d72b17869c92b8b7054e7
vocx-fc <vocx-fc@users.noreply.github.com>
2020-07-31 00:08:20 -0500

    FEM: fix Python 2 error with UTF8 character

--- src/Mod/Fem/femexamples/equation_electrostatics_electricforce_elmer_nongui6.py
+++ src/Mod/Fem/femexamples/equation_electrostatics_electricforce_elmer_nongui6.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ***************************************************************************
 # *   Copyright (c) 2020 Sudhanshu Dubey <sudhanshu.thethunder@gmail.com>   *
 # *                                                                         *
