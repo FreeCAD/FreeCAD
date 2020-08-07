6592a76ac323331a4b97986f18412aebe8244e20
wmayer <wmayer@users.sourceforge.net>
2020-08-07 13:32:29 +0200

    Part: [skip ci] use of ViewOrthoProjMatrix in ViewProvider2DObject::updateData to compute the grid

--- src/Mod/Part/Gui/ViewProvider2DObject.cpp
+++ src/Mod/Part/Gui/ViewProvider2DObject.cpp
@@ -252,7 +252,7 @@ void ViewProvider2DObject::updateData(const App::Property* prop)
             Gui::coinRemoveAllChildren(GridRoot);
             Base::Placement place = static_cast<const Part::PropertyPartShape*>(prop)->getComplexData()->getPlacement();
             place.invert();
-            Base::ViewProjMatrix proj(place.toMatrix());
+            Base::ViewOrthoProjMatrix proj(place.toMatrix());
             Base::BoundBox2d bbox2d = bbox.ProjectBox(&proj);
             this->MinX = bbox2d.MinX;
             this->MaxX = bbox2d.MaxX;
