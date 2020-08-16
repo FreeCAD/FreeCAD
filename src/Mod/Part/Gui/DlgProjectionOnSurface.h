71cf0e64f33cdd75f9155afb66ddd42d406d6a99
luz paz <luzpaz@users.noreply.github.com>
2020-08-16 07:01:13 -0400

    Part: fix typos in source function names and variables
    Found via `codespell`

--- src/Mod/Part/Gui/DlgProjectionOnSurface.h
+++ src/Mod/Part/Gui/DlgProjectionOnSurface.h
@@ -116,7 +116,7 @@ private:
   void create_face_extrude(std::vector<SShapeStore>& iCurrentShape);
   void store_wire_in_vector(const SShapeStore& iCurrentShape, const TopoDS_Shape& iParentShape, std::vector<SShapeStore>& iStoreVec, const unsigned int iColor);
   void set_xyz_dir_spinbox(QDoubleSpinBox* icurrentSpinBox);
-  void transform_shape_to_global_postion(TopoDS_Shape& ioShape, Part::Feature* iPart);
+  void transform_shape_to_global_position(TopoDS_Shape& ioShape, Part::Feature* iPart);
 
 private:
   /** Checks if the given document is about to be closed */
