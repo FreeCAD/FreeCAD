8eb466d3e783d87094a20cda7ea99c6c3cae5f0e
vocx-fc <vocx-fc@users.noreply.github.com>
2020-09-30 18:32:57 -0500

    Surface, MeshPart: add more information to the CurveOnMesh command
    In the Surface Workbench, the command is called `Surface_CurveOnMesh`
    and the only thing it does is call the corresponding command
    in the MeshPart Workbench, where it is defined
    under the name `MeshPart_CurveOnMesh`.
    
    Currently the MeshPart Workbench is not exposed on the graphical user
    interface but it is loaded at startup, and its commands
    are in fact used from the Mesh Workbench and, in this case,
    the Surface Workbench.
    
    Also improve the tooltip of the other commands in the Surface Workbench:
    `Cut`, `Filling`, `GeomFillSurface`, `ExtendFace`, and `Sections`.
    The `Cut` command does not do anything at this moment
    so it is not visible in the interface.

--- src/Mod/MeshPart/Gui/Command.cpp
+++ src/Mod/MeshPart/Gui/Command.cpp
@@ -311,7 +311,15 @@ CmdMeshPartCurveOnMesh::CmdMeshPartCurveOnMesh()
     sAppModule    = "MeshPart";
     sGroup        = QT_TR_NOOP("Mesh");
     sMenuText     = QT_TR_NOOP("Curve on mesh...");
-    sToolTipText  = QT_TR_NOOP("Curve on mesh");
+    sToolTipText  = QT_TR_NOOP("Creates an approximated curve on top of the selected mesh.\n"
+                               "Press 'Start', then pick points on the mesh; "
+                               "when enough points have been set,\n"
+                               "right-click and choose 'Create'.\n"
+                               "\n"
+                               "This command only works with a 'mesh' object, "
+                               "not a regular face or surface.\n"
+                               "To convert an object to a mesh "
+                               "use the tools of the Mesh Workbench.");
     sWhatsThis    = "MeshPart_CurveOnMesh";
     sStatusTip    = sToolTipText;
 }
