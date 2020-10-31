cc04b2674fdea25ec43f30ddb39442ccbd3f0aac
ceanwang <ceanwang@gmail.com>
2020-11-01 08:49:05 +1100

    Added  readNastran95()

--- src/Mod/Fem/App/FemMesh.h
+++ src/Mod/Fem/App/FemMesh.h
@@ -169,6 +169,7 @@ public:
 private:
     void copyMeshData(const FemMesh&);
     void readNastran(const std::string &Filename);
+    void readNastran95(const std::string &Filename);
     void readZ88(const std::string &Filename);
     void readAbaqus(const std::string &Filename);
 
