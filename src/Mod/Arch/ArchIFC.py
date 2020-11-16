1917ef73689f6cc1a3ffe6254e47bedc769809ca
luz paz <luzpaz@users.noreply.github.com>
2020-11-16 07:20:42 -0500

    Arch: Added licence blurbs with attributions to authors
    [skip ci]

--- src/Mod/Arch/ArchIFC.py
+++ src/Mod/Arch/ArchIFC.py
@@ -1,3 +1,26 @@
+#***************************************************************************
+#*   Copyright (c) 2019 Dion Moult <dion@thinkmoult.com>                   *
+#*   Copyright (c) 2019 Yorik van Havre <yorik@uncreated.net>              *
+#*   Copyright (c) 2020 FreeCAD Developers                                 *
+#*                                                                         *
+#*   This program is free software; you can redistribute it and/or modify  *
+#*   it under the terms of the GNU Lesser General Public License (LGPL)    *
+#*   as published by the Free Software Foundation; either version 2 of     *
+#*   the License, or (at your option) any later version.                   *
+#*   for detail see the LICENCE text file.                                 *
+#*                                                                         *
+#*   This program is distributed in the hope that it will be useful,       *
+#*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+#*   GNU Library General Public License for more details.                  *
+#*                                                                         *
+#*   You should have received a copy of the GNU Library General Public     *
+#*   License along with this program; if not, write to the Free Software   *
+#*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+#*   USA                                                                   *
+#*                                                                         *
+#***************************************************************************
+
 """This modules sets up and manages the IFC-related properties, types
 and attributes of Arch/BIM objects.
 """
