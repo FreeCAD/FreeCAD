31d6c7528de19cfc16a99dacae177fe57a4510a4
Aapo <aapo.lankinen@gmail.com>
2020-11-18 21:54:52 +0200

    Refactor code in DrawViewDimension::getRefType() shorter and simpler. No functional change except subelement sizes > 3 are supported, even though they are not supposed to happen.

--- src/Mod/TechDraw/App/DrawViewDimension.h
+++ src/Mod/TechDraw/App/DrawViewDimension.h
@@ -139,12 +139,8 @@ public:
 
     virtual DrawViewPart* getViewPart() const;
     virtual QRectF getRect() const override { return QRectF(0,0,1,1);}          //pretend dimensions always fit!
-    static int getRefType1(const std::string s);
-    static int getRefType2(const std::string s1, const std::string s2);
-    static int getRefType3(const std::string g1,
-                           const std::string g2,
-                           const std::string g3);
-    virtual int getRefType() const;                                   //Vertex-Vertex, Edge, Edge-Edge
+    virtual int getRefType() const;             //Vertex-Vertex, Edge, Edge-Edge
+    static int getRefTypeSubElements(const std::vector<std::string> &);             //Vertex-Vertex, Edge, Edge-Edge
     void setAll3DMeasurement();
     void clear3DMeasurements(void);
     virtual bool checkReferences2D(void) const;
