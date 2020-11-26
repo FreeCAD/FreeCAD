f2bf17ea1eb42d314675bfc752420d8d910e2ef4
Bernd Hahnebach <bernd@bimstatik.org>
2020-11-26 09:05:17 +0100

    FEM: dummy usage of some vars for flake8 no longer needed

--- src/Mod/Fem/femtaskpanels/task_result_mechanical.py
+++ src/Mod/Fem/femtaskpanels/task_result_mechanical.py
@@ -469,13 +469,6 @@ class _TaskPanel:
             maxm = max(UserDefinedFormula)
             self.update_colors_stats(UserDefinedFormula, "", minm, maxm)
 
-        # Dummy use of the variables to get around flake8 error
-        del x, y, z, T, vM, Peeq, P1, P2, P3
-        del sxx, syy, szz, sxy, sxz, syz
-        del exx, eyy, ezz, exy, exz, eyz
-        del MF, NP, rx, ry, rz, mc
-        del s1x, s1y, s1z, s2x, s2y, s2z, s3x, s3y, s3z
-
     def get_scalar_disp_list(self, vector_list, axis):
         # list is needed, as zib-object is not subscriptable in py3
         d = list(zip(*self.result_obj.DisplacementVectors))
