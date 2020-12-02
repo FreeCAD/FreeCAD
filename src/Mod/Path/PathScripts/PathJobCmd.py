e63b90edb571aef26da761d69e3190dc4ec929c3
Markus Lampert <markus@bibi.ca>
2020-12-01 18:25:13 -0800

    Added consistent encoding headers and fixed whitespaces

--- src/Mod/Path/PathScripts/PathJobCmd.py
+++ src/Mod/Path/PathScripts/PathJobCmd.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ***************************************************************************
 # *   Copyright (c) 2017 sliptonic <shopinthewoods@gmail.com>               *
 # *                                                                         *
