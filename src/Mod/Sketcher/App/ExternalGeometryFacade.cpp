abdullahtahiriyo/FreeCAD_sf_master

Sketcher: Extend GeometryFacade and ExternaGeometryFacade to GeometryMode

src/Mod/Sketcher/App/ExternalGeometryFacade.cpp
@@ -36,7 +36,7 @@
 
 using namespace Sketcher;
 
-TYPESYSTEM_SOURCE(Sketcher::ExternalGeometryFacade,Part::GeometryExtension)
+TYPESYSTEM_SOURCE(Sketcher::ExternalGeometryFacade,Base::Persistence)
 
 ExternalGeometryFacade::ExternalGeometryFacade(): Geo(nullptr), SketchGeoExtension(nullptr), ExternalGeoExtension(nullptr)
 {

src/Mod/Sketcher/App/GeometryFacade.cpp
@@ -36,7 +36,7 @@
 
 using namespace Sketcher;
 
-TYPESYSTEM_SOURCE(Sketcher::GeometryFacade,Part::GeometryExtension)
+TYPESYSTEM_SOURCE(Sketcher::GeometryFacade,Base::Persistence)
 
 GeometryFacade::GeometryFacade(): Geo(nullptr), SketchGeoExtension(nullptr)
 {

FreeCAD/FreeCAD

src/Mod/Sketcher/App/ExternalGeometryFacade.cpp
@@ -36,7 +36,7 @@
 
 using namespace Sketcher;
 
-TYPESYSTEM_SOURCE(Sketcher::ExternalGeometryFacade,Part::GeometryExtension)
+TYPESYSTEM_SOURCE(Sketcher::ExternalGeometryFacade,Base::BaseClass)
 
 ExternalGeometryFacade::ExternalGeometryFacade(): Geo(nullptr), SketchGeoExtension(nullptr), ExternalGeoExtension(nullptr)
 {

src/Mod/Sketcher/App/GeometryFacade.cpp
@@ -36,7 +36,7 @@
 
 using namespace Sketcher;
 
-TYPESYSTEM_SOURCE(Sketcher::GeometryFacade,Part::GeometryExtension)
+TYPESYSTEM_SOURCE(Sketcher::GeometryFacade,Base::BaseClass)
 
 GeometryFacade::GeometryFacade(): Geo(nullptr), SketchGeoExtension(nullptr)
 {