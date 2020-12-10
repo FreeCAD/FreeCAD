7c43b059df1b33ceffb3aa57d7e2b162a310f5e1
luz paz <luzpaz@users.noreply.github.com>
2020-12-09 23:15:52 -0500

    Part: fix header uniformity + trailing whitespace
    [skip ci]

--- src/Mod/Part/App/TopologyPy.cpp
+++ src/Mod/Part/App/TopologyPy.cpp
@@ -1,6 +1,5 @@
-
 /***************************************************************************
- *   (c) Jürgen Riegel (juergen.riegel@web.de) 2002                        *
+ *   Copyright (c) 2002 Jürgen Riegel <juergen.riegel@web.de>              *
  *                                                                         *
  *   This file is part of the FreeCAD CAx development system.              *
  *                                                                         *
@@ -20,7 +19,6 @@
  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
  *   USA                                                                   *
  *                                                                         *
- *   Juergen Riegel 2002                                                   *
  ***************************************************************************/
 
 
