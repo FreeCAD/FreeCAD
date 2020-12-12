a9605dc462642d3cae76b4cfdee8b141a285c1a6
wmayer <wmayer@users.sourceforge.net>
2020-12-12 10:53:20 +0100

    CMake: [skip ci] use FILTER of list command only for v3.6 or later

--- cMake/FindOpenCasCade.cmake
+++ cMake/FindOpenCasCade.cmake
@@ -37,10 +37,12 @@ else(OCE_FOUND) #look for OpenCASCADE
   # we first try to find opencascade directly:
   if(NOT OCCT_CMAKE_FALLBACK)
     find_package(OpenCASCADE CONFIG QUIET)
-    get_property(flags DIRECTORY PROPERTY COMPILE_DEFINITIONS)
-    # OCCT 7.5 adds this define that causes hundreds of compiler warnings with Qt5.x, so remove it again
-    list(FILTER flags EXCLUDE REGEX [[GL_GLEXT_LEGACY]])
-    set_property(DIRECTORY PROPERTY COMPILE_DEFINITIONS ${flags})
+    if(NOT (CMAKE_VERSION VERSION_LESS 3.6.0))
+        get_property(flags DIRECTORY PROPERTY COMPILE_DEFINITIONS)
+        # OCCT 7.5 adds this define that causes hundreds of compiler warnings with Qt5.x, so remove it again
+        list(FILTER flags EXCLUDE REGEX [[GL_GLEXT_LEGACY]])
+        set_property(DIRECTORY PROPERTY COMPILE_DEFINITIONS ${flags})
+    endif()
   endif(NOT OCCT_CMAKE_FALLBACK)
   if(OpenCASCADE_FOUND)
     set(OCC_FOUND ${OpenCASCADE_FOUND})
