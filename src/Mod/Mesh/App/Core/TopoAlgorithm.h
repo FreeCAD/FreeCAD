c9927ec404aa21505ed79e1e67cd61775a3d7046
Josh Soref <jsoref@users.noreply.github.com>
2021-01-03 02:22:21 -0500

    spelling: except
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/Mod/Mesh/App/Core/TopoAlgorithm.h
+++ src/Mod/Mesh/App/Core/TopoAlgorithm.h
@@ -335,7 +335,7 @@ public:
     void SearchForComponents(TMode tMode, std::vector<std::vector<unsigned long> >& aclT) const;
 
     /**
-     * Does basically the same as the method above escept that only the faces in
+     * Does basically the same as the method above except that only the faces in
      * \a aSegment are regarded.
      */
     void SearchForComponents(TMode tMode, const std::vector<unsigned long>& aSegment,
