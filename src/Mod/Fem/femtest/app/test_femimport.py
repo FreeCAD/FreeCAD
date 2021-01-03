2f29adc76699a29915bd01afda8a4f97b3c62ea8
Josh Soref <jsoref@users.noreply.github.com>
2021-01-03 02:22:39 -0500

    spelling: existence
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/Mod/Fem/femtest/app/test_femimport.py
+++ src/Mod/Fem/femtest/app/test_femimport.py
@@ -80,8 +80,8 @@ class TestFemImport(unittest.TestCase):
 # ************************************************************************************************
 # ************************************************************************************************
 # to be sure this is run on very first of FEM test this is here and not in objects
-class TestObjectExistance(unittest.TestCase):
-    fcc_print("import TestObjectExistance")
+class TestObjectExistence(unittest.TestCase):
+    fcc_print("import TestObjectExistence")
 
     # ********************************************************************************************
     def setUp(
@@ -105,14 +105,14 @@ class TestObjectExistance(unittest.TestCase):
     ):
         # since method name starts with 00 this will be run first
         # this test just prints a line with stars
-        fcc_print("\n{0}\n{1} run FEM TestObjectExistance tests {2}\n{0}".format(
+        fcc_print("\n{0}\n{1} run FEM TestObjectExistence tests {2}\n{0}".format(
             100 * "*",
             10 * "*",
             55 * "*"
         ))
 
     # ********************************************************************************************
-    def test_objects_existance(
+    def test_objects_existence(
         self
     ):
 
