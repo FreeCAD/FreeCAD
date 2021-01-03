bd9b782911d6641726c94808cb56a05768497729
Josh Soref <jsoref@users.noreply.github.com>
2021-01-03 02:04:34 -0500

    spelling: correspond
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/Mod/Assembly/App/Product.h
+++ src/Mod/Assembly/App/Product.h
@@ -44,7 +44,7 @@ public:
     App::PropertyLinkList   Items;
 
    /** @name base properties of all Assembly Items 
-     * This properties corospond mostly to the meta information
+     * This properties correspond mostly to the meta information
      * in the App::Document class
      */
     //@{
