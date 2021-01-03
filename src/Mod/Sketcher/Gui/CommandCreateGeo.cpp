fb0ea4cdc17881754ec369c22e431d2b5e335e28
Josh Soref <jsoref@users.noreply.github.com>
2021-01-03 03:19:04 -0500

    spelling: sketch
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/Mod/Sketcher/Gui/CommandCreateGeo.cpp
+++ src/Mod/Sketcher/Gui/CommandCreateGeo.cpp
@@ -5878,7 +5878,7 @@ public:
 
                     // adding external geometry does not require a solve() per se (the DoF is the same),
                     // however a solve is required to update the amount of solver geometry, because we only
-                    // redraw a changed Sketch if the solver geometry amount is the same as the SkethObject
+                    // redraw a changed Sketch if the solver geometry amount is the same as the SketchObject
                     // geometry amount (as this avoids other issues).
                     // This solver is a very low cost one anyway (there is actually nothing to solve).
                     tryAutoRecomputeIfNotSolve(static_cast<Sketcher::SketchObject *>(sketchgui->getObject()));
