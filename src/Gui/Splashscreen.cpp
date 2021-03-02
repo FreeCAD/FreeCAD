c8fa2e8751b44dd68101d9f1d2df0c3e61fff8ba
Chris Hennes <chennes@pioneerlibrarysystem.org>
2021-03-02 10:52:38 -0600

    Make About window non-resizeable

--- src/Gui/Splashscreen.cpp
+++ src/Gui/Splashscreen.cpp
@@ -238,6 +238,7 @@ AboutDialog::AboutDialog(bool showLic, QWidget* parent)
 
     setModal(true);
     ui->setupUi(this);
+    layout()->setSizeConstraint(QLayout::SetFixedSize);
 #if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
     QRect rect = QApplication::primaryScreen()->availableGeometry();
 #else
