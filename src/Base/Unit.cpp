0cbad1f2d5870da71e2f2dc3d800b0798f136a80
donovaly <uwestoehr@lyx.org>
2021-03-22 01:01:07 +0100

    [Base] add electrical conductivity
    - this is necessary to perform electrodynamics simulations. Since the typical values are in the range of 1e6 S/m, we need the new units kS and MS
    
    - since this is for simulations, also add electrical conductivity to the FEM material cards

--- src/Base/Unit.cpp
+++ src/Base/Unit.cpp
@@ -447,6 +447,7 @@ QString Unit::getTypeString(void) const
     if(*this == Unit::ElectricalInductance        )       return QString::fromLatin1("ElectricalInductance");
     if(*this == Unit::ElectricalConductance       )       return QString::fromLatin1("ElectricalConductance");
     if(*this == Unit::ElectricalResistance        )       return QString::fromLatin1("ElectricalResistance");
+    if(*this == Unit::ElectricalConductivity      )       return QString::fromLatin1("ElectricalConductivity");
     if(*this == Unit::AmountOfSubstance           )       return QString::fromLatin1("AmountOfSubstance");
     if(*this == Unit::LuminousIntensity           )       return QString::fromLatin1("LuminousIntensity");
     if(*this == Unit::Pressure                    )       return QString::fromLatin1("Pressure");
@@ -495,6 +496,7 @@ Unit Unit::ElectricalCapacitance(-2,-1,4,2);
 Unit Unit::ElectricalInductance(2,1,-2,-2);
 Unit Unit::ElectricalConductance(-2,-1,3,2);
 Unit Unit::ElectricalResistance(2,1,-3,-2);
+Unit Unit::ElectricalConductivity(-3,-1,3,2);
 Unit Unit::AmountOfSubstance(0,0,0,0,0,1);
 Unit Unit::LuminousIntensity(0,0,0,0,0,0,1);
 
