51f010d972d09cd0f9b61d7958016093ebb7d6f0
Abdullah Tahiri <abdullah.tahiri.yo@gmail.com>
2021-03-28 19:25:55 +0200

    Sketcher: No vertex color update (selection/preselection) after box selection
    =============================================================================
    
    The issue:
    https://forum.freecadweb.org/viewtopic.php?f=10&t=56550#p486554
    
    The fix:
    Setting the render type to Image causes the issue, but neither do I know why it is
    necessary to set it to Image, nor do I understand why this is causing the issue. I
    only know it solves the issue.

--- src/Mod/Sketcher/Gui/ViewProviderSketch.cpp
+++ src/Mod/Sketcher/Gui/ViewProviderSketch.cpp
@@ -948,9 +948,6 @@ bool ViewProviderSketch::mouseButtonPressed(int Button, bool pressed, const SbVe
                     doBoxSelection(prvCursorPos, cursorPos, viewer);
                     rubberband->setWorking(false);
 
-                    //disable framebuffer drawing in viewer
-                    const_cast<Gui::View3DInventorViewer *>(viewer)->setRenderType(Gui::View3DInventorViewer::Native);
-
                     // a redraw is required in order to clear the rubberband
                     draw(true,false);
                     const_cast<Gui::View3DInventorViewer*>(viewer)->redraw();
@@ -1372,7 +1369,6 @@ bool ViewProviderSketch::mouseMove(const SbVec2s &cursorPos, Gui::View3DInventor
         case STATUS_SKETCH_StartRubberBand: {
             Mode = STATUS_SKETCH_UseRubberBand;
             rubberband->setWorking(true);
-            viewer->setRenderType(Gui::View3DInventorViewer::Image);
             return true;
         }
         case STATUS_SKETCH_UseRubberBand: {
