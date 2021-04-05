64dbe342e1a4c04bf36cf145a44c9842cf6815ed
wmayer <wmayer@users.sourceforge.net>
2021-04-05 13:25:36 +0200

    PartDesign: for Prism feature use PrismExtension

--- src/Mod/PartDesign/App/FeaturePrimitive.cpp
+++ src/Mod/PartDesign/App/FeaturePrimitive.cpp
@@ -553,11 +553,8 @@ Prism::Prism()
     ADD_PROPERTY_TYPE(Polygon, (6.0), "Prism", App::Prop_None, "Number of sides in the polygon, of the prism");
     ADD_PROPERTY_TYPE(Circumradius, (2.0), "Prism", App::Prop_None, "Circumradius (centre to vertex) of the polygon, of the prism");
     ADD_PROPERTY_TYPE(Height, (10.0f), "Prism", App::Prop_None, "The height of the prism");
-    ADD_PROPERTY_TYPE(FirstAngle, (0.0f), "Prism", App::Prop_None, "Angle in first direction");
-    ADD_PROPERTY_TYPE(SecondAngle, (0.0f), "Prism", App::Prop_None, "Angle in second direction");
-    static const App::PropertyQuantityConstraint::Constraints angleConstraint = { -89.99999, 89.99999, 1.0 };
-    FirstAngle.setConstraints(&angleConstraint);
-    SecondAngle.setConstraints(&angleConstraint);
+
+    Part::PrismExtension::initExtension(this);
 
     primitiveType = FeaturePrimitive::Prism;
 }
@@ -587,11 +584,8 @@ App::DocumentObjectExecReturn* Prism::execute(void)
         mkPoly.Add(gp_Pnt(v.x,v.y,v.z));
         BRepBuilderAPI_MakeFace mkFace(mkPoly.Wire());
         // the direction vector for the prism is the height for z and the given angle
-        BRepPrimAPI_MakePrism mkPrism(mkFace.Face(),
-            gp_Vec(Height.getValue() * tan(Base::toRadians<double>(FirstAngle.getValue())),
-                   Height.getValue() * tan(Base::toRadians<double>(SecondAngle.getValue())),
-                   Height.getValue()));
-        return FeaturePrimitive::execute(mkPrism.Shape());
+        TopoDS_Shape prism = makePrism(Height.getValue(), mkFace.Face());
+        return FeaturePrimitive::execute(prism);
     }
     catch (Standard_Failure& e) {
         return new App::DocumentObjectExecReturn(e.GetMessageString());
@@ -607,10 +601,6 @@ short int Prism::mustExecute() const
     if (Circumradius.isTouched())
         return 1;
     if (Height.isTouched())
-        return 1;
-    if (FirstAngle.isTouched())
-        return 1;
-    if (SecondAngle.isTouched())
         return 1;
 
     return FeaturePrimitive::mustExecute();
