043dd45da0dd27bb7617902b8785e4993bd5ac6b
0penBrain <48731257+0penBrain@users.noreply.github.com>
2021-04-18 19:49:43 +0200

    [Sketcher] Visibility automation : add ability to open sketch in Section View mode

--- src/Mod/Sketcher/Gui/ViewProviderSketch.h
+++ src/Mod/Sketcher/Gui/ViewProviderSketch.h
@@ -114,6 +114,7 @@ public:
     App::PropertyBool ShowLinks;
     App::PropertyBool ShowSupport;
     App::PropertyBool RestoreCamera;
+    App::PropertyBool SectionView;
     App::PropertyString EditingWorkbench;
 
     /// Draw all constraint icons
