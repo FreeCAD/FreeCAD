0b19139afe11950a90133d0914ba0b2233079dce
Pino Toscano <toscano.pino@tiscali.it>
2021-06-08 07:35:02 +0200

    Rename XDG app icon to org.freecadweb.FreeCAD
    The desktop file already specifies org.freecadweb.FreeCAD as icon name;
    hence, rename all the icons installed in the XDG hicolor icon theme as
    org.freecadweb.FreeCAD; this way, XDG icon theme loaders can use the
    PNG icons for small sizes without the need to rescale the SVG icon.
    
    This also adapts various places to the new name.

--- src/Main/FreeCADGuiPy.cpp
+++ src/Main/FreeCADGuiPy.cpp
@@ -368,7 +368,7 @@ PyMOD_INIT_FUNC(FreeCADGui)
 {
     try {
         Base::Interpreter().loadModule("FreeCAD");
-        App::Application::Config()["AppIcon"] = "freecad";
+        App::Application::Config()["AppIcon"] = "org.freecadweb.FreeCAD";
         App::Application::Config()["SplashScreen"] = "freecadsplash";
         App::Application::Config()["CopyrightInfo"] = "\xc2\xa9 Juergen Riegel, Werner Mayer, Yorik van Havre and others 2001-2020\n";
         App::Application::Config()["LicenseInfo"] = "FreeCAD is free and open-source software licensed under the terms of LGPL2+ license.\n";
