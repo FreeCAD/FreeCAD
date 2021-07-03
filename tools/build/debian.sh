cb939c352a3babbc957b9ab94690fddbc2db1bec
Christopher West <cwest@thedigitaledge.co.uk>
2021-07-03 16:19:01 +0100

    Matched debian and ubuntu install script for eaiser comparison

--- tools/build/debian.sh
+++ tools/build/debian.sh
@@ -1,13 +1,13 @@
 #!/bin/sh 
-apt-get install --no-install-recommends --yes build-essential cmake doxygen git \
-    libboost-all-dev libcoin80c libcoin-dev libeigen3-dev libgtkglext1-dev libgts-bin \
-    libgts-dev libkdtree++-dev libkml-dev libmedc-dev libocct-data-exchange-dev \
-    libocct-draw-dev libocct-foundation-dev libocct-modeling-algorithms-dev \
-    libocct-modeling-data-dev libocct-ocaf-dev libocct-visualization-dev libopencv-dev \
-    libproj-dev libpyside2-dev libqt5core5a libqt5svg5-dev libqt5webkit5-dev \
-    libqt5xmlpatterns5-dev libshiboken2-dev libvtk9-dev libvtk9-qt-dev libvtk-dicom-dev \
-    libx11-dev libxerces-c-dev libxmu6 libxmu-dev libxmu-headers libxmuu1 libxmuu-dev \
-    libzipios++-dev netgen netgen-headers oce-draw pybind11-dev pyside2-tools \
-    python3-dev python3-matplotlib python3-pivy python3-pyside2.qtuitools qtchooser \
-    qttools5-dev shiboken2 swig
+apt-get install --no-install-recommends --yes build-essential cmake doxygen \
+    git libboost-all-dev libcoin80c libcoin-dev libeigen3-dev libgtkglext1-dev \
+    libgts-bin libgts-dev libkdtree++-dev libkml-dev libmedc-dev \
+    libocct-data-exchange-dev libocct-draw-dev libocct-foundation-dev \
+    libocct-modeling-algorithms-dev libocct-modeling-data-dev libocct-ocaf-dev \
+    libocct-visualization-dev libopencv-dev libproj-dev libpyside2-dev libqt5core5a \
+    libqt5svg5-dev libqt5webkit5-dev libqt5xmlpatterns5-dev libshiboken2-dev \
+    libvtk9-dev libvtk9-qt-dev libvtk-dicom-dev libx11-dev libxerces-c-dev libxmu6 \
+    libxmu-dev libxmu-headers libxmuu1 libxmuu-dev libzipios++-dev netgen \
+    netgen-headers oce-draw pybind11-dev pyside2-tools python3-dev python3-matplotlib \
+    python3-pivy python3-pyside2.qtuitools qtchooser qttools5-dev shiboken2 swig
 
