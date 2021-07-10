81d04186c719488db5d91db5c9f80d704f9f1d99
Bernd Hahnebach <bernd@bimstatik.org>
2021-07-10 21:35:07 +0200

    FEM: calculix writer, move constraint methods into separate modules

---
+++ src/Mod/Fem/femsolver/calculix/con_displacement.py
@@ -0,0 +1,62 @@
+# ***************************************************************************
+# *   Copyright (c) 2021 Bernd Hahnebach <bernd@bimstatik.org>              *
+# *                                                                         *
+# *   This file is part of the FreeCAD CAx development system.              *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENCE text file.                                 *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Library General Public License for more details.                  *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with this program; if not, write to the Free Software   *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# ***************************************************************************
+
+__title__ = "FreeCAD FEM calculix constraint displacement"
+__author__ = "Bernd Hahnebach"
+__url__ = "https://www.freecadweb.org"
+
+
+def write_node_sets_nodes_constraints_displacement(f, femobj, disp_obj, ccxwriter):
+    f.write("*NSET,NSET={}\n".format(disp_obj.Name))
+    for n in femobj["Nodes"]:
+        f.write("{},\n".format(n))
+
+
+def constraint_displacement_writer(f, femobj, disp_obj, ccxwriter):
+    f.write("*BOUNDARY\n")
+    if disp_obj.xFix:
+        f.write("{},1\n".format(disp_obj.Name))
+    elif not disp_obj.xFree:
+        f.write("{},1,1,{}\n".format(disp_obj.Name, disp_obj.xDisplacement))
+    if disp_obj.yFix:
+        f.write("{},2\n".format(disp_obj.Name))
+    elif not disp_obj.yFree:
+        f.write("{},2,2,{}\n".format(disp_obj.Name, disp_obj.yDisplacement))
+    if disp_obj.zFix:
+        f.write("{},3\n".format(disp_obj.Name))
+    elif not disp_obj.zFree:
+        f.write("{},3,3,{}\n".format(disp_obj.Name, disp_obj.zDisplacement))
+
+    if ccxwriter.beamsection_objects or ccxwriter.shellthickness_objects:
+        if disp_obj.rotxFix:
+            f.write("{},4\n".format(disp_obj.Name))
+        elif not disp_obj.rotxFree:
+            f.write("{},4,4,{}\n".format(disp_obj.Name, disp_obj.xRotation))
+        if disp_obj.rotyFix:
+            f.write("{},5\n".format(disp_obj.Name))
+        elif not disp_obj.rotyFree:
+            f.write("{},5,5,{}\n".format(disp_obj.Name, disp_obj.yRotation))
+        if disp_obj.rotzFix:
+            f.write("{},6\n".format(disp_obj.Name))
+        elif not disp_obj.rotzFree:
+            f.write("{},6,6,{}\n".format(disp_obj.Name, disp_obj.zRotation))
---
+++ src/Mod/Fem/femsolver/calculix/con_pressure.py
@@ -0,0 +1,47 @@
+# ***************************************************************************
+# *   Copyright (c) 2021 Bernd Hahnebach <bernd@bimstatik.org>              *
+# *                                                                         *
+# *   This file is part of the FreeCAD CAx development system.              *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENCE text file.                                 *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Library General Public License for more details.                  *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with this program; if not, write to the Free Software   *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# ***************************************************************************
+
+__title__ = "FreeCAD FEM calculix constraint pressure"
+__author__ = "Bernd Hahnebach"
+__url__ = "https://www.freecadweb.org"
+
+
+def write_faceloads_constraints_pressure(f, femobj, prs_obj, ccxwriter):
+    rev = -1 if prs_obj.Reversed else 1
+    f.write("*DLOAD\n")
+    for ref_shape in femobj["PressureFaces"]:
+        # the loop is needed for compatibility reason
+        # in deprecated method get_pressure_obj_faces_depreciated
+        # the face ids where per ref_shape
+        f.write("** " + ref_shape[0] + "\n")
+        for face, fno in ref_shape[1]:
+            if fno > 0:  # solid mesh face
+                f.write("{},P{},{}\n".format(face, fno, rev * prs_obj.Pressure))
+            # on shell mesh face: fno == 0
+            # normal of element face == face normal
+            elif fno == 0:
+                f.write("{},P,{}\n".format(face, rev * prs_obj.Pressure))
+            # on shell mesh face: fno == -1
+            # normal of element face opposite direction face normal
+            elif fno == -1:
+                f.write("{},P,{}\n".format(face, -1 * rev * prs_obj.Pressure))
