FreeCAD/FreeCAD

commit 820e88f95b5c6d34da09d2152fc3aa3b268e7a95

[Core] (Partially?) Fix data loss on dir rename (#4996)
* Fix lost filename in err msg

In some circumstances, FileExceptions are constructed empty, then have a
filename assigned to them, but the error message in these scenarios is
left as the default "unknown" one, which is sometimes shown to users.
This change fixes that case to be consistent with instances that are
constructed with the filename.

The exception can happen when trying to save the file in a location that does
not exist, or when the user does not have permission to write there. If it
comes when saving after closing the document, all previous changes can be lost.

Partially fixes issue #4098.

Co-authored-by: Heewa Barfchin <heewa.b@gmail.com>

src/App/Document.cpp
@@ -66,6 +66,7 @@ recompute path. Also, it enables more complicated dependencies beyond trees.
 # include <climits>
 # include <bitset>
 # include <random>
+# include <boost/filesystem.hpp>
 #endif
 
 #include <boost/algorithm/string.hpp>
@@ -141,6 +142,8 @@ using namespace zipios;
 #if FC_DEBUG
 #  define FC_LOGFEATUREUPDATE
 #endif
+
+namespace fs = boost::filesystem;
 
 // typedef boost::property<boost::vertex_root_t, DocumentObject* > VertexProperty;
 typedef boost::adjacency_list <
@@ -2610,6 +2613,8 @@ bool Document::saveToFile(const char* filename) const
         fn += uuid;
     }
     Base::FileInfo tmp(fn);
+    // In case some folders in the path do not exist
+    fs::create_directories(fs::path(filename).parent_path());
 
     // open extra scope to close ZipWriter properly
     {

src/Gui/Document.cpp
@@ -1158,6 +1158,25 @@ bool Document::save(void)
                 if(gdoc) gdoc->setModified(false);
             }
         }
+        catch (const Base::FileException& e) {
+            int ret = QMessageBox::question(
+                getMainWindow(),
+                QObject::tr("Could not Save Document"),
+                QObject::tr("There was an issue trying to save the file. "
+                            "This may be because some of the parent folders do not exist, "
+                            "or you do not have sufficient permissions, "
+                            "or for other reasons. Error details:\n\n\"%1\"\n\n"
+                            "Would you like to save the file with a different name?")
+                .arg(QString::fromLatin1(e.what())),
+                QMessageBox::Yes, QMessageBox::No);
+            if (ret == QMessageBox::No) {
+                // TODO: Understand what exactly is supposed to be returned here
+                getMainWindow()->showMessage(QObject::tr("Saving aborted"), 2000);
+                return false;
+            } else if (ret == QMessageBox::Yes) {
+                return saveAs();
+            }
+        }
         catch (const Base::Exception& e) {
             QMessageBox::critical(getMainWindow(), QObject::tr("Saving document failed"),
                 QString::fromLatin1(e.what()));
@@ -1195,6 +1214,25 @@ bool Document::saveAs(void)
             fi.setFile(QString::fromUtf8(d->_pcDocument->FileName.getValue()));
             setModified(false);
             getMainWindow()->appendRecentFile(fi.filePath());
+        }
+        catch (const Base::FileException& e) {
+            int ret = QMessageBox::question(
+                getMainWindow(),
+                QObject::tr("Could not Save Document"),
+                QObject::tr("There was an issue trying to save the file. "
+                            "This may be because some of the parent folders do not exist, "
+                            "or you do not have sufficient permissions, "
+                            "or for other reasons. Error details:\n\n\"%1\"\n\n"
+                            "Would you like to save the file with a different name?")
+                .arg(QString::fromLatin1(e.what())),
+                QMessageBox::Yes, QMessageBox::No);
+            if (ret == QMessageBox::No) {
+                // TODO: Understand what exactly is supposed to be returned here
+                getMainWindow()->showMessage(QObject::tr("Saving aborted"), 2000);
+                return false;
+            } else if (ret == QMessageBox::Yes) {
+                return saveAs();
+            }
         }
         catch (const Base::Exception& e) {
             QMessageBox::critical(getMainWindow(), QObject::tr("Saving document failed"),
@@ -1942,11 +1980,33 @@ bool Document::canClose (bool checkModify, bool checkLink)
 
     bool ok = true;
     if (checkModify && isModified() && !getDocument()->testStatus(App::Document::PartialDoc)) {
-        int res = getMainWindow()->confirmSave(getDocument()->Label.getValue(),getActiveView());
-        if(res>0)
+        const char *docName = getDocument()->Label.getValue();
+        int res = getMainWindow()->confirmSave(docName, getActiveView());
+        switch (res)
+        {
+        case MainWindow::ConfirmSaveResult::Cancel:
+            ok = false;
+            break;
+        case MainWindow::ConfirmSaveResult::SaveAll:
+        case MainWindow::ConfirmSaveResult::Save:
             ok = save();
-        else
-            ok = res<0;
+            if (!ok) {
+                int ret = QMessageBox::question(
+                    getActiveView(),
+                    QObject::tr("Document not saved"),
+                    QObject::tr("The document%1 could not be saved. Do you want to cancel closing it?")
+                    .arg(docName?(QString::fromUtf8(" ")+QString::fromUtf8(docName)):QString()),
+                    QMessageBox::Discard | QMessageBox::Cancel,
+                    QMessageBox::Discard);
+                if (ret == QMessageBox::Discard)
+                    ok = true;
+            }
+            break;
+        case MainWindow::ConfirmSaveResult::DiscardAll:
+        case MainWindow::ConfirmSaveResult::Discard:
+            ok = true;
+            break;
+        }
     }
 
     if (ok) {

src/Gui/MainWindow.cpp
@@ -612,15 +612,15 @@ int MainWindow::confirmSave(const char *docName, QWidget *parent, bool addCheckb
         discardBtn->setShortcut(QKeySequence::mnemonic(text));
     }
 
-    int res = 0;
+    int res = ConfirmSaveResult::Cancel;
     box.adjustSize(); // Silence warnings from Qt on Windows
     switch (box.exec())
     {
     case QMessageBox::Save:
-        res = checkBox.isChecked()?2:1;
+        res = checkBox.isChecked()?ConfirmSaveResult::SaveAll:ConfirmSaveResult::Save;
         break;
     case QMessageBox::Discard:
-        res = checkBox.isChecked()?-2:-1;
+        res = checkBox.isChecked()?ConfirmSaveResult::DiscardAll:ConfirmSaveResult::Discard;
         break;
     }
     if(addCheckbox && res)
@@ -632,12 +632,13 @@ bool MainWindow::closeAllDocuments (bool close)
 {
     auto docs = App::GetApplication().getDocuments();
     try {
-        docs = App::Document::getDependentDocuments(docs,true);
+        docs = App::Document::getDependentDocuments(docs, true);
     }catch(Base::Exception &e) {
         e.ReportException();
     }
     bool checkModify = true;
     bool saveAll = false;
+    int failedSaves = 0;
     for(auto doc : docs) {
         auto gdoc = Application::Instance->getDocument(doc);
         if(!gdoc)
@@ -650,19 +651,36 @@ bool MainWindow::closeAllDocuments (bool close)
             continue;
         bool save = saveAll;
         if(!save && checkModify) {
-            int res = confirmSave(doc->Label.getStrValue().c_str(),this,docs.size()>1);
-            if(res==0)
+            int res = confirmSave(doc->Label.getStrValue().c_str(), this, docs.size()>1);
+            switch (res)
+            {
+            case ConfirmSaveResult::Cancel:
                 return false;
-            if(res>0) {
+            case ConfirmSaveResult::SaveAll:
+                saveAll = true;
+            case ConfirmSaveResult::Save:
                 save = true;
-                if(res==2)
-                    saveAll = true;
-            } else if(res==-2)
+                break;
+            case ConfirmSaveResult::DiscardAll:
                 checkModify = false;
+            }
         }
+
         if(save && !gdoc->save())
+            failedSaves++;
+    }
+
+    if (failedSaves > 0) {
+        int ret = QMessageBox::question(
+            getMainWindow(),
+            QObject::tr("%1 Document(s) not saved").arg(QString::number(failedSaves)),
+            QObject::tr("Some documents could not be saved. Do you want to cancel closing?"),
+            QMessageBox::Discard | QMessageBox::Cancel,
+            QMessageBox::Discard);
+        if (ret == QMessageBox::Cancel)
             return false;
     }
+
     if(close)
         App::GetApplication().closeAllDocuments();
     // d->mdiArea->closeAllSubWindows();

src/Gui/MainWindow.h
@@ -77,6 +77,13 @@ class GuiExport MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
+    enum ConfirmSaveResult {
+        Cancel = 0,
+        Save,
+        SaveAll,
+        Discard,
+        DiscardAll
+    };
     /**
      * Constructs an empty main window. For default \a parent is 0, as there usually is
      * no toplevel window there.

commit d5c42fda592f2d90fe31792c862a95b8149ff42f

[App] Fix regression introduced by PR #4996
Files opened by running `freecad xxxx.FCStd` or similar relative paths could not
be saved because of new code introduced to create parent directories. See
https://github.com/FreeCAD/FreeCAD/pull/4996#issuecomment-922370077 and
https://forum.freecadweb.org/viewtopic.php?f=13&t=62319&p=534156#p534156.

This commit resolves the issue by ensuring the parent path is absolute while
saving. We would probably like to make sure the file name is itself stored with
absolute path, but there's a few problems
1. There may be other occurences where files are loaded with relative paths. So
having a check here is a good idea.
2. Files opened by `freecad xxxx.FCStd` somehow don't have the issue where
renaming parent directory throws an exception on save. Instead the file is saved
in the new location.

src/App/Document.cpp
@@ -2613,9 +2613,8 @@ bool Document::saveToFile(const char* filename) const
     }
     Base::FileInfo tmp(fn);
     // In case some folders in the path do not exist
-    fs::path parent = fs::path(filename).parent_path();
-    if (!parent.empty() && !parent.filename_is_dot() && !parent.filename_is_dot_dot())
-        fs::create_directories(parent);
+    auto parentPath = fs::absolute(fs::path(filename)).parent_path();
+    fs::create_directories(parentPath);
 
     // open extra scope to close ZipWriter properly
     {