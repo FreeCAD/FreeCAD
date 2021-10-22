3924d410730ecb7c5dcccef87cc815b9b714c105
wmayer <wmayer@users.sourceforge.net>
2021-10-22 18:49:49 +0200

    Part: do not open a second transaction when there is already a pending transaction

--- src/Mod/Part/Gui/TaskAttacher.cpp
+++ src/Mod/Part/Gui/TaskAttacher.cpp
@@ -1060,7 +1060,8 @@ TaskDlgAttacher::~TaskDlgAttacher()
 void TaskDlgAttacher::open()
 {
     Gui::Document* document = Gui::Application::Instance->getDocument(ViewProvider->getObject()->getDocument());
-    document->openCommand(QT_TRANSLATE_NOOP("Command", "Edit attachment"));
+    if (!document->hasPendingCommand())
+        document->openCommand(QT_TRANSLATE_NOOP("Command", "Edit attachment"));
 }
 
 void TaskDlgAttacher::clicked(int)
