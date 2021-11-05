4543c982670a0d6044ebed7812ac7df43feb1c4a
luzpaz <luzpaz@users.noreply.github.com>
2021-11-05 19:15:00 -0400

    MeshPart: fix header uniformity + remove superfluous whitespace (#5135)
    * MeshPart: fix header uniformity + remove superfluous whitespace

--- src/Mod/MeshPart/InitGui.py
+++ src/Mod/MeshPart/InitGui.py
@@ -1,4 +1,4 @@
-# MeshPart gui init module  
+# MeshPart gui init module
 # (c) 2003 Juergen Riegel
 #
 # Gathering all the information to start FreeCAD
@@ -6,7 +6,7 @@
 # runs when the gui is up
 
 #***************************************************************************
-#*   (c) Juergen Riegel (juergen.riegel@web.de) 2002                       *
+#*   Copyright (c) 2002 Juergen Riegel <juergen.riegel@web.de>             *
 #*                                                                         *
 #*   This file is part of the FreeCAD CAx development system.              *
 #*                                                                         *
@@ -26,44 +26,46 @@
 #*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
 #*   USA                                                                   *
 #*                                                                         *
-#*   Juergen Riegel 2002                                                   *
 #***************************************************************************/
 
 
 class MeshPartWorkbench ( Workbench ):
-	"MeshPart workbench object"
-	Icon = """
-			/* XPM */
-			static const char *MeshPart_Box[]={
-			"16 16 3 1",
-			". c None",
-			"# c #000000",
-			"a c #c6c642",
-			"................",
-			".......#######..",
-			"......#aaaaa##..",
-			".....#aaaaa###..",
-			"....#aaaaa##a#..",
-			"...#aaaaa##aa#..",
-			"..#aaaaa##aaa#..",
-			".########aaaa#..",
-			".#aaaaa#aaaaa#..",
-			".#aaaaa#aaaa##..",
-			".#aaaaa#aaa##...",
-			".#aaaaa#aa##....",
-			".#aaaaa#a##... .",
-			".#aaaaa###......",
-			".########.......",
-			"................"};
-			"""
-	MenuText = "MeshPart"
-	ToolTip = "MeshPart workbench"
+    "MeshPart workbench object"
+    Icon = """
+            /* XPM */
+            static const char *MeshPart_Box[]={
+            "16 16 3 1",
+            ". c None",
+            "# c #000000",
+            "a c #c6c642",
+            "................",
+            ".......#######..",
+            "......#aaaaa##..",
+            ".....#aaaaa###..",
+            "....#aaaaa##a#..",
+            "...#aaaaa##aa#..",
+            "..#aaaaa##aaa#..",
+            ".########aaaa#..",
+            ".#aaaaa#aaaaa#..",
+            ".#aaaaa#aaaa##..",
+            ".#aaaaa#aaa##...",
+            ".#aaaaa#aa##....",
+            ".#aaaaa#a##... .",
+            ".#aaaaa###......",
+            ".########.......",
+            "................"};
+            """
+    MenuText = "MeshPart"
+    ToolTip = "MeshPart workbench"
 
-	def Initialize(self):
-		# load the module
-		import MeshPartGui
-		import MeshPart
-	def GetClassName(self):
-		return "MeshPartGui::Workbench"
 
-#Gui.addWorkbench(MeshPartWorkbench())
+    def Initialize(self):
+        # load the module
+        import MeshPartGui
+        import MeshPart
+
+
+    def GetClassName(self):
+        return "MeshPartGui::Workbench"
+
+# Gui.addWorkbench(MeshPartWorkbench())
