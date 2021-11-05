f069150ef5c2b31c81095c9cf118775795ae6d17
Chris Hennes <chennes@pioneerlibrarysystem.org>
2021-11-04 20:31:11 -0500

    Addon manager: correct check for Markdown library

--- src/Mod/AddonManager/addonmanager_workers.py
+++ src/Mod/AddonManager/addonmanager_workers.py
@@ -425,7 +425,7 @@ class ShowWorker(QtCore.QThread):
                         p = p.decode("utf-8")
                     u.close()
                     desc = utils.fix_relative_links(p, readmeurl.rsplit("/README.md")[0])
-                    if NOMARKDOWN or not have_markdown:
+                    if have_markdown and not NOMARKDOWN:
                         desc = markdown.markdown(desc, extensions=["md_in_html"])
                     else:
                         message = """
