8e27e8010fe2f1f2ac5abfeb8c6ab73af1219aa6
P-C-R <47773180+P-C-R@users.noreply.github.com>
2021-11-15 08:15:07 +0100

    Bugfix for "Before Tool Change" Commands

--- src/Mod/Path/PathScripts/post/rrf_post.py
+++ src/Mod/Path/PathScripts/post/rrf_post.py
@@ -599,6 +599,8 @@ def parse(pathobj):
                     outlist[-1] =('T' + str(int(c.Parameters['T'])))
 
                 if not OUTPUT_TOOL_CHANGE and OUTPUT_COMMENTS:
+                    for line in TOOL_CHANGE.splitlines(True):
+                        out += linenumber() + line + '\n'
 # next 2 lines could also be replaced by a single line as "outlist = []"                   
                     outlist[0] = ' ' 
                     outlist[-1] = ' '
