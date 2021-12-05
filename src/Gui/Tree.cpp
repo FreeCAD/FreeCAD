12468d81b53ca0a2aa25c414d4d0d5574c95ed01
Zheng, Lei <realthunder.dev@gmail.com>
2021-12-05 22:16:03 +0800

    Gui: do not roll back when tree view drag and drop failed
    To give user a chance to manually fix the problem

--- src/Gui/Tree.cpp
+++ src/Gui/Tree.cpp
@@ -3639,7 +3639,9 @@ void TreeWidget::dropEvent(QDropEvent *event)
             errMsg = "Unknown exception";
         }
         if(errMsg.size()) {
-            committer.close(true);
+            // Do not roll back to give user a chance to manually correct
+            //
+            // committer.close(true);
             QMessageBox::critical(getMainWindow(), QObject::tr("Drag & drop failed"),
                     QString::fromUtf8(errMsg.c_str()));
             return;
@@ -3825,7 +3827,9 @@ void TreeWidget::dropEvent(QDropEvent *event)
             errMsg = "Unknown exception";
         }
         if(errMsg.size()) {
-            committer.close(true);
+            // Do not roll back to give user a chance to manually correct
+            //
+            // committer.close(true);
             QMessageBox::critical(getMainWindow(), QObject::tr("Drag & drop failed"),
                     QString::fromUtf8(errMsg.c_str()));
             return;
