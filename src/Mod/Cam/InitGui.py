09822d5a4d753c0d56e5b23fd2a6cbe8f595aee3
luz paz <luzpaz@users.noreply.github.com>
2021-12-09 16:45:14 -0500

    Cam: Adjust header uniformity

--- src/Mod/Cam/InitGui.py
+++ src/Mod/Cam/InitGui.py
@@ -1,4 +1,4 @@
-# Cam gui init module  
+# Cam gui init module
 # (c) 2003 Juergen Riegel
 #
 # Gathering all the information to start FreeCAD
@@ -6,7 +6,7 @@
 # runs when the gui is up
 
 #***************************************************************************
-#*   (c) Juergen Riegel (juergen.riegel@web.de) 2002                       *
+#*   Copyright (c) 2002 Juergen Riegel <juergen.riegel@web.de>             *
 #*                                                                         *
 #*   This file is part of the FreeCAD CAx development system.              *
 #*                                                                         *
@@ -26,7 +26,6 @@
 #*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
 #*   USA                                                                   *
 #*                                                                         *
-#*   Juergen Riegel 2002                                                   *
 #***************************************************************************/
 
 
