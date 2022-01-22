b2d57363718dece2edc80c0441f95feb1acbfc52
luz paz <luzpaz@users.noreply.github.com>
2022-01-22 08:21:02 -0500

    TD: Uniformity tweaks left over from #5383

--- src/Mod/TechDraw/TechDrawTools/TDToolsUtil.py
+++ src/Mod/TechDraw/TechDrawTools/TDToolsUtil.py
@@ -1,5 +1,5 @@
 # ***************************************************************************
-# *   Copyright (c) 2022 - Wanderer Fan <wandererfan@gmail.com>             *
+# *   Copyright (c) 2022 Wanderer Fan <wandererfan@gmail.com>               *
 # *                                                                         *
 # *   This program is free software; you can redistribute it and/or modify  *
 # *   it under the terms of the GNU Lesser General Public License (LGPL)    *
