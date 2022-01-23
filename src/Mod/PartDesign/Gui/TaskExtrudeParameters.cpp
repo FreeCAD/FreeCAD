3c7ed893061f5af163f5b52601e566941bc3e0fa
Uwe <donovaly@users.noreply.github.com>
2022-01-23 04:45:12 +0100

    [PD] correct copyright year
    both files were created in 2021

--- src/Mod/PartDesign/Gui/TaskExtrudeParameters.cpp
+++ src/Mod/PartDesign/Gui/TaskExtrudeParameters.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (c) 2020 Werner Mayer <wmayer[at]users.sourceforge.net>     *
+ *   Copyright (c) 2021 Werner Mayer <wmayer[at]users.sourceforge.net>     *
  *                                                                         *
  *   This file is part of the FreeCAD CAx development system.              *
  *                                                                         *
