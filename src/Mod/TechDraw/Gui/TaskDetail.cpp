0218ba47f191c5d3279a0783f67208997b3f7f10
Wanderer Fan <wandererfan@gmail.com>
2022-01-31 14:55:40 -0500

    [TD]Prevent segfault on exit with open task dialog

--- src/Mod/TechDraw/Gui/TaskDetail.cpp
+++ src/Mod/TechDraw/Gui/TaskDetail.cpp
@@ -235,7 +235,6 @@ TaskDetail::TaskDetail(TechDraw::DrawViewDetail* detailFeat):
 
 TaskDetail::~TaskDetail()
 {
-    m_ghost->deleteLater();  //this might not exist if scene is destroyed before TaskDetail is deleted?
 }
 
 void TaskDetail::updateTask()
