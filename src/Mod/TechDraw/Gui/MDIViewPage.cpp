7333667bc3f28dac27d4b2f75e2d006fb0b27f62
luz paz <luzpaz@users.noreply.github.com>
2022-02-19 18:25:20 -0500

    TD: Substitute string 'Dxf' for 'DXF' [skip ci]
    This fix was suggested by translators on crowdin.

--- src/Mod/TechDraw/Gui/MDIViewPage.cpp
+++ src/Mod/TechDraw/Gui/MDIViewPage.cpp
@@ -897,9 +897,9 @@ void MDIViewPage::saveDXF()
 {
     QString defaultDir;
     QString fileName = Gui::FileDialog::getSaveFileName(Gui::getMainWindow(),
-                                                   QString::fromUtf8(QT_TR_NOOP("Save Dxf File")),
+                                                   QString::fromUtf8(QT_TR_NOOP("Save DXF file")),
                                                    defaultDir,
-                                                   QString::fromUtf8(QT_TR_NOOP("Dxf (*.dxf)")));
+                                                   QString::fromUtf8(QT_TR_NOOP("DXF (*.dxf)")));
     if (fileName.isEmpty()) {
         return;
     }
