a23ee678fba3e5bbadf7ff0375b4558dd8d75109
Larry Woestman <LarryWoestman@users.noreply.github.com>
2022-02-24 20:20:35 -0800

    Added a much more complex algorithm for generating gcode adapted from PathPost.py

--- src/Mod/Path/PathTests/TestPathPost.py
+++ src/Mod/Path/PathTests/TestPathPost.py
@@ -22,7 +22,6 @@
 # ***************************************************************************
 
 import difflib
-import importlib
 import os
 import unittest
 
@@ -36,6 +35,8 @@ import PathScripts.PathToolController
 import PathScripts.PathUtil
 import PathScripts.post
 import PathScripts.PostUtils as PostUtils
+
+from PathScripts.PathPostProcessor import PostProcessor
 
 # If KEEP_DEBUG_OUTPUT is False, remove the gcode file after the test.
 # If KEEP_DEBUG_OUTPUT is True, then leave the gcode file behind in the
@@ -59,7 +60,159 @@ class TestPathPost(unittest.TestCase):
         """Tear down after the postprocessor tests."""
         pass
 
-    def _run_a_test(self, freecad_document, postprocesser_file, postprocessor_args, gcode_file, reference_file):
+    class _TempObject:
+        # pylint: disable=no-init
+        Path = None
+        Name = "Fixture"
+        InList = []
+        Label = "Fixture"
+
+    def _generate_gcode(self, job, postprocessor_name, postprocessor_args, gcode_file):
+        """Generate the gcode for a job"""
+
+        wcslist = job.Fixtures
+        orderby = job.OrderOutputBy
+        split = job.SplitOutput
+
+        postlist = []
+
+        if orderby == "Fixture":
+            # Order by fixture means all operations and tool changes will be completed in one
+            # fixture before moving to the next.
+
+            currTool = None
+            for index, f in enumerate(wcslist):
+                # create an object to serve as the fixture path
+                fobj = self._TempObject()
+                c1 = Path.Command(f)
+                fobj.Path = Path.Path([c1])
+                if index != 0:
+                    c2 = Path.Command(
+                        "G0 Z"
+                        + str(
+                            job.Stock.Shape.BoundBox.ZMax
+                            + job.SetupSheet.ClearanceHeightOffset.Value
+                        )
+                    )
+                    fobj.Path.addCommands(c2)
+                fobj.InList.append(job)
+                sublist = [fobj]
+
+                # Now generate the gcode
+                for obj in job.Operations.Group:
+                    tc = PathScripts.PathUtil.toolControllerForOp(obj)
+                    if tc is not None and PathScripts.PathUtil.opProperty(obj, "Active"):
+                        if tc.ToolNumber != currTool or split is True:
+                            sublist.append(tc)
+                            currTool = tc.ToolNumber
+                    sublist.append(obj)
+                postlist.append(sublist)
+
+        elif orderby == "Tool":
+            # Order by tool means tool changes are minimized.
+            # all operations with the current tool are processed in the current
+            # fixture before moving to the next fixture.
+
+            currTool = None
+            fixturelist = []
+            for f in wcslist:
+                # create an object to serve as the fixture path
+                fobj = self._TempObject()
+                c1 = Path.Command(f)
+                c2 = Path.Command(
+                    "G0 Z"
+                    + str(
+                        job.Stock.Shape.BoundBox.ZMax
+                        + job.SetupSheet.ClearanceHeightOffset.Value
+                    )
+                )
+                fobj.Path = Path.Path([c1, c2])
+                fobj.InList.append(job)
+                fixturelist.append(fobj)
+
+            # Now generate the gcode
+            curlist = []  # list of ops for tool, will repeat for each fixture
+            sublist = []  # list of ops for output splitting
+
+            for idx, obj in enumerate(job.Operations.Group):
+
+                # check if the operation is active
+                active = PathScripts.PathUtil.opProperty(obj, "Active")
+
+                tc = PathScripts.PathUtil.toolControllerForOp(obj)
+                if tc is None or tc.ToolNumber == currTool and active:
+                    curlist.append(obj)
+                elif (
+                    tc.ToolNumber != currTool and currTool is None and active
+                ):  # first TC
+                    sublist.append(tc)
+                    curlist.append(obj)
+                    currTool = tc.ToolNumber
+                elif (
+                    tc.ToolNumber != currTool and currTool is not None and active
+                ):  # TC
+                    for fixture in fixturelist:
+                        sublist.append(fixture)
+                        sublist.extend(curlist)
+                    postlist.append(sublist)
+                    sublist = [tc]
+                    curlist = [obj]
+                    currTool = tc.ToolNumber
+
+                if idx == len(job.Operations.Group) - 1:  # Last operation.
+                    for fixture in fixturelist:
+                        sublist.append(fixture)
+                        sublist.extend(curlist)
+                    postlist.append(sublist)
+
+        elif orderby == "Operation":
+            # Order by operation means ops are done in each fixture in
+            # sequence.
+            currTool = None
+            firstFixture = True
+
+            # Now generate the gcode
+            for obj in job.Operations.Group:
+                if PathScripts.PathUtil.opProperty(obj, "Active"):
+                    sublist = []
+                    for f in wcslist:
+                        fobj = self._TempObject()
+                        c1 = Path.Command(f)
+                        fobj.Path = Path.Path([c1])
+                        if not firstFixture:
+                            c2 = Path.Command(
+                                "G0 Z"
+                                + str(
+                                    job.Stock.Shape.BoundBox.ZMax
+                                    + job.SetupSheet.ClearanceHeightOffset.Value
+                                )
+                            )
+                            fobj.Path.addCommands(c2)
+                        fobj.InList.append(job)
+                        sublist.append(fobj)
+                        firstFixture = False
+                        tc = PathScripts.PathUtil.toolControllerForOp(obj)
+                        if tc is not None:
+                            if job.SplitOutput or (tc.ToolNumber != currTool):
+                                sublist.append(tc)
+                                currTool = tc.ToolNumber
+                        sublist.append(obj)
+                    postlist.append(sublist)
+
+        if split:
+            all_gcode = []
+            for slist in postlist:
+                processor = PostProcessor.load(postprocessor_name)
+                gcode = processor.export(slist, gcode_file, postprocessor_args)
+                all_gcode.append(gcode)
+            return(all_gcode)
+        else:
+            finalpostlist = [item for slist in postlist for item in slist]
+            processor = PostProcessor.load(postprocessor_name)
+            gcode = processor.export(finalpostlist, gcode_file, postprocessor_args)
+            return (gcode)
+
+    def _run_a_test(self, freecad_document, postprocessor_file, postprocessor_args, gcode_file, reference_file):
         """
         Run one test based on the arguments.
 
@@ -86,21 +239,11 @@ class TestPathPost(unittest.TestCase):
         )
         self.doc = FreeCAD.open(freecad_document_path)
         self.job = FreeCAD.ActiveDocument.getObject("Job")
-        self.postlist = []
-        currTool = None
-        for obj in self.job.Operations.Group:
-            if not isinstance(obj.Proxy, PathScripts.PathToolController.ToolController):
-                tc = PathScripts.PathUtil.toolControllerForOp(obj)
-                if tc is not None:
-                    if tc.ToolNumber != currTool:
-                        self.postlist.append(tc)
-                self.postlist.append(obj)
-
-        full_postprocessor_file_name = "PathScripts.post." + postprocesser_file
-        postprocessor = importlib.import_module(name=full_postprocessor_file_name)
-        gcode = postprocessor.export(self.postlist, gcode_file, postprocessor_args)
+        gcode = self._generate_gcode(self.job,
+                                     postprocessor_file,
+                                     postprocessor_args,
+                                     gcode_file)
         FreeCAD.closeDocument(freecad_document)
-
         if not KEEP_DEBUG_OUTPUT:
             os.remove(gcode_file)
 
@@ -123,7 +266,7 @@ class TestPathPost(unittest.TestCase):
             "--no-header --no-show-editor"
         )
         self._run_a_test(freecad_document="boxtest1",
-                         postprocesser_file="linuxcnc_post",
+                         postprocessor_file="linuxcnc",
                          postprocessor_args=postprocessor_args,
                          gcode_file="test_linuxcnc.ngc",
                          reference_file="test_linuxcnc_01")
@@ -131,10 +274,10 @@ class TestPathPost(unittest.TestCase):
     def test_linuxcnc_imperial(self):
         """Test the linuxcnc postprocessor in Imperial mode."""
         postprocessor_args = (
-            "--no-header --no-comments --no-show-editor --precision=2 --inches"
+            "--no-header --no-show-editor --inches"
         )
         self._run_a_test(freecad_document="boxtest1",
-                         postprocesser_file="linuxcnc_post",
+                         postprocessor_file="linuxcnc",
                          postprocessor_args=postprocessor_args,
                          gcode_file="test_linuxcnc_imperial.ngc",
                          reference_file="test_linuxcnc_10")
