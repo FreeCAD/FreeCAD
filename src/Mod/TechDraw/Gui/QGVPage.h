FreeCAD/FreeCAD

commit eb4a5384c0cab90207df470598683b999a551663

[TD]add alignment grid to page

src/Mod/TechDraw/Gui/QGVPage.h
@@ -23,11 +23,17 @@
 #ifndef TECHDRAWGUI_QGVIEW_H
 #define TECHDRAWGUI_QGVIEW_H
 
+#include <Mod/TechDraw/TechDrawGlobal.h>
+
 #include <QGraphicsView>
 #include <QGraphicsScene>
 #include <QLabel>
 
 class QTemporaryFile;
+
+namespace App {
+class DocumentObject;
+}
 
 namespace TechDraw {
 class DrawView;
@@ -122,6 +128,9 @@ public:
     void saveSvg(QString filename);
     void postProcessXml(QTemporaryFile& tempFile, QString filename, QString pagename);
 
+    void makeGrid(int width, int height, int step);
+    void showGrid(bool state) {m_showGrid = state;}
+    void updateViewport(void) {viewport()->repaint();}
 
 public Q_SLOTS:
     void setHighQualityAntialiasing(bool highQualityAntialiasing);
@@ -150,6 +159,7 @@ protected:
 
     void activateCursor(QCursor cursor);
     void resetCursor();
+    virtual void drawForeground(QPainter *painter, const QRectF &rect) override;
 
 private:
     RendererType m_renderer;
@@ -172,6 +182,9 @@ private:
 
     QPoint panOrigin;
     bool panningActive;
+
+    bool m_showGrid;
+    QPainterPath m_gridPath;
 };
 
 } // namespace 

src/Mod/TechDraw/App/EdgeWalker.h
@@ -28,6 +28,8 @@
 #ifndef TECHDRAW_EDGEWALKER_H
 #define TECHDRAW_EDGEWALKER_H
 
+#include <Mod/TechDraw/TechDrawGlobal.h>
+
 #include <vector>
 #include <boost_graph_adjacency_list.hpp>
 #include <boost/graph/properties.hpp>
@@ -72,7 +74,7 @@ typedef
                                 >
         planar_embedding_t;
 
-class WalkerEdge
+class TechDrawExport WalkerEdge
 {
 public:
     static bool weCompare(WalkerEdge i, WalkerEdge j);
@@ -85,7 +87,7 @@ public:
     int idx;
 };
 
-class ewWire
+class TechDrawExport ewWire
 {
 public:
     bool isEqual(ewWire w);
@@ -96,7 +98,7 @@ public:
     int size(void);
 };
 
-class ewWireList
+class TechDrawExport ewWireList
 {
 public:
     ewWireList removeDuplicateWires();
@@ -108,7 +110,7 @@ public:
 
 
 
-class edgeVisitor : public boost::planar_face_traversal_visitor
+class TechDrawExport edgeVisitor : public boost::planar_face_traversal_visitor
 {
 public:
     template <typename Edge>
@@ -124,7 +126,7 @@ private:
     TechDraw::graph m_g;
 };
 
-class incidenceItem
+class TechDrawExport incidenceItem
 {
 public:
     incidenceItem() {iEdge = 0; angle = 0.0;}
@@ -137,7 +139,7 @@ public:
     edge_t eDesc;
 };
 
-class embedItem
+class TechDrawExport embedItem
 {
 public:
     embedItem();
@@ -152,7 +154,7 @@ public:
 };
 
 
-class EdgeWalker
+class TechDrawExport EdgeWalker
 {
 public:
     EdgeWalker(void);

src/Mod/TechDraw/Gui/DlgPrefsTechDrawColorsImp.cpp
@@ -61,6 +61,7 @@ void DlgPrefsTechDrawColorsImp::saveSettings()
     ui->pcbVertexColor->onSave();
     ui->pcbMarkup->onSave();
     ui->pcbHighlight->onSave();
+    ui->pcb_Grid->onSave();
 }
 
 void DlgPrefsTechDrawColorsImp::loadSettings()
@@ -81,6 +82,7 @@ void DlgPrefsTechDrawColorsImp::loadSettings()
     ui->pcbVertexColor->onRestore();
     ui->pcbMarkup->onRestore();
     ui->pcbHighlight->onRestore();
+    ui->pcb_Grid->onRestore();
 }
 
 /**

src/Mod/TechDraw/Gui/DlgPrefsTechDrawGeneral.ui
@@ -6,8 +6,8 @@
    <rect>
     <x>0</x>
     <y>0</y>
-    <width>440</width>
-    <height>581</height>
+    <width>499</width>
+    <height>776</height>
    </rect>
   </property>
   <property name="sizePolicy">
@@ -275,9 +275,7 @@ for ProjectionGroups</string>
            <string>Font for labels</string>
           </property>
           <property name="currentFont">
-           <font>
-            <pointsize>10</pointsize>
-           </font>
+           <font/>
           </property>
           <property name="prefEntry" stdset="0">
            <cstring>LabelFont</cstring>
@@ -789,6 +787,76 @@ for ProjectionGroups</string>
     </widget>
    </item>
    <item>
+    <widget class="QGroupBox" name="gbGrid">
+     <property name="title">
+      <string>Grid</string>
+     </property>
+     <layout class="QVBoxLayout" name="verticalLayout_5">
+      <item>
+       <layout class="QGridLayout" name="gridLayout" columnstretch="1,0,1">
+        <item row="1" column="2">
+         <widget class="Gui::PrefSpinBox" name="psb_GridSpacing">
+          <property name="alignment">
+           <set>Qt::AlignRight|Qt::AlignTrailing|Qt::AlignVCenter</set>
+          </property>
+          <property name="value">
+           <number>10</number>
+          </property>
+          <property name="prefEntry" stdset="0">
+           <cstring>gridSpacing</cstring>
+          </property>
+          <property name="prefPath" stdset="0">
+           <cstring>/Mod/TechDraw/General</cstring>
+          </property>
+         </widget>
+        </item>
+        <item row="0" column="0">
+         <widget class="Gui::PrefCheckBox" name="cb_ShowGrid">
+          <property name="text">
+           <string>Show Grid</string>
+          </property>
+          <property name="checked">
+           <bool>true</bool>
+          </property>
+          <property name="prefEntry" stdset="0">
+           <cstring>showGrid</cstring>
+          </property>
+          <property name="prefPath" stdset="0">
+           <cstring>/Mod/TechDraw/General</cstring>
+          </property>
+         </widget>
+        </item>
+        <item row="1" column="0">
+         <widget class="QLabel" name="label_4">
+          <property name="font">
+           <font>
+            <italic>true</italic>
+           </font>
+          </property>
+          <property name="text">
+           <string>Grid Spacing</string>
+          </property>
+         </widget>
+        </item>
+        <item row="1" column="1">
+         <spacer name="horizontalSpacer">
+          <property name="orientation">
+           <enum>Qt::Horizontal</enum>
+          </property>
+          <property name="sizeHint" stdset="0">
+           <size>
+            <width>40</width>
+            <height>20</height>
+           </size>
+          </property>
+         </spacer>
+        </item>
+       </layout>
+      </item>
+     </layout>
+    </widget>
+   </item>
+   <item>
     <widget class="QLabel" name="label_12">
      <property name="font">
       <font>
@@ -797,25 +865,12 @@ for ProjectionGroups</string>
       </font>
      </property>
      <property name="text">
-      <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=" font-weight:600;"&gt;Note:&lt;/span&gt; Items in &lt;span style=" font-style:italic;"&gt;italics&lt;/span&gt; are default values for new objects. They have no effect on existing objects.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+      <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Note:&lt;/span&gt; Items in &lt;span style=&quot; font-style:italic;&quot;&gt;italics&lt;/span&gt; are default values for new objects. They have no effect on existing objects.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
      </property>
      <property name="wordWrap">
       <bool>true</bool>
      </property>
     </widget>
-   </item>
-   <item>
-    <spacer name="verticalSpacer">
-     <property name="orientation">
-      <enum>Qt::Vertical</enum>
-     </property>
-     <property name="sizeHint" stdset="0">
-      <size>
-       <width>20</width>
-       <height>19</height>
-      </size>
-     </property>
-    </spacer>
    </item>
   </layout>
  </widget>
@@ -833,6 +888,11 @@ for ProjectionGroups</string>
   <customwidget>
    <class>Gui::PrefFileChooser</class>
    <extends>Gui::FileChooser</extends>
+   <header>Gui/PrefWidgets.h</header>
+  </customwidget>
+  <customwidget>
+   <class>Gui::PrefSpinBox</class>
+   <extends>QSpinBox</extends>
    <header>Gui/PrefWidgets.h</header>
   </customwidget>
   <customwidget>

src/Mod/TechDraw/Gui/DlgPrefsTechDrawGeneralImp.cpp
@@ -71,6 +71,8 @@ void DlgPrefsTechDrawGeneralImp::saveSettings()
     ui->pfc_Welding->onSave();
     ui->pfc_FilePattern->onSave();
     ui->le_NamePattern->onSave();
+    ui->cb_ShowGrid->onSave();
+    ui->psb_GridSpacing->onSave();
 }
 
 void DlgPrefsTechDrawGeneralImp::loadSettings()
@@ -106,6 +108,8 @@ void DlgPrefsTechDrawGeneralImp::loadSettings()
     ui->pfc_Welding->onRestore();
     ui->pfc_FilePattern->onRestore();
     ui->le_NamePattern->onRestore();
+    ui->cb_ShowGrid->onRestore();
+    ui->psb_GridSpacing->onRestore();
 }
 
 /**

src/Mod/TechDraw/Gui/PreferencesGui.cpp
@@ -211,3 +211,36 @@ QString PreferencesGui::weldingDirectory()
     return qSymbolDir;
 }
 
+
+App::Color PreferencesGui::gridColor()
+{
+    Base::Reference<ParameterGrp> hGrp = App::GetApplication().GetUserParameter().
+                                         GetGroup("BaseApp")->GetGroup("Preferences")->
+                                         GetGroup("Mod/TechDraw/Colors");
+    App::Color result;
+    result.setPackedValue(hGrp->GetUnsigned("gridColor", 0x000000FF));  //#000000 black
+    return result;
+}
+
+QColor PreferencesGui::gridQColor()
+{
+    return PreferencesGui::gridColor().asValue<QColor>();
+}
+
+int PreferencesGui::gridSpacing()
+{
+    Base::Reference<ParameterGrp> hGrp = App::GetApplication().GetUserParameter().
+                                         GetGroup("BaseApp")->GetGroup("Preferences")->
+                                         GetGroup("Mod/TechDraw/General");
+    int spacing = hGrp->GetInt("gridSpacing", 10);
+    return spacing;
+}
+
+bool PreferencesGui::showGrid()
+{
+    Base::Reference<ParameterGrp> hGrp = App::GetApplication().GetUserParameter().
+                                         GetGroup("BaseApp")->GetGroup("Preferences")->
+                                         GetGroup("Mod/TechDraw/General");
+    bool show = hGrp->GetInt("showGrid", true);
+    return show;
+}

src/Mod/TechDraw/Gui/PreferencesGui.h
@@ -63,6 +63,11 @@ static int         mattingStyle();
 
 static QString     weldingDirectory();
 
+static bool showGrid();
+static App::Color gridColor();
+static QColor gridQColor();
+static int gridSpacing();
+
 };
 
 } //end namespace TechDrawGui

commit 4184218284f80130a5e4b29be7d2d9c1abe17829

[TD] fix compilation error introduced with https://github.com/FreeCAD/FreeCAD/pull/6465

src/Mod/TechDraw/Gui/QGVPage.h
@@ -25,9 +25,10 @@
 
 #include <Mod/TechDraw/TechDrawGlobal.h>
 
-#include <QGraphicsView>
 #include <QGraphicsScene>
+#include <QGraphicsView>
 #include <QLabel>
+#include <QPainterPath>
 
 class QTemporaryFile;
 