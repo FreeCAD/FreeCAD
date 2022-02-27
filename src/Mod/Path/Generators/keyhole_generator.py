814c87bcb91c91b4a58782592e7959ca0923e46d
Way of Wood <info@wayofwood.com>
2022-02-27 16:13:39 +0100

    Generator and Test for Keyhole Path

---
+++ src/Mod/Path/Generators/keyhole_generator.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+# ***************************************************************************
+# *   Copyright (c) 2021 wayofwood <code@wayofwood.com>               *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENCE text file.                                 *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Library General Public License for more details.                  *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with this program; if not, write to the Free Software   *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# ***************************************************************************
+
+import PathScripts.PathLog as PathLog
+import Path
+
+__title__ = "Keyhole Path Generator"
+__author__ = "wayofwood"
+__url__ = "https://www.freecadweb.org"
+__doc__ = "Generates the toolpath for keyhole "
+
+if True:
+    PathLog.setLevel(PathLog.Level.DEBUG, PathLog.thisModule())
+    PathLog.trackModule(PathLog.thisModule())
+else:
+    PathLog.setLevel(PathLog.Level.INFO, PathLog.thisModule())
+
+
+def generate(face, startPoint,  endPoint):
+    # Build up command stack
+    cmdStack = []
+
+    cmdStack.append( Path.Command("G1", {"X": startPoint.x,
+                                         "Y": startPoint.y,
+                                         "Z": startPoint.z }) )
+
+    cmdStack.append( Path.Command("G1", {"X": endPoint.x,
+                                         "Y": endPoint.y,
+                                         "Z": endPoint.z }) )
+
+    cmdStack.append( Path.Command("G1", {"X": startPoint.x,
+                                         "Y": startPoint.y,
+                                         "Z": startPoint.z }) )
+
+    return cmdStack
---
+++ src/Mod/Path/PathTests/TestPathKeyHoleGenerator.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+# ***************************************************************************
+# *   Copyright (c) 2021 wayofwood <code@wayofwood.com>               *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENCE text file.                                 *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Library General Public License for more details.                  *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with this program; if not, write to the Free Software   *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# ***************************************************************************
+
+import Path
+import FreeCAD
+import Generators.keyhole_generator as generator
+import PathScripts.PathLog as PathLog
+import PathTests.PathTestUtils as PathTestUtils
+
+PathLog.setLevel(PathLog.Level.INFO, PathLog.thisModule())
+PathLog.trackModule(PathLog.thisModule())
+
+class TestPathKeyHoleGenerator(PathTestUtils.PathTestBase):
+    def test00(self):
+        """Test KeyHole in X-direction """
+        # Circle in the center of the coordinate system
+        sp = FreeCAD.Base.Vector(0, 0, 0)
+        ep = FreeCAD.Base.Vector(10, 0, 0)
+
+        result = generator.generate(sp, ep)
+
+        self.assertTrue(type(result) is list)
+        self.assertTrue(type(result[0]) is Path.Command)
+
+        command = result[0]
+        self.assertTrue(command.Name == "G1")
+        self.assertTrue(command.Parameters["X"] < 1e-06)
+        self.assertTrue(command.Parameters["Y"] < 1e-06)
+        self.assertTrue(command.Parameters["Z"] == 0)
+
+        command = result[1]
+        self.assertTrue(command.Name == "G1")
+        print (command.Parameters["X"])
+        self.assertTrue(command.Parameters["X"] - 10 < 1e-06)
+        self.assertTrue(command.Parameters["X"] - 10 > -1e-06)
+        self.assertTrue(command.Parameters["Y"] < 1e-06)
+        self.assertTrue(command.Parameters["Z"] == 0)
+
+        command = result[2]
+        self.assertTrue(command.Name == "G1")
+        self.assertTrue(command.Parameters["X"] < 1e-06)
+        self.assertTrue(command.Parameters["Y"] < 1e-06)
+        self.assertTrue(command.Parameters["Z"] == 0)
+
+    def test10(self):
+        """Test KeyHole in Y-direction """
+        sp = FreeCAD.Base.Vector(0, 0, 0)
+        ep = FreeCAD.Base.Vector(0, 10, 0)
+
+        result = generator.generate(sp, ep)
+
+        self.assertTrue(type(result) is list)
+        self.assertTrue(type(result[0]) is Path.Command)
+
+        command = result[0]
+        self.assertTrue(command.Name == "G1")
+        self.assertTrue(command.Parameters["X"] < 1e-06)
+        self.assertTrue(command.Parameters["Y"] < 1e-06)
+        self.assertTrue(command.Parameters["Z"] == 0)
+
+        command = result[1]
+        self.assertTrue(command.Name == "G1")
+        self.assertTrue(command.Parameters["X"] < 1e-06)
+        self.assertTrue(command.Parameters["Y"] - 10 < 1e-06)
+        self.assertTrue(command.Parameters["Y"] - 10 > -1e-06)
+        self.assertTrue(command.Parameters["Z"] == 0)
+
+        command = result[2]
+        self.assertTrue(command.Name == "G1")
+        self.assertTrue(command.Parameters["X"] < 1e-06)
+        self.assertTrue(command.Parameters["Y"] < 1e-06)
+        self.assertTrue(command.Parameters["Z"] == 0)
