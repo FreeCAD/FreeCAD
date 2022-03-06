64ddcc68fcc9d8133dc1f1046f5b041cfbe37d02
Uwe <donovaly@users.noreply.github.com>
2022-03-06 14:51:58 +0100

    [Base] ConsoleObserver: correct copyright

--- src/Base/ConsoleObserver.cpp
+++ src/Base/ConsoleObserver.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (c) 2002 Jürgen Riegel <juergen.riegel@web.de>              *
+ *   Copyright (c) 2022 Werner Mayer <wmayer[at]users.sourceforge.net>     *
  *                                                                         *
  *   This file is part of the FreeCAD CAx development system.              *
  *                                                                         *
