2c86fe2ab1881806da9d89f875d20ff9cb9bc0bc
Uwe <donovaly@users.noreply.github.com>
2022-03-07 13:57:01 +0100

    [App] AutoTransaction: a change to silence MSVC compiler
    as proposed here: https://github.com/FreeCAD/FreeCAD/pull/6519#discussion_r820641762

--- src/App/AutoTransaction.h
+++ src/App/AutoTransaction.h
@@ -33,7 +33,7 @@ class Application;
 class AppExport AutoTransaction {
 private:
     /// Private new operator to prevent heap allocation
-    void* operator new(std::size_t size);
+    void* operator new (std::size_t) = delete;
 
 public:
     /** Constructor
@@ -122,7 +122,7 @@ public:
 
 private:
     /// Private new operator to prevent heap allocation
-    void* operator new(std::size_t size);
+    void* operator new (std::size_t) = delete;
 
 private:
     bool active;
