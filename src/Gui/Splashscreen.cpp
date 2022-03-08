7a68a950e564b137dcf4abc35cbbe098313df65d
wmayer <wmayer@users.sourceforge.net>
2022-03-08 12:54:46 +0100

    Gui: [skip ci] remove text

--- src/Gui/Splashscreen.cpp
+++ src/Gui/Splashscreen.cpp
@@ -9,7 +9,7 @@
  *   version 2 of the License, or (at your option) any later version.      *
  *                                                                         *
  *   This library  is distributed in the hope that it will be useful,      *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *ench
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
  *   GNU Library General Public License for more details.                  *
  *                                                                         *
