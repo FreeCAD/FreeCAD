d5739b8fa107c769406c281efd18edd009c0a782
Suzanne Soy <jsmaniac.github@suzanne.soy>
2022-03-12 19:30:31 +0000

    apply fix from https://github.com/SuzanneSoy/SearchBar/issues/10

--- src/Mod/SearchBar/InitGui.py
+++ src/Mod/SearchBar/InitGui.py
@@ -1,27 +1,34 @@
 # Avoid garbage collection by storing the action in a global variable
 wax = None
+sea = None
+tbr = None
 
 def addToolSearchBox():
   import FreeCADGui
   from PySide import QtGui
   import SearchBoxLight
-  global wax, sea
+  global wax, sea, tbr
   mw = FreeCADGui.getMainWindow()
   if mw:
-    sea = SearchBoxLight.SearchBoxLight(getItemGroups   = lambda: __import__('GetItemGroups').getItemGroups(),
-                                        getToolTip      = lambda groupId, setParent: __import__('GetItemGroups').getToolTip(groupId, setParent),
-                                        getItemDelegate = lambda: __import__('IndentedItemDelegate').IndentedItemDelegate())
-    sea.resultSelected.connect(lambda index, groupId: __import__('GetItemGroups').onResultSelected(index, groupId))
-    wax = QtGui.QWidgetAction(None)
-    wax.setWhatsThis('Use this search bar to find tools, document objects, preferences and more')
+    if sea is None:
+        sea = SearchBoxLight.SearchBoxLight(getItemGroups   = lambda: __import__('GetItemGroups').getItemGroups(),
+                                            getToolTip      = lambda groupId, setParent: __import__('GetItemGroups').getToolTip(groupId, setParent),
+                                            getItemDelegate = lambda: __import__('IndentedItemDelegate').IndentedItemDelegate())
+        sea.resultSelected.connect(lambda index, groupId: __import__('GetItemGroups').onResultSelected(index, groupId))
+
+    if wax is None:
+        wax = QtGui.QWidgetAction(None)
+        wax.setWhatsThis('Use this search bar to find tools, document objects, preferences and more')
+
     sea.setWhatsThis('Use this search bar to find tools, document objects, preferences and more')
     wax.setDefaultWidget(sea)
     ##mbr.addWidget(sea)
     #mbr.addAction(wax)
-    tbr = QtGui.QToolBar("SearchBar") #QtGui.QDockWidget()
-    # Include FreeCAD in the name so that one can find windows labeled with FreeCAD easily in window managers which allow search through the list of open windows.
-    tbr.setObjectName("SearchBar")
-    tbr.addAction(wax)
+    if tbr is None:
+        tbr = QtGui.QToolBar("SearchBar") #QtGui.QDockWidget()
+        # Include FreeCAD in the name so that one can find windows labeled with FreeCAD easily in window managers which allow search through the list of open windows.
+        tbr.setObjectName("SearchBar")
+        tbr.addAction(wax)
     mw.addToolBar(tbr)
     tbr.show()
 
