32d6a689e7cab93e2770eceb122be5063c719d47
Uwe <donovaly@users.noreply.github.com>
2022-03-20 17:15:22 +0100

    [TD] remove Python.h and Xerces
    seems not to be necessary anymore

--- src/Mod/TechDraw/App/AppTechDraw.cpp
+++ src/Mod/TechDraw/App/AppTechDraw.cpp
@@ -18,11 +18,7 @@
  *                                                                         *
  ***************************************************************************/
 
-
 #include "PreCompiled.h"
-#ifndef _PreComp_
-# include <Python.h>
-#endif
 
 #include <Base/Console.h>
 #include <Base/PyObjectBase.h>
