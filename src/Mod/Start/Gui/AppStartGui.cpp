51b94abfde8a2260870b61387f04423d100b11b7
wmayer <wmayer@users.sourceforge.net>
2022-03-23 19:14:15 +0100

    Start: modernize C++11 * use nullptr

--- src/Mod/Start/Gui/AppStartGui.cpp
+++ src/Mod/Start/Gui/AppStartGui.cpp
@@ -70,7 +70,7 @@ PyMOD_INIT_FUNC(StartGui)
 {
     if (!Gui::Application::Instance) {
         PyErr_SetString(PyExc_ImportError, "Cannot load Gui module in console application.");
-        PyMOD_Return(0);
+        PyMOD_Return(nullptr);
     }
 
     // load dependent module
@@ -79,7 +79,7 @@ PyMOD_INIT_FUNC(StartGui)
     }
     catch(const Base::Exception& e) {
         PyErr_SetString(PyExc_ImportError, e.what());
-        PyMOD_Return(0);
+        PyMOD_Return(nullptr);
     }
     catch (Py::Exception& e) {
         Py::Object o = Py::type(e);
