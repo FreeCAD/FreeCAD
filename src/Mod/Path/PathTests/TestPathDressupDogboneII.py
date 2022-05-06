ac55cab540f638f2cb645aad9fc393315dc0c00e
Markus Lampert <markus@bibi.ca>
2022-05-05 21:02:55 -0700

    Added Instruction and tangents support for G2/3 moves

--- src/Mod/Path/PathTests/TestPathDressupDogboneII.py
+++ src/Mod/Path/PathTests/TestPathDressupDogboneII.py
@@ -31,11 +31,18 @@ CmdMoveStraight = PathGeom.CmdMoveStraight + PathGeom.CmdMoveRapid
 
 PI = math.pi
 
-class Instruction (object):
-    '''An Instruction is pure python replacement of Path.Command which also tracks its start position.'''
+def normalizeAngle(a):
+    while a > PI:
+        a = a - 2 * PI
+    while a < -PI:
+        a = a + 2 * PI
+    return a
 
-    def __init__(self, start, cmd, param=None):
-        self.start = start
+class Instruction (object):
+    '''An Instruction is pure python replacement of Path.Command which also tracks its begin position.'''
+
+    def __init__(self, begin, cmd, param=None):
+        self.begin = begin
         if type(cmd) == Path.Command:
             self.cmd = Path.Name
             self.param = Path.Parameters
@@ -46,13 +53,13 @@ class Instruction (object):
             else:
                 self.param = param
 
-    def positionStart(self):
-        '''positionStart() ... returns a Vector of the start position'''
-        return self.start
+    def positionBegin(self):
+        '''positionBegin() ... returns a Vector of the begin position'''
+        return self.begin
 
     def positionEnd(self):
         '''positionEnd() ... returns a Vector of the end position'''
-        return FreeCAD.Vector(self.x(start.x), self.y(start.y), self.z(start.z))
+        return FreeCAD.Vector(self.x(begin.x), self.y(begin.y), self.z(begin.z))
 
     def x(self, default=0):
         return self.param.get('X', default)
@@ -63,18 +70,63 @@ class Instruction (object):
     def z(self, default=0):
         return self.param.get('Z', default)
 
+    def i(self, default=0):
+        return self.param.get('I', default)
+
+    def j(self, default=0):
+        return self.param.get('J', default)
+
+    def k(self, default=0):
+        return self.param.get('K', default)
+
+    def xyBegin(self):
+        '''xyBegin() ... internal convenience function'''
+        return FreeCAD.Vector(self.begin.x, self.begin.y, 0)
+    def xyEnd(self):
+        '''xyEnd() ... internal convenience function'''
+        return FreeCAD.Vector(self.x(self.begin.x), self.y(self.begin.y), 0)
+
 class MoveStraight (Instruction):
 
     def anglesOfTangents(self):
-        '''angleOfTangents() ... return a tuple with the tangent angle at start and end position'''
-        start = FreeCAD.Vector(self.start.x, self.start.y, 0)
-        end  = FreeCAD.Vector(self.x(self.start.x), self.y(self.start.y), 0)
-        if end == start:
+        '''angleOfTangents() ... return a tuple with the tangent angles at begin and end position'''
+        begin = self.xyBegin()
+        end = self.xyEnd()
+        if end == begin:
             return (0, 0)
-        a = PathGeom.getAngle(end - start)
+        a = PathGeom.getAngle(end - begin)
         return (a, a)
 
+class MoveArc (Instruction):
+
+    def anglesOfTangents(self):
+        '''angleOfTangents() ... return a tuple with the tangent angles at begin and end position'''
+        begin = self.xyBegin()
+        end = self.xyEnd()
+        center = FreeCAD.Vector(self.begin.x + self.i(), self.begin.y + self.j(), 0)
+        # calculate angle of the hypotenuse at begin and end
+        s0 = PathGeom.getAngle(begin - center)
+        s1 = PathGeom.getAngle(end - center)
+        # the tangents are perpendicular to the hypotenuse with the sign determined by the
+        # direction of the arc
+        return (normalizeAngle(s0 + self.arcDirection()), normalizeAngle(s1 + self.arcDirection()))
+
+class MoveArcCW (MoveArc):
+    def arcDirection(self):
+        return -PI/2
+
+class MoveArcCCW (MoveArc):
+    def arcDirection(self):
+        return PI/2
+
 def INSTR(s, c, x, y, z=None, i=None, j=None, k=None):
+    if len(s) == 1:
+        s = FreeCAD.Vector(s[0], 0, 0)
+    elif len(s) == 2:
+        s = FreeCAD.Vector(s[0], s[1], 0)
+    else:
+        s = FreeCAD.Vector(s[0], s[1], s[2])
+
     def upd(d, l, v):
         if not v is None:
             d[l] = v
@@ -86,19 +138,25 @@ def INSTR(s, c, x, y, z=None, i=None, j=None, k=None):
     upd(param, 'K', k)
     if c in CmdMoveStraight:
         return MoveStraight(s, c, param)
+    if c in PathGeom.CmdMoveCW:
+        return MoveArcCW(s, c, param)
+    if c in PathGeom.CmdMoveCCW:
+        return MoveArcCCW(s, c, param)
     return Instruction(s, c, param)
+
+def G1(s, x, y):
+    return INSTR(s, 'G1', x, y)
+
+def G2(s, x, y, i, j):
+    return INSTR(s, 'G2', x, y, None, i, j)
+def G3(s, x, y, i, j):
+    return INSTR(s, 'G3', x, y, None, i, j)
 
 def POS(x, y, z=None):
     return FreeCAD.Vector(x, y, 0 if z is None else z)
 
-def TAN(p, c, x, y):
-    if len(p) == 1:
-        p = FreeCAD.Vector(p[0], 0, 0)
-    elif len(p) == 2:
-        p = FreeCAD.Vector(p[0], p[1], 0)
-    else:
-        p = FreeCAD.Vector(p[0], p[1], p[2])
-    return INSTR(p, c, x, y).anglesOfTangents()
+def TAN(i):
+    return i.anglesOfTangents()
 
 class TestDressupDogboneII(PathTestBase):
     """Unit tests for the Dogbone dressup."""
@@ -111,13 +169,22 @@ class TestDressupDogboneII(PathTestBase):
     def test00(self):
         """Get tangents of moves."""
 
-        self.assertTangents(TAN((0, 0), 'G0',  0,  0), (0, 0)) # by declaration
-        self.assertTangents(TAN((0, 0), 'G0',  1,  0), (0, 0))
-        self.assertTangents(TAN((0, 0), 'G0', -1,  0), (PI, PI))
-        self.assertTangents(TAN((0, 0), 'G0',  0,  1), (PI/2,  PI/2))
-        self.assertTangents(TAN((0, 0), 'G0',  0, -1), (-PI/2,  -PI/2))
-        self.assertTangents(TAN((0, 0), 'G0',  1,  1), (PI/4,  PI/4))
-        self.assertTangents(TAN((0, 0), 'G0', -1,  1), (3*PI/4,  3*PI/4))
-        self.assertTangents(TAN((0, 0), 'G0', -1, -1), (-3*PI/4,  -3*PI/4))
-        self.assertTangents(TAN((0, 0), 'G0',  1, -1), (-PI/4,  -PI/4))
+        self.assertTangents(TAN(G1((0, 0),  0,  0)), (0, 0)) # by declaration
+        self.assertTangents(TAN(G1((0, 0),  1,  0)), (0, 0))
+        self.assertTangents(TAN(G1((0, 0), -1,  0)), (PI, PI))
+        self.assertTangents(TAN(G1((0, 0),  0,  1)), (PI/2,  PI/2))
+        self.assertTangents(TAN(G1((0, 0),  0, -1)), (-PI/2,  -PI/2))
+        self.assertTangents(TAN(G1((0, 0),  1,  1)), (PI/4,  PI/4))
+        self.assertTangents(TAN(G1((0, 0), -1,  1)), (3*PI/4,  3*PI/4))
+        self.assertTangents(TAN(G1((0, 0), -1, -1)), (-3*PI/4,  -3*PI/4))
+        self.assertTangents(TAN(G1((0, 0),  1, -1)), (-PI/4,  -PI/4))
+
+        self.assertTangents(TAN(G2((0, 0),  2,  0,  1,  0)), (PI/2, -PI/2))
+        self.assertTangents(TAN(G2((0, 0),  2,  2,  1,  1)), (3*PI/4, -PI/4))
+        self.assertTangents(TAN(G2((0, 0),  0, -2,  0, -1)), (0, -PI))
+
+        self.assertTangents(TAN(G3((0, 0),  2,  0,  1,  0)), (-PI/2, PI/2))
+        self.assertTangents(TAN(G3((0, 0),  2,  2,  1,  1)), (-PI/4, 3*PI/4))
+        self.assertTangents(TAN(G3((0, 0),  0, -2,  0, -1)), (PI, 0))
+
 
