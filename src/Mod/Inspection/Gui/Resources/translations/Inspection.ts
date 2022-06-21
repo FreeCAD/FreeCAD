166306469a29af88ed34fdc8723250739dd1ae9a
Kuzemko Alexsandr <alexandrkuzemko@gmail.com>
2022-06-21 17:35:36 +0300

    Add missing file.

---
+++ src/Mod/Inspection/Gui/Resources/translations/Inspection.ts
@@ -0,0 +1,133 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>CmdInspectElement</name>
+    <message>
+        <location filename="../../Command.cpp" line="75"/>
+        <source>Inspection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Command.cpp" line="76"/>
+        <source>Inspection...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Command.cpp" line="77"/>
+        <source>Get distance information</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CmdVisualInspection</name>
+    <message>
+        <location filename="../../Command.cpp" line="49"/>
+        <source>Inspection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Command.cpp" line="50"/>
+        <source>Visual inspection...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Command.cpp" line="51"/>
+        <location filename="../../Command.cpp" line="52"/>
+        <source>Visual inspection</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Command</name>
+    <message>
+        <location filename="../../VisualInspection.cpp" line="230"/>
+        <source>Visual Inspection</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>InspectionGui::VisualInspection</name>
+    <message>
+        <location filename="../../VisualInspection.ui" line="14"/>
+        <source>Visual Inspection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../VisualInspection.ui" line="23"/>
+        <source>Actual</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../VisualInspection.ui" line="39"/>
+        <location filename="../../VisualInspection.ui" line="66"/>
+        <source>Objects</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../VisualInspection.ui" line="50"/>
+        <source>Nominal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../VisualInspection.ui" line="77"/>
+        <source>Parameter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../VisualInspection.ui" line="97"/>
+        <source>Search distance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../VisualInspection.ui" line="129"/>
+        <location filename="../../VisualInspection.ui" line="174"/>
+        <source> mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../VisualInspection.ui" line="145"/>
+        <source>Thickness</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../ViewProviderInspection.cpp" line="405"/>
+        <source>Remove annotations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../ViewProviderInspection.cpp" line="406"/>
+        <source>Do you want to remove all annotations?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../ViewProviderInspection.cpp" line="458"/>
+        <source>Annotation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../ViewProviderInspection.cpp" line="461"/>
+        <source>Leave info mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../ViewProviderInspection.cpp" line="582"/>
+        <source>Distance: &gt; %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../ViewProviderInspection.cpp" line="585"/>
+        <source>Distance: &lt; %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../ViewProviderInspection.cpp" line="604"/>
+        <location filename="../../ViewProviderInspection.cpp" line="620"/>
+        <source>Distance: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
