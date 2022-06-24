98781cc4345bedc4bd7d6958920e7af6bd483d46
Kuzemko Alexsandr <alexandrkuzemko@gmail.com>
2022-06-24 07:51:30 +0300

    Crowdin Add to updatets.py App.ts

---
+++ src/App/Resources/translations/App.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>LinkParams</name>
+    <message>
+        <location filename="../../Link.cpp" line="118"/>
+        <source>Stores the last user choice of whether to apply CopyOnChange setup to all link
+that links to the same configurable object</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../Application.cpp" line="439"/>
+        <source>Unnamed</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
