3cbb9f0c11205af622f375b6883e7ae00442dd7b
Uwe <donovaly@users.noreply.github.com>
2022-07-03 22:51:16 +0200

    [Part] OCCError.h: remove unneeded includes
    - also sort includes

--- src/Mod/Part/App/OffsetSurfacePyImp.cpp
+++ src/Mod/Part/App/OffsetSurfacePyImp.cpp
@@ -20,20 +20,16 @@
  *                                                                         *
  ***************************************************************************/
 
-
 #include "PreCompiled.h"
 #ifndef _PreComp_
 # include <Geom_OffsetSurface.hxx>
 # include <memory>
 #endif
 
-#include <Base/VectorPy.h>
-#include <Base/Vector3D.h>
-
 #include "OCCError.h"
-#include "Geometry.h"
-#include <Mod/Part/App/OffsetSurfacePy.h>
-#include <Mod/Part/App/OffsetSurfacePy.cpp>
+#include "OffsetSurfacePy.h"
+#include "OffsetSurfacePy.cpp"
+
 
 using namespace Part;
 
