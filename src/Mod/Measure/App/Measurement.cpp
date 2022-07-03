49f707e885941d9dc4948e6e3bd12be6999e0ffe
Uwe <donovaly@users.noreply.github.com>
2022-07-03 21:38:52 +0200

    [Measure] Measurement.cpp: add missing include

--- src/Mod/Measure/App/Measurement.cpp
+++ src/Mod/Measure/App/Measurement.cpp
@@ -27,6 +27,7 @@
 # include <BRepExtrema_DistShapeShape.hxx>
 # include <BRepGProp.hxx>
 # include <GCPnts_AbscissaPoint.hxx>
+# include <gp_Circ.hxx>
 # include <gp_Lin.hxx>
 # include <GProp_GProps.hxx>
 # include <TopoDS.hxx>
