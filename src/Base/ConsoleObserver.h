FreeCAD/FreeCAD

commit ea0c3e8598db6f6c4e64b9fdd270c64a28ee8e5b

[Base] fix a -Wunused CI compiler warning

src/Base/ConsoleObserver.h
@@ -21,14 +21,12 @@
  *                                                                         *
  ***************************************************************************/
 
-
-
-
 #ifndef BASE_CONSOLEOBSERVER_H
 #define BASE_CONSOLEOBSERVER_H
 
 #include <Base/Console.h>
 #include <Base/Stream.h>
+
 
 namespace Base {
 
@@ -101,8 +99,8 @@ conObs(co)
 
 ILoggerBlocker::~ILoggerBlocker()
 {
-    auto debug = Console().SetEnabledMsgType(conObs, msgTypesBlocked, true);
 #ifdef FC_DEBUG
+    auto debug = Console().SetEnabledMsgType(conObs, msgTypesBlocked, true);
     if (debug != msgTypesBlocked)
         Console().Warning("Enabled message types have been changed while ILoggerBlocker was set\n");
 #endif

src/Base/ConsoleObserver.cpp
@@ -25,7 +25,7 @@
 
 #ifndef _PreComp_
 # ifdef FC_OS_WIN32
-#   include <windows.h>
+#  include <windows.h>
 # endif
 # include <cstring>
 # include <Python.h>

commit 761ff7bab46e7d5c07c10b60daf40ab4aa7a332b

Base: [skip ci] fix regression in ~ILoggerBlocker caused by ea0c3e8598db6f

src/Base/ConsoleObserver.h
@@ -91,8 +91,7 @@ private:
     const char* conObs; //  Stores console observer name that blocker acts on
 };
 
-ILoggerBlocker::ILoggerBlocker(const char* co, ConsoleMsgFlags msgTypes):
-conObs(co)
+ILoggerBlocker::ILoggerBlocker(const char* co, ConsoleMsgFlags msgTypes) : conObs(co)
 {
     msgTypesBlocked = Console().SetEnabledMsgType(conObs, msgTypes, false);
 }
@@ -103,6 +102,8 @@ ILoggerBlocker::~ILoggerBlocker()
     auto debug = Console().SetEnabledMsgType(conObs, msgTypesBlocked, true);
     if (debug != msgTypesBlocked)
         Console().Warning("Enabled message types have been changed while ILoggerBlocker was set\n");
+#else
+    Console().SetEnabledMsgType(conObs, msgTypesBlocked, true);
 #endif
 }
 