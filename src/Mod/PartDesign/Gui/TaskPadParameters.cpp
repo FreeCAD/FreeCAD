094ae9367837901fa94a251ed684f2f3de141be0
Uwe <donovaly@users.noreply.github.com>
2022-07-17 02:03:35 +0200

    [PD] fix Pad/Pocket logic issue
    - fixes the bug reported in https://forum.freecadweb.org/viewtopic.php?f=3&t=70266
      as Method #2
      -> When switching to two dimensions, symmetric must be unchecked if it is checked

--- src/Mod/PartDesign/Gui/TaskPadParameters.cpp
+++ src/Mod/PartDesign/Gui/TaskPadParameters.cpp
@@ -107,6 +107,9 @@ void TaskPadParameters::onModeChanged(int index)
         break;
     case Modes::TwoDimensions:
         pcPad->Type.setValue("TwoLengths");
+        // symmetric is then not possible
+        if (ui->checkBoxMidplane->isChecked())
+            ui->checkBoxMidplane->setChecked(false);
         break;
     }
 
