7a198ca32e26efe60e54830dc79338ca6910f93e
wmayer <wmayer@users.sourceforge.net>
2022-07-27 09:56:50 +0200

    CMake: use system version of FindCoin3D.cmake

--- cMake/FreeCAD_Helpers/SetupCoin3D.cmake
+++ cMake/FreeCAD_Helpers/SetupCoin3D.cmake
@@ -8,4 +8,8 @@ macro(SetupCoin3D)
                             "=================\n")
     endif(NOT COIN3D_FOUND)
 
+    IF(NOT PIVY_VERSION)
+      execute_process (COMMAND ${Python3_EXECUTABLE} -c "import pivy as p; print(p.__version__,end='')" OUTPUT_VARIABLE PIVY_VERSION)
+    ENDIF()
+
 endmacro(SetupCoin3D)
