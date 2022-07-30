790bf5363e05c3de35d3c1350fd887b94f27a656
Uwe <donovaly@users.noreply.github.com>
2022-07-30 17:34:36 +0200

    [App] sort properties
    - to quickly get an overview

--- src/App/PropertyUnits.h
+++ src/App/PropertyUnits.h
@@ -119,52 +119,17 @@ protected:
     const Constraints* _ConstStruct;
 };
 
-/** Distance property
- * This is a property for representing distances. It is basically a float
- * property. On the Gui it has a quantity like m or mm.
+/** Acceleration property
+ * This is a property for representing acceleration. It is basically a float
+ * property. On the Gui it has a quantity like m/s^2.
  */
-class AppExport PropertyDistance: public PropertyQuantity
+class AppExport PropertyAcceleration: public PropertyQuantity
 {
     TYPESYSTEM_HEADER();
-public:
-    PropertyDistance(void);
-    virtual ~PropertyDistance(){}
-};
 
-/** Length property
- * This is a property for representing lengths. It is basically a float
- * property which must not be negative. On the Gui it has a quantity like m or mm.
- */
-class AppExport PropertyLength : public PropertyQuantityConstraint
-{
-    TYPESYSTEM_HEADER();
 public:
-    PropertyLength(void);
-    virtual ~PropertyLength(){}
-};
-
-/** Area property
- * This is a property for representing areas. It is basically a float
- * property which must not be negative. On the Gui it has a quantity like m^2 or mm^2.
- */
-class AppExport PropertyArea : public PropertyQuantityConstraint
-{
-    TYPESYSTEM_HEADER();
-public:
-    PropertyArea(void);
-    virtual ~PropertyArea(){}
-};
-
-/** Volume property
- * This is a property for representing volumes. It is basically a float
- * property which must not be negative. On the Gui it has a quantity like m^3 or mm^3.
- */
-class AppExport PropertyVolume : public PropertyQuantityConstraint
-{
-    TYPESYSTEM_HEADER();
-public:
-    PropertyVolume(void);
-    virtual ~PropertyVolume(){}
+    PropertyAcceleration(void);
+    virtual ~PropertyAcceleration() {}
 };
 
 /** Angle property
@@ -174,10 +139,50 @@ public:
 class AppExport PropertyAngle: public PropertyQuantityConstraint
 {
     TYPESYSTEM_HEADER();
+
 public:
     PropertyAngle(void);
-    virtual ~PropertyAngle(){}
-    virtual const char* getEditorName(void) const { return "Gui::PropertyEditor::PropertyAngleItem"; }
+    virtual ~PropertyAngle() {}
+    virtual const char *getEditorName(void) const { return "Gui::PropertyEditor::PropertyAngleItem"; }
+};
+
+/** Area property
+ * This is a property for representing areas. It is basically a float
+ * property which must not be negative. On the Gui it has a quantity like m^2 or mm^2.
+ */
+class AppExport PropertyArea: public PropertyQuantityConstraint
+{
+    TYPESYSTEM_HEADER();
+
+public:
+    PropertyArea(void);
+    virtual ~PropertyArea() {}
+};
+
+/** Distance property
+ * This is a property for representing distances. It is basically a float
+ * property. On the Gui it has a quantity like m or mm.
+ */
+class AppExport PropertyDistance: public PropertyQuantity
+{
+    TYPESYSTEM_HEADER();
+
+public:
+    PropertyDistance(void);
+    virtual ~PropertyDistance() {}
+};
+
+/** ElectricPotential property
+ * This is a property for electric potentials. It is basically a float
+ * property. On the Gui it has a quantity of Volt.
+ */
+class AppExport PropertyElectricPotential: public PropertyQuantity
+{
+    TYPESYSTEM_HEADER();
+
+public:
+    PropertyElectricPotential(void);
+    virtual ~PropertyElectricPotential() {}
 };
 
 /** Frequency property
@@ -187,57 +192,10 @@ public:
 class AppExport PropertyFrequency: public PropertyQuantity
 {
     TYPESYSTEM_HEADER();
+
 public:
     PropertyFrequency(void);
-    virtual ~PropertyFrequency(){}
-};
-
-/** Speed property
- * This is a property for representing speed. It is basically a float
- * property. On the Gui it has a quantity like m/s or km/h.
- */
-class AppExport PropertySpeed: public PropertyQuantity
-{
-    TYPESYSTEM_HEADER();
-public:
-    PropertySpeed(void);
-    virtual ~PropertySpeed(){}
-};
-
-/** Acceleration property
- * This is a property for representing acceleration. It is basically a float
- * property. On the Gui it has a quantity like m/s^2.
- */
-class AppExport PropertyAcceleration: public PropertyQuantity
-{
-    TYPESYSTEM_HEADER();
-public:
-    PropertyAcceleration(void);
-    virtual ~PropertyAcceleration(){}
-};
-
-/** Pressure property
- * This is a property for representing acceleration. It is basically a float
- * property. On the Gui it has a quantity like m/s^2.
- */
-class AppExport PropertyPressure: public PropertyQuantity
-{
-    TYPESYSTEM_HEADER();
-public:
-    PropertyPressure(void);
-    virtual ~PropertyPressure(){}
-};
-
-/** Stiffness property
- * This is a property for representing stiffness. It is basically a float
- * property. On the Gui it has a quantity like m/s^2.
- */
-class AppExport PropertyStiffness: public PropertyQuantity
-{
-    TYPESYSTEM_HEADER();
-public:
-    PropertyStiffness(void);
-    virtual ~PropertyStiffness(){}
+    virtual ~PropertyFrequency() {}
 };
 
 /** Force property
@@ -247,21 +205,62 @@ public:
 class AppExport PropertyForce: public PropertyQuantity
 {
     TYPESYSTEM_HEADER();
+
 public:
     PropertyForce(void);
-    virtual ~PropertyForce(){}
+    virtual ~PropertyForce() {}
 };
 
-/** ElectricPotential property
- * This is a property for electric potentials. It is basically a float
- * property. On the Gui it has a quantity of Volt.
+/** Length property
+ * This is a property for representing lengths. It is basically a float
+ * property which must not be negative. On the Gui it has a quantity like m or mm.
  */
-class AppExport PropertyElectricPotential : public PropertyQuantity
+class AppExport PropertyLength: public PropertyQuantityConstraint
 {
     TYPESYSTEM_HEADER();
+
 public:
-    PropertyElectricPotential(void);
-    virtual ~PropertyElectricPotential() {}
+    PropertyLength(void);
+    virtual ~PropertyLength() {}
+};
+
+/** Pressure property
+ * This is a property for representing acceleration. It is basically a float
+ * property. On the Gui it has a quantity like m/s^2.
+ */
+class AppExport PropertyPressure: public PropertyQuantity
+{
+    TYPESYSTEM_HEADER();
+
+public:
+    PropertyPressure(void);
+    virtual ~PropertyPressure() {}
+};
+
+/** Speed property
+ * This is a property for representing speed. It is basically a float
+ * property. On the Gui it has a quantity like m/s or km/h.
+ */
+class AppExport PropertySpeed: public PropertyQuantity
+{
+    TYPESYSTEM_HEADER();
+
+public:
+    PropertySpeed(void);
+    virtual ~PropertySpeed() {}
+};
+
+/** Stiffness property
+ * This is a property for representing stiffness. It is basically a float
+ * property. On the Gui it has a quantity like m/s^2.
+ */
+class AppExport PropertyStiffness: public PropertyQuantity
+{
+    TYPESYSTEM_HEADER();
+
+public:
+    PropertyStiffness(void);
+    virtual ~PropertyStiffness() {}
 };
 
 /** VacuumPermittivity property
@@ -271,11 +270,25 @@ public:
 class AppExport PropertyVacuumPermittivity: public PropertyQuantity
 {
     TYPESYSTEM_HEADER();
+
 public:
     PropertyVacuumPermittivity(void);
-    virtual ~PropertyVacuumPermittivity(){}
+    virtual ~PropertyVacuumPermittivity() {}
 };
 
-} // namespace App
+/** Volume property
+ * This is a property for representing volumes. It is basically a float
+ * property which must not be negative. On the Gui it has a quantity like m^3 or mm^3.
+ */
+class AppExport PropertyVolume: public PropertyQuantityConstraint
+{
+    TYPESYSTEM_HEADER();
 
-#endif // APP_PROPERTYUNITS_H
+public:
+    PropertyVolume(void);
+    virtual ~PropertyVolume() {}
+};
+
+}// namespace App
+
+#endif// APP_PROPERTYUNITS_H
