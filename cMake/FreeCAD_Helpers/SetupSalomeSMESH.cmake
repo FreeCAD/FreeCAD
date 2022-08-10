659ad4fe6663858167030ea799323f8eb2a2e2ec
andrea <realeandrea@yahoo.it>
2022-08-10 11:23:29 +0200

    remove duplicate assignments

--- cMake/FreeCAD_Helpers/SetupSalomeSMESH.cmake
+++ cMake/FreeCAD_Helpers/SetupSalomeSMESH.cmake
@@ -133,13 +133,7 @@ macro(SetupSalomeSMESH)
         endif()
 
         set(SMESH_FOUND TRUE)
-        #ifndef SMESH_VERSION_H
-			#define SMESH_VERSION_H
-			#define SMESH_VERSION_MAJOR ${SMESH_VERSION_MAJOR}
-			#define SMESH_VERSION_MINOR ${SMESH_VERSION_MINOR}
-			#define SMESH_VERSION_PATCH ${SMESH_VERSION_PATCH}
-			#define SMESH_VERSION_TWEAK ${SMESH_VERSION_TWEAK}
-		#endif // SMESH_VERSION_H
+
 
     endif(BUILD_SMESH)
 
