1ce78efb2997133462067ef07b08668172e441bb
wmayer <wmayer@users.sourceforge.net>
2022-08-24 11:55:39 +0200

    Import: [skip ci] whitespace improvements

--- src/Mod/Import/stepZ.py
+++ src/Mod/Import/stepZ.py
@@ -13,14 +13,16 @@
 
 import six
 
-import FreeCAD,FreeCADGui
+import FreeCAD
+import FreeCADGui
 import shutil
-import os, re
+import os
+import re
 import ImportGui
 import PySide
-from PySide import QtGui, QtCore
+from PySide import QtCore
+from PySide import QtGui
 import tempfile
-import shutil
 
 ___stpZversion___ = "1.3.9"
 # support both gz and zipfile archives
@@ -51,7 +53,7 @@ def mkz_unicode(input):
     else:
         input = input.decode('utf-8')
         return input
- 
+
 ####
 def sayz(msg):
     FreeCAD.Console.PrintMessage(msg)
@@ -73,7 +75,7 @@ def import_stpz(fn,fc,doc):
     fname=os.path.splitext(os.path.basename(fn))[0]
     basepath=os.path.split(fn)[0]
     filepath = os.path.join(basepath,fname + u'.stp')
-    
+
     tempdir = tempfile.gettempdir() # get the current temporary directory
     tempfilepath = os.path.join(tempdir,fname + u'.stp')
 
@@ -95,7 +97,7 @@ def import_stpz(fn,fc,doc):
 def open(filename,doc=None):
 
     sayz("stpZ version "+___stpZversion___)
-        
+
     if zf.is_zipfile(filename):
         with zf.ZipFile(filename, 'r') as fz:
             file_names = fz.namelist()
