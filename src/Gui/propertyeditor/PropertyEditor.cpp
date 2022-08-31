fe085edd79f48150122f1771c1fa694d64bddf70
0penBrain <48731257+0penBrain@users.noreply.github.com>
2022-08-31 11:43:12 +0200

    [Bugfix]Gui: allow set up expression even if property value is currently being edited

--- src/Gui/propertyeditor/PropertyEditor.cpp
+++ src/Gui/propertyeditor/PropertyEditor.cpp
@@ -747,6 +747,7 @@ void PropertyEditor::contextMenuEvent(QContextMenuEvent *) {
     case MA_Expression:
         if(contextIndex == currentIndex()) {
             Base::FlagToggler<> flag(binding);
+            closeEditor();
             openEditor(contextIndex);
         }
         break;
