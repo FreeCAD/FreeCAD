375741c75ed2fa0ea75785046746cf1e70d368a6
Andrea <realeandrea@yahoo.it>
2022-09-05 17:16:15 +0200

    fix new folder

--- cMake/FreeCAD_Helpers/SetLibraryVersions.cmake
+++ cMake/FreeCAD_Helpers/SetLibraryVersions.cmake
@@ -5,6 +5,6 @@ macro(SetLibraryVersions)
         set(HAVE_OCC_VERSION 1)
     endif(OCC_INCLUDE_DIR AND EXISTS ${OCC_INCLUDE_DIR}/Standard_Version.hxx)
 
-    configure_file(${CMAKE_SOURCE_DIR}/cMake/header/LibraryVersions.h.cmake ${CMAKE_CURRENT_BINARY_DIR}/LibraryVersions.h)
+    configure_file(${CMAKE_SOURCE_DIR}/cMake/headers/LibraryVersions.h.cmake ${CMAKE_CURRENT_BINARY_DIR}/LibraryVersions.h)
 
 endmacro(SetLibraryVersions)
