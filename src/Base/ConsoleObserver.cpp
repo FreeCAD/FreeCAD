62dd15c0340dfdb7b9d8f9c40e24c1f2bcb141ba
Zheng, Lei <realthunder.dev@gmail.com>
2022-09-22 21:25:08 +0800

    Base: fix crash on logging Python call frame

--- src/Base/ConsoleObserver.cpp
+++ src/Base/ConsoleObserver.cpp
@@ -34,6 +34,7 @@
 #include <frameobject.h>
 
 #include "ConsoleObserver.h"
+#include "Interpreter.h"
 
 
 using namespace Base;
@@ -263,6 +264,7 @@ std::stringstream &LogLevel::prefix(std::stringstream &str, const char *src, int
     }
     if (print_tag) str << '<' << tag << "> ";
     if (print_src==2) {
+        PyGILStateLocker lock;
         PyFrameObject* frame = PyEval_GetFrame();
         if (frame) {
             line = PyFrame_GetLineNumber(frame);
