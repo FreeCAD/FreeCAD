68ac5c1c8c0517ff9ef8b97ab6032f9c611cce14
wmayer <wmayer@users.sourceforge.net>
2022-11-05 13:33:18 +0100

    Gui: [skip ci] fix possible crash in FilterStatusBar::eventFilter()

--- src/Gui/CommandWindow.cpp
+++ src/Gui/CommandWindow.cpp
@@ -374,9 +374,9 @@ public:
 //    virtual ~FilterStatusBar() {}
 protected:
     Action * action;
-    bool eventFilter(QObject *obj, QEvent *event)
     {
-        if (getMainWindow()->findChild<QStatusBar *>() != nullptr && obj == getMainWindow()->statusBar() && ((event->type() == QEvent::Hide) || (event->type() == QEvent::Show))) {
+        if (getMainWindow() && getMainWindow()->findChild<QStatusBar *>() && obj == getMainWindow()->statusBar() &&
+            ((event->type() == QEvent::Hide) || (event->type() == QEvent::Show))) {
             this->action->setChecked(getMainWindow()->statusBar()->isVisible());
         }
         return false;
