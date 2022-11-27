f3f916a010704505ad230ef5e2e2aef588625cda
berniev <bernievbb@icloud.com>
2022-11-28 07:19:15 +1000

    fix invalid unicode

--- src/Gui/TreeItemMode.h
+++ src/Gui/TreeItemMode.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (c) 2004 Jürgen Riegel <juergen.riegel@web.de>              *
+ *   Copyright (c) 2004 JÃ¼rgen Riegel <juergen.riegel@web.de>              *
  *                                                                         *
  *   This file is part of the FreeCAD CAx development system.              *
  *                                                                         *
