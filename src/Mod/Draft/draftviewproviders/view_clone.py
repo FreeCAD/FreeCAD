cc8d6afee74a5513a2357d6a3153f760e31728b1
Roy-043 <info@b-k-g.nl>
2022-11-27 11:11:24 +0100

    Draft: fix clone DiffuseColor

--- src/Mod/Draft/draftviewproviders/view_clone.py
+++ src/Mod/Draft/draftviewproviders/view_clone.py
@@ -56,22 +56,28 @@ class ViewProviderClone:
         colors = []
         for o in groups.get_group_contents(vobj.Object.Objects):
             if o.isDerivedFrom("Part::Feature"):
-                if len(o.ViewObject.DiffuseColor) > 1:
+                if len(o.ViewObject.DiffuseColor) == len(o.Shape.Faces):
                     colors.extend(o.ViewObject.DiffuseColor)
                 else:
                     c = o.ViewObject.ShapeColor
-                    c = (c[0],c[1],c[2],o.ViewObject.Transparency/100.0)
-                    colors += [c] * len(o.Shape.Faces) # TODO: verify this line
+                    c = (c[0], c[1], c[2], o.ViewObject.Transparency / 100.0)
+                    colors += [c] * len(o.Shape.Faces)
             elif o.hasExtension("App::GeoFeatureGroupExtension"):
-                for so in vobj.Object.Group:
+                for so in o.Group:
                     if so.isDerivedFrom("Part::Feature"):
-                        if len(so.ViewObject.DiffuseColor) > 1:
+                        if len(so.ViewObject.DiffuseColor) == len(so.Shape.Faces):
                             colors.extend(so.ViewObject.DiffuseColor)
                         else:
                             c = so.ViewObject.ShapeColor
-                            c = (c[0],c[1],c[2],so.ViewObject.Transparency/100.0)
+                            c = (c[0], c[1], c[2], so.ViewObject.Transparency / 100.0)
                             colors += [c] * len(so.Shape.Faces)
         if colors:
+            first_color = colors[0]
+            for next_color in colors[1:]:
+                if next_color != first_color:
+                    break
+            else:
+                colors = [first_color]
             vobj.DiffuseColor = colors
 
 
