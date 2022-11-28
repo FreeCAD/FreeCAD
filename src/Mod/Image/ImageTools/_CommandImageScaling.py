aecddeedcc022813a352f642b59280c4700ce7f4
luz paz <luzpaz@pm.me>
2022-11-28 08:17:16 -0500

    Image: fix header uniformity
    Make uniform all Image workbench headers.

--- src/Mod/Image/ImageTools/_CommandImageScaling.py
+++ src/Mod/Image/ImageTools/_CommandImageScaling.py
@@ -1,6 +1,5 @@
 # ***************************************************************************
-# *                                                                         *
-# *   Copyright (c) 2016 - Victor Titov (DeepSOIC) <vv.titov@gmail.com>     *
+# *   Copyright (c) 2016 Victor Titov (DeepSOIC) <vv.titov@gmail.com>       *
 # *                                                                         *
 # *   This program is free software; you can redistribute it and/or modify  *
 # *   it under the terms of the GNU Lesser General Public License (LGPL)    *
