3a6e8ab004030a5b17cbf8254dbbe86c7779f02c
Zheng, Lei <realthunder.dev@gmail.com>
2022-11-29 18:26:58 +0800

    Base: fix Windows build issue

--- src/Base/Observer.h
+++ src/Base/Observer.h
@@ -216,6 +216,15 @@ protected:
   std::set<Observer <_MessageType> *> _ObserverSet;
 };
 
+#if defined (FC_OS_WIN32) || defined(FC_OS_CYGWIN)
+#   ifdef FCBase
+template class BaseExport Observer<const char*>;
+template class BaseExport Subject<const char*>;
+#   else
+extern template class BaseExport Observer<const char*>;
+extern template class BaseExport Subject<const char*>;
+#   endif
+#endif
 
 } //namespace Base
 
