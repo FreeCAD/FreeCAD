178ac02c571b817b2c2c5f89bf03743d59e67bea
wmayer <wmayer@users.sourceforge.net>
2022-12-01 16:10:19 +0100

    Test: [skip ci] make all example tests pass

--- tests/src/test1.cpp
+++ tests/src/test1.cpp
@@ -1,19 +1,21 @@
 #include "gtest/gtest.h"
 
 TEST(FirstSuite, FirstTest){
-    EXPECT_NE(1, 1) << "are in fact equal";
+    EXPECT_EQ(1, 1) << "are in fact equal";
 }
 
 TEST(FirstSuite, SecondTest){
-    EXPECT_EQ(1, 2) << "not equal";
+    EXPECT_NE(1, 2) << "not equal";
 }
 
 TEST(FirstSuite, ThirdTest){
     ASSERT_STREQ("A", "A") << "str not equal";
 }
+
 TEST(FirstSuite,FifthTest){
-    ASSERT_STRNE("am", "am") << "str equal";
+    ASSERT_STREQ("am", "am") << "str equal";
 }
+
 TEST(FirstSuite, FourthTest){
-    ASSERT_STREQ("am", "A") << "str not equal";
+    ASSERT_STRNE("am", "A") << "str not equal";
 }
