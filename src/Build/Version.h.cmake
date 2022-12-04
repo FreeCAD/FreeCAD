0e5f6c8e75d0b276ce59a6155d34e239dbdc9e24
Adrian Insaurralde Avalos <adrianinsaval@gmail.com>
2022-12-04 18:57:43 -0300

    Unify version patch number management in cmake

--- src/Build/Version.h.cmake
+++ src/Build/Version.h.cmake
@@ -3,6 +3,7 @@
 #define FCVersionMajor "${PACKAGE_VERSION_MAJOR}"
 #define FCVersionMinor "${PACKAGE_VERSION_MINOR}"
 #define FCVersionName  "${PACKAGE_VERSION_NAME}"
+#define FCVersionPoint "${PACKAGE_VERSION_PATCH}"
 // test: $Format:Hash (%H), Date: %ci$
 #define FCRevision      "${PACKAGE_WCREF}"      //Highest committed revision number
 #define FCRevisionDate  "${PACKAGE_WCDATE}"     //Date of highest committed revision
