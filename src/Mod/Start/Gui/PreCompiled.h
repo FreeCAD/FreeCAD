78e7286ceaea17365d623a4ed8fbc875a3e1d119
Uwe <donovaly@users.noreply.github.com>
2022-12-07 04:38:02 +0100

    [Start] remove unused includes
    - also some sorting

--- src/Mod/Start/Gui/PreCompiled.h
+++ src/Mod/Start/Gui/PreCompiled.h
@@ -26,21 +26,6 @@
 #include <FCConfig.h>
 
 #ifdef _PreComp_
-// standard
-#include <iostream>
-#include <cassert>
-#include <cmath>
-
-// STL
-#include <vector>
-#include <map>
-#include <string>
-#include <list>
-#include <set>
-#include <algorithm>
-#include <stack>
-#include <queue>
-#include <bitset>
 
 #ifdef FC_OS_WIN32
 # include <windows.h>
