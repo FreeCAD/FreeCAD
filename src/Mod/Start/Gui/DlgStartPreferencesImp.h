e5cb1b24cd5871983a76b3390650db77fb35c7a6
Uwe <donovaly@users.noreply.github.com>
2022-12-07 04:38:02 +0100

    [Start] remove unused includes
    - also some sorting

--- src/Mod/Start/Gui/DlgStartPreferencesImp.h
+++ src/Mod/Start/Gui/DlgStartPreferencesImp.h
@@ -20,12 +20,12 @@
  *                                                                         *
  ***************************************************************************/
 
-
 #ifndef STARTGUI_DLGSTARTPREFERENCESIMP_H
 #define STARTGUI_DLGSTARTPREFERENCESIMP_H
 
-#include <Gui/PropertyPage.h>
 #include <memory>
+#include <Gui/PropertyPage.h>
+
 
 class Ui_DlgStartPreferences;
 namespace StartGui {
