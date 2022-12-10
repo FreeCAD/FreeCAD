c684b1a399186cfa4253082fb5093adca9850bfe
LarryWoestman <LarryWoestman2@gmail.com>
2022-12-10 13:26:05 -0800

    Path:  Fixed regression in input statement.

--- src/Mod/Path/Path/Main/Gui/PreferencesJob.py
+++ src/Mod/Path/Path/Main/Gui/PreferencesJob.py
@@ -23,7 +23,7 @@
 import FreeCAD
 import Path
 import Path.Main.Stock as PathStock
-import Path.Post.Processor as PostProcessor
+from Path.Post.Processor import PostProcessor
 import json
 
 from FreeCAD import Units
