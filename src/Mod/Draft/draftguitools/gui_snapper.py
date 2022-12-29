fe02d63c8c9b1280978be841d04e68a0a55cceb9
Roy-043 <70520633+Roy-043@users.noreply.github.com>
2022-12-29 23:20:38 +0100

    Draft: update inspect.getargspec to inspect.getfullargspec (#8101)

--- src/Mod/Draft/draftguitools/gui_snapper.py
+++ src/Mod/Draft/draftguitools/gui_snapper.py
@@ -1438,7 +1438,7 @@ class Snapper:
             Gui.Snapper.off()
             self.ui.offUi()
             if callback:
-                if len(inspect.getargspec(callback).args) > 1:
+                if len(inspect.getfullargspec(callback).args) > 1:
                     obj = None
                     if self.snapInfo and ("Object" in self.snapInfo) and self.snapInfo["Object"]:
                         obj = App.ActiveDocument.getObject(self.snapInfo["Object"])
@@ -1457,7 +1457,7 @@ class Snapper:
             Gui.Snapper.off()
             self.ui.offUi()
             if callback:
-                if len(inspect.getargspec(callback).args) > 1:
+                if len(inspect.getfullargspec(callback).args) > 1:
                     callback(None, None)
                 else:
                     callback(None)
