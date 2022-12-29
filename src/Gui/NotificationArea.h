60cb06e20c1cce0d362f490d00661fb8fef11824
Abdullah Tahiri <abdullah.tahiri.yo@gmail.com>
2022-12-29 15:57:44 +0100

    Gui: Notification Area
    ======================
    
    A notification area consisting of one area for non-intrusive notifications, and a widget showing previous
    errors, warnings, critical messages and notifications.
    
    Notifications being marked with QT_TRANSLATION_NOOP with context "Notifications" are translated.
    
    Messages are shown during a minimum show time, unless pop up (clicked inside). Messages stay until
    a maximum show time. Between the minimum and the maximum show times left mouse button events make
    the non-intrusive notification disappear.
    
    The widget stores a configurable amount of messages after which old messages are removed to make room for
    new ones.
    
    Unread messages are shown in bold.

---
+++ src/Gui/NotificationArea.h
@@ -0,0 +1,78 @@
+/***************************************************************************
+ *   Copyright (c) 2022 Abdullah Tahiri <abdullah.tahiri.yo@gmail.com>     *
+ *                                                                         *
+ *   This file is part of the FreeCAD CAx development system.              *
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Library General Public           *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2 of the License, or (at your option) any later version.      *
+ *                                                                         *
+ *   This library  is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU Library General Public License for more details.                  *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this library; see the file COPYING.LIB. If not,    *
+ *   write to the Free Software Foundation, Inc., 59 Temple Place,         *
+ *   Suite 330, Boston, MA  02111-1307, USA                                *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef GUI_NOTIFICATIONAREA_H
+#define GUI_NOTIFICATIONAREA_H
+
+#include <QPushButton>
+#include <QString>
+
+#include <map>
+#include <memory>
+
+#include <Base/Observer.h>
+#include <Base/Parameter.h>
+
+namespace Gui
+{
+
+struct NotificationAreaP;
+
+class NotificationArea: public QPushButton
+{
+public:
+    class ParameterObserver: public ParameterGrp::ObserverType
+    {
+    public:
+        explicit ParameterObserver(NotificationArea* notificationarea);
+        ~ParameterObserver();
+
+        void OnChange(Base::Subject<const char*>& rCaller, const char* sReason) override;
+
+    private:
+        NotificationArea* notificationArea;
+        ParameterGrp::handle hGrp;
+        std::map<std::string, std::function<void(const std::string& string)>> parameterMap;
+    };
+
+    NotificationArea(QWidget* parent = nullptr);
+    ~NotificationArea();
+
+    void pushNotification(const QString& notifiername, const QString& message,
+                          Base::LogStyle level);
+
+private:
+    void showInNotificationArea();
+    bool confirmationRequired(Base::LogStyle level);
+    void showConfirmationDialog(const QString& notifiername, const QString& message);
+    void slotRestoreFinished(const App::Document&);
+
+    void mousePressEvent(QMouseEvent* e) override;
+
+private:
+    std::unique_ptr<NotificationAreaP> pImp;
+};
+
+
+}// namespace Gui
+
+#endif// GUI_NOTIFICATIONAREA_H
