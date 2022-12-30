2e022d8e0bc627b23183006a1476ef0b43dd5ce1
wmayer <wmayer@users.sourceforge.net>
2022-12-31 00:17:37 +0100

    Test: [skip ci] simplify and add more tests

--- tests/src/Base/tst_Tools.cpp
+++ tests/src/Base/tst_Tools.cpp
@@ -3,30 +3,40 @@
 
 TEST(BaseToolsSuite, TestUniqueName1)
 {
-    EXPECT_STREQ(Base::Tools::getUniqueName("Body", {}).c_str(), "Body");
+    EXPECT_EQ(Base::Tools::getUniqueName("Body", {}), "Body");
 }
 
 TEST(BaseToolsSuite, TestUniqueName2)
 {
-    EXPECT_STREQ(Base::Tools::getUniqueName("Body", {"Body"}, 1).c_str(), "Body1");
+    EXPECT_EQ(Base::Tools::getUniqueName("Body", {"Body"}, 1), "Body1");
 }
 
 TEST(BaseToolsSuite, TestUniqueName3)
 {
-    EXPECT_STREQ(Base::Tools::getUniqueName("Body", {"Body"}, 3).c_str(), "Body001");
+    EXPECT_EQ(Base::Tools::getUniqueName("Body", {"Body"}, 3), "Body001");
 }
 
 TEST(BaseToolsSuite, TestUniqueName4)
 {
-    EXPECT_STREQ(Base::Tools::getUniqueName("Body", {"Body001"}, 3).c_str(), "Body002");
+    EXPECT_EQ(Base::Tools::getUniqueName("Body", {"Body001"}, 3), "Body002");
 }
 
 TEST(BaseToolsSuite, TestUniqueName5)
 {
-    EXPECT_STREQ(Base::Tools::getUniqueName("Body", {"Body001"}, 3).c_str(), "Body002");
+    EXPECT_EQ(Base::Tools::getUniqueName("Body", {"Body", "Body001"}, 3), "Body002");
 }
 
 TEST(BaseToolsSuite, TestUniqueName6)
 {
-    EXPECT_STREQ(Base::Tools::getUniqueName("Body001", {"Body", "Body001"}, 3).c_str(), "Body002");
+    EXPECT_EQ(Base::Tools::getUniqueName("Body001", {"Body", "Body001"}, 3), "Body002");
+}
+
+TEST(BaseToolsSuite, TestUniqueName7)
+{
+    EXPECT_EQ(Base::Tools::getUniqueName("Body001", {"Body"}, 3), "Body002");
+}
+
+TEST(BaseToolsSuite, TestUniqueName8)
+{
+    EXPECT_EQ(Base::Tools::getUniqueName("Body12345", {"Body"}, 3), "Body12346");
 }
