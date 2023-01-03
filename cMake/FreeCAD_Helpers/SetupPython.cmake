b0ae66b94c3f63efb2fea10d5e6ab54faac6b0f0
berniev <bernievbb@icloud.com>
2023-01-03 14:12:19 +1000

    Remove obsolete cmake code (<3.16.3)

--- cMake/FreeCAD_Helpers/SetupPython.cmake
+++ cMake/FreeCAD_Helpers/SetupPython.cmake
@@ -114,46 +114,19 @@ macro(SetupPython)
 
     endif(${CMAKE_SYSTEM_NAME} MATCHES "Darwin" AND NOT BUILD_WITH_CONDA)
 
-    if(${CMAKE_VERSION} VERSION_LESS "3.12")
-        set(Python_ADDITIONAL_VERSIONS ${Python_ADDITIONAL_VERSIONS} "3.10" "3.9" "3.8" "3.7" "3.6")
-        find_package(PythonInterp REQUIRED)
-        set(Python_ADDITIONAL_VERSIONS ${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR})
-        if (NOT DEFINED PYTHON_VERSION_STRING)
-            find_package(PythonLibs REQUIRED)
-        else (NOT DEFINED PYTHON_VERSION_STRING)
-            find_package(PythonLibs ${PYTHON_VERSION_STRING} EXACT)
-        endif(NOT DEFINED PYTHON_VERSION_STRING)
-        if(NOT PYTHONLIBS_FOUND)
-            message(FATAL_ERROR "=================================\n"
-                                "Python not found, install Python!\n"
-                                "=================================\n")
-        endif()
-        
-        # For forward compatibility with new CMake scripts
-        set(Python3_EXECUTABLE ${PYTHON_EXECUTABLE})
-        set(Python3_LIBRARIES ${PYTHON_LIBRARIES})
-        set(Python3_INCLUDE_DIRS ${PYTHON_INCLUDE_DIRS})
-        set(Python3_LIBRARY_DIRS ${PYTHON_LIBRARY_DIRS})
-        set(Python3_VERSION ${PYTHON_VERSION_STRING})
-        set(Python3_VERSION_MAJOR ${PYTHON_VERSION_MAJOR})
-        set(Python3_VERSION_MINOR ${PYTHON_VERSION_MINOR})
-        set(Python3_VERSION_PATCH ${PYTHON_VERSION_PATCH})
-        set(Python3_Interpreter_FOUND ${PYTHONINTERP_FOUND})
-    else()
-        find_package(Python3 COMPONENTS Interpreter Development REQUIRED)
+    find_package(Python3 COMPONENTS Interpreter Development REQUIRED)
 
-        # For backwards compatibility with old CMake scripts
-        set(PYTHON_EXECUTABLE ${Python3_EXECUTABLE})
-        set(PYTHON_LIBRARIES ${Python3_LIBRARIES})
-        set(PYTHON_INCLUDE_DIRS ${Python3_INCLUDE_DIRS})
-        set(PYTHON_LIBRARY_DIRS ${Python3_LIBRARY_DIRS})
-        set(PYTHON_VERSION_STRING ${Python3_VERSION})
-        set(PYTHON_VERSION_MAJOR ${Python3_VERSION_MAJOR})
-        set(PYTHON_VERSION_MINOR ${Python3_VERSION_MINOR})
-        set(PYTHON_VERSION_PATCH ${Python3_VERSION_PATCH})
-        set(PYTHONINTERP_FOUND ${Python3_Interpreter_FOUND})
-    endif()
-    
+    # For backwards compatibility with old CMake scripts
+    set(PYTHON_EXECUTABLE ${Python3_EXECUTABLE})
+    set(PYTHON_LIBRARIES ${Python3_LIBRARIES})
+    set(PYTHON_INCLUDE_DIRS ${Python3_INCLUDE_DIRS})
+    set(PYTHON_LIBRARY_DIRS ${Python3_LIBRARY_DIRS})
+    set(PYTHON_VERSION_STRING ${Python3_VERSION})
+    set(PYTHON_VERSION_MAJOR ${Python3_VERSION_MAJOR})
+    set(PYTHON_VERSION_MINOR ${Python3_VERSION_MINOR})
+    set(PYTHON_VERSION_PATCH ${Python3_VERSION_PATCH})
+    set(PYTHONINTERP_FOUND ${Python3_Interpreter_FOUND})
+
     if (${PYTHON_VERSION_STRING} VERSION_LESS "3.8")
          message(FATAL_ERROR "To build FreeCAD you need at least Python 3.8\n")
     endif()
