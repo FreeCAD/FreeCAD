1476fdcecea141bcbdfe186e6f2e70c458f22e78
luzpaz <luzpaz@users.noreply.github.com>
2023-01-09 13:59:37 +0000

    Increment year in copyright notice (2023) (#8156)
    * Increment year in copyright notice

--- src/Main/MainGui.cpp
+++ src/Main/MainGui.cpp
@@ -54,7 +54,7 @@
 
 void PrintInitHelp(void);
 
-const char sBanner[] = "\xc2\xa9 Juergen Riegel, Werner Mayer, Yorik van Havre and others 2001-2022\n"\
+const char sBanner[] = "\xc2\xa9 Juergen Riegel, Werner Mayer, Yorik van Havre and others 2001-2023\n"\
 "FreeCAD is free and open-source software licensed under the terms of LGPL2+ license.\n"\
 "FreeCAD wouldn't be possible without FreeCAD community.\n"\
 "  #####                 ####  ###   ####  \n" \
