e188802ca6997d2564e7570ab648462e6a059f87
Chris Hennes <chennes@gmail.com>
2023-01-28 12:27:13 -0600

    Addon Manager: Update license headers

--- src/Mod/AddonManager/TestAddonManagerApp.py
+++ src/Mod/AddonManager/TestAddonManagerApp.py
@@ -1,6 +1,6 @@
 # ***************************************************************************
 # *                                                                         *
-# *   Copyright (c) 2022 FreeCAD Project Association                        *
+# *   Copyright (c) 2022-2023 FreeCAD Project Association                   *
 # *                                                                         *
 # *   This file is part of FreeCAD.                                         *
 # *                                                                         *
