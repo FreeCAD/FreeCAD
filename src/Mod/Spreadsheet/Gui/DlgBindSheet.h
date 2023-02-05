5ff1b531123b3fd8eb4d13ae38b2ba0f81e9cb13
luzpaz <luzpaz@users.noreply.github.com>
2023-02-05 14:18:59 +0000

    Fix whitespace issues

--- src/Mod/Spreadsheet/Gui/DlgBindSheet.h
+++ src/Mod/Spreadsheet/Gui/DlgBindSheet.h
@@ -35,11 +35,11 @@ namespace SpreadsheetGui {
 class DlgBindSheet : public QDialog
 {
     Q_OBJECT
-    
+
 public:
     explicit DlgBindSheet(Spreadsheet::Sheet *sheet, const std::vector<App::Range> &range, QWidget *parent = nullptr);
     ~DlgBindSheet() override;
-    
+
     void accept() override;
 
 public Q_SLOTS:
--- tools/build/manjaro.sh
+++ tools/build/manjaro.sh
@@ -3,5 +3,5 @@ pacman --noconfirm --sync boost boost-libs cmake coin curl desktop-file-utils do
     eigen gc gcc-fortran git glew gnu-free-fonts guile hicolor-icon-theme jsoncpp \
     libspnav libtool make med opencascade pyside2 pyside2-tools python-matplotlib \
     python-netcdf4 python-pivy qt5-svg qt5-tools qt5-webengine qt5-webkit \
-    shared-mime-info shiboken2 swig texinfo xerces-c 
+    shared-mime-info shiboken2 swig texinfo xerces-c
 
