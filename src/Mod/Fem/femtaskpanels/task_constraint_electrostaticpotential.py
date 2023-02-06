48640a6d8bb8ac116e9c58d092e58433b2ca3085
Uwe <donovaly@users.noreply.github.com>
2023-02-06 05:32:02 +0100

    [FEM] Elmer: extent potential constraint
    - it is now possible to specify a an electric potential vector field

--- src/Mod/Fem/femtaskpanels/task_constraint_electrostaticpotential.py
+++ src/Mod/Fem/femtaskpanels/task_constraint_electrostaticpotential.py
@@ -1,6 +1,7 @@
 # ***************************************************************************
 # *   Copyright (c) 2017 Markus Hovorka <m.hovorka@live.de>                 *
 # *   Copyright (c) 2020 Bernd Hahnebach <bernd@bimstatik.org>              *
+# *   Copyright (c) 2023 Uwe Stöhr <uwestoehr@lyx.org>                      *
 # *                                                                         *
 # *   This file is part of the FreeCAD CAx development system.              *
 # *                                                                         *
@@ -23,12 +24,14 @@
 # ***************************************************************************
 
 __title__ = "FreeCAD FEM constraint electrostatic potential task panel for the document object"
-__author__ = "Markus Hovorka, Bernd Hahnebach"
+__author__ = "Markus Hovorka, Bernd Hahnebach, Uwe Stöhr"
 __url__ = "https://www.freecadweb.org"
 
 ## @package task_constraint_electrostaticpotential
 #  \ingroup FEM
 #  \brief task panel for constraint electrostatic potential object
+
+from PySide import QtCore
 
 import FreeCAD
 import FreeCADGui
@@ -69,6 +72,24 @@ class _TaskPanel(object):
         self._partVisible = None
         self._meshVisible = None
 
+        # start with vector inputs hidden if no vector is set
+        if self._obj.AV_re_1_Disabled and \
+            self._obj.AV_re_2_Disabled and \
+            self._obj.AV_re_3_Disabled and \
+            self._obj.AV_im_Disabled and \
+            self._obj.AV_im_1_Disabled and \
+            self._obj.AV_im_2_Disabled and \
+            self._obj.AV_im_3_Disabled:
+            self._vectorField_visibility(False)
+        QtCore.QObject.connect(
+            self._paramWidget.vectorFieldBox,
+            QtCore.SIGNAL("toggled(bool)"),
+            self._vectorField_visibility
+        )
+
+    def _vectorField_visibility(self, visible):
+        self._paramWidget.vectorFieldGB.setVisible(visible)
+
     def open(self):
         if self._mesh is not None and self._part is not None:
             self._meshVisible = self._mesh.ViewObject.isVisible()
@@ -108,6 +129,57 @@ class _TaskPanel(object):
             self._paramWidget.potentialQSB).bind(self._obj, "Potential")
         self._paramWidget.potentialBox.setChecked(
             not self._obj.PotentialEnabled)
+
+        # the vector potentials
+        # realScalarQSB always the same value as potentialQSB
+        self._paramWidget.realScalarQSB.setProperty(
+            'value', self._obj.Potential)
+        FreeCADGui.ExpressionBinding(
+            self._paramWidget.realScalarQSB).bind(self._obj, "Potential")
+        self._paramWidget.realXQSB.setProperty(
+            'value', self._obj.AV_re_1)
+        FreeCADGui.ExpressionBinding(
+            self._paramWidget.realXQSB).bind(self._obj, "AV_re_1")
+        self._paramWidget.realYQSB.setProperty(
+            'value', self._obj.AV_re_2)
+        FreeCADGui.ExpressionBinding(
+            self._paramWidget.realYQSB).bind(self._obj, "AV_re_2")
+        self._paramWidget.realZQSB.setProperty(
+            'value', self._obj.AV_re_3)
+        FreeCADGui.ExpressionBinding(
+            self._paramWidget.realZQSB).bind(self._obj, "AV_re_3")
+        self._paramWidget.imagScalarQSB.setProperty(
+            'value', self._obj.AV_im)
+        FreeCADGui.ExpressionBinding(
+            self._paramWidget.imagScalarQSB).bind(self._obj, "AV_im")
+        self._paramWidget.imagXQSB.setProperty(
+            'value', self._obj.AV_im_1)
+        FreeCADGui.ExpressionBinding(
+            self._paramWidget.imagXQSB).bind(self._obj, "AV_im_1")
+        self._paramWidget.imagYQSB.setProperty(
+            'value', self._obj.AV_im_2)
+        FreeCADGui.ExpressionBinding(
+            self._paramWidget.imagYQSB).bind(self._obj, "AV_im_2")
+        self._paramWidget.imagZQSB.setProperty(
+            'value', self._obj.AV_im_3)
+        FreeCADGui.ExpressionBinding(
+            self._paramWidget.imagZQSB).bind(self._obj, "AV_im_3")
+
+        self._paramWidget.reXunspecBox.setChecked(
+            self._obj.AV_re_1_Disabled)
+        self._paramWidget.reYunspecBox.setChecked(
+            self._obj.AV_re_2_Disabled)
+        self._paramWidget.reZunspecBox.setChecked(
+            self._obj.AV_re_3_Disabled)
+        self._paramWidget.imScalarunspecBox.setChecked(
+            self._obj.AV_im_Disabled)
+        self._paramWidget.imXunspecBox.setChecked(
+            self._obj.AV_im_1_Disabled)
+        self._paramWidget.imYunspecBox.setChecked(
+            self._obj.AV_im_2_Disabled)
+        self._paramWidget.imZunspecBox.setChecked(
+            self._obj.AV_im_3_Disabled)
+
         self._paramWidget.potentialConstantBox.setChecked(
             self._obj.PotentialConstant)
 
@@ -124,21 +196,63 @@ class _TaskPanel(object):
         self._paramWidget.capacitanceBody_spinBox.setEnabled(
             not self._paramWidget.capacitanceBodyBox.isChecked())
 
+    def _applyPotentialChanges(self, enabledBox, potentialQSB):
+        enabled = enabledBox.isChecked()
+        potential = None
+        try:
+            potential = potentialQSB.property('value')
+        except ValueError:
+            FreeCAD.Console.PrintMessage(
+                "Wrong input. Not recognised input: '{}' "
+                "Potential has not been set.\n".format(potentialQSB.text())
+            )
+            potential = '0.0 mm^2*kg/(s^3*A)'
+        return enabled, potential
+
     def _applyWidgetChanges(self):
-        self._obj.PotentialEnabled = \
-            not self._paramWidget.potentialBox.isChecked()
-        if self._obj.PotentialEnabled:
-            potential = None
-            try:
-                potential = self._paramWidget.potentialQSB.property('value')
-            except ValueError:
-                FreeCAD.Console.PrintMessage(
-                    "Wrong input. Not recognised input: '{}' "
-                    "Potential has not been set.\n"
-                    .format(self._paramWidget.potentialQSB.text())
-                )
-            if potential is not None:
-                self._obj.Potential = potential
+        # apply the voltages and their enabled state
+        self._obj.PotentialEnabled, self._obj.Potential = \
+            self._applyPotentialChanges(
+                self._paramWidget.potentialBox,
+                self._paramWidget.potentialQSB
+            )
+        self._obj.AV_re_1_Disabled, self._obj.AV_re_1 = \
+            self._applyPotentialChanges(
+                self._paramWidget.reXunspecBox,
+                self._paramWidget.realXQSB
+            )
+        self._obj.AV_re_2_Disabled, self._obj.AV_re_2 = \
+            self._applyPotentialChanges(
+                self._paramWidget.reYunspecBox,
+                self._paramWidget.realYQSB
+            )
+        self._obj.AV_re_3_Disabled, self._obj.AV_re_3 = \
+            self._applyPotentialChanges(
+                self._paramWidget.reZunspecBox,
+                self._paramWidget.realZQSB
+            )
+        self._obj.AV_im_Disabled, self._obj.AV_im = \
+            self._applyPotentialChanges(
+                self._paramWidget.imScalarunspecBox,
+                self._paramWidget.imagScalarQSB
+            )
+        self._obj.AV_im_1_Disabled, self._obj.AV_im_1 = \
+            self._applyPotentialChanges(
+                self._paramWidget.imXunspecBox,
+                self._paramWidget.imagXQSB
+            )
+        self._obj.AV_im_2_Disabled, self._obj.AV_im_2 = \
+            self._applyPotentialChanges(
+                self._paramWidget.imYunspecBox,
+                self._paramWidget.imagYQSB
+            )
+        self._obj.AV_im_3_Disabled, self._obj.AV_im_3 = \
+            self._applyPotentialChanges(
+                self._paramWidget.imZunspecBox,
+                self._paramWidget.imagZQSB
+            )
+        # because this is an enable the others are disabled, reverse
+        self._obj.PotentialEnabled = not self._obj.PotentialEnabled
 
         self._obj.PotentialConstant = self._paramWidget.potentialConstantBox.isChecked()
 
