4dea0df06c4d088f71849229dfbbb6e38aeeac26
Abdullah Tahiri <abdullah.tahiri.yo@gmail.com>
2023-02-13 16:55:41 +0100

    App: Extension/ExtensionContainer - handle property change
    ==========================================================
    
    Currently changes of name or type of properties in a property container are handled by:
    void PropertyContainer::handleChangedPropertyName(Base::XMLReader &reader, const char * TypeName, const char *PropName)
    void PropertyContainer::changedPropertyType(Base::XMLReader &reader, const char * TypeName, Property * prop)
    
    There is no mechanism for handling property changes by extensions. Sometimes the solution is to explicitly call the extension
    from the container. However, this is a breach of the SRP, as the container should not be in a position to decide whether the
    extension needs or not handle property changes. The handling code of the container changes for two different reasons, for
    adapting the container to a property change of its own, and for adapting that of a property of the extension.
    
    Illustrating it with an example, following inheritance, it goes like this:
    PropertyContainer => ExtensionContainer => TransactionalObject => ViewProvider
    App::Extension => ViewProviderExtension
    
    The extension is currently not notified by the ExtensionContainer that a property needs handling. So a change in a property of
    a ViewProviderExtension needs code at the ViewProvider it was added to.
    
    This commit provides a mechanism in ExtensionContainer to call the extensions so that they can handle property changes. This
    functions:
    
      virtual bool extensionHandleChangedPropertyName(Base::XMLReader &reader, const char * TypeName, const char *PropName);
      virtual bool extensionHandleChangedPropertyType(Base::XMLReader &reader, const char * TypeName, Property * prop);
    
    Containers should always call the base class for any unhandled property change. If a sub-class container of ExtensionContainer
    handles property changes itself, but not the ones of the extensions, this call to the base class ultimately ensures that if the
    property was not handled by the container hierarchy, any extension is given an opportunity to handle it.
    
    Some examples:
    
    * A container handles the extension property change or its own:
    
    void ContainerSubClass::handleChangedPropertyType(...)
    {
        if (prop == &PropertyOfExt) {
    
        }
        else if (prop == &PropertyOfCont) {
    
        }
        else {
            ContainerBaseClass::handleChangedPropertyType(...);
        }
    }
    
    * A container and the extension handle their own:
    
    void ContainerSubClass::handleChangedPropertyType(...)
    {
        if (prop == &PropertyOfCont) {
    
        }
        else {
            // This will call ExtensionContainer::handleChangedPropertyType
            ContainerBaseClass::handleChangedPropertyType(...);
        }
    }
    
    bool ExtensionSubClass::extensionHandleChangedPropertyType(...)
    {
        if (prop == &PropertyOfCont) {
    
            return true;
        }
        return false;
    }

--- src/App/Extension.h
+++ src/App/Extension.h
@@ -285,6 +285,11 @@ protected:
 
     virtual void extensionOnChanged(const Property* p) {(void)(p);}
 
+    /// returns true if the property name change was handled by the extension.
+    virtual bool extensionHandleChangedPropertyName(Base::XMLReader &reader, const char * TypeName, const char *PropName);
+    /// returns true if the property type change was handled by the extension.
+    virtual bool extensionHandleChangedPropertyType(Base::XMLReader &reader, const char * TypeName, Property * prop);
+
     friend class App::ExtensionContainer;
 
 protected:
