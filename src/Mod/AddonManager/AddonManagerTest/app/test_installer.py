18d1686dbc72a8e89607819cf9f944f0ef00970d
Chris Hennes <chennes@gmail.com>
2023-02-20 12:09:02 -0600

    Addon Manager: Add SPDX license ID to all files

--- src/Mod/AddonManager/AddonManagerTest/app/test_installer.py
+++ src/Mod/AddonManager/AddonManagerTest/app/test_installer.py
@@ -1,3 +1,4 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
 # ***************************************************************************
 # *                                                                         *
 # *   Copyright (c) 2022 FreeCAD Project Association                        *
