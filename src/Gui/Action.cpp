150097af31c6f08e99435d19cd06ac0be5f55a6d
Syres916 <46537884+Syres916@users.noreply.github.com>
2023-02-23 14:06:04 +0000

    [Gui] Recent macros, improve wording of Hint msg

--- src/Gui/Action.cpp
+++ src/Gui/Action.cpp
@@ -1173,12 +1173,13 @@ void RecentMacrosAction::setFiles(const QStringList& files)
         for (int index = 0; index < accel_col.count(); index++) {
             msgMain = msgMain + QStringLiteral(" %1").arg(accel_col[index]);
         }
-        msgMain = msgMain + QStringLiteral(" disabled because conflicting with");
+        msgMain = msgMain + QStringLiteral(" disabled because of conflicts with");
         for (int index = 0; index < existingCommands.count(); index++) {
             msgMain = msgMain + QStringLiteral(" %1").arg(existingCommands[index]);
         }
         msgMain = msgMain + QStringLiteral(" respectively.\nHint: In Preferences -> Macros -> Recent Macros -> Keyboard Modifiers"
-                                           " contains Ctrl+Shift+ as default, this may have been changed unintentionally.");
+                                           " this should be Ctrl+Shift+ by default, if this is now blank then you should revert"
+                                           " it back to Ctrl+Shift+ by pressing both keys at the same time.");
         Base::Console().Warning("%s\n", qPrintable(msgMain));
     }
 }
