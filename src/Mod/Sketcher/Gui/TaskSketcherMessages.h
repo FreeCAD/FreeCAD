3161213c52d133c3e619a5806bd2def6ef2b0ee1
Uwe <donovaly@users.noreply.github.com>
2023-03-11 17:26:13 +0100

    [Sketch] reverse commit 944c02f12be37
    - as discussed in #8838 (the PR will be improved after the weekend)

--- src/Mod/Sketcher/Gui/TaskSketcherMessages.h
+++ src/Mod/Sketcher/Gui/TaskSketcherMessages.h
@@ -39,19 +39,20 @@ namespace SketcherGui {
 
 class ViewProviderSketch;
 
-class TaskSketcherMessages : public QWidget
+class TaskSketcherMessages : public Gui::TaskView::TaskBox
 {
     Q_OBJECT
 
 public:
-    explicit TaskSketcherMessages(ViewProviderSketch* sketchView);
+    explicit TaskSketcherMessages(ViewProviderSketch *sketchView);
     ~TaskSketcherMessages() override;
 
-    void slotSetUp(const QString& state, const QString& msg, const QString& link, const QString& linkText);
+    void slotSetUp(const QString &state, const QString &msg, const QString& link, const QString& linkText);
 
 private Q_SLOTS:
     void on_labelConstrainStatusLink_linkClicked(const QString &);
-    void onAutoUpdateStateChanged();
+    void on_autoUpdate_stateChanged(int state);
+    void on_autoRemoveRedundants_stateChanged(int state);
     void on_manualUpdate_clicked(bool checked);
     
 protected:
@@ -59,6 +60,7 @@ protected:
     Connection connectionSetUp;
 
 private:
+    QWidget* proxy;
     std::unique_ptr<Ui_TaskSketcherMessages> ui;
 };
 
