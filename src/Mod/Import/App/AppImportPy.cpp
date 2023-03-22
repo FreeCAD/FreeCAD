956a0f25319413f0eeeeb693aea92acc7da26e6c
wmayer <wmayer@users.sourceforge.net>
2023-03-22 20:06:52 +0100

    Import: issue #8884: C++ DXF importer never imports text (annotations This reverts commit e1ebcef39e8

--- src/Mod/Import/App/AppImportPy.cpp
+++ src/Mod/Import/App/AppImportPy.cpp
@@ -407,7 +407,7 @@ private:
         char* Name;
         const char* DocName=nullptr;
         const char* optionSource = nullptr;
-        std::string defaultOptions = "User parameter:BaseApp/Preferences/Mod/Import";
+        std::string defaultOptions = "User parameter:BaseApp/Preferences/Mod/Draft";
         bool IgnoreErrors=true;
         if (!PyArg_ParseTuple(args.ptr(), "et|sbs","utf-8",&Name,&DocName,&IgnoreErrors,&optionSource))
             throw Py::Exception();
