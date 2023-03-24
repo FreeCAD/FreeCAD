048b358c018cbd3a4b1e842132bf8cd3eacd431d
Kevin Martin <kpmartin@papertrail.ca>
2023-03-24 18:03:52 -0400

    [Import] DXF: correction to documentation
    Correction to documentation comment noticed while addressing these issues.
    The comment for readDXF did not document its fourth option_source argument.

--- src/Mod/Import/App/AppImportPy.cpp
+++ src/Mod/Import/App/AppImportPy.cpp
@@ -108,7 +108,7 @@ public:
             "export(list,string) -- Export a list of objects into a single file."
         );
          add_varargs_method("readDXF",&Module::readDXF,
-            "readDXF(filename,[document,ignore_errors]): Imports a DXF file into the given document. ignore_errors is True by default."
+            "readDXF(filename,[document,ignore_errors,option_source]): Imports a DXF file into the given document. ignore_errors is True by default."
         );
         add_varargs_method("writeDXFShape",&Module::writeDXFShape,
             "writeDXFShape([shape],filename [version,usePolyline,optionSource]): Exports Shape(s) to a DXF file."
