461e30bc3b57a9a47c03d3b209cc7ef75dcfaf6b
Abdullah Tahiri <abdullah.tahiri.yo@gmail.com>
2023-05-25 15:18:51 +0200

    Sketcher: /Mod/Sketcher python code black formatted

--- src/Mod/Sketcher/SketcherExample.py
+++ src/Mod/Sketcher/SketcherExample.py
@@ -10,22 +10,26 @@ EndPoint = 2
 MiddlePoint = 3
 
 # create a document and a Sketch object
-if(App.activeDocument() is None):App.newDocument()
+if App.activeDocument() is None:
+    App.newDocument()
 
-f = App.activeDocument().addObject("Sketcher::SketchObject","Sketch")
+f = App.activeDocument().addObject("Sketcher::SketchObject", "Sketch")
 
 # add geometry to the sketch
-f.Geometry = [LineSegment(Vector(0,0,0),Vector(2,20,0)),LineSegment(Vector(0,0,0),Vector(20,2,0))]
+f.Geometry = [
+    LineSegment(Vector(0, 0, 0), Vector(2, 20, 0)),
+    LineSegment(Vector(0, 0, 0), Vector(20, 2, 0)),
+]
 
 # add constraints to the sketch
-f.Constraints = [Constraint('Vertical',0),Constraint('Horizontal',1)]
+f.Constraints = [Constraint("Vertical", 0), Constraint("Horizontal", 1)]
 
 # recompute (solving) the sketch
 App.activeDocument().recompute()
 
 # add another constraint to tie the start points together
 l = f.Constraints
-l.append(Constraint('Coincident',0,StartPoint,1,StartPoint))
+l.append(Constraint("Coincident", 0, StartPoint, 1, StartPoint))
 f.Constraints = l
 
 # again recompute
