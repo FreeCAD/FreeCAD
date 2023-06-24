03a8954f6b726129a7e66b76a222a0e7915e93f5
0penBrain <48731257+0penBrain@users.noreply.github.com>
2023-06-24 09:45:09 +0200

    [Bugfix]Sketcher: fix cross section side switch not working except for XY plane

--- src/Mod/Show/mTempoVis.py
+++ src/Mod/Show/mTempoVis.py
@@ -450,9 +450,7 @@ class TempoVis(object):
         toggle = {False: 0, True: 1, None: -1}[enable]
 
         if reverted:
-            skNorm = pla.Rotation.multVec(App.Vector(0,0,1))
-            skInvNorm = skNorm.negative()
-            pla = pla * App.Rotation(skNorm, skInvNorm)
+            pla = pla * App.Rotation(0, 1, 0, 0)
 
         if enable: # clip plane shall be disabled so new placement can be applied
             self.modify(ClipPlane(doc, 0))
