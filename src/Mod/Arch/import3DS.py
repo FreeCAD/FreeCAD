49da7907426cd84863c30d1ca7b1640299f9cf7f
Roy-043 <info@b-k-g.nl>
2023-06-26 20:14:35 +0200

    Arch: Fix encoding error (caused by #9316)

--- src/Mod/Arch/import3DS.py
+++ src/Mod/Arch/import3DS.py
@@ -50,7 +50,7 @@ def open(filename):
     "called when freecad wants to open a file"
     if not check3DS():
         return
-    docname = (os.path.splitext(os.path.basename(filename))[0]).encode("utf8")
+    docname = os.path.splitext(os.path.basename(filename))[0]
     doc = FreeCAD.newDocument(docname)
     doc.Label = docname
     FreeCAD.ActiveDocument = doc
