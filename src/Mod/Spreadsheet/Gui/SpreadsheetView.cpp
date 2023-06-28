db92e6b955365024c517635c3d396db05d0beeb2
RobbeDGreef <robbedegreef@gmail.com>
2023-06-28 11:12:08 +0200

    fix: Spreadsheet applies content/alias even when focus lost
    The original stylesheet behavior was that the alias or content fields
    could be edited, but only after enter was pressed the field would
    actually update. This change makes the field apply when focus is lost
    on top of when enter is pressed. This makes it easier to enter the
    alias of a lot of fields at once.

--- src/Mod/Spreadsheet/Gui/SpreadsheetView.cpp
+++ src/Mod/Spreadsheet/Gui/SpreadsheetView.cpp
@@ -94,8 +94,8 @@ SheetView::SheetView(Gui::Document *pcDocument, App::DocumentObject *docObj, QWi
             this, &SheetView::rowResized);
 
     connect(delegate, &SpreadsheetDelegate::finishedWithKey, this, &SheetView::editingFinishedWithKey);
-    connect(ui->cellContent, &ExpressionLineEdit::returnPressed, this, [this]() {confirmContentChanged(ui->cellContent->text()); });
-    connect(ui->cellAlias, &ExpressionLineEdit::returnPressed, this, [this]() {confirmAliasChanged(ui->cellAlias->text()); });
+    connect(ui->cellContent, &ExpressionLineEdit::editingFinished, this, [this]() {confirmContentChanged(ui->cellContent->text()); });
+    connect(ui->cellAlias, &ExpressionLineEdit::editingFinished, this, [this]() {confirmAliasChanged(ui->cellAlias->text()); });
     connect(ui->cellAlias, &LineEdit::textEdited, this, &SheetView::aliasChanged);
 
     columnWidthChangedConnection = sheet->columnWidthChanged.connect(bind(&SheetView::resizeColumn, this, bp::_1, bp::_2));
