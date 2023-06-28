1c4d2ef7c44d34c36739a1b0e1dbb5343369178b
David Carter <dcarter@davidcarter.ca>
2023-06-28 01:35:46 -0400

    Code refactoring

--- src/Mod/Material/App/MaterialLoader.h
+++ src/Mod/Material/App/MaterialLoader.h
@@ -28,46 +28,9 @@
 #include <yaml-cpp/yaml.h>
 
 #include "Materials.h"
+#include "trim.h"
 
 namespace Materials {
-
-// trim from start (in place)
-static inline void ltrim(std::string &s) {
-    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
-        return !std::isspace(ch);
-    }));
-}
-
-// trim from end (in place)
-static inline void rtrim(std::string &s) {
-    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
-        return !std::isspace(ch);
-    }).base(), s.end());
-}
-
-// trim from both ends (in place)
-static inline void trim(std::string &s) {
-    rtrim(s);
-    ltrim(s);
-}
-
-// trim from start (copying)
-static inline std::string ltrim_copy(std::string s) {
-    ltrim(s);
-    return s;
-}
-
-// trim from end (copying)
-static inline std::string rtrim_copy(std::string s) {
-    rtrim(s);
-    return s;
-}
-
-// trim from both ends (copying)
-static inline std::string trim_copy(std::string s) {
-    trim(s);
-    return s;
-}
 
 class MaterialEntry
 {
