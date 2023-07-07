696208116b80c5e2c6b7fab0d8aaf8d726a77d56
Syres916 <46537884+Syres916@users.noreply.github.com>
2023-07-07 10:44:02 +0100

    Reduce number of lines of code

--- src/Mod/Start/StartPage/LoadCustom.py
+++ src/Mod/Start/StartPage/LoadCustom.py
@@ -42,28 +42,12 @@ if cfolders:
         "DefaultImport" + ext, ""
     )
     if (
-        ext.lower() == "fcstd"
-        or ext.lower() == "step"
-        or ext.lower() == "stp"
-        or ext.lower() == "iges"
-        or ext.lower() == "igs"
+        ext.lower() in ("fcstd", "stp", "step", "iges", "igs")
     ):
         FreeCAD.loadFile(os.path.join(cfolder, f), mod)
     elif (
-        ext.lower() == "bmp"
-        or ext.lower() == "cur"
-        or ext.lower() == "gif"
-        or ext.lower() == "ico"
-        or ext.lower() == "pbm"
-        or ext.lower() == "pgm"
-        or ext.lower() == "png"
-        or ext.lower() == "jpg"
-        or ext.lower() == "jpeg"
-        or ext.lower() == "ppm"
-        or ext.lower() == "svg"
-        or ext.lower() == "svgz"
-        or ext.lower() == "xbm"
-        or ext.lower() == "xpm"
+        ext.lower() in ("bmp", "cur", "gif", "ico", "pbm", "pgm", "png", "jpg",
+                        "jpeg", "ppm", "svg", "svgz", "xbm", "xpm")
     ):
         FreeCAD.newDocument()
         FreeCADGui.insert(os.path.join(cfolder, f), FreeCAD.activeDocument().Name)
