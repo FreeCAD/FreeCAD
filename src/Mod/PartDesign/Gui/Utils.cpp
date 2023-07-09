FreeCAD/FreeCAD

commit a3c4f4bb01f5201daf666852444a8f9c5adbda8d

PD: fixes #9538: sometimes can't deselect things in newly created sketches

src/Mod/PartDesign/Gui/Utils.cpp
@@ -55,6 +55,23 @@ using namespace Attacher;
 
 namespace PartDesignGui {
 
+// TODO: Refactor DocumentObjectItem::getSubName() that has similar logic
+App::DocumentObject* getParent(App::DocumentObject* obj, std::string& subname)
+{
+    auto inlist = obj->getInList();
+    for (auto it : inlist) {
+        if (it->hasExtension(App::GeoFeatureGroupExtension::getExtensionClassTypeId())) {
+            std::string parent;
+            parent += obj->getNameInDocument();
+            parent += '.';
+            subname = parent + subname;
+            return getParent(it, subname);
+        }
+    }
+
+    return obj;
+}
+
 bool setEdit(App::DocumentObject *obj, PartDesign::Body *body) {
     if (!obj || !obj->getNameInDocument()) {
         FC_ERR("invalid object");
@@ -76,9 +93,9 @@ bool setEdit(App::DocumentObject *obj, PartDesign::Body *body) {
     if (activeBody != body) {
         parent = obj;
         subname.clear();
-    } else {
-        subname += obj->getNameInDocument();
-        subname += '.';
+    }
+    else {
+        parent = getParent(obj, subname);
     }
 
     Gui::cmdGuiDocument(parent, std::ostringstream() << "setEdit("

commit a2e3459068b0001913e361bea4a62a1a620556df

Fix #9792: PD tasks fail to launch if body is inside Part and activated
introduced by PR #9557

src/Mod/PartDesign/Gui/Utils.cpp
@@ -89,10 +89,9 @@ bool setEdit(App::DocumentObject *obj, PartDesign::Body *body) {
         return false;
     App::DocumentObject *parent = nullptr;
     std::string subname;
-    auto activeBody = activeView->getActiveObject<PartDesign::Body*>(PDBODYKEY,&parent,&subname);
+    auto activeBody = activeView->getActiveObject<PartDesign::Body*>(PDBODYKEY);
     if (activeBody != body) {
         parent = obj;
-        subname.clear();
     }
     else {
         parent = getParent(obj, subname);