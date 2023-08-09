FreeCAD/FreeCAD

commit 3b6a01095f1230a8121ad0f1422a9cee1d79224e

fixes #10127: Usage of std::filesystem in test bumps minimum GCC version to GCC 8

tests/src/Base/Reader.cpp
@@ -5,11 +5,11 @@
 #include "Base/Exception.h"
 #include "Base/Reader.h"
 #include <array>
-#include <filesystem>
+#include <boost/filesystem.hpp>
 #include <fmt/format.h>
 #include <fstream>
 
-namespace fs = std::filesystem;
+namespace fs = boost::filesystem;
 
 class ReaderTest: public ::testing::Test
 {
@@ -24,8 +24,8 @@ protected:
 
     void TearDown() override
     {
-        if (std::filesystem::exists(_tempFile)) {
-            std::filesystem::remove(_tempFile);
+        if (fs::exists(_tempFile)) {
+            fs::remove(_tempFile);
         }
     }
 

commit 79c6cbd75f8220bc38adec94a90ef3a7a0a6a808

MSVC: fix build failure caused by PR #10138

tests/src/Base/Reader.cpp
@@ -9,7 +9,12 @@
 #include <fmt/format.h>
 #include <fstream>
 
+#ifdef _MSC_VER
+#include <filesystem>
+namespace fs = std::filesystem;
+#else
 namespace fs = boost::filesystem;
+#endif
 
 class ReaderTest: public ::testing::Test
 {