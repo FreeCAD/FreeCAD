3d12a4b9fd05d5b0d5d299a46f9f1321a99f2c50
David Kaufman <davidgilkaufman@gmail.com>
2023-08-22 11:00:29 -0400

    [Path] Fix bug where ramp dressup makes big circles

--- src/Mod/Path/Path/Dressup/Gui/RampEntry.py
+++ src/Mod/Path/Path/Dressup/Gui/RampEntry.py
@@ -479,9 +479,9 @@ class ObjectDressup:
         ):
             return Part.makeLine(startPoint, endPoint)
         elif type(originalEdge.Curve) == Part.Circle:
-            arcMid = originalEdge.valueAt(
-                (originalEdge.FirstParameter + originalEdge.LastParameter) / 2
-            )
+            firstParameter = originalEdge.Curve.parameter(startPoint)
+            lastParameter = originalEdge.Curve.parameter(endPoint)
+            arcMid = originalEdge.valueAt((firstParameter + lastParameter) / 2)
             arcMid.z = (startPoint.z + endPoint.z) / 2
             return Part.Arc(startPoint, arcMid, endPoint).toShape()
         else:
