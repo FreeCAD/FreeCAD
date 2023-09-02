77878deb3b41181f557cd8d01edaa79c2ef6dc3a
Chris Hennes <chennes@gmail.com>
2023-09-02 12:47:07 -0500

    Tests: Reformat to current clang-format standard

--- tests/src/Mod/Sketcher/App/planegcs/GCS.cpp
+++ tests/src/Mod/Sketcher/App/planegcs/GCS.cpp
@@ -4,9 +4,11 @@
 
 #include "Mod/Sketcher/App/planegcs/GCS.h"
 
-class SystemTest : public GCS::System{
+class SystemTest: public GCS::System
+{
 public:
-    size_t getNumberOfConstraints(int tagID = -1) {
+    size_t getNumberOfConstraints(int tagID = -1)
+    {
         return _getNumberOfConstraints(tagID);
     }
 };
@@ -33,7 +35,7 @@ private:
     std::unique_ptr<SystemTest> _system;
 };
 
-TEST_F(GCSTest, clearConstraints) // NOLINT
+TEST_F(GCSTest, clearConstraints)// NOLINT
 {
     // Arrange
     const size_t numConstraints {100};
