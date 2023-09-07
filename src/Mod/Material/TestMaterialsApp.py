232eb28616b659798601a3d4d9df4e209fedf2de
David Carter <dcarter@davidcarter.ca>
2023-09-07 02:27:54 -0400

    Reset

--- src/Mod/Material/TestMaterialsApp.py
+++ src/Mod/Material/TestMaterialsApp.py
@@ -34,20 +34,18 @@ class MaterialTestCases(unittest.TestCase):
         self.assertIn("Models", dir(self.ModelManager))
 
     def testModelLoad(self):
-        print("Check that we're updating")
-        self.assertRaises(LookupError, self.ModelManager.getModel("454661e5-265b-4320-8e6f-fcf6223ac3af"))
-        # density = self.ModelManager.getModel("454661e5-265b-4320-8e6f-fcf6223ac3af")
-        # self.assertIsNotNone(density)
-        # self.assertEqual(density.Name, "Density")
-        # self.assertEqual(density.UUID, "454661e5-265b-4320-8e6f-fcf6223ac3af")
-        # self.assertIn("Density", density.Properties)
-        # prop = density.Properties["Density"]
-        # self.assertIn("Description", dir(prop))
-        # self.assertIn("Name", dir(prop))
-        # self.assertIn("Type", dir(prop))
-        # self.assertIn("URL", dir(prop))
-        # self.assertIn("Units", dir(prop))
-        # self.assertEqual(prop.Name, "Density")
+        density = self.ModelManager.getModel("454661e5-265b-4320-8e6f-fcf6223ac3af")
+        self.assertIsNotNone(density)
+        self.assertEqual(density.Name, "Density")
+        self.assertEqual(density.UUID, "454661e5-265b-4320-8e6f-fcf6223ac3af")
+        self.assertIn("Density", density.Properties)
+        prop = density.Properties["Density"]
+        self.assertIn("Description", dir(prop))
+        self.assertIn("Name", dir(prop))
+        self.assertIn("Type", dir(prop))
+        self.assertIn("URL", dir(prop))
+        self.assertIn("Units", dir(prop))
+        self.assertEqual(prop.Name, "Density")
 
     def testCalculiXSteel(self):
         steel = self.MaterialManager.getMaterial("92589471-a6cb-4bbc-b748-d425a17dea7d")
