51cd6d61ac8b9ffb4fccac6022738de563d3a19f
David Carter <dcarter@davidcarter.ca>
2023-09-09 02:20:29 -0400

    Empty test file

--- tests/src/Mod/Material/App/Model.cpp
+++ tests/src/Mod/Material/App/Model.cpp
@@ -14,27 +14,27 @@
 #include <Mod/Material/App/ModelManager.h>
 
 // clang-format off
-TEST(Material, TestResources)
-{
-    try {
-        auto param = App::GetApplication().GetParameterGroupByPath(
-            "User parameter:BaseApp/Preferences/Mod/Material/Resources");
-        EXPECT_NE(param, nullptr);
-    }
-    catch (const std::exception &e)
-    {
-        FAIL() << "Exception: " << e.what() << "\n";
-    }
-}
+// TEST(Material, TestResources)
+// {
+//     try {
+//         auto param = App::GetApplication().GetParameterGroupByPath(
+//             "User parameter:BaseApp/Preferences/Mod/Material/Resources");
+//         EXPECT_NE(param, nullptr);
+//     }
+//     catch (const std::exception &e)
+//     {
+//         FAIL() << "Exception: " << e.what() << "\n";
+//     }
+// }
 
-TEST(Material, TestModelLoad)
-{
-    auto manager = Materials::ModelManager::getManager();
-    EXPECT_NE(manager, nullptr);
-    auto density = manager->getModel(QString::fromStdString("454661e5-265b-4320-8e6f-fcf6223ac3af"));
-    // EXPECT_NE(density, nullptr);
-    EXPECT_NE(density.getName(), QString::fromStdString("density"));
-    EXPECT_EQ(density.getUUID(), QString::fromStdString("454661e5-265b-4320-8e6f-fcf6223ac3af"));
-}
+// TEST(Material, TestModelLoad)
+// {
+//     auto manager = Materials::ModelManager::getManager();
+//     EXPECT_NE(manager, nullptr);
+//     auto density = manager->getModel(QString::fromStdString("454661e5-265b-4320-8e6f-fcf6223ac3af"));
+//     // EXPECT_NE(density, nullptr);
+//     EXPECT_NE(density.getName(), QString::fromStdString("density"));
+//     EXPECT_EQ(density.getUUID(), QString::fromStdString("454661e5-265b-4320-8e6f-fcf6223ac3af"));
+// }
 
 // clang-format on
