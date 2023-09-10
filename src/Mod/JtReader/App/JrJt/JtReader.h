6a29122b041660b86b09d4558b1ddee5483e6132
wmayer <wmayer@users.sourceforge.net>
2023-09-10 11:17:04 +0200

    JtReader: Apply clang format

--- src/Mod/JtReader/App/JrJt/JtReader.h
+++ src/Mod/JtReader/App/JrJt/JtReader.h
@@ -1,33 +1,30 @@
 #pragma once
 
+#include "TOC_Entry.h"
 #include <string>
 #include <vector>
-#include "TOC_Entry.h"
 
 class LodHandler;
-
 
 
 class JtReader
 {
 public:
-	JtReader();
-	~JtReader();
+    JtReader();
+    ~JtReader();
 
-	void setFile(const std::string fileName);
+    void setFile(const std::string fileName);
 
-	const std::vector<TOC_Entry>& readToc();
+    const std::vector<TOC_Entry>& readToc();
 
-	void readSegment(int tocIndex);
+    void readSegment(int tocIndex);
 
-	void readLodSegment(const TOC_Entry&, LodHandler&);
+    void readLodSegment(const TOC_Entry&, LodHandler&);
 
-	static const GUID TriStripSetShapeLODElement_ID;
+    static const GUID TriStripSetShapeLODElement_ID;
 
 
 protected:
-	std::string _fileName;
-	vector<TOC_Entry> TocEntries;
-
+    std::string _fileName;
+    vector<TOC_Entry> TocEntries;
 };
-
