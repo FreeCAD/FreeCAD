5edb1b024328cfe53bff5c67d7e0d8f21a845405
Paddle <PaddleStroke@users.noreply.github.com>
2023-09-20 12:23:25 +0200

    Add "Std_LinkSelectActions" to the view menu. Remove "Std_LinkSelectActions" from the toolbar. Remove "Std_SelBack" & "Std_SelForward" from the toolbar Remove "Std_SelBoundingBox" from the toolbar Reorder the toolbar entries.

--- src/Gui/Workbench.cpp
+++ src/Gui/Workbench.cpp
@@ -690,6 +690,7 @@ MenuItem* StdWorkbench::setupMenuBar() const
           << "Std_ToggleNavigation"
           << "Std_SetAppearance" << "Std_RandomColor" << "Separator"
           << "Std_Workbench" << "Std_ToolBarMenu" << "Std_DockViewMenu" << "Separator"
+          << "Std_LinkSelectActions"
           << "Std_TreeViewActions"
           << "Std_ViewStatusBar";
 
@@ -791,11 +792,11 @@ ToolBarItem* StdWorkbench::setupToolBars() const
     // View
     auto view = new ToolBarItem( root );
     view->setCommand("View");
-    *view << "Std_ViewFitAll" << "Std_ViewFitSelection" << "Std_DrawStyle" << "Std_SelBoundingBox"
-          << "Separator" << "Std_SelectFilter" << "Std_SelBack" << "Std_SelForward"
-          << "Std_LinkSelectActions"<< "Separator" << "Std_TreeViewActions" << "Std_ViewIsometric"
-          << "Std_ViewFront"<< "Std_ViewTop" << "Std_ViewRight" << "Separator" << "Std_ViewRear"
-          << "Separator" << "Std_ViewBottom"<< "Std_ViewLeft"  << "Separator" << "Std_MeasureDistance";
+    *view << "Std_ViewFitAll" << "Std_ViewFitSelection" << "Std_ViewIsometric"
+          << "Std_ViewFront"<< "Std_ViewTop" << "Std_ViewRight"
+          << "Std_ViewRear" << "Std_ViewBottom"<< "Std_ViewLeft"
+          << "Separator" << "Std_DrawStyle" << "Std_SelectFilter" << "Std_TreeViewActions"
+          << "Separator" << "Std_MeasureDistance";
 
     // Structure
     auto structure = new ToolBarItem( root );
