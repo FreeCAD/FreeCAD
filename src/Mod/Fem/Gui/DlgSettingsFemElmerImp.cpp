836c237d3c40d925a5447d1f52841cbb9c25986f
wmayer <wmayer@users.sourceforge.net>
2023-09-25 12:58:37 +0200

    Fem: Apply clang-format

--- src/Mod/Fem/Gui/DlgSettingsFemElmerImp.cpp
+++ src/Mod/Fem/Gui/DlgSettingsFemElmerImp.cpp
@@ -106,7 +106,7 @@ void DlgSettingsFemElmerImp::onfileNameChanged(QString FileName)
     if (!QFileInfo::exists(FileName)) {
         QMessageBox::critical(this,
                               tr("File does not exist"),
-                              tr("The specified executable \n'%1'\n does not exist!\n"
+                              tr("The specified executable\n'%1'\n does not exist!\n"
                                  "Specify another file please.")
                                   .arg(FileName));
     }
