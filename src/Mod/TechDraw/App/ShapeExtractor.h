2eec7f0bca61dca46a87d0bb1b1c02b7a0dcd05b
wandererfan <wandererfan@gmail.com>
2023-10-02 11:01:02 -0400

    [TD]fix sketch location in view

--- src/Mod/TechDraw/App/ShapeExtractor.h
+++ src/Mod/TechDraw/App/ShapeExtractor.h
@@ -50,10 +50,12 @@ public:
     static bool isEdgeType(App::DocumentObject* obj);
     static bool isPointType(App::DocumentObject* obj);
     static bool isDraftPoint(App::DocumentObject* obj);
-    static Base::Vector3d getLocation3dFromFeat(App::DocumentObject* obj);
+    static Base::Vector3d getLocation3dFromFeat(App::DocumentObject *obj);
     static bool prefAdd2d();
 
     static TopoDS_Shape stripInfiniteShapes(TopoDS_Shape inShape);
+
+    static TopoDS_Shape getLocatedShape(const App::DocumentObject* docObj);
 
 protected:
 
