f6d639d807ec9e98a94e03c6bbaf98ba7f0db20c
Florian Foinant-Willig <flachyjoe@gmail.com>
2023-10-08 23:02:52 +0200

    Core: fix a compiler warning
    src/App/ExtensionContainer.h:188:10: warning: 'Restore' overrides a
    member function but is not marked 'override'
    [-Winconsistent-missing-override]
    void Restore(Base::DocumentReader&
    reader,XERCES_CPP_NAMESPACE_QUALIFIER DOMElement *containerEl);
             ^
    src/App/PropertyContainer.h:233:8: note: overridden virtual function is
    here
    void Restore(Base::DocumentReader &reader,
    XERCES_CPP_NAMESPACE_QUALIFIER DOMElement *containerEl) override;
           ^
    1 warning generated.

--- src/App/ExtensionContainer.h
+++ src/App/ExtensionContainer.h
@@ -185,7 +185,7 @@ public:
 
     void Save(Base::Writer& writer) const override;
     void Restore(Base::XMLReader& reader) override;
-    void Restore(Base::DocumentReader& reader,XERCES_CPP_NAMESPACE_QUALIFIER DOMElement *containerEl);
+    void Restore(Base::DocumentReader& reader,XERCES_CPP_NAMESPACE_QUALIFIER DOMElement *containerEl) override;
 
     //those methods save/restore the dynamic extensions without handling properties, which is something
     //done by the default Save/Restore methods.
