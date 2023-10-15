c53e4fb56c2dd1c1801a5772dd83f7f960881d5b
Roy-043 <info@b-k-g.nl>
2023-10-15 14:26:49 +0200

    Typo

--- src/Mod/Draft/WorkingPlane.py
+++ src/Mod/Draft/WorkingPlane.py
@@ -1702,7 +1702,7 @@ class PlaneGui(PlaneBase):
         self._history["idx"] = len(self._history["data_list"]) - 1
 
     def _update_old_plane(self):
-        """ Update the old DraftWorkingPlane for compatiblity.
+        """ Update the old DraftWorkingPlane for compatibility.
         The tracker and snapper code currently still depend on it.
         """
         if not hasattr(FreeCAD, "DraftWorkingPlane"):
