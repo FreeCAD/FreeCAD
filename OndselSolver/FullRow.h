e135d26163e3327a0d70c691eb24d120dfa5f144
Aik-Siong Koh <askoh@askoh.com>
2023-11-14 16:32:42 -0700

    Windows version running.

--- OndselSolver/FullRow.h
+++ OndselSolver/FullRow.h
@@ -172,37 +172,6 @@ namespace MbD {
 		return answer;
 	}
 	template<typename T>
-	inline std::shared_ptr<FullRow<T>> FullRow<T>::clonesptr()
-	{
-		return std::make_shared<FullRow<T>>(*this);
-	}
-	template<typename T>
-	inline double FullRow<T>::dot(std::shared_ptr<FullVector<T>> vec)
-	{
-		int n = (int)this->size();
-		double answer = 0.0;
-		for (int i = 0; i < n; i++) {
-			answer += this->at(i) * vec->at(i);
-		}
-		return answer;
-	}
-	template<typename T>
-	inline std::shared_ptr<FullVector<T>> FullRow<T>::dot(std::shared_ptr<std::vector<std::shared_ptr<FullColumn<T>>>> vecvec)
-	{
-		auto ncol = (int)this->size();
-		auto nelem = vecvec->at(0)->size();
-		auto answer = std::make_shared<FullVector<T>>(nelem);
-		for (int k = 0; k < nelem; k++) {
-			auto sum = 0.0;
-			for (int i = 0; i < ncol; i++)
-			{
-				sum += this->at(i) * vecvec->at(i)->at(k);
-			}
-			answer->at(k) = sum;
-		}
-		return answer;
-	}
-	template<typename T>
 	inline std::ostream& FullRow<T>::printOn(std::ostream& s) const
 	{
 		s << "FullRow{";
