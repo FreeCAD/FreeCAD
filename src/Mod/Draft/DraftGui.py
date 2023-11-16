daea162ae28edc98154a4fd44431a9ae69605063
Roy-043 <info@b-k-g.nl>
2023-11-16 11:40:23 +0100

    Draft: prefs: remove copymode, useSupport no longer via UI
    This PR removes the copymode (Global copy mode) preference.
    
    Forum topic:
    https://forum.freecad.org/viewtopic.php?t=82679
    
    Additionally useSupport is removed from preferences-draft.ui, turning it into a fine-tuning preference. Use of this feature is rare and may not be supported in the future. No need to present this preference to new users in the UI.
    
    Forum topic:
    https://forum.freecad.org/viewtopic.php?t=80689

--- src/Mod/Draft/DraftGui.py
+++ src/Mod/Draft/DraftGui.py
@@ -944,8 +944,7 @@ class DraftToolBar:
         self.isCopy.show()
         self.isSubelementMode.show()
         p = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Mod/Draft")
-        if p.GetBool("copymode",False):
-            self.isCopy.setChecked(p.GetBool("copymodeValue",False))
+        self.isCopy.setChecked(p.GetBool("copymodeValue",False))
         self.continueCmd.show()
 
     def checkLocal(self):
@@ -996,11 +995,11 @@ class DraftToolBar:
 
     def setCopymode(self,val=0):
         p = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Mod/Draft")
-        p.SetBool("copymodeValue",bool(val))
         # special value for offset command
-        if self.sourceCmd:
-            if self.sourceCmd.featureName == "Offset":
-                p.SetBool("OffsetCopyMode",bool(val))
+        if self.sourceCmd and self.sourceCmd.featureName == "Offset":
+            p.SetBool("OffsetCopyMode",bool(val))
+        else:
+            p.SetBool("copymodeValue",bool(val))
 
     def setSubelementMode(self):
         self.sourceCmd.set_ghosts()
