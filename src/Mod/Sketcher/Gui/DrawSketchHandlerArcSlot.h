5969e171c6023d5f447aa551a4cef7d437be3c07
Paddle <PaddleStroke@users.noreply.github.com>
2023-11-17 10:00:47 +0100

    Fix autoconstraints of arcslot tool.

--- src/Mod/Sketcher/Gui/DrawSketchHandlerArcSlot.h
+++ src/Mod/Sketcher/Gui/DrawSketchHandlerArcSlot.h
@@ -204,9 +204,11 @@ private:
 
         if (constructionMethod() == ConstructionMethod::ArcSlot) {
             generateAutoConstraintsOnElement(ac2,
+                                             getHighestCurveIndex() - 2,
+                                             Sketcher::PointPos::mid);
+            generateAutoConstraintsOnElement(ac3,
                                              getHighestCurveIndex() - 1,
                                              Sketcher::PointPos::mid);
-            generateAutoConstraintsOnElement(ac3, getHighestCurveIndex(), Sketcher::PointPos::mid);
         }
         else {
             generateAutoConstraintsOnElement(ac2,
