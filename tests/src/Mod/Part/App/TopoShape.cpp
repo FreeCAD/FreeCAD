3d6dd870461bb5748a2309f0e877dfafd0123f23
John Dupuy <jdupuy98@gmail.com>
2023-11-20 09:34:15 -0600

    testing elements of TopoShape

--- tests/src/Mod/Part/App/TopoShape.cpp
+++ tests/src/Mod/Part/App/TopoShape.cpp
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
+#include <BRepPrimAPI_MakeBox.hxx>
+#include <TopoDS.hxx>
+#include "App/ComplexGeoData.h"
 #include "gtest/gtest.h"
 #include "Mod/Part/App/TopoShape.h"
 
@@ -15,7 +18,11 @@ protected:
         _declaredLoc1234.SetScale(gp_Pnt(1.0, 2.0, 3.0), 4.0);
         _testLocation1234 = TopLoc_Location(_declaredLoc1234);
 
-        _topoShape = Part::TopoShape();
+        gp_Pnt lowerLeftCornerOfBox(1.0, 2.0, 2.0);
+        BRepPrimAPI_MakeBox boxMaker(lowerLeftCornerOfBox, 10, 11, 12);
+        auto box = boxMaker.Shape();
+
+        _topoShape = Part::TopoShape(box);
     }
 
     void TearDown() override {
@@ -26,7 +33,6 @@ public:
     TopLoc_Location _testLocation1234;
     gp_Trsf _declaredLoc1234;
 };
-
 
 TEST_F(TopoShapeTest, setShapeGetShape)
 {
@@ -43,3 +49,68 @@ TEST_F(TopoShapeTest, setShapeGetShape)
     EXPECT_EQ(shapeSeen.Location().Transformation().ScaleFactor(), 4.0);
 }
 
+TEST_F(TopoShapeTest, impliedNamingOfSelf)
+{
+    // Arrange
+    auto silent = false;
+
+    // Act
+    auto generalName = _topoShape.shapeName(silent);
+    auto typeAndIndex = _topoShape.shapeTypeAndIndex("Solid");
+
+    // Assert
+    EXPECT_EQ(generalName, "Solid");
+    EXPECT_EQ(typeAndIndex.first, TopAbs_SOLID);
+    EXPECT_EQ(typeAndIndex.second, 0);
+}
+
+TEST_F(TopoShapeTest, pointsAndFaces)
+{
+    // Arrange
+    std::vector<Base::Vector3d> points;
+    std::vector<Data::ComplexGeoData::Facet> faces;
+    double accuracy(0.0);
+
+    // Act
+    _topoShape.getFaces(points, faces, accuracy); // box at 1,2,3 with dimensions 10,11,12
+
+    // Assert
+    auto thirdPoint = points[2];
+    auto thirdFace = faces[2];
+    EXPECT_EQ(points.size(), 8);
+    EXPECT_EQ(thirdPoint.x, 1);
+    EXPECT_EQ(thirdPoint.y, 11+2);
+    EXPECT_EQ(thirdPoint.z, 2);
+    EXPECT_EQ(faces.size(), 12);
+    EXPECT_EQ(thirdFace.I1, 4);
+    EXPECT_EQ(thirdFace.I2, 5);
+    EXPECT_EQ(thirdFace.I3, 6);
+}
+
+TEST_F(TopoShapeTest, faceNaming)
+{
+    // Arrange
+    std::vector<Base::Vector3d> points;
+    std::vector<Data::ComplexGeoData::Facet> faces;
+    double accuracy(0.0);
+    std::string face = "Face"; // force to string to enable proper std:find matching
+    std::string edge = "Edge";
+    std::string vertex = "Vertex";
+    _topoShape.getFaces(points, faces, accuracy); // box at 1,2,3 with dimensions 10,11,12
+
+    // Act
+    auto elementTypes = _topoShape.getElementTypes();
+    auto faceSubShapes = _topoShape.getSubShapes(TopAbs_FACE);
+    auto thirdFaceByType = faceSubShapes[2];
+
+    // Assert
+//    auto thirdPoint = points[2];
+    EXPECT_TRUE(
+        std::find(elementTypes.begin(), elementTypes.end(), face) != elementTypes.end());
+    EXPECT_TRUE(
+        std::find(elementTypes.begin(), elementTypes.end(), edge) != elementTypes.end());
+    EXPECT_TRUE(
+        std::find(elementTypes.begin(), elementTypes.end(), vertex) != elementTypes.end());
+    EXPECT_EQ(faceSubShapes.size(), 6);
+    EXPECT_EQ(faces.size(), 12);  // why is this twelve???
+}
