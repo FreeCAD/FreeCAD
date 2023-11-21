6fb9a5b271f4a9b507cc06e4e642227b13e95863
wmayer <wmayer@users.sourceforge.net>
2023-11-21 19:04:21 +0100

    Qt: define Q_DISABLE_COPY_MOVE only if needed

---
+++ src/QtCore.h.cmake
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+/***************************************************************************
+ *   Copyright (c) 2023 Werner Mayer <wmayer[at]users.sourceforge.net>     *
+ *                                                                         *
+ *   This file is part of FreeCAD.                                         *
+ *                                                                         *
+ *   FreeCAD is free software: you can redistribute it and/or modify it    *
+ *   under the terms of the GNU Lesser General Public License as           *
+ *   published by the Free Software Foundation, either version 2.1 of the  *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   FreeCAD is distributed in the hope that it will be useful, but        *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with FreeCAD. If not, see                               *
+ *   <https://www.gnu.org/licenses/>.                                      *
+ *                                                                         *
+ **************************************************************************/
+
+
+#ifndef FREECAD_QTCORE_H
+#define FREECAD_QTCORE_H
+
+/* QtCore.h.  Generated from QtCore.h.cmake by cmake.  */
+
+/* Define to 1 Qt provides the macro Q_DISABLE_COPY_MOVE. */
+#cmakedefine HAVE_Q_DISABLE_COPY_MOVE
+
+#endif  // FREECAD_QTCORE_H
