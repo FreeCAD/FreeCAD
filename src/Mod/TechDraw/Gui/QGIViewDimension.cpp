54da0413926494f221d7ce3d6bdf2fa756dc2309
wandererfan <wandererfan@gmail.com>
2023-11-24 09:39:08 -0500

    [TD]fix dimension location

--- src/Mod/TechDraw/Gui/QGIViewDimension.cpp
+++ src/Mod/TechDraw/Gui/QGIViewDimension.cpp
@@ -513,6 +513,14 @@ QVariant QGIViewDimension::itemChange(GraphicsItemChange change, const QVariant&
             datumLabel->setSelected(false);
         }
         draw();
+        return value;
+    }
+    if(change == ItemPositionChange && scene()) {
+        // QGIVDimension doesn't really change position the way other views do.
+        // If we call QGIView::itemChange it will set the position to (0,0) instead of
+        // using the label's position, and the Dimension will be in the wrong place.
+        // QGIVBalloon behaves the same way.
+        return QGraphicsItem::itemChange(change, value);
     }
     return QGIView::itemChange(change, value);
 }
