b1a96bde5ff66adbb371cdbf58f470b60b9af8f4
bgbsww <120601209+bgbsww@users.noreply.github.com>
2023-11-29 15:15:59 -0500

    Don't apply the transformations recursively, just make sure the right one is applied to the top subobject (#11539)

--- src/Mod/Mesh/App/Exporter.cpp
+++ src/Mod/Mesh/App/Exporter.cpp
@@ -107,7 +107,7 @@ int Exporter::addObject(App::DocumentObject* obj, float tol)
     for (std::string& sub : expandSubObjectNames(obj, subObjectNameCache, 0)) {
         Base::Matrix4D matrix;
         auto sobj = obj->getSubObject(sub.c_str(), nullptr, &matrix);
-        auto linked = sobj->getLinkedObject(true, &matrix, true);
+        auto linked = sobj->getLinkedObject(true, &matrix, false);
         auto it = meshCache.find(linked);
         if (it == meshCache.end()) {
             if (linked->isDerivedFrom(Mesh::Feature::getClassTypeId())) {
