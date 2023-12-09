a54b99678234a441f82b94545d59a2a998517dd0
bgbsww <bgbsww@gmail.com>
2023-12-09 15:38:46 -0500

    Possible fix for 10482 circular reference regression with tests

--- src/Mod/Sketcher/SketcherTests/TestSketchExpression.py
+++ src/Mod/Sketcher/SketcherTests/TestSketchExpression.py
@@ -57,11 +57,37 @@ class TestSketchExpression(unittest.TestCase):
         sketch.addConstraint(conList)
         del geoList, conList
 
+        # Check that circular references get detected
         length = sketch.addConstraint(Sketcher.Constraint("Distance", 0, 6.0))
         height = sketch.addConstraint(Sketcher.Constraint("Distance", 3, 5.0))
-
         sketch.renameConstraint(length, "Length")
-        sketch.setExpression("Constraints[{}]".format(height), ".Constraints.Length")
+        sketch.renameConstraint(height, "Height")
+
+        sketch.setExpression("Constraints[{}]".format(height), ".Constraints.Length * 2")
+
+        with self.assertRaises(RuntimeError) as context:
+            sketch.setExpression("Constraints.Length", ".Constraints.Length * 2")
+        assert ".Constraints.Length reference creates a cyclic dependency." in str(context.exception)
+
+        with self.assertRaises(RuntimeError) as context:
+            sketch.setExpression("Constraints.Length", ".Constraints.Height * 2")
+        assert ".Constraints.Length reference creates a cyclic dependency." in str(context.exception)
+
+        cube = self.Doc.addObject("Part::Box","Cube")
+        cube.setExpression('Length', 'Width + 10mm')
+        with self.assertRaises(RuntimeError) as context:
+            cube.setExpression('Width', 'Length + 10mm')
+        assert "Width reference creates a cyclic dependency." in str(context.exception)
+
+        cube.setExpression('.Placement.Base.x', '.Placement.Base.y + 10mm')
+        with self.assertRaises(RuntimeError) as context:
+            cube.setExpression('.Placement.Base.y', '.Placement.Base.x + 10mm')
+        assert ".Placement.Base.y reference creates a cyclic dependency." in str(context.exception)
+
+        cube.recompute()
+        v1 = cube.Placement.Base
+        cube.recompute()
+        assert cube.Placement.Base.isEqual(v1,1e-6)
 
     def tearDown(self):
         # comment out to omit closing document for debugging
