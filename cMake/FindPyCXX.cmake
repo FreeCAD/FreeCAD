4093bb2fad38806c11910ebd5c7c57089ef09943
mosfet80 <realeandrea@yahoo.it>
2024-01-07 15:56:33 +0100

    Update FindPyCXX.cmake
    removed old pcxx V6.2.x API
    Update FindPyCXX.cmake

--- cMake/FindPyCXX.cmake
+++ cMake/FindPyCXX.cmake
@@ -116,12 +116,9 @@ if(PYCXX_FOUND)
         ${PYCXX_SOURCE_DIR}/cxx_extensions.cxx
         ${PYCXX_SOURCE_DIR}/cxxsupport.cxx
         ${PYCXX_SOURCE_DIR}/IndirectPythonInterface.cxx
+        ${PYCXX_SOURCE_DIR}/cxx_exceptions.cxx
     )
-    if(NOT ${PYCXX_VERSION} VERSION_LESS 6.3.0)
-        list(APPEND PYCXX_SOURCES
-            ${PYCXX_SOURCE_DIR}/cxx_exceptions.cxx)
-        add_definitions(-DPYCXX_6_2_COMPATIBILITY)
-    endif()
+
 else(PYCXX_FOUND)
     MESSAGE(STATUS "PyCXX not found")
 endif(PYCXX_FOUND)
