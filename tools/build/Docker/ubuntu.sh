d51d2f674fb01bf6d958799919614cac8782311f
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2024-01-08 17:49:38 +0100

    Remove code related to qtxmlpatterns (#11755)

--- tools/build/Docker/ubuntu.sh
+++ tools/build/Docker/ubuntu.sh
@@ -8,7 +8,7 @@ apt-get install --no-install-recommends --yes build-essential cmake doxygen git 
     libocct-draw-dev libocct-foundation-dev libocct-modeling-algorithms-dev \
     libocct-modeling-data-dev libocct-ocaf-dev libocct-visualization-dev \
     libopencv-dev libproj-dev libpyside2-dev libqt5svg5-dev libqt5webkit5-dev \
-    libqt5xmlpatterns5-dev libshiboken2-dev libvtk9-dev libvtk9-qt-dev \
+    libshiboken2-dev libvtk9-dev libvtk9-qt-dev \
     libvtk-dicom-dev libx11-dev libxerces-c-dev libxmu-dev libxmuu-dev \
     libzipios++-dev netgen netgen-headers pyside2-tools python3-dev \
     python3-matplotlib python3-pivy python3-ply python3-pyside2.qtsvg \
