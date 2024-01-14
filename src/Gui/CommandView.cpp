9728131e39e5df3f6d1ec275bad605c4a14ac626
hlorus <david@friedli-be.ch>
2024-01-14 21:43:24 +0100

    [Gui] Add command and task for unified measurement facility

--- src/Gui/CommandView.cpp
+++ src/Gui/CommandView.cpp
@@ -76,6 +76,8 @@
 #include "SelectionObject.h"
 #include "SoAxisCrossKit.h"
 #include "SoFCOffscreenRenderer.h"
+#include "SoFCUnifiedSelection.h"
+#include "TaskMeasure.h"
 #include "TextureMapping.h"
 #include "Tools.h"
 #include "Tree.h"
@@ -3297,6 +3299,38 @@ bool StdCmdMeasureDistance::isActive()
 }
 
 //===========================================================================
+// Std_Measure
+// this is the Unified Measurement Facility Measure command
+//===========================================================================
+
+
+DEF_STD_CMD_A(StdCmdMeasure)
+
+StdCmdMeasure::StdCmdMeasure()
+  :Command("Std_Measure")
+{
+    sGroup        = "Measure";
+    sMenuText     = QT_TR_NOOP("&Measure");
+    sToolTipText  = QT_TR_NOOP("Measure a feature (UMF)");
+    sWhatsThis    = "Std_Measure";
+    sStatusTip    = QT_TR_NOOP("Measure a feature");
+    sPixmap       = "umf-measurement";
+}
+
+void StdCmdMeasure::activated(int iMsg)
+{
+    Q_UNUSED(iMsg);
+
+    TaskMeasure *task = new TaskMeasure();
+    Gui::Control().showDialog(task);
+}
+
+
+bool StdCmdMeasure::isActive(){
+    return true;
+}
+
+//===========================================================================
 // Std_SceneInspector
 //===========================================================================
 
@@ -4224,6 +4258,7 @@ void CreateViewStdCommands()
     rcCmdMgr.addCommand(new StdCmdTreeCollapse());
     rcCmdMgr.addCommand(new StdCmdTreeSelectAllInstances());
     rcCmdMgr.addCommand(new StdCmdMeasureDistance());
+    rcCmdMgr.addCommand(new StdCmdMeasure());
     rcCmdMgr.addCommand(new StdCmdSceneInspector());
     rcCmdMgr.addCommand(new StdCmdTextureMapping());
     rcCmdMgr.addCommand(new StdCmdDemoMode());
