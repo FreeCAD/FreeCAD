7ae8c1694e8db242f480a777d77d70a4930beffc
LemonBoy <thatlemon@gmail.com>
2024-02-12 20:12:45 +0100

    fixup! Be more specific in error messages

--- src/Mod/Part/App/Attacher.cpp
+++ src/Mod/Part/App/Attacher.cpp
@@ -1890,9 +1890,7 @@ Base::Placement AttachEngineLine::calculateAttachedPlacement(const Base::Placeme
         case mm1Intersection:{
             if (shapes.size() < 2)
                 throw Base::ValueError("AttachEngineLine::calculateAttachedPlacement: Intersection mode requires two shapes; only one is supplied");
-            if (shapes[0]->IsNull())
-                throw Base::ValueError("Null shape in AttachEngineLine::calculateAttachedPlacement()!");
-            if (shapes[1]->IsNull())
+            if (shapes[0]->IsNull() || shapes[1]->IsNull())
                 throw Base::ValueError("Null shape in AttachEngineLine::calculateAttachedPlacement()!");
 
             const TopoDS_Face &face1 = TopoDS::Face(*(shapes[0]));
@@ -1902,14 +1900,17 @@ Base::Placement AttachEngineLine::calculateAttachedPlacement(const Base::Placeme
             Handle(Geom_Surface) hSurf2 = BRep_Tool::Surface(face2);
             GeomAPI_IntSS intersector(hSurf1, hSurf2, Precision::Confusion());
             if (!intersector.IsDone())
-                throw Base::ValueError("Intersection failed");
+                throw Base::ValueError("AttachEngineLine::calculateAttachedPlacement: Intersection failed");
 
-            if (intersector.NbLines() != 1)
-                throw Base::ValueError("Intersection failed");
+            const Standard_Integer intLines = intersector.NbLines();
+            if (intLines == 0)
+                throw Base::ValueError("AttachEngineLine::calculateAttachedPlacement: The two shapes don't intersect");
+            if (intLines != 1)
+                throw Base::ValueError("AttachEngineLine::calculateAttachedPlacement: Intersection is not a single curve");
 
             GeomAdaptor_Curve adapt(intersector.Line(1));
             if (adapt.GetType() != GeomAbs_Line)
-                throw Base::ValueError("Intersection failed, intersection is not a straight line");
+                throw Base::ValueError("AttachEngineLine::calculateAttachedPlacement: Intersection is not a straight line");
 
             LineBasePoint = adapt.Line().Location();
             LineDir = adapt.Line().Direction();
