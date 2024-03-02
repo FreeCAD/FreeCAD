2f6219a9c79e0f9d8ac52f75c3e305eabb1e34e6
FEA-eng <59876896+FEA-eng@users.noreply.github.com>
2024-03-02 17:03:44 +0100

    Update material_mechanicalnonlinear.py

--- src/Mod/Fem/femobjects/material_mechanicalnonlinear.py
+++ src/Mod/Fem/femobjects/material_mechanicalnonlinear.py
@@ -91,6 +91,11 @@ class MaterialMechanicalNonlinear(base_fempythonobject.BaseFemPythonObject):
             obj.MaterialModelNonlinearity = choices_nonlinear_material_models
             obj.MaterialModelNonlinearity = choices_nonlinear_material_models[0]
 
+        if hasattr(obj, "MaterialModelNonlinearity") and obj.MaterialModelNonlinearity == "simple hardening":
+            updated_choices_nonlinear_material_models = ["isotropic hardening", "kinematic hardening"]
+            obj.MaterialModelNonlinearity = updated_choices_nonlinear_material_models
+            obj.MaterialModelNonlinearity = updated_choices_nonlinear_material_models[0]
+
         if not hasattr(obj, "YieldPoints"):
             obj.addProperty(
                 "App::PropertyStringList",
