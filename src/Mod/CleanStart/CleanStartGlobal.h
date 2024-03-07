0b704ea7d5d83e9da5e1a1d000f7c2ca0dd5e76f
Chris Hennes <chennes@pioneerlibrarysystem.org>
2024-03-06 22:47:31 -0600

    Start: Recreate start as a QtWidgets-based app
    Removes all Start network access and dependency on Chromium for rendering. Written
    entirely in C++ (no HTML, CSS, JS, etc.).

---
+++ src/Mod/CleanStart/CleanStartGlobal.h
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+/****************************************************************************
+ *                                                                          *
+ *   Copyright (c) 2024 The FreeCAD Project Association AISBL               *
+ *                                                                          *
+ *   This file is part of FreeCAD.                                          *
+ *                                                                          *
+ *   FreeCAD is free software: you can redistribute it and/or modify it     *
+ *   under the terms of the GNU Lesser General Public License as            *
+ *   published by the Free Software Foundation, either version 2.1 of the   *
+ *   License, or (at your option) any later version.                        *
+ *                                                                          *
+ *   FreeCAD is distributed in the hope that it will be useful, but         *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU       *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU Lesser General Public       *
+ *   License along with FreeCAD. If not, see                                *
+ *   <https://www.gnu.org/licenses/>.                                       *
+ *                                                                          *
+ ***************************************************************************/
+
+#include <FCGlobal.h>
+
+#ifndef LAUNCHER_GLOBAL_H
+#define LAUNCHER_GLOBAL_H
+
+
+// CleanStart
+#ifndef CleanStartExport
+#ifdef CleanStart_EXPORTS
+#define CleanStartExport FREECAD_DECL_EXPORT
+#else
+#define CleanStartExport FREECAD_DECL_IMPORT
+#endif
+#endif
+
+// CleanStartGui
+#ifndef CleanStartGuiExport
+#ifdef CleanStartGui_EXPORTS
+#define CleanStartGuiExport FREECAD_DECL_EXPORT
+#else
+#define CleanStartGuiExport FREECAD_DECL_IMPORT
+#endif
+#endif
+
+#endif  // LAUNCHER_GLOBAL_H
