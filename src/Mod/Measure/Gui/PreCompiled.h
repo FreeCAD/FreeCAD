c5acc062df8543a49228f3d5f1012b1958337878
wandererfan <wandererfan@gmail.com>
2024-03-07 15:00:53 -0500

    [Meas]fix gui cmake and precompiled for local build on win

--- src/Mod/Measure/Gui/PreCompiled.h
+++ src/Mod/Measure/Gui/PreCompiled.h
@@ -20,23 +20,77 @@
  *                                                                         *
  ***************************************************************************/
 
-#ifndef APP_PRECOMPILED_H
-#define APP_PRECOMPILED_H
+#ifndef MEASUREGUI_PRECOMPILED_H
+#define MEASUREGUI_PRECOMPILED_H
 
 #include <FCConfig.h>
+
+#include <Mod/Measure/MeasureGlobal.h>
+
+// point at which warnings of overly long specifiers disabled (needed for VC6)
+#ifdef _MSC_VER
+# pragma warning( disable : 4251 )
+# pragma warning( disable : 4503 )
+# pragma warning( disable : 4786 )  // specifier longer then 255 chars
+# pragma warning( disable : 4273 )
+#endif
+
+#ifdef FC_OS_WIN32
+# ifndef NOMINMAX
+#  define NOMINMAX
+# endif
+# include <windows.h>
+#endif
 
 #ifdef _PreComp_
 
 // standard
-#include <stdexcept>
+#include <cfloat>
+#include <cmath>
 
 // STL
-#include <memory>
+#include <algorithm>
+#include <map>
 #include <sstream>
+#include <string>
+#include <vector>
 
-// Qt
-#include <QCoreApplication>
-#include <QTcpSocket>
+// OpenCasCade
+#include <Mod/Part/App/OpenCascadeAll.h>
+
+// Boost
+#include <boost/regex.hpp>
+#include <boost/algorithm/string/predicate.hpp>
+
+// GL
+// Include glext before QtAll/InventorAll
+#ifdef FC_OS_WIN32
+# include <GL/gl.h>
+# include <GL/glext.h>
+#else
+# ifdef FC_OS_MACOSX
+#  include <OpenGL/gl.h>
+#  include <OpenGL/glext.h>
+# else
+#  ifndef GL_GLEXT_PROTOTYPES
+#   define GL_GLEXT_PROTOTYPES 1
+#  endif
+#  include <GL/gl.h>
+#  include <GL/glext.h>
+# endif //FC_OS_MACOSX
+#endif //FC_OS_WIN32
+// Should come after glext.h to avoid warnings
+#include <Inventor/C/glue/gl.h>
+
+// Qt Toolkit
+#ifndef __QtAll__
+# include <Gui/QtAll.h>
+#endif
+
+// Inventor includes OpenGL
+#ifndef __InventorAll__
+# include <Gui/InventorAll.h>
+#endif
 
 #endif //_PreComp_
 
