231a1fbe16fa4026ddb39b2c7c1da9d64f878075
PaddleStroke <pierrelouis.boyer@gmail.com>
2024-03-13 14:25:11 +0100

    Core: Fix GreedySelection: clicking on empty space was clearing selection while greedyselection was active. Fixes #12884

--- src/Gui/SoFCUnifiedSelection.cpp
+++ src/Gui/SoFCUnifiedSelection.cpp
@@ -742,7 +742,8 @@ SoFCUnifiedSelection::handleEvent(SoHandleEventAction * action)
             // check to see if the mouse is over a geometry...
             auto infos = this->getPickedList(action,!Selection().needPickedList());
             bool greedySel = Gui::Selection().getSelectionStyle() == Gui::SelectionSingleton::SelectionStyle::GreedySelection;
-            if(setSelection(infos, event->wasCtrlDown() || greedySel))
+            greedySel = greedySel || event->wasCtrlDown();
+            if(setSelection(infos, greedySel) || greedySel)
                 action->setHandled();
         } // mouse release
     }
