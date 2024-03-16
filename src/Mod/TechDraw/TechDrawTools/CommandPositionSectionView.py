5c4bd457d888fd6e8dfd18a9743fd812f861f813
luzpaz <luzpaz@users.noreply.github.com>
2024-03-16 10:32:26 -0400

    TD: fix whitespace in CommandPositionSectionView.py
    Fix header whitespace

--- src/Mod/TechDraw/TechDrawTools/CommandPositionSectionView.py
+++ src/Mod/TechDraw/TechDrawTools/CommandPositionSectionView.py
@@ -1,5 +1,5 @@
 # ***************************************************************************
-# *   Copyright (c) 2023 edi <edi271@a1.net>               *
+# *   Copyright (c) 2023 edi <edi271@a1.net>                                *
 # *                                                                         *
 # *   This program is free software; you can redistribute it and/or modify  *
 # *   it under the terms of the GNU Lesser General Public License (LGPL)    *
