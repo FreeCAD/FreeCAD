655d5e4fbd71053ebf3e37345d5c33e9cbee9f52
wmayer <wmayer@users.sourceforge.net>
2024-03-20 17:01:05 +0100

    Base: enable user site-packages for Py >= 3.11
    For more details see: https://forum.freecad.org/viewtopic.php?p=748296#p748296

--- src/Base/Interpreter.cpp
+++ src/Base/Interpreter.cpp
@@ -592,6 +592,8 @@ void initInterpreter(int argc, char* argv[])
     PyStatus status;
     PyConfig config;
     PyConfig_InitIsolatedConfig(&config);
+    config.isolated = 0;
+    config.user_site_directory = 1;
 
     status = PyConfig_SetBytesArgv(&config, argc, argv);
     if (PyStatus_Exception(status)) {
