e577c86664a08515ffd74b5973424e059425f104
wmayer <wmayer@users.sourceforge.net>
2024-04-03 19:13:59 +0200

    Sketch: add command to context-menu to change value of constraint
    The edit datum dialog is opened by double-clicking on a dimensional constraint in a sketch. However, the double-clicking event doesn't work realiably on
    some systems. As a workaround this PR adds the command to the context-menu.
    
    For more details see the forum thread: https://forum.freecad.org/viewtopic.php?t=71137

--- src/Mod/Sketcher/Gui/ViewProviderSketch.cpp
+++ src/Mod/Sketcher/Gui/ViewProviderSketch.cpp
@@ -4119,6 +4119,9 @@ void ViewProviderSketch::generateContextMenu()
         }
         // context menu if only constraints are selected
         else if (selectedConstraints >= 1) {
+            if (selectedConstraints == 1) {
+                menu << "Sketcher_ChangeDimensionConstraint";
+            }
             menu << "Sketcher_ToggleDrivingConstraint"
                  << "Sketcher_ToggleActiveConstraint"
                  << "Sketcher_SelectElementsAssociatedWithConstraints"
