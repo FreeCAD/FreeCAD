0a6ff497deb06853dce16774dfa63fd1927d74e2
Yorik van Havre <yorik@uncreated.net>
2024-05-06 11:33:45 +0200

    BIM: Fixed resource contents

--- src/Mod/BIM/Resources/create_qrc.py
+++ src/Mod/BIM/Resources/create_qrc.py
@@ -5,7 +5,9 @@ for subdir in ["icons", "ui", "translations"]:
     subpath = os.path.join(cdir, subdir)
     for f in sorted(os.listdir(subpath)):
         if f not in ["Arch.ts", "BIM.ts"]:
-            txt += "        <file>" + subdir + "/" + f + "</file>\n"
+            ext = os.path.splitext(f)[1]
+            if ext.lower() in [".ts", ".qm", ".svg", ".ui"]:
+                txt += "        <file>" + subdir + "/" + f + "</file>\n"
 txt += "    </qresource>\n</RCC>\n"
 with open(os.path.join(cdir, "Arch.qrc"), "w") as resfile:
     resfile.write(txt)
