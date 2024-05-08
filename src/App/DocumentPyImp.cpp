03a371d6e37f4ce47f5ffed7d5b039c5866a7e05
wmayer <wmayer@users.sourceforge.net>
2024-05-08 23:40:27 +0200

    Core: Fix memory leaks with PySequence_GetItem

--- src/App/DocumentPyImp.cpp
+++ src/App/DocumentPyImp.cpp
@@ -70,14 +70,8 @@ PyObject*  DocumentPy::addProperty(PyObject *args, PyObject *kwd)
 
     // enum support
     auto* propEnum = dynamic_cast<App::PropertyEnumeration*>(prop);
-    if (propEnum) {
-        if (enumVals && PySequence_Check(enumVals)) {
-            std::vector<std::string> enumValsAsVector;
-            for (Py_ssize_t i = 0; i < PySequence_Length(enumVals); ++i) {
-                enumValsAsVector.emplace_back(PyUnicode_AsUTF8(PySequence_GetItem(enumVals,i)));
-            }
-            propEnum->setEnums(enumValsAsVector);
-        }
+    if (propEnum && enumVals) {
+        propEnum->setPyObject(enumVals);
     }
 
     return Py::new_reference_to(this);
