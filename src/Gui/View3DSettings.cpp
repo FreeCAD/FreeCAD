46df290976fb40c80b4eb7aa4cadd2a0910e95cf
Syres916 <46537884+Syres916@users.noreply.github.com>
2024-05-14 17:57:32 +0100

    [Gui] Fix Lint Warning

--- src/Gui/View3DSettings.cpp
+++ src/Gui/View3DSettings.cpp
@@ -70,8 +70,9 @@ int View3DSettings::stopAnimatingIfDeactivated() const
 void View3DSettings::applySettings()
 {
     // Check if Sketcher Edit Mode exited cleanly
-    auto sketcherEditLastExit = hGrp->GetInt("HeadlightIntensityExisting", 101);
-    if (sketcherEditLastExit != 101) {
+    int overMaxHeadlightIntensity = 101;
+    int sketcherEditLastExit = hGrp->GetInt("HeadlightIntensityExisting", overMaxHeadlightIntensity);
+    if (sketcherEditLastExit != overMaxHeadlightIntensity) {
         // must mean a seg fault or abnormal exit last time
         hGrp->SetInt("HeadlightIntensity", sketcherEditLastExit);
         hGrp->RemoveInt("HeadlightIntensityExisting");
