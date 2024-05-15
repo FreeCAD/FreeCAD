9c63f264da4cd15fca3f83af03808b2bca106fe8
Furgo <148809153+furgo16@users.noreply.github.com>
2024-05-15 22:36:54 +0200

    Update copyright notice

--- src/Mod/Draft/draftutils/grid_observer.py
+++ src/Mod/Draft/draftutils/grid_observer.py
@@ -1,6 +1,7 @@
 # ***************************************************************************
 # *   Copyright (c) 2009, 2010 Ken Cline <cline@frii.com>                   *
 # *   Copyright (c) 2023 FreeCAD Project Association                        *
+# *   Copyright (c) 2024 Syres                                              *
 # *                                                                         *
 # *   This program is free software; you can redistribute it and/or modify  *
 # *   it under the terms of the GNU Lesser General Public License (LGPL)    *
