33e4a5baede0b9bc92e1ef87ec478212a2a483e1
marioalexis84 <53127171+marioalexis84@users.noreply.github.com>
2024-05-20 13:00:48 -0300

    Fem: Improve constraint section print (#14046)
    Co-authored-by: Chris Hennes <chennes@pioneerlibrarysystem.org>

--- src/Mod/Fem/femobjects/constraint_sectionprint.py
+++ src/Mod/Fem/femobjects/constraint_sectionprint.py
@@ -31,6 +31,7 @@ __url__ = "https://www.freecad.org"
 
 from . import base_fempythonobject
 
+_PropHelper = base_fempythonobject._PropHelper
 
 class ConstraintSectionPrint(base_fempythonobject.BaseFemPythonObject):
     """
@@ -41,3 +42,30 @@ class ConstraintSectionPrint(base_fempythonobject.BaseFemPythonObject):
 
     def __init__(self, obj):
         super(ConstraintSectionPrint, self).__init__(obj)
+
+        for prop in self._get_properties():
+            prop.add_to_object(obj)
+
+
+    def _get_properties(self):
+        prop = []
+
+        prop.append(_PropHelper(
+            type  = "App::PropertyEnumeration",
+            name  = "Variable",
+            group = "Constraint Section Print",
+            doc   = "Set facial variable",
+            value = ["Section Force", "Heat Flux", "Drag Stress"]
+            )
+        )
+
+        return prop
+
+
+    def onDocumentRestored(self, obj):
+        # update old project with new properties
+        for prop in self._get_properties():
+            try:
+                obj.getPropertyByName(prop.name)
+            except:
+                prop.add_to_object(obj)
