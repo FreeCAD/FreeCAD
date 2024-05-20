cf788e345165cc70b648abdb6391a39e64b1a870
mosfet80+ <realeandrea@yahoo.it>
2024-05-20 21:22:37 +0200

    [mod:part] fix compile warning

--- src/Mod/Sketcher/App/SketchObject.cpp
+++ src/Mod/Sketcher/App/SketchObject.cpp
@@ -1368,7 +1368,6 @@ int SketchObject::movePoint(int GeoId, PointPos PosId, const Base::Vector3d& toP
     if (lastSolverStatus == 0) {
         std::vector<Part::Geometry*> geomlist = solvedSketch.extractGeometry();
         Geometry.setValues(geomlist);
-        // Constraints.acceptGeometry(getCompleteGeometry());
         for (std::vector<Part::Geometry*>::iterator it = geomlist.begin(); it != geomlist.end();
              ++it) {
             if (*it)
@@ -2884,9 +2883,6 @@ int SketchObject::fillet(int GeoId1, int GeoId2, const Base::Vector3d& refPnt1,
 
                 Base::Vector3d refp1 = curve1->pointAtParameter(refparam1);
                 Base::Vector3d refp2 = curve2->pointAtParameter(refparam2);
-
-                // Base::Console().Log("refpoints:
-                // (%f,%f,%f);(%f,%f,%f)",refp1.x,refp1.y,refp1.z,refp2.x,refp2.y,refp2.z);
 
                 Base::Vector3d normalintersect(
                     (-dir1.x * dir2.x * refp1.y + dir1.x * dir2.x * refp2.y
@@ -10920,7 +10916,6 @@ int SketchObject::port_reversedExternalArcs(bool justAnalyze)
 bool SketchObject::AutoLockTangencyAndPerpty(Constraint* cstr, bool bForce, bool bLock)
 {
     try {
-        // assert ( cstr->Type == Tangent  ||  cstr->Type == Perpendicular);
         /*tangency type already set. If not bForce - don't touch.*/
         if (cstr->getValue() != 0.0 && !bForce)
             return true;
