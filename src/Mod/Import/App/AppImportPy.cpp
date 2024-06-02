03b331ffce7a0f978e71209c6a0cb700c3f96696
Kuzma30 <kuzemkoa@gmail.com>
2024-06-02 12:48:01 +0000

    Fix error on Ubuntu 20-04

--- src/Mod/Import/App/AppImportPy.cpp
+++ src/Mod/Import/App/AppImportPy.cpp
@@ -157,7 +157,9 @@ private:
             hApp->NewDocument(TCollection_ExtendedString("MDTV-CAF"), hDoc);
 
             if (file.hasExtension({"stp", "step"})) {
+#if OCC_VERSION_HEX >= 0x070800
                 Resource_FormatType cp = Resource_FormatType_UTF8;
+#endif
                 try {
                     Import::ReaderStep reader(file);
 #if OCC_VERSION_HEX < 0x070800
