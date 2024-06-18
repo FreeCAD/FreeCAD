287da804de712157a1c0f3d52471e1b97d5edace
lyphrowny <strukov.as@edu.spbstu.ru>
2024-06-18 13:31:29 +0300

    FEM: add tests for parse_diff in FemApp test suite

--- src/Mod/Fem/TestFemApp.py
+++ src/Mod/Fem/TestFemApp.py
@@ -1,26 +1,22 @@
-# ***************************************************************************
-# *   Copyright (c) 2018 Przemo Firszt <przemo@firszt.eu>                   *
-# *   Copyright (c) 2018 Bernd Hahnebach <bernd@bimstatik.org>              *
-# *                                                                         *
-# *   This file is part of the FreeCAD CAx development system.              *
-# *                                                                         *
-# *   This program is free software; you can redistribute it and/or modify  *
-# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
-# *   as published by the Free Software Foundation; either version 2 of     *
-# *   the License, or (at your option) any later version.                   *
-# *   for detail see the LICENCE text file.                                 *
-# *                                                                         *
-# *   This program is distributed in the hope that it will be useful,       *
-# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
-# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
-# *   GNU Library General Public License for more details.                  *
-# *                                                                         *
-# *   You should have received a copy of the GNU Library General Public     *
-# *   License along with this program; if not, write to the Free Software   *
-# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
-# *   USA                                                                   *
-# *                                                                         *
-# ***************************************************************************
+# *************************************************************************
+#  Copyright (c) 2018-2024 lyphrowny                                      *
+#                                                                         *
+#  This file is part of the FreeCAD development system.                   *
+#                                                                         *
+#  This program is free software; you can redistribute it and/or modify   *
+#  it under the terms of the GNU Lesser General Public License (LGPL)     *
+#  as published by the Free Software Foundation; either version 2 of      *
+#  the License, or (at your option) any later version.                    *
+#  for detail see the LICENCE text file.                                  *
+#                                                                         *
+#  This program is distributed in the hope that it will be useful,        *
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+#  GNU Library General Public License for more details.                   *
+#                                                                         *
+#  You should have received a copy of the GNU General Public License      *
+#  along with this program. If not, see <https://www.gnu.org/licenses/>.  *
+# *************************************************************************
 
 # Unit test for the FEM module
 # to get the right order import as is used
@@ -38,6 +34,8 @@ from femtest.app.test_ccxtools import TestCcxTools as FemTest11
 from femtest.app.test_solver_elmer import TestSolverElmer as FemTest13
 from femtest.app.test_solver_z88 import TestSolverZ88 as FemTest14
 
+from femtest.function_tests.test_support_utils import TestParseDiff as FemTest15
+
 # dummy usage to get flake8 and lgtm quiet
 False if FemTest01.__name__ else True
 False if FemTest02.__name__ else True
@@ -52,3 +50,4 @@ False if FemTest10.__name__ else True
 False if FemTest11.__name__ else True
 False if FemTest13.__name__ else True
 False if FemTest14.__name__ else True
+False if FemTest15.__name__ else True
--- src/Mod/Fem/femtest/function_tests/test_support_utils.py
+++ src/Mod/Fem/femtest/function_tests/test_support_utils.py
@@ -1,6 +1,6 @@
 import unittest
 
-from ..app import support_utils
+from femtest.app import support_utils
 
 
 class TestParseDiff(unittest.TestCase):
