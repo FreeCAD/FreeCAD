e4eac1fa22bc80060f3aa10c6f549d143585516a
lyphrowny <strukov.as@edu.spbstu.ru>
2024-06-19 01:13:37 +0300

    FEM: fix rewritten license notices; fix typos in comments

--- src/Mod/Fem/TestFemApp.py
+++ src/Mod/Fem/TestFemApp.py
@@ -1,22 +1,26 @@
-# *************************************************************************
-#  Copyright (c) 2018-2024 lyphrowny                                      *
-#                                                                         *
-#  This file is part of the FreeCAD development system.                   *
-#                                                                         *
-#  This program is free software; you can redistribute it and/or modify   *
-#  it under the terms of the GNU Lesser General Public License (LGPL)     *
-#  as published by the Free Software Foundation; either version 2 of      *
-#  the License, or (at your option) any later version.                    *
-#  for detail see the LICENCE text file.                                  *
-#                                                                         *
-#  This program is distributed in the hope that it will be useful,        *
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
-#  GNU Library General Public License for more details.                   *
-#                                                                         *
-#  You should have received a copy of the GNU General Public License      *
-#  along with this program. If not, see <https://www.gnu.org/licenses/>.  *
-# *************************************************************************
+# ***************************************************************************
+# *   Copyright (c) 2018 Przemo Firszt <przemo@firszt.eu>                   *
+# *   Copyright (c) 2018 Bernd Hahnebach <bernd@bimstatik.org>              *
+# *                                                                         *
+# *   This file is part of the FreeCAD CAx development system.              *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENCE text file.                                 *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Library General Public License for more details.                  *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with this program; if not, write to the Free Software   *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# ***************************************************************************
 
 # Unit test for the FEM module
 # to get the right order import as is used
