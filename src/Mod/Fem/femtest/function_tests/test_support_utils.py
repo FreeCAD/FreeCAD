f631e32df0f5798792806b86ca533fef3591b8ef
lyphrowny <strukov.as@edu.spbstu.ru>
2024-06-18 15:10:01 +0300

    FEM: add license notice

--- src/Mod/Fem/femtest/function_tests/test_support_utils.py
+++ src/Mod/Fem/femtest/function_tests/test_support_utils.py
@@ -1,3 +1,23 @@
+# *************************************************************************
+#  Copyright (c) 2024 lyphrowny                                           *
+#                                                                         *
+#  This file is part of the FreeCAD development system.                   *
+#                                                                         *
+#  This program is free software; you can redistribute it and/or modify   *
+#  it under the terms of the GNU Lesser General Public License (LGPL)     *
+#  as published by the Free Software Foundation; either version 2 of      *
+#  the License, or (at your option) any later version.                    *
+#  for detail see the LICENCE text file.                                  *
+#                                                                         *
+#  This program is distributed in the hope that it will be useful,        *
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+#  GNU Library General Public License for more details.                   *
+#                                                                         *
+#  You should have received a copy of the GNU General Public License      *
+#  along with this program. If not, see <https://www.gnu.org/licenses/>.  *
+# *************************************************************************
+
 import unittest
 
 from femtest.app import support_utils
