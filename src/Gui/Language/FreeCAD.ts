0007d84cd5f6ba6a3d8492d1f8f1b04f8269beb0
Yorik van Havre <yorik@uncreated.net>
2024-07-05 15:39:17 +0200

    Updated ts files

--- src/Gui/Language/FreeCAD.ts
+++ src/Gui/Language/FreeCAD.ts
@@ -194,18 +194,18 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../TaskView/TaskImage.cpp" line="254"/>
+        <location filename="../TaskView/TaskImage.cpp" line="260"/>
         <location filename="../TaskView/TaskOrientation.cpp" line="63"/>
         <source>Edit image</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../CommandFeat.cpp" line="122"/>
+        <location filename="../CommandFeat.cpp" line="127"/>
         <source>Set Random Color</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../CommandFeat.cpp" line="168"/>
+        <location filename="../CommandFeat.cpp" line="175"/>
         <source>Toggle freeze</source>
         <translation type="unfinished"></translation>
     </message>
@@ -9267,13 +9267,13 @@ underscore, and must not start with a digit.</source>
 <context>
     <name>StdCmdFeatRecompute</name>
     <message>
-        <location filename="../CommandFeat.cpp" line="56"/>
+        <location filename="../CommandFeat.cpp" line="57"/>
         <source>&amp;Recompute</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../CommandFeat.cpp" line="57"/>
-        <location filename="../CommandFeat.cpp" line="59"/>
+        <location filename="../CommandFeat.cpp" line="58"/>
+        <location filename="../CommandFeat.cpp" line="60"/>
         <source>Recompute feature or document</source>
         <translation type="unfinished"></translation>
     </message>
@@ -9906,13 +9906,13 @@ underscore, and must not start with a digit.</source>
 <context>
     <name>StdCmdRandomColor</name>
     <message>
-        <location filename="../CommandFeat.cpp" line="79"/>
+        <location filename="../CommandFeat.cpp" line="80"/>
         <source>Random color</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../CommandFeat.cpp" line="80"/>
-        <location filename="../CommandFeat.cpp" line="82"/>
+        <location filename="../CommandFeat.cpp" line="81"/>
+        <location filename="../CommandFeat.cpp" line="83"/>
         <source>Set each selected object to a randomly-selected color</source>
         <translation type="unfinished"></translation>
     </message>
@@ -10116,13 +10116,13 @@ underscore, and must not start with a digit.</source>
 <context>
     <name>StdCmdSendToPythonConsole</name>
     <message>
-        <location filename="../CommandFeat.cpp" line="205"/>
+        <location filename="../CommandFeat.cpp" line="212"/>
         <source>&amp;Send to Python Console</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../CommandFeat.cpp" line="206"/>
-        <location filename="../CommandFeat.cpp" line="208"/>
+        <location filename="../CommandFeat.cpp" line="213"/>
+        <location filename="../CommandFeat.cpp" line="215"/>
         <source>Sends the selected object to the Python console</source>
         <translation type="unfinished"></translation>
     </message>
@@ -12404,23 +12404,28 @@ dot/period will always be printed.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="241"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="246"/>
+        <source>Looking for more themes? You can obtain them using &lt;a href=&quot;freecad:Std_AddonMgr&quot;&gt;Addon Manager&lt;/a&gt;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="256"/>
         <source>Size of toolbar icons:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="248"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="263"/>
         <source>Choose your preference for toolbar icon size. You can adjust
 this according to your screen size or personal taste</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="256"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="271"/>
         <source>Tree view and Property view mode:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="263"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="278"/>
         <source>Customize how tree view is shown in the panel (restart required).
 
 &apos;Combined&apos;: combine Tree view and Property view into one panel.
@@ -12428,170 +12433,170 @@ this according to your screen size or personal taste</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="273"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="288"/>
         <source>Size of recent file list:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="280"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="295"/>
         <source>How many files should be listed in recent files list</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="296"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="311"/>
         <source>Background of the main window (when no document is opened) will consist of tiles of a special image.
 See the FreeCAD Wiki for details about the image.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="300"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="315"/>
         <source>Enable tiled background</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="307"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="322"/>
         <source>The text cursor will be blinking</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="310"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="325"/>
         <source>Enable cursor blinking</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="326"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="341"/>
         <source>A Splash screen is a small loading window that is shown
 when FreeCAD is launching. If this option is checked, FreeCAD will
 display the splash screen</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="331"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="346"/>
         <source>Enable splash screen at start up</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="347"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="362"/>
         <source>Activate overlay handling of dock windows</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="350"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="365"/>
         <source>Activate overlay handling</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="369"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="384"/>
         <source>Preference packs</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="435"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="450"/>
         <source>Name</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="446"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="461"/>
         <source>Type</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="457"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="472"/>
         <source>Load</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="480"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="495"/>
         <source>Import config...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="487"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="502"/>
         <source>Save new...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="500"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="515"/>
         <source>Manage...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="507"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.ui" line="522"/>
         <source>Revert...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="87"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="90"/>
         <source>Manage preference packs</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="475"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="478"/>
         <source>Small (%1px)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="476"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="479"/>
         <source>Medium (%1px)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="477"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="480"/>
         <source>Large (%1px)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="478"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="481"/>
         <source>Extra large (%1px)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="480"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="483"/>
         <source>Custom (%1px)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="549"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="552"/>
         <source>Combined</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="550"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="553"/>
         <source>Independent</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="577"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="580"/>
         <source>Preference Pack Name</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="578"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="581"/>
         <source>Tags</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="615"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="618"/>
         <source>Apply</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="616"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="619"/>
         <source>Apply the %1 preference pack</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="677"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="680"/>
         <source>Choose a FreeCAD config file to import</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="688"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="691"/>
         <source>File exists</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="689"/>
+        <location filename="../PreferencePages/DlgSettingsGeneral.cpp" line="692"/>
         <source>A preference pack with that name already exists. Overwrite?</source>
         <translation type="unfinished"></translation>
     </message>
@@ -13255,12 +13260,12 @@ the region are non-opaque.</source>
 <context>
     <name>StdCmdToggleFreeze</name>
     <message>
-        <location filename="../CommandFeat.cpp" line="153"/>
+        <location filename="../CommandFeat.cpp" line="160"/>
         <source>Toggle freeze</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../CommandFeat.cpp" line="155"/>
+        <location filename="../CommandFeat.cpp" line="162"/>
         <source>Toggles freeze state of the selected objects. A frozen object is not recomputed when its parents change.</source>
         <translation type="unfinished"></translation>
     </message>
