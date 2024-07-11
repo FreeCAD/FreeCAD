4f8abda8bb55a341ad5bfe6032d3fc395794e283
bgbsww <bgbsww@gmail.com>
2024-07-11 15:41:36 -0400

    Toponaming: Process mapped names during selection

--- src/App/PropertyLinks.cpp
+++ src/App/PropertyLinks.cpp
@@ -415,7 +415,6 @@ bool PropertyLinkBase::_updateElementReference(DocumentObject *feature,
         const char* oldElement = Data::findElementName(shadow.second.c_str());
         if (!Data::hasMissingElement(oldElement)) {
             const auto& names = geo->searchElementCache(oldElement);
-//        std::vector<std::string> names;  // searchElementCache isn't implemented.
             if (names.size()) {
                 missing = false;
                 std::string newsub(subname, strlen(subname) - strlen(element));
