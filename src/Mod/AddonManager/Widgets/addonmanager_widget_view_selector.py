FreeCAD/FreeCAD

commit 84063a99f9a10b39a8b553bd81a57cf9fec35901

[AddonManager] Correct composite button object name... (#15676)
* [AddonManager] Correct composite button object name...

... and disable button if Qt version is below 5.15.x

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre-commit.ci

---------

Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

src/Mod/AddonManager/Widgets/addonmanager_widget_view_selector.py
@@ -112,14 +112,20 @@ class WidgetViewSelector(QtWidgets.QWidget):
         )
 
         self.composite_button = QtWidgets.QToolButton(self)
-        self.composite_button.setObjectName("expanded_button")
-        self.composite_button.setCheckable(True)
-        self.composite_button.setChecked(True)
+        self.composite_button.setObjectName("composite_button")
+        qtcore_major_ver = int(float(QtCore.qVersion()[0]))
+        qtcore_minor_ver = int(float(QtCore.qVersion()[2]))
+        if qtcore_major_ver == 5 and qtcore_minor_ver < 15:
+            self.composite_button.setEnabled(False)
+            self.composite_button.setCheckable(False)
+            self.composite_button.setChecked(False)
+        else:
+            self.composite_button.setCheckable(True)
+            self.composite_button.setChecked(True)
         self.composite_button.setAutoExclusive(True)
         self.composite_button.setIcon(
             QtGui.QIcon.fromTheme("composite_button", QtGui.QIcon(":/icons/composite_view.svg"))
         )
-
         self.horizontal_layout.addWidget(self.compact_button)
         self.horizontal_layout.addWidget(self.expanded_button)
         self.horizontal_layout.addWidget(self.composite_button)

commit c845c976aa42fbe3135c57e82cc76e4215ea1bac

[AddonManager] Fix regression introduced by #15676 (me)... (#15688)
* [AddonManager] Fix regression introduced by #15676...

...it disabled composite view for more than intended

* [AddonManager] Use QLibraryInfo major and minor version

src/Mod/AddonManager/Widgets/addonmanager_widget_view_selector.py
@@ -113,9 +113,10 @@ class WidgetViewSelector(QtWidgets.QWidget):
 
         self.composite_button = QtWidgets.QToolButton(self)
         self.composite_button.setObjectName("composite_button")
-        qtcore_major_ver = int(float(QtCore.qVersion()[0]))
-        qtcore_minor_ver = int(float(QtCore.qVersion()[2]))
-        if qtcore_major_ver == 5 and qtcore_minor_ver < 15:
+        if (
+            QtCore.QLibraryInfo.version().majorVersion() == 5
+            and QtCore.QLibraryInfo.version().minorVersion() < 15
+        ):
             self.composite_button.setEnabled(False)
             self.composite_button.setCheckable(False)
             self.composite_button.setChecked(False)