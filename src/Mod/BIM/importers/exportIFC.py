FreeCAD/FreeCAD

commit a125770d636dfbd6366ce9a2b4560b778550e8f1

[Arch ExportIFC] Bug-fix Consecutive Face Touch at Corner
Bug-fix Consecutive face (Wall segment) touch at corner.

Forum Discussion : https://forum.freecad.org/viewtopic.php?p=771954#p771954

src/Mod/BIM/importers/exportIFC.py
@@ -2080,10 +2080,22 @@ def getRepresentation(
                                 profile = profiledefs[pstr]
                                 shapetype = "reusing profile"
                             else:
-                                profile = getProfile(ifcfile,pi)
-                                if profile:
-                                    profiledefs[pstr] = profile
-                            if profile and not(DraftVecUtils.isNull(evi)):
+                                # Fix bug in Forum Discussion
+                                # https://forum.freecad.org/viewtopic.php?p=771954#p771954
+                                if not isinstance(pi, Part.Compound):
+                                    profile = getProfile(ifcfile,pi)
+                                    if profile:
+                                        profiledefs[pstr] = profile
+                                        profiles=[profile]
+                                else:  # i.e. Part.Compound
+                                    profiles=[]
+                                    for pif in pi.Faces:
+                                        profile = getProfile(ifcfile,pif)
+                                        if profile:
+                                            profiledefs[pstr] = profile
+                                            profiles.append(profile)
+                            if profiles and not(DraftVecUtils.isNull(evi)):
+                              for profile in profiles:
                                 #ev = pl.Rotation.inverted().multVec(evi)
                                 #print("evi:",evi)
                                 if not tostore:

commit d6dc56b03ec84267b2dbfe69f0d67767afd2d005

BIM: Fix oversight in #15530
Fix bug introduced in #15530.

Forum topic:
https://forum.freecad.org/viewtopic.php?t=89790

@paullee0 can you check please? Thanks.

src/Mod/BIM/importers/exportIFC.py
@@ -2078,6 +2078,7 @@ def getRepresentation(
                             pstr = str([v.Point for v in p[i].Vertexes])
                             if pstr in profiledefs:
                                 profile = profiledefs[pstr]
+                                profiles = [profile]
                                 shapetype = "reusing profile"
                             else:
                                 # Fix bug in Forum Discussion