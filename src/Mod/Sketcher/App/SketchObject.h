257a3a5ad242420eb1829d8c840c37e5c90dd7dd
Ajinkya Dahale <AjinkyaDahale@users.noreply.github.com>
2024-08-31 20:04:05 +0530

    [Sketcher] Refactor `SketchObject::onChanged()`
    Break into pieces depending on which property has been passed.

--- src/Mod/Sketcher/App/SketchObject.h
+++ src/Mod/Sketcher/App/SketchObject.h
@@ -951,6 +951,14 @@ protected:
     /// b-splines need their own treatment
     int deleteUnusedInternalGeometryWhenBSpline(int GeoId, bool delgeoid = false);
 
+    void onGeometryChanged();
+    void onConstraintsChanged();
+    void onExternalGeoChanged();
+    void onExternalGeometryChanged();
+    void onPlacementChanged();
+    void onExpressionEngineChanged();
+    void onAttachmentSupportChanged();
+
     void onDocumentRestored() override;
     void restoreFinished() override;
     void onSketchRestore();
