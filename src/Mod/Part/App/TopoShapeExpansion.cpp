80c1ad4a0a8934244cffb27850b2e3adadd3bda2
Florian Foinant-Willig <FlachyJoe@users.noreply.github.com>
2024-08-31 20:09:39 +0200

    [PD] Fix Pad a Plane attached Sketch upto a Plane (#16056)

--- src/Mod/Part/App/TopoShapeExpansion.cpp
+++ src/Mod/Part/App/TopoShapeExpansion.cpp
@@ -4403,6 +4403,10 @@ TopoShape& TopoShape::makeElementPrismUntil(const TopoShape& _base,
 
                 srcShapes.push_back(result);
 
+                if (result.isInfinite()){
+                    result = face;
+                }
+
                 PrismMaker.Init(result.getShape(),
                                 face.getShape(),
                                 TopoDS::Face(supportFace.getShape()),
