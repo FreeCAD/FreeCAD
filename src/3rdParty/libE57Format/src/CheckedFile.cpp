86806cb353410155cd8937cecf051569e7dbcd33
wmayer <wmayer@freecad.org>
2024-09-13 14:23:13 +0200

    Port: Include changes of FreeBSD port

--- src/3rdParty/libE57Format/src/CheckedFile.cpp
+++ src/3rdParty/libE57Format/src/CheckedFile.cpp
@@ -25,6 +25,11 @@
  * DEALINGS IN THE SOFTWARE.
  */
 
+// convenience for all the BSDs
+#if defined( __FreeBSD__) || defined( __NetBSD__) || defined( __OpenBSD__)
+#define __BSD
+#endif
+
 #if defined( _WIN32 )
 #if defined( _MSC_VER )
 #include <codecvt>
@@ -44,7 +49,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
-#elif defined(__OpenBSD__)
+#elif defined(__BSD)
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -487,7 +492,7 @@ uint64_t CheckedFile::lseek64( int64_t offset, int whence )
 #endif
 #elif defined( __linux__ )
    int64_t result = ::lseek64( fd_, offset, whence );
-#elif defined( __APPLE__ ) || defined(__OpenBSD__)
+#elif defined( __APPLE__ ) || defined(__BSD)
    int64_t result = ::lseek( fd_, offset, whence );
 #else
 #error "no supported OS platform defined"
