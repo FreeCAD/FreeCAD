1a1b0dd566886467e49e7fccf441839d5fab9769
wmayer <wmayer@freecad.org>
2024-09-13 11:51:52 +0200

    Test: use /tmp/FreeCAD as custom user home directory for test applications
    See https://forum.freecad.org/viewtopic.php?p=781716#p781716

--- tests/src/App/InitApplication.h
+++ tests/src/App/InitApplication.h
@@ -1,7 +1,9 @@
 #ifndef TEST_APPLICATION_H
 #define TEST_APPLICATION_H
 
+#include <FCConfig.h>
 #include <App/Application.h>
+#include <Base/FileInfo.h>
 
 namespace tests
 {
@@ -9,6 +11,16 @@ namespace tests
 static void initApplication()
 {
     if (App::Application::GetARGC() == 0) {
+#if defined(FC_OS_LINUX) || defined(FC_OS_BSD)
+        std::string tmp = Base::FileInfo::getTempPath();
+        tmp += "/FreeCAD";
+        Base::FileInfo fi(tmp);
+        fi.createDirectory();
+        std::string env = "FREECAD_USER_HOME=";
+        env += fi.filePath();
+        putenv(env.data());
+#endif
+
         constexpr int argc = 1;
         std::array<const char*, argc> argv {"FreeCAD"};
         App::Application::Config()["ExeName"] = "FreeCAD";
