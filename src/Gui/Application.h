f82bfd5ddcddfa96359789093190c639ab088e24
bgbsww <bgbsww@gmail.com>
2024-09-18 15:42:34 -0400

    Move the recompute dialog to after Gui opens instead of per document signal.

--- src/Gui/Application.h
+++ src/Gui/Application.h
@@ -74,6 +74,8 @@ public:
     void exportTo(const char* FileName, const char* DocName, const char* Module);
     /// Reload a partial opened document
     App::Document *reopen(App::Document *doc);
+    /// Prompt about recomputing if needed
+    static void checkForRecomputes();
     //@}
 
 
@@ -131,8 +133,6 @@ public:
     boost::signals2::signal<void ()> signalRefreshWorkbenches;
     /// signal on show hidden items
     boost::signals2::signal<void (const Gui::Document&)> signalShowHidden;
-    /// signal on document restore complete
-    boost::signals2::signal<void (const Gui::Document&)> signalFinishRestoreDocument;
     /// signal on activating view
     boost::signals2::signal<void (const Gui::MDIView*)> signalActivateView;
     /// signal on entering in edit mode
@@ -153,7 +153,6 @@ protected:
     void slotRenameDocument(const App::Document&);
     void slotActiveDocument(const App::Document&);
     void slotShowHidden(const App::Document&);
-    void slotFinishRestoreDocument(const App::Document&);
     void slotNewObject(const ViewProvider&);
     void slotDeletedObject(const ViewProvider&);
     void slotChangedObject(const ViewProvider&, const App::Property& Prop);
