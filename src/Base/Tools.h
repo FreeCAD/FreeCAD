4573f54c436d55c06e37d5acb5dce150f2e6cdee
Benjamin Bræstrup Sayoc <benj5378@outlook.com>
2024-10-17 01:08:34 +0200

    Deprecate manipulator

--- src/Base/Tools.h
+++ src/Base/Tools.h
@@ -30,7 +30,6 @@
 #include <functional>
 #include <algorithm>
 #include <cmath>
-#include <iostream>
 #include <vector>
 #include <string>
 
@@ -56,51 +55,6 @@ public:
 private:
     T n;
 };
-
-// ----------------------------------------------------------------------------
-
-template<class T>
-class manipulator
-{
-    T i_;
-    std::ostream& (*f_)(std::ostream&, T);
-
-public:
-    manipulator(std::ostream& (*f)(std::ostream&, T), T i)
-        : i_(i)
-        , f_(f)
-    {}
-    friend std::ostream& operator<<(std::ostream& os, manipulator m)
-    {
-        return m.f_(os, m.i_);
-    }
-};
-
-inline std::ostream& tabsN(std::ostream& os, int n)
-{
-    for (int i = 0; i < n; i++) {
-        os << "\t";
-    }
-    return os;
-}
-
-inline std::ostream& blanksN(std::ostream& os, int n)
-{
-    for (int i = 0; i < n; i++) {
-        os << " ";
-    }
-    return os;
-}
-
-inline manipulator<int> tabs(int n)
-{
-    return {&tabsN, n};
-}
-
-inline manipulator<int> blanks(int n)
-{
-    return {&blanksN, n};
-}
 
 // ----------------------------------------------------------------------------
 
