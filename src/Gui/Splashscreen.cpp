ecf005e23b64165bfce6f16e777c2855ba646cfb
Michael Fuchs <michi11724dev@googlemail.com>
2024-10-18 19:01:29 +0200

    Remove unused LibraryInfo class

--- src/Gui/Splashscreen.cpp
+++ src/Gui/Splashscreen.cpp
@@ -454,14 +454,6 @@ void AboutDialog::setupLabels()
     }
 }
 
-class AboutDialog::LibraryInfo {
-public:
-    QString name;
-    QString href;
-    QString url;
-    QString version;
-};
-
 void AboutDialog::showCredits()
 {
     auto creditsFileURL = QLatin1String(":/doc/CONTRIBUTORS");
@@ -572,8 +564,7 @@ void AboutDialog::showLibraryInformation()
     ui->tabWidget->addTab(tab_library, tr("Libraries"));
     auto hlayout = new QVBoxLayout(tab_library);
     auto textField = new QTextBrowser(tab_library);
-    textField->setOpenExternalLinks(false);
-    textField->setOpenLinks(false);
+    textField->setOpenExternalLinks(true);
     hlayout->addWidget(textField);
 
     QString baseurl = QString::fromLatin1("file:///%1/ThirdPartyLibraries.html")
@@ -581,8 +572,6 @@ void AboutDialog::showLibraryInformation()
     QUrl librariesFileUrl = QUrl(baseurl);
 
     textField->setSource(librariesFileUrl);
-
-    connect(textField, &QTextBrowser::anchorClicked, this, &AboutDialog::linkActivated);
 }
 
 void AboutDialog::showCollectionInformation()
