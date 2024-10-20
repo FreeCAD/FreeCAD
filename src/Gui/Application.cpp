5862b92a21091f6b078698ec6937651bf7860a2c
wmayer <wmayer@freecad.org>
2024-10-20 20:13:01 +0200

    Core: Set unit scheme when switching between unsaved documents
    Fixes #17297

--- src/Gui/Application.cpp
+++ src/Gui/Application.cpp
@@ -1031,7 +1031,7 @@ void Application::slotActiveDocument(const App::Document& Doc)
         // Update the application to show the unit change
         ParameterGrp::handle hGrp = App::GetApplication().GetParameterGroupByPath(
             "User parameter:BaseApp/Preferences/Units");
-        if (Doc.FileName.getValue()[0] != '\0' && !hGrp->GetBool("IgnoreProjectSchema")) {
+        if (!hGrp->GetBool("IgnoreProjectSchema")) {
             int userSchema = Doc.UnitSystem.getValue();
             Base::UnitsApi::setSchema(static_cast<Base::UnitSystem>(userSchema));
             getMainWindow()->setUserSchema(userSchema);
