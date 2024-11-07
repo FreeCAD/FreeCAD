d31d24fd4ee9cbd075f8b772d7e9f0a7c2e23c47
PaddleStroke <pierrelouis.boyer@gmail.com>
2024-11-07 10:01:52 +0100

    Assembly: Fix regressions of https://github.com/FreeCAD/FreeCAD/pull/16671

--- src/Mod/Assembly/UtilsAssembly.py
+++ src/Mod/Assembly/UtilsAssembly.py
@@ -1237,30 +1237,30 @@ def getParentPlacementIfNeeded(part):
     return Base.Placement()
 
 
-def generatePropertySettings(objectName, documentObject):
+def generatePropertySettings(documentObject):
     commands = []
     if hasattr(documentObject, "Name"):
-        commands.append(f'{objectName} = App.ActiveDocument.getObject("{documentObject.Name}")')
+        commands.append(f'obj = App.ActiveDocument.getObject("{documentObject.Name}")')
     for propertyName in documentObject.PropertiesList:
         propertyValue = documentObject.getPropertyByName(propertyName)
         propertyType = documentObject.getTypeIdOfProperty(propertyName)
         # Note: OpenCascade precision is 1e-07, angular precision is 1e-05.  For purposes of creating a Macro,
         # we are forcing a reduction in precision so as to get round numbers like 0 instead of tiny near 0 values
         if propertyType == "App::PropertyFloat":
-            commands.append(f"{objectName}.{propertyName} = {propertyValue:.5f}")
+            commands.append(f"obj.{propertyName} = {propertyValue:.5f}")
         elif propertyType == "App::PropertyInt" or propertyType == "App::PropertyBool":
-            commands.append(f"{objectName}.{propertyName} = {propertyValue}")
+            commands.append(f"obj.{propertyName} = {propertyValue}")
         elif propertyType == "App::PropertyString" or propertyType == "App::PropertyEnumeration":
-            commands.append(f'{objectName}.{propertyName} = "{propertyValue}"')
+            commands.append(f'obj.{propertyName} = "{propertyValue}"')
         elif propertyType == "App::PropertyPlacement":
             commands.append(
-                f"{objectName}.{propertyName} = App.Placement("
+                f"obj.{propertyName} = App.Placement("
                 f"App.Vector({propertyValue.Base.x:.5f},{propertyValue.Base.y:.5f},{propertyValue.Base.z:.5f}),"
                 f"App.Rotation(*{[round(n,5) for n in propertyValue.Rotation.getYawPitchRoll()]}))"
             )
         elif propertyType == "App::PropertyXLinkSubHidden":
             commands.append(
-                f'{objectName}.{propertyName} = [App.ActiveDocument.getObject("{propertyValue[0].Name}"), {propertyValue[1]}]'
+                f'obj.{propertyName} = [App.ActiveDocument.getObject("{propertyValue[0].Name}"), {propertyValue[1]}]'
             )
         else:
             # print("Not processing properties of type ", propertyType)
