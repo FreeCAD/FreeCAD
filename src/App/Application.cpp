e8e82c4ff6c6b9839a91f0689602c94e76b5cdec
Jacob Oursland <jacob.oursland@gmail.com>
2024-11-07 22:56:59 -0800

    App: Reduce BOOST_VERSION workaround to fix boost symbol linker error on Win-Qt5.

--- src/App/Application.cpp
+++ src/App/Application.cpp
@@ -3060,7 +3060,7 @@ void Application::LoadParameters()
     }
 }
 
-#if defined(_MSC_VER) && BOOST_VERSION < 108300
+#if defined(_MSC_VER) && BOOST_VERSION < 108200
     // fix weird error while linking boost (all versions of VC)
     // VS2010: https://forum.freecad.org/viewtopic.php?f=4&t=1886&p=12553&hilit=boost%3A%3Afilesystem%3A%3Aget#p12553
     namespace boost { namespace program_options { std::string arg="arg"; } }
