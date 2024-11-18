dddf64a5b45afc81bd5a73d5c5481bb4a0e928e2
Jacob Oursland <jacob.oursland@gmail.com>
2024-11-17 18:14:09 -0800

    conda: add -y to mamba to overwrite environment.

--- conda/setup-environment.sh
+++ conda/setup-environment.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 # create the conda environment as a subdirectory
-mamba env create --root-prefix .conda/freecad -p .conda/freecad -f conda/conda-env.yaml
+mamba env create -y -p .conda/freecad -f conda/conda-env.yaml
 
 # add the environment subdirectory to the conda configuration
 conda config --add envs_dirs $CONDA_PREFIX/envs
@@ -12,4 +12,4 @@ conda config --add envs_dirs $(pwd)/.conda
 conda config --set env_prompt "({name})"
 
 # install the FreeCAD dependencies into the environment
-mamba run --live-stream -n freecad mamba-devenv --no-prune -f conda/environment.devenv.yml
+mamba run --live-stream -n freecad mamba-devenv -y --no-prune -f conda/environment.devenv.yml
