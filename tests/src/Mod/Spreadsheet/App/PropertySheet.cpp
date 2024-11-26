e186920ad2a52e5ec67ef45650436dfc395e5d6a
Chris Hennes <chennes@pioneerlibrarysystem.org>
2024-11-25 23:29:21 -0600

    Spreadsheet: Add tests for validAddressName and validAliases

--- tests/src/Mod/Spreadsheet/App/PropertySheet.cpp
+++ tests/src/Mod/Spreadsheet/App/PropertySheet.cpp
@@ -37,7 +37,48 @@ private:
     std::unique_ptr<Spreadsheet::PropertySheet> _propertySheet;
 };
 
-TEST_F(PropertySheetTest, isValidCellAddressName)  // NOLINT
+TEST_F(PropertySheetTest, isValidCellAddressNameValidNames)  // NOLINT
 {
-    // Test some things
+    std::vector<std::string> validAddressNames {"A1", "Z1024", "AA42", "ZZ4096"};
+    for (const auto& name : validAddressNames) {
+        EXPECT_TRUE(propertySheet()->isValidCellAddressName(name))
+            << "\"" << name << "\" was not accepted as a cell name, and should be";
+    }
+}
+
+TEST_F(PropertySheetTest, isValidCellAddressNameInvalidNames)  // NOLINT
+{
+    std::vector<std::string> invalidAddressNames {
+        "Bork",
+        "Bork_de_bork",
+        "A",
+        "42",
+        "AAA1",     // Too many characters to start, AAA is not a valid column
+        "ZZ123456"  // Too large a number to end, 123456 is not a valid row
+    };
+    for (const auto& name : invalidAddressNames) {
+        EXPECT_FALSE(propertySheet()->isValidCellAddressName(name))
+            << "\"" << name << "\" was accepted as a cell name, and should not be";
+    }
+}
+
+TEST_F(PropertySheetTest, validAliases)  // NOLINT
+{
+    std::vector<std::string> validAliases {"Bork",
+                                           "Bork_de_bork"
+                                           "A",
+                                           "AA123456"};
+    for (const auto& name : validAliases) {
+        EXPECT_TRUE(propertySheet()->isValidAlias(name))
+            << "\"" << name << "\" was not accepted as an alias name, and should be";
+    }
+}
+
+TEST_F(PropertySheetTest, invalidAliases)  // NOLINT
+{
+    std::vector<std::string> invalidAliases {"A1", "ZZ1234", "mm"};
+    for (const auto& name : invalidAliases) {
+        EXPECT_FALSE(propertySheet()->isValidAlias(name))
+            << "\"" << name << "\" was accepted as an alias name, and should not be";
+    }
 }
