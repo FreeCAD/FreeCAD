1eb8496aae1c509ecd7dce198d5723d02be07fbd
Florian Foinant-Willig <flachyjoe@gmail.com>
2024-12-01 17:48:15 +0100

    [Core] Fix for frozen status
    A frozen object prevents the document to be saved
    The frozen status propagates into object inheritance
    A frozen sketch is no more editable
    Transform and Placement can't be changed for a frozen object
    The freeze icon is reduced

--- src/Gui/CommandFeat.cpp
+++ src/Gui/CommandFeat.cpp
@@ -172,22 +172,27 @@ StdCmdToggleFreeze::StdCmdToggleFreeze()
 void StdCmdToggleFreeze::activated(int iMsg)
 {
     Q_UNUSED(iMsg);
-    getActiveGuiDocument()->openCommand(QT_TRANSLATE_NOOP("Command", "Toggle freeze"));
 
     std::vector<Gui::SelectionSingleton::SelObj> sels = Gui::Selection().getCompleteSelection();
 
+    Command::openCommand(QT_TRANSLATE_NOOP("Command", "Toggle freeze"));
     for (Gui::SelectionSingleton::SelObj& sel : sels) {
         App::DocumentObject* obj = sel.pObject;
         if (!obj)
             continue;
 
-        if (obj->isFreezed())
+        if (obj->isFreezed()){
             obj->unfreeze();
-        else
+            for (auto child : obj->getInListRecursive())
+                child->unfreeze();
+        } else {
             obj->freeze();
-    }
+            for (auto parent : obj->getOutListRecursive())
+                parent->freeze();
+        }
 
-    getActiveGuiDocument()->commitCommand();
+    }
+    Command::commitCommand();
 }
 
 bool StdCmdToggleFreeze::isActive()
