9c3eef03dc92a26aec3a4eaeb6efa9ec44f622f9
FEA-eng <59876896+FEA-eng@users.noreply.github.com>
2024-12-06 22:15:20 +0100

    FEM: Update resulttools.py

--- src/Mod/Fem/femresult/resulttools.py
+++ src/Mod/Fem/femresult/resulttools.py
@@ -62,11 +62,9 @@ def purge_results(analysis):
             analysis.Document.removeObject(m.Name)
     analysis.Document.recompute()
 
-    # output text object
+    # text object
     for m in analysis.Group:
-        if is_of_type(m, "App::TextDocument") and (
-            m.Name.startswith("ccx_dat_file") or m.Name.startswith("SolverElmerOutput")
-        ):
+        if is_of_type(m, "App::TextDocument"):
             analysis.Document.removeObject(m.Name)
     analysis.Document.recompute()
 
