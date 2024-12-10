919457fa29e4c49ce25273999dda6c813e94e4f4
wmayer <wmayer@freecad.org>
2024-12-10 16:27:42 +0100

    Core: Fix pre-selection of coordinate system

--- src/Gui/ViewProviderDatum.cpp
+++ src/Gui/ViewProviderDatum.cpp
@@ -107,13 +107,15 @@ void ViewProviderDatum::attach(App::DocumentObject* pcObject)
     highlight->documentName = getObject()->getDocument()->getName();
     highlight->style = SoFCSelection::EMISSIVE_DIFFUSE;
 
+    // Visible features
+    auto visible = new SoSeparator();
     // Style for normal (visible) lines
     auto style = new SoDrawStyle();
     style->lineWidth = lineThickness;
-    highlight->addChild(style);
+    visible->addChild(style);
 
     // Visible lines
-    highlight->addChild(pRoot);
+    visible->addChild(pRoot);
 
     // Hidden features
     auto hidden = new SoAnnotation();
@@ -127,9 +129,9 @@ void ViewProviderDatum::attach(App::DocumentObject* pcObject)
     // Hidden lines
     hidden->addChild(pRoot);
 
-    highlight->addChild(hidden);
+    visible->addChild(hidden);
 
-    sep->addChild(highlight);
+    sep->addChild(visible);
 
 
     // Scale feature to the given size
@@ -140,7 +142,9 @@ void ViewProviderDatum::attach(App::DocumentObject* pcObject)
     soScale->setPart("shape", sep);
     soScale->scaleFactor = sz;
 
-    addDisplayMaskMode(soScale, "Base");
+    highlight->addChild(soScale);
+
+    addDisplayMaskMode(highlight, "Base");
 }
 
 
