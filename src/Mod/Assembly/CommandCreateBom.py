d1707fdf10e0468155bf57ace64e219e2ab17b7e
Demolux <marvin.bauch@gmx.de>
2024-12-20 10:43:13 +0100

    BOM generation missing columns and .doCommandEval bugfix
     BOM generation bugfix: Columns are persistent and .doCommandEval dependency circumvented #18615 backport to 1.0

--- src/Mod/Assembly/CommandCreateBom.py
+++ src/Mod/Assembly/CommandCreateBom.py
@@ -160,6 +160,7 @@ class TaskAssemblyCreateBom(QtCore.QObject):
         self.updateColumnList()
 
     def accept(self):
+        self.updateColumnList()
         self.deactivate()
         App.closeActiveTransaction()
 
@@ -334,7 +335,10 @@ class TaskAssemblyCreateBom(QtCore.QObject):
         else:
             commands = 'bomObj = App.activeDocument().addObject("Assembly::BomObject", "Bill of Materials")'
         Gui.doCommand(commands)
-        self.bomObj = Gui.doCommandEval("bomObj")
+        bom_objects = [
+            obj for obj in App.ActiveDocument.Objects if obj.TypeId == "Assembly::BomObject"
+        ]
+        self.bomObj = bom_objects[-1]
 
     def export(self):
         self.bomObj.recompute()
