0bc9737ca61e23dc9b38b27c038aa2a842e581b2
Roy-043 <info@b-k-g.nl>
2025-01-03 16:51:42 +0100

    Draft: Introduce base classes for Draft tests
    To avoid code duplication.

--- src/Mod/Draft/drafttests/test_airfoildat.py
+++ src/Mod/Draft/drafttests/test_airfoildat.py
@@ -1,6 +1,7 @@
 # ***************************************************************************
 # *   Copyright (c) 2013 Yorik van Havre <yorik@uncreated.net>              *
 # *   Copyright (c) 2019 Eliud Cabrera Castillo <e.cabrera-castillo@tum.de> *
+# *   Copyright (c) 2025 FreeCAD Project Association                        *
 # *                                                                         *
 # *   This file is part of the FreeCAD CAx development system.              *
 # *                                                                         *
@@ -29,34 +30,17 @@
 ## \addtogroup drafttests
 # @{
 import os
-import unittest
 
 import FreeCAD as App
 import Draft
 import drafttests.auxiliary as aux
 
 from draftutils.messages import _msg
+from drafttests import test_base
 
 
-class DraftAirfoilDAT(unittest.TestCase):
+class DraftAirfoilDAT(test_base.DraftTestCaseDoc):
     """Test reading and writing of AirfoilDAT with Draft."""
-
-    def setUp(self):
-        """Set up a new document to hold the tests.
-
-        This is executed before every test, so we create a document
-        to hold the objects.
-        """
-        aux.draw_header()
-        self.doc_name = self.__class__.__name__
-        if App.ActiveDocument:
-            if App.ActiveDocument.Name != self.doc_name:
-                App.newDocument(self.doc_name)
-        else:
-            App.newDocument(self.doc_name)
-        App.setActiveDocument(self.doc_name)
-        self.doc = App.ActiveDocument
-        _msg("  Temporary document '{}'".format(self.doc_name))
 
     def test_read_airfoildat(self):
         """Read an airfoil DAT file and import its elements as objects."""
@@ -86,12 +70,5 @@ class DraftAirfoilDAT(unittest.TestCase):
         Draft.export_airfoildat = aux.fake_function
         obj = Draft.export_airfoildat(out_file)
         self.assertTrue(obj, "'{}' failed".format(operation))
-
-    def tearDown(self):
-        """Finish the test.
-
-        This is executed after each test, so we close the document.
-        """
-        App.closeDocument(self.doc_name)
 
 ## @}
