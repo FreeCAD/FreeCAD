08d9be581e6242a9100310da82cd2c7b3fd9647e
Morgan Hardwood <bugs@londonlight.org>
2025-01-04 01:37:49 +0100

    Auto scroll to constraint on click
    When clicking on a contrasint in Sketcher, that constraint is now
    automatically scrolled to in the Constraints listview. If selecting
    multiple constraints, the last one is scrolled to.
    Closes #17361

--- src/Mod/Sketcher/Gui/TaskSketcherConstraints.cpp
+++ src/Mod/Sketcher/Gui/TaskSketcherConstraints.cpp
@@ -1371,6 +1371,8 @@ void TaskSketcherConstraints::onSelectionChanged(const Gui::SelectionChanges& ms
                                 auto tmpBlock = ui->listWidgetConstraints->blockSignals(true);
                                 item->setSelected(select);
                                 ui->listWidgetConstraints->blockSignals(tmpBlock);
+                                QModelIndex index = ui->listWidgetConstraints->model()->index(i, 0);
+                                ui->listWidgetConstraints->scrollTo(index, QAbstractItemView::PositionAtCenter);
                                 break;
                             }
                         }
