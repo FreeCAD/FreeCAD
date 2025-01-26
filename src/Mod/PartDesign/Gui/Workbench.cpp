cfc0b8a75f5da6cd7ef8e2ef172a318a63de1373
wmayer <wmayer@freecad.org>
2025-01-27 00:47:01 +0100

    PD: Show commands in task panel when selecting (sub) shape binders
    This fixes issue 19098

--- src/Mod/PartDesign/Gui/Workbench.cpp
+++ src/Mod/PartDesign/Gui/Workbench.cpp
@@ -296,16 +296,63 @@ void Workbench::activated()
         "PartDesign_Hole",
         "PartDesign_Revolution",
         "PartDesign_Groove",
-        "PartDesign_AdditivePipe",
-        "PartDesign_SubtractivePipe",
         "PartDesign_AdditiveLoft",
         "PartDesign_SubtractiveLoft",
+        "PartDesign_AdditivePipe",
+        "PartDesign_SubtractivePipe",
         "PartDesign_AdditiveHelix",
         "PartDesign_SubtractiveHelix",
         nullptr};
     Watcher.push_back(new Gui::TaskView::TaskWatcherCommands(
         "SELECT Sketcher::SketchObject COUNT 1",
         Sketch,
+        "Modeling tools",
+        "PartDesign_Body"
+    ));
+
+    const char* Sketches[] = {
+        "PartDesign_AdditiveLoft",
+        "PartDesign_SubtractiveLoft",
+        "PartDesign_AdditivePipe",
+        "PartDesign_SubtractivePipe",
+        nullptr};
+    Watcher.push_back(new Gui::TaskView::TaskWatcherCommands(
+        "SELECT Sketcher::SketchObject COUNT 2..",
+        Sketches,
+        "Modeling tools",
+        "PartDesign_Body"
+    ));
+
+    const char* ShapeBinder[] = {
+        "PartDesign_Pad",
+        "PartDesign_Pocket",
+        "PartDesign_Revolution",
+        "PartDesign_Groove",
+        "PartDesign_AdditiveLoft",
+        "PartDesign_SubtractiveLoft",
+        "PartDesign_AdditivePipe",
+        "PartDesign_SubtractivePipe",
+        nullptr};
+    Watcher.push_back(new Gui::TaskView::TaskWatcherCommands(
+        "SELECT PartDesign::ShapeBinder COUNT 1",
+        ShapeBinder,
+        "Modeling tools",
+        "PartDesign_Body"
+    ));
+
+    const char* SubShapeBinder[] = {
+        "PartDesign_Pad",
+        "PartDesign_Pocket",
+        "PartDesign_Revolution",
+        "PartDesign_Groove",
+        "PartDesign_AdditiveLoft",
+        "PartDesign_SubtractiveLoft",
+        "PartDesign_AdditivePipe",
+        "PartDesign_SubtractivePipe",
+        nullptr};
+    Watcher.push_back(new Gui::TaskView::TaskWatcherCommands(
+        "SELECT PartDesign::SubShapeBinder COUNT 1",
+        SubShapeBinder,
         "Modeling tools",
         "PartDesign_Body"
     ));
