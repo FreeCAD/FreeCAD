864b517e5e47270f39570eddbc2e37a80a184379
wmayer <wmayer@freecad.org>
2025-01-27 22:08:11 +0100

    PD: Fix regression about single-solid check
    For several PD features the single-solid check fails. The regression
    is caused by PR 13960. The reason for the failure is that the first
    solid of the output shape is retrieved and then checked for a single
    solid. This test will always pass, of course.
    
    The single-solid is fixed for these features:
    * Pad
    * Pocket (never worked there)
    * Fillet
    * Chamfer
    * Groove (never worked there)
    * Revolution (never worked there)
    * Loft
    
    This fixes issue 19002

--- src/Mod/PartDesign/App/FeatureFillet.cpp
+++ src/Mod/PartDesign/App/FeatureFillet.cpp
@@ -104,7 +104,6 @@ App::DocumentObjectExecReturn *Fillet::execute()
 
         TopTools_ListOfShape aLarg;
         aLarg.Append(baseShape.getShape());
-        bool failed = false;
         if (!BRepAlgo::IsValid(aLarg, shape.getShape(), Standard_False, Standard_False)) {
             ShapeFix_ShapeTolerance aSFT;
             aSFT.LimitTolerance(shape.getShape(),
@@ -113,20 +112,15 @@ App::DocumentObjectExecReturn *Fillet::execute()
                                 TopAbs_SHAPE);
         }
 
-        if (!failed) {
-            // store shape before refinement
-            this->rawShape = shape;
-            shape = refineShapeIfActive(shape);
-            shape = getSolid(shape);
-        }
+        // store shape before refinement
+        this->rawShape = shape;
+        shape = refineShapeIfActive(shape);
         if (!isSingleSolidRuleSatisfied(shape.getShape())) {
             return new App::DocumentObjectExecReturn(QT_TRANSLATE_NOOP("Exception", "Result has multiple solids: that is not currently supported."));
         }
-        this->Shape.setValue(shape);
 
-        if (failed) {
-            return new App::DocumentObjectExecReturn("Resulting shape is invalid");
-        }
+        shape = getSolid(shape);
+        this->Shape.setValue(shape);
         return App::DocumentObject::StdReturn;
     }
     catch (Standard_Failure& e) {
