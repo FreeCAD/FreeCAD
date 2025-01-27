27ae68f8476937fd6b4779d605ae9f799104b922
wmayer <wmayer@freecad.org>
2025-01-27 10:21:32 +0100

    PD: Restore GUI commands for PartDesign datum objects
    With PR 19114 the commands to create PD datum objects are replaced with
    the counterparts of Part. However, PD datum objects are much more powerful
    than the Part datum objects.
    (see e.g. https://forum.freecad.org/viewtopic.php?p=806960)
    
    So, this change brings these commands back to the task panel.

--- src/Mod/PartDesign/Gui/Workbench.cpp
+++ src/Mod/PartDesign/Gui/Workbench.cpp
@@ -163,6 +163,19 @@ void Workbench::activated()
         "PartDesign_Body"
     ));
 
+    const char* Vertex1[] = {
+        "PartDesign_Point",
+        "PartDesign_Line",
+        "PartDesign_Plane",
+        "PartDesign_CoordinateSystem",
+        nullptr};
+    Watcher.push_back(new Gui::TaskView::TaskWatcherCommands(
+        "SELECT Part::Feature SUBELEMENT Vertex COUNT 1..",
+        Vertex1,
+        "Datum objects",
+        "PartDesign_CoordinateSystem"
+    ));
+
     const char* Edge[] = {
         "PartDesign_Fillet",
         "PartDesign_Chamfer",
@@ -176,6 +189,19 @@ void Workbench::activated()
         Edge,
         "Edge Tools",
         "PartDesign_Body"
+    ));
+
+    const char* Edge1[] = {
+        "PartDesign_Point",
+        "PartDesign_Line",
+        "PartDesign_Plane",
+        "PartDesign_CoordinateSystem",
+        nullptr};
+    Watcher.push_back(new Gui::TaskView::TaskWatcherCommands(
+        "SELECT Part::Feature SUBELEMENT Edge COUNT 1..",
+        Edge1,
+        "Datum objects",
+        "PartDesign_CoordinateSystem"
     ));
 
     const char* Face[] = {
@@ -194,6 +220,19 @@ void Workbench::activated()
         Face,
         "Face Tools",
         "PartDesign_Body"
+    ));
+
+    const char* Face1[] = {
+        "PartDesign_Point",
+        "PartDesign_Line",
+        "PartDesign_Plane",
+        "PartDesign_CoordinateSystem",
+        nullptr};
+    Watcher.push_back(new Gui::TaskView::TaskWatcherCommands(
+        "SELECT Part::Feature SUBELEMENT Face COUNT 1",
+        Face1,
+        "Datum objects",
+        "PartDesign_CoordinateSystem"
     ));
 
     const char* Body[] = {
@@ -229,6 +268,7 @@ void Workbench::activated()
         "Helper Tools",
         "PartDesign_Body"
     ));
+
     const char* Plane2[] = {
         "PartDesign_NewSketch",
         "Part_DatumPoint",
@@ -243,6 +283,32 @@ void Workbench::activated()
         "PartDesign_Body"
     ));
 
+    const char* Plane3[] = {
+        "PartDesign_Point",
+        "PartDesign_Line",
+        "PartDesign_Plane",
+        "PartDesign_CoordinateSystem",
+        nullptr};
+    Watcher.push_back(new Gui::TaskView::TaskWatcherCommands(
+        "SELECT App::Plane COUNT 1",
+        Plane3,
+        "Datum objects",
+        "PartDesign_CoordinateSystem"
+    ));
+
+    const char* Plane4[] = {
+        "PartDesign_Point",
+        "PartDesign_Line",
+        "PartDesign_Plane",
+        "PartDesign_CoordinateSystem",
+        nullptr};
+    Watcher.push_back(new Gui::TaskView::TaskWatcherCommands(
+        "SELECT PartDesign::Plane COUNT 1",
+        Plane4,
+        "Datum objects",
+        "PartDesign_CoordinateSystem"
+    ));
+
     const char* Line[] = {
         "Part_DatumPoint",
         "Part_DatumLine",
@@ -253,6 +319,18 @@ void Workbench::activated()
         Line,
         "Helper Tools",
         "PartDesign_Body"
+    ));
+
+    const char* Line1[] = {
+        "PartDesign_Point",
+        "PartDesign_Line",
+        "PartDesign_Plane",
+        nullptr};
+    Watcher.push_back(new Gui::TaskView::TaskWatcherCommands(
+        "SELECT PartDesign::Line COUNT 1",
+        Line1,
+        "Datum objects",
+        "PartDesign_CoordinateSystem"
     ));
 
     const char* Point[] = {
@@ -266,6 +344,19 @@ void Workbench::activated()
         Point,
         "Helper Tools",
         "PartDesign_Body"
+    ));
+
+    const char* Point1[] = {
+        "PartDesign_Point",
+        "PartDesign_Line",
+        "PartDesign_Plane",
+        "PartDesign_CoordinateSystem",
+        nullptr};
+    Watcher.push_back(new Gui::TaskView::TaskWatcherCommands(
+        "SELECT PartDesign::Point COUNT 1",
+        Point1,
+        "Datum objects",
+        "PartDesign_CoordinateSystem"
     ));
 
     const char* NoSel[] = {
