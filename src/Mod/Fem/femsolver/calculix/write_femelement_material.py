a503e78e6da2b15b95683a5af6c6bff96f6ca022
FEA-eng <59876896+FEA-eng@users.noreply.github.com>
2025-01-27 19:33:06 +0100

    FEM: Update write_femelement_material.py

--- src/Mod/Fem/femsolver/calculix/write_femelement_material.py
+++ src/Mod/Fem/femsolver/calculix/write_femelement_material.py
@@ -81,6 +81,7 @@ def write_femelement_material(f, ccxwriter):
             if mat_obj.Category == "Solid":
                 TEC = FreeCAD.Units.Quantity(mat_obj.Material["ThermalExpansionCoefficient"])
                 TEC_in_mmK = TEC.getValueAs("mm/mm/K").Value
+                RT = float(mat_obj.Material["ThermalExpansionReferenceTemperature"])
             elif mat_obj.Category == "Fluid":
                 KV = FreeCAD.Units.Quantity(mat_obj.Material["KinematicViscosity"])
                 KV_in_mm2s = KV.getValueAs("mm^2/s").Value
@@ -100,7 +101,7 @@ def write_femelement_material(f, ccxwriter):
             if mat_obj.Category == "Solid":
                 f.write("*CONDUCTIVITY\n")
                 f.write(f"{TC_in_WmK:.13G}\n")
-                f.write("*EXPANSION\n")
+                f.write("*EXPANSION, ZERO={RT:.13G}\n")
                 f.write(f"{TEC_in_mmK:.13G}\n")
                 f.write("*SPECIFIC HEAT\n")
                 f.write(f"{SH_in_JkgK:.13G}\n")
