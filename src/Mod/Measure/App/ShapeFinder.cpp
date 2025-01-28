5ce6a90d8b9979e7f1948c064d4af57f1a23cf83
luzpaz <luzpaz@users.noreply.github.com>
2025-01-28 18:28:15 -0500

    Fix typos Found via codespell

--- src/Mod/Measure/App/ShapeFinder.cpp
+++ src/Mod/Measure/App/ShapeFinder.cpp
@@ -321,8 +321,8 @@ ShapeFinder::getGlobalTransform(const App::DocumentObject& rootObject, const std
 }
 
 
-//! trys to get the global position and scale for a object with no information about the path
-//! through the tree from a root to cursor object.
+//! tries to get the global position and scale for a object with no information about the
+//! path through the tree from a root to cursor object.
 std::pair<Base::Placement, Base::Matrix4D>
 ShapeFinder::getGlobalTransform(const App::DocumentObject* cursorObject)
 {
