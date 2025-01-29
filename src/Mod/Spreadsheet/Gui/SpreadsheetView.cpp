caa40023ecd04cdb01c703909655f54780a3ef72
Chris Hennes <chennes@gmail.com>
2025-01-29 11:54:04 +0100

    Spreadsheet: Migrate to using App::Application::getNameWithVersion

--- src/Mod/Spreadsheet/Gui/SpreadsheetView.cpp
+++ src/Mod/Spreadsheet/Gui/SpreadsheetView.cpp
@@ -23,7 +23,6 @@
 #include "PreCompiled.h"
 
 #ifndef _PreComp_
-#include <QApplication>
 #include <QPalette>
 #include <QPrintDialog>
 #include <QPrintPreviewDialog>
@@ -309,16 +308,7 @@ void SheetView::printPdf()
         printer.setPageOrientation(QPageLayout::Landscape);
         printer.setOutputFormat(QPrinter::PdfFormat);
         printer.setOutputFileName(filename);
-
-        QString appname = QCoreApplication::applicationName();
-        auto config = App::Application::Config();
-        QString major = QString::fromUtf8(config["BuildVersionMajor"].c_str());
-        QString minor = QString::fromUtf8(config["BuildVersionMinor"].c_str());
-        QString point = QString::fromUtf8(config["BuildVersionPoint"].c_str());
-        QString suffix = QString::fromUtf8(config["BuildVersionSuffix"].c_str());
-        printer.setCreator(QString::fromUtf8("%1 Spreadsheet %2.%3.%4%5")
-                               .arg(appname, major, minor, point, suffix));
-
+        printer.setCreator(QString::fromStdString(App::Application::getNameWithVersion()));
         print(&printer);
     }
 }
