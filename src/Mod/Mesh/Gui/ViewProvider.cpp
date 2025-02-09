4c066b68b44e32f0740d0eb0e22e9c016370f561
Benjamin Bræstrup Sayoc <benj5378@outlook.com>
2025-02-09 18:13:19 +0100

    Mesh: Use QStringLiteral

--- src/Mod/Mesh/Gui/ViewProvider.cpp
+++ src/Mod/Mesh/Gui/ViewProvider.cpp
@@ -1746,8 +1746,8 @@ void ViewProviderMesh::faceInfoCallback(void* ud, SoEventCallback* cb)
                 static_cast<const SoPointDetail*>(faceDetail->getPoint(2))->getCoordinateIndex();
             auto flag = new Gui::Flag;
             flag->setText(QObject::tr("Index: %1").arg(uFacet));
-            QString toolTip = QString::fromLatin1("Facet index: %1\n"
-                                                  "Points: <%2, %3, %4>")
+            QString toolTip = QStringLiteral("Facet index: %1\n"
+                                             "Points: <%2, %3, %4>")
                                   .arg(uFacet)
                                   .arg(point1)
                                   .arg(point2)
