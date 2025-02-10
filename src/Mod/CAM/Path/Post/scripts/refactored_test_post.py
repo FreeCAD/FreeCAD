0edcb29f09f0950d26980afffdaf216adfbd277b
LarryWoestman <68401843+LarryWoestman@users.noreply.github.com>
2025-02-10 08:37:57 -0800

    CAM:  converted the refactored* postprocessors to the new (more object-oriented) API (#19006)
    * CAM:  converted the refactored* postprocessors to the new API
    
          with corresponding tests
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/Mod/CAM/Path/Post/scripts/refactored_test_post.py
+++ src/Mod/CAM/Path/Post/scripts/refactored_test_post.py
@@ -1,6 +1,7 @@
 # ***************************************************************************
 # *   Copyright (c) 2014 sliptonic <shopinthewoods@gmail.com>               *
-# *   Copyright (c) 2022 Larry Woestman <LarryWoestman2@gmail.com>          *
+# *   Copyright (c) 2022 - 2025 Larry Woestman <LarryWoestman2@gmail.com>   *
+# *   Copyright (c) 2024 Ondsel <development@ondsel.com>                    *
 # *                                                                         *
 # *   This file is part of the FreeCAD CAx development system.              *
 # *                                                                         *
@@ -22,200 +23,264 @@
 # *                                                                         *
 # ***************************************************************************
 
-
 import argparse
 
-from typing import Any, Dict, Union
+from typing import Any, Dict, List, Optional, Tuple, Union
 
-import Path.Post.UtilsArguments as UtilsArguments
-import Path.Post.UtilsExport as UtilsExport
+from Path.Post.Processor import PostProcessor
+import Path.Post.UtilsArguments as PostUtilsArguments
+import Path.Post.UtilsExport as PostUtilsExport
 
-# Define some types that are used throughout this file
+import Path
+import FreeCAD
+
+translate = FreeCAD.Qt.translate
+
+DEBUG = False
+if DEBUG:
+    Path.Log.setLevel(Path.Log.Level.DEBUG, Path.Log.thisModule())
+    Path.Log.trackModule(Path.Log.thisModule())
+else:
+    Path.Log.setLevel(Path.Log.Level.INFO, Path.Log.thisModule())
+
+#
+# Define some types that are used throughout this file.
+#
+Defaults = Dict[str, bool]
+FormatHelp = str
+GCodeOrNone = Optional[str]
+GCodeSections = List[Tuple[str, GCodeOrNone]]
 Parser = argparse.ArgumentParser
+ParserArgs = Union[None, str, argparse.Namespace]
+Postables = Union[List, List[Tuple[str, List]]]
+Section = Tuple[str, List]
+Sublist = List
+Units = str
 Values = Dict[str, Any]
-
-#
-# The following variables need to be global variables
-# to keep the PathPostProcessor.load method happy:
-#
-#    TOOLTIP
-#    TOOLTIP_ARGS
-#    UNITS
-#
-#    The "argument_defaults", "arguments_visible", and the "values" hashes
-#    need to be defined before the "init_shared_arguments" routine can be
-#    called to create TOOLTIP_ARGS, so they also end up having to be globals.
-#
-TOOLTIP: str = """This is a postprocessor file for the Path workbench. It is used to
-test the postprocessor code.  It probably isn't useful for "real" gcode.
-
-import refactored_test_post
-refactored_test_post.export(object,"/path/to/file.ncc","")
-"""
-#
-# Default to metric mode
-#
-UNITS: str = "G21"
+Visible = Dict[str, bool]
 
 
-def init_values(values: Values) -> None:
-    """Initialize values that are used throughout the postprocessor."""
-    #
-    global UNITS
+class Refactored_Test(PostProcessor):
+    """The Refactored Test post processor class."""
 
-    UtilsArguments.init_shared_values(values)
-    #
-    # Set any values here that need to override the default values set
-    # in the init_shared_values routine.
-    #
-    # Turn off as much functionality as possible by default.
-    # Then the tests can turn back on the appropriate options as needed.
-    #
-    # Used in the argparser code as the "name" of the postprocessor program.
-    # This would normally show up in the usage message in the TOOLTIP_ARGS,
-    # but we are suppressing the usage message, so it doesn't show up after all.
-    #
-    values["MACHINE_NAME"] = "test"
-    #
-    # Don't output comments by default
-    #
-    values["OUTPUT_COMMENTS"] = False
-    #
-    # Don't output the header by default
-    #
-    values["OUTPUT_HEADER"] = False
-    #
-    # Convert M56 tool change commands to comments,
-    # which are then suppressed by default.
-    #
-    values["OUTPUT_TOOL_CHANGE"] = False
-    values["POSTPROCESSOR_FILE_NAME"] = __name__
-    #
-    # Do not show the editor by default since we are testing.
-    #
-    values["SHOW_EDITOR"] = False
-    #
-    # Don't show the current machine units by default
-    #
-    values["SHOW_MACHINE_UNITS"] = False
-    #
-    # Don't show the current operation label by default.
-    #
-    values["SHOW_OPERATION_LABELS"] = False
-    #
-    # Don't output an M5 command to stop the spindle after an M6 tool change by default.
-    #
-    values["STOP_SPINDLE_FOR_TOOL_CHANGE"] = False
-    #
-    # Don't output a G43 tool length command following tool changes by default.
-    #
-    values["USE_TLO"] = False
-    values["UNITS"] = UNITS
+    def __init__(self, job) -> None:
+        super().__init__(
+            job=job,
+            tooltip=translate("CAM", "Refactored Test post processor"),
+            tooltipargs=[""],
+            units="Metric",
+        )
+        self.reinitialize()
+        Path.Log.debug("Refactored Test post processor initialized")
 
+    def reinitialize(self) -> None:
+        """Initialize or reinitialize the 'core' data structures for the postprocessor."""
+        #
+        # This is also used to reinitialize the data structures between tests.
+        #
+        self.values: Values = {}
+        self.init_values(self.values)
+        self.argument_defaults: Defaults = {}
+        self.init_argument_defaults(self.argument_defaults)
+        self.arguments_visible: Visible = {}
+        self.init_arguments_visible(self.arguments_visible)
+        self.parser: Parser = self.init_arguments(
+            self.values, self.argument_defaults, self.arguments_visible
+        )
+        #
+        # Create another parser just to get a list of all possible arguments
+        # that may be output using --output_all_arguments.
+        #
+        self.all_arguments_visible: Visible = {}
+        for k in iter(self.arguments_visible):
+            self.all_arguments_visible[k] = True
+        self.all_visible: Parser = self.init_arguments(
+            self.values, self.argument_defaults, self.all_arguments_visible
+        )
 
-def init_argument_defaults(argument_defaults: Dict[str, bool]) -> None:
-    """Initialize which arguments (in a pair) are shown as the default argument."""
-    UtilsArguments.init_argument_defaults(argument_defaults)
-    #
-    # Modify which argument to show as the default in flag-type arguments here.
-    # If the value is True, the first argument will be shown as the default.
-    # If the value is False, the second argument will be shown as the default.
-    #
-    # For example, if you want to show Metric mode as the default, use:
-    #   argument_defaults["metric_inch"] = True
-    #
-    # If you want to show that "Don't pop up editor for writing output" is
-    # the default, use:
-    #   argument_defaults["show-editor"] = False.
-    #
-    # Note:  You also need to modify the corresponding entries in the "values" hash
-    #        to actually make the default value(s) change to match.
-    #
+    def init_values(self, values: Values) -> None:
+        """Initialize values that are used throughout the postprocessor."""
+        #
+        PostUtilsArguments.init_shared_values(values)
+        #
+        # Set any values here that need to override the default values set
+        # in the init_shared_values routine.
+        #
+        # Used in the argparser code as the "name" of the postprocessor program.
+        #
+        values["MACHINE_NAME"] = "test"
+        #
+        # Don't output comments by default.
+        #
+        values["OUTPUT_COMMENTS"] = False
+        #
+        # Don't output the header by default.
+        #
+        values["OUTPUT_HEADER"] = False
+        #
+        # Convert M56 tool change commands to comments,
+        # which are then suppressed by default.
+        #
+        values["OUTPUT_TOOL_CHANGE"] = False
+        values["POSTPROCESSOR_FILE_NAME"] = __name__
+        #
+        # Do not show the editor by default since we are testing.
+        #
+        values["SHOW_EDITOR"] = False
+        #
+        # Don't show the current machine units by default.
+        #
+        values["SHOW_MACHINE_UNITS"] = False
+        #
+        # Don't show the current operation label by default.
+        #
+        values["SHOW_OPERATION_LABELS"] = False
+        #
+        # Don't output an M5 command to stop the spindle after an M6 tool change by default.
+        #
+        values["STOP_SPINDLE_FOR_TOOL_CHANGE"] = False
+        #
+        # Don't output a G43 tool length command following tool changes by default.
+        #
+        values["USE_TLO"] = False
+        values["UNITS"] = self._units
 
+    def init_argument_defaults(self, argument_defaults: Defaults) -> None:
+        """Initialize which arguments (in a pair) are shown as the default argument."""
+        PostUtilsArguments.init_argument_defaults(argument_defaults)
+        #
+        # Modify which argument to show as the default in flag-type arguments here.
+        # If the value is True, the first argument will be shown as the default.
+        # If the value is False, the second argument will be shown as the default.
+        #
+        # For example, if you want to show Metric mode as the default, use:
+        #   argument_defaults["metric_inch"] = True
+        #
+        # If you want to show that "Don't pop up editor for writing output" is
+        # the default, use:
+        #   argument_defaults["show-editor"] = False.
+        #
+        # Note:  You also need to modify the corresponding entries in the "values" hash
+        #        to actually make the default value(s) change to match.
+        #
 
-def init_arguments_visible(arguments_visible: Dict[str, bool]) -> None:
-    """Initialize which argument pairs are visible in TOOLTIP_ARGS."""
-    key: str
+    def init_arguments_visible(self, arguments_visible: Visible) -> None:
+        """Initialize which argument pairs are visible in TOOLTIP_ARGS."""
+        PostUtilsArguments.init_arguments_visible(arguments_visible)
+        #
+        # Modify the visibility of any arguments from the defaults here.
+        #
+        # Make all arguments invisible by default.
+        #
+        for key in iter(arguments_visible):
+            arguments_visible[key] = False
 
-    UtilsArguments.init_arguments_visible(arguments_visible)
-    #
-    # Modify the visibility of any arguments from the defaults here.
-    #
-    #
-    # Make all arguments invisible by default.
-    #
-    for key in iter(arguments_visible):
-        arguments_visible[key] = False
+    def init_arguments(
+        self,
+        values: Values,
+        argument_defaults: Defaults,
+        arguments_visible: Visible,
+    ) -> Parser:
+        """Initialize the shared argument definitions."""
+        _parser: Parser = PostUtilsArguments.init_shared_arguments(
+            values, argument_defaults, arguments_visible
+        )
+        #
+        # Add any argument definitions that are not shared with other postprocessors here.
+        #
+        return _parser
 
+    def process_arguments(self) -> Tuple[bool, ParserArgs]:
+        """Process any arguments to the postprocessor."""
+        #
+        # This function is separated out to make it easier to inherit from this postprocessor.
+        #
+        args: ParserArgs
+        flag: bool
 
-def init_arguments(
-    values: Values,
-    argument_defaults: Dict[str, bool],
-    arguments_visible: Dict[str, bool],
-) -> Parser:
-    """Initialize the shared argument definitions."""
-    parser: Parser
+        (flag, args) = PostUtilsArguments.process_shared_arguments(
+            self.values, self.parser, self._job.PostProcessorArgs, self.all_visible, "-"
+        )
+        #
+        # If the flag is True, then all of the arguments should be processed normally.
+        #
+        if flag:
+            #
+            # Process any additional arguments here.
+            #
+            #
+            # Update any variables that might have been modified while processing the arguments.
+            #
+            self._units = self.values["UNITS"]
+        #
+        # If the flag is False, then args is either None (indicating an error while
+        # processing the arguments) or a string containing the argument list formatted
+        # for output.  Either way the calling routine will need to handle the args value.
+        #
+        return (flag, args)
 
-    parser = UtilsArguments.init_shared_arguments(values, argument_defaults, arguments_visible)
-    #
-    # Add any argument definitions that are not shared with all other
-    # postprocessors here.
-    #
-    return parser
+    def process_postables(self) -> GCodeSections:
+        """Postprocess the 'postables' in the job to g code sections."""
+        #
+        # This function is separated out to make it easier to inherit from this postprocessor.
+        #
+        gcode: GCodeOrNone
+        g_code_sections: GCodeSections
+        partname: str
+        postables: Postables
+        section: Section
+        sublist: Sublist
 
+        postables = self._buildPostList()
 
-#
-# Creating global variables and using functions to modify them
-# is useful for being able to test things later.
-#
-global_values: Values = {}
-init_values(global_values)
-global_argument_defaults: Dict[str, bool] = {}
-init_argument_defaults(global_argument_defaults)
-global_arguments_visible: Dict[str, bool] = {}
-init_arguments_visible(global_arguments_visible)
-global_parser: Parser = init_arguments(
-    global_values, global_argument_defaults, global_arguments_visible
-)
-#
-# The TOOLTIP_ARGS value is created from the help information about the arguments.
-#
-TOOLTIP_ARGS: str = global_parser.format_help()
-#
-# Create another parser just to get a list of all possible arguments
-# that may be output using --output_all_arguments.
-#
-global_all_arguments_visible: Dict[str, bool] = {}
-k: str
-for k in iter(global_arguments_visible):
-    global_all_arguments_visible[k] = True
-global_all_visible: Parser = init_arguments(
-    global_values, global_argument_defaults, global_all_arguments_visible
-)
+        Path.Log.debug(f"postables count: {len(postables)}")
 
+        g_code_sections = []
+        for _, section in enumerate(postables):
+            partname, sublist = section
+            gcode = PostUtilsExport.export_common(self.values, sublist, "-")
+            g_code_sections.append((partname, gcode))
 
-def export(objectslist, filename: str, argstring: str) -> str:
-    """Postprocess the objects in objectslist to filename."""
-    args: Union[str, argparse.Namespace]
-    flag: bool
+        return g_code_sections
 
-    global UNITS  # pylint: disable=global-statement
+    def export(self) -> Union[None, GCodeSections]:
+        """Process the parser arguments, then postprocess the 'postables'."""
+        args: ParserArgs
+        flag: bool
 
-    # print(parser.format_help())
+        Path.Log.debug("Exporting the job")
 
-    (flag, args) = UtilsArguments.process_shared_arguments(
-        global_values, global_parser, argstring, global_all_visible, filename
-    )
-    if not flag:
-        return args  # type: ignore
-    #
-    # Process any additional arguments here
-    #
+        (flag, args) = self.process_arguments()
+        #
+        # If the flag is True, then continue postprocessing the 'postables'.
+        #
+        if flag:
+            return self.process_postables()
+        #
+        # The flag is False meaning something unusual happened.
+        #
+        # If args is None then there was an error during argument processing.
+        #
+        if args is None:
+            return None
+        #
+        # Otherwise args will contain the argument list formatted for output
+        # instead of the "usual" gcode.
+        #
+        return [("allitems", args)]  # type: ignore
 
-    #
-    # Update the global variables that might have been modified
-    # while processing the arguments.
-    #
-    UNITS = global_values["UNITS"]
+    @property
+    def tooltip(self):
+        tooltip: str = """
+        This is a postprocessor file for the CAM workbench.  It is used
+        to test the postprocessor code.  It probably isn't useful for "real" gcode.
+        """
+        return tooltip
 
-    return UtilsExport.export_common(global_values, objectslist, filename)
+    @property
+    def tooltipArgs(self) -> FormatHelp:
+        return self.parser.format_help()
+
+    @property
+    def units(self) -> Units:
+        return self._units
