a87b5c495f01ef661cbb6e47efc309c6657fab70
Chris Hennes <chennes@pioneerlibrarysystem.org>
2025-02-12 10:20:03 -0600

    TD: Add guards around QMessageBox include

--- src/Mod/TechDraw/Gui/CommandAlign.cpp
+++ src/Mod/TechDraw/Gui/CommandAlign.cpp
@@ -19,6 +19,9 @@
  ***************************************************************************/
 
 #include "PreCompiled.h"
+#ifndef _PreComp_
+#include <QMessageBox>
+#endif
 
 #include <App/DocumentObject.h>
 #include <Base/Console.h>
