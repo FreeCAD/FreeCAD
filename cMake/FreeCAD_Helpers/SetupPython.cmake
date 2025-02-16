b67ab979afdba808fc03196faf9c5f8830df471f
Joao Matos <joao@tritao.eu>
2025-02-16 16:39:38 +0000

    CMake: Update minimum Python version to 3.10

--- cMake/FreeCAD_Helpers/SetupPython.cmake
+++ cMake/FreeCAD_Helpers/SetupPython.cmake
@@ -3,8 +3,8 @@ macro(SetupPython)
 
     find_package(Python3 COMPONENTS Interpreter Development REQUIRED)
 
-    if (${Python3_VERSION} VERSION_LESS "3.8")
-         message(FATAL_ERROR "To build FreeCAD you need at least Python 3.8\n")
+    if (${Python3_VERSION} VERSION_LESS "3.10")
+         message(FATAL_ERROR "To build FreeCAD you need at least Python 3.10\n")
     endif()
 
 endmacro(SetupPython)
