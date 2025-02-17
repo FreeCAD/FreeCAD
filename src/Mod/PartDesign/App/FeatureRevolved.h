c0e306e69ff0274de952d36213d2166ed33af70e
wmayer <wmayer@freecad.org>
2025-02-17 15:49:33 +0100

    PD: Clean-up revolution & groove code

--- src/Mod/PartDesign/App/FeatureRevolved.h
+++ src/Mod/PartDesign/App/FeatureRevolved.h
@@ -71,9 +71,6 @@ public:
     };
 
 protected:
-    /// updates Axis from ReferenceAxis
-    void updateAxis();
-
     /** Recalculate the feature
       * Revolves the Sketch around the given Axis (with basepoint Base)
       * The angle of the revolution is given by Angle.
@@ -82,6 +79,22 @@ protected:
       * The created material will be fused with the sketch support (if there is one)
       */
     App::DocumentObjectExecReturn *executeRevolved(Part::RevolMode revolMode);
+
+private:
+    App::DocumentObjectExecReturn *tryExecuteRevolved(Part::RevolMode revolMode);
+    TopoShape tryGetBaseShape() const;
+    TopoShape tryGetSupportShape() const;
+    TopoShape tryToRevolveToFace(const TopLoc_Location& invObjLoc,
+                                 gp_Pnt pnt,
+                                 gp_Dir dir,
+                                 TopoShape& base,
+                                 TopoShape& supportface,
+                                 const TopoShape& sketchshape,
+                                 Part::RevolMode revolMode) const;
+    void setResult(const TopoShape& base, const TopoShape& revolved);
+
+    /// updates Axis from ReferenceAxis
+    void updateAxis();
 
     RevolMethod methodFromString(const std::string& methodStr);
 
