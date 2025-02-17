78237057c8e3e7f4db0902034503dd5d88102428
wmayer <wmayer@freecad.org>
2025-02-17 14:43:58 +0100

    PD: Move common code of Revolution and Groove to base class

--- src/Mod/PartDesign/App/AppPartDesign.cpp
+++ src/Mod/PartDesign/App/AppPartDesign.cpp
@@ -108,6 +108,7 @@ PyMOD_INIT_FUNC(_PartDesign)
     PartDesign::Pad                         ::init();
     PartDesign::Pocket                      ::init();
     PartDesign::Fillet                      ::init();
+    PartDesign::Revolved                    ::init();
     PartDesign::Revolution                  ::init();
     PartDesign::Groove                      ::init();
     PartDesign::Chamfer                     ::init();
