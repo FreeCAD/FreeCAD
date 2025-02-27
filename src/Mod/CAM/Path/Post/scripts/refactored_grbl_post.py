f680ee7af983f2d0ef9a841a2d36d4e7808f4045
Lawrence Woestman <larrywoestman2@gmail.com>
2025-02-27 15:08:58 -0800

    CAM:  Used inheritance to simplify the refactored postprocessors more
        Updated the Masso tests now that it is using the new API.

--- src/Mod/CAM/Path/Post/scripts/refactored_grbl_post.py
+++ src/Mod/CAM/Path/Post/scripts/refactored_grbl_post.py
@@ -25,11 +25,9 @@
 
 import argparse
 
-from typing import Any, Dict, List, Optional, Tuple, Union
+from typing import Any, Dict
 
 from Path.Post.Processor import PostProcessor
-import Path.Post.UtilsArguments as PostUtilsArguments
-import Path.Post.UtilsExport as PostUtilsExport
 
 import Path
 import FreeCAD
@@ -47,15 +45,6 @@ else:
 # Define some types that are used throughout this file.
 #
 Defaults = Dict[str, bool]
-FormatHelp = str
-GCodeOrNone = Optional[str]
-GCodeSections = List[Tuple[str, GCodeOrNone]]
-Parser = argparse.ArgumentParser
-ParserArgs = Union[None, str, argparse.Namespace]
-Postables = Union[List, List[Tuple[str, List]]]
-Section = Tuple[str, List]
-Sublist = List
-Units = str
 Values = Dict[str, Any]
 Visible = Dict[str, bool]
 
@@ -63,48 +52,28 @@ Visible = Dict[str, bool]
 class Refactored_Grbl(PostProcessor):
     """The Refactored Grbl post processor class."""
 
-    def __init__(self, job) -> None:
+    def __init__(
+        self,
+        job,
+        tooltip=translate("CAM", "Refactored Grbl post processor"),
+        tooltipargs=[""],
+        units="Metric",
+    ) -> None:
         super().__init__(
             job=job,
-            tooltip=translate("CAM", "Refactored Grbl post processor"),
-            tooltipargs=[""],
-            units="Metric",
+            tooltip=tooltip,
+            tooltipargs=tooltipargs,
+            units=units,
         )
-        self.reinitialize()
         Path.Log.debug("Refactored Grbl post processor initialized.")
-
-    def reinitialize(self) -> None:
-        """Initialize or reinitialize the 'core' data structures for the postprocessor."""
-        #
-        # This is also used to reinitialize the data structures between tests.
-        #
-        self.values: Values = {}
-        self.init_values(self.values)
-        self.argument_defaults: Defaults = {}
-        self.init_argument_defaults(self.argument_defaults)
-        self.arguments_visible: Visible = {}
-        self.init_arguments_visible(self.arguments_visible)
-        self.parser: Parser = self.init_arguments(
-            self.values, self.argument_defaults, self.arguments_visible
-        )
-        #
-        # Create another parser just to get a list of all possible arguments
-        # that may be output using --output_all_arguments.
-        #
-        self.all_arguments_visible: Visible = {}
-        for k in iter(self.arguments_visible):
-            self.all_arguments_visible[k] = True
-        self.all_visible: Parser = self.init_arguments(
-            self.values, self.argument_defaults, self.all_arguments_visible
-        )
 
     def init_values(self, values: Values) -> None:
         """Initialize values that are used throughout the postprocessor."""
         #
-        PostUtilsArguments.init_shared_values(values)
+        super().init_values(values)
         #
         # Set any values here that need to override the default values set
-        # in the init_shared_values routine.
+        # in the parent routine.
         #
         #
         # If this is set to True, then commands that are placed in
@@ -168,7 +137,6 @@ M2"""
         # Do not show the current machine units just before the PRE_OPERATION.
         #
         values["SHOW_MACHINE_UNITS"] = False
-        values["UNITS"] = self._units
         #
         # Default to not outputting a G43 following tool changes
         #
@@ -176,7 +144,7 @@ M2"""
 
     def init_argument_defaults(self, argument_defaults: Defaults) -> None:
         """Initialize which arguments (in a pair) are shown as the default argument."""
-        PostUtilsArguments.init_argument_defaults(argument_defaults)
+        super().init_argument_defaults(argument_defaults)
         #
         # Modify which argument to show as the default in flag-type arguments here.
         # If the value is True, the first argument will be shown as the default.
@@ -197,7 +165,7 @@ M2"""
 
     def init_arguments_visible(self, arguments_visible: Visible) -> None:
         """Initialize which argument pairs are visible in TOOLTIP_ARGS."""
-        PostUtilsArguments.init_arguments_visible(arguments_visible)
+        super().init_arguments_visible(arguments_visible)
         #
         # Modify the visibility of any arguments from the defaults here.
         #
@@ -209,100 +177,6 @@ M2"""
         arguments_visible["translate_drill"] = True
         arguments_visible["wait-for-spindle"] = True
 
-    def init_arguments(
-        self,
-        values: Values,
-        argument_defaults: Defaults,
-        arguments_visible: Visible,
-    ) -> Parser:
-        """Initialize the shared argument definitions."""
-        _parser: Parser = PostUtilsArguments.init_shared_arguments(
-            values, argument_defaults, arguments_visible
-        )
-        #
-        # Add any argument definitions that are not shared with other postprocessors here.
-        #
-        return _parser
-
-    def process_arguments(self) -> Tuple[bool, ParserArgs]:
-        """Process any arguments to the postprocessor."""
-        #
-        # This function is separated out to make it easier to inherit from this postprocessor.
-        #
-        args: ParserArgs
-        flag: bool
-
-        (flag, args) = PostUtilsArguments.process_shared_arguments(
-            self.values, self.parser, self._job.PostProcessorArgs, self.all_visible, "-"
-        )
-        #
-        # If the flag is True, then all of the arguments should be processed normally.
-        #
-        if flag:
-            #
-            # Process any additional arguments here.
-            #
-            #
-            # Update any variables that might have been modified while processing the arguments.
-            #
-            self._units = self.values["UNITS"]
-        #
-        # If the flag is False, then args is either None (indicating an error while
-        # processing the arguments) or a string containing the argument list formatted
-        # for output.  Either way the calling routine will need to handle the args value.
-        #
-        return (flag, args)
-
-    def process_postables(self) -> GCodeSections:
-        """Postprocess the 'postables' in the job to g code sections."""
-        #
-        # This function is separated out to make it easier to inherit from this postprocessor.
-        #
-        gcode: GCodeOrNone
-        g_code_sections: GCodeSections
-        partname: str
-        postables: Postables
-        section: Section
-        sublist: Sublist
-
-        postables = self._buildPostList()
-
-        Path.Log.debug(f"postables count: {len(postables)}")
-
-        g_code_sections = []
-        for _, section in enumerate(postables):
-            partname, sublist = section
-            gcode = PostUtilsExport.export_common(self.values, sublist, "-")
-            g_code_sections.append((partname, gcode))
-
-        return g_code_sections
-
-    def export(self) -> Union[None, GCodeSections]:
-        """Process the parser arguments, then postprocess the 'postables'."""
-        args: ParserArgs
-        flag: bool
-
-        Path.Log.debug("Exporting the job")
-
-        (flag, args) = self.process_arguments()
-        #
-        # If the flag is True, then continue postprocessing the 'postables'.
-        #
-        if flag:
-            return self.process_postables()
-        #
-        # The flag is False meaning something unusual happened.
-        #
-        # If args is None then there was an error during argument processing.
-        #
-        if args is None:
-            return None
-        #
-        # Otherwise args will contain the argument list formatted for output
-        # instead of the "usual" gcode.
-        #
-        return [("allitems", args)]  # type: ignore
-
     @property
     def tooltip(self):
         tooltip: str = """
@@ -311,11 +185,3 @@ M2"""
         and output 'real' GCode suitable for a Grbl 3 axis mill.
         """
         return tooltip
-
-    @property
-    def tooltipArgs(self) -> FormatHelp:
-        return self.parser.format_help()
-
-    @property
-    def units(self) -> Units:
-        return self._units
