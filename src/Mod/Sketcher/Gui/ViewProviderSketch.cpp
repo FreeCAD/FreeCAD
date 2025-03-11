f0656103e3342ba7d14ef52ebb860426384ab899
tringenbach <tim.ringenbach@gmail.com>
2025-03-10 20:34:51 -0500

    Apply suggestions from code review
    Co-authored-by: Kacper Donat <kadet1090@gmail.com>
    (cherry picked from commit 28b52086e9cceae0e095421985479c546df49e44)

--- src/Mod/Sketcher/Gui/ViewProviderSketch.cpp
+++ src/Mod/Sketcher/Gui/ViewProviderSketch.cpp
@@ -3971,9 +3971,8 @@ int ViewProviderSketch::defaultFontSizePixels() const
 
 qreal ViewProviderSketch::getDevicePixelRatio() const
 {
-    auto activeView = this->getActiveView();
-    if (activeView) {
-        auto glWidget = qobject_cast<Gui::View3DInventor*>(activeView)->getViewer()->getGLWidget();
+    if (auto activeView = qobject_cast<Gui::View3DInventor*>(this->getActiveView())) {
+        auto glWidget = activeView->getViewer()->getGLWidget();
         return glWidget->devicePixelRatio();
     }
 
