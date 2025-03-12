46c2d76fe0e7c9cb315fedfeb0114e813647d6d7
bofdahof <172177156+bofdahof@users.noreply.github.com>
2025-03-12 17:48:38 +1000

    Measure: apply std::ranges

--- src/Mod/Measure/Gui/ViewProviderMeasureBase.cpp
+++ src/Mod/Measure/Gui/ViewProviderMeasureBase.cpp
@@ -40,6 +40,8 @@
 #include <Inventor/SbViewportRegion.h>
 #endif
 
+#include <ranges>
+
 #include <App/DocumentObject.h>
 #include <Base/Console.h>
 #include <Gui/BitmapFactory.h>
@@ -351,8 +353,7 @@ void ViewProviderMeasureBase::updateData(const App::Property* prop)
 
     // Check if one of the input properties has been changed
     auto inputProps = obj->getInputProps();
-    if (std::find(inputProps.begin(), inputProps.end(), std::string(prop->getName()))
-        != inputProps.end()) {
+    if (std::ranges::find(inputProps, std::string(prop->getName())) != inputProps.end()) {
         doUpdate = true;
 
         // Add connections to be notified when the measured objects are changed
