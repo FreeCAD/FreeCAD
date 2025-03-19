a54735a79ecf08b0d97db452e1d0cc8d5496ed57
PaddleStroke <pierrelouis.boyer@gmail.com>
2025-03-19 12:29:04 +0100

    fix edge case bug

--- src/Mod/Sketcher/Gui/DrawSketchHandlerSlot.h
+++ src/Mod/Sketcher/Gui/DrawSketchHandlerSlot.h
@@ -469,7 +469,7 @@ void DSHSlotController::adaptParameters(Base::Vector2d onSketchPos)
                                         Base::toDegrees(range),
                                         Base::Unit::Angle);
             }
-            else {
+            else if (vec.Length() > Precision::Confusion()) {
                 double ovpRange =
                     Base::toRadians(onViewParameters[OnViewParameter::Fourth]->getValue());
 
