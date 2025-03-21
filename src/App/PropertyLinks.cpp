fc777ec32e0e7fc4d253cc828efc44631ed89f6f
Chris Hennes <chennes@pioneerlibrarysystem.org>
2025-03-20 20:57:06 -0500

    App: Don't use std::forward inside loop
    std::forward implies that sometimes its argument will be *moved*, leaving it invalid in the calling code. If that call is inside a loop that tries to reuse the data we have undefined behavior.

--- src/App/PropertyLinks.cpp
+++ src/App/PropertyLinks.cpp
@@ -1955,7 +1955,7 @@ std::vector<std::string> updateLinkSubs(const App::DocumentObject* obj,
     std::vector<std::string> res;
     for (auto it = subs.begin(); it != subs.end(); ++it) {
         const auto& sub = *it;
-        auto new_sub = (*f)(obj, sub.c_str(), std::forward<Args>(args)...);
+        auto new_sub = (*f)(obj, sub.c_str(), args...);
         if (new_sub.size()) {
             if (res.empty()) {
                 res.reserve(subs.size());
