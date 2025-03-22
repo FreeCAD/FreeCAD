5d13fcb7c52c28a4bb07663da62cbfe0eae7717c
Adrian Insaurralde <adrianinsaval@gmail.com>
2025-03-22 12:06:11 -0300

    Enable pcl by default on non windows systems

--- cMake/FreeCAD_Helpers/InitializeFreeCADBuildOptions.cmake
+++ cMake/FreeCAD_Helpers/InitializeFreeCADBuildOptions.cmake
@@ -178,7 +178,7 @@ macro(InitializeFreeCADBuildOptions)
     endif(MSVC)
     if(NOT MSVC)
         option(BUILD_FEM_NETGEN "Build the FreeCAD FEM module with the NETGEN mesher" OFF)
-        option(FREECAD_USE_PCL "Build the features that use PCL libs" OFF)
+        option(FREECAD_USE_PCL "Build the features that use PCL libs" ON)
     endif(NOT MSVC)
 
     # if this is set override some options
