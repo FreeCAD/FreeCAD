d7771a45f413fc8737717f8e8d8639ff0b424a5e
marioalexis <mario.passaglia@gmail.com>
2025-03-24 01:23:15 -0300

    Fem: Add method to rename pipeline VTK data arrays

--- src/Mod/Fem/App/FemPostPipelinePyImp.cpp
+++ src/Mod/Fem/App/FemPostPipelinePyImp.cpp
@@ -288,6 +288,29 @@ PyObject* FemPostPipelinePy::holdsPostObject(PyObject* args)
     return Py_BuildValue("O", (ok ? Py_True : Py_False));
 }
 
+PyObject* FemPostPipelinePy::renameArrays(PyObject* args)
+{
+    PyObject* pyObj;
+    if (!PyArg_ParseTuple(args, "O!", &(PyDict_Type), &pyObj)) {
+        return nullptr;
+    }
+
+    Py::Dict pyNames {pyObj};
+    std::map<std::string, std::string> names {};
+    for (auto it = pyNames.begin(); it != pyNames.end(); ++it) {
+        const auto& value = *it;
+        if (!value.first.isString() || !static_cast<Py::Object>(value.second).isString()) {
+            PyErr_SetString(PyExc_TypeError, "Names must be string objects");
+            return nullptr;
+        }
+        names[value.first.as_string()] = static_cast<Py::Object>(value.second).as_string();
+    }
+
+    getFemPostPipelinePtr()->renameArrays(names);
+
+    Py_Return;
+}
+
 PyObject* FemPostPipelinePy::getCustomAttributes(const char* /*attr*/) const
 {
     return nullptr;
