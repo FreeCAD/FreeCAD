46e44cf7b466b607f5fe612577b52cabe28cd945
Joao Matos <joao@tritao.eu>
2025-03-24 19:06:26 +0000

    Material: Convert XML bindings to Python.

---
+++ src/Mod/Material/App/MaterialProperty.pyi
@@ -0,0 +1,25 @@
+from Base.Metadata import export
+from ModelProperty import ModelProperty
+from typing import Final
+
+@export(
+    Include="Mod/Material/App/Materials.h",
+    Namespace="Materials",
+    FatherInclude="Mod/Material/App/Model.h",
+    FatherNamespace="Materials",
+    Constructor=True,
+    Delete=False,
+)
+class MaterialProperty(ModelProperty):
+    """
+    Material property descriptions.
+    
+    Author: DavidCarter (dcarter@davidcarter.ca)
+    Licence: LGPL
+    """
+
+    Value: Final[object] = None
+    """The value of the material property."""
+
+    Empty: Final[bool] = False
+    """The property value is undefined."""
