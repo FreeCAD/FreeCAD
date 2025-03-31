8724ebc56b430103f502caeb943c1ec0ec20df1f
J-Dunn <dunn@piments.com>
2025-03-31 10:47:00 +0200

    Add Helix FeedRate radial adjustment
    Calculate spindle feed rate compensation to keep tool cutting edge speed roughly constant as helix radius varies. 
    Reduces spindle feedrate when outer edge is cutting
    Increases spindle rate when cutting outside in to maintain tool speed on inner cuts.

--- src/Mod/CAM/Path/Base/Generator/helix.py
+++ src/Mod/CAM/Path/Base/Generator/helix.py
@@ -21,8 +21,9 @@
 # ***************************************************************************
 
 
-from numpy import ceil, linspace, isclose
+from numpy import ceil, linspace, isclose, delete
 import Path
+import math 
 
 __title__ = "Helix toolpath Generator"
 __author__ = "sliptonic (Brad Collette)"
@@ -47,6 +48,7 @@ def generate(
     inner_radius=0.0,
     direction="CW",
     startAt="Outside",
+    feedRateAdj=False,
 ):
     """generate(edge, hole_radius, inner_radius, step_over) ... generate helix commands.
     hole_radius, inner_radius: outer and inner radius of the hole
@@ -68,11 +70,12 @@ def generate(
             tool_diameter,
             direction,
             startAt,
+            feedRateAdj,
         )
     )
 
-    # inner_radius contains not a radius but the value from Extra Offset, which is the distance between the hole radius as designed and the hole radius to be cut.
-    # hole_radius contains the designed hole radius - inner_radius.
+    # hole_radius contains the designed hole radius - OffsetExtra.
+    # inner_radius contains the start radius + OffsetExtra.
 
     if type(hole_radius) not in [float, int]:
         raise TypeError("Invalid type for hole radius")
@@ -83,20 +86,28 @@ def generate(
     if type(inner_radius) not in [float, int]:
         raise TypeError("inner_radius must be a float")
 
+    if inner_radius < -tool_diameter / 2:
+		# inner_radius also depends on StartRadius
+        raise ValueError(
+            "inner_radius {0} with a tool of diameter {1} gives a negative helix radius !".format(
+                inner_radius, tool_diameter
+            )
+        )
+
     if type(tool_diameter) not in [float, int]:
         raise TypeError("tool_diameter must be a float")
 
-    if not hole_radius * 2 > tool_diameter:
+    if not hole_radius * 2 >= tool_diameter:  # will allow plunge cut, zero radius helix
         raise ValueError(
             "Cannot helix a hole of diameter {0} with a tool of diameter {1}".format(
                 2 * hole_radius, tool_diameter
             )
         )
 
-    elif startAt not in ["Inside", "Outside"]:
+    if startAt not in ["Inside", "Outside"]:
         raise ValueError("Invalid value for parameter 'startAt'")
 
-    elif direction not in ["CW", "CCW"]:
+    if direction not in ["CW", "CCW"]:
         raise ValueError("Invalid value for parameter 'direction'")
 
     if type(step_over) not in [float, int]:
@@ -115,39 +126,54 @@ def generate(
     if startPoint.z < endPoint.z:
         raise ValueError("start point is below end point")
 
+    tool_radius = tool_diameter / 2
     if hole_radius <= tool_diameter:
-        Path.Log.debug("(single helix mode)\n")
-        radii = [hole_radius - tool_diameter / 2]
-        if radii[0] <= 0:
+        Path.Log.debug("(single helix path)\n")
+        radii = [hole_radius - tool_radius]
+        if radii[0] < 0:
             raise ValueError(
-                "Cannot helix a hole of diameter {0} with a tool of diameter {1}".format(
+                "Cannot creatre a helical path of diameter {0} with a tool of diameter {1}".format(
                     2 * hole_radius, tool_diameter
                 )
             )
-        outer_radius = hole_radius
-
-    else:  # inner_radius > 0:
-        Path.Log.debug("(annulus mode / full hole)\n")
-        outer_radius = hole_radius - tool_diameter / 2
-        step_radius = inner_radius + tool_diameter / 2
-        if abs((outer_radius - step_radius) / step_over_distance) < 1e-5:
-            radii = [(outer_radius + inner_radius) / 2]
+    else:  # inner_radius >= 0:
+        Path.Log.debug("(annular clearance or entire hole)\n")
+        outer_path_radius = hole_radius - tool_radius
+        inner_path_radius = inner_radius + tool_radius
+        
+        if abs((outer_path_radius - inner_path_radius) / step_over_distance) < 1e-5:
+            radii = [outer_path_radius] # below tolerance cutoff, just use outer radius
         else:
-            nr = max(int(ceil((outer_radius - inner_radius) / step_over_distance)), 2)
-            radii = linspace(outer_radius, step_radius, nr)
+            nr = int( ceil((outer_path_radius - inner_path_radius ) / step_over_distance) +1 )
+            radii = linspace(outer_path_radius, inner_path_radius , nr)
+            if (startAt == "Outside") and (inner_radius <= 0): 
+                radii = delete(radii,-1)  # cutting air. useful for spacing
 
     Path.Log.debug("Radii: {}".format(radii))
     # calculate the number of full and partial turns required
     # Each full turn is two 180 degree arcs. Zsteps is equally spaced step
     # down values
-    turncount = max(int(ceil((startPoint.z - endPoint.z) / step_down)), 2)
-    zsteps = linspace(startPoint.z, endPoint.z, 2 * turncount + 1)
+    turncount = max(int(ceil((startPoint.z - endPoint.z) / step_down)) ,2)
+    zsteps = linspace(startPoint.z, endPoint.z, 2 * turncount+1)
 
     def helix_cut_r(r):
         commandlist = []
         arc_cmd = "G2" if direction == "CW" else "G3"
         commandlist.append(Path.Command("G0", {"X": startPoint.x + r, "Y": startPoint.y}))
         commandlist.append(Path.Command("G1", {"Z": startPoint.z}))
+
+        #FeedRateCheckbox.checked
+        if not feedRateAdj : feedRateRatio = 0
+        else:
+            if (startAt=="Inside") or (r == radii[0]): # first cut, always adj for outer wall chip-load
+                feedRateRatio = r/(r+tool_radius)  # classic (hole_rad-tool_rad)/hole_rad adjustment
+            else:
+                if tool_radius > 200*r : feedRateRatio = 200 # prevent div zero
+                else: feedRateRatio = (r+tool_radius)/r # startAt outside: increase spindle feed rate to maintain inner-cut chip-load
+
+
+        # Note: if (r == -tool_radius), helix is plugne, hence VH_gradient==0; feedrate==vFeed , OK
+        print ("rad = ",r,",  feedRateRatio = ", feedRateRatio)
         for i in range(1, turncount + 1):
             commandlist.append(
                 Path.Command(
@@ -161,6 +187,7 @@ def generate(
                     },
                 )
             )
+            if feedRateAdj: commandlist[-1].Parameters["F"] = feedRateRatio
             commandlist.append(
                 Path.Command(
                     arc_cmd,
@@ -173,6 +200,7 @@ def generate(
                     },
                 )
             )
+            if feedRateAdj: commandlist[-1].Parameters["F"] = feedRateRatio
         commandlist.append(
             Path.Command(
                 arc_cmd,
@@ -182,9 +210,10 @@ def generate(
                     "Z": endPoint.z,
                     "I": -r,
                     "J": 0.0,
-                },
+               },
             )
         )
+        if feedRateAdj: commandlist[-1].Parameters["F"] = feedRateRatio
         commandlist.append(
             Path.Command(
                 arc_cmd,
@@ -197,31 +226,33 @@ def generate(
                 },
             )
         )
+        if feedRateAdj: commandlist[-1].Parameters["F"] = feedRateRatio
         return commandlist
 
     def retract():
-        # try to move to a safe place to retract without leaving a dwell
-        # mark
+        # try to move to a safe place to retract without leaving a dwell mark
         retractcommands = []
-        # Calculate retraction
-        if hole_radius <= tool_diameter:  # simple case where center is clear
-            center_clear = True
-
-        elif startAt == "Inside" and inner_radius == 0.0:  # middle is clear
-            center_clear = True
+        if tool_diameter >= hole_radius:
+            center_clear = True  # single cut operation
         else:
-            center_clear = False
+            center_clear = (startAt == "Inside") and (inner_radius <= 0.0)
+            # hole centre is already clear (hole inner radius<=tool_radius)
 
-        if center_clear:
+        # use G1 since tool tip still in contact with workpiece.
+        if center_clear and (prev_r is None):
+            retractcommands.append(Path.Command("G1", {"X": endPoint.x, "Y": endPoint.y}))
+        elif prev_r is not None:
+            dwell_r = (r + prev_r) / 2
             retractcommands.append(
-                Path.Command("G0", {"X": endPoint.x, "Y": endPoint.y, "Z": endPoint.z})
+                Path.Command(
+                    "G1",
+                    {
+                        "X": endPoint.x + dwell_r,
+                        "Y": endPoint.y,
+                    },
+                )
             )
-
-        # Technical Debt.
-        # If the operation is clearing multiple passes in annulus mode (inner
-        # radius > 0.0 and len(radii) > 1) then there is a derivable
-        # safe place which does not touch the inner or outer wall on all radii except
-        # the first.  This is left as a future improvement.
+            # else annular slot, no pulloff, just retract along wall
 
         retractcommands.append(Path.Command("G0", {"Z": startPoint.z}))
 
@@ -231,8 +262,11 @@ def generate(
         radii = radii[::-1]
 
     commands = []
+    prev_r = None
     for r in radii:
         commands.extend(helix_cut_r(r))
         commands.extend(retract())
-
+        prev_r = r
     return commands
+
+ 
