b43b52c55ccb9297c6446ce8660a656e3dff87bb
jonzirk76 <jonzirk@gmail.com>
2025-04-03 05:13:47 -0400

    Moved DrawUtil.cpp member functions to DrawViewPart.cpp

--- src/Mod/TechDraw/App/DrawViewPart.cpp
+++ src/Mod/TechDraw/App/DrawViewPart.cpp
@@ -1498,6 +1498,40 @@ void DrawViewPart::handleChangedPropertyType(Base::XMLReader &reader, const char
     }
 }
 
+// true if owner->element is a cosmetic vertex
+bool DrawUtil::isCosmeticVertex(App::DocumentObject* owner, std::string element)
+{
+    auto ownerView = static_cast<TechDraw::DrawViewPart*>(owner);
+    auto vertexIndex = DrawUtil::getIndexFromName(element);
+    auto vertex = ownerView->getProjVertexByIndex(vertexIndex);
+    if (vertex) {
+        return vertex->getCosmetic();
+    }
+    return false;
+}
+
+// true if owner->element is a cosmetic edge
+bool DrawUtil::isCosmeticEdge(App::DocumentObject* owner, std::string element)
+{
+    auto ownerView = static_cast<TechDraw::DrawViewPart*>(owner);
+    auto edge = ownerView->getEdge(element);
+    if (edge && edge->source() == SourceType::COSMETICEDGE && edge->getCosmetic()) {
+        return true;
+    }
+    return false;
+}
+
+// true if owner->element is a center line
+bool DrawUtil::isCenterLine(App::DocumentObject* owner, std::string element)
+{
+    auto ownerView = static_cast<TechDraw::DrawViewPart*>(owner);
+    auto edge = ownerView->getEdge(element);
+    if (edge && edge->source() == SourceType::CENTERLINE && edge->getCosmetic()) {
+        return true;
+    }
+    return false;
+}
+
 // debugging ----------------------------------------------------------------------------
 
 void DrawViewPart::dumpVerts(std::string text)
