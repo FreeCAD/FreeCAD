29e3fd193f820dc015e5b6da5c137645fd69a76d
Ladislav Michl <ladis@triops.cz>
2025-04-11 01:53:09 +0200

    App: Quantity: use predefined unit types

--- src/App/FeatureTest.cpp
+++ src/App/FeatureTest.cpp
@@ -145,7 +145,7 @@ FeatureTest::FeatureTest()
     ADD_PROPERTY(QuantityLength, (1.0));
     QuantityLength.setUnit(Base::Unit::Length);
     ADD_PROPERTY(QuantityOther, (5.0));
-    QuantityOther.setUnit(Base::Unit(-3, 1));
+    QuantityOther.setUnit(Base::Unit::Density);
     // clang-format on
 }
 
