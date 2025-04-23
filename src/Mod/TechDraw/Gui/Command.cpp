26e7c4ffbd7b5f34c0d86c826c79b4d8e32c5ec6
Furgo <148809153+furgo16@users.noreply.github.com>
2025-04-23 12:53:27 +0200

    Fix if one-liner syntax

--- src/Mod/TechDraw/Gui/Command.cpp
+++ src/Mod/TechDraw/Gui/Command.cpp
@@ -1711,8 +1711,8 @@ void CmdTechDrawArchView::activated(int iMsg)
               SourceName.c_str());
     doCommand(Doc, "App.activeDocument().%s.addView(App.activeDocument().%s)", PageName.c_str(),
               FeatName.c_str());
-    doCommand(Doc, "App.activeDocument().%s.Scale = App.activeDocument().%s.Scale if App.activeDocument().%s.Scale",
-              FeatName.c_str(), PageName.c_str(), PageName.c_str()); 
+    doCommand(Doc, "if App.activeDocument().%s.Scale: App.activeDocument().%s.Scale = App.activeDocument().%s.Scale",
+        PageName.c_str(), FeatName.c_str(), PageName.c_str());
     updateActive();
     commitCommand();
 }
