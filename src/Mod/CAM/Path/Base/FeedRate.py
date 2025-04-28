f6970e84e13f4ac40c329a85160e7f4ee344e8f1
J-Dunn <dunn@piments.com>
2025-04-28 14:19:20 +0200

    add FeedRate adjusted "F" to Helix/ThreadMill
    check for pseudo params FR, DR 
    DR used to blend vFeed, hFeed instead of forcing one or the other
    FR ratio adjust path feedrate "F" to account for actual cutting radius of tool on G2,G3

--- src/Mod/CAM/Path/Base/FeedRate.py
+++ src/Mod/CAM/Path/Base/FeedRate.py
@@ -24,6 +24,7 @@ import FreeCAD
 import Path
 import Path.Base.MachineState as PathMachineState
 import Part
+import math
 
 __title__ = "Feed Rate Helper Utility"
 __author__ = "sliptonic (Brad Collette)"
@@ -41,7 +42,7 @@ else:
     Path.Log.setLevel(Path.Log.Level.INFO, Path.Log.thisModule())
 
 
-def setFeedRate(commandlist, ToolController):
+def setFeedRate(commandlist_arg, ToolController):
     """Set the appropriate feed rate for a list of Path commands using the information from a Tool Controller
 
     Every motion command in the list will have a feed rate parameter added or overwritten based
@@ -51,35 +52,71 @@ def setFeedRate(commandlist, ToolController):
     def _isVertical(currentposition, command):
         x = command.Parameters["X"] if "X" in command.Parameters else currentposition.x
         y = command.Parameters["Y"] if "Y" in command.Parameters else currentposition.y
+        return Path.Geom.isRoughly(x,currentposition.x) and Path.Geom.isRoughly(y,currentposition.y)
+
+    def _isHorizontal(currentposition, command):
         z = command.Parameters["Z"] if "Z" in command.Parameters else currentposition.z
-        endpoint = FreeCAD.Vector(x, y, z)
-        if Path.Geom.pointsCoincide(currentposition, endpoint):
-            return True
-        return Path.Geom.isVertical(Part.makeLine(currentposition, endpoint))
+        return Path.Geom.isRoughly(z,currentposition.z) 
+
 
     machine = PathMachineState.MachineState()
 
-    for command in commandlist:
+    hFeed = ToolController.HorizFeed.Value
+    vFeed = ToolController.VertFeed.Value
+    #print("commandlist_arg[-1] = ", commandlist_arg[-1])
+    #print("commandlist_arg[1].Parameters = ", commandlist_arg[1].Parameters.keys[1])
+    #print("commandlist_arg[1].Parameters = ", commandlist_arg[1].Parameters.values[1])
+    for i in range(len(commandlist_arg)):
+        print("Name: ", commandlist_arg[i].Name)
+        print("Parameters: ", commandlist_arg[i].Parameters)
+        #print(" commandlist_arg[i]", commandlist_arg[i]) 
+        #print(" commandlist_arg", commandlist_arg) 
+        
+    for command in commandlist_arg:
         if command.Name not in Path.Geom.CmdMoveAll:
             continue
 
-        if _isVertical(machine.getPosition(), command):
+        params = command.Parameters
+
+        if _isVertical(machine.getPosition(), command): # also true for plunge helix
             rate = (
                 ToolController.VertRapid.Value
                 if command.Name in Path.Geom.CmdMoveRapid
-                else ToolController.VertFeed.Value
+                else vFeed
             )
         else:
-            rate = (
-                ToolController.HorizRapid.Value
-                if command.Name in Path.Geom.CmdMoveRapid
-                else ToolController.HorizFeed.Value
-            )
+            if "FR" in params: hFeed_adj = hFeed * params["FR"]
+            else: hFeed_adj = hFeed
 
-        params = command.Parameters
+            if _isHorizontal(machine.getPosition(), command):
+                rate = (
+                    ToolController.HorizRapid.Value
+                    if command.Name in Path.Geom.CmdMoveRapid
+                    else hFeed_adj
+                )
+            elif (command.Name in ["G2", "G3"]):
+                if ("DR" in params):
+                    if params["DR"]==0:
+                         rate = hFeed_adj
+                    elif abs(params["DR"]) > 5: # arbitrary, why 5 ?
+                         rate = hFeed_adj  # steep helix: use V feedrate
+                    else:
+                        descentAngle = math.atan(abs(params["DR"]))
+                        rate = math.sqrt(vFeed * vFeed + hFeed_adj * hFeed_adj)  # vector sum
+                        # ensure v,h feedrates not exceeded by "FR" rate
+                        rate = min(rate, vFeed / math.cos(descentAngle))
+                        rate = min(rate, hFeed_adj / math.sin(descentAngle))
+    
+                        # TODO modify pocket/contour arcs or ramps to use this code.
+            else:
+                rate = hFeed  # fallback  non v, non H, non arc == ramp??? ### should never
+
+        params.pop("FR", None)
+        params.pop("DR", None)
+
         params["F"] = rate
         command.Parameters = params
 
         machine.addCommand(command)
 
-    return commandlist
+    return # commandlist_arg # return value unused !
