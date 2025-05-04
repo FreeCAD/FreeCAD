24009e35693d449b4fa1df1c57713ab7940702ed
Jacob Oursland <jacob.oursland@gmail.com>
2025-05-04 10:56:05 -0700

    CI: add missing command line args to codespell.py.

--- tools/lint/codespell.py
+++ tools/lint/codespell.py
@@ -55,12 +55,34 @@ def main():
         description="Run codespell on files and append a Markdown report."
     )
     parser.add_argument(
+        "--files",
+        action="extend",
+        nargs="+",
+        required=True,
+        help="List of files to spell check."
+    )
+    parser.add_argument(
         "--ignore-words",
         required=True,
         help="Comma-separated list of words to ignore (from codespellignore).",
     )
     parser.add_argument(
+        "--log-dir",
+        required=True,
+        help="Directory to store the log file."
+    )
+    parser.add_argument(
+        "--report-file",
+        required=True,
+        help="Name of the report file."
+    )
+    parser.add_argument(
         "--skip", required=True, help="Comma-separated list of file patterns to skip."
+    )
+    parser.add_argument(
+        "--verbose",
+        default=False,
+        help="Use verbose output."
     )
     args = parser.parse_args()
     init_environment(args)
