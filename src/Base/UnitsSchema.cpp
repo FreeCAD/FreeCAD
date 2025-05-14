FreeCAD/FreeCAD

commit 1155f0d75281e9b9d70536d9b81dde7caececadd

Base: simplify UnitsSchemas management
Fixes: Maintaining schemas is difficult and error-prone

- Facilitate easy schemas add, remove, change, etc.
- Remove 14 files containing approx 2,190 lines of if/else code and data
- Place data in one file (UnitsSchemasData.h) using a normalized structure (including special functions)
- Isolate and simplify data operations (code)
- Remove schemas enum to keep data independent of code
- Separate responsibilities: Specifications, data, schemas, schema
- Add schema data 'isDefault'
- Add schema data name
- Prefer algorithms to raw loops
- Add schemas unit tests
- Tweak quantity unit tests

src/Base/UnitsSchema.cpp
@@ -1,41 +1,97 @@
-/***************************************************************************
- *   Copyright (c) 2009 Jürgen Riegel <FreeCAD@juergen-riegel.net>         *
- *                                                                         *
- *   This file is part of the FreeCAD CAx development system.              *
- *                                                                         *
- *   This library is free software; you can redistribute it and/or         *
- *   modify it under the terms of the GNU Library General Public           *
- *   License as published by the Free Software Foundation; either          *
- *   version 2 of the License, or (at your option) any later version.      *
- *                                                                         *
- *   This library  is distributed in the hope that it will be useful,      *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU Library General Public License for more details.                  *
- *                                                                         *
- *   You should have received a copy of the GNU Library General Public     *
- *   License along with this library; see the file COPYING.LIB. If not,    *
- *   write to the Free Software Foundation, Inc., 59 Temple Place,         *
- *   Suite 330, Boston, MA  02111-1307, USA                                *
- *                                                                         *
- ***************************************************************************/
+/************************************************************************
+ *                                                                      *
+ *   This file is part of the FreeCAD CAx development system.           *
+ *                                                                      *
+ *   This library is free software; you can redistribute it and/or      *
+ *   modify it under the terms of the GNU Library General Public        *
+ *   License as published by the Free Software Foundation; either       *
+ *   version 2 of the License, or (at your option) any later version.   *
+ *                                                                      *
+ *   This library  is distributed in the hope that it will be useful,   *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of     *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      *
+ *   GNU Library General Public License for more details.               *
+ *                                                                      *
+ *   You should have received a copy of the GNU Library General Public  *
+ *   License along with this library; see the file COPYING.LIB. If not, *
+ *   write to the Free Software Foundation, Inc., 59 Temple Place,      *
+ *   Suite 330, Boston, MA  02111-1307, USA                             *
+ *                                                                      *
+ ************************************************************************/
 
 #include "PreCompiled.h"
-#ifdef __GNUC__
-#include <unistd.h>
-#endif
+
+#include <iomanip>
+#include <sstream>
+#include <string>
 
 #include <QLocale>
 #include <QString>
 
 #include "Quantity.h"
 #include "UnitsSchema.h"
+#include "UnitsSchemasData.h"
+#include "UnitsSchemasSpecs.h"
+#include "Exception.h"
+#include "Quantity.h"
 
-using namespace Base;
+using Base::UnitsSchema;
+using Base::UnitsSchemaSpec;
 
-std::string UnitsSchema::toLocale(const Base::Quantity& quant,
-                                  double factor,
-                                  const std::string& unitString) const
+
+UnitsSchema::UnitsSchema(UnitsSchemaSpec spec)
+    : spec {std::move(spec)}
+{}
+
+std::string UnitsSchema::translate(const Quantity& quant) const
+{  // to satisfy GCC
+    double dummy1 {};
+    std::string dummy2;
+    return translate(quant, dummy1, dummy2);
+}
+
+std::string
+UnitsSchema::translate(const Quantity& quant, double& factor, std::string& unitString) const
+{
+    if (spec.translationSpecs.empty()) {
+        return toLocale(quant, 1.0, unitString);
+    }
+
+    const auto unitName = quant.getUnit().getTypeString();
+
+    if (spec.translationSpecs.count(unitName) == 0) {
+        // no schema-level translation. Use defaults.
+        factor = 1.0;
+        unitString = quant.getUnit().getString();
+
+        return toLocale(quant, factor, unitString);
+    }
+
+    const auto value = quant.getValue();
+
+    auto isSuitable = [&](const UnitTranslationSpec& row) {
+        return row.threshold > value || row.threshold == 0;  // zero indicates default
+    };
+
+    auto unitSpecs = spec.translationSpecs.at(unitName);
+    const auto unitSpec = std::find_if(unitSpecs.begin(), unitSpecs.end(), isSuitable);
+    if (unitSpec == unitSpecs.end()) {
+        throw RuntimeError("Suitable threshhold not found. Schema: " + spec.name
+                           + " value: " + std::to_string(value));
+    }
+
+    if (unitSpec->factor == 0) {
+        return UnitsSchemasData::runSpecial(unitSpec->unitString, value);
+    }
+
+    factor = unitSpec->factor;
+    unitString = unitSpec->unitString;
+
+    return toLocale(quant, factor, unitString);
+}
+
+std::string
+UnitsSchema::toLocale(const Quantity& quant, const double factor, const std::string& unitString)
 {
     QLocale Lc;
     const QuantityFormat& format = quant.getFormat();
@@ -44,6 +100,43 @@ std::string UnitsSchema::toLocale(const Base::Quantity& quant,
         Lc.setNumberOptions(static_cast<QLocale::NumberOptions>(opt));
     }
 
-    QString Ln = Lc.toString((quant.getValue() / factor), format.toFormat(), format.precision);
-    return QStringLiteral("%1 %2").arg(Ln, QString::fromStdString(unitString)).toStdString();
+    std::string valueString =
+        Lc.toString((quant.getValue() / factor), format.toFormat(), format.precision).toStdString();
+
+    return fmt::format(
+        "{}{}{}",
+        valueString,
+        unitString.empty() || unitString == "°" || unitString == "″" || unitString == "′" ? ""
+                                                                                          : " ",
+        unitString);
+}
+
+bool UnitsSchema::isMultiUnitLength() const
+{
+    return spec.isMultUnitLen;
+}
+
+bool UnitsSchema::isMultiUnitAngle() const
+{
+    return spec.isMultUnitAngle;
+}
+
+std::string UnitsSchema::getBasicLengthUnit() const
+{
+    return spec.basicLengthUnitStr;
+}
+
+std::string UnitsSchema::getName() const
+{
+    return spec.name;
+}
+
+std::string UnitsSchema::getDescription() const
+{
+    return spec.description;
+}
+
+int UnitsSchema::getNum() const
+{
+    return static_cast<int>(spec.num);
 }

tests/src/Base/SchemaTests.cpp
@@ -1,0 +1,373 @@
+/************************************************************************
+ *                                                                      *
+ *   This file is part of the FreeCAD CAx development system.           *
+ *                                                                      *
+ *   This library is free software; you can redistribute it and/or      *
+ *   modify it under the terms of the GNU Library General Public        *
+ *   License as published by the Free Software Foundation; either       *
+ *   version 2 of the License, or (at your option) any later version.   *
+ *                                                                      *
+ *   This library  is distributed in the hope that it will be useful,   *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of     *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      *
+ *   GNU Library General Public License for more details.               *
+ *                                                                      *
+ *   You should have received a copy of the GNU Library General Public  *
+ *   License along with this library; see the file COPYING.LIB. If not, *
+ *   write to the Free Software Foundation, Inc., 59 Temple Place,      *
+ *   Suite 330, Boston, MA  02111-1307, USA                             *
+ *                                                                      *
+ ************************************************************************/
+
+#include <gtest/gtest.h>
+#include "Base/Exception.h"
+#include "Base/Unit.h"
+#include "Base/Quantity.h"
+#include "Base/UnitsApi.h"
+#include "Base/UnitsSchemasData.h"
+#include "Base/UnitsSchemas.h"
+
+#include <QLocale>
+#include <string>
+
+using Base::Quantity;
+using Base::QuantityFormat;
+using Base::RuntimeError;
+using Base::Unit;
+using Base::UnitsApi;
+using Base::UnitsSchema;
+using Base::UnitsSchemas;
+
+class SchemaTest: public testing::Test
+{
+protected:
+    void SetUp() override
+    {
+        const QLocale loc(QLocale::C);
+        QLocale::setDefault(loc);
+    }
+
+    void TearDown() override
+    {}
+
+    static std::string
+    set(const std::string& schemaName, const Unit unit, const double value)  // NOLINT
+    {
+        UnitsApi::setSchema(schemaName);
+        const auto quantity = Quantity {value, unit};
+        return quantity.getSafeUserString();
+    }
+
+    static std::string setWithPrecision(const std::string& name,
+                                        const double value,
+                                        const Unit unit,
+                                        const int precision)
+    {
+        UnitsApi::setSchema(name);
+        Quantity quantity {value, unit};
+        QuantityFormat format = quantity.getFormat();
+        format.precision = precision;
+        quantity.setFormat(format);
+        return quantity.getSafeUserString();
+    }
+
+    std::unique_ptr<UnitsSchemas> schemas;  // NOLINT
+};
+
+TEST_F(SchemaTest, imperial_decimal_1_mm_default_precision)
+{
+    const std::string result = set("ImperialDecimal", Unit::Length, 1.0);
+    const auto expect {"0.04 in"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, internal_1_mm_precision_0)
+{
+    const std::string result = setWithPrecision("Internal", 1.0, Unit::Length, 0);
+    const auto expect {"1 mm"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, internal_100_mm_precision_0)
+{
+    const std::string result = setWithPrecision("Internal", 100.0, Unit::Length, 0);
+    const auto expect {"100 mm"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, internal_100_mm_precision_1)
+{
+    const std::string result = setWithPrecision("Internal", 100.0, Unit::Length, 1);
+    const auto expect {"100.0 mm"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, internal_20000_mm_precision_2)
+{
+    const std::string result = setWithPrecision("Internal", 20000.0, Unit::Length, 2);
+    const auto expect {"20.00 m"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_decimal_1_mm_precision_0)
+{
+    const std::string result = setWithPrecision("ImperialDecimal", 1.0, Unit::Length, 0);
+    const auto expect {"1 mm"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_decimal_10_mm_precision_0)
+{
+    const std::string result = setWithPrecision("ImperialDecimal", 10.0, Unit::Length, 0);
+    const auto expect {"10 mm"};
+    // https://github.com/FreeCAD/FreeCAD/commit/569154b73f818c6a88b010def687d5e684ce64c2
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_decimal_20_mm_precision_0)
+{
+    const std::string result = setWithPrecision("ImperialDecimal", 20.0, Unit::Length, 0);
+    const auto expect {"1 in"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_1_mm_precision_0)
+{
+    const std::string result = setWithPrecision("Imperial", 1.0, Unit::Length, 0);
+    const auto expect {"39 thou"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_0_mm_precision_0)
+{
+    const std::string result = setWithPrecision("Imperial", 0.0, Unit::Length, 0);
+    const auto expect {"0 in"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_0_mm_precision_1)
+{
+    const std::string result = setWithPrecision("Imperial", 0.0, Unit::Length, 1);
+    const auto expect {"0.0 in"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_decimal_0_mm_precision_0)
+{
+    const std::string result = setWithPrecision("ImperialDecimal", 0.0, Unit::Length, 0);
+    const auto expect {"0 in"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_decimal_0_mm_precision_1)
+{
+    const std::string result = setWithPrecision("ImperialDecimal", 0.0, Unit::Length, 1);
+    const auto expect {"0.0 in"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_civil_0_mm_precision_0)
+{
+    const std::string result = setWithPrecision("ImperialCivil", 0.0, Unit::Length, 0);
+    const auto expect {"0 mm"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_civil_0_mm_precision_1)
+{
+    const std::string result = setWithPrecision("ImperialCivil", 0.0, Unit::Length, 1);
+    const auto expect {"0.0 mm"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_building_0_mm_precision_0)
+{
+    const std::string result = setWithPrecision("ImperialBuilding", 0.0, Unit::Length, 0);
+    const auto expect {"0"};  // don't know why
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_building_0_mm_precision_1)
+{
+    const std::string result = setWithPrecision("ImperialBuilding", 0.0, Unit::Length, 1);
+    const auto expect {"0"};  // don't know why
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_decimal_1_mm_precision_1)
+{
+    const std::string result = setWithPrecision("ImperialDecimal", 1.0, Unit::Length, 1);
+    const auto expect {"1 mm"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_decimal_100_mm_precision_0)
+{
+    const std::string result = setWithPrecision("ImperialDecimal", 100.0, Unit::Length, 0);
+    const auto expect {"4 in"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_decimal_100_mm_precision_1)
+{
+    const std::string result = setWithPrecision("ImperialDecimal", 100.0, Unit::Length, 1);
+    const auto expect {"3.9 in"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_decimal_100_mm_precision_2)
+{
+    const std::string result = setWithPrecision("ImperialDecimal", 100.0, Unit::Length, 2);
+    const auto expect {"3.94 in"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_decimal_1_mm_precision_2)
+{
+    const std::string result = setWithPrecision("ImperialDecimal", 1.0, Unit::Length, 2);
+    const auto expect {"0.04 in"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_decimal_1_mm_precision_4)
+{
+    const std::string result = setWithPrecision("ImperialDecimal", 1.0, Unit::Length, 4);
+    const auto expect {"0.0394 in"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_safe_user_str_same)
+{
+    constexpr auto val {304.8};
+    const auto result = set("Imperial", Unit::Length, val);
+    const auto expect {"1.00′"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_safe_user_str_more)
+{
+    constexpr auto val {310.0};
+    const auto result = set("Imperial", Unit::Length, val);
+    const auto expect {"1.02′"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_safe_user_str_less)
+{
+    constexpr auto val {300.0};
+    const auto result = set("Imperial", Unit::Length, val);
+    const auto expect {"11.81″"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_safe_user_str_one_inch)
+{
+    constexpr auto val {25.4};
+    const auto result = set("Imperial", Unit::Length, val);
+    const auto expect {"1.00″"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_building_special_function_length_inch)
+{
+    constexpr auto val {25.4};
+    const auto result = set("ImperialBuilding", Unit::Length, val);
+    const auto expect {"1″"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_building_special_function_length_foot)
+{
+    constexpr auto val {25.4 * 12};
+    const auto result = set("ImperialBuilding", Unit::Length, val);
+    const auto expect {"1′"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_building_special_function_length)
+{
+    constexpr auto val {360.6};
+    const auto result = set("ImperialBuilding", Unit::Length, val);
+    const auto expect {"1′2-1/4″"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_building_special_function_length_neg)
+{
+    constexpr auto val {-360.6};
+    const auto result = set("ImperialBuilding", Unit::Length, val);
+    const auto expect {"-1′2-1/4″"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_civil_special_function_angle_degrees)
+{
+    constexpr auto val {180};
+    const auto result = set("ImperialCivil", Unit::Angle, val);
+    const auto expect {"180°"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_civil_special_function_angle_minutes)
+{
+    constexpr auto val {180.5};
+    const auto result = set("ImperialCivil", Unit::Angle, val);
+    const auto expect {"180°30′"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_civil_special_function_angle_seconds)
+{
+    constexpr auto val {180.11};
+    const auto result = set("ImperialCivil", Unit::Angle, val);
+    const auto expect {"180°6′36″"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, imperial_civil_special_function_angle_no_degrees)
+{
+    constexpr auto val {0.11};
+    const auto result = set("ImperialCivil", Unit::Angle, val);
+    const auto expect {"0°6′36″"};
+
+    EXPECT_EQ(result, expect);
+}
+
+TEST_F(SchemaTest, unknown_schema_name_throws)
+{
+    EXPECT_THROW(UnitsApi::setSchema("Unknown"), RuntimeError);
+}

src/App/Application.cpp
@@ -2795,12 +2795,10 @@ void Application::initApplication()
     // set up Unit system default
     const ParameterGrp::handle hGrp = GetApplication().GetParameterGroupByPath
        ("User parameter:BaseApp/Preferences/Units");
-    Base::UnitsApi::setSchema(static_cast<Base::UnitSystem>(hGrp->GetInt("UserSchema", 0)));
-    Base::UnitsApi::setDecimals(static_cast<int>(hGrp->GetInt("Decimals", Base::UnitsApi::getDecimals())));
-
-    // In case we are using fractional inches, get user setting for min unit
-    const int denom = static_cast<int>(hGrp->GetInt("FracInch", Base::QuantityFormat::getDefaultDenominator()));
-    Base::QuantityFormat::setDefaultDenominator(denom);
+    Base::UnitsApi::setSchema(hGrp->GetInt("UserSchema", Base::UnitsApi::getDefSchemaNum()));
+    Base::UnitsApi::setDecimals(hGrp->GetInt("Decimals", Base::UnitsApi::getDecimals()));
+    Base::QuantityFormat::setDefaultDenominator(
+        hGrp->GetInt("FracInch", Base::QuantityFormat::getDefaultDenominator()));
 
 #if defined (_DEBUG)
     Base::Console().Log("Application is built with debug information\n");

src/App/Document.cpp
@@ -876,13 +876,8 @@ Document::Document(const char* documentName)
                       "Additional tag to save the name of the company");
     ADD_PROPERTY_TYPE(UnitSystem, (""), 0, Prop_None, "Unit system to use in this project");
     // Set up the possible enum values for the unit system
-    int num = static_cast<int>(Base::UnitSystem::NumUnitSystemTypes);
-    std::vector<std::string> enumValsAsVector;
-    for (int i = 0; i < num; i++) {
-        QString item = Base::UnitsApi::getDescription(static_cast<Base::UnitSystem>(i));
-        enumValsAsVector.emplace_back(item.toStdString());
-    }
-    UnitSystem.setEnums(enumValsAsVector);
+
+    UnitSystem.setEnums(Base::UnitsApi::getDescriptions());
     // Get the preferences/General unit system as the default for a new document
     ParameterGrp::handle hGrpu =
         App::GetApplication().GetParameterGroupByPath("User parameter:BaseApp/Preferences/Units");

src/Base/CMakeLists.txt
@@ -150,20 +150,10 @@ SET(FreeCADBase_UNITAPI_SRCS
     UnitsApi.h
     UnitsSchema.h
     UnitsSchema.cpp
-    UnitsSchemaInternal.h
-    UnitsSchemaInternal.cpp
-    UnitsSchemaMKS.h
-    UnitsSchemaMKS.cpp
-    UnitsSchemaImperial1.h
-    UnitsSchemaImperial1.cpp
-    UnitsSchemaCentimeters.h
-    UnitsSchemaCentimeters.cpp
-    UnitsSchemaMmMin.h
-    UnitsSchemaMmMin.cpp
-    UnitsSchemaFemMilliMeterNewton.h
-    UnitsSchemaFemMilliMeterNewton.cpp
-    UnitsSchemaMeterDecimal.h
-    UnitsSchemaMeterDecimal.cpp
+    UnitsSchemas.cpp
+    UnitsSchemas.h
+    UnitsSchemasData.h
+    UnitsSchemasSpecs.h
     Quantity.h
     Quantity.cpp
     QuantityPyImp.cpp

src/Base/Quantity.cpp
@@ -64,7 +64,7 @@ int QuantityFormat::defaultDenominator = 8;  // for 1/8"
 QuantityFormat::QuantityFormat()
     : option(OmitGroupSeparator | RejectGroupSeparator)
     , format(Fixed)
-    , precision(UnitsApi::getDecimals())
+    , precision(static_cast<int>(UnitsApi::getDecimals()))
     , denominator(defaultDenominator)
 {}
 
@@ -256,14 +256,15 @@ std::string Quantity::getUserString(double& factor, std::string& unitString) con
 std::string
 Quantity::getUserString(UnitsSchema* schema, double& factor, std::string& unitString) const
 {
-    return schema->schemaTranslate(*this, factor, unitString);
+    return schema->translate(*this, factor, unitString);
 }
 
 std::string Quantity::getSafeUserString() const
 {
     auto userStr = getUserString();
     if (myValue != 0.0 && parse(userStr).getValue() == 0) {
-        userStr = fmt::format("{} {}", myValue, getUnit().getString());
+        auto unitStr = getUnit().getString();
+        userStr = fmt::format("{}{}{}", myValue, unitStr.empty() ? "" : " ", unitStr);
     }
 
     return Tools::escapeQuotesFromString(userStr);

commit beccce4d3cf01dc8bd1a094ff42eb908267d2cc1

Base: revert to using ASCII chararacters for imperial lengths
The new unit schema management is using U+2032 and U+2033 characters
for feet and inches while parser is expecting only ' and ", while
U+2032 and U+2033 are used for arcminute and arcsecond.

While this is not an ideal solution and parser should deal with both,
revert back to ASCII for now.

Fixes: 1155f0d75281 ("Base: simplify UnitsSchemas management")

src/Base/UnitsSchema.cpp
@@ -103,12 +103,13 @@ UnitsSchema::toLocale(const Quantity& quant, const double factor, const std::str
     std::string valueString =
         Lc.toString((quant.getValue() / factor), format.toFormat(), format.precision).toStdString();
 
-    return fmt::format(
-        "{}{}{}",
-        valueString,
-        unitString.empty() || unitString == "°" || unitString == "″" || unitString == "′" ? ""
-                                                                                          : " ",
-        unitString);
+    return fmt::format("{}{}{}",
+                       valueString,
+                       unitString.empty() || unitString == "°" || unitString == "″"
+                               || unitString == "′" || unitString == "\"" || unitString == "'"
+                           ? ""
+                           : " ",
+                       unitString);
 }
 
 bool UnitsSchema::isMultiUnitLength() const

src/Base/UnitsSchemasData.h
@@ -581,8 +581,8 @@ inline const UnitsSchemaSpec s7
         { "Length", {
             { 0.00000254      , "in"       , 25.4              },
             { 2.54            , "thou"     , 0.0254            },
-            { 304.8           , "″"        , 25.4              },
-            { 914.4           , "′"        , 304.8             },
+            { 304.8           , "\""       , 25.4              },
+            { 914.4           , "'"        , 304.8             },
             { 1'609'344.0     , "yd"       , 914.4             },
             { 1'609'344'000.0 , "mi"       , 1'609'344.0       },
             { 0               , "in"       , 25.4              }}
@@ -649,7 +649,7 @@ inline std::size_t greatestCommonDenominator(const std::size_t a, const std::siz
 }
 
 /**
- * double -> [feet′][inches[-fraction]″], e.g.: 3′4-1/4″
+ * double -> [feet'][inches[-fraction]"], e.g.: 3'4-1/4"
  */
 inline std::string toFractional(const double value)
 {
@@ -676,19 +676,19 @@ inline std::string toFractional(const double value)
     if (inches > 0) {
         resultParts.push_back(fmt::format("{}", inches));
         if (numerator == 0) {
-            resultParts.emplace_back("″");
+            resultParts.emplace_back("\"");
         }
     }
     if (numerator > 0) {
         if (inches > 0) {
             resultParts.emplace_back("-");
         }
-        resultParts.push_back(fmt::format("{}/{}″", numerator, denominator));
+        resultParts.push_back(fmt::format("{}/{}\"", numerator, denominator));
     }
 
     return fmt::format("{}{}{}",
                        value < 0 ? "-" : "",
-                       feet > 0 ? fmt::format("{}′", feet) : "",
+                       feet > 0 ? fmt::format("{}'", feet) : "",
                        fmt::join(resultParts, ""));
 }
 

tests/src/Base/SchemaTests.cpp
@@ -21,6 +21,7 @@
 
 #include <gtest/gtest.h>
 #include "Base/Exception.h"
+#include "Base/Tools.h"
 #include "Base/Unit.h"
 #include "Base/Quantity.h"
 #include "Base/UnitsApi.h"
@@ -33,6 +34,7 @@
 using Base::Quantity;
 using Base::QuantityFormat;
 using Base::RuntimeError;
+using Base::Tools;
 using Base::Unit;
 using Base::UnitsApi;
 using Base::UnitsSchema;
@@ -263,7 +265,7 @@ TEST_F(SchemaTest, imperial_safe_user_str_same)
 {
     constexpr auto val {304.8};
     const auto result = set("Imperial", Unit::Length, val);
-    const auto expect {"1.00′"};
+    const auto expect = Tools::escapeQuotesFromString("1.00'");
 
     EXPECT_EQ(result, expect);
 }
@@ -272,7 +274,7 @@ TEST_F(SchemaTest, imperial_safe_user_str_more)
 {
     constexpr auto val {310.0};
     const auto result = set("Imperial", Unit::Length, val);
-    const auto expect {"1.02′"};
+    const auto expect = Tools::escapeQuotesFromString("1.02'");
 
     EXPECT_EQ(result, expect);
 }
@@ -281,7 +283,7 @@ TEST_F(SchemaTest, imperial_safe_user_str_less)
 {
     constexpr auto val {300.0};
     const auto result = set("Imperial", Unit::Length, val);
-    const auto expect {"11.81″"};
+    const auto expect = Tools::escapeQuotesFromString("11.81\"");
 
     EXPECT_EQ(result, expect);
 }
@@ -290,7 +292,7 @@ TEST_F(SchemaTest, imperial_safe_user_str_one_inch)
 {
     constexpr auto val {25.4};
     const auto result = set("Imperial", Unit::Length, val);
-    const auto expect {"1.00″"};
+    const auto expect = Tools::escapeQuotesFromString("1.00\"");
 
     EXPECT_EQ(result, expect);
 }
@@ -299,7 +301,7 @@ TEST_F(SchemaTest, imperial_building_special_function_length_inch)
 {
     constexpr auto val {25.4};
     const auto result = set("ImperialBuilding", Unit::Length, val);
-    const auto expect {"1″"};
+    const auto expect = Tools::escapeQuotesFromString("1\"");
 
     EXPECT_EQ(result, expect);
 }
@@ -308,7 +310,7 @@ TEST_F(SchemaTest, imperial_building_special_function_length_foot)
 {
     constexpr auto val {25.4 * 12};
     const auto result = set("ImperialBuilding", Unit::Length, val);
-    const auto expect {"1′"};
+    const auto expect = Tools::escapeQuotesFromString("1'");
 
     EXPECT_EQ(result, expect);
 }
@@ -317,7 +319,7 @@ TEST_F(SchemaTest, imperial_building_special_function_length)
 {
     constexpr auto val {360.6};
     const auto result = set("ImperialBuilding", Unit::Length, val);
-    const auto expect {"1′2-1/4″"};
+    const auto expect = Tools::escapeQuotesFromString("1'2-1/4\"");
 
     EXPECT_EQ(result, expect);
 }
@@ -326,7 +328,7 @@ TEST_F(SchemaTest, imperial_building_special_function_length_neg)
 {
     constexpr auto val {-360.6};
     const auto result = set("ImperialBuilding", Unit::Length, val);
-    const auto expect {"-1′2-1/4″"};
+    const auto expect = Tools::escapeQuotesFromString("-1'2-1/4\"");
 
     EXPECT_EQ(result, expect);
 }