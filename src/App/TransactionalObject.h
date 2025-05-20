41e76bf37aaa317d1622680f99f1a0609ebcf570
Pieter Hijma <pieterhijma@users.noreply.github.com>
2025-05-20 16:34:52 +0200

    Doc: Improve the documentation of transactions

--- src/App/TransactionalObject.h
+++ src/App/TransactionalObject.h
@@ -32,20 +32,47 @@ namespace App
 class Document;
 class TransactionObject;
 
-/** Base class of transactional objects
+/**
+ * @brief Base class of transactional objects.
+ *
+ * A transactional object provides functionality that is inherited by its
+ * children to ensure that these children objects can be targeted by a
+ * transaction.
  */
 class AppExport TransactionalObject: public App::ExtensionContainer
 {
     PROPERTY_HEADER_WITH_OVERRIDE(App::TransactionalObject);
 
 public:
-    /// Constructor
+    /// Construct a transactional object.
     TransactionalObject();
+
+    /// Destruct a transactional object.
     ~TransactionalObject() override;
+
+    /**
+     * @brief Check if this object is attached to a document.
+     *
+     * @return true if this object is attached to a document; otherwise false.
+     */
     virtual bool isAttachedToDocument() const;
+
+    /**
+     * @brief Deteach this object from the document.
+     *
+     * @return the name of the document this object was attached to.
+     */
     virtual const char* detachFromDocument();
 
 protected:
+    /**
+     * @brief Notify the document that a property is about to be changed.
+     *
+     * This function is called before the property is changed.
+     *
+     * @param[in,out] doc the document this object is attached to.
+     * @param[in] prop the property that is about to be changed.
+     */
     void onBeforeChangeProperty(Document* doc, const Property* prop);
 };
 
