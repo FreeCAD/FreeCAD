40926209ddd9add61af08d72b424cdc96a4c07fe
longrackslabs <longrackslabs@gmail.com>
2025-05-25 13:26:09 -0700

    Add Escape Key to cancel hints

--- src/Mod/Sketcher/Gui/DrawSketchHandlerPoint.h
+++ src/Mod/Sketcher/Gui/DrawSketchHandlerPoint.h
@@ -72,6 +72,9 @@ private:
                 hints.push_back(
                     InputHint(QCoreApplication::translate("Sketcher", "%1 click to place a point"),
                               {UserInput::MouseLeft}));
+
+                hints.push_back(InputHint(QCoreApplication::translate("Sketcher", "%1 cancel"),
+                                          {UserInput::KeyEscape}));
                 break;
             default:
                 break;
