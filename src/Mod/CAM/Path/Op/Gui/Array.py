5e9862fe5bc1828d024fe7e9d2fb80decd068aba
tarman3 <joice501790018@tutamail.com>
2025-05-26 13:13:00 +0300

    CAM: Changes in warnings of old Array

--- src/Mod/CAM/Path/Op/Gui/Array.py
+++ src/Mod/CAM/Path/Op/Gui/Array.py
@@ -25,6 +25,7 @@ import FreeCADGui
 import Path
 import PathScripts
 import PathScripts.PathUtils as PathUtils
+import Path.Base.Util as PathUtil
 from Path.Dressup.Utils import toolController
 from PySide import QtCore
 from PySide import QtGui
@@ -146,8 +147,6 @@ class ObjectArray:
         self.setEditorModes(obj)
         obj.Proxy = self
 
-        self.FirstRun = True
-
     def dumps(self):
         return None
 
@@ -206,11 +205,13 @@ class ObjectArray:
 
         self.setEditorModes(obj)
 
-        self.FirstRun = True
-
-    def execute(self, obj):
-        if FreeCAD.GuiUp and self.FirstRun:
-            self.FirstRun = False
+        global warningMessageAboutOldArrayAlreadyShowed
+        if (
+            FreeCAD.GuiUp
+            and not self.isBaseCompatible(obj)
+            and not warningMessageAboutOldArrayAlreadyShowed
+        ):
+            warningMessageAboutOldArrayAlreadyShowed = True
             QtGui.QMessageBox.warning(
                 None,
                 QT_TRANSLATE_NOOP("CAM_ArrayOp", "Operation is deprecated"),
@@ -225,22 +226,21 @@ class ObjectArray:
                     ),
                 ),
             )
+
+    def execute(self, obj):
         # backwards compatibility for PathArrays created before support for multiple bases
         if isinstance(obj.Base, list):
             base = obj.Base
         else:
             base = [obj.Base]
 
-        if len(base) == 0:
+        # Do not generate paths and clear current Path data
+        # if operation not Active or no base operations or operations not compatible
+        if not obj.Active or len(base) == 0 or not self.isBaseCompatible(obj):
+            obj.Path = Path.Path()
             return
 
         obj.ToolController = toolController(base[0])
-
-        # Do not generate paths and clear current Path data if operation not
-        if not obj.Active:
-            if obj.Path:
-                obj.Path = Path.Path()
-            return
 
         # use seed if specified, otherwise default to object name for consistency during recomputes
         seed = obj.JitterSeed or obj.Name
@@ -261,6 +261,37 @@ class ObjectArray:
         )
 
         obj.Path = pa.getPath()
+
+    def isBaseCompatible(self, obj):
+        if not obj.Base:
+            return False
+        tcs = []
+        cms = []
+        for sel in obj.Base:
+            if not sel.isDerivedFrom("Path::Feature"):
+                return False
+            tcs.append(toolController(sel))
+            cms.append(PathUtil.coolantModeForOp(sel))
+
+        if tcs == {None} or len(set(tcs)) > 1:
+            Path.Log.warning(
+                translate(
+                    "PathArray",
+                    "Arrays of toolpaths having different tool controllers or tool controller not selected.",
+                )
+            )
+            return False
+
+        if set(cms) != {"None"}:
+            Path.Log.warning(
+                translate(
+                    "PathArray",
+                    "Arrays not compatible with coolant modes.",
+                )
+            )
+            return False
+
+        return True
 
 
 class PathArray:
@@ -319,10 +350,6 @@ class PathArray:
         """getPath() ... Call this method on an instance of the class to generate and return
         path data for the requested path array."""
 
-        if len(self.baseList) == 0:
-            Path.Log.error(translate("PathArray", "No base objects for PathArray."))
-            return None
-
         base = self.baseList
         for b in base:
             if not b.isDerivedFrom("Path::Feature"):
@@ -333,15 +360,6 @@ class PathArray:
             b_tool_controller = toolController(b)
             if not b_tool_controller:
                 return
-
-            if b_tool_controller != toolController(base[0]):
-                # this may be important if Job output is split by tool controller
-                Path.Log.warning(
-                    translate(
-                        "PathArray",
-                        "Arrays of toolpaths having different tool controllers are handled according to the tool controller of the first path.",
-                    )
-                )
 
         # build copies
         output = ""
@@ -468,14 +486,32 @@ class CommandPathArray:
         return {
             "Pixmap": "CAM_Array",
             "MenuText": QT_TRANSLATE_NOOP("CAM_Array", "Array"),
-            "ToolTip": QT_TRANSLATE_NOOP("CAM_Array", "Creates an array from selected toolpath(s)"),
+            "ToolTip": QT_TRANSLATE_NOOP(
+                "CAM_Array",
+                "Creates an array from selected toolpath(s)\nwith identical tool controllers and without coolant",
+            ),
         }
 
     def IsActive(self):
-        selections = [
-            sel.isDerivedFrom("Path::Feature") for sel in FreeCADGui.Selection.getSelection()
-        ]
-        return selections and all(selections)
+        selection = FreeCADGui.Selection.getSelection()
+        if not selection:
+            return False
+        tcs = []
+        for sel in selection:
+            if not sel.isDerivedFrom("Path::Feature"):
+                return False
+            tc = toolController(sel)
+            if tc:
+                # Active only for operations with identical tool controller
+                tcs.append(tc)
+                if len(set(tcs)) != 1:
+                    return False
+            else:
+                return False
+            if PathUtil.coolantModeForOp(sel) != "None":
+                # Active only for operations without cooling
+                return False
+        return True
 
     def Activated(self):
 
@@ -514,5 +550,7 @@ class CommandPathArray:
 
 
 if FreeCAD.GuiUp:
+    global warningMessageAboutOldArrayAlreadyShowed
+    warningMessageAboutOldArrayAlreadyShowed = False
     # register the FreeCAD command
     FreeCADGui.addCommand("CAM_Array", CommandPathArray())
