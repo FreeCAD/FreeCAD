9d7b91bf15d98c79f3cd328d2a5d3b705157735d
Pieter Hijma <pieterhijma@users.noreply.github.com>
2025-06-02 09:39:15 +0200

    Doc: Improve the documentation of transactions

--- src/App/Transactions.cpp
+++ src/App/Transactions.cpp
@@ -57,10 +57,6 @@ Transaction::Transaction(int id)
     transID = id;
 }
 
-/**
- * A destructor.
- * A more elaborate description of the destructor.
- */
 Transaction::~Transaction()
 {
     auto& index = _Objects.get<0>();
@@ -281,16 +277,8 @@ TYPESYSTEM_SOURCE_ABSTRACT(App::TransactionObject, Base::Persistence)
 //**************************************************************************
 // Construction/Destruction
 
-/**
- * A constructor.
- * A more elaborate description of the constructor.
- */
 TransactionObject::TransactionObject() = default;
 
-/**
- * A destructor.
- * A more elaborate description of the destructor.
- */
 TransactionObject::~TransactionObject()
 {
     for (auto& v : _PropChangeMap) {
@@ -319,7 +307,7 @@ void TransactionObject::applyChn(Document& /*Doc*/, TransactionalObject* pcObj, 
             }
 
             // getPropertyName() is specially coded to be safe even if prop has
-            // been destroies. We must prepare for the case where user removed
+            // been destroyed. We must prepare for the case where user removed
             // a dynamic property but does not recordered as transaction.
             auto name = pcObj->getPropertyName(prop);
             if (!name || (!data.name.empty() && data.name != name)
