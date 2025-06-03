ca09f2d3f19a53d82a5831310f9eac12cf46e318
tarman3 <joice501790018@tutamail.com>
2025-06-03 15:58:45 +0300

    CAM: Threshold

--- src/Mod/CAM/Path/Op/Profile.py
+++ src/Mod/CAM/Path/Op/Profile.py
@@ -190,6 +190,15 @@ class ObjectProfile(PathAreaOp.ObjectOp):
                     "If doing multiple passes, the extra offset of each additional pass",
                 ),
             ),
+            (
+                "App::PropertyLength",
+                "RetractThreshold",
+                "Profile",
+                QT_TRANSLATE_NOOP(
+                    "App::Property",
+                    "Set distance which will attempts to avoid unnecessary retractions",
+                ),
+            ),
         ]
 
     @classmethod
@@ -333,6 +342,16 @@ class ObjectProfile(PathAreaOp.ObjectOp):
                 QT_TRANSLATE_NOOP(
                     "App::Property",
                     "If doing multiple passes, the extra offset of each additional pass",
+                ),
+            )
+        if not hasattr(obj, "RetractThreshold"):
+            obj.addProperty(
+                "App::PropertyLength",
+                "RetractThreshold",
+                "Pocket",
+                QT_TRANSLATE_NOOP(
+                    "App::Property",
+                    "Set distance which will attempts to avoid unnecessary retractions.",
                 ),
             )
 
