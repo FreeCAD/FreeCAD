be9c621434217f9fa7c5b3372f19c1f01badcc9a
Furgo <148809153+furgo16@users.noreply.github.com>
2025-06-13 04:30:53 +0200

    Import: DXF parser, add DXF block count

--- src/Mod/Import/App/dxf/dxf.cpp
+++ src/Mod/Import/App/dxf/dxf.cpp
@@ -2349,6 +2349,7 @@ bool CDxfRead::ReadBlockInfo()
     int blockType = 0;
     std::string blockName;
     InitializeAttributes();
+    m_stats.entityCounts["BLOCK"]++;
     // Both 2 and 3 are the block name.
     SetupStringAttribute(eName, blockName);
     SetupStringAttribute(eExtraText, blockName);
