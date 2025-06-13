e2346dabd6080aafe0622a80524067eafa67f8f1
paddle <pierrelouis.boyer@gmail.com>
2025-06-13 16:57:43 +0200

    Sketcher: Port and fix internal faces from RealThunder's branch
    Co-authored-by: Zheng Lei <realthunder.dev@gmail.com>
    Co-authored-by: Kacper Donat <kacper@kadet.net>
    Co-authored-by: Pierre-Louis Boyer <pierrelouis.boyer@gmail.com>

--- src/Mod/Sketcher/Gui/SketcherSettings.cpp
+++ src/Mod/Sketcher/Gui/SketcherSettings.cpp
@@ -113,6 +113,7 @@ void SketcherSettings::saveSettings()
     ui->checkBoxHorVerAuto->onSave();
     ui->checkBoxLineGroup->onSave();
     ui->checkBoxAddExtGeo->onSave();
+    ui->checkBoxMakeInternals->onSave();
 
     enum
     {
@@ -190,8 +191,7 @@ void SketcherSettings::loadSettings()
     ui->checkBoxHorVerAuto->onRestore();
     setProperty("checkBoxHorVerAuto", ui->checkBoxHorVerAuto->isChecked());
     ui->checkBoxAddExtGeo->onRestore();
-    setProperty("checkBoxLineGroup", ui->checkBoxLineGroup->isChecked());
-    ui->checkBoxAddExtGeo->onRestore();
+    ui->checkBoxMakeInternals->onRestore();
 
     // Dimensioning constraints mode
     ui->dimensioningMode->clear();
@@ -597,6 +597,8 @@ void SketcherSettingsAppearance::saveSettings()
     ui->ExternalWidth->onSave();
     ui->ExternalDefiningWidth->onSave();
 
+    ui->InternalFaceColor->onSave();
+
     ParameterGrp::handle hGrp = App::GetApplication().GetParameterGroupByPath(
         "User parameter:BaseApp/Preferences/Mod/Sketcher/View");
     QVariant data = ui->EdgePattern->itemData(ui->EdgePattern->currentIndex());
@@ -651,6 +653,9 @@ void SketcherSettingsAppearance::loadSettings()
     ui->InternalWidth->onRestore();
     ui->ExternalWidth->onRestore();
     ui->ExternalDefiningWidth->onRestore();
+
+    ui->InternalFaceColor->setAllowTransparency(true);
+    ui->InternalFaceColor->onRestore();
 
     ParameterGrp::handle hGrp = App::GetApplication().GetParameterGroupByPath(
         "User parameter:BaseApp/Preferences/Mod/Sketcher/View");
