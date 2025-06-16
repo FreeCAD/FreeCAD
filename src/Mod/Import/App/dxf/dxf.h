88ecc9972ccfc3c423825f1367a31ba077eb8cc2
Furgo <148809153+furgo16@users.noreply.github.com>
2025-06-16 14:32:54 +0200

    Import: DXF (all) report anonymous blocks separately

--- src/Mod/Import/App/dxf/dxf.h
+++ src/Mod/Import/App/dxf/dxf.h
@@ -188,6 +188,8 @@ struct DxfImportStats
     std::map<std::string, int> entityCounts;
     std::map<std::string, std::string> importSettings;
     std::map<std::string, std::vector<std::pair<int, std::string>>> unsupportedFeatures;
+    std::map<std::string, int> systemBlockCounts;
+
     int totalEntitiesCreated = 0;
 };
 
