145c7c91444dec1ee17d0c18f4cb36d9ef3d59d2
Ryan K <114723629+ryankembrey@users.noreply.github.com>
2025-06-24 16:49:59 +1000

    Update src/Mod/TechDraw/Gui/CommandExtensionPack.cpp
    Co-authored-by: Max Wilfinger <6246609+maxwxyz@users.noreply.github.com>

--- src/Mod/TechDraw/Gui/CommandExtensionPack.cpp
+++ src/Mod/TechDraw/Gui/CommandExtensionPack.cpp
@@ -1664,7 +1664,7 @@ CmdTechDrawExtendShortenLineGroup::CmdTechDrawExtendShortenLineGroup()
     sAppModule = "TechDraw";
     sGroup = QT_TR_NOOP("TechDraw");
     sMenuText = QT_TR_NOOP("Extend line");
-    sToolTipText = QT_TR_NOOP("Extends a selected cosmetic line or centerline at both ends:<br>\
+    sToolTipText = QT_TR_NOOP("Extends a selected cosmetic line or centerline at both ends by the specified delta distance")
 - Specify the delta distance (optional)<br>\
 - Select a single line<br>\
 - Click this tool");
