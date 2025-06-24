1950073569f5ad21dbfab049f54fc74d33dc03fa
Ryan K <114723629+ryankembrey@users.noreply.github.com>
2025-06-24 16:46:30 +1000

    Update src/Mod/TechDraw/Gui/CommandExtensionPack.cpp
    Co-authored-by: Max Wilfinger <6246609+maxwxyz@users.noreply.github.com>

--- src/Mod/TechDraw/Gui/CommandExtensionPack.cpp
+++ src/Mod/TechDraw/Gui/CommandExtensionPack.cpp
@@ -1285,7 +1285,7 @@ CmdTechDrawExtensionLineParallel::CmdTechDrawExtensionLineParallel()
     sAppModule = "TechDraw";
     sGroup = QT_TR_NOOP("TechDraw");
     sMenuText = QT_TR_NOOP("Add cosmetic parallel line");
-    sToolTipText = QT_TR_NOOP("Adds a cosmetic line parallel to another line through a vertex:<br>\
+    sToolTipText = QT_TR_NOOP("Adds a cosmetic line parallel to the selected line through the selected vertex")
 - Select a line<br>\
 - Select a vertex<br>\
 - Click this tool");
