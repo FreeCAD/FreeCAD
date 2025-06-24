fa6a14364675e2156bd0eaf6068f1ed63f2d2176
Ryan K <114723629+ryankembrey@users.noreply.github.com>
2025-06-24 16:44:36 +1000

    Update src/Mod/TechDraw/Gui/CommandExtensionPack.cpp
    Co-authored-by: Max Wilfinger <6246609+maxwxyz@users.noreply.github.com>

--- src/Mod/TechDraw/Gui/CommandExtensionPack.cpp
+++ src/Mod/TechDraw/Gui/CommandExtensionPack.cpp
@@ -1002,7 +1002,7 @@ CmdTechDrawExtensionDrawCosmCircle::CmdTechDrawExtensionDrawCosmCircle()
 {
     sAppModule = "TechDraw";
     sGroup = QT_TR_NOOP("TechDraw");
-    sMenuText = QT_TR_NOOP("Add cosmetic circle");
+    sMenuText = QT_TR_NOOP("Cosmetic circle");
     sToolTipText = QT_TR_NOOP("Adds a cosmetic circle based on two vertices:<br>\
 - Specify the line attributes (optional)<br>\
 - Select vertex 1 (center point)<br>\
