FreeCAD/FreeCAD

commit 935bdf9a0f1a93707518cc984a67090c99c1f979

PartDesign: Refactor single-solid rule enforcement
This refactors a single solid rule checking code from using the solid
count directly to using well abstracted `isSingleSolidRuleSatisfied`
method. This makes code easier to read and is the basis for next step
which is allowing users to disable this checks.

src/Mod/PartDesign/App/FeatureChamfer.cpp
@@ -270,10 +270,11 @@ App::DocumentObjectExecReturn *Chamfer::execute()
                 return new App::DocumentObjectExecReturn(QT_TRANSLATE_NOOP("Exception", "Resulting shape is invalid"));
             }
         }
-        int solidCount = countSolids(shape);
-        if (solidCount > 1) {
+        
+        if (!isSingleSolidRuleSatisfied(shape)) {
             return new App::DocumentObjectExecReturn(QT_TRANSLATE_NOOP("Exception", "Result has multiple solids: that is not currently supported."));
         }
+
         shape = refineShapeIfActive(shape);
         this->Shape.setValue(getSolid(shape));
         return App::DocumentObject::StdReturn;

src/Mod/PartDesign/App/FeatureFillet.cpp
@@ -193,8 +193,7 @@ App::DocumentObjectExecReturn *Fillet::execute()
             }
         }
 
-        int solidCount = countSolids(shape);
-        if (solidCount > 1) {
+        if (!isSingleSolidRuleSatisfied(shape)) {
             return new App::DocumentObjectExecReturn(QT_TRANSLATE_NOOP("Exception", "Result has multiple solids: that is not currently supported."));
         }
 

src/Mod/PartDesign/App/FeatureGroove.cpp
@@ -188,8 +188,7 @@ App::DocumentObjectExecReturn *Groove::execute()
             TopoDS_Shape subshape = refineShapeIfActive(mkCut.Shape());
             this->AddSubShape.setValue(subshape);
 
-            int resultCount = countSolids(result);
-            if (resultCount > 1) {
+            if (!isSingleSolidRuleSatisfied(result)) {
                 return new App::DocumentObjectExecReturn("Groove: Result has multiple solids. This is not supported at this time.");
             }
 
@@ -221,8 +220,7 @@ App::DocumentObjectExecReturn *Groove::execute()
             solRes = refineShapeIfActive(solRes);
             this->Shape.setValue(getSolid(solRes));
 
-            int solidCount = countSolids(solRes);
-            if (solidCount > 1) {
+            if (!isSingleSolidRuleSatisfied(solRes)) {
                 return new App::DocumentObjectExecReturn(QT_TRANSLATE_NOOP("Exception", "Result has multiple solids: that is not currently supported."));
             }
         }

src/Mod/PartDesign/App/FeatureLoft.cpp
@@ -288,13 +288,15 @@ App::DocumentObjectExecReturn *Loft::execute()
             BRepAlgoAPI_Fuse mkFuse(base, result);
             if (!mkFuse.IsDone())
                 return new App::DocumentObjectExecReturn(QT_TRANSLATE_NOOP("Exception", "Loft: Adding the loft failed"));
+                
             // we have to get the solids (fuse sometimes creates compounds)
             TopoDS_Shape boolOp = this->getSolid(mkFuse.Shape());
+
             // lets check if the result is a solid
             if (boolOp.IsNull())
                 return new App::DocumentObjectExecReturn(QT_TRANSLATE_NOOP("Exception", "Resulting shape is not a solid"));
-            int solidCount = countSolids(boolOp);
-            if (solidCount > 1) {
+
+            if (!isSingleSolidRuleSatisfied(boolOp)) {
                 return new App::DocumentObjectExecReturn(QT_TRANSLATE_NOOP("Exception", "Result has multiple solids: that is not currently supported."));
             }
 
@@ -306,13 +308,15 @@ App::DocumentObjectExecReturn *Loft::execute()
             BRepAlgoAPI_Cut mkCut(base, result);
             if (!mkCut.IsDone())
                 return new App::DocumentObjectExecReturn(QT_TRANSLATE_NOOP("Exception", "Loft: Subtracting the loft failed"));
+
             // we have to get the solids (fuse sometimes creates compounds)
             TopoDS_Shape boolOp = this->getSolid(mkCut.Shape());
+
             // lets check if the result is a solid
             if (boolOp.IsNull())
                 return new App::DocumentObjectExecReturn(QT_TRANSLATE_NOOP("Exception", "Resulting shape is not a solid"));
-            int solidCount = countSolids(boolOp);
-            if (solidCount > 1) {
+
+            if (!isSingleSolidRuleSatisfied(boolOp)) {
                 return new App::DocumentObjectExecReturn(QT_TRANSLATE_NOOP("Exception", "Result has multiple solids: that is not currently supported."));
             }
 

src/Mod/PartDesign/App/Feature.cpp
@@ -151,6 +151,12 @@ int Feature::countSolids(const TopoDS_Shape& shape, TopAbs_ShapeEnum type)
     return result;
 }
 
+bool Feature::isSingleSolidRuleSatisfied(const TopoDS_Shape& shape, TopAbs_ShapeEnum type)
+{
+    int solidCount = countSolids(shape, type);
+
+    return solidCount <= 1;
+}
 
 
 const gp_Pnt Feature::getPointFromFace(const TopoDS_Face& f)

src/Mod/PartDesign/App/Feature.h
@@ -100,6 +100,11 @@ protected:
     TopoShape getSolid(const TopoShape&);
     static int countSolids(const TopoDS_Shape&, TopAbs_ShapeEnum type = TopAbs_SOLID);
 
+    /**
+     * Checks if the single-solid body rule is fulfilled.
+     */
+    static bool isSingleSolidRuleSatisfied(const TopoDS_Shape&, TopAbs_ShapeEnum type = TopAbs_SOLID);
+
     /// Grab any point from the given face
     static const gp_Pnt getPointFromFace(const TopoDS_Face& f);
     /// Make a shape from a base plane (convenience method)

src/Mod/PartDesign/App/FeatureBoolean.cpp
@@ -153,8 +153,7 @@ App::DocumentObjectExecReturn *Boolean::execute()
 
     result = refineShapeIfActive(result);
 
-    int solidCount = countSolids(result);
-    if (solidCount > 1) {
+    if (!isSingleSolidRuleSatisfied(result)) {
         return new App::DocumentObjectExecReturn(QT_TRANSLATE_NOOP("Exception", "Result has multiple solids: that is not currently supported."));
     }
 

src/Mod/PartDesign/App/FeatureDraft.cpp
@@ -318,8 +318,7 @@ App::DocumentObjectExecReturn *Draft::execute()
         if (shape.IsNull())
             return new App::DocumentObjectExecReturn(QT_TRANSLATE_NOOP("Exception", "Resulting shape is null"));
 
-        int solidCount = countSolids(shape);
-        if (solidCount > 1) {
+        if (!isSingleSolidRuleSatisfied(shape)) {
             return new App::DocumentObjectExecReturn(QT_TRANSLATE_NOOP("Exception", "Result has multiple solids: that is not currently supported."));
         }
 

commit eb52dd4a9a238fd70b1e9b8cfe8a59ac2727750e

PD: Fix regression about single-solid check
For several PD features the single-solid check fails. The regression
is caused by PR 13960 and reported as issue 19002. The reason for the
failure is that the first solid of the output shape is retrieved and
then checked for a single solid. This test will always pass, of course.

The single-solid is fixed for these features:
* Pad
* Pocket (never worked there)
* Fillet
* Chamfer
* Groove (never worked there)
* Revolution (never worked there)
* Loft

Fixes: 935bdf9a0f1a ("PartDesign: Refactor single-solid rule enforcement")

src/Mod/PartDesign/App/FeatureChamfer.cpp
@@ -159,7 +159,6 @@ App::DocumentObjectExecReturn *Chamfer::execute()
 
         TopTools_ListOfShape aLarg;
         aLarg.Append(TopShape.getShape());
-        bool failed = false;
         if (!BRepAlgo::IsValid(aLarg, shape.getShape(), Standard_False, Standard_False)) {
             ShapeFix_ShapeTolerance aSFT;
             aSFT.LimitTolerance(shape.getShape(),
@@ -167,21 +166,16 @@ App::DocumentObjectExecReturn *Chamfer::execute()
                                 Precision::Confusion(),
                                 TopAbs_SHAPE);
         }
-        if (!failed) {
 
-            // store shape before refinement
-            this->rawShape = shape;
-            shape = refineShapeIfActive(shape);
-            shape = getSolid(shape);
-        }
+        // store shape before refinement
+        this->rawShape = shape;
+        shape = refineShapeIfActive(shape);
         if (!isSingleSolidRuleSatisfied(shape.getShape())) {
             return new App::DocumentObjectExecReturn(QT_TRANSLATE_NOOP("Exception", "Result has multiple solids: that is not currently supported."));
         }
+
+        shape = getSolid(shape);
         this->Shape.setValue(shape);
-        if (failed) {
-            return new App::DocumentObjectExecReturn(
-                QT_TRANSLATE_NOOP("Exception", "Resulting shape is invalid"));
-        }
         return App::DocumentObject::StdReturn;
     }
     catch (Standard_Failure& e) {

src/Mod/PartDesign/App/FeatureExtrude.cpp
@@ -809,7 +809,7 @@ App::DocumentObjectExecReturn* FeatureExtrude::buildExtrusion(ExtrudeOptions opt
 
             // store shape before refinement
             this->rawShape = result;
-            solRes = refineShapeIfActive(solRes);
+            solRes = refineShapeIfActive(result);
 
             if (!isSingleSolidRuleSatisfied(solRes.getShape())) {
                 return new App::DocumentObjectExecReturn(QT_TRANSLATE_NOOP("Exception", "Result has multiple solids: that is not currently supported."));
@@ -824,10 +824,10 @@ App::DocumentObjectExecReturn* FeatureExtrude::buildExtrusion(ExtrudeOptions opt
             // store shape before refinement
             this->rawShape = prism;
             prism = refineShapeIfActive(prism);
-            prism = getSolid(prism);
             if (!isSingleSolidRuleSatisfied(prism.getShape())) {
                 return new App::DocumentObjectExecReturn(QT_TRANSLATE_NOOP("Exception", "Result has multiple solids: that is not currently supported."));
             }
+            prism = getSolid(prism);
             this->Shape.setValue(prism);
         }
         else {

src/Mod/PartDesign/App/FeatureFillet.cpp
@@ -104,7 +104,6 @@ App::DocumentObjectExecReturn *Fillet::execute()
 
         TopTools_ListOfShape aLarg;
         aLarg.Append(baseShape.getShape());
-        bool failed = false;
         if (!BRepAlgo::IsValid(aLarg, shape.getShape(), Standard_False, Standard_False)) {
             ShapeFix_ShapeTolerance aSFT;
             aSFT.LimitTolerance(shape.getShape(),
@@ -113,20 +112,15 @@ App::DocumentObjectExecReturn *Fillet::execute()
                                 TopAbs_SHAPE);
         }
 
-        if (!failed) {
-            // store shape before refinement
-            this->rawShape = shape;
-            shape = refineShapeIfActive(shape);
-            shape = getSolid(shape);
-        }
+        // store shape before refinement
+        this->rawShape = shape;
+        shape = refineShapeIfActive(shape);
         if (!isSingleSolidRuleSatisfied(shape.getShape())) {
             return new App::DocumentObjectExecReturn(QT_TRANSLATE_NOOP("Exception", "Result has multiple solids: that is not currently supported."));
         }
-        this->Shape.setValue(shape);
 
-        if (failed) {
-            return new App::DocumentObjectExecReturn("Resulting shape is invalid");
-        }
+        shape = getSolid(shape);
+        this->Shape.setValue(shape);
         return App::DocumentObject::StdReturn;
     }
     catch (Standard_Failure& e) {

src/Mod/PartDesign/App/FeatureGroove.cpp
@@ -185,17 +185,17 @@ App::DocumentObjectExecReturn *Groove::execute()
         }catch(Standard_Failure &) {
             return new App::DocumentObjectExecReturn("Failed to cut base feature");
         }
-        boolOp = this->getSolid(boolOp);
-        if (boolOp.isNull())
+        TopoShape solid = this->getSolid(boolOp);
+        if (solid.isNull())
             return new App::DocumentObjectExecReturn("Resulting shape is not a solid");
 
         // store shape before refinement
         this->rawShape = boolOp;
         boolOp = refineShapeIfActive(boolOp);
-        boolOp = getSolid(boolOp);
         if (!isSingleSolidRuleSatisfied(boolOp.getShape())) {
             return new App::DocumentObjectExecReturn(QT_TRANSLATE_NOOP("Exception", "Result has multiple solids: that is not currently supported."));
         }
+        boolOp = getSolid(boolOp);
         Shape.setValue(boolOp);
         return App::DocumentObject::StdReturn;
     }

src/Mod/PartDesign/App/FeatureLoft.cpp
@@ -266,10 +266,10 @@ App::DocumentObjectExecReturn *Loft::execute()
         // store shape before refinement
         this->rawShape = boolOp;
         boolOp = refineShapeIfActive(boolOp);
-        boolOp = getSolid(boolOp);
         if (!isSingleSolidRuleSatisfied(boolOp.getShape())) {
             return new App::DocumentObjectExecReturn(QT_TRANSLATE_NOOP("Exception", "Result has multiple solids: that is not currently supported."));
         }
+        boolOp = getSolid(boolOp);
         Shape.setValue(boolOp);
         return App::DocumentObject::StdReturn;
     }

src/Mod/PartDesign/App/FeatureRevolution.cpp
@@ -228,7 +228,11 @@ App::DocumentObjectExecReturn* Revolution::execute()
                 this->rawShape = result;
                 result = refineShapeIfActive(result);
             }
-            this->Shape.setValue(getSolid(result));
+            if (!isSingleSolidRuleSatisfied(result.getShape())) {
+                return new App::DocumentObjectExecReturn(QT_TRANSLATE_NOOP("Exception", "Result has multiple solids: that is not currently supported."));
+            }
+            result = getSolid(result);
+            this->Shape.setValue(result);
         }
         else {
             return new App::DocumentObjectExecReturn(