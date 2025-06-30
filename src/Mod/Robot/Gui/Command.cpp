4718388d05c02f52bcec90ad3c97781216567159
Max Wilfinger <6246609+maxwxyz@users.noreply.github.com>
2025-06-30 09:52:15 +0200

    Update src/Mod/Robot/Gui/Command.cpp
    Co-authored-by: Ryan K <114723629+ryankembrey@users.noreply.github.com>

--- src/Mod/Robot/Gui/Command.cpp
+++ src/Mod/Robot/Gui/Command.cpp
@@ -162,7 +162,7 @@ CmdRobotConstraintAxle::CmdRobotConstraintAxle()
     sAppModule = "Robot";
     sGroup = QT_TR_NOOP("Robot");
     sMenuText = QT_TR_NOOP("Place Robot…");
-    sToolTipText = QT_TR_NOOP("Places a robot");
+    sToolTipText = QT_TR_NOOP("Places a robot in the document");
     sWhatsThis = "Robot_Create";
     sStatusTip = sToolTipText;
     sPixmap = "Robot_CreateRobot";
