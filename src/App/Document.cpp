55b5bb603f142ec80ecbd0943b1fcbcf285b96d7
theo-vt <theo.veilleux.trinh@proton.me>
2025-07-03 10:34:15 -0400

    Fix some out of order operations

--- src/App/Document.cpp
+++ src/App/Document.cpp
@@ -585,6 +585,7 @@ bool Document::_commitTransaction(const bool notify)
         std::cerr<<"Close transaction #"<<id<<"\n";
         mUndoTransactions.push_back(d->activeUndoTransaction);
         d->activeUndoTransaction = nullptr;
+        d->bookedTransaction = 0;
 
         // check the stack for the limits
         if (mUndoTransactions.size() > d->UndoMaxStackSize) {
@@ -599,7 +600,6 @@ bool Document::_commitTransaction(const bool notify)
             GetApplication().closeActiveTransaction(false, id);
         }
     }
-    d->bookedTransaction = 0;
     return true;
 }
 
@@ -635,9 +635,9 @@ void Document::_abortTransaction()
         mUndoMap.erase(d->activeUndoTransaction->getID());
         delete d->activeUndoTransaction;
         d->activeUndoTransaction = nullptr;
+        d->bookedTransaction = 0;
         signalAbortTransaction(*this);
     }
-    d->bookedTransaction = 0;
 }
 
 bool Document::hasPendingTransaction() const
