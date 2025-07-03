9b9c6e43756e7569097130650d0dd6ebc127bc11
theo-vt <theo.veilleux.trinh@proton.me>
2025-07-03 15:50:41 -0400

    Replace last use of Application::editDocument() in cpp

--- src/Gui/OverlayWidgets.cpp
+++ src/Gui/OverlayWidgets.cpp
@@ -1162,12 +1162,13 @@ bool OverlayTabWidget::checkAutoHide() const
     // if it is/is not in edit, how to make this work with multiple
     // edit document? Compare with the active document?
     // e.g. ->isInEdit(ActiveDocument())
+    bool activeDocInEdit = Application::Instance->isInEdit(Application::Instance->activeDocument());
     if (autoMode == AutoMode::EditShow) {
-        return !Application::Instance->editDocument()
+        return !activeDocInEdit
             && (!Control().taskPanel() || Control().taskPanel()->isEmpty(false));
     }
 
-    if (autoMode == AutoMode::EditHide && Application::Instance->editDocument()) {
+    if (autoMode == AutoMode::EditHide && activeDocInEdit) {
         return true;
     }
 
