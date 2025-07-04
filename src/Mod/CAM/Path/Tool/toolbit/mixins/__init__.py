b0403c8587140d0043929305d5756d3f03083fc9
Daniel Wood <s.d.wood.82@googlemail.com>
2025-07-04 20:20:11 +0100

    Add isoshape model

--- src/Mod/CAM/Path/Tool/toolbit/mixins/__init__.py
+++ src/Mod/CAM/Path/Tool/toolbit/mixins/__init__.py
@@ -2,8 +2,10 @@
 
 from .rotary import RotaryToolBitMixin
 from .cutting import CuttingToolMixin
+from .turning import TurningToolMixin
 
 __all__ = [
     "RotaryToolBitMixin",
     "CuttingToolMixin",
+    "TurningToolMixin",
 ]
