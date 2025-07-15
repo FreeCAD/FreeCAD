3d07a67264d78054f7b9c9fd5462fc23ddbeb5fb
Furgo <148809153+furgo16@users.noreply.github.com>
2025-07-15 02:54:32 +0200

    Import: DXF exporter, simplify file structure

--- src/Mod/Import/App/dxf/ImpExpDxf.h
+++ src/Mod/Import/App/dxf/ImpExpDxf.h
@@ -26,6 +26,7 @@
 #include <set>
 #include <gp_Pnt.hxx>
 
+#include <Base/PyObjectBase.h>
 #include <App/Document.h>
 #include <App/Link.h>
 #include <TopoDS_Shape.hxx>
@@ -562,6 +563,13 @@ protected:
 
     Base::Vector3d m_projectionDir;
 };
+
+/**
+ * The core, non-GUI DXF export logic. This function is exported from the
+ * App module to be shared with the Gui module.
+ */
+ImportExport void
+executeDxfExport(PyObject* objectList, ImpExpDxfWrite& writer, PyObject* helperModule);
 
 }  // namespace Import
 
