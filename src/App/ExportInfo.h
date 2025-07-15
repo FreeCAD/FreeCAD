559bc3466b2c07c8d7c6ae249742053ac7f17556
theo-vt <theo.veilleux.trinh@proton.me>
2025-07-15 19:49:34 -0400

    Update ExportInfo.h

--- src/App/ExportInfo.h
+++ src/App/ExportInfo.h
@@ -12,8 +12,7 @@ namespace App
 /// Struct that holds information about the last
 /// export so that the user does not have to reenter
 /// them on each export. The export command either uses
-/// a document's export info or the global application's
-/// export info depending on the "SingularExportPathPerDocument" parameter
+/// the target document's export info
 struct ExportInfo {
     std::string location {};
     std::string filename {};
