a8a44aab0a0f148f82348ee27522d8d6d5d33eca
Roy-043 <info@b-k-g.nl>
2025-07-15 20:29:44 +0200

    Draft: fix handling of MouseDelay
    The MouseDelay sets a point value to None. In some cases this point was then still used resulting in attribute errors.
    
    See:
    https://github.com/FreeCAD/FreeCAD/issues/19531#issuecomment-2651341711

--- src/Mod/Draft/draftguitools/gui_rectangles.py
+++ src/Mod/Draft/draftguitools/gui_rectangles.py
@@ -158,6 +158,8 @@ class Rectangle(gui_base_original.Creator):
         if arg["Type"] == "SoKeyboardEvent":
             if arg["Key"] == "ESCAPE":
                 self.finish()
+        elif not self.ui.mouse:
+            pass
         elif arg["Type"] == "SoLocation2Event":  # mouse movement detection
             self.point, ctrlPoint, info = gui_tool_utils.getPoint(self, arg, noTracker=True)
             self.rect.update(self.point)
