461e3475d22f28c1bc068bab51c0b6dc3f23bd15
theo-vt <theo.veilleux.trinh@proton.me>
2025-07-16 13:09:18 -0400

    Add a mecanism to tell taskview dialogs they are/are not active -- and use it on TaskMeasure

--- src/Mod/Measure/Gui/TaskMeasure.cpp
+++ src/Mod/Measure/Gui/TaskMeasure.cpp
@@ -62,8 +62,6 @@ using SelectionStyle = Gui::SelectionSingleton::SelectionStyle;
 
 TaskMeasure::TaskMeasure()
 {
-    qApp->installEventFilter(this);
-
     this->setButtonPosition(TaskMeasure::South);
     auto taskbox = new Gui::TaskView::TaskBox(Gui::BitmapFactory().pixmap("umf-measurement"),
                                               tr("Measurement"),
@@ -74,12 +72,7 @@ TaskMeasure::TaskMeasure()
     settings.beginGroup(QLatin1String(taskMeasureSettingsGroup));
     delta = settings.value(QLatin1String(taskMeasureShowDeltaSettingsName), true).toBool();
     mAutoSave = settings.value(QLatin1String(taskMeasureAutoSaveSettingsName), mAutoSave).toBool();
-    if (settings.value(QLatin1String(taskMeasureGreedySelection), false).toBool()) {
-        Gui::Selection().setSelectionStyle(SelectionStyle::GreedySelection);
-    }
-    else {
-        Gui::Selection().setSelectionStyle(SelectionStyle::NormalSelection);
-    }
+    mGreedySelection = settings.value(QLatin1String(taskMeasureGreedySelection), false).toBool();
     settings.endGroup();
 
     showDelta = new QCheckBox();
@@ -159,9 +152,6 @@ TaskMeasure::TaskMeasure()
 
     Content.emplace_back(taskbox);
 
-    // engage the selectionObserver
-    attachSelection();
-
     mTargetDoc = Gui::Application::Instance->activeDocument();
     if (mTargetDoc) {
         mTargetDoc->openCommand("Add Measurement");
@@ -173,11 +163,7 @@ TaskMeasure::TaskMeasure()
 }
 
 TaskMeasure::~TaskMeasure()
-{
-    Gui::Selection().setSelectionStyle(SelectionStyle::NormalSelection);
-    detachSelection();
-    qApp->removeEventFilter(this);
-}
+{}
 
 
 void TaskMeasure::modifyStandardButtons(QDialogButtonBox* box)
@@ -435,6 +421,19 @@ void TaskMeasure::reset()
 
     this->update();
 }
+void TaskMeasure::activate()
+{
+    updateSelectionType();
+    // engage the selectionObserver
+    attachSelection();
+    qApp->installEventFilter(this);
+}
+void TaskMeasure::deactivate()
+{
+    Gui::Selection().setSelectionStyle(SelectionStyle::NormalSelection);
+    detachSelection();
+    qApp->removeEventFilter(this);
+}
 
 
 void TaskMeasure::removeObject()
@@ -557,15 +556,20 @@ void TaskMeasure::newMeasurementBehaviourChanged(bool checked)
 {
     QSettings settings;
     settings.beginGroup(QLatin1String(taskMeasureSettingsGroup));
-    if (!checked) {
-        Gui::Selection().setSelectionStyle(SelectionStyle::NormalSelection);
-        settings.setValue(QLatin1String(taskMeasureGreedySelection), false);
+    settings.setValue(QLatin1String(taskMeasureGreedySelection), true);
+    mGreedySelection = checked;
+    updateSelectionType();
+
+    settings.endGroup();
+}
+void TaskMeasure::updateSelectionType()
+{
+    if (mGreedySelection) {
+        Gui::Selection().setSelectionStyle(SelectionStyle::GreedySelection);
     }
     else {
-        Gui::Selection().setSelectionStyle(SelectionStyle::GreedySelection);
-        settings.setValue(QLatin1String(taskMeasureGreedySelection), true);
+        Gui::Selection().setSelectionStyle(SelectionStyle::NormalSelection);
     }
-    settings.endGroup();
 }
 
 void TaskMeasure::setModeSilent(App::MeasureType* mode)
