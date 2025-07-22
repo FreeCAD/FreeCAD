f08c477ae21508cecd9ec8104a02a41ed8202adb
theo-vt <theo.veilleux.trinh@proton.me>
2025-07-22 16:41:05 -0400

    Introduce Hole::guessNormalDirection

--- src/Mod/PartDesign/App/FeatureHole.cpp
+++ src/Mod/PartDesign/App/FeatureHole.cpp
@@ -1412,7 +1412,7 @@ double Hole::getThreadProfileAngle()
 void Hole::findClosestDesignation()
 {
     int threadType = ThreadType.getValue();
-    const int numTypes = static_cast<int>(std::size(threadDescription)); 
+    const int numTypes = static_cast<int>(std::size(threadDescription));
 
     if (threadType < 0 || threadType >= numTypes) {
         throw Base::IndexError(QT_TRANSLATE_NOOP("Exception", "Thread type is invalid"));
@@ -1760,9 +1760,9 @@ void Hole::setupObject()
 
     Base::Reference<ParameterGrp> hGrp = App::GetApplication().GetUserParameter()
         .GetGroup("BaseApp")->GetGroup("Preferences")->GetGroup("Mod/PartDesign");
-    
+
     BaseProfileType.setValue(baseProfileOption_idxToBitmask(hGrp->GetInt("defaultBaseTypeHole", 1)));
-    
+
     ProfileBased::setupObject();
 }
 
@@ -1889,9 +1889,9 @@ static gp_Pnt toPnt(gp_Vec dir)
 App::DocumentObjectExecReturn* Hole::execute()
 {
     TopoShape profileshape =
-        getProfileShape(  Part::ShapeOption::NeedSubElement 
+        getProfileShape(  Part::ShapeOption::NeedSubElement
                         | Part::ShapeOption::ResolveLink
-                        | Part::ShapeOption::Transform 
+                        | Part::ShapeOption::Transform
                         | Part::ShapeOption::DontSimplifyCompound);
 
     // Find the base shape
@@ -1920,26 +1920,8 @@ App::DocumentObjectExecReturn* Hole::execute()
         /* Build the prototype hole */
 
         // Get vector normal to profile
-        Base::Vector3d SketchVector;
+        Base::Vector3d SketchVector = guessNormalDirection(profileshape);
 
-        // If trying to build a hole from a cylinder face
-        // we must try to find the direction ourselves as
-        // getProfileNormal() will try to find the normal to
-        // the middle of the face
-        if (profileshape.hasSubShape(TopAbs_FACE)) {
-            BRepAdaptor_Surface sf(TopoDS::Face(profileshape.getSubShape(TopAbs_FACE, 1)));
-
-            if (sf.GetType() == GeomAbs_Cylinder) {
-                sf.Cylinder().Axis().Direction().Coord(SketchVector.x,
-                                                       SketchVector.y,
-                                                       SketchVector.z);
-            } else {
-                throw(Base::Exception("Cannot create hole from non cylindrical face"));
-            }
-
-        } else {
-            SketchVector = getProfileNormal();
-        }
         if (Reversed.getValue())
             SketchVector *= -1.0;
 
@@ -2275,6 +2257,27 @@ gp_Vec Hole::computePerpendicular(const gp_Vec& zDir) const
     xDir.Normalize();
     return xDir;
 }
+Base::Vector3d Hole::guessNormalDirection(const TopoShape& profileshape) const
+{
+    // If trying to build a hole from a cylinder face
+    // we must try to find the direction ourselves as
+    // getProfileNormal() will try to find the normal to
+    // the middle of the face
+    if (profileshape.hasSubShape(TopAbs_FACE)) {
+        BRepAdaptor_Surface sf(TopoDS::Face(profileshape.getSubShape(TopAbs_FACE, 1)));
+
+        if (sf.GetType() == GeomAbs_Cylinder) {
+            Base::Vector3d out;
+            sf.Cylinder().Axis().Direction().Coord(out.x,
+                                                   out.y,
+                                                   out.z);
+            return out;
+        } else {
+            throw(Base::Exception("Cannot create hole from non cylindrical face"));
+        }
+    }
+    return getProfileNormal();
+}
 TopoShape Hole::findHoles(std::vector<TopoShape> &holes,
                           const TopoShape& profileshape,
                           const TopoDS_Shape& protoHole) const
@@ -2300,7 +2303,7 @@ TopoShape Hole::findHoles(std::vector<TopoShape> &holes,
     int baseProfileType = BaseProfileType.getValue();
 
     // Iterate over edges and filter out non-circle/non-arc types
-    if (baseProfileType & BaseProfileTypeOptions::OnCircles || 
+    if (baseProfileType & BaseProfileTypeOptions::OnCircles ||
        baseProfileType & BaseProfileTypeOptions::OnArcs) {
         for (const auto &profileEdge : profileshape.getSubTopoShapes(TopAbs_EDGE)) {
             TopoDS_Edge edge = TopoDS::Edge(profileEdge.getShape());
@@ -2314,7 +2317,7 @@ TopoShape Hole::findHoles(std::vector<TopoShape> &holes,
             if (!(baseProfileType & BaseProfileTypeOptions::OnCircles) && adaptor.IsClosed()) {
                 continue;
             }
-            
+
             // Filter for arcs
             if (!(baseProfileType & BaseProfileTypeOptions::OnArcs) && !adaptor.IsClosed()) {
                 continue;
@@ -2747,7 +2750,7 @@ int Hole::baseProfileOption_idxToBitmask(int index)
     }
      if (index == 2) {
         return PartDesign::Hole::BaseProfileTypeOptions::OnPoints;
-    } 
+    }
     Base::Console().error("Unexpected hole base profile combobox index: %i", index);
     return 0;
 }
@@ -2755,10 +2758,10 @@ int Hole::baseProfileOption_bitmaskToIdx(int bitmask)
 {
     if (bitmask == PartDesign::Hole::BaseProfileTypeOptions::OnCirclesArcs) {
         return 0;
-    } 
+    }
     if (bitmask == PartDesign::Hole::BaseProfileTypeOptions::OnPointsCirclesArcs) {
         return 1;
-    } 
+    }
     if (bitmask == PartDesign::Hole::BaseProfileTypeOptions::OnPoints) {
         return 2;
     }
