25f1d284687cb650a93b8bdb14a00fbc1c249012
Chris Hennes <chennes@gmail.com>
2025-07-23 16:56:36 -0500

    Tests: Ensure tests use a fixture that inits app

--- tests/src/Base/Axis.cpp
+++ tests/src/Base/Axis.cpp
@@ -4,21 +4,32 @@
 #include <Base/Placement.h>
 #include <Base/Rotation.h>
 
-TEST(Axis, TestDefault)
+#include <src/App/InitApplication.h>
+
+class Axis: public ::testing::Test
+{
+protected:
+    static void SetUpTestSuite()
+    {
+        tests::initApplication();
+    }
+};
+
+TEST_F(Axis, TestDefault)
 {
     Base::Axis axis;
     EXPECT_EQ(axis.getBase(), Base::Vector3d());
     EXPECT_EQ(axis.getDirection(), Base::Vector3d());
 }
 
-TEST(Axis, TestCustom)
+TEST_F(Axis, TestCustom)
 {
     Base::Axis axis(Base::Vector3d(0, 0, 1), Base::Vector3d(1, 1, 1));
     EXPECT_EQ(axis.getBase(), Base::Vector3d(0, 0, 1));
     EXPECT_EQ(axis.getDirection(), Base::Vector3d(1, 1, 1));
 }
 
-TEST(Axis, TestSetter)
+TEST_F(Axis, TestSetter)
 {
     Base::Axis axis;
     axis.setBase(Base::Vector3d(0, 0, 1));
@@ -27,7 +38,7 @@ TEST(Axis, TestSetter)
     EXPECT_EQ(axis.getDirection(), Base::Vector3d(1, 1, 1));
 }
 
-TEST(Axis, TestAssign)
+TEST_F(Axis, TestAssign)
 {
     Base::Axis axis;
     Base::Axis move;
@@ -38,7 +49,7 @@ TEST(Axis, TestAssign)
     EXPECT_EQ(move.getDirection(), Base::Vector3d(1, 1, 1));
 }
 
-TEST(Axis, TestReverse)
+TEST_F(Axis, TestReverse)
 {
     Base::Axis axis(Base::Vector3d(0, 0, 1), Base::Vector3d(1, 1, 1));
     axis.reverse();
@@ -46,7 +57,7 @@ TEST(Axis, TestReverse)
     EXPECT_EQ(axis.getDirection(), Base::Vector3d(-1, -1, -1));
 }
 
-TEST(Axis, TestReversed)
+TEST_F(Axis, TestReversed)
 {
     Base::Axis axis(Base::Vector3d(0, 0, 1), Base::Vector3d(1, 1, 1));
     Base::Axis rev(axis.reversed());
@@ -55,7 +66,7 @@ TEST(Axis, TestReversed)
     EXPECT_EQ(rev.getDirection(), Base::Vector3d(-1, -1, -1));
 }
 
-TEST(Axis, TestMove)
+TEST_F(Axis, TestMove)
 {
     Base::Axis axis(Base::Vector3d(0, 0, 1), Base::Vector3d(1, 1, 1));
     axis.move(Base::Vector3d(1, 2, 3));
@@ -63,7 +74,7 @@ TEST(Axis, TestMove)
     EXPECT_EQ(axis.getDirection(), Base::Vector3d(1, 1, 1));
 }
 
-TEST(Axis, TestMult)
+TEST_F(Axis, TestMult)
 {
     Base::Axis axis(Base::Vector3d(0, 0, 1), Base::Vector3d(0, 0, 1));
     axis *= Base::Placement(Base::Vector3d(1, 1, 1), Base::Rotation(1, 0, 0, 0));
