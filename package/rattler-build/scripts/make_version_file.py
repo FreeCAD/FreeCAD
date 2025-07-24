80439316af7383288f3d326afbc8c70151eb3c7b
ColdWindScholar <3590361911@qq.com>
2025-07-24 22:24:50 +0800

    code:make_version_file: format the code

--- package/rattler-build/scripts/make_version_file.py
+++ package/rattler-build/scripts/make_version_file.py
@@ -10,33 +10,34 @@ sys.path.append(f"{os.getcwd()}/src/Tools")
 import SubWCRev
 
 gitInfo = SubWCRev.GitControl()
-gitInfo.extractInfo("","")
+gitInfo.extractInfo("", "")
 gitDescription = os.environ['BUILD_TAG']
 
 i = open("src/Build/Version.h.cmake")
 content = []
 for line in i.readlines():
-	line = line.replace("-${PACKAGE_VERSION_SUFFIX}",gitDescription)
-	line = line.replace("${PACKAGE_WCREF}",gitInfo.rev)
-	line = line.replace("${PACKAGE_WCDATE}",gitInfo.date)
-	line = line.replace("${PACKAGE_WCURL}",gitInfo.url)
-	content.append(line)
+    line = line.replace("-${PACKAGE_VERSION_SUFFIX}", gitDescription)
+    line = line.replace("${PACKAGE_WCREF}", gitInfo.rev)
+    line = line.replace("${PACKAGE_WCDATE}", gitInfo.date)
+    line = line.replace("${PACKAGE_WCURL}", gitInfo.url)
+    content.append(line)
 
 with open("src/Build/Version.h.cmake", "w") as o:
-	content.append('// Git relevant stuff\n')
-	content.append('#define FCRepositoryHash   "%s"\n' % gitInfo.hash)
-	content.append('#define FCRepositoryBranch "%s"\n' % gitInfo.branch)
-	o.writelines(content)
+    content.append('// Git relevant stuff\n')
+    content.append('#define FCRepositoryHash   "%s"\n' % gitInfo.hash)
+    content.append('#define FCRepositoryBranch "%s"\n' % gitInfo.branch)
+    o.writelines(content)
 # os.sys.argv is not exists.should be sys.argv
 with open(sys.argv[1], "w") as f:
-	f.write(f"rev_number: {gitInfo.rev}\n")
-	f.write(f"branch_name: {gitInfo.branch}\n")
-	f.write(f"commit_date: {gitInfo.date}\n")
-	f.write(f"commit_hash: {gitInfo.hash}\n")
-	f.write(f"remote_url: {gitInfo.url}\n")
+    f.write(f"rev_number: {gitInfo.rev}\n")
+    f.write(f"branch_name: {gitInfo.branch}\n")
+    f.write(f"commit_date: {gitInfo.date}\n")
+    f.write(f"commit_hash: {gitInfo.hash}\n")
+    f.write(f"remote_url: {gitInfo.url}\n")
 
-p = subprocess.Popen(["git", "-c", "user.name='github-actions[bot]'", "-c", "user.email='41898282+github-actions[bot]@users.noreply.github.com'",
-		       "commit", "-a", "-m", "add git information"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+p = subprocess.Popen(["git", "-c", "user.name='github-actions[bot]'", "-c",
+                      "user.email='41898282+github-actions[bot]@users.noreply.github.com'",
+                      "commit", "-a", "-m", "add git information"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 out, err = p.communicate()
 
