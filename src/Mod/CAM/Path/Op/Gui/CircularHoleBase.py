5bc43bae9ab0c647f4004e8d494341cad22e69d0
J-Dunn <dunn@piments.com>
2025-08-04 21:23:50 +0200

    Add clearBase btn callback

--- src/Mod/CAM/Path/Op/Gui/CircularHoleBase.py
+++ src/Mod/CAM/Path/Op/Gui/CircularHoleBase.py
@@ -162,14 +162,23 @@ class TaskPanelHoleGeometryPage(PathOpGui.TaskPanelBaseGeometryPage):
         self.form.addBase.clicked.connect(self.addBase)
         self.form.deleteBase.clicked.connect(self.deleteBase)
         self.form.resetBase.clicked.connect(self.resetBase)
+        self.form.clearBase.clicked.connect(self.clearBase)
         self.form.baseList.itemChanged.connect(self.checkedChanged)
 
     def resetBase(self):
-        """resetBase() ... push button callback"""
+        """resetBase() ... Reset push button callback"""
         self.obj.Base = []
         self.obj.Disabled = []
         self.obj.Proxy.findAllHoles(self.obj)
 
+        self.obj.Proxy.execute(self.obj)
+        FreeCAD.ActiveDocument.recompute()
+
+    def clearBase(self):
+        print("entering clearBase")
+        """clearBase() ... Clear push button callback"""
+        self.obj.Base = []
+        self.obj.Disabled = []
         self.obj.Proxy.execute(self.obj)
         FreeCAD.ActiveDocument.recompute()
 
