195e5d74fb60ad309035d4d5f300abe8579d5c65
B0cho <obiekt44@gmail.com>
2025-08-06 17:05:18 +0200

    Core: Remove redundant spacers from Expression editor

--- src/Gui/Dialogs/DlgExpressionInput.cpp
+++ src/Gui/Dialogs/DlgExpressionInput.cpp
@@ -114,7 +114,6 @@ DlgExpressionInput::DlgExpressionInput(const App::ObjectIdentifier & _path,
         ui->msg->setWordWrap(true);
         ui->msg->setMaximumHeight(500);
         ui->msg->setMinimumWidth(200);
-        ui->horizontalSpacer_3->changeSize(0, 2);
         ui->verticalLayout->setContentsMargins(9, 9, 9, 9);
         this->adjustSize();
         // It is strange that (at least on Linux) DlgExpressionInput will shrink
