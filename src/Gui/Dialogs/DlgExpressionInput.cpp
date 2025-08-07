da895edc96c8d338b8959c15ddde0d6f96a70451
B0cho <obiekt44@gmail.com>
2025-08-07 12:24:24 +0200

    Core: Make too long 'Result' message elide in Expression editor

--- src/Gui/Dialogs/DlgExpressionInput.cpp
+++ src/Gui/Dialogs/DlgExpressionInput.cpp
@@ -112,7 +112,7 @@ DlgExpressionInput::DlgExpressionInput(const App::ObjectIdentifier & _path,
     else {
         ui->expression->setMinimumHeight(50);
         ui->msg->setWordWrap(true);
-        ui->msg->setMaximumHeight(500);
+        ui->msg->setMaximumHeight(200);
         ui->msg->setMinimumWidth(200);
         ui->verticalLayout->setContentsMargins(9, 9, 9, 9);
         this->adjustSize();
@@ -786,8 +786,8 @@ void DlgExpressionInput::setMsgText()
     // find words longer than length of msg widget
     // then insert newline to wrap it
     std::string wrappedMsg{};
-    static constexpr int msgContentMargins = 10;
-    const int maxWordLength = (ui->msg->width() - msgContentMargins) / msgFontMetrics.averageCharWidth(); 
+    const int msgContentWidth = ui->msg->width() - 10; // 10 is the margin/padding
+    const int maxWordLength = msgContentWidth / msgFontMetrics.averageCharWidth(); 
 
     const auto wrappableWordPattern = std::regex{ "\\S{" + std::to_string(maxWordLength) + "}" };
     auto it = std::sregex_iterator{ this->message.cbegin(), this->message.cend(), wrappableWordPattern };
@@ -802,6 +802,14 @@ void DlgExpressionInput::setMsgText()
     wrappedMsg += this->message.substr(lastPos);
 
     ui->msg->setText(QString::fromStdString(wrappedMsg));
+    
+    // elide text if it is going out of widget bounds 
+    // note: this is only 'rough elide', as this text is usually not very long;
+    const int msgLinesLimit = 3;
+    if (wrappedMsg.size() > msgContentWidth / msgFontMetrics.averageCharWidth() * msgLinesLimit) {
+        const QString elidedMsg = msgFontMetrics.elidedText(QString::fromStdString(wrappedMsg), Qt::ElideRight, msgContentWidth * msgLinesLimit);
+        ui->msg->setText(elidedMsg);
+    }
 }
 
 #include "moc_DlgExpressionInput.cpp"
