4f1502606c4726f98bf7e13f79dc60651d90ddfc
wandererfan <wandererfan@gmail.com>
2025-08-07 19:23:49 -0400

    [TD]lint clean up

--- src/Mod/TechDraw/Gui/QGIViewClip.cpp
+++ src/Mod/TechDraw/Gui/QGIViewClip.cpp
@@ -43,7 +43,9 @@
 using namespace TechDrawGui;
 using namespace TechDraw;
 
-QGIViewClip::QGIViewClip()
+QGIViewClip::QGIViewClip() :
+    m_frame(new QGCustomRect()),
+    m_cliparea(new QGCustomClip())
 {
     setHandlesChildEvents(false);
     // setHandlesChildEvents(true);
@@ -52,22 +54,23 @@ QGIViewClip::QGIViewClip()
     setFlag(QGraphicsItem::ItemIsSelectable, true);
     setFlag(QGraphicsItem::ItemIsMovable, true);
 
-    m_cliparea = new QGCustomClip();
     addToGroup(m_cliparea);
-    m_cliparea->setPos(0., 0.);
-    m_cliparea->setRect(0., 0., Rez::guiX(5.), Rez::guiX(5.));
 
-    m_frame = new QGCustomRect();
+    constexpr double DefaultSize{5};
+    m_cliparea->setPos(0., 0.);
+    m_cliparea->setRect(0., 0., Rez::guiX(DefaultSize), Rez::guiX(DefaultSize));
+
     addToGroup(m_frame);
     m_frame->setPos(0., 0.);
-    m_frame->setRect(0., 0., Rez::guiX(5.), Rez::guiX(5.));
+    m_frame->setRect(0., 0., Rez::guiX(DefaultSize), Rez::guiX(DefaultSize));
 }
 
 void QGIViewClip::updateView(bool update)
 {
     auto viewClip( dynamic_cast<TechDraw::DrawViewClip *>(getViewObject()) );
-    if (!viewClip)
+    if (!viewClip) {
         return;
+    }
 
     if (update ||
         viewClip->isTouched() ||
@@ -75,7 +78,6 @@ void QGIViewClip::updateView(bool update)
         viewClip->Width.isTouched() ||
         viewClip->ShowFrame.isTouched() ||
         viewClip->Views.isTouched() ) {
-
         draw();
     }
 
@@ -98,13 +100,14 @@ void QGIViewClip::drawClip()
 {
     auto viewClip( dynamic_cast<TechDraw::DrawViewClip *>(getViewObject()) );
 
-    if (!viewClip)
+    if (!viewClip) {
         return;
+    }
 
     prepareGeometryChange();
     double h = viewClip->Height.getValue();
     double w = viewClip->Width.getValue();
-    QRectF r = QRectF(-Rez::guiX(w)/2.0, -Rez::guiX(h)/2.0, Rez::guiX(w), Rez::guiX(h));
+    QRectF r = QRectF(-Rez::guiX(w)/2, -Rez::guiX(h)/2, Rez::guiX(w), Rez::guiX(h));
     m_frame->setRect(r);                    // (-50, -50) -> (50, 50)
     m_frame->setPos(0., 0.);
     if (viewClip->ShowFrame.getValue()) {
@@ -122,8 +125,8 @@ void QGIViewClip::drawClip()
 
     std::vector<std::string> childNames = viewClip->getChildViewNames();
     //for all child Views in Clip, add the graphics representation of the View to the Clip group
-    for(std::vector<std::string>::iterator it = childNames.begin(); it != childNames.end(); it++) {
-        QGIView* qgiv = getQGIVByName((*it));
+    for (auto& name : childNames) {
+        QGIView* qgiv = getQGIVByName((name));
         if (qgiv) {
             //TODO: why is qgiv never already in a group?
             if (qgiv->group() != m_cliparea) {
@@ -134,30 +137,23 @@ void QGIViewClip::drawClip()
                 double x = Rez::guiX(qgiv->getViewObject()->X.getValue());
                 double y = Rez::guiX(qgiv->getViewObject()->Y.getValue());
                 qgiv->setPosition(clipOrigin.x() + x, clipOrigin.y() + y);
-//                if (viewClip->ShowLabels.getValue()) {
-//                    qgiv->toggleBorder(true);
-//                } else {
-//                    qgiv->toggleBorder(false);
-//                }
                 qgiv->show();
             }
         } else {
-            Base::Console().warning("Logic error? - drawClip() - qgiv for %s not found\n", (*it).c_str());   //gview for feature !exist
+            Base::Console().warning("Logic error? - drawClip() - qgiv for %s not found\n", name.c_str());   //gview for feature !exist
         }
     }
 
     //for all graphic views in qgigroup, remove from qgigroup the ones that aren't in ViewClip
     QList<QGraphicsItem*> qgItems = m_cliparea->childItems();
-    QList<QGraphicsItem*>::iterator it = qgItems.begin();
-    for (; it != qgItems.end(); it++) {
-        QGIView* qv = dynamic_cast<QGIView*>((*it));
+    for (auto& item :  qgItems) {
+        auto* qv = dynamic_cast<QGIView*>(item);
         if (qv) {
             if (auto qvName = std::string(qv->getViewName());
                 std::ranges::find(childNames, qvName) == childNames.end()) {
                 m_cliparea->removeFromGroup(qv);
                 removeFromGroup(qv);
                 qv->isInnerView(false);
-//                qv->toggleBorder(true);
             }
         }
     }
@@ -216,4 +212,5 @@ bool QGIViewClip::forwardEventToSelection(QGIView* qview, QEvent* event) const
 
     return qview->pseudoEventFilter(qview, event);
 }
+
 
