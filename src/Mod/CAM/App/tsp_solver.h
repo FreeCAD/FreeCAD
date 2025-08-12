9b5d7d37f08c4b542c589118687c5d9469787b0c
Billy Huddleston <billy@ivdc.com>
2025-08-12 11:30:02 -0400

    Add 2-Opt TSP solver integration, GUI sorting enhancements, and migration fixes
    This update introduces a new C++ 2-Opt TSP solver with Python bindings for improved hole
    sorting performance, enhances the GUI with new sorting modes and manual reordering, and
    adds migration logic to ensure robust property
    
    src/Mod/CAM/App/CMakeLists.txt:
    * Add build and install rules for the new pybind11-based tsp_solver Python module
    
    src/Mod/CAM/App/tsp_solver.cpp:
    * Add new C++ implementation of a 2-Opt TSP solver with nearest-neighbor initialization
    
    src/Mod/CAM/App/tsp_solver.h:
    * Add TSPPoint struct and TSPSolver class with 2-Opt solve method
    
    src/Mod/CAM/App/tsp_solver_pybind.cpp:
    * Add pybind11 wrapper exposing the TSP solver to Python as tsp_solver.solve
    
    src/Mod/CAM/Gui/Resources/panels/PageBaseHoleGeometryEdit.ui:
    * Add Sorting mode combo box with "Automatic", "Automatic TSP", and "Manual" options
    * Add UI improvements for hole sorting, selection, and filtering
    
    src/Mod/CAM/Path/Op/CircularHoleBase.py:
    * Add migration logic to ensure SortingMode property exists and is up to date
    * Use sort_locations_tsp when SortingMode is "Automatic TSP"
    
    src/Mod/CAM/Path/Op/Gui/CircularHoleBase.py:
    * Add RowMoveTableWidget for manual row reordering in the GUI
    * Update TaskPanelHoleGeometryPage to support new sorting modes and improved UI behavior
    
    src/Mod/CAM/PathScripts/PathUtils.py:
    * Add sort_locations_tsp Python wrapper for the C++ TSP solver
    * Use tsp_solver.solve for TSP-based

---
+++ src/Mod/CAM/App/tsp_solver.h
@@ -0,0 +1,42 @@
+/***************************************************************************
+ *   Copyright (c) 2025 Billy Huddleston <billy@ivdc.com>                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+ *   as published by the Free Software Foundation; either version 2 of     *
+ *   the License, or (at your option) any later version.                   *
+ *   for detail see the LICENCE text file.                                 *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU Library General Public License for more details.                  *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the Free Software   *
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+ *   USA                                                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#pragma once
+#include <vector>
+#include <utility>
+#include <limits>
+#include <cmath>
+
+struct TSPPoint
+{
+    double x, y;
+    TSPPoint(double x_, double y_)
+        : x(x_)
+        , y(y_)
+    {}
+};
+
+class TSPSolver
+{
+public:
+    // Returns a vector of indices representing the visit order using 2-Opt
+    static std::vector<int> solve(const std::vector<TSPPoint>& points);
+};
