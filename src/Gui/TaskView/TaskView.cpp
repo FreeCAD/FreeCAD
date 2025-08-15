f5115572c15dbb9fea36d2c7bd8d5d52c315af46
theo-vt <theo.veilleux.trinh@proton.me>
2025-08-15 11:33:32 -0400

    Gui: Show empty taskwatcher on close taskview dialog when ShowTaskWatcher setting is set to false

--- src/Gui/TaskView/TaskView.cpp
+++ src/Gui/TaskView/TaskView.cpp
@@ -836,6 +836,7 @@ void TaskView::clearTaskWatcher()
 void TaskView::addTaskWatcher()
 {
     if (!showTaskWatcher) {
+        setShownTaskInfo(-1); // Switch to the empty taskwatcher panel
         return;
     }
     // add all widgets for all watcher to the task view
