7d641648a1b174274ff3e8875dee231f13b564f3
Céleste Wouters <foss@elementw.net>
2025-08-19 15:08:25 +0200

    Gui: fix file dialog type filters being too long (fixes #23139)

--- src/Gui/FileDialog.h
+++ src/Gui/FileDialog.h
@@ -62,11 +62,17 @@ class GuiExport FileDialog : public QFileDialog
 
 public:
     static QString getOpenFileName( QWidget * parent = nullptr, const QString & caption = QString(), const QString & dir = QString(),
+                                    const QStringList & filters = QStringList(), QString * selectedFilter = nullptr, Options options = Options() );
+    static QString getOpenFileName( QWidget * parent = nullptr, const QString & caption = QString(), const QString & dir = QString(),
                                     const QString & filter = QString(), QString * selectedFilter = nullptr, Options options = Options() );
+    static QString getSaveFileName( QWidget * parent = nullptr, const QString & caption = QString(), const QString & dir = QString(),
+                                    const QStringList & filters = QStringList(), QString * selectedFilter = nullptr, Options options = Options() );
     static QString getSaveFileName( QWidget * parent = nullptr, const QString & caption = QString(), const QString & dir = QString(),
                                     const QString & filter = QString(), QString * selectedFilter = nullptr, Options options = Options() );
     static QString getExistingDirectory( QWidget * parent = nullptr, const QString & caption = QString(), const QString & dir = QString(),
                                          Options options = ShowDirsOnly );
+    static QStringList getOpenFileNames( QWidget * parent = nullptr, const QString & caption = QString(), const QString & dir = QString(),
+                                         const QStringList & filters = QStringList(), QString * selectedFilter = nullptr, Options options = Options() );
     static QStringList getOpenFileNames( QWidget * parent = nullptr, const QString & caption = QString(), const QString & dir = QString(),
                                          const QString & filter = QString(), QString * selectedFilter = nullptr, Options options = Options() );
 
@@ -89,7 +95,6 @@ private:
     bool hasSuffix(const QString&) const;
     static QList<QUrl> fetchSidebarUrls();
     static QString workingDirectory;
-    static void getSuffixesDescription(QStringList& suffixes, const QString* suffixDescriptions);
 };
 
 // ----------------------------------------------------------------------
