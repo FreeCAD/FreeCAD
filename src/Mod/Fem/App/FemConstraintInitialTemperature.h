9eba8b1f583bb6839ab1eaf90af7210d92fb29fc
FEA-eng <59876896+FEA-eng@users.noreply.github.com>
2025-08-21 14:42:16 +0200

    FEM: Update FemConstraintInitialTemperature.h

--- src/Mod/Fem/App/FemConstraintInitialTemperature.h
+++ src/Mod/Fem/App/FemConstraintInitialTemperature.h
@@ -42,7 +42,8 @@ public:
 
     // Temperature parameters
     App::PropertyTemperature initialTemperature;
-
+    App::PropertyBool EnableFinalTemperature;
+    App::PropertyTemperature finalTemperature;
 
     /// recalculate the object
     App::DocumentObjectExecReturn* execute() override;
