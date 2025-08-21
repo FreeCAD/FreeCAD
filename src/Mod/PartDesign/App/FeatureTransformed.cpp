6b16d8b405affcc5713ed4413f917ec2d3e3e48e
FEA-eng <59876896+FEA-eng@users.noreply.github.com>
2025-08-21 23:38:07 +0200

    PartDesign: Update FeatureTransformed.cpp

--- src/Mod/PartDesign/App/FeatureTransformed.cpp
+++ src/Mod/PartDesign/App/FeatureTransformed.cpp
@@ -410,7 +410,7 @@ App::DocumentObjectExecReturn* Transformed::execute()
 
     supportShape = refineShapeIfActive((supportShape));
     if (!isSingleSolidRuleSatisfied(supportShape.getShape())) {
-        Base::Console().warning("Transformed: Result has multiple solids. Only keeping the first.\n");
+        Base::Console().warning("Transformed: Result has multiple solids. Only keeping the first. Or enable compounds in a Body.\n");
     }
 
     this->Shape.setValue(getSolid(supportShape));  // picking the first solid
@@ -440,3 +440,4 @@ TopoDS_Shape Transformed::getRemainingSolids(const TopoDS_Shape& shape)
 }
 
 }  // namespace PartDesign
+
