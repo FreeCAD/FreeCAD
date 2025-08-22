b7c233b94e71fdc3401765d523aba623018a5d67
FEA-eng <59876896+FEA-eng@users.noreply.github.com>
2025-08-22 20:29:50 +0200

    PartDesign: Update error message about multiple solids (#23286)

--- src/Mod/PartDesign/App/FeatureDraft.cpp
+++ src/Mod/PartDesign/App/FeatureDraft.cpp
@@ -288,7 +288,7 @@ App::DocumentObjectExecReturn *Draft::execute()
         }
 
         if (!isSingleSolidRuleSatisfied(shape.getShape())) {
-            return new App::DocumentObjectExecReturn(QT_TRANSLATE_NOOP("Exception", "Result has multiple solids: that is not currently supported."));
+            return new App::DocumentObjectExecReturn(QT_TRANSLATE_NOOP("Exception", "Result has multiple solids: enable 'Allow Compounds' in the active body."));
         }
 
         this->Shape.setValue(getSolid(shape));
@@ -299,3 +299,5 @@ App::DocumentObjectExecReturn *Draft::execute()
         return new App::DocumentObjectExecReturn(e.GetMessageString());
     }
 }
+
+
--- src/Mod/PartDesign/App/FeatureExtrude.cpp
+++ src/Mod/PartDesign/App/FeatureExtrude.cpp
@@ -812,7 +812,7 @@ App::DocumentObjectExecReturn* FeatureExtrude::buildExtrusion(ExtrudeOptions opt
             solRes = refineShapeIfActive(result);
 
             if (!isSingleSolidRuleSatisfied(solRes.getShape())) {
-                return new App::DocumentObjectExecReturn(QT_TRANSLATE_NOOP("Exception", "Result has multiple solids: that is not currently supported."));
+                return new App::DocumentObjectExecReturn(QT_TRANSLATE_NOOP("Exception", "Result has multiple solids: enable 'Allow Compounds' in the active body."));
             }
             this->Shape.setValue(getSolid(solRes));
         }
