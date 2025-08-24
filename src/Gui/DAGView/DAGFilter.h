e757271465a9dc79f81f4c5b246d485d631e1e89
Jiří Pinkava <j-pi@seznam.cz>
2025-08-24 09:32:03 +0200

    DAG: drop unused type parameter

--- src/Gui/DAGView/DAGFilter.h
+++ src/Gui/DAGView/DAGFilter.h
@@ -35,17 +35,11 @@ namespace Gui
     class FilterBase
     {
     public:
-      enum class Type
-      {
-        None = 0, //!< no type designation. shouldn't be used.
-        Exclusion
-      };
       FilterBase();
       //! @return is whether we have a match or not.
       virtual bool goFilter(const Vertex &vertexIn, const Graph &graphIn, const GraphLinkContainer &linkIn) const = 0;
       QString name;
       bool enabled = true;
-      Type type = Type::Exclusion;
     };
   }
 }
