8512585a41956eebe621ead7be3613a90d53149c
wwmayer <wmayer@freecad.org>
2025-08-27 16:01:54 +0200

    Measure: Fix quick measure command
    * Clear status bar when switching off quick measure
    * Activate quick measure by default
    * Remove command from toolbar as there is currently no icon

--- src/Gui/Workbench.cpp
+++ src/Gui/Workbench.cpp
@@ -813,7 +813,7 @@ ToolBarItem* StdWorkbench::setupToolBars() const
     auto view = new ToolBarItem( root );
     view->setCommand("View");
     *view << "Std_ViewFitAll" << "Std_ViewFitSelection" << "Std_ViewGroup" << "Std_AlignToSelection"
-          << "Separator" << "Std_DrawStyle" << "Std_TreeViewActions" << "Std_Measure" << "Std_QuickMeasure";
+          << "Separator" << "Std_DrawStyle" << "Std_TreeViewActions" << "Std_Measure";
 
     // Individual views
     auto individualViews = new ToolBarItem(root, ToolBarItem::DefaultVisibility::Hidden);
