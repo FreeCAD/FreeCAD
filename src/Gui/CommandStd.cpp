09128c3b691145ad536ddd16678be87861a947b9
tarman3 <joice501790018@tutamail.com>
2025-08-28 22:26:47 +0300

    Gui: Fix Recent files and Recent Macros

--- src/Gui/CommandStd.cpp
+++ src/Gui/CommandStd.cpp
@@ -169,6 +169,7 @@ Action* StdCmdRecentFiles::createAction()
     auto pcAction = new RecentFilesAction(this, getMainWindow());
     pcAction->setObjectName(QLatin1String("recentFiles"));
     pcAction->setDropDownMenu(true);
+    pcAction->setRememberLast(false);
     applyCommandData(this->className(), pcAction);
     return pcAction;
 }
@@ -212,6 +213,7 @@ Action* StdCmdRecentMacros::createAction()
     auto pcAction = new RecentMacrosAction(this, getMainWindow());
     pcAction->setObjectName(QLatin1String("recentMacros"));
     pcAction->setDropDownMenu(true);
+    pcAction->setRememberLast(false);
     applyCommandData(this->className(), pcAction);
     return pcAction;
 }
