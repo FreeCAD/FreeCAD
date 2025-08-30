11d916e895201858a21df29bd30bd6c9b36cdc43
Furgo <148809153+furgo16@users.noreply.github.com>
2025-08-30 10:31:38 +0200

    Import: DXF exporter, define STANDARD dimension style
    This fixes the issue whereby $DIMSCALE is undefined and read as 0 by
    some DXF viewers/editors

--- src/Mod/Import/App/dxf/dxf.h
+++ src/Mod/Import/App/dxf/dxf.h
@@ -280,6 +280,7 @@ private:
     std::ostringstream* m_ssBlkRecord;
     std::ostringstream* m_ssEntity;
     std::ostringstream* m_ssLayer;
+    std::ostringstream* m_ssDimstyle;
 
 protected:
     static Base::Vector3d toVector3d(const double* coordinatesXYZ)
@@ -472,6 +473,7 @@ public:
     void makeBlockRecordTableHead();
     void makeBlockRecordTableBody();
     void makeBlockSectionHead();
+    void makeDimstyleTable();
     void setColor(int aci)
     {
         m_currentColor = aci;
