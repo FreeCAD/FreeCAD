18e012d275e35b25836892dc96451c6de022ac8a
Ian 'z0r0' Abreu <z0r0@shurikenlabs.com>
2025-08-31 13:58:17 -0400

    Adding spreadsheet interfaces.

--- src/Mod/Spreadsheet/App/PropertyRowHeights.pyi
+++ src/Mod/Spreadsheet/App/PropertyRowHeights.pyi
@@ -2,17 +2,11 @@ from Base.Metadata import export
 from Base.Persistence import Persistence
 
 @export(
-    Father="PersistencePy",
-    Name="PropertyRowHeightsPy",
-    Twin="PropertyRowHeights",
-    TwinPointer="PropertyRowHeights",
     Include="Mod/Spreadsheet/App/PropertyRowHeights.h",
     Namespace="Spreadsheet",
-    FatherInclude="Base/PersistencePy.h",
-    FatherNamespace="Base",
     Constructor=True,
 )
-class PropertyRowHeightsPy(Persistence):
+class PropertyRowHeights(Persistence):
     """
     Internal spreadsheet object
     """
