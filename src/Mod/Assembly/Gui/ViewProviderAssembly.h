3ce94527718193105167c4b8f4163895b8ab253a
PaddleStroke <pierrelouis.boyer@gmail.com>
2025-09-01 23:29:06 +0200

    Assembly: Solver message taskbox. UI setup, App not implemented yet. (#23420)
    * Assembly: Solver message taskbox. UI setup, App not implemented yet.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update TaskAssemblyMessages.cpp
    
    * Update ViewProviderAssembly.cpp
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/Mod/Assembly/Gui/ViewProviderAssembly.h
+++ src/Mod/Assembly/Gui/ViewProviderAssembly.h
@@ -25,6 +25,7 @@
 #define ASSEMBLYGUI_VIEWPROVIDER_ViewProviderAssembly_H
 
 #include <QCoreApplication>
+#include <boost/signals2.hpp>
 
 #include <Mod/Assembly/AssemblyGlobal.h>
 
@@ -44,6 +45,7 @@ class View3DInventorViewer;
 
 namespace AssemblyGui
 {
+class TaskAssemblyMessages;
 
 struct MovingObject
 {
@@ -200,6 +202,8 @@ public:
 
     static Base::Vector3d getCenterOfBoundingBox(const std::vector<MovingObject>& movingObjs);
 
+    void UpdateSolverInformation();
+
     DragMode dragMode;
     bool canStartDragging;
     bool partMoving;
@@ -229,6 +233,10 @@ public:
     SoFieldSensor* translationSensor = nullptr;
     SoFieldSensor* rotationSensor = nullptr;
 
+    boost::signals2::signal<
+        void(const QString& state, const QString& msg, const QString& url, const QString& linkText)>
+        signalSetUp;
+
 private:
     bool tryMouseMove(const SbVec2s& cursorPos, Gui::View3DInventorViewer* viewer);
     void tryInitMove(const SbVec2s& cursorPos, Gui::View3DInventorViewer* viewer);
@@ -237,6 +245,9 @@ private:
                                const std::string& subNamePrefix,
                                App::DocumentObject* currentObject,
                                bool onlySolids);
+
+    TaskAssemblyMessages* taskSolver;
+    boost::signals2::connection connectSolverUpdate;
 };
 
 }  // namespace AssemblyGui
