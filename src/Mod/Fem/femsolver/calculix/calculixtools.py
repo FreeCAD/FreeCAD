3698da955b4734e554939cab9e192ee11f01f645
FEA-eng <59876896+FEA-eng@users.noreply.github.com>
2025-09-01 16:35:10 +0200

    FEM: Update calculixtools.py

--- src/Mod/Fem/femsolver/calculix/calculixtools.py
+++ src/Mod/Fem/femsolver/calculix/calculixtools.py
@@ -116,6 +116,8 @@ class CalculiXTools:
             "AnalysisNumCPUs", QThread.idealThreadCount()
         )
         env.insert("OMP_NUM_THREADS", str(num_cpu))
+        if getattr(self.obj, "DisablePastixMixedPrecision", True):
+            env.insert("PASTIX_MIXED_PRECISION","0")
         self.process.setProcessEnvironment(env)
         self.process.setWorkingDirectory(self.obj.WorkingDirectory)
 
