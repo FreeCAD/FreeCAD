b014d5290f6b568afd39a4ecedf78222eed66cd3
Roy-043 <70520633+Roy-043@users.noreply.github.com>
2025-09-03 22:51:05 +0200

    Update CommandStd.cpp

--- src/Gui/CommandStd.cpp
+++ src/Gui/CommandStd.cpp
@@ -449,7 +449,7 @@ StdCmdDlgCustomize::StdCmdDlgCustomize()
   :Command("Std_DlgCustomize")
 {
     sGroup        = "Tools";
-    sMenuText     = QT_TR_NOOP("Cu&stomize…");
+    sMenuText     = QT_TR_NOOP("Cu&stomize");
     sToolTipText  = QT_TR_NOOP("Opens a dialog to edit toolbars, shortcuts, and macros");
     sWhatsThis    = "Std_DlgCustomize";
     sStatusTip    = sToolTipText;
@@ -960,3 +960,4 @@ void CreateStdCommands()
 }
 
 } // namespace Gui
+
