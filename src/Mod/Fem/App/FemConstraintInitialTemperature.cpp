f00aa86afc2fc42626f7918f9e79553cf35798d2
FEA-eng <59876896+FEA-eng@users.noreply.github.com>
2025-09-04 05:15:25 +0200

    FEM: Amplitude support for final temperature field (#23530)
    * FEM: Update FemConstraintInitialTemperature.cpp
    
    * FEM: Update FemConstraintInitialTemperature.h
    
    * FEM: Update write_amplitude.py
    
    * FEM: Update write_constraint_finaltemperature.py

--- src/Mod/Fem/App/FemConstraintInitialTemperature.cpp
+++ src/Mod/Fem/App/FemConstraintInitialTemperature.cpp
@@ -37,6 +37,16 @@ ConstraintInitialTemperature::ConstraintInitialTemperature()
     ADD_PROPERTY(initialTemperature, (300.0));
     ADD_PROPERTY(EnableFinalTemperature, (false));
     ADD_PROPERTY(FinalTemperature, (300.0));
+    ADD_PROPERTY_TYPE(EnableAmplitude,
+                      (false),
+                      "",
+                      (App::PropertyType)(App::Prop_None),
+                      "Amplitude of the final temperature field");
+    ADD_PROPERTY_TYPE(AmplitudeValues,
+                      (std::vector<std::string> {"0, 0", "1, 1"}),
+                      "",
+                      (App::PropertyType)(App::Prop_None),
+                      "Amplitude values");
 }
 
 App::DocumentObjectExecReturn* ConstraintInitialTemperature::execute()
