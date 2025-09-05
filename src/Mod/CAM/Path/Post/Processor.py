0f1fe0e609180120a30ff628f8764b654ea0364a
tarman3 <joice501790018@tutamail.com>
2025-09-05 14:08:16 +0300

    CAM: Array - Mutli tool controller and coolant

--- src/Mod/CAM/Path/Post/Processor.py
+++ src/Mod/CAM/Path/Post/Processor.py
@@ -686,6 +686,49 @@ class PostProcessor:
                 getattr(self._job.Operations, "Group", []) if self._job is not None else []
             )
 
+        if self._operations:
+            self.processArrays()
+
+    # Prepare operations from arrays
+    def processArrays(self):
+        arrays = [
+            {"index": i, "array": op}
+            for i, op in enumerate(self._operations)
+            if getattr(op, "ArrayGroup", None) and op.Active
+        ]
+        if not arrays:
+            return
+
+        if self._job.OrderOutputBy == "Tool":
+            # place copies after base op to minimize tool changes
+            for array in reversed(arrays):
+                # remove Array object from operations list
+                self._operations.pop(array["index"])
+                for opFromArray in reversed(array["array"].ArrayGroup):
+                    for i, op in enumerate(self._operations):
+                        if op.Name == opFromArray.Base[-1]:
+                            # insert copy after base op
+                            self._operations.insert(i + 1, opFromArray)
+                            break
+                        elif (
+                            isinstance(op.Proxy, Path.Op.Gui.Array.ObjectArrayChild)
+                            and op.Base[-1] == opFromArray.Base[-1]
+                        ):
+                            # export without base operation
+                            self._operations.insert(i, opFromArray)
+                            break
+                    else:
+                        # export without base operation
+                        self._operations.insert(array["index"], opFromArray)
+        else:
+            # place copies as is
+            for array in reversed(arrays):
+                # remove Array object from operations list
+                self._operations.pop(array["index"])
+                # insert all operations from Array group
+                for opFromArray in reversed(array["array"].ArrayGroup):
+                    self._operations.insert(array["index"], opFromArray)
+
     @classmethod
     def exists(cls, processor):
         return processor in Path.Preferences.allAvailablePostProcessors()
