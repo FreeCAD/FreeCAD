1ee3992436b18a65461ed7286d55107292faa27c
marioalexis <mario.passaglia@gmail.com>
2025-09-06 17:28:50 -0300

    Fem: Fix Elmer preferences file chooser

--- src/Mod/Fem/Gui/DlgSettingsFemElmerImp.cpp
+++ src/Mod/Fem/Gui/DlgSettingsFemElmerImp.cpp
@@ -25,7 +25,7 @@
 #include "PreCompiled.h"
 #ifndef _PreComp_
 #include <QMessageBox>
-#include <QThread>
+#include <QStandardPaths>
 #endif
 
 #include "DlgSettingsFemElmerImp.h"
@@ -40,56 +40,40 @@ DlgSettingsFemElmerImp::DlgSettingsFemElmerImp(QWidget* parent)
 {
     ui->setupUi(this);
 
-    // determine number of CPU cores
-    processor_count = QThread::idealThreadCount();
-    ui->sb_elmer_num_cores->setMaximum(processor_count);
-
     connect(ui->fc_grid_binary_path,
-            &Gui::PrefFileChooser::fileNameChanged,
+            &Gui::PrefFileChooser::fileNameSelected,
             this,
-            &DlgSettingsFemElmerImp::onfileNameChanged);
+            &DlgSettingsFemElmerImp::onfileNameSelected);
     connect(ui->fc_elmer_binary_path,
-            &Gui::PrefFileChooser::fileNameChanged,
+            &Gui::PrefFileChooser::fileNameSelected,
             this,
-            &DlgSettingsFemElmerImp::onfileNameChanged);
-    connect(ui->fc_elmer_binary_path,
-            &Gui::PrefFileChooser::fileNameChanged,
-            this,
-            &DlgSettingsFemElmerImp::onfileNameChangedMT);
-    connect(ui->sb_elmer_num_cores,
-            qOverload<int>(&Gui::PrefSpinBox::valueChanged),
-            this,
-            &DlgSettingsFemElmerImp::onCoresValueChanged);
+            &DlgSettingsFemElmerImp::onfileNameSelected);
 }
 
 DlgSettingsFemElmerImp::~DlgSettingsFemElmerImp() = default;
 
 void DlgSettingsFemElmerImp::saveSettings()
 {
-    ui->cb_elmer_binary_std->onSave();
     ui->fc_elmer_binary_path->onSave();
-
-    ui->cb_grid_binary_std->onSave();
     ui->fc_grid_binary_path->onSave();
 
-    ui->sb_elmer_num_cores->onSave();
-    ui->cb_elmer_filtering->onSave();
-    ui->ckb_elmer_format->onSave();
-    ui->ckb_elmer_geom_id->onSave();
+    ui->sb_num_processes->onSave();
+
+    ui->cb_filtering->onSave();
+    ui->ckb_binary_format->onSave();
+    ui->ckb_geom_id->onSave();
 }
 
 void DlgSettingsFemElmerImp::loadSettings()
 {
-    ui->cb_elmer_binary_std->onRestore();
     ui->fc_elmer_binary_path->onRestore();
-
-    ui->cb_grid_binary_std->onRestore();
     ui->fc_grid_binary_path->onRestore();
 
-    ui->sb_elmer_num_cores->onRestore();
-    ui->cb_elmer_filtering->onRestore();
-    ui->ckb_elmer_format->onRestore();
-    ui->ckb_elmer_geom_id->onRestore();
+    ui->sb_num_processes->onRestore();
+
+    ui->cb_filtering->onRestore();
+    ui->ckb_binary_format->onRestore();
+    ui->ckb_geom_id->onRestore();
 }
 
 /**
@@ -105,55 +89,10 @@ void DlgSettingsFemElmerImp::changeEvent(QEvent* e)
     }
 }
 
-void DlgSettingsFemElmerImp::onfileNameChanged(QString FileName)
+void DlgSettingsFemElmerImp::onfileNameSelected(const QString& fileName)
 {
-    if (!QFileInfo::exists(FileName)) {
-        QMessageBox::critical(this,
-                              tr("File does not exist"),
-                              tr("The specified executable\n'%1'\n does not exist!\n"
-                                 "Specify another file.")
-                                  .arg(FileName));
-    }
-}
-
-void DlgSettingsFemElmerImp::onfileNameChangedMT(QString FileName)
-{
-    ui->sb_elmer_num_cores->setMaximum(processor_count);
-
-    if (ui->sb_elmer_num_cores->value() == 1) {
-        return;
-    }
-
-#if defined(FC_OS_WIN32)
-    // name ends with "_mpi.exe"
-    if (!FileName.endsWith(QLatin1String("_mpi.exe"))) {
-        QMessageBox::warning(this,
-                             tr("FEM Elmer: Not suitable for multithreading"),
-                             tr("Wrong Elmer setting: You use more than one CPU core.\n"
-                                "Therefore an executable with the suffix '_mpi.exe' is required."));
-        ui->sb_elmer_num_cores->setValue(1);
-        ui->sb_elmer_num_cores->setMaximum(1);
-        return;
-    }
-#elif defined(FC_OS_LINUX) || defined(FC_OS_CYGWIN) || defined(FC_OS_MACOSX) || defined(FC_OS_BSD)
-    // name ends with "_mpi"
-    if (!FileName.endsWith(QLatin1String("_mpi"))) {
-        QMessageBox::warning(this,
-                             tr("FEM Elmer: Not suitable for multithreading"),
-                             tr("Wrong Elmer setting: You use more than one CPU core.\n"
-                                "Therefore an executable with the suffix '_mpi' is required."));
-        ui->sb_elmer_num_cores->setValue(1);
-        ui->sb_elmer_num_cores->setMaximum(1);
-        return;
-    }
-#endif
-}
-
-void DlgSettingsFemElmerImp::onCoresValueChanged(int cores)
-{
-    if (cores > 1) {
-        // check if the right executable is loaded
-        onfileNameChangedMT(ui->fc_elmer_binary_path->fileName());
+    if (!fileName.isEmpty() && QStandardPaths::findExecutable(fileName).isEmpty()) {
+        QMessageBox::critical(this, tr("Elmer"), tr("Executable '%1' not found").arg(fileName));
     }
 }
 
