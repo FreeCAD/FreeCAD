7faf899b8db2d9e905be17784f67d8f104a2e725
Alfredo Monclus <alfredomonclus@gmail.com>
2025-09-05 18:16:53 -0600

    PD: fix hole thread depth being reset when changing to through all
    the property is correctly updated by the updateThreadDepthParam below

--- src/Mod/PartDesign/App/FeatureHole.cpp
+++ src/Mod/PartDesign/App/FeatureHole.cpp
@@ -1714,7 +1714,6 @@ void Hole::onChanged(const App::Property* prop)
             if (isNotDimension) {
                 // if through all, set the depth accordingly
                 Depth.setValue(getThroughAllLength());
-                ThreadDepth.setValue(getThroughAllLength());
             }
             updateThreadDepthParam();
         }
