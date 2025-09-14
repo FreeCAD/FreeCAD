b3fd31b108bc22b7691853edc7b1e2a0993fab3c
Kacper Donat <kadet1090@gmail.com>
2025-09-14 17:09:00 +0200

    Gui: Render profile preview in proper coordinate space

--- src/Mod/PartDesign/Gui/ViewProviderSketchBased.cpp
+++ src/Mod/PartDesign/Gui/ViewProviderSketchBased.cpp
@@ -33,6 +33,7 @@
 #include "StyleParameters.h"
 
 #include <Gui/Inventor/So3DAnnotation.h>
+#include <Mod/Part/App/BodyBase.h>
 
 
 using namespace PartDesignGui;
@@ -96,7 +97,12 @@ void ViewProviderSketchBased::updateProfileShape()
     }
 
     auto profileBased = getObject<PartDesign::ProfileBased>();
-    updatePreviewShape(profileBased->getTopoShapeVerifiedFace(true), pcProfileShape);
+    auto profileShape = profileBased->getTopoShapeVerifiedFace(true);
+
+    // set the correct coordinate space for the profile shape
+    profileShape.setPlacement(profileShape.getPlacement() * profileBased->Placement.getValue().inverse());
+
+    updatePreviewShape(profileShape, pcProfileShape);
 }
 
 void ViewProviderSketchBased::updateData(const App::Property* prop)
