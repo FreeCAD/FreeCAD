75a225746232c1cb83b4fa74b7c235e18b7bf96f
Ladislav Michl <ladis@triops.cz>
2025-09-16 20:07:44 +0200

    Sketcher: Avoid fmt::join to output elements
    Elements are written to the stream, so use it directly without
    fmt::join helper.

--- src/Mod/Sketcher/App/Constraint.cpp
+++ src/Mod/Sketcher/App/Constraint.cpp
@@ -192,11 +192,19 @@ void Constraint::Save(Writer& writer) const
         auto posIds = elements
             | std::views::transform([](const GeoElementId& e) { return e.posIdAsInt(); });
 
-        const std::string ids = fmt::format("{}", fmt::join(geoIds, " "));
-        const std::string positions = fmt::format("{}", fmt::join(posIds, " "));
+        auto writeOut = [&](const auto& name, const auto& ids) {
+            if (!ids.empty()) {
+                auto id = ids.begin();
+                writer.Stream() << name << "=\"" << *id;
+                while (ids.end() != ++id) {
+                    writer.Stream() << " " << *id;
+                }
+                writer.Stream() << "\" ";
+            }
+        };
 
-        writer.Stream() << "ElementIds=\"" << ids << "\" "
-                        << "ElementPositions=\"" << positions << "\" ";
+        writeOut("ElementIds", geoIds);
+        writeOut("ElementPositions", posIds);
     }
 
     writer.Stream() << "/>\n";
