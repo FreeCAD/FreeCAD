eccabd01efa5fdcd88dc3d576f45d4ad225cd325
Dimitris75 <pressto@otenet.gr>
2025-09-28 15:49:25 +0300

    misalignment
    Fix misalignment and other minor issues

--- src/Mod/CAM/Path/Op/Adaptive.py
+++ src/Mod/CAM/Path/Op/Adaptive.py
@@ -1,26 +1,26 @@
 # -*- coding: utf-8 -*-
-# *****************************************************************************
-# * Copyright (c) 2018 Kresimir Tusek <kresimir.tusek@gmail.com>               *
-# * Copyright (c) 2019-2021 Schildkroet                                       *
-# *                                                                           *
-# * This file is part of the FreeCAD CAx development system.                  *
-# *                                                                           *
-# * This library is free software; you can redistribute it and/or             *
-# * modify it under the terms of the GNU Library General Public               *
-# * License as published by the Free Software Foundation; either              *
-# * version 2 of the License, or (at your option) any later version.          *
-# *                                                                           *
-# * This library is distributed in the hope that it will be useful,           *
-# * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
-# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
-# * GNU Library General Public License for more details.                      *
-# *                                                                           *
-# * You should have received a copy of the GNU Library General Public         *
-# * License along with this library; see the file COPYING.LIB.  If not,       *
-# * write to the Software Foundation, Inc., 59 Temple Place,                  *
-# * Suite 330, Boston, MA 02111-1307, USA                                     *
-# *                                                                           *
-# *****************************************************************************
+# ***************************************************************************
+# *   Copyright (c) 2018 Kresimir Tusek <kresimir.tusek@gmail.com>          *
+# *   Copyright (c) 2019-2021 Schildkroet                                   *
+# *                                                                         *
+# *   This file is part of the FreeCAD CAx development system.              *
+# *                                                                         *
+# *   This library is free software; you can redistribute it and/or         *
+# *   modify it under the terms of the GNU Library General Public           *
+# *   License as published by the Free Software Foundation; either          *
+# *   version 2 of the License, or (at your option) any later version.      *
+# *                                                                         *
+# *   This library  is distributed in the hope that it will be useful,      *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Library General Public License for more details.                  *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with this library; see the file COPYING.LIB. If not,    *
+# *   write to the Free Software Foundation, Inc., 59 Temple Place,         *
+# *   Suite 330, Boston, MA  02111-1307, USA                                *
+# *                                                                         *
+# ***************************************************************************
 
 # NOTE: "isNull() note"
 # After performing cut operations, checking the resulting shape.isNull() will
@@ -36,7 +36,7 @@ import time
 import json
 import math
 import area
-import numpy as np  # Added for vector math
+import numpy as np
 from PySide.QtCore import QT_TRANSLATE_NOOP
 
 if FreeCAD.GuiUp:
