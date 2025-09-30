337ec0462c615cf31fe350b20c38101e25bdcdf9
Florian Foinant-Willig <flachyjoe@gmail.com>
2025-09-30 10:17:45 +0200

    Sketcher: PointOnArcRange Constraint

--- src/Mod/Sketcher/Gui/TaskSketcherConstraints.cpp
+++ src/Mod/Sketcher/Gui/TaskSketcherConstraints.cpp
@@ -373,6 +373,7 @@ public:
             case Sketcher::Block:
             case Sketcher::PointOnObject:
             case Sketcher::PointOnSegment:
+            case Sketcher::PointOnArcRange:
             case Sketcher::Parallel:
             case Sketcher::Perpendicular:
             case Sketcher::Tangent:
