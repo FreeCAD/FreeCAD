b9e0ce14544154a4f81604649bdd79124a2b6aab
wsteffe <walter.steffe.it@gmail.com>
2025-10-08 09:38:57 +0200

    Fixed a bug in src/Mod/Part/Gui/Utils.cpp

--- src/Mod/Part/Gui/Utils.cpp
+++ src/Mod/Part/Gui/Utils.cpp
@@ -1,31 +1,25 @@
-/***************************************************************************
- *   Copyright (C) 2015 Alexander Golubev (Fat-Zer) <fatzer2@gmail.com>    *
- *                                                                         *
- *   This file is part of the FreeCAD CAx development system.              *
- *                                                                         *
- *   This library is free software; you can redistribute it and/or         *
- *   modify it under the terms of the GNU Library General Public           *
- *   License as published by the Free Software Foundation; either          *
- *   version 2 of the License, or (at your option) any later version.      *
- *                                                                         *
- *   This library  is distributed in the hope that it will be useful,      *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU Library General Public License for more details.                  *
- *                                                                         *
- *   You should have received a copy of the GNU Library General Public     *
- *   License along with this library; see the file COPYING.LIB. If not,    *
- *   write to the Free Software Foundation, Inc., 59 Temple Place,         *
- *   Suite 330, Boston, MA  02111-1307, USA                                *
- *                                                                         *
+// SPDX-License-Identifier: LGPL-2.1-or-later
+/****************************************************************************
+ *                                                                          *
+ *   Copyright (c) 2025 Walter Steffè  <walter.steffe.it@gmail.com>         *
+ *                                                                          *
+ *   This file is part of FreeCAD.                                          *
+ *                                                                          *
+ *   FreeCAD is free software: you can redistribute it and/or modify it     *
+ *   under the terms of the GNU Lesser General Public License as            *
+ *   published by the Free Software Foundation, either version 2.1 of the   *
+ *   License, or (at your option) any later version.                        *
+ *                                                                          *
+ *   FreeCAD is distributed in the hope that it will be useful, but         *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU       *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU Lesser General Public       *
+ *   License along with FreeCAD. If not, see                                *
+ *   <https://www.gnu.org/licenses/>.                                       *
+ *                                                                          *
  ***************************************************************************/
-
-#include "PreCompiled.h"
-
-#ifndef _PreComp_
-#include <QMessageBox>
-#include <QString>
-#endif
 
 #include <App/GeoFeature.h>
 #include <App/Part.h>
@@ -39,10 +33,6 @@
 FC_LOG_LEVEL_INIT("PartGui",true,true)
 
 
-
-namespace
-{
-}  // namespace
 
 
 //===========================================================================
@@ -63,7 +53,7 @@ QString getAutoGroupCommandStr(bool useActiveBody)
                 PARTKEY);
         }
     }
-    if (activeObj) {
+    if (!activeObj) {
         QString activeName = QString::fromLatin1(activeObj->getNameInDocument());
         return QStringLiteral("App.ActiveDocument.getObject('%1\').addObject(obj)\n")
             .arg(activeName);
