c78b483dd5702b3179eb9fc897dffb128543c2b6
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2025-10-07 22:57:54 -0400

    Updated SPDX License Identifiers

--- src/Gui/TaskView/TaskSolverMessages.h
+++ src/Gui/TaskView/TaskSolverMessages.h
@@ -1,4 +1,4 @@
-// SPDX - License - Identifier: LGPL - 2.1 - or -later
+// SPDX-License-Identifier: LGPL-2.1-or-later
 /****************************************************************************
  *                                                                          *
  *   Copyright (c) 2011 Jürgen Riegel <juergen.riegel@web.de>              *
