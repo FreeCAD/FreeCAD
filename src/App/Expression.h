4c69d7b5e054c3c02fed19bc55f15b7ccc4cbf54
Pieter Hijma <pieterhijma@users.noreply.github.com>
2025-10-28 15:32:21 +0100

    Doc: Move doc comments in App::Expression
    Several doc comments in App/Expression.cpp are moved to header files.

--- src/App/Expression.h
+++ src/App/Expression.h
@@ -125,6 +125,14 @@ public:
 
     ~Expression() override;
 
+    /**
+     * @brief Check if the expression is touched.
+     *
+     * An expression is touched if one of the properties it references is
+     * touched.
+     *
+     * @return true if the expression is touched, false otherwise.
+     */
     virtual bool isTouched() const { return false; }
 
     Expression * eval() const;
@@ -136,6 +144,14 @@ public:
 
     Expression * copy() const;
 
+    /**
+     * @brief Get the operator priority.
+     *
+     * This is used to determine whether parentheses are needed when
+     * converting the expression to a string.
+     *
+     * @return The operator priority.
+     */
     virtual int priority() const;
 
     void getIdentifiers(std::map<App::ObjectIdentifier,bool> &) const;
@@ -162,6 +178,15 @@ public:
 
     bool adjustLinks(const std::set<App::DocumentObject*> &inList);
 
+    /**
+     * @brief Simplify the expression.
+     *
+     * In contrast to @ref eval, which evaluates the expression to a value,
+     * this function simplifies the expression by computing all constant
+     * expressions.
+     *
+     * @return The simplified expression.
+     */
     virtual Expression * simplify() const = 0;
 
     void visit(ExpressionVisitor & v);
