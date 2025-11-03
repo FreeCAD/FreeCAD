249ee11f5c3505de83033b5621ec5a4ff644b330
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2025-11-02 21:20:51 -0500

    [ Start ]: Update SPDX License Identifiers

--- src/Mod/Start/Gui/DlgStartPreferencesImp.cpp
+++ src/Mod/Start/Gui/DlgStartPreferencesImp.cpp
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
 /***************************************************************************
  *   Copyright (c) 2018 Yorik van Havre <yorik@uncreated.net>              *
  *                                                                         *
