4e6853c7c5b5f9542c3fb62c43a91bf3aa4a83c4
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2025-11-07 23:11:39 -0500

    [ TechDraw ]: Update SPDX License Identifiers

--- src/Mod/TechDraw/Gui/TaskSurfaceFinishSymbols.h
+++ src/Mod/TechDraw/Gui/TaskSurfaceFinishSymbols.h
@@ -1,4 +1,6 @@
-﻿/***************************************************************************
+﻿// SPDX-License-Identifier: LGPL-2.1-or-later
+
+/***************************************************************************
  *   Copyright (c) 2022 edi                                                *
  *                                                                         *
  *   This file is part of the FreeCAD CAx development system.              *
--- src/Mod/TechDraw/TDTest/DrawViewPartTest.py
+++ src/Mod/TechDraw/TDTest/DrawViewPartTest.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# SPDX-License-Identifier: LGPL-2.1-or-later
 
 # basic test script for TechDraw module
 # creates a page and 1 view
