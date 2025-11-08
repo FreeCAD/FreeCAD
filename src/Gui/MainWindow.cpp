ffea3da87266911e37e3a1b88911ca16edd4659b
chris <chris.r.jones.1983@gmail.com>
2025-11-07 20:24:26 -0600

    freecad issue #25069 fix issue with selecting the plot tab / window from the menu bar

--- src/Gui/MainWindow.cpp
+++ src/Gui/MainWindow.cpp
@@ -1294,7 +1294,16 @@ void MainWindow::setActiveSubWindow(QWidget* window)
     }
 
     auto view = qobject_cast<MDIView*>(mdi->widget());
-    setActiveWindow(view);
+    if (view) {
+        setActiveWindow(view);
+    }
+    else {
+        // check for non mdiview widgets ie. the plot widget
+        d->mdiArea->setActiveSubWindow(mdi);
+        if (mdi->widget()) {
+            mdi->widget()->setFocus();
+        }
+    }
 }
 
 void MainWindow::setActiveWindow(MDIView* view)
