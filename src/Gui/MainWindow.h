f26d60ddfafce63687ec69b590def882a1d1b46d
graelo <graelo@graelo.cc>
2025-11-10 22:43:28 +0100

    feat(macos): add quicklook extensions

--- src/Gui/MainWindow.h
+++ src/Gui/MainWindow.h
@@ -368,6 +368,12 @@ private Q_SLOTS:
      * \internal
      */
     void delayedStartup();
+#ifdef Q_OS_MAC
+    /**
+     * \internal
+     */
+    void registerQuickLookExtensions();
+#endif
     /**
      * \internal
      */
