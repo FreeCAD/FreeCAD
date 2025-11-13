7e810cb2b332f3326c1c82e3d371548eda808297
FEA-eng <59876896+FEA-eng@users.noreply.github.com>
2025-11-13 10:23:18 +0100

    FEM: Update ccx_disc_cyclic_symm_centrif.py

--- src/Mod/Fem/femexamples/ccx_disc_cyclic_symm_centrif.py
+++ src/Mod/Fem/femexamples/ccx_disc_cyclic_symm_centrif.py
@@ -1,25 +1,23 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
 # ***************************************************************************
-# *   Copyright (c) 2020 Sudhanshu Dubey <sudhanshu.thethunder@gmail.com>   *
-# *   Copyright (c) 2021 Bernd Hahnebach <bernd@bimstatik.org>              *
-# *   Copyright (c) 2025 Jakub Michalski <jakub.j.michalski@gmail.com>              *
+# *   Copyright (c) 2025 Jakub Michalski <jakub.j.michalski[at]gmail.com>         *
 # *                                                                         *
-# *   This file is part of the FreeCAD CAx development system.              *
+# *   This file is part of FreeCAD.                                         *
 # *                                                                         *
-# *   This program is free software; you can redistribute it and/or modify  *
-# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
-# *   as published by the Free Software Foundation; either version 2 of     *
-# *   the License, or (at your option) any later version.                   *
-# *   for detail see the LICENCE text file.                                 *
+# *   FreeCAD is free software: you can redistribute it and/or modify it    *
+# *   under the terms of the GNU Lesser General Public License as           *
+# *   published by the Free Software Foundation, either version 2.1 of the  *
+# *   License, or (at your option) any later version.                       *
 # *                                                                         *
-# *   This program is distributed in the hope that it will be useful,       *
-# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
-# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
-# *   GNU Library General Public License for more details.                  *
+# *   FreeCAD is distributed in the hope that it will be useful, but        *
+# *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      *
+# *   Lesser General Public License for more details.                       *
 # *                                                                         *
-# *   You should have received a copy of the GNU Library General Public     *
-# *   License along with this program; if not, write to the Free Software   *
-# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
-# *   USA                                                                   *
+# *   You should have received a copy of the GNU Lesser General Public      *
+# *   License along with FreeCAD. If not, see                               *
+# *   <https://www.gnu.org/licenses/>.                                      *
 # *                                                                         *
 # ***************************************************************************
 
@@ -57,7 +55,7 @@ To run the example from Python console use:
 from femexamples.ccx_disc_cyclic_symm_centrif import setup
 setup()
 
-Analytical solution - max von Mises stress 109.78 MPa = 1.0987e8
+Analytical solution - max von Mises stress 109.78 MPa = 1.0987e8 Pa
 
 """
     )
@@ -184,4 +182,5 @@ def setup(doc=None, solvertype="ccxtools"):
 
     doc.recompute()
     return doc
+
 
