84d6ea9fc8a67846a4d312b3fd77b25aaf0e5c32
Gaël Écorchard <gael.ecorchard@cvut.cz>
2025-11-17 18:15:18 +0100

    Remove functions that require Kuka files (#24984)
    (cherry picked from commit c77e1d840c3b843c4f6e08766e4af6c53a342f1a)

--- src/Mod/Robot/Gui/Resources/translations/Robot_pt-BR.ts
+++ src/Mod/Robot/Gui/Resources/translations/Robot_pt-BR.ts
@@ -110,78 +110,6 @@
     </message>
   </context>
   <context>
-    <name>CmdRobotInsertKukaIR125</name>
-    <message>
-      <location filename="../../CommandInsertRobot.cpp" line="190"/>
-      <source>Robot</source>
-      <translation>Robô</translation>
-    </message>
-    <message>
-      <location filename="../../CommandInsertRobot.cpp" line="191"/>
-      <source>Kuka IR125</source>
-      <translation>Kuka IR125</translation>
-    </message>
-    <message>
-      <location filename="../../CommandInsertRobot.cpp" line="192"/>
-      <source>Inserts a Kuka IR125 robot into the scene</source>
-      <translation type="unfinished">Inserts a Kuka IR125 robot into the scene</translation>
-    </message>
-  </context>
-  <context>
-    <name>CmdRobotInsertKukaIR16</name>
-    <message>
-      <location filename="../../CommandInsertRobot.cpp" line="95"/>
-      <source>Robot</source>
-      <translation>Robô</translation>
-    </message>
-    <message>
-      <location filename="../../CommandInsertRobot.cpp" line="96"/>
-      <source>Kuka IR16</source>
-      <translation>Kuka IR16</translation>
-    </message>
-    <message>
-      <location filename="../../CommandInsertRobot.cpp" line="97"/>
-      <source>Inserts a Kuka IR16 robot into the scene</source>
-      <translation type="unfinished">Inserts a Kuka IR16 robot into the scene</translation>
-    </message>
-  </context>
-  <context>
-    <name>CmdRobotInsertKukaIR210</name>
-    <message>
-      <location filename="../../CommandInsertRobot.cpp" line="143"/>
-      <source>Robot</source>
-      <translation>Robô</translation>
-    </message>
-    <message>
-      <location filename="../../CommandInsertRobot.cpp" line="144"/>
-      <source>Kuka IR210</source>
-      <translation>Kuka IR210</translation>
-    </message>
-    <message>
-      <location filename="../../CommandInsertRobot.cpp" line="145"/>
-      <source>Inserts a Kuka IR210 robot into the scene</source>
-      <translation type="unfinished">Inserts a Kuka IR210 robot into the scene</translation>
-    </message>
-  </context>
-  <context>
-    <name>CmdRobotInsertKukaIR500</name>
-    <message>
-      <location filename="../../CommandInsertRobot.cpp" line="44"/>
-      <source>Robot</source>
-      <translation>Robô</translation>
-    </message>
-    <message>
-      <location filename="../../CommandInsertRobot.cpp" line="45"/>
-      <source>Kuka IR500</source>
-      <translation>Kuka IR500</translation>
-    </message>
-    <message>
-      <location filename="../../CommandInsertRobot.cpp" line="46"/>
-      <source>Inserts a Kuka IR500 into the document</source>
-      <translation type="unfinished">Inserts a Kuka IR500 into the document</translation>
-    </message>
-  </context>
-  <context>
     <name>CmdRobotInsertWaypoint</name>
     <message>
       <location filename="../../CommandTrajectory.cpp" line="87"/>
@@ -483,16 +411,6 @@
       <location filename="../../ViewProviderTrajectory.cpp" line="142"/>
       <source>Modify</source>
       <translation>Modificar</translation>
-    </message>
-    <message>
-      <location filename="../../Workbench.cpp" line="71"/>
-      <source>No robot files installed</source>
-      <translation>Nenhum arquivo de robô instalado</translation>
-    </message>
-    <message>
-      <location filename="../../Workbench.cpp" line="72"/>
-      <source>Visit %1 and copy the robot VRML and CSV files to %2</source>
-      <translation type="unfinished">Visit %1 and copy the robot VRML and CSV files to %2</translation>
     </message>
   </context>
   <context>
