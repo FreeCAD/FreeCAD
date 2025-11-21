b4b15761a4ddd716f639a70415a869cf7e0aff4c
Ajinkya Dahale <AjinkyaDahale@users.noreply.github.com>
2025-11-22 04:02:59 +0530

    Sketcher: Add offset curve support in `Sketch::addGeometry()`

--- src/Mod/Sketcher/App/Sketch.h
+++ src/Mod/Sketcher/App/Sketch.h
@@ -232,6 +232,8 @@ public:
     int addArcOfParabola(const Part::GeomArcOfParabola& parabolaSegment, bool fixed = false);
     /// add a BSpline
     int addBSpline(const Part::GeomBSplineCurve& spline, bool fixed = false);
+    /// add an offset curve
+    int addOffsetCurve(const Part::GeomOffsetCurve& offc, bool fixed = false);
     //@}
 
 
@@ -547,7 +549,8 @@ public:
         ArcOfEllipse = 6,
         ArcOfHyperbola = 7,
         ArcOfParabola = 8,
-        BSpline = 9
+        BSpline = 9,
+        OffsetCurve = 10
     };
 
 private:
@@ -619,6 +622,7 @@ private:
     std::vector<GCS::ArcOfHyperbola> ArcsOfHyperbola;
     std::vector<GCS::ArcOfParabola> ArcsOfParabola;
     std::vector<GCS::BSpline> BSplines;
+    std::vector<GCS::OffsetCurve> OffsetCurves;
 
     bool isInitMove;
     bool isFine;
