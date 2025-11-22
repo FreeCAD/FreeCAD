4d99ac750e79818841fd2037d93a06a83745a49b
Ajinkya Dahale <AjinkyaDahale@users.noreply.github.com>
2025-11-22 08:31:17 +0530

    Sketcher: Add Coin conversion for offset curves
    Allows visibility in sketcher edit mode.

--- src/Mod/Sketcher/Gui/EditModeGeometryCoinConverter.h
+++ src/Mod/Sketcher/Gui/EditModeGeometryCoinConverter.h
@@ -154,6 +154,14 @@ public:
         return std::move(arcGeoIds);
     }
 
+    /**
+     * returns the GeoIds of OffsetCurve geometries
+     */
+    auto getOffsetCurveGeoIds()
+    {
+        return std::move(offsetCurveGeoIds);
+    }
+
 private:
     template<typename GeoType, PointsMode pointmode, CurveMode curvemode, AnalyseMode analysemode>
     void convert(
@@ -190,6 +198,7 @@ private:
                               // calculation.
     std::vector<int> bsplineGeoIds;
     std::vector<int> arcGeoIds;
+    std::vector<int> offsetCurveGeoIds;
 };
 
 
