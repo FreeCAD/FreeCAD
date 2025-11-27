bbb9e19437236cc79da9566e1e72feea1e0a786b
PaddleStroke <pierrelouis.boyer@gmail.com>
2025-11-27 11:58:51 +0100

    Update JointObject.py

--- src/Mod/Assembly/JointObject.py
+++ src/Mod/Assembly/JointObject.py
@@ -1907,11 +1907,15 @@ class TaskAssemblyCreateJoint(QtCore.QObject):
         ref1 = self.joint.Reference1
         ref2 = self.joint.Reference2
 
-        self.refs.append(ref1)
-        self.refs.append(ref2)
+        if UtilsAssembly.isRefValid(ref1, 2):
+            self.refs.append(ref1)
+            sub1 = UtilsAssembly.addTipNameToSub(ref1)
+            Gui.Selection.addSelection(ref1[0].Document.Name, ref1[0].Name, sub1)
 
-        Gui.Selection.addSelection(ref1[0].Document.Name, ref1[0].Name, ref1[1][0])
-        Gui.Selection.addSelection(ref2[0].Document.Name, ref2[0].Name, ref2[1][0])
+        if UtilsAssembly.isRefValid(ref2, 2):
+            self.refs.append(ref2)
+            sub2 = UtilsAssembly.addTipNameToSub(ref2)
+            Gui.Selection.addSelection(ref2[0].Document.Name, ref2[0].Name, sub2)
 
         self.jForm.angleSpinbox.setProperty("rawValue", self.joint.Angle.Value)
         self.jForm.distanceSpinbox.setProperty("rawValue", self.joint.Distance)
