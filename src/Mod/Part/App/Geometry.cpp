5fa2b8cb42e2c5ccd1b4c1f0a6fde43bd596887c
Ajinkya Dahale <AjinkyaDahale@users.noreply.github.com>
2025-11-27 13:17:22 +0530

    [WIP]Sketcher: Add offset curve and constraint support in `Sketch.cpp`

--- src/Mod/Part/App/Geometry.cpp
+++ src/Mod/Part/App/Geometry.cpp
@@ -4939,7 +4939,7 @@ Base::Vector3d GeomOffsetCurve::getEndPoint() const
     return this->value(endParam);
 }
 
-void GeomOffsetCurve::setBasis(const Handle(Geom_Curve)& newBasis)
+void GeomOffsetCurve::setBasis(const Handle(Geom_Curve) & newBasis)
 {
     Handle(Geom_OffsetCurve) myOffsetCurve = Handle(Geom_OffsetCurve)::DownCast(myCurve);
     myOffsetCurve->SetBasisCurve(newBasis);
