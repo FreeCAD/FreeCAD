0a623d1cae4b681d40cd4a38267e73c1a06b9561
Ajinkya Dahale <AjinkyaDahale@users.noreply.github.com>
2025-12-01 09:41:23 +0530

    [WIP]Sketcher: Add restricted curves in `Sketch`

--- src/Mod/Sketcher/App/Sketch.h
+++ src/Mod/Sketcher/App/Sketch.h
@@ -232,6 +232,8 @@ public:
     int addBSpline(const Part::GeomBSplineCurve& spline, bool fixed = false);
     /// add an offset curve
     int addOffsetCurve(const Part::GeomOffsetCurve& offc, bool fixed = false);
+    /// add a restricted curve
+    int addRestrictedCurve(const Part::GeomRestrictedCurve& offc, bool fixed = false);
     //@}
 
 
@@ -557,7 +559,8 @@ public:
         ArcOfHyperbola = 7,
         ArcOfParabola = 8,
         BSpline = 9,
-        OffsetCurve = 10
+        OffsetCurve = 10,
+        RestrictedCurve = 11
     };
 
 private:
@@ -631,6 +634,7 @@ private:
     std::vector<GCS::ArcOfParabola> ArcsOfParabola;
     std::vector<GCS::BSpline> BSplines;
     std::vector<GCS::OffsetCurve> OffsetCurves;
+    std::vector<GCS::RestrictedCurve> RestrictedCurves;
 
     bool isInitMove;
     bool isFine;
