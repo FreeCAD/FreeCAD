2ce664749ea4f0c0d51601d80b8d51867ea74097
Ajinkya Dahale <AjinkyaDahale@users.noreply.github.com>
2025-12-01 11:40:31 +0530

    [WIP]planegcs: Add restriction constraint
    TODO: How would this look? The fact it tells the curve is useful, but it is not
    enforcible by GCS. Best would be as a non-driving constraint.

--- src/Mod/Sketcher/App/planegcs/Constraints.h
+++ src/Mod/Sketcher/App/planegcs/Constraints.h
@@ -1402,4 +1402,21 @@ public:
     ConstraintType getTypeId() override;
 };
 
+// Restriction
+class ConstraintRestriction: public Constraint
+{
+private:
+    RestrictedCurve _offCurve;
+    double* myParam()
+    {
+        return pvec[0];
+    }
+    void ReconstructGeomPointers();  // writes pointers in pvec to the parameters of _offCurve
+    void errorgrad(double* err, double* grad, double* param) override;
+
+public:
+    ConstraintRestriction(RestrictedCurve& offc, double* offValue);
+    ConstraintType getTypeId() override;
+};
+
 }  // namespace GCS
