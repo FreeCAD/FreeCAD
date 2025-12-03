15ce7b43217368e24527656b9ff4a3637063e598
PaddleStroke <pierrelouis.boyer@gmail.com>
2025-12-03 08:39:16 +0100

    Part: revert changes to ViewProviderExt::getDetails

--- src/Mod/Part/Gui/ViewProviderExt.cpp
+++ src/Mod/Part/Gui/ViewProviderExt.cpp
@@ -609,39 +609,7 @@ SoDetail* ViewProviderPartExt::getDetail(const char* subelement) const
     std::string element = type.first;
     int index = type.second;
 
-    // 2. If standard parsing failed, try resolving as a Topological Name
-    if (index <= 0 && subelement && *subelement) {
-        // Get the underlying shape
-        const Part::TopoShape& shape = Part::Feature::getTopoShape(
-            getObject(),
-            Part::ShapeOption::ResolveLink | Part::ShapeOption::Transform
-        );
-        // Attempt to resolve the complex string to a sub-shape
-        TopoDS_Shape subShape = shape.getSubShape(subelement);
-
-        if (!subShape.IsNull()) {
-            // If found, identify what type it is and find its 1-based index
-            if (subShape.ShapeType() == TopAbs_FACE) {
-                element = "Face";
-                index = shape.findShape(subShape);
-            }
-            else if (subShape.ShapeType() == TopAbs_EDGE) {
-                element = "Edge";
-                index = shape.findShape(subShape);
-            }
-            else if (subShape.ShapeType() == TopAbs_VERTEX) {
-                element = "Vertex";
-                index = shape.findShape(subShape);
-            }
-        }
-    }
-
-    // 3. If we still don't have a valid index, return null
-    if (index <= 0) {
-        return nullptr;
-    }
-
-    // 4. Create the Coin3D Detail
+    // 2. Create the Coin3D Detail
     if (element == "Face") {
         SoFaceDetail* detail = new SoFaceDetail();
         detail->setPartIndex(index - 1);
@@ -1570,3 +1538,4 @@ void ViewProviderPartExt::handleChangedPropertyName(
         Gui::ViewProviderGeometryObject::handleChangedPropertyName(reader, TypeName, PropName);
     }
 }
+
