0803cfeab16387fd8bef978a27b2f80a616c75f4
PaddleStroke <pierrelouis.boyer@gmail.com>
2025-12-10 10:41:40 +0100

    small fix for link groups

--- src/Mod/Assembly/UtilsAssembly.py
+++ src/Mod/Assembly/UtilsAssembly.py
@@ -1259,7 +1259,7 @@ def getComponentReference(assembly, root_obj, sub_string):
         if isLinkGroup(obj):
             continue
 
-        if obj.isDerivedFrom("App::Link"):
+        if isLink(obj):
             linkedObj = obj.getLinkedObject()
             if linkedObj and not linkedObj.isDerivedFrom("App::GeoFeature"):
                 continue
