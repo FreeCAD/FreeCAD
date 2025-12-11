33ec751dd21823b9dd0eafab59e9e721b0aa04be
PaddleStroke <pierrelouis.boyer@gmail.com>
2025-12-11 14:46:47 +0100

    Update GeoFeature.cpp

--- src/App/GeoFeature.cpp
+++ src/App/GeoFeature.cpp
@@ -326,7 +326,7 @@ Base::Placement GeoFeature::getGlobalPlacement(App::DocumentObject* targetObj,
 Base::Placement GeoFeature::getGlobalPlacement(App::DocumentObject* targetObj,
                                                App::PropertyXLinkSub* prop)
 {
-    if (!targetObj || !prop) {
+    if (!prop) {
         return Base::Placement();
     }
 
@@ -354,4 +354,5 @@ Base::Placement GeoFeature::getGlobalPlacement(const DocumentObject* obj)
 
     return placementProperty->getValue();
 }
+
 
