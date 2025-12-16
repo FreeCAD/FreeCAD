c9ca3d01e321d5a1e300b5cb7d262a955671c75b
Roy-043 <70520633+Roy-043@users.noreply.github.com>
2025-12-16 18:45:31 +0100

    BIM: ifc_tools.py fix handling of IfcGridAxis
    Fix errors found after this linter warning:
    https://github.com/FreeCAD/FreeCAD/pull/26219/changes#diff-2f58fe8ffd31a6d9302296668f6280d27d696e1507b259cc42fde224ef10da0eL759
    
    (cherry picked from commit d93da36b41182a46ee8da1538f010af006368e62)

--- src/Mod/BIM/nativeifc/ifc_tools.py
+++ src/Mod/BIM/nativeifc/ifc_tools.py
@@ -757,9 +757,9 @@ def add_properties(obj, ifcfile=None, ifcentity=None, links=False, shapemode=0, 
                 obj.addProperty("App::PropertyLength", "Length", "Axis", locked=True)
             if "Text" not in obj.PropertiesList:
                 obj.addProperty("App::PropertyStringList", "Text", "Base", locked=True)
-            obj.Text = [text.Literal]
-            obj.Placement = ifc_export.get_placement(ifcentity.ObjectPlacement, ifcfile)
+            obj.Placement = axisdata[0]
             obj.Length = axisdata[1]
+            # axisdata[2] is the axis tag, it is already applied by other code
     elif ifcentity.is_a("IfcAnnotation"):
         sectionplane = ifc_export.get_sectionplane(ifcentity)
         if sectionplane:
