28200adad3b2342fd62674ab7b7b70e8f315c3fc
Furgo <148809153+furgo16@users.noreply.github.com>
2025-12-21 01:25:28 +0100

    BIM: fix lock removal of Sill property

--- src/Mod/BIM/ArchWindow.py
+++ src/Mod/BIM/ArchWindow.py
@@ -358,7 +358,7 @@ class _Window(ArchComponent.Component):
         # Sill -> SillHeight property rename migration
         if hasattr(obj, "Sill"):
             obj.SillHeight = obj.Sill
-            obj.setPropertyStatus(prop, "-LockDynamic")
+            obj.setPropertyStatus("Sill", "-LockDynamic")
             obj.removeProperty("Sill")
 
     def loads(self, state):
