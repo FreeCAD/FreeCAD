1228d320f20dac858a42c743e9fd54635c23e9dc
tarman3 <joice501790018@tutamail.com>
2025-12-21 22:40:30 +0200

    CAM: Dogbone Dressup - Minor changes

--- src/Mod/CAM/Path/Dressup/DogboneII.py
+++ src/Mod/CAM/Path/Dressup/DogboneII.py
@@ -358,7 +358,7 @@ class Proxy(object):
         closedProfilesIndex = []
         startArea = None
         endArea = None
-        for i, instr in enumerate(source):
+        for i in range(len(source)):
             if (
                 startArea is None
                 and endArea is None
@@ -376,7 +376,7 @@ class Proxy(object):
                 # end mill index of the area
                 endArea = i
 
-            if startArea and endArea:
+            if startArea is not None and endArea is not None:
                 p1 = source[startArea].positionBegin()
                 p2 = source[endArea].positionEnd()
                 if Path.Geom.pointsCoincide(p1, p2):
@@ -417,7 +417,7 @@ class Proxy(object):
             source = PathLanguage.Maneuver.FromPath(PathUtils.getPathWithPlacement(obj.Base)).instr
 
             # get indexes of outer closed profile in each multi work area
-            if hasattr(obj, "OnlyClosedProfiles") and obj.OnlyClosedProfiles:
+            if getattr(obj, "OnlyClosedProfiles", None):
                 closedProfilesIndex = self.getIndexOuterClosedProfiles(source)
             else:
                 closedProfilesIndex = None
@@ -427,19 +427,26 @@ class Proxy(object):
                 if instr.isMove():
                     thisMove = instr
                     bone = None
-                    if thisMove.isPlunge() or (
-                        closedProfilesIndex is not None and index not in closedProfilesIndex
-                    ):
+                    if thisMove.isPlunge():
+                        # plunge indicate end of the profile
                         if lastMove and moveAfterPlunge and lastMove.leadsInto(moveAfterPlunge):
+                            # first Bone skipped, so add Bone before plunge in the profile end
                             bone = self.createBone(obj, lastMove, moveAfterPlunge)
+                        # prepare for the next profile
+                        lastMove = None
+                        moveAfterPlunge = None
+                    elif closedProfilesIndex is not None and index not in closedProfilesIndex:
+                        # skip inner profiles in case 'OnlyClosedProfiles'
                         lastMove = None
                         moveAfterPlunge = None
                     else:
                         if moveAfterPlunge is None:
                             moveAfterPlunge = thisMove
                         if lastMove:
+                            # create Bone for the profile
                             bone = self.createBone(obj, lastMove, thisMove)
                         lastMove = thisMove
+
                     if bone:
                         enabled = len(bones) not in obj.BoneBlacklist
                         if enabled and not (
