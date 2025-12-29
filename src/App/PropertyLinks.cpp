4957304bb4e011bd6ae6d49fe87d977377c0d429
Yash Suthar <yashsuthar983@gmail.com>
2025-12-29 22:05:01 +0530

    Fix : added empty link check to prevent null entry in document link map (#26406)
    Signed-off-by: Yash Suthar <yashsuthar983@gmail.com>
    (cherry picked from commit ee7d84bb03eb4dce1b92401bd1d88d5607beabae)

--- src/App/PropertyLinks.cpp
+++ src/App/PropertyLinks.cpp
@@ -4550,7 +4550,7 @@ PropertyXLink::getDocumentInList(App::Document* doc)
 {
     std::map<App::Document*, std::set<App::Document*>> ret;
     for (auto& v : _DocInfoMap) {
-        if (!v.second->pcDoc || (doc && doc != v.second->pcDoc)) {
+        if (!v.second->pcDoc || (doc && doc != v.second->pcDoc) || v.second->links.empty()) {
             continue;
         }
         auto& docs = ret[v.second->pcDoc];
