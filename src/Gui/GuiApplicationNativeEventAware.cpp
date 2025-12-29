FreeCAD/FreeCAD

commit e094940f79a805afe2a49ecd213cf94008dcd347

Add support for special spaceball keys
- Add support for mapping spaceball buttons to Ctrl, Shift, Alt, and Esc keys.
- The Esc key works to exit sketch-edit mode for example but not to exit
  the "Clarify Selection" dialog.
- The special keys will loose their special-character if remapped.

Signed-off-by: Gaël Écorchard <gael@km-robotics.cz>

src/Gui/GuiApplicationNativeEventAware.cpp
@@ -21,8 +21,13 @@
  ***************************************************************************/
 
 
+#include <algorithm>
+#include <array>
+#include <cstddef>
 #include <iomanip>
+#include <qapplication.h>
 #include <sstream>
+#include <vector>
 
 #include <QMainWindow>
 #include <FCConfig.h>
@@ -44,6 +49,60 @@
 #  include "3Dconnexion/GuiNativeEventMac.h"
 # endif  // Platform switch
 #endif   // Spacemice
+
+#if defined(_USE_3DCONNEXION_SDK) || defined(SPNAV_FOUND)
+namespace {
+
+// Strings found in `src/Gui/3Dconnexion/3DConnexion.xml`.
+constexpr auto ctrlString = "CTRL";
+constexpr auto altString = "ALT";
+constexpr auto shiftString = "SHIFT";
+constexpr auto escString = "ESC";
+
+enum class SpecialKey {
+  None = 0,
+  Ctrl,
+  Alt,
+  Shift,
+  Esc
+};
+
+// @brief Pressed or released special key on Spaceball
+//
+// @Return {"Ctrl", "Alt", "Shift", "Esc"} or None if not a special
+// or if mapped to a command (what makes is unspecial).
+SpecialKey getSpecialKey(int buttonNumber) {
+  const ParameterGrp::handle group = App::GetApplication()
+      .GetUserParameter()
+      .GetGroup("BaseApp")
+      ->GetGroup("Spaceball")
+      ->GetGroup("Buttons");
+  QByteArray groupName(QVariant(buttonNumber).toByteArray());
+  if (group->HasGroup(groupName.data())) {
+      ParameterGrp::handle commandGroup = group->GetGroup(groupName.data());
+      const std::string commandName(commandGroup->GetASCII("Command"));
+      if (!commandName.empty()) {
+          return SpecialKey::None;
+      }
+      const std::string buttonName(commandGroup->GetASCII("Description"));
+      if (buttonName == escString) {
+          return SpecialKey::Esc;
+      }
+      if (buttonName == altString) {
+          return SpecialKey::Alt;
+      }
+      if (buttonName == shiftString) {
+          return SpecialKey::Shift;
+      }
+      if (buttonName == ctrlString) {
+          return SpecialKey::Ctrl;
+      }
+  }
+  return SpecialKey::None;
+}
+
+} // Anonymous namespace
+#endif
 
 Gui::GUIApplicationNativeEventAware::GUIApplicationNativeEventAware(int& argc, char* argv[])
     : QApplication(argc, argv)
@@ -100,9 +159,35 @@ bool Gui::GUIApplicationNativeEventAware::processSpaceballEvent(QObject* object,
             return true;
         }
         if (!buttonEvent->isHandled()) {
-            // make a new event and post to parent.
-            auto newEvent = new Spaceball::ButtonEvent(*buttonEvent);
-            postEvent(object->parent(), newEvent);
+            const auto specialKey = getSpecialKey(buttonEvent->buttonNumber());
+            if (specialKey == SpecialKey::Esc) {
+                /* If button number is 10, create and send a keyboard event, Esc key. */
+                auto keyEvent = QKeyEvent{
+                    buttonEvent->buttonStatus() == Spaceball::ButtonState::Pressed ? QEvent::KeyPress : QEvent::KeyRelease,
+                    Qt::Key_Escape,
+                    Qt::KeyboardModifier::NoModifier
+                };
+                QWidget *focusWidget = QApplication::focusWidget();
+                if (focusWidget) {
+                    QApplication::postEvent(focusWidget, new QKeyEvent(keyEvent));
+                }
+            }
+            else if (specialKey == SpecialKey::Alt) {
+                spaceballAltIsPressed = (buttonEvent->buttonStatus() == Spaceball::ButtonState::Pressed);
+            }
+            else if (specialKey == SpecialKey::Shift) {
+                spacebackShiftIsPressed = (buttonEvent->buttonStatus() == Spaceball::ButtonState::Pressed);
+            }
+            else if (specialKey == SpecialKey::Ctrl) {
+                spaceballCtrlIsPressed = (buttonEvent->buttonStatus() == Spaceball::ButtonState::Pressed);
+            }
+            else
+            {
+                // Other buttons.
+                // make a new event and post to parent.
+                auto newEvent = new Spaceball::ButtonEvent(*buttonEvent);
+                postEvent(object->parent(), newEvent);
+            }
         }
     }
     return true;

src/Gui/GuiApplicationNativeEventAware.h
@@ -57,12 +57,29 @@ public:
     void postMotionEvent(std::vector<int> motionDataArray);
     void postButtonEvent(int buttonNumber, int buttonPress);
 
+protected:
+    [[nodiscard]] bool isSpaceballAltPressed() const
+    {
+        return spaceballAltIsPressed;
+    }
+    [[nodiscard]] bool isSpaceballShiftPressed() const
+    {
+        return spacebackShiftIsPressed;
+    }
+    [[nodiscard]] bool isSpaceballCtrlPressed() const
+    {
+        return spaceballCtrlIsPressed;
+    }
+
 private:
     bool spaceballPresent;
     void importSettings(std::vector<int>& motionDataArray);
     float convertPrefToSensitivity(int value);
 #if defined(_USE_3DCONNEXION_SDK) || defined(SPNAV_FOUND)
     GuiNativeEvent* nativeEvent;
+    bool spaceballAltIsPressed = false;
+    bool spacebackShiftIsPressed = false;
+    bool spaceballCtrlIsPressed = false;
 #endif
 };  // end class GUIApplicationNativeEventAware
 

src/Gui/GuiApplication.cpp
@@ -99,6 +99,18 @@ bool GUIApplication::notify(QObject* receiver, QEvent* event)
             || event->type() == Spaceball::MotionEvent::MotionEventType) {
             return processSpaceballEvent(receiver, event);
         }
+        // If the event is a pointer device event, modify according to `isSpaceballCtrlPressed`.
+        if (isSpaceballCtrlPressed()) {
+            if (event->type() == QEvent::MouseMove
+                    || event->type() == QEvent::Wheel
+                    || event->type() == QEvent::MouseButtonPress
+                    || event->type() == QEvent::MouseButtonRelease) {
+                auto inputEvent = static_cast<QInputEvent*>(event);
+                Qt::KeyboardModifiers mods = inputEvent->modifiers();
+                mods |= Qt::ControlModifier;
+                inputEvent->setModifiers(mods);
+            }
+        }
         return QApplication::notify(receiver, event);
     }
     catch (const Base::SystemExitException& e) {

commit 438e4f72fce44703da6a146e6649db08765f9a8a

Fix compilation on Mac
Commit e094940f79a8 introduced a compilation error on Mac because
of declarations under a `#define` conditional.

Signed-off-by: Gaël Écorchard <gael@km-robotics.cz>

src/Gui/GuiApplicationNativeEventAware.cpp
@@ -50,7 +50,6 @@
 # endif  // Platform switch
 #endif   // Spacemice
 
-#if defined(_USE_3DCONNEXION_SDK) || defined(SPNAV_FOUND)
 namespace
 {
 
@@ -105,7 +104,6 @@ SpecialKey getSpecialKey(int buttonNumber)
 }
 
 }  // Anonymous namespace
-#endif
 
 Gui::GUIApplicationNativeEventAware::GUIApplicationNativeEventAware(int& argc, char* argv[])
     : QApplication(argc, argv)

src/Gui/GuiApplicationNativeEventAware.h
@@ -73,13 +73,13 @@ protected:
 
 private:
     bool spaceballPresent;
+    bool spaceballAltIsPressed = false;
+    bool spacebackShiftIsPressed = false;
+    bool spaceballCtrlIsPressed = false;
     void importSettings(std::vector<int>& motionDataArray);
     float convertPrefToSensitivity(long value);
 #if defined(_USE_3DCONNEXION_SDK) || defined(SPNAV_FOUND)
     GuiNativeEvent* nativeEvent;
-    bool spaceballAltIsPressed = false;
-    bool spacebackShiftIsPressed = false;
-    bool spaceballCtrlIsPressed = false;
 #endif
 };  // end class GUIApplicationNativeEventAware
 