3cb9cf72490f9e9620824217b3482a0264d393d0
tritao <joao@tritao.eu>
2026-01-05 15:09:16 +0000

    Tests: cover Tools escapeEncodeString/Filename

--- tests/src/Base/Tools.cpp
+++ tests/src/Base/Tools.cpp
@@ -113,6 +113,22 @@ TEST(BaseToolsSuite, TestEscapeQuotesFromString)
     EXPECT_EQ(Base::Tools::escapeQuotesFromString("\""), "\\\"");
     EXPECT_EQ(Base::Tools::escapeQuotesFromString("\\"), "\\");
 }
+
+TEST(BaseToolsSuite, TestEscapeEncodeString)
+{
+    EXPECT_EQ(Base::Tools::escapeEncodeString("a\\b"), "a\\\\b");
+    EXPECT_EQ(Base::Tools::escapeEncodeString("a\"b"), "a\\\"b");
+    EXPECT_EQ(Base::Tools::escapeEncodeString("a'b"), "a\\\'b");
+    EXPECT_EQ(Base::Tools::escapeEncodeString("plain"), "plain");
+}
+
+TEST(BaseToolsSuite, TestEscapeEncodeFilename)
+{
+    EXPECT_EQ(Base::Tools::escapeEncodeFilename("a\"b"), "a\\\"b");
+    EXPECT_EQ(Base::Tools::escapeEncodeFilename("a'b"), "a\\\'b");
+    EXPECT_EQ(Base::Tools::escapeEncodeFilename("plain"), "plain");
+    EXPECT_EQ(Base::Tools::escapeEncodeFilename("a\\b"), "a\\b");
+}
 TEST(BaseToolsSuite, TestGetIdentifier)
 {
     // ASCII and edge cases
