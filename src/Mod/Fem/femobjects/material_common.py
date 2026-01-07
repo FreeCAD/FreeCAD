ee777d831c56901c8f9553b70a9ce78e24cdfb25
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2026-01-07 11:23:55 -0500

    [ FEM ]: Update SPDX License Identifiers

--- src/Mod/Fem/femobjects/material_common.py
+++ src/Mod/Fem/femobjects/material_common.py
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
 # ***************************************************************************
 # *   Copyright (c) 2013 Juergen Riegel <FreeCAD@juergen-riegel.net>        *
 # *   Copyright (c) 2016 Bernd Hahnebach <bernd@bimstatik.org>              *
--- src/Mod/Fem/femviewprovider/view_element_fluid1D.py
+++ src/Mod/Fem/femviewprovider/view_element_fluid1D.py
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
 # ***************************************************************************
 # *   Copyright (c) 2016 Ofentse Kgoa <kgoaot@eskom.co.za>                  *
 # *   Copyright (c) 2018 Bernd Hahnebach <bernd@bimstatik.org>              *
