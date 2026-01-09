1008d2a876c8689af8b3362dad455e40f4e6633e
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2026-01-08 22:01:26 -0500

    [ OpenSCAD ]: Update licensing info

--- src/Mod/OpenSCAD/prototype.py
+++ src/Mod/OpenSCAD/prototype.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python
 # SPDX-License-Identifier: LGPL-2.1-or-later
+# SPDX-FileNotice: Part of the FreeCAD project.
 
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License (LGPL)
-# as published by the Free Software Foundation; either version 2 of
-# the License, or (at your option) any later version.
+################################################################################
+#                                                                              #
+#   © 2012 Sebastian Hoogen <github@sebastianhoogen.de>                        #
+#                                                                              #
+#   FreeCAD is free software: you can redistribute it and/or modify            #
+#   it under the terms of the GNU Lesser General Public License as             #
+#   published by the Free Software Foundation, either version 2.1              #
+#   of the License, or (at your option) any later version.                     #
+#                                                                              #
+#   FreeCAD is distributed in the hope that it will be useful,                 #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty                #
+#   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                    #
+#   See the GNU Lesser General Public License for more details.                #
+#                                                                              #
+#   You should have received a copy of the GNU Lesser General Public           #
+#   License along with FreeCAD. If not, see https://www.gnu.org/licenses       #
+#                                                                              #
+################################################################################
+
 
 import FreeCAD
 import math
