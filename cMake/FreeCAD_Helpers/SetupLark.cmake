2c6f9b170e1fde550478899934ebd22b67c8f990
chris jones @ipatch <chris.r.jones.1983@gmail.com>
2026-01-10 15:42:30 -0600

    cmake: bim: update cmake messages

--- cMake/FreeCAD_Helpers/SetupLark.cmake
+++ cMake/FreeCAD_Helpers/SetupLark.cmake
@@ -2,5 +2,6 @@ macro(SetupLark)
     # ------------------------------ Lark ------------------------------
 
     find_package(LARK MODULE REQUIRED)
+    message(STATUS "Found Lark: version ${LARK_VERSION}")
 
 endmacro()
