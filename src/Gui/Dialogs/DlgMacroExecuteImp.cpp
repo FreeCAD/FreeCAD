3e7ad68fda3a9b9bafb355b96063bc3151140213
Asuki Kono <asukiaaa@gmail.com>
2026-01-18 14:55:09 +0900

    Show directory for macto that has main.FCMacro

--- src/Gui/Dialogs/DlgMacroExecuteImp.cpp
+++ src/Gui/Dialogs/DlgMacroExecuteImp.cpp
@@ -193,8 +193,19 @@ QStringList DlgMacroExecuteImp::filterFiles(const QString& folder)
     QString fileFilter = ui->LineEditFind->text();         // used to search by filename
     QString searchText = ui->LineEditFindInFiles->text();  // used to search in file content
 
+    QStringList dirsUnfiltered = dir.entryList(QDir::AllDirs|QDir::NoDotAndDotDot);
+    QStringList dirsWithMain;
+    QString fileMain = "main.FCMacro";
+    for (auto dirName : dirsUnfiltered) {
+        QString dirPath = dir.filePath(dirName);
+        QDir dirMacro(dirPath, fileMain);
+        if (!dirMacro.entryList().isEmpty()) {
+            dirsWithMain.append(dirName);
+        }
+    }
+
     if (fileFilter.isEmpty() && searchText.isEmpty()) {  // skip filtering if no filters
-        return unfiltered;
+        return unfiltered + dirsWithMain;
     }
     QStringList filteredByFileName;
     if (fileFilter.isEmpty()) {
