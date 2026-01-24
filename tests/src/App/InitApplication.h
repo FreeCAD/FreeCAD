32821f1368a9d1fe4cc9ce6ee5d8c4cdb595af46
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2026-01-24 17:54:14 -0500

    [ Tests ]: Use Pragma

--- tests/src/App/InitApplication.h
+++ tests/src/App/InitApplication.h
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
-#ifndef TEST_APPLICATION_H
-#define TEST_APPLICATION_H
+#pragma once
 
 #include <App/Application.h>
 
@@ -19,5 +18,3 @@ static void initApplication()
 }
 
 }  // namespace tests
-
-#endif  // TEST_APPLICATION_H
