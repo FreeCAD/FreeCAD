6aa17d7383f20c3e2dd26bf4981a1d5ac694c736
PaddleStroke <pierrelouis.boyer@gmail.com>
2026-01-26 12:28:47 +0100

    Assembly: Fix isolate not working on sub assembly components

--- src/Mod/Assembly/Gui/ViewProviderAssembly.cpp
+++ src/Mod/Assembly/Gui/ViewProviderAssembly.cpp
@@ -1381,6 +1381,7 @@ void ViewProviderAssembly::applyIsolationRecursively(
         for (auto* child : group->Group.getValues()) {
             applyIsolationRecursively(child, isolateSet, mode, visited);
         }
+        return;
     }
     else if (auto* part = dynamic_cast<App::Part*>(current)) {
         // As App::Part currently don't have material override
@@ -1396,6 +1397,7 @@ void ViewProviderAssembly::applyIsolationRecursively(
         for (auto* child : part->Group.getValues()) {
             applyIsolationRecursively(child, isolateSet, mode, visited);
         }
+        return;
     }
 
     auto* vp = Gui::Application::Instance->getViewProvider(current);
