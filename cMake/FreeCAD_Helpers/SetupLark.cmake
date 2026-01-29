3d538beb80a3281e0485868e3cd7eaa7748a862d
Chris Hennes <chennes@pioneerlibrarysystem.org>
2026-01-29 12:36:09 +0100

    Revert "Build: cmake: fixes #26247 update cmake to work with new required dep…"
    This reverts commit 61b87e5ed041358f80cc33858b63cfdb77bc461e.

--- cMake/FreeCAD_Helpers/SetupLark.cmake
+++
@@ -1,7 +0,0 @@
-macro(SetupLark)
-    # ------------------------------ Lark ------------------------------
-
-    find_package(LARK MODULE REQUIRED)
-    message(STATUS "Found Lark: version ${LARK_VERSION}")
-
-endmacro()
