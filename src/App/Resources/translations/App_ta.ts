9228b210fd18680c0ba5b1dcf6ceb3f95adecc3f
தமிழ் நேரம் <67970539+TamilNeram@users.noreply.github.com>
2026-02-02 04:38:18 +0530

    Add files via upload

---
+++ src/App/Resources/translations/App_ta.ts
@@ -0,0 +1,71 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS>
+<context>
+    <name>LinkParams</name>
+    <message>
+        <source>Stores the last user choice of whether to apply CopyOnChange setup to all links
+that reference the same configurable object</source>
+        <translation>அனைத்து இணைப்புகளுக்கும் CopyOnChange அமைப்பைப் பயன்படுத்த வேண்டுமா என்ற கடைசி பயனர் விருப்பத்தை சேமிக்கிறது 
+அதே கட்டமைக்கக்கூடிய பொருளைக் குறிக்கிறது</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Unnamed</source>
+        <translation>பெயரில்லாதது</translation>
+    </message>
+</context>
+<context>
+    <name>App::OriginGroupExtension</name>
+    <message>
+        <source>Origin</source>
+        <translation>தோற்றம்</translation>
+    </message>
+</context>
+<context>
+    <name>Notifications</name>
+    <message>
+        <source>
+It is recommended that the user right-click the root of the document and select Mark to recompute.
+The user should then click the Refresh button in the main toolbar.
+</source>
+        <translation>
+ஆவணத்தின் மூலத்தை பயனர் வலது சொடுக்கு செய்து, மீண்டும் கணக்கிட குறி என்பதைத் தேர்ந்தெடுக்க பரிந்துரைக்கப்படுகிறது. 
+பயனர் முதன்மை கருவிப்பட்டியில் உள்ள புதுப்பி பொத்தானைக் சொடுக்கு செய்ய வேண்டும்.
+</translation>
+    </message>
+</context>
+<context>
+    <name>App::LocalCoordinateSystem</name>
+    <message>
+        <source>X-axis</source>
+        <translation>X-அச்சு</translation>
+    </message>
+    <message>
+        <source>Y-axis</source>
+        <translation>ஒய்-அச்சு</translation>
+    </message>
+    <message>
+        <source>Z-axis</source>
+        <translation>Z-அச்சு</translation>
+    </message>
+    <message>
+        <source>XY-plane</source>
+        <translation>XY-விமானம்</translation>
+    </message>
+    <message>
+        <source>XZ-plane</source>
+        <translation>XZ-விமானம்</translation>
+    </message>
+    <message>
+        <source>YZ-plane</source>
+        <translation>YZ-விமானம்</translation>
+    </message>
+    <message>
+        <source>Origin</source>
+        <translation>தோற்றம்</translation>
+    </message>
+</context>
+</TS>
