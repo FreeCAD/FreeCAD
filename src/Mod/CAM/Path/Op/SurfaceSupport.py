2b766d99ee6e2dcc725ecbe56a9ee9e7e037da8d
Dimitris75 <30848292+Dimitris75@users.noreply.github.com>
2026-02-06 01:04:48 +0200

    Add Support for taperedballnose
    Add Support for taperedballnose

--- src/Mod/CAM/Path/Op/SurfaceSupport.py
+++ src/Mod/CAM/Path/Op/SurfaceSupport.py
@@ -2687,6 +2687,7 @@ class OCL_Tool:
                 "endmill": "CylCutter",
                 "ballend": "BallCutter",
                 "bullnose": "BullCutter",
+                "taperedballnose": "BallCutter",
                 "drill": "ConeCutter",
                 "engraver": "ConeCutter",
                 "v_bit": "ConeCutter",
