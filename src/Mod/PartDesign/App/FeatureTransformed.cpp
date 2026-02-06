2ab8537c52e85f25479286b99f5b1e672c77a227
Ladislav Michl <ladis@triops.cz>
2026-02-06 14:00:50 +0100

    PartDesign: Use Base::Sequencer to abort operations

--- src/Mod/PartDesign/App/FeatureTransformed.cpp
+++ src/Mod/PartDesign/App/FeatureTransformed.cpp
@@ -37,8 +37,8 @@
 
 #include <Base/Console.h>
 #include <Base/Exception.h>
-#include <Base/ProgressIndicator.h>
 #include <Base/Reader.h>
+#include <Base/Sequencer.h>
 #include <Mod/Part/App/modelRefine.h>
 
 #include "FeatureTransformed.h"
@@ -50,14 +50,12 @@
 #include "FeaturePolarPattern.h"
 #include "FeatureSketchBased.h"
 #include "Mod/Part/App/TopoShapeOpCode.h"
-#include "Mod/Part/App/OCCTProgressIndicator.h"
 
 
 using namespace PartDesign;
 
 namespace PartDesign
 {
-using Part::OCCTProgressIndicator;
 extern bool getPDRefineModelParameter();
 
 PROPERTY_SOURCE(PartDesign::Transformed, PartDesign::FeatureRefine)
@@ -371,7 +369,7 @@ App::DocumentObjectExecReturn* Transformed::execute()
         auto transformIter = transformations.cbegin();
         transformIter++;
         for (; transformIter != transformations.end(); transformIter++) {
-            if (OCCTProgressIndicator::getAppIndicator().UserBreak()) {
+            if (Base::Sequencer().wasCanceled()) {
                 return std::vector<TopoShape>();
             }
             auto opName = Data::indexSuffix(idx++);
@@ -416,14 +414,14 @@ App::DocumentObjectExecReturn* Transformed::execute()
                 }
                 if (!fuseShape.isNull()) {
                     auto shapes = getTransformedCompShape(supportShape, fuseShape);
-                    if (OCCTProgressIndicator::getAppIndicator().UserBreak()) {
+                    if (Base::Sequencer().wasCanceled()) {
                         return new App::DocumentObjectExecReturn("User aborted");
                     }
                     supportShape.makeElementFuse(shapes);
                 }
                 if (!cutShape.isNull()) {
                     auto shapes = getTransformedCompShape(supportShape, cutShape);
-                    if (OCCTProgressIndicator::getAppIndicator().UserBreak()) {
+                    if (Base::Sequencer().wasCanceled()) {
                         return new App::DocumentObjectExecReturn("User aborted");
                     }
                     supportShape.makeElementCut(shapes);
@@ -432,7 +430,7 @@ App::DocumentObjectExecReturn* Transformed::execute()
             break;
         case Mode::WholeShape: {
             auto shapes = getTransformedCompShape(supportShape, supportShape);
-            if (OCCTProgressIndicator::getAppIndicator().UserBreak()) {
+            if (Base::Sequencer().wasCanceled()) {
                 return new App::DocumentObjectExecReturn("User aborted");
             }
             supportShape.makeElementFuse(shapes);
