14b62ddd11b122fa74dc7992abba850f7ac4ab84
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2026-02-07 14:25:05 -0500

    [ Gui ]: Relocate file notices

--- src/Gui/GraphicsViewZoom.cpp
+++ src/Gui/GraphicsViewZoom.cpp
@@ -1,8 +1,8 @@
+// SPDX-FileNotice: Part of the FreeCAD project.
+
 /***************************************************************************
  *   Copyright (c) 2015 Pavel Strakhov <ri@idzaaus.org>                    *
  *   Copyright (c) 2015 Eivind Kvedalen <eivind@kvedalen.name>             *
- *                                                                         *
- *   This file is part of the FreeCAD CAx development system.              *
  *                                                                         *
  *   This library is free software; you can redistribute it and/or         *
  *   modify it under the terms of the GNU Library General Public           *
