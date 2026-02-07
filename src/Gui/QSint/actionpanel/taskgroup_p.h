ab20870ea6d40e78ddc0ec2ad66c1e09848c1ece
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2026-02-07 16:18:32 -0500

    [ QSint ]: Relocate file notices

--- src/Gui/QSint/actionpanel/taskgroup_p.h
+++ src/Gui/QSint/actionpanel/taskgroup_p.h
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: LGPL-3.0-only
+// SPDX-FileNotice: Part of the FreeCAD project.
+
 /***************************************************************************
  *                                                                         *
  *   Copyright: https://code.google.com/p/qsint/                           *
