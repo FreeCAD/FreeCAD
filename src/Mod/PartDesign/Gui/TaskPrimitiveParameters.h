3237a215805297c6b97e6868c851b3e56a5de6b3
Captain <87000456+captain0xff@users.noreply.github.com>
2026-02-09 02:06:04 +0530

    PartDesign: add interactive gizmos for box, cylinder and sphere operations (#23700)

--- src/Mod/PartDesign/Gui/TaskPrimitiveParameters.h
+++ src/Mod/PartDesign/Gui/TaskPrimitiveParameters.h
@@ -27,6 +27,8 @@
 #define GUI_TASKVIEW_TaskPrimitiveParameters_H
 
 #include <memory>
+#include <unordered_map>
+
 #include <Gui/DocumentObserver.h>
 #include <Gui/TaskView/TaskDialog.h>
 #include <Gui/TaskView/TaskView.h>
@@ -43,7 +45,9 @@ class Property;
 namespace Gui
 {
 class ViewProvider;
-}
+class GizmoContainer;
+class LinearGizmo;
+}  // namespace Gui
 
 namespace PartDesignGui
 {
@@ -102,6 +106,8 @@ public Q_SLOTS:
     void onWedgeZ2maxChanged(double);
     void onWedgeZ2minChanged(double);
 
+    void onPlacementChanged();
+
 private:
     /** Notifies when the object is about to be removed. */
     void slotDeletedObject(const Gui::ViewProviderDocumentObject& Obj) override;
@@ -121,6 +127,14 @@ private:
     QWidget* proxy;
     std::unique_ptr<Ui_DlgPrimitives> ui;
     ViewProviderPrimitive* vp;
+
+    std::unique_ptr<Gui::GizmoContainer> gizmoContainer;
+    Gui::LinearGizmo* lengthGizmo = nullptr;
+    Gui::LinearGizmo* heightGizmo = nullptr;
+    Gui::LinearGizmo* widthGizmo = nullptr;
+    Gui::LinearGizmo* radiusGizmo = nullptr;
+    void setupGizmos();
+    void setGizmoPositions();
 };
 
 class TaskDlgPrimitiveParameters: public TaskDlgFeatureParameters
