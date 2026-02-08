94b1c905c71728510d85c5efbf00258d10647053
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2026-02-08 08:40:35 -0500

    [ Contrib ]: Relocate file notices

--- contrib/clion/pixi/pixi-default.bash
+++ contrib/clion/pixi/pixi-default.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+# SPDX-FileNotice: Part of the FreeCAD project.
 
 # Change to the directory of this script (any subdirectory in the repository should work)
 cd "$(cd "$(dirname "${BASH_SOURCE[0]:-$0}")" &> /dev/null && pwd -P)"
