b89592461ae00e99e75eac4554b37ac0f1af68ea
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2026-02-09 18:45:25 -0500

    [ Robot ]: Relocate file notices

--- src/Mod/Robot/App/kdl_cp/treeiksolverpos_online.cpp
+++ src/Mod/Robot/App/kdl_cp/treeiksolverpos_online.cpp
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
+// SPDX-FileNotice: Part of the FreeCAD project.
 
 // Copyright  (C)  2011  PAL Robotics S.L.  All rights reserved.
 // Copyright  (C)  2007-2008  Ruben Smits <ruben dot smits at mech dot kuleuven dot be>
--- src/Mod/Robot/App/kdl_cp/velocityprofile_dirac.hpp
+++ src/Mod/Robot/App/kdl_cp/velocityprofile_dirac.hpp
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
+// SPDX-FileNotice: Part of the FreeCAD project.
 
 /***************************************************************************
   tag: Peter Soetens  Fri Feb 11 15:59:12 CET 2005  velocityprofile_dirac.h
