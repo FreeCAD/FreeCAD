83a260f0c77625ac087896b66a40dec86c569c8a
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2026-02-09 17:04:36 -0500

    [ Surface ]: Relocate file notices

--- src/Mod/Surface/Gui/Blending/ViewProviderBlendCurve.h
+++ src/Mod/Surface/Gui/Blending/ViewProviderBlendCurve.h
@@ -3,8 +3,6 @@
 /***************************************************************************
  *   Copyright (c) 2022 Matteo Grellier <matteogrellier@gmail.com>         *
  *                                                                         *
- *   This file is part of the FreeCAD CAx development system.              *
- *                                                                         *
  *   This library is free software; you can redistribute it and/or         *
  *   modify it under the terms of the GNU Library General Public           *
  *   License as published by the Free Software Foundation; either          *
