45beb35796097b54f2f361409dcb31bcad2c7880
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2026-02-09 17:45:20 -0500

    [ Spreadsheet ]: Relocate file notices

--- src/Mod/Spreadsheet/App/create_regexps.sh
+++ src/Mod/Spreadsheet/App/create_regexps.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # SPDX-License-Identifier: LGPL-2.1-or-later
+# SPDX-FileNotice: Part of the FreeCAD project.
 
 # DerivedGeneralCategory.txt used below can be downloaded from
 # http://www.unicode.org/Public/UNIDATA/extracted/DerivedGeneralCategory.txt
--- src/Mod/Spreadsheet/Gui/qtcolorpicker.cpp
+++ src/Mod/Spreadsheet/Gui/qtcolorpicker.cpp
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: LGPL-2.1-only OR GPL-3.0-only
+// SPDX-FileNotice: Part of the FreeCAD project.
 
 /****************************************************************************
 **
