d358d17b49111f89a8e6d4fd3ce11741034fe43c
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2026-02-08 21:33:59 -0500

    [ Tools ]: Relocate file notices

--- src/Tools/params_utils.py
+++ src/Tools/params_utils.py
@@ -1,4 +1,5 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
+# SPDX-FileNotice: Part of the FreeCAD project.
 
 # ***************************************************************************
 # *   Copyright (c) 2022 Zheng Lei (realthunder) <realthunder.dev@gmail.com>*
