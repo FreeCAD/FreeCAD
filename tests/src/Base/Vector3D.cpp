7f6cb7b623d00b2135d812d2997840bf3a14248e
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2026-02-08 20:49:39 -0500

    [ Tests ]: Relocate file notices

--- tests/src/Base/Vector3D.cpp
+++ tests/src/Base/Vector3D.cpp
@@ -1,3 +1,5 @@
+// SPDX-FileNotice: Part of the FreeCAD project.
+
 #include <gtest/gtest.h>
 #include <Base/Vector3D.h>
 
--- tests/src/Mod/Measure/App/MeasureDistance.cpp
+++ tests/src/Mod/Measure/App/MeasureDistance.cpp
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
+// SPDX-FileNotice: Part of the FreeCAD project.
 
 #include <src/App/InitApplication.h>
 #include <App/Document.h>
