ed80741a6bc5882fc5dc0dfa3e66b797f937cf6b
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2026-02-10 15:51:20 -0500

    [ Draft ]: Relocate file notices

--- src/Mod/Draft/draftguitools/gui_edit_base_object.py
+++ src/Mod/Draft/draftguitools/gui_edit_base_object.py
@@ -1,4 +1,5 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
+# SPDX-FileNotice: Part of the FreeCAD project.
 
 # ***************************************************************************
 # *   Copyright (c) 2019, 2020 Carlo Pavan <carlopav@gmail.com>             *
