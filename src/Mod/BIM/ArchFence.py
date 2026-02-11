e55d18279abdc3efb39acf0c748d9f49b2d39381
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2026-02-10 19:43:23 -0500

    Update ArchFence.py

--- src/Mod/BIM/ArchFence.py
+++ src/Mod/BIM/ArchFence.py
@@ -5,8 +5,6 @@
 # *                                                                           *
 # *   Copyright (c) 2019 furti <daniel.furtlehner@gmx.net>                    *
 # *                                                                           *
-# *   This file is part of FreeCAD.                                           *
-# *                                                                           *
 # *   FreeCAD is free software: you can redistribute it and/or modify it      *
 # *   under the terms of the GNU Lesser General Public License as             *
 # *   published by the Free Software Foundation, either version 2.1 of the    *
