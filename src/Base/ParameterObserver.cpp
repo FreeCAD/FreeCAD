cb4fb54473a084a07544edbfff8aa10631bc320e
wwmayer <wmayer@freecad.org>
2026-02-12 17:25:04 +0100

    Base: Fix lookup problem in ParameterObserver
    Implement a hasher class for the unordered map of the ParameterObserver
    class. This is needed as otherwise the memory addresses are used that
    can lead to failures when looking up for a parameter key.

---
+++ src/Base/ParameterObserver.cpp
@@ -0,0 +1,158 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+/***************************************************************************
+ *   Copyright (c) 2025 Werner Mayer <wmayer[at]users.sourceforge.net>     *
+ *                                                                         *
+ *   This file is part of FreeCAD.                                         *
+ *                                                                         *
+ *   FreeCAD is free software: you can redistribute it and/or modify it    *
+ *   under the terms of the GNU Lesser General Public License as           *
+ *   published by the Free Software Foundation, either version 2.1 of the  *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   FreeCAD is distributed in the hope that it will be useful, but        *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with FreeCAD. If not, see                               *
+ *   <https://www.gnu.org/licenses/>.                                      *
+ *                                                                         *
+ **************************************************************************/
+
+#include "PreCompiled.h"
+#include <string_view>
+
+#include "ParameterObserver.h"
+
+using namespace Base;
+
+ParameterObserver::ParameterObserver() = default;
+
+void ParameterObserver::attachToParameter(ParameterGrp::handle parameter)
+{
+    handle = parameter;
+    handle->Attach(this);
+}
+
+void ParameterObserver::initParameters()
+{
+    for (const auto& it : parameters) {
+        this->OnChange(*handle, it.first);
+    }
+}
+
+void ParameterObserver::OnChange(Base::Subject<const char*>& subject, const char* sReason)
+{
+    std::ignore = subject;
+    if (!sReason) {
+        return;
+    }
+    auto it = parameters.find(sReason);
+    if (it == parameters.end()) {
+        return;
+    }
+    it->second.fetch(handle, sReason);
+}
+
+void ParameterObserver::addParameter(const char* key, const Object& value)
+{
+    parameters.emplace(key, value);
+}
+
+void ParameterObserver::setBoolean(const char* key, bool value)
+{
+    setValue(key, value);
+}
+
+bool ParameterObserver::getBoolean(const char* key) const
+{
+    return getValue<bool>(key);
+}
+
+bool ParameterObserver::getDefaultBoolean(const char* key) const
+{
+    return getDefault<bool>(key);
+}
+
+void ParameterObserver::setInt(const char* key, long value)
+{
+    setValue(key, value);
+}
+
+long ParameterObserver::getInt(const char* key) const
+{
+    return getValue<long>(key);
+}
+
+long ParameterObserver::getDefaultInt(const char* key) const
+{
+    return getDefault<long>(key);
+}
+
+void ParameterObserver::setUnsigned(const char* key, unsigned long value)
+{
+    setValue(key, value);
+}
+
+unsigned long ParameterObserver::getUnsigned(const char* key) const
+{
+    return getValue<unsigned long>(key);
+}
+
+unsigned long ParameterObserver::getDefaultUnsigned(const char* key) const
+{
+    return getDefault<unsigned long>(key);
+}
+
+void ParameterObserver::setFloat(const char* key, double value)
+{
+    setValue(key, value);
+}
+
+double ParameterObserver::getFloat(const char* key) const
+{
+    return getValue<double>(key);
+}
+
+double ParameterObserver::getDefaultFloat(const char* key) const
+{
+    return getDefault<double>(key);
+}
+
+void ParameterObserver::setString(const char* key, const char* value)
+{
+    setValue<std::string>(key, value);
+}
+
+std::string ParameterObserver::getString(const char* key) const
+{
+    return getValue<std::string>(key);
+}
+
+std::string ParameterObserver::getDefaultString(const char* key) const
+{
+    return getDefault<std::string>(key);
+}
+
+std::size_t ParameterObserver::Hasher::operator()(const char* s) const
+{
+    if (!s) {
+        return 0;
+    }
+
+    std::string_view view(s);
+    return boost::hash_range(view.begin(), view.end());
+}
+
+bool ParameterObserver::Hasher::operator()(const char* a, const char* b) const
+{
+    if (!a) {
+        return !b;
+    }
+    if (!b) {
+        return false;
+    }
+    return std::strcmp(a, b) == 0;
+}
