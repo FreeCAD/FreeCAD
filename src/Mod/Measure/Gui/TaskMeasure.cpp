69badacd9a7c3f50693a9014aa5e483445aacdb8
Yash Suthar <yashsuthar983@gmail.com>
2026-02-23 00:20:34 +0530

    Measure: fix measurements are saved if exit tool by opening a sketch
    Signed-off-by: Yash Suthar <yashsuthar983@gmail.com>

--- src/Mod/Measure/Gui/TaskMeasure.cpp
+++ src/Mod/Measure/Gui/TaskMeasure.cpp
@@ -591,6 +591,11 @@ bool TaskMeasure::reject()
     return false;
 }
 
+void TaskMeasure::closed()
+{
+    reject();
+}
+
 void TaskMeasure::reset()
 {
     // Reset tool state
--- src/Mod/Measure/Gui/TaskMeasure.h
+++ src/Mod/Measure/Gui/TaskMeasure.h
@@ -68,6 +68,7 @@ public:
     bool apply(bool reset);
     bool reject() override;
     void reset();
+    void closed() override;
 
     bool hasSelection();
     void clearSelection();
