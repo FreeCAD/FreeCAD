35158c549d834dda34ba510a459856fcbc7be1fb
Pieter Hijma <pieterhijma@users.noreply.github.com>
2026-02-25 11:18:21 +0100

    Adapt FreeCAD to OCCT 8
    Resolve ambiguity as a result of an API change.

--- src/Mod/Part/App/TopoShapeExpansion.cpp
+++ src/Mod/Part/App/TopoShapeExpansion.cpp
@@ -4981,9 +4981,9 @@ TopoShape& TopoShape::makeElementBSplineFace(
         auto e4 = mk4.Edge();
 
         ShapeMapper mapper;
-        mapper.populate(MappingStatus::Modified, e, {e1, e2, e3, e4});
-        mapper.populate(MappingStatus::Generated, v, {TopExp::FirstVertex(e1)});
-        mapper.populate(MappingStatus::Generated, v, {TopExp::LastVertex(e4)});
+        mapper.populate(MappingStatus::Modified, e, std::vector<Part::TopoShape>{e1, e2, e3, e4});
+        mapper.populate(MappingStatus::Generated, v, std::vector<Part::TopoShape>{TopExp::FirstVertex(e1)});
+        mapper.populate(MappingStatus::Generated, v, std::vector<Part::TopoShape>{TopExp::LastVertex(e4)});
 
         BRep_Builder builder;
         TopoDS_Compound comp;
