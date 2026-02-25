91f07e6a7a630e6030b0507293616180327d22de
tarman3 <joice501790018@tutamail.com>
2026-02-25 13:43:17 +0200

    CAM: CircularHoleBase - Show position in Task panel

--- src/Mod/CAM/Path/Op/Gui/CircularHoleBase.py
+++ src/Mod/CAM/Path/Op/Gui/CircularHoleBase.py
@@ -28,6 +28,7 @@ import Path
 import Path.Op.Gui.Base as PathOpGui
 import PathGui
 
+from Path.Base import Drillable
 from Path.Base.Gui.Util import NaturalSortTableWidgetItem
 from PySide import QtCore, QtGui
 
@@ -49,6 +50,9 @@ else:
 COL_ORDER = 0
 COL_FEATURE = 1
 COL_DIAMETER = 2
+COL_X = 3
+COL_Y = 4
+COL_BLIND = 5
 
 
 class RowMoveTableWidget(QtGui.QTableWidget):
@@ -247,14 +251,50 @@ class TaskPanelHoleGeometryPage(PathOpGui.TaskPanelBaseGeometryPage):
                 item.setFlags(item.flags() & ~QtCore.Qt.ItemIsDropEnabled)
                 self.form.baseList.setItem(self.form.baseList.rowCount() - 1, COL_FEATURE, item)
 
-                dia = obj.Proxy.holeDiameter(base, sub)
-                diameterQty = FreeCAD.Units.Quantity(dia, FreeCAD.Units.Length)
-                displayString = diameterQty.getUserPreferred("Length")[0]
-                item = NaturalSortTableWidgetItem(displayString)
+                pos = obj.Proxy.holePosition(base, sub)
+                if pos:
+                    dia = obj.Proxy.holeDiameter(base, sub)
+                    diameterQty = FreeCAD.Units.Quantity(dia, FreeCAD.Units.Length)
+                    diaString = diameterQty.getUserPreferred("Length")[0]
+                    prec = int(diameterQty.Format["Precision"] / 2)
+                    posXString = f"{pos[0]:.{prec}f}"
+                    posYString = f"{pos[1]:.{prec}f}"
+                else:
+                    diaString = "---"
+                    posXString = "---"
+                    posYString = "---"
+
+                blind = Drillable.isBlind(base, sub)
+                if blind is None:
+                    blindString = "---"
+                elif blind:
+                    blindString = "Yes"
+                else:
+                    blindString = "No"
+
+                item = NaturalSortTableWidgetItem(diaString)
                 item.setTextAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
                 item.setFlags(item.flags() & ~QtCore.Qt.ItemIsEditable)
                 item.setFlags(item.flags() & ~QtCore.Qt.ItemIsDropEnabled)
                 self.form.baseList.setItem(self.form.baseList.rowCount() - 1, COL_DIAMETER, item)
+
+                item = NaturalSortTableWidgetItem(posXString)
+                item.setTextAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
+                item.setFlags(item.flags() & ~QtCore.Qt.ItemIsEditable)
+                item.setFlags(item.flags() & ~QtCore.Qt.ItemIsDropEnabled)
+                self.form.baseList.setItem(self.form.baseList.rowCount() - 1, COL_X, item)
+
+                item = NaturalSortTableWidgetItem(posYString)
+                item.setTextAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
+                item.setFlags(item.flags() & ~QtCore.Qt.ItemIsEditable)
+                item.setFlags(item.flags() & ~QtCore.Qt.ItemIsDropEnabled)
+                self.form.baseList.setItem(self.form.baseList.rowCount() - 1, COL_Y, item)
+
+                item = NaturalSortTableWidgetItem(blindString)
+                item.setTextAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
+                item.setFlags(item.flags() & ~QtCore.Qt.ItemIsEditable)
+                item.setFlags(item.flags() & ~QtCore.Qt.ItemIsDropEnabled)
+                self.form.baseList.setItem(self.form.baseList.rowCount() - 1, COL_BLIND, item)
 
         # Ensure the combo box reflects the current SortingMode
         idx = self.form.sortMode.findText(obj.SortingMode)
@@ -273,6 +313,9 @@ class TaskPanelHoleGeometryPage(PathOpGui.TaskPanelBaseGeometryPage):
         header.setSectionResizeMode(COL_ORDER, QtGui.QHeaderView.ResizeToContents)
         header.setSectionResizeMode(COL_FEATURE, QtGui.QHeaderView.ResizeToContents)
         header.setSectionResizeMode(COL_DIAMETER, QtGui.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(COL_X, QtGui.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(COL_Y, QtGui.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(COL_BLIND, QtGui.QHeaderView.ResizeToContents)
 
         self.form.baseList.resizeColumnToContents(0)
         self.form.baseList.blockSignals(False)
@@ -348,6 +391,17 @@ class TaskPanelHoleGeometryPage(PathOpGui.TaskPanelBaseGeometryPage):
                 disabled.append(item.data(self.DataFeatureName))
         self.obj.Disabled = disabled
         FreeCAD.ActiveDocument.recompute()
+
+    def updateChecked(self):
+        self.updating = True
+        for i in range(self.form.baseList.rowCount()):
+            item = self.form.baseList.item(i, COL_FEATURE)
+            base = item.data(self.DataObject)
+            sub = str(item.data(self.DataObjectSub))
+            state = item.checkState() == QtCore.Qt.Checked
+            if not self.obj.Proxy.isHoleEnabled(self.obj, base, sub) and state:
+                item.setCheckState(QtCore.Qt.Unchecked)
+        self.updating = False
 
     def updateOrderNumbers(self):
         """Update the order numbers in the first column after row reordering or sorting."""
@@ -441,7 +495,8 @@ class TaskPanelHoleGeometryPage(PathOpGui.TaskPanelBaseGeometryPage):
         self.obj.Base = []
         self.obj.Disabled = []
         self.form.baseList.horizontalHeader().setSortIndicator(-1, QtCore.Qt.AscendingOrder)
-        self.obj.Proxy.findAllHoles(self.obj)
+        selectionEx = FreeCADGui.Selection.getSelectionEx()
+        self.obj.Proxy.findAllHoles(self.obj, selectionEx)
         self.obj.Proxy.execute(self.obj)
         FreeCAD.ActiveDocument.recompute()
 
@@ -479,6 +534,9 @@ class TaskPanelHoleGeometryPage(PathOpGui.TaskPanelBaseGeometryPage):
                 order_item = self.form.baseList.item(row, COL_ORDER)
                 feature_item = self.form.baseList.item(row, COL_FEATURE)
                 diameter_item = self.form.baseList.item(row, COL_DIAMETER)
+                posX_item = self.form.baseList.item(row, COL_X)
+                posY_item = self.form.baseList.item(row, COL_Y)
+                blind_item = self.form.baseList.item(row, COL_BLIND)
                 try:
                     order = int(order_item.text())
                 except (ValueError, AttributeError):
@@ -490,6 +548,9 @@ class TaskPanelHoleGeometryPage(PathOpGui.TaskPanelBaseGeometryPage):
                             order_item.clone() if order_item else None,
                             feature_item.clone() if feature_item else None,
                             diameter_item.clone() if diameter_item else None,
+                            posX_item.clone() if posX_item else None,
+                            posY_item.clone() if posY_item else None,
+                            blind_item.clone() if blind_item else None,
                         ],
                     )
                 )
@@ -512,7 +573,7 @@ class TaskPanelHoleGeometryPage(PathOpGui.TaskPanelBaseGeometryPage):
 
     def itemChanged(self, item):
         """itemChanged(item) ... callback when any item in the table changes"""
-        if item.column() == COL_FEATURE:
+        if not self.updating and item.column() == COL_FEATURE:
             self.checkedChanged()
 
 
@@ -522,6 +583,14 @@ class TaskPanelOpPage(PathOpGui.TaskPanelPage):
     def taskPanelBaseGeometryPage(self, obj, features):
         """taskPanelBaseGeometryPage(obj, features) ... Return circular hole specific page controller for Base Geometry."""
         return TaskPanelHoleGeometryPage(obj, features)
+
+    def pageUpdateData(self, obj, prop):
+        if prop == "Disabled" and getattr(self, "parent", None):
+            for page in self.parent.featurePages:
+                if isinstance(page, TaskPanelHoleGeometryPage):
+                    page.updateChecked()
+
+        self.updateData(obj, prop)
 
 
 class LineEditEventFilter(QtCore.QObject):
