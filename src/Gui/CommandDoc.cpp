9f17b3bb8822a263cc9df11831f0a2900c8c43e2
PaddleStroke <pierrelouis.boyer@gmail.com>
2026-03-04 17:23:28 +0100

    Prevent crash on transform of a link

--- src/Gui/CommandDoc.cpp
+++ src/Gui/CommandDoc.cpp
@@ -1776,7 +1776,9 @@ void StdCmdTransformManip::activated(int iMsg)
         getActiveGuiDocument()->resetEdit();
     }
     std::vector<App::DocumentObject*> sel = Gui::Selection().getObjectsOfType(
-        App::GeoFeature::getClassTypeId()
+        App::GeoFeature::getClassTypeId(),
+        nullptr,
+        ResolveMode::FollowLink
     );
     Gui::ViewProvider* vp = Application::Instance->getViewProvider(sel.front());
     // FIXME: Need a way to force 'Transform' edit mode
