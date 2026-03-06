FreeCAD/FreeCAD

commit ef794c31bd85cd2d5a11df47b8a07e93e8982be3

CAM: Adjusted Fanuc post processing script to not inherit behaviour between calls
Reset line number when using --line-numbers.  Restore all default values when a
command line argument is not used.

This helps the test scripts ensure that the arguments passed during one test is the only
one taking effect during this test.

src/Mod/CAM/Path/Post/scripts/fanuc_post.py
@@ -60,7 +60,7 @@ parser.add_argument(
     action="store_true",
     help="don't pop up editor before writing output",
 )
-parser.add_argument("--precision", default="3", help="number of digits of precision, default=3")
+parser.add_argument("--precision", help="number of digits of precision, default=3 (mm) or 4 (in)")
 parser.add_argument(
     "--preamble",
     help='set commands to be issued before the first command, default="G17 G54 G40 G49 G80 G90\\n"',
@@ -116,11 +116,11 @@ PRECISION = 3
 tapSpeed = 0
 
 # Preamble text will appear at the beginning of the GCODE output file.
-PREAMBLE = """G17 G54 G40 G49 G80 G90
+DEFAULT_PREAMBLE = """G17 G54 G40 G49 G80 G90
 """
 
 # Postamble text will appear following the last operation.
-POSTAMBLE = """M05
+DEFAULT_POSTAMBLE = """M05
 G17 G54 G90 G80 G40
 M6 T0
 M2
@@ -150,34 +150,60 @@ def processArguments(argstring):
     global MODAL
     global USE_TLO
     global OUTPUT_DOUBLES
+    global LINENR
 
     try:
         args = parser.parse_args(shlex.split(argstring))
         if args.no_header:
             OUTPUT_HEADER = False
+        else:
+            OUTPUT_HEADER = True
         if args.no_comments:
             OUTPUT_COMMENTS = False
+        else:
+            OUTPUT_COMMENTS = True
         if args.line_numbers:
             OUTPUT_LINE_NUMBERS = True
+            LINENR = 100
+        else:
+            OUTPUT_LINE_NUMBERS = False
         if args.no_show_editor:
             SHOW_EDITOR = False
+        else:
+            SHOW_EDITOR = True
         print("Show editor = %s" % SHOW_EDITOR)
-        PRECISION = args.precision
         if args.preamble is not None:
             PREAMBLE = args.preamble.replace("\\n", "\n")
+        else:
+            PREAMBLE = DEFAULT_PREAMBLE
         if args.postamble is not None:
             POSTAMBLE = args.postamble.replace("\\n", "\n")
+        else:
+            POSTAMBLE = DEFAULT_POSTAMBLE
         if args.inches:
             UNITS = "G20"
             UNIT_SPEED_FORMAT = "in/min"
             UNIT_FORMAT = "in"
             PRECISION = 4
+        else:
+            UNITS = "G21"
+            UNIT_SPEED_FORMAT = "mm/min"
+            UNIT_FORMAT = "mm"
+            PRECISION = 3
+        if args.precision:
+            PRECISION = int(args.precision)
         if args.no_modal:
             MODAL = False
+        else:
+            MODAL = True
         if args.no_tlo:
             USE_TLO = False
+        else:
+            USE_TLO = True
         if args.no_axis_modal:
             OUTPUT_DOUBLES = True
+        else:
+            OUTPUT_DOUBLES = False
 
     except Exception:
         return False

commit 4c6eed1e63347dec8c7198f65b07b662d4da8236

CAM: Improved Fanuc support (header, python warnings).
Adjusted G code output to include FreeCAD body and job information.
The first comment in the G code is shown on the machine controller,
and should contain useful information for operators to to identify
jobs.  Fetch the body and job label using findParentJob and insert it
into the G code.  For some reason the body and job information is not
available when called from TestFanucPost.py, so handle case where it
is undefined as earlier.  This new use of findParentJob exposes error
in mock code used in code tests.  This is fixed in a different pull
request to make this patch easily backportable to the 1.1 branch.

Fixed issues with Fanuc post processor discovered by lint.  Made sure
global variables used are declared.  This fixes issue updating the
postamble and preamble introduced in
ef794c31bd85cd2d5a11df47b8a07e93e8982be3

Added docstrings, adjusted import statements and wrapped long line to
keep linter happy.

Reformatted code with black for consistent formatting.

Also reverted obsolete setText() workaround from commit
9c78ced00c67a6c2554d8fbfbeb84282a90d3363 now
that https://github.com/FreeCAD/FreeCAD/pull/26008 is merged.

src/Mod/CAM/Path/Post/scripts/fanuc_post.py
@@ -1,5 +1,11 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
+"""
+
+CAM post processor for CNC machines with a Fanuc controller.
+
+"""
+
 # ***************************************************************************
 # *   Copyright (c) 2014 sliptonic <shopinthewoods@gmail.com>               *
 # *   Copyright (c) 2021 shadowbane1000 <tyler@colberts.us>                 *
@@ -33,7 +39,7 @@ import shlex
 import os.path
 import Path.Base.Util as PathUtil
 import Path.Post.Utils as PostUtils
-import PathScripts.PathUtils as PathUtils
+from PathScripts import PathUtils
 from builtins import open as pyopen
 
 TOOLTIP = """
@@ -79,7 +85,8 @@ parser.add_argument(
 )
 parser.add_argument(
     "--postamble",
-    help='set commands to be issued after the last command, default="'
+    help="set commands to be issued after the last command, "
+    + 'default="'
     + DEFAULT_POSTAMBLE.replace("\n", "\\n")
     + '"',
 )
@@ -155,8 +162,17 @@ TOOL_CHANGE = """G28 G91 Z0
 DRILL_OPERATION = ("G73", "G81", "G82", "G83", "G85")
 DRILL_PARAM_REQ = ("L", "P", "Q", "R", "Z")
 
+# The settings shared between methods
+PREAMBLE = None
+POSTAMBLE = None
+
 
 def processArguments(argstring):
+    """
+    Apply default values and command line arguments before
+    processing commands.
+
+    """
     global OUTPUT_HEADER
     global OUTPUT_COMMENTS
     global OUTPUT_LINE_NUMBERS
@@ -265,9 +281,16 @@ def export(objectslist, filename, argstring):
         major = int(FreeCAD.ConfigGet("BuildVersionMajor"))
         minor = int(FreeCAD.ConfigGet("BuildVersionMinor"))
 
-        # the filename variable always contain "-", so unable to
-        # provide more accurate information.
-        gcode += "(" + "FREECAD-FILENAME-GOES-HERE" + ", " + "JOB-NAME-GOES-HERE" + ")\n"
+        # the filename variable always contain "-", use more relevant
+        # information
+        job = PathUtils.findParentJob(objectslist[0])
+        if job:
+            body, job = job.FullName.split("#")
+        else:
+            # Workaround for the TestFanucPost code, where there is no
+            # job returned by findParentJob
+            body, job = ("FREECAD-FILENAME-GOES-HERE", "JOB-NAME-GOES-HERE")
+        gcode += "(" + body.upper() + ", " + job.upper() + ")\n"
         gcode += (
             linenumber() + "(POST PROCESSOR: FANUC USING FREECAD %d.%d" % (major, minor) + ")\n"
         )
@@ -365,13 +388,7 @@ def export(objectslist, filename, argstring):
 
     if FreeCAD.GuiUp and SHOW_EDITOR:
         dia = PostUtils.GCodeEditorDialog()
-
-        # Workaround for 1.1 while we wait for
-        # https://github.com/FreeCAD/FreeCAD/pull/26008 to be merged.
-        if hasattr(dia.editor, "setPlainText"):
-            dia.editor.setPlainText(gcode)
-        else:
-            dia.editor.setText(gcode)
+        dia.editor.setText(gcode)
         result = dia.exec_()
         if result:
             final = dia.editor.toPlainText()