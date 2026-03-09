ab7eab8a158ab5ef228cc4ba97323be8f613eea9
PaddleStroke <pierrelouis.boyer@gmail.com>
2026-03-09 15:49:27 +0100

    Gui: Fix shortcut warning

--- src/Gui/TaskView/TaskView.cpp
+++ src/Gui/TaskView/TaskView.cpp
@@ -717,7 +717,6 @@ void TaskView::removeDialog()
     tryRestoreWidth();
     triggerMinimumSizeHint();
 
-    clearFocus();
     OverlayManager::instance()->refresh();
 }
 void TaskView::setShowTaskWatcher(bool show)
@@ -997,3 +996,4 @@ void TaskView::removeContextualPanel(QWidget* panel)
 }
 
 #include "moc_TaskView.cpp"
+
