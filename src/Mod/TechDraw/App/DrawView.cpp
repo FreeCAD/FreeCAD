86f18c41e67a5ac0d2020259f9e7bc21874dcfff
André Lopes <andre.carvalho.lopes@tecnico.ulisboa.pt>
2026-03-11 16:59:35 +0000

    Fix #24079: clarify TechDraw Scale tooltip
    The Scale property tooltip suggested entering fractions (e.g. 1:100 as
    =1/100), but the property editor expects a decimal value.
    
    Clarify that fractions should be entered via an expression (e.g. =1/10).

--- src/Mod/TechDraw/App/DrawView.cpp
+++ src/Mod/TechDraw/App/DrawView.cpp
@@ -99,7 +99,7 @@ DrawView::DrawView():
 
     ScaleType.setEnums(ScaleTypeEnums);
     ADD_PROPERTY_TYPE(ScaleType, (prefScaleType()), group, App::Prop_Output, "Scale Type");
-    ADD_PROPERTY_TYPE(Scale, (prefScale()), group, App::Prop_None, "Scale factor of the view. Scale factors like 1:100 can be written as =1/100");
+    ADD_PROPERTY_TYPE(Scale, (prefScale()), group, App::Prop_None, "Scale factor of the view (decimal value). For fractions, use an expression (e.g. =1/10).");
     Scale.setConstraints(&scaleRange);
 
     ADD_PROPERTY_TYPE(Caption, (""), group, App::Prop_Output, "Short text about the view");
