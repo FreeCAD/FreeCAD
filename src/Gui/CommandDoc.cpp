d4444de61abb3272343ec7767eeb68a161fd2999
Parag Debnath <paragforwork@gmail.com>
2026-03-12 21:14:14 +0530

    Gui: Added improved and accurate shortcuts

--- src/Gui/CommandDoc.cpp
+++ src/Gui/CommandDoc.cpp
@@ -2012,7 +2012,7 @@ StdCmdTransformManip::StdCmdTransformManip()
     sStatusTip = sToolTipText;
     sWhatsThis = "Std_TransformManip";
     sPixmap = "Std_TransformManip";
-    sAccel = "Shift+T";
+    sAccel = "Ctrl+Alt+T";
 }
 
 void StdCmdTransformManip::activated(int iMsg)
