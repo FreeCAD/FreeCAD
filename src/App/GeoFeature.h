fc2e1328c31ff2185adf99ae7050480caa385d07
andershol <andershol@users.noreply.github.com>
2026-03-29 19:31:20 +0200

    Remove ComplexGeoData.h from GeoFeature.h

--- src/App/GeoFeature.h
+++ src/App/GeoFeature.h
@@ -29,7 +29,13 @@
 #include "PropertyGeo.h"
 #include "MappedElement.h"
 #include "Material.h"
-#include "ComplexGeoData.h"
+#include <Base/Bitmask.h>
+
+namespace Data
+{
+enum class SearchOption : int;
+typedef Base::Flags<SearchOption> SearchOptions;
+}  // namespace Data
 
 namespace App
 {
@@ -183,7 +189,7 @@ public:
      */
     virtual const std::vector<std::string>&
     searchElementCache(const std::string& element,
-                       Data::SearchOptions options = Data::SearchOption::CheckGeometry,
+                       Data::SearchOptions options,
                        double tol = 1e-7,
                        double atol = 1e-10) const;
 
