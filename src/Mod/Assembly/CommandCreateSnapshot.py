f6ab1a1477699f3b13696b9dec1d80039eb9654e
PaddleStroke <pierrelouis.boyer@gmail.com>
2026-04-14 15:00:22 +0200

    Update CommandCreateSnapshot.py

--- src/Mod/Assembly/CommandCreateSnapshot.py
+++ src/Mod/Assembly/CommandCreateSnapshot.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 # /**************************************************************************
 #                                                                           *
-#    Copyright (c) 2024 Ondsel <development@ondsel.com>                     *
+#    Copyright (c) 2025 AstoCAD <hello@astocad.com>                         *
 #                                                                           *
 #    This file is part of FreeCAD.                                          *
 #                                                                           *
