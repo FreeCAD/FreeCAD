7fcd40aaa5e8d20a56b6e81505eccc22872080c0
tarman3 <joice501790018@tutamail.com>
2026-04-17 13:10:38 +0300

    CAM: DressupTag - Minor fixes

--- src/Mod/CAM/CAMTests/TestPathDressupHoldingTags.py
+++ src/Mod/CAM/CAMTests/TestPathDressupHoldingTags.py
@@ -33,7 +33,7 @@ class TestHoldingTags(PathTestUtils.PathTestBase):
 
     def test00(self):
         """Check Tag origin."""
-        tag = Tag(0, 77, 13, 4, 5, 90, True)
+        tag = Tag(0, 77, 13, 4, 5, 90, 0, True)
         self.assertCoincide(tag.originAt(3), Vector(77, 13, 3))
 
     def test01(self):
@@ -42,7 +42,7 @@ class TestHoldingTags(PathTestUtils.PathTestBase):
         tag.createSolidsAt(17, 0)
 
         self.assertIsNotNone(tag.solid)
-        self.assertCylinderAt(tag.solid, Vector(100, 200, 17 - 5 * 0.01), 2, 5 * 1.01)
+        self.assertCylinderAt(tag.solid, Vector(100, 200, 17 - 0.1), 2, 5 + 0.1)
 
     def test02(self):
         """Verify trapezoidal tag has a cone shape with a lid."""
@@ -50,21 +50,21 @@ class TestHoldingTags(PathTestUtils.PathTestBase):
         tag.createSolidsAt(0, 0)
 
         self.assertIsNotNone(tag.solid)
-        self.assertConeAt(tag.solid, Vector(0, 0, -0.05), 9, 3.95, 5.05)
+        self.assertConeAt(tag.solid, Vector(0, 0, -0.1), 9, 3.9, 5 + 0.1)
 
     def test03(self):
         """Verify pointy cone shape of tag with pointy end if width, angle and height match up."""
         tag = Tag(0, 0, 0, 10, 5, 45, 0, True)
         tag.createSolidsAt(0, 0)
         self.assertIsNotNone(tag.solid)
-        h = 5 * 1.01
-        self.assertConeAt(tag.solid, Vector(0, 0, -h * 0.01), 5, 0, h)
+        h = 5 + 0.1
+        self.assertConeAt(tag.solid, Vector(0, 0, -0.1), 5, 0, h)
 
     def test04(self):
         """Verify height adjustment if tag isn't wide eough for angle."""
         tag = Tag(0, 0, 0, 5, 17, 60, 0, True)
         tag.createSolidsAt(0, 0)
         self.assertIsNotNone(tag.solid)
-        h = 2.5 * math.tan((60 / 180.0) * math.pi) * 1.01
+        h = 2.5 * math.tan((60 / 180.0) * math.pi) + 0.1
         print(h)
-        self.assertConeAt(tag.solid, Vector(0, 0, -h * 0.01), 2.5, 0, h)
+        self.assertConeAt(tag.solid, Vector(0, 0, -0.1), 2.5, 0, h)
