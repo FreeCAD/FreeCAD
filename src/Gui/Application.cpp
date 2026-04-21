6099a3295fc929a8beb670c5173017fae9f32a9d
wwmayer <wmayer@freecad.org>
2026-04-21 11:40:35 +0200

    Gui: Readd ViewProviderOrigin
    With https://github.com/FreeCAD/FreeCAD/pull/18126 the class
    ViewProviderOrigin was renamed to ViewProviderCoordinateSystem.
    
    However, this change breaks backward compatibility of old project files
    where the type Gui::ViewProviderOrigin is explicitly set as ViewType in
    Document.xml.
    
    To fix this the class ViewProviderOrigin is readded as subclass of
    ViewProviderCoordinateSystem.
    
    An example file can be found here: https://forum.freecad.org/
    viewtopic.php?p=228225#p228225
    
    # Conflicts:
    #	src/Gui/ViewProviderCoordinateSystem.h

--- src/Gui/Application.cpp
+++ src/Gui/Application.cpp
@@ -2454,6 +2454,7 @@ void Application::initTypes()
     Gui::ViewProviderOriginGroup                ::init();
     Gui::ViewProviderPart                       ::init();
     Gui::ViewProviderCoordinateSystem           ::init();
+    Gui::ViewProviderOrigin                     ::init();
     Gui::ViewProviderMaterialObject             ::init();
     Gui::ViewProviderMaterialObjectPython       ::init();
     Gui::ViewProviderTextDocument               ::init();
