tigert/TigertCAD

commit 760a72c33c15de30926f08cb8071823389853201

Measure: Improve Angle creation, UX and Sector Support (#27135)
* Measure: introduced Arrow for arc angle and improve UX

Signed-off-by: Yash Suthar <yashsuthar983@gmail.com>

* Measure: Prevent arrow Overlap and scale based on screenspace

Signed-off-by: Yash Suthar <yashsuthar983@gmail.com>

* Measure: fix measure angle visualization

Signed-off-by: Yash Suthar <yashsuthar983@gmail.com>

* Measure: Fix render loop due in SoScreenSpaceScale

Signed-off-by: Yash Suthar <yashsuthar983@gmail.com>

* Measure: make the angle deterministic and implimented vertically opposite drage

Signed-off-by: Yash Suthar <yashsuthar983@gmail.com>

* Measure: Only allow angle flip when tool is active

Signed-off-by: Yash Suthar <yashsuthar983@gmail.com>

* Measure: added imaginary origin for angle measurment

Signed-off-by: Yash Suthar <yashsuthar983@gmail.com>

* Measure: Added geometric tol of commonedge detection

Signed-off-by: Yash Suthar <yashsuthar983@gmail.com>

* Measure: Improve  visualization of skew angles

Signed-off-by: Yash Suthar <yashsuthar983@gmail.com>

* Measure: Fix the faceEdge case and refactored the code

Signed-off-by: Yash Suthar <yashsuthar983@gmail.com>

* Measure: Fix the tranform issue and arc normal color

Signed-off-by: Yash Suthar <yashsuthar983@gmail.com>

* Measure: fix old conflict code in viewprovidermeasureandle

Signed-off-by: Yash Suthar <yashsuthar983@gmail.com>

* Measure: refactored measureAngle

Signed-off-by: Yash Suthar <yashsuthar983@gmail.com>

* Measure: normalise the faceEdge case

Signed-off-by: Yash Suthar <yashsuthar983@gmail.com>

* Update src/Mod/Measure/App/MeasureAngle.cpp

Co-authored-by: Benjamin Nauck <benjamin@nauck.se>

* Measure: remove wip

Signed-off-by: Yash Suthar <yashsuthar983@gmail.com>

* Measure: Added label start with middle of arc on angle creation

Signed-off-by: Yash Suthar <yashsuthar983@gmail.com>

* Measure: used makeRotationByAxes instead of raw matrix build

src/Mod/Measure/Gui/ViewProviderMeasureBase.cpp
@@ -108,6 +108,27 @@ ViewProviderMeasureBase::ViewProviderMeasureBase()
         App::Prop_None,
         "Size of measurement text"
     );
+    ADD_PROPERTY_TYPE(
+        ArrowHeight,
+        (Preferences::defaultArrowHeight()),
+        agroup,
+        App::Prop_None,
+        "Height of arrow indicators"
+    );
+    ADD_PROPERTY_TYPE(
+        ArrowRadius,
+        (Preferences::defaultArrowRadius()),
+        agroup,
+        App::Prop_None,
+        "Radius of arrow indicators"
+    );
+    ADD_PROPERTY_TYPE(
+        LabelPosition,
+        (Base::Vector3d(0, 0, 0)),
+        agroup,
+        App::Prop_None,
+        "Position of measurement label"
+    );
     // NOLINTEND
 
     pGlobalSeparator = new SoSeparator();
@@ -212,6 +233,11 @@ ViewProviderMeasureBase::ViewProviderMeasureBase()
     FontSize.touch();
     LineColor.touch();
     fieldFontSize.setValue(FontSize.getValue());
+    // Arrow properties
+    ArrowHeight.touch();
+    ArrowRadius.touch();
+    fieldArrowHeight.setValue(ArrowHeight.getValue());
+    fieldArrowRadius.setValue(ArrowRadius.getValue());
 }
 
 ViewProviderMeasureBase::~ViewProviderMeasureBase()
@@ -250,6 +276,10 @@ void ViewProviderMeasureBase::setDisplayMode(const char* ModeName)
 
 void ViewProviderMeasureBase::finishRestoring()
 {
+    // Restore dragger position from saved property
+    Base::Vector3d pos = LabelPosition.getValue();
+    pDragger->translation.setValue(SbVec3f(pos.x, pos.y, pos.z));
+
     if (Visibility.getValue() && isSubjectVisible()) {
         show();
     }
@@ -275,6 +305,12 @@ void ViewProviderMeasureBase::onChanged(const App::Property* prop)
     else if (prop == &FontSize) {
         pLabel->size = FontSize.getValue();
         fieldFontSize.setValue(FontSize.getValue());
+    }
+    else if (prop == &ArrowHeight) {
+        fieldArrowHeight.setValue(ArrowHeight.getValue());
+    }
+    else if (prop == &ArrowRadius) {
+        fieldArrowRadius.setValue(ArrowRadius.getValue());
     }
 
     ViewProviderDocumentObject::onChanged(prop);

src/Mod/Measure/App/MeasureAngle.cpp
@@ -28,8 +28,24 @@
 #include <App/MeasureManager.h>
 #include <Base/Tools.h>
 #include <Base/Precision.h>
+#include <GeomAPI_ProjectPointOnCurve.hxx>
+#include <Geom_Line.hxx>
+#include <Geom_Plane.hxx>
+#include <GeomAPI_IntSS.hxx>
+#include <GeomAPI_ExtremaCurveCurve.hxx>
+#include <TopExp_Explorer.hxx>
+#include <TopoDS.hxx>
+#include <TopoDS_Shape.hxx>
+#include <TopoDS_Edge.hxx>
+#include <TopoDS_Vertex.hxx>
+#include <TopExp.hxx>
+#include <BRep_Tool.hxx>
 
 #include "MeasureAngle.h"
+#include <Mod/Part/App/Geometry.h>
+#include <Mod/Part/App/PartFeature.h>
+#include <Mod/Part/App/Tools.h>
+#include <TopTools_IndexedMapOfShape.hxx>
 
 using namespace Measure;
 
@@ -211,6 +227,243 @@ gp_Vec MeasureAngle::location2()
     return {temp.x, temp.y, temp.z};
 }
 
+bool MeasureAngle::setOrigin(TopoDS_Shape& s1, TopoDS_Shape& s2)
+{
+    if (s1.IsNull() || s2.IsNull()) {
+        return false;
+    }
+
+    switch (mCase) {
+        case MeasurementCase::FaceFace:
+            return computeOriginFaceFace(s1, s2);
+        case MeasurementCase::EdgeEdge:
+            return computeOriginEdgeEdge(s1, s2);
+        case MeasurementCase::FaceEdge:
+            return computeOriginFaceEdge(s1, s2);
+    }
+
+    // cant reach here
+    return false;
+}
+
+bool MeasureAngle::computeOriginFaceFace(TopoDS_Shape& s1, TopoDS_Shape& s2)
+{
+    auto setOriginFromEdgeMidpoint = [this](const TopoDS_Edge& edge) -> bool {
+        TopoDS_Vertex v1, v2;
+        TopExp::Vertices(edge, v1, v2);
+        outOrigin = gp_Pnt((BRep_Tool::Pnt(v1).XYZ() + BRep_Tool::Pnt(v2).XYZ()) / 2);
+        return true;
+    };
+
+    TopTools_IndexedMapOfShape edges1, edges2;
+    TopExp::MapShapes(s1, TopAbs_EDGE, edges1);
+    TopExp::MapShapes(s2, TopAbs_EDGE, edges2);
+
+    for (int i = 1; i <= edges1.Extent(); i++) {
+        const TopoDS_Edge& ed1 = TopoDS::Edge(edges1(i));
+        for (int j = 1; j <= edges2.Extent(); j++) {
+            const TopoDS_Edge& ed2 = TopoDS::Edge(edges2(j));
+
+            if (ed1.IsSame(ed2)) {
+                return setOriginFromEdgeMidpoint(ed1);
+            }
+
+            auto geom1 = Part::Geometry::fromShape(ed1, true);
+            auto geom2 = Part::Geometry::fromShape(ed2, true);
+            if (geom1 && geom2
+                && geom1->isSame(*geom2, Precision::Confusion(), Precision::Angular())) {
+                return setOriginFromEdgeMidpoint(ed1);
+            }
+        }
+    }
+
+    _isImgOrigin = true;
+
+    gp_Pln pln1(gp_Pnt(location1().XYZ()), gp_Dir(vector1()));
+    gp_Pln pln2(gp_Pnt(location2().XYZ()), gp_Dir(vector2()));
+
+    gp_Lin intersectionLine;
+    if (Part::intersect(pln1, pln2, intersectionLine)) {
+        gp_Pnt refPnt(location1().XYZ());
+        GeomAPI_ProjectPointOnCurve proj(refPnt, new Geom_Line(intersectionLine));
+        if (proj.NbPoints() > 0) {
+            outOrigin = proj.Point(1);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool MeasureAngle::computeOriginEdgeEdge(TopoDS_Shape& s1, TopoDS_Shape& s2)
+{
+    TopoDS_Edge e1 = TopoDS::Edge(s1);
+    TopoDS_Edge e2 = TopoDS::Edge(s2);
+    TopoDS_Vertex common;
+
+    if (TopExp::CommonVertex(e1, e2, common)) {
+        outOrigin = BRep_Tool::Pnt(common);
+        return true;
+    }
+
+    // get geometrically same vertex
+    TopoDS_Vertex v1_1, v1_2, v2_1, v2_2;
+    TopExp::Vertices(e1, v1_1, v1_2);
+    TopExp::Vertices(e2, v2_1, v2_2);
+
+    gp_Pnt p1_1 = BRep_Tool::Pnt(v1_1);
+    gp_Pnt p1_2 = BRep_Tool::Pnt(v1_2);
+    gp_Pnt p2_1 = BRep_Tool::Pnt(v2_1);
+    gp_Pnt p2_2 = BRep_Tool::Pnt(v2_2);
+
+    double tol = Precision::Confusion();
+    if (p1_1.IsEqual(p2_1, tol) || p1_1.IsEqual(p2_2, tol)) {
+        outOrigin = p1_1;
+        return true;
+    }
+    if (p1_2.IsEqual(p2_1, tol) || p1_2.IsEqual(p2_2, tol)) {
+        outOrigin = p1_2;
+        return true;
+    }
+
+    _isImgOrigin = true;
+
+    gp_Lin lin1(gp_Pnt(location1().XYZ()), gp_Dir(vector1()));
+    gp_Lin lin2(gp_Pnt(location2().XYZ()), gp_Dir(vector2()));
+    gp_Pnt p1, p2;
+    Part::closestPointsOnLines(lin1, lin2, p1, p2);
+    outOrigin = p1;
+    return true;
+}
+
+bool MeasureAngle::computeOriginFaceEdge(TopoDS_Shape& s1, TopoDS_Shape& s2)
+{
+    _isImgOrigin = true;
+
+    bool faceIsS1 = (s1.ShapeType() == TopAbs_FACE);
+    gp_Vec faceNormal = ((faceIsS1) ? vector1() : vector2()).Normalized();
+    gp_Vec edgeDir = ((faceIsS1) ? vector2() : vector1()).Normalized();
+    gp_Pnt faceLoc = gp_Pnt(((faceIsS1) ? location1() : location2()).XYZ());
+    gp_Pnt edgeLoc = gp_Pnt(((faceIsS1) ? location2() : location1()).XYZ());
+
+    // projection direction from the edge onto the face
+    Base::Vector3d edgeDirVec(edgeDir.X(), edgeDir.Y(), edgeDir.Z());
+    Base::Vector3d normalVec(faceNormal.X(), faceNormal.Y(), faceNormal.Z());
+    edgeDirVec.ProjectToPlane(Base::Vector3d(0, 0, 0), normalVec);
+
+    gp_Vec projection(edgeDirVec.x, edgeDirVec.y, edgeDirVec.z);
+    projection.Normalize();
+
+    gp_Lin linEdge(edgeLoc, gp_Dir(edgeDir));
+    gp_Lin linProj(faceLoc, gp_Dir(projection));
+    gp_Pnt p1, p2;
+    Part::closestPointsOnLines(linEdge, linProj, p1, p2);
+
+    if (faceIsS1) {
+        outOrigin = p2;
+    }
+    else {
+        gp_Vec facePtToP1(faceLoc, p1);
+        double dist = facePtToP1.Dot(faceNormal);
+        outOrigin = gp_Pnt(p1.XYZ() - dist * faceNormal.XYZ());
+    }
+
+    return true;
+}
+
+
+bool MeasureAngle::getOrigin(gp_Pnt& outOrigin)
+{
+    outOrigin = this->outOrigin;
+    return true;
+}
+
+bool MeasureAngle::setDirections(TopoDS_Shape& s1, TopoDS_Shape& s2)
+{
+
+    direction1 = vector1();
+    direction2 = vector2();
+
+    gp_Vec loc1 = location1();
+    gp_Vec loc2 = location2();
+
+    // For edges with a common vertex, we need to orient the vectors
+    // so they point away from the common vertex
+    if (mCase == MeasurementCase::EdgeEdge) {
+
+        if (direction1.Dot(gp_Vec(outOrigin.XYZ()) - loc1) > 0) {
+            direction1.Reverse();
+        }
+        if (direction2.Dot(gp_Vec(outOrigin.XYZ()) - loc2) > 0) {
+            direction2.Reverse();
+        }
+    }
+    else if (mCase == MeasurementCase::FaceFace) {
+
+        gp_Vec between = loc2 - loc1;
+
+        if (direction1.Dot(between) < 0) {
+            direction1.Reverse();
+        }
+        if (direction2.Dot(between) > 0) {
+            direction2.Reverse();
+        }
+    }
+    else if (mCase == MeasurementCase::FaceEdge) {
+        bool faceIsS1 = (s1.ShapeType() == TopAbs_FACE);
+
+        gp_Vec& faceNormal = (faceIsS1 ? direction1 : direction2);
+        gp_Vec& edgeDir = faceIsS1 ? direction2 : direction1;
+        gp_Vec edgeLoc = faceIsS1 ? loc2 : loc1;
+
+        faceNormal.Normalize();
+        edgeDir.Normalize();
+
+        // project edge exactly onto the face plane
+        Base::Vector3d edgeDirVec(edgeDir.X(), edgeDir.Y(), edgeDir.Z());
+        Base::Vector3d normalVec(faceNormal.X(), faceNormal.Y(), faceNormal.Z());
+        edgeDirVec.ProjectToPlane(Base::Vector3d(0, 0, 0), normalVec);
+
+        gp_Vec projection(edgeDirVec.x, edgeDirVec.y, edgeDirVec.z);
+        projection.Normalize();
+
+        if (edgeDir.Dot(gp_Vec(outOrigin.XYZ()) - edgeLoc) > 0) {
+            edgeDir.Reverse();
+        }
+
+        if (!projection.IsParallel(edgeDir, Precision::Angular())) {
+            if (projection.Dot(edgeDir) < 0) {
+                projection.Reverse();
+            }
+
+            faceNormal = projection;
+        }
+    }
+    else {
+        // should not reach here
+        return false;
+    }
+
+    return true;
+}
+
+bool MeasureAngle::getDirections(gp_Vec& dir1, gp_Vec& dir2)
+{
+    if (direction1.Magnitude() < Precision::Confusion()
+        || direction2.Magnitude() < Precision::Confusion()) {
+        return false;
+    }
+    dir1 = direction1;
+    dir2 = direction2;
+
+    return true;
+}
+
+bool MeasureAngle::isImgOrigin()
+{
+    return _isImgOrigin;
+}
+
 App::DocumentObjectExecReturn* MeasureAngle::execute()
 {
     App::DocumentObject* ob1 = Element1.getValue();
@@ -218,6 +471,8 @@ App::DocumentObjectExecReturn* MeasureAngle::execute()
 
     App::DocumentObject* ob2 = Element2.getValue();
     std::vector<std::string> subs2 = Element2.getSubValues();
+
+    _isImgOrigin = false;
 
     if (!ob1 || !ob1->isValid() || !ob2 || !ob2->isValid()) {
         return new App::DocumentObjectExecReturn("Submitted object(s) is not valid");
@@ -227,34 +482,41 @@ App::DocumentObjectExecReturn* MeasureAngle::execute()
         return new App::DocumentObjectExecReturn("No geometry element picked");
     }
 
-    Base::Vector3d vec1;
-    getVec(*ob1, subs1.at(0), vec1);
-
-    Base::Vector3d vec2;
-    getVec(*ob2, subs2.at(0), vec2);
-
-    if (vec1.IsParallel(vec2, Base::Precision::Angular())) {
-        // handle case when both vectors are parallel
-        Angle.setValue(0);
+    TopoDS_Shape s1 = Part::Feature::getShape(
+        ob1,
+        Part::ShapeOption::NeedSubElement | Part::ShapeOption::ResolveLink
+            | Part::ShapeOption::Transform,
+        subs1.at(0).c_str()
+    );
+    TopoDS_Shape s2 = Part::Feature::getShape(
+        ob2,
+        Part::ShapeOption::NeedSubElement | Part::ShapeOption::ResolveLink
+            | Part::ShapeOption::Transform,
+        subs2.at(0).c_str()
+    );
+    if (s1.ShapeType() == TopAbs_FACE && s2.ShapeType() == TopAbs_FACE) {
+        mCase = MeasurementCase::FaceFace;
+    }
+    else if (s1.ShapeType() == TopAbs_EDGE && s2.ShapeType() == TopAbs_EDGE) {
+        mCase = MeasurementCase::EdgeEdge;
     }
     else {
-        // get oriented vectors based on common origin
-        Base::Vector3d loc1 = getLoc(*ob1, subs1.at(0));
-        Base::Vector3d loc2 = getLoc(*ob2, subs2.at(0));
-        Base::Vector3d origin = (loc1 + loc2) * 0.5;
-
-        // flip if needed, to make them point away from origin
-        if ((loc1 - origin).Dot(vec1) < 0) {
-            vec1 = -vec1;
-        }
-        if ((loc2 - origin).Dot(vec2) < 0) {
-            vec2 = -vec2;
-        }
-
-        // get oriented angle wrt normal axis
-        Base::Vector3d normalAxis = (vec1.Cross(vec2)).Normalize();
-        Angle.setValue(Base::toDegrees(vec1.GetAngleOriented(vec2, normalAxis)));
+        mCase = MeasurementCase::FaceEdge;
     }
+
+    if (!setOrigin(s1, s2) || !setDirections(s1, s2)) {
+        return new App::DocumentObjectExecReturn("Failed to Set Origin");
+    }
+
+
+    double angleRad = direction1.Angle(direction2);
+
+    // because of face normal are perpendicular to face
+    if (mCase == MeasurementCase::FaceFace) {
+        angleRad = std::numbers::pi - angleRad;
+    }
+
+    Angle.setValue(Base::toDegrees(angleRad));
 
     return DocumentObject::StdReturn;
 }

src/Mod/Measure/App/MeasureAngle.h
@@ -26,6 +26,7 @@
 
 #include <Mod/Measure/MeasureGlobal.h>
 
+#include <TopoDS_Edge.hxx>
 #include <gp_Vec.hxx>
 
 #include <App/PropertyGeo.h>
@@ -47,6 +48,13 @@ class MeasureExport MeasureAngle: public Measure::MeasureBaseExtendable<Part::Me
     PROPERTY_HEADER_WITH_OVERRIDE(Measure::MeasureAngle);
 
 public:
+    enum class MeasurementCase
+    {
+        FaceFace,
+        EdgeEdge,
+        FaceEdge
+    };
+
     /// Constructor
     MeasureAngle();
     ~MeasureAngle() override;
@@ -90,7 +98,30 @@ public:
     gp_Vec location1();
     gp_Vec location2();
 
+    MeasurementCase measurementCase() const
+    {
+        return mCase;
+    };
+    bool isImgOrigin();
+    bool getOrigin(gp_Pnt& outOrigin);               // get origin of the angle
+    bool getDirections(gp_Vec& vec1, gp_Vec& vec2);  // this is not the normals(its is adjusted for
+                                                     // arc visualization)
+
 private:
+    gp_Vec direction1;
+    gp_Vec direction2;
+    gp_Pnt outOrigin;
+    MeasurementCase mCase {MeasurementCase::EdgeEdge};
+    // if no common vertex/edge found, use imaginary origin by extending
+    bool _isImgOrigin {false};
+
+    bool setOrigin(TopoDS_Shape& s1, TopoDS_Shape& s2);
+    bool computeOriginFaceFace(TopoDS_Shape& s1, TopoDS_Shape& s2);
+    bool computeOriginEdgeEdge(TopoDS_Shape& s1, TopoDS_Shape& s2);
+    bool computeOriginFaceEdge(TopoDS_Shape& s1, TopoDS_Shape& s2);
+
+    bool setDirections(TopoDS_Shape& s1, TopoDS_Shape& s2);  // not the actual normals adjusted
+                                                             // for arc visualization
     void onChanged(const App::Property* prop) override;
 };
 

src/Mod/Measure/App/Preferences.cpp
@@ -77,3 +77,13 @@ int Preferences::defaultFontSize()
 {
     return getPreferenceGroup("Appearance")->GetInt("DefaultFontSize", 18);
 }
+
+int Preferences::defaultArrowRadius()
+{
+    return getPreferenceGroup("Appearance")->GetInt("DefaultArrowRadius", 6);
+}
+
+int Preferences::defaultArrowHeight()
+{
+    return getPreferenceGroup("Appearance")->GetInt("DefaultArrowHeight", 18);
+}

src/Mod/Measure/App/Preferences.h
@@ -49,6 +49,8 @@ public:
     static Base::Color defaultTextColor();
     static int defaultFontSize();
     static Base::Color defaultTextBackgroundColor();
+    static int defaultArrowRadius();
+    static int defaultArrowHeight();
 };
 
 

src/Mod/Measure/Gui/AppMeasureGui.cpp
@@ -36,6 +36,7 @@
 #include "ViewProviderMeasureAngle.h"
 #include "ViewProviderMeasureDistance.h"
 #include "ViewProviderMeasureBase.h"
+#include "SoScreenSpaceScale.h"
 #include "ViewProviderMassPropertiesResult.h"
 
 
@@ -93,6 +94,7 @@ PyMOD_INIT_FUNC(MeasureGui)
 
     // clang-format off
     MeasureGui::DimensionLinear::initClass();
+    MeasureGui::SoScreenSpaceScale::initClass();
 
     MeasureGui::ViewProviderMeasureGroup               ::init();
     MeasureGui::ViewProviderMeasureBase                ::init();

src/Mod/Measure/Gui/CMakeLists.txt
@@ -48,6 +48,8 @@ SET(MeasureGui_SRCS
     ViewProviderMeasureAngle.h
     ViewProviderMeasureDistance.cpp
     ViewProviderMeasureDistance.h
+    SoScreenSpaceScale.cpp
+    SoScreenSpaceScale.h
     ViewProviderMassPropertiesResult.cpp
     ViewProviderMassPropertiesResult.h
     DlgPrefsMeasureAppearanceImp.ui

FreeCAD/FreeCAD

commit 798815340d1f548da089f80a223c6e5edc3f8d1e

Measure: fix crash and issues caused by #27135
Signed-off-by: Yash Suthar <yashsuthar983@gmail.com>

src/Mod/Measure/Gui/TaskMeasure.cpp
@@ -610,12 +610,13 @@ bool TaskMeasure::apply(bool reset)
 bool TaskMeasure::reject()
 {
     removeObject();
-    closeDialog();
 
-    // Abort transaction
+    // Commit after removing the preview measurement so only intended changes (sector flip)
+    // remain in the document transaction.
     if (mTargetDoc) {
-        mTargetDoc->abortCommand();
+        mTargetDoc->commitCommand();
     }
+    closeDialog();
     return false;
 }
 

src/Mod/Measure/Gui/ViewProviderMeasureAngle.cpp
@@ -668,8 +668,9 @@ void ViewProviderMeasureAngle::positionAnno(const Measure::MeasureBase* measureO
 
 void ViewProviderMeasureAngle::onLabelMoved()
 {
-    if (!Gui::Control().activeDialog()
-        || !dynamic_cast<MeasureGui::TaskMeasure*>(Gui::Control().activeDialog())) {
+    auto* activeDialog = Gui::Control().activeDialog();
+    const bool isTaskMeasure = activeDialog && dynamic_cast<MeasureGui::TaskMeasure*>(activeDialog);
+    if (!isTaskMeasure) {
         return;
     }
     SbVec3f trans = pLabelTranslation->translation.getValue();
@@ -702,10 +703,7 @@ void ViewProviderMeasureAngle::onLabelMoved()
 
 void ViewProviderMeasureAngle::onLabelMoveFinish()
 {
-    if (!Gui::Control().activeDialog()
-        || !dynamic_cast<MeasureGui::TaskMeasure*>(Gui::Control().activeDialog())) {
-        return;
-    }
+    ViewProviderMeasureBase::onLabelMoveFinish();
     IsFlipped.setValue(isArcFlipped.getValue());
 }
 

src/Mod/Measure/Gui/ViewProviderMeasureBase.cpp
@@ -278,7 +278,7 @@ void ViewProviderMeasureBase::finishRestoring()
 {
     // Restore dragger position from saved property
     Base::Vector3d pos = LabelPosition.getValue();
-    pDragger->translation.setValue(SbVec3f(pos.x, pos.y, pos.z));
+    setLabelTranslation(toSbVec3f(pos));
 
     if (Visibility.getValue() && isSubjectVisible()) {
         show();
@@ -358,8 +358,9 @@ void ViewProviderMeasureBase::onLabelMoveStart()
 void ViewProviderMeasureBase::onLabelMoveFinish()
 {
     SbVec3f currentLabelPos = pLabelTranslation->translation.getValue();
-    pDragger->translation.setValue(SbVec3f(0.0f, 0.0f, 0.0f));
     pDraggerFrame->translation.setValue(currentLabelPos);
+    pDragger->translation.setValue(SbVec3f(0.0f, 0.0f, 0.0f));
+    LabelPosition.setValue(Base::Vector3d(currentLabelPos[0], currentLabelPos[1], currentLabelPos[2]));
 }
 
 void ViewProviderMeasureBase::setLabelValue(const Base::Quantity& value)