tigert/TigertCAD

commit 474de538ab6f363e28528ebaa6be58d0ee29d44d

Draft: fix arrow alignment for BezCurve and BSpline (#29222)
* Draft: fix arrow alignment for BezCurve and BSpline

* Draft: fix arrow alignment for BezCurve and BSpline

src/Mod/Draft/draftviewproviders/view_wire.py
@@ -117,15 +117,21 @@ class ViewProviderWire(ViewProviderDraft):
             and hasattr(self, "coords1")
             and hasattr(self, "coords2")
         ):
+            if utils.get_type(obj) == "BSpline":
+                shp = obj.Shape
+                rot = obj.Placement.Rotation.inverted()
+                v1 = rot.multVec(shp.tangentAt(shp.FirstParameter))
+                v2 = -rot.multVec(shp.tangentAt(shp.LastParameter))
+            else:
+                v1 = obj.Points[1].sub(obj.Points[0])
+                v2 = obj.Points[-2].sub(obj.Points[-1])
             self.coords1.translation.setValue(*obj.Points[0])
-            v1 = obj.Points[1].sub(obj.Points[0])
             if not DraftVecUtils.isNull(v1):
                 v1.normalize()
                 rot1 = coin.SbRotation()
                 rot1.setValue(coin.SbVec3f(1, 0, 0), coin.SbVec3f(*v1))
                 self.coords1.rotation.setValue(rot1)
             self.coords2.translation.setValue(*obj.Points[-1])
-            v2 = obj.Points[-2].sub(obj.Points[-1])
             if not DraftVecUtils.isNull(v2):
                 v2.normalize()
                 rot2 = coin.SbRotation()

src/Mod/Draft/draftfunctions/svg.py
@@ -1169,11 +1169,19 @@ def get_svg(
             and hasattr(vobj, "ArrowTypeEnd")
             and hasattr(vobj, "ArrowSizeStart")
             and hasattr(vobj, "ArrowSizeEnd")
-            and len(obj.Shape.Vertexes) > 1
+            and len(obj.Points) > 1
         ):
-            # Draft_Wire
+            # Draft_Wire, Draft_BezCurve and Draft_BSpline.
+            shp = obj.Shape
+            if utils.get_type(obj) == "BSpline":
+                v1 = shp.tangentAt(shp.FirstParameter)
+                v2 = -shp.tangentAt(shp.LastParameter)
+            else:
+                rot = obj.Placement.Rotation
+                v1 = rot.multVec(obj.Points[1].sub(obj.Points[0]))
+                v2 = rot.multVec(obj.Points[-2].sub(obj.Points[-1]))
             p1 = get_proj(obj.Shape.Vertexes[0].Point, plane)
-            p2 = get_proj(obj.Shape.Vertexes[1].Point, plane)
+            p2 = get_proj(obj.Shape.Vertexes[0].Point + v1, plane)
             svg += get_arrow(
                 obj,
                 vobj.ArrowTypeStart,
@@ -1184,7 +1192,7 @@ def get_svg(
                 -DraftVecUtils.angle(p2 - p1),
             )
             p1 = get_proj(obj.Shape.Vertexes[-1].Point, plane)
-            p2 = get_proj(obj.Shape.Vertexes[-2].Point, plane)
+            p2 = get_proj(obj.Shape.Vertexes[-1].Point + v2, plane)
             svg += get_arrow(
                 obj,
                 vobj.ArrowTypeEnd,

FreeCAD/FreeCAD

commit e15a54f46ad37d052e900e000dbe0a97b1404d4b

Draft: fix Bspline regression caused by #29222
Fixes #29770.

See: https://github.com/FreeCAD/FreeCAD/issues/29770#issuecomment-4378085405

src/Mod/Draft/draftviewproviders/view_wire.py
@@ -111,14 +111,17 @@ class ViewProviderWire(ViewProviderDraft):
 
     def updateData(self, obj, prop):
         if (
-            prop == "Points"
+            prop == "Shape"
             and hasattr(obj, "Points")
             and len(obj.Points) >= 2
             and hasattr(self, "coords1")
             and hasattr(self, "coords2")
         ):
+            shp = obj.Shape
+            if shp.isNull():
+                return
+
             if utils.get_type(obj) == "BSpline":
-                shp = obj.Shape
                 rot = obj.Placement.Rotation.inverted()
                 v1 = rot.multVec(shp.tangentAt(shp.FirstParameter))
                 v2 = -rot.multVec(shp.tangentAt(shp.LastParameter))