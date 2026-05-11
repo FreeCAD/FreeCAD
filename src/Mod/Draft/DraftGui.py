FreeCAD/FreeCAD

commit 146ed0d704b62317cc06015f48ed1a0baf0ceb74

Draft: allow more than 1 character for in-command shortcuts (#26950)

src/Mod/Draft/DraftGui.py
@@ -72,7 +72,7 @@ class DraftBaseWidget(QtWidgets.QWidget):
         super().__init__(parent)
 
     def eventFilter(self, widget, event):
-        if event.type() == QtCore.QEvent.KeyPress and event.text().upper() == _get_incmd_shortcut(
+        if event.type() == QtCore.QEvent.KeyPress and event.text().upper() in _get_incmd_shortcut(
             "CycleSnap"
         ):
             if hasattr(FreeCADGui, "Snapper"):
@@ -1324,84 +1324,84 @@ class DraftToolBar:
         spec = False
         self.last_point = self.get_last_point()
         # Most frequently used shortcuts first:
-        if txt == _get_incmd_shortcut("Relative"):
+        if txt in _get_incmd_shortcut("Relative"):
             if self.isRelative.isVisible():
                 self.isRelative.setChecked(not self.isRelative.isChecked())
                 # setRelative takes care of rest
             spec = True
-        elif txt == _get_incmd_shortcut("Global"):
+        elif txt in _get_incmd_shortcut("Global"):
             if self.isGlobal.isVisible():
                 self.isGlobal.setChecked(not self.isGlobal.isChecked())
                 # setGlobal takes care of rest
             spec = True
-        elif txt == _get_incmd_shortcut("Length"):
+        elif txt in _get_incmd_shortcut("Length"):
             if self.lengthValue.isVisible():
                 self.constrain("angle")
             self.displayPoint(self.new_point, self.last_point)
             spec = True
-        elif txt == _get_incmd_shortcut("RestrictX"):
+        elif txt in _get_incmd_shortcut("RestrictX"):
             self.constrain("x")
             self.displayPoint(self.new_point, self.last_point)
             spec = True
-        elif txt == _get_incmd_shortcut("RestrictY"):
+        elif txt in _get_incmd_shortcut("RestrictY"):
             self.constrain("y")
             self.displayPoint(self.new_point, self.last_point)
             spec = True
-        elif txt == _get_incmd_shortcut("RestrictZ"):
+        elif txt in _get_incmd_shortcut("RestrictZ"):
             self.constrain("z")
             self.displayPoint(self.new_point, self.last_point)
             spec = True
-        elif txt == _get_incmd_shortcut("Copy"):
+        elif txt in _get_incmd_shortcut("Copy"):
             if self.isCopy.isVisible():
                 self.isCopy.setChecked(not self.isCopy.isChecked())
             spec = True
-        elif txt == _get_incmd_shortcut("Exit"):
+        elif txt in _get_incmd_shortcut("Exit"):
             if self.finishButton.isVisible():
                 self.finish()
-        elif txt == _get_incmd_shortcut("Close"):
+        elif txt in _get_incmd_shortcut("Close"):
             if self.closeButton.isVisible():
                 self.closeLine()
-        elif txt == _get_incmd_shortcut("AddHold"):
+        elif txt in _get_incmd_shortcut("AddHold"):
             if hasattr(FreeCADGui, "Snapper"):
                 FreeCADGui.Snapper.addHoldPoint()
             spec = True
-        elif txt == _get_incmd_shortcut("Recenter"):
+        elif txt in _get_incmd_shortcut("Recenter"):
             if hasattr(FreeCADGui, "Snapper"):
                 FreeCADGui.Snapper.recenter_workingplane()
             spec = True
-        elif txt == _get_incmd_shortcut("Snap"):
+        elif txt in _get_incmd_shortcut("Snap"):
             self.togglesnap()
             spec = True
-        elif txt == _get_incmd_shortcut("MakeFace"):
+        elif txt in _get_incmd_shortcut("MakeFace"):
             if self.makeFace.isVisible():
                 self.makeFace.setChecked(not self.makeFace.isChecked())
             spec = True
-        elif txt == _get_incmd_shortcut("Continue"):
+        elif txt in _get_incmd_shortcut("Continue"):
             if self.continueCmd.isVisible():
                 self.toggleContinue()
             spec = True
-        elif txt == _get_incmd_shortcut("SetWP"):
+        elif txt in _get_incmd_shortcut("SetWP"):
             if self.orientWPButton.isVisible():
                 self.orientWP()
             spec = True
-        elif txt == _get_incmd_shortcut("SelectEdge"):
+        elif txt in _get_incmd_shortcut("SelectEdge"):
             self.selectEdge()
             spec = True
-        elif txt == _get_incmd_shortcut("SubelementMode"):
+        elif txt in _get_incmd_shortcut("SubelementMode"):
             if self.isSubelementMode.isVisible():
                 self.isSubelementMode.setChecked(not self.isSubelementMode.isChecked())
             spec = True
-        elif txt == _get_incmd_shortcut("Wipe"):
+        elif txt in _get_incmd_shortcut("Wipe"):
             if self.wipeButton.isVisible():
                 self.wipeLine()
             spec = True
-        elif txt == _get_incmd_shortcut("Undo"):
+        elif txt in _get_incmd_shortcut("Undo"):
             self.undoSegment()
             spec = True
-        elif txt == _get_incmd_shortcut("IncreaseRadius"):
+        elif txt in _get_incmd_shortcut("IncreaseRadius"):
             self.toggleradius(1)
             spec = True
-        elif txt == _get_incmd_shortcut("DecreaseRadius"):
+        elif txt in _get_incmd_shortcut("DecreaseRadius"):
             self.toggleradius(-1)
             spec = True
 

src/Mod/Draft/Resources/ui/preferences-draftinterface.ui
@@ -31,15 +31,12 @@
        <widget class="Gui::PrefLineEdit" name="lineEdit_inCommandShortcutRelative">
         <property name="maximumSize">
          <size>
-          <width>25</width>
+          <width>40</width>
           <height>16777215</height>
          </size>
         </property>
         <property name="text">
          <string>R</string>
-        </property>
-        <property name="maxLength">
-         <number>1</number>
         </property>
         <property name="clearButtonEnabled">
          <bool>false</bool>
@@ -79,15 +76,12 @@
        <widget class="Gui::PrefLineEdit" name="lineEdit_inCommandShortcutGlobal">
         <property name="maximumSize">
          <size>
-          <width>25</width>
+          <width>40</width>
           <height>16777215</height>
          </size>
         </property>
         <property name="text">
          <string>G</string>
-        </property>
-        <property name="maxLength">
-         <number>1</number>
         </property>
         <property name="clearButtonEnabled">
          <bool>false</bool>
@@ -127,15 +121,12 @@
        <widget class="Gui::PrefLineEdit" name="lineEdit_inCommandShortcutLength">
         <property name="maximumSize">
          <size>
-          <width>25</width>
+          <width>40</width>
           <height>16777215</height>
          </size>
         </property>
         <property name="text">
          <string>L</string>
-        </property>
-        <property name="maxLength">
-         <number>1</number>
         </property>
         <property name="clearButtonEnabled">
          <bool>false</bool>
@@ -175,15 +166,12 @@
        <widget class="Gui::PrefLineEdit" name="lineEdit_inCommandShortcutMakeFace">
         <property name="maximumSize">
          <size>
-          <width>25</width>
+          <width>40</width>
           <height>16777215</height>
          </size>
         </property>
         <property name="text">
          <string>F</string>
-        </property>
-        <property name="maxLength">
-         <number>1</number>
         </property>
         <property name="clearButtonEnabled">
          <bool>false</bool>
@@ -207,15 +195,12 @@
        <widget class="Gui::PrefLineEdit" name="lineEdit_inCommandShortcutSelectEdge">
         <property name="maximumSize">
          <size>
-          <width>25</width>
+          <width>40</width>
           <height>16777215</height>
          </size>
         </property>
         <property name="text">
          <string>E</string>
-        </property>
-        <property name="maxLength">
-         <number>1</number>
         </property>
         <property name="clearButtonEnabled">
          <bool>false</bool>
@@ -239,15 +224,12 @@
        <widget class="Gui::PrefLineEdit" name="lineEdit_inCommandShortcutSubelementMode">
         <property name="maximumSize">
          <size>
-          <width>25</width>
+          <width>40</width>
           <height>16777215</height>
          </size>
         </property>
         <property name="text">
          <string>B</string>
-        </property>
-        <property name="maxLength">
-         <number>1</number>
         </property>
         <property name="clearButtonEnabled">
          <bool>false</bool>
@@ -271,15 +253,12 @@
        <widget class="Gui::PrefLineEdit" name="lineEdit_inCommandShortcutCopy">
         <property name="maximumSize">
          <size>
-          <width>25</width>
+          <width>40</width>
           <height>16777215</height>
          </size>
         </property>
         <property name="text">
          <string>C</string>
-        </property>
-        <property name="maxLength">
-         <number>1</number>
         </property>
         <property name="clearButtonEnabled">
          <bool>false</bool>
@@ -303,15 +282,12 @@
        <widget class="Gui::PrefLineEdit" name="lineEdit_inCommandShortcutUndo">
         <property name="maximumSize">
          <size>
-          <width>25</width>
+          <width>40</width>
           <height>16777215</height>
          </size>
         </property>
         <property name="text">
          <string notr="true">/</string>
-        </property>
-        <property name="maxLength">
-         <number>1</number>
         </property>
         <property name="clearButtonEnabled">
          <bool>false</bool>
@@ -335,15 +311,12 @@
        <widget class="Gui::PrefLineEdit" name="lineEdit_inCommandShortcutWipe">
         <property name="maximumSize">
          <size>
-          <width>25</width>
+          <width>40</width>
           <height>16777215</height>
          </size>
         </property>
         <property name="text">
          <string>W</string>
-        </property>
-        <property name="maxLength">
-         <number>1</number>
         </property>
         <property name="clearButtonEnabled">
          <bool>false</bool>
@@ -367,15 +340,12 @@
        <widget class="Gui::PrefLineEdit" name="lineEdit_inCommandShortcutClose">
         <property name="maximumSize">
          <size>
-          <width>25</width>
+          <width>40</width>
           <height>16777215</height>
          </size>
         </property>
         <property name="text">
          <string>O</string>
-        </property>
-        <property name="maxLength">
-         <number>1</number>
         </property>
         <property name="clearButtonEnabled">
          <bool>false</bool>
@@ -399,15 +369,12 @@
        <widget class="Gui::PrefLineEdit" name="lineEdit_inCommandShortcutExit">
         <property name="maximumSize">
          <size>
-          <width>25</width>
+          <width>40</width>
           <height>16777215</height>
          </size>
         </property>
         <property name="text">
          <string>A</string>
-        </property>
-        <property name="maxLength">
-         <number>1</number>
         </property>
         <property name="clearButtonEnabled">
          <bool>false</bool>
@@ -431,15 +398,12 @@
        <widget class="Gui::PrefLineEdit" name="lineEdit_inCommandShortcutContinue">
         <property name="maximumSize">
          <size>
-          <width>25</width>
+          <width>40</width>
           <height>16777215</height>
          </size>
         </property>
         <property name="text">
          <string>N</string>
-        </property>
-        <property name="maxLength">
-         <number>1</number>
         </property>
         <property name="clearButtonEnabled">
          <bool>false</bool>
@@ -463,15 +427,12 @@
        <widget class="Gui::PrefLineEdit" name="lineEdit_inCommandShortcutCycleSnap">
         <property name="maximumSize">
          <size>
-          <width>25</width>
+          <width>40</width>
           <height>16777215</height>
          </size>
         </property>
         <property name="text">
          <string notr="true">`</string>
-        </property>
-        <property name="maxLength">
-         <number>1</number>
         </property>
         <property name="clearButtonEnabled">
          <bool>false</bool>
@@ -495,15 +456,12 @@
        <widget class="Gui::PrefLineEdit" name="lineEdit_inCommandShortcutAddHold">
         <property name="maximumSize">
          <size>
-          <width>25</width>
+          <width>40</width>
           <height>16777215</height>
          </size>
         </property>
         <property name="text">
          <string>Q</string>
-        </property>
-        <property name="maxLength">
-         <number>1</number>
         </property>
         <property name="clearButtonEnabled">
          <bool>false</bool>
@@ -527,15 +485,12 @@
        <widget class="Gui::PrefLineEdit" name="lineEdit_inCommandShortcutSetWP">
         <property name="maximumSize">
          <size>
-          <width>25</width>
+          <width>40</width>
           <height>16777215</height>
          </size>
         </property>
         <property name="text">
          <string>U</string>
-        </property>
-        <property name="maxLength">
-         <number>1</number>
         </property>
         <property name="clearButtonEnabled">
          <bool>false</bool>
@@ -559,15 +514,12 @@
        <widget class="Gui::PrefLineEdit" name="lineEdit_inCommandShortcutSnap">
         <property name="maximumSize">
          <size>
-          <width>25</width>
+          <width>40</width>
           <height>16777215</height>
          </size>
         </property>
         <property name="text">
          <string>S</string>
-        </property>
-        <property name="maxLength">
-         <number>1</number>
         </property>
         <property name="clearButtonEnabled">
          <bool>false</bool>
@@ -591,15 +543,12 @@
        <widget class="Gui::PrefLineEdit" name="lineEdit_inCommandShortcutIncreaseRadius">
         <property name="maximumSize">
          <size>
-          <width>25</width>
+          <width>40</width>
           <height>16777215</height>
          </size>
         </property>
         <property name="text">
          <string notr="true">P</string>
-        </property>
-        <property name="maxLength">
-         <number>1</number>
         </property>
         <property name="clearButtonEnabled">
          <bool>false</bool>
@@ -623,15 +572,12 @@
        <widget class="Gui::PrefLineEdit" name="lineEdit_inCommandShortcutDecreaseRadius">
         <property name="maximumSize">
          <size>
-          <width>25</width>
+          <width>40</width>
           <height>16777215</height>
          </size>
         </property>
         <property name="text">
          <string notr="true">M</string>
-        </property>
-        <property name="maxLength">
-         <number>1</number>
         </property>
         <property name="clearButtonEnabled">
          <bool>false</bool>
@@ -655,15 +601,12 @@
        <widget class="Gui::PrefLineEdit" name="lineEdit_inCommandShortcutRestrictX">
         <property name="maximumSize">
          <size>
-          <width>25</width>
+          <width>40</width>
           <height>16777215</height>
          </size>
         </property>
         <property name="text">
          <string>X</string>
-        </property>
-        <property name="maxLength">
-         <number>1</number>
         </property>
         <property name="clearButtonEnabled">
          <bool>false</bool>
@@ -687,15 +630,12 @@
        <widget class="Gui::PrefLineEdit" name="lineEdit_inCommandShortcutRestrictY">
         <property name="maximumSize">
          <size>
-          <width>25</width>
+          <width>40</width>
           <height>16777215</height>
          </size>
         </property>
         <property name="text">
          <string>Y</string>
-        </property>
-        <property name="maxLength">
-         <number>1</number>
         </property>
         <property name="clearButtonEnabled">
          <bool>false</bool>
@@ -719,15 +659,12 @@
        <widget class="Gui::PrefLineEdit" name="lineEdit_inCommandShortcutRestrictZ">
         <property name="maximumSize">
          <size>
-          <width>25</width>
+          <width>40</width>
           <height>16777215</height>
          </size>
         </property>
         <property name="text">
          <string>Z</string>
-        </property>
-        <property name="maxLength">
-         <number>1</number>
         </property>
         <property name="clearButtonEnabled">
          <bool>false</bool>
@@ -751,15 +688,12 @@
        <widget class="Gui::PrefLineEdit" name="lineEdit_inCommandShortcutRecenter">
         <property name="maximumSize">
          <size>
-          <width>25</width>
+          <width>40</width>
           <height>16777215</height>
          </size>
         </property>
         <property name="text">
          <string>D</string>
-        </property>
-        <property name="maxLength">
-         <number>1</number>
         </property>
         <property name="clearButtonEnabled">
          <bool>false</bool>

commit bebe5278bfe2fa9990a1a194c50cf95d5b6a4a98

Draft: fix input cursor problem caused by #26950
Fixes a regression caused by #26950 posted on the forum:
https://forum.freecad.org/viewtopic.php?t=104143

Key press events (such as from the cursor keys) can have an empty `event.text()`. PR #26950 changed the code from `event.text().upper() ==` to `event.text().upper() in`. The latter also catches empty texts leading to cursor key events being swallowed.

The problem has been fixed by adding a non-empty string check.

src/Mod/Draft/DraftGui.py
@@ -71,8 +71,10 @@ class DraftBaseWidget(QtWidgets.QWidget):
         super().__init__(parent)
 
     def eventFilter(self, widget, event):
-        if event.type() == QtCore.QEvent.KeyPress and event.text().upper() in _get_incmd_shortcut(
-            "CycleSnap"
+        if (
+            event.type() == QtCore.QEvent.KeyPress
+            and event.text()
+            and event.text()[0].upper() in _get_incmd_shortcut("CycleSnap")
         ):
             if hasattr(FreeCADGui, "Snapper"):
                 FreeCADGui.Snapper.cycleSnapObject()