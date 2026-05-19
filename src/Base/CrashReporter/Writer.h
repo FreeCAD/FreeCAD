d2085d646bd3156dccfdf06f9f36d76ab32d8140
Chris Hennes <chennes@gmail.com>
2026-05-19 15:48:46 -0500

    Build: Add optional dependency on cpptrace

---
+++ src/Base/CrashReporter/Writer.h
@@ -0,0 +1,18 @@
+//
+// Created by chennes on 6/7/26.
+//
+
+#ifndef FREECAD_WRITER_H
+#define FREECAD_WRITER_H
+
+namespace Base {
+namespace CrashReporter {
+
+class Writer {
+
+};
+
+} // CrashReporter
+} // Base
+
+#endif //FREECAD_WRITER_H
