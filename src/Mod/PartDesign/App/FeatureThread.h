07eeb571c3d7e4d76033e148f4850884259a0174
caio-venancio <caio.venancio784@gmail.com>
2026-05-26 15:07:04 -0300

    PartDesign: Add major new feature files
    This commit adds the files required to setup a new feature in PartDesign, in FreeCAD. It does not implement the feature, but it makes the feature testable by linking the new files in build.

--- src/Mod/PartDesign/App/FeatureThread.h
+++ src/Mod/PartDesign/App/FeatureThread.h
@@ -2,16 +2,36 @@
 
 #pragma once
 
-#include "FeatureSketchBased.h"
+#include <App/PropertyStandard.h>
+#include <App/PropertyUnits.h>
+
+#include "FeatureDressUp.h"
 
 namespace PartDesign
 {
 
-class PartDesignExport Thread: public ProfileBased
+class PartDesignExport Thread: public DressUp
 {
+    PROPERTY_HEADER_WITH_OVERRIDE(PartDesign::Thread);
 
 public:
     Thread();
+
+    App::PropertyEnumeration ThreadFit;
+    App::PropertyLength ThreadDiameter;
+    App::PropertyEnumeration ThreadDirection;
+
+    /** @name methods override feature */
+    //@{
+    /// recalculate the feature
+    App::DocumentObjectExecReturn* execute() override;
+    // short mustExecute() const override;
+    /// returns the type name of the view provider
+    // const char* getViewProviderName() const override
+    // {
+        // return "PartDesignGui::ViewProviderThread";
+    // }
+    //@}
 
 protected:
     void onChanged(const App::Property* prop) override;
