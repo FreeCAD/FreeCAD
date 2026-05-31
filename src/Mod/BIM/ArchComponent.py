4c02357789ef6d23c3833e4105ff157aa17ef373
Roy-043 <70520633+Roy-043@users.noreply.github.com>
2026-05-31 09:18:28 +0200

    BIM: fix faulty Presets paths (#29913)
    * BIM: fix faulty Presets paths
    
    * BIM: fix faulty Presets paths
    
    (cherry picked from commit dda08980055fa9e1a0dcc30da7e8757c47fc73f8)

--- src/Mod/BIM/ArchComponent.py
+++ src/Mod/BIM/ArchComponent.py
@@ -2426,7 +2426,7 @@ class ComponentTaskPanel:
         ]
         self.psetdefs = {}
         psetspath = os.path.join(
-            FreeCAD.getResourceDir(), "Mod", "Arch", "Presets", "pset_definitions.csv"
+            FreeCAD.getResourceDir(), "Mod", "BIM", "Presets", "pset_definitions.csv"
         )
         if os.path.exists(psetspath):
             with open(psetspath, "r") as csvfile:
