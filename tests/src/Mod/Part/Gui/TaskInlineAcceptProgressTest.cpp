0269de96805323a726e87d8bf0b16b1bc6a57e78
Joao Matos <joao@tritao.eu>
2026-05-31 18:42:43 +0100

    Tests: cover Part inline accept progress broadly

--- tests/src/Mod/Part/Gui/TaskInlineAcceptProgressTest.cpp
+++ tests/src/Mod/Part/Gui/TaskInlineAcceptProgressTest.cpp
@@ -22,6 +22,7 @@
 #include <QProgressDialog>
 #include <QTest>
 #include <QTimer>
+#include <QTreeWidget>
 
 #include <App/Application.h>
 #include <App/Document.h>
@@ -36,10 +37,14 @@
 #include <Gui/QuantitySpinBox.h>
 #include <Gui/TaskView/TaskDialog.h>
 #include <Gui/TaskView/TaskView.h>
+#include <Gui/Widgets.h>
 #include <Mod/Part/App/FeatureOffset.h>
 #include <Mod/Part/App/FeaturePartBox.h>
 #include <Mod/Part/App/PartFeatures.h>
+#include <Mod/Part/App/PrimitiveFeature.h>
+#include <Mod/Part/Gui/TaskLoft.h>
 #include <Mod/Part/Gui/TaskOffset.h>
+#include <Mod/Part/Gui/TaskSweep.h>
 #include <Mod/Part/Gui/TaskThickness.h>
 
 #include <src/App/InitApplication.h>
@@ -70,6 +75,22 @@ class BlockingThicknessTest: public Part::Thickness
 public:
     BlockingThicknessTest() = default;
     ~BlockingThicknessTest() override = default;
+
+    static void resetBlocker();
+    static void armBlocker();
+    static int getExecutionCount();
+    static void releaseBlocker();
+
+    App::DocumentObjectExecReturn* execute() override;
+};
+
+class BlockingLineTest: public Part::Line
+{
+    PROPERTY_HEADER_WITH_OVERRIDE(Part::BlockingLineTest);
+
+public:
+    BlockingLineTest() = default;
+    ~BlockingLineTest() override = default;
 
     static void resetBlocker();
     static void armBlocker();
@@ -205,6 +226,12 @@ BlockingExecutionState& blockingThicknessState()
     return state;
 }
 
+BlockingExecutionState& blockingLineState()
+{
+    static BlockingExecutionState state;
+    return state;
+}
+
 template<typename TaskWidget>
 TaskWidget* findTaskWidget(Gui::TaskView::TaskDialog* dialog)
 {
@@ -294,6 +321,18 @@ void acceptWithInlineProgress(
                 && statusLabel->text().contains(expectedStatus)) {
                 sawInlineAcceptStatus = true;
             }
+
+            auto* acceptStatusLabel = taskWidgetGuard->findChild<QLabel*>(
+                QStringLiteral("fcAsyncAcceptProgressLabel")
+            );
+            auto* acceptStatusWidget = taskWidgetGuard->findChild<QWidget*>(
+                QStringLiteral("fcAsyncAcceptProgress")
+            );
+            if (acceptStatusLabel && acceptStatusWidget && !acceptStatusLabel->isHidden()
+                && !acceptStatusWidget->isHidden()
+                && acceptStatusLabel->text().contains(expectedStatus)) {
+                sawInlineAcceptStatus = true;
+            }
         }
 
         auto* activeButtonBox = findActiveDialogButtonBox();
@@ -344,6 +383,67 @@ void acceptWithInlineProgress(
         sawDisabledDialogButtons,
         "Part task accept should disable task dialog buttons until final recompute settles"
     );
+}
+
+Gui::ActionSelector* findActionSelector(QWidget* taskWidget)
+{
+    return taskWidget ? taskWidget->findChild<Gui::ActionSelector*>(QStringLiteral("selector"))
+                      : nullptr;
+}
+
+void moveAvailableObjectToSelected(QWidget* taskWidget, const char* objectName)
+{
+    auto* selector = findActionSelector(taskWidget);
+    QVERIFY(selector != nullptr);
+
+    auto* available = selector->availableTreeWidget();
+    auto* selected = selector->selectedTreeWidget();
+    QVERIFY(available != nullptr);
+    QVERIFY(selected != nullptr);
+
+    const QString expectedName = QString::fromLatin1(objectName);
+    for (int i = 0; i < available->topLevelItemCount(); ++i) {
+        auto* item = available->topLevelItem(i);
+        if (item && item->data(0, Qt::UserRole).toString() == expectedName) {
+            item = available->takeTopLevelItem(i);
+            selected->addTopLevelItem(item);
+            selected->setCurrentItem(item);
+            return;
+        }
+    }
+
+    QFAIL(qPrintable(
+        QStringLiteral("Object %1 was not available in the task selector").arg(expectedName)
+    ));
+}
+
+void setCreateSolid(QWidget* taskWidget, bool createSolid)
+{
+    auto* checkSolid = taskWidget ? taskWidget->findChild<QCheckBox*>(QStringLiteral("checkSolid"))
+                                  : nullptr;
+    QVERIFY(checkSolid != nullptr);
+    checkSolid->setChecked(createSolid);
+    QCoreApplication::processEvents();
+}
+
+void setFrenet(QWidget* taskWidget, bool frenet)
+{
+    auto* checkFrenet = taskWidget ? taskWidget->findChild<QCheckBox*>(QStringLiteral("checkFrenet"))
+                                   : nullptr;
+    QVERIFY(checkFrenet != nullptr);
+    checkFrenet->setChecked(frenet);
+    QCoreApplication::processEvents();
+}
+
+void setLine(Part::Line* line, double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    QVERIFY(line != nullptr);
+    line->X1.setValue(x1);
+    line->Y1.setValue(y1);
+    line->Z1.setValue(z1);
+    line->X2.setValue(x2);
+    line->Y2.setValue(y2);
+    line->Z2.setValue(z2);
 }
 
 }  // namespace
@@ -434,7 +534,34 @@ App::DocumentObjectExecReturn* Part::BlockingThicknessTest::execute()
     });
 }
 
-class testTaskOffsetThickness final: public QObject
+PROPERTY_SOURCE(Part::BlockingLineTest, Part::Line)
+
+void Part::BlockingLineTest::resetBlocker()
+{
+    blockingLineState().reset();
+}
+
+void Part::BlockingLineTest::armBlocker()
+{
+    blockingLineState().arm();
+}
+
+int Part::BlockingLineTest::getExecutionCount()
+{
+    return blockingLineState().getExecutionCount();
+}
+
+void Part::BlockingLineTest::releaseBlocker()
+{
+    blockingLineState().release();
+}
+
+App::DocumentObjectExecReturn* Part::BlockingLineTest::execute()
+{
+    return blockingLineState().execute([this]() { return Part::Line::execute(); });
+}
+
+class testTaskInlineAcceptProgress final: public QObject
 {
     Q_OBJECT
 
@@ -443,9 +570,11 @@ private Q_SLOTS:
     {
         tests::initApplication();
         Base::Interpreter().runString("import Part");
-        Q_UNUSED(
-            ensureGuiHarness({&Part::BlockingOffsetTest::init, &Part::BlockingThicknessTest::init})
-        );
+        Q_UNUSED(ensureGuiHarness(
+            {&Part::BlockingOffsetTest::init,
+             &Part::BlockingThicknessTest::init,
+             &Part::BlockingLineTest::init}
+        ));
 
         asyncParams = App::GetApplication().GetParameterGroupByPath(
             "User parameter:BaseApp/Preferences/Document"
@@ -474,12 +603,15 @@ private Q_SLOTS:
 
     void init()  // NOLINT
     {
-        Q_UNUSED(
-            ensureGuiHarness({&Part::BlockingOffsetTest::init, &Part::BlockingThicknessTest::init})
-        );
+        Q_UNUSED(ensureGuiHarness(
+            {&Part::BlockingOffsetTest::init,
+             &Part::BlockingThicknessTest::init,
+             &Part::BlockingLineTest::init}
+        ));
 
         Part::BlockingOffsetTest::resetBlocker();
         Part::BlockingThicknessTest::resetBlocker();
+        Part::BlockingLineTest::resetBlocker();
 
         docName = App::GetApplication().getUniqueDocumentName("part_inline_accept_progress");
         App::DocumentInitFlags initFlags;
@@ -504,6 +636,7 @@ private Q_SLOTS:
     {
         Part::BlockingOffsetTest::releaseBlocker();
         Part::BlockingThicknessTest::releaseBlocker();
+        Part::BlockingLineTest::releaseBlocker();
         QCoreApplication::processEvents();
 
         if (doc && Gui::Control().activeDialog(doc)) {
@@ -593,6 +726,82 @@ private Q_SLOTS:
         QCOMPARE(Part::BlockingThicknessTest::getExecutionCount(), 1);
     }
 
+    void loftAcceptUsesInlineProgressInsteadOfModalDialog()  // NOLINT
+    {
+        auto* blockingProfile = doc->addObject<Part::BlockingLineTest>("BlockingLoftProfile");
+        QVERIFY(blockingProfile != nullptr);
+        setLine(blockingProfile, 0.0, 0.0, 0.0, 10.0, 0.0, 0.0);
+
+        auto* secondProfile = doc->addObject<Part::Line>("LoftProfile");
+        QVERIFY(secondProfile != nullptr);
+        setLine(secondProfile, 0.0, 0.0, 5.0, 10.0, 0.0, 5.0);
+        doc->recompute();
+
+        auto* dialog = new PartGui::TaskLoft();
+        QPointer<PartGui::TaskLoft> guard(dialog);
+        Gui::Control().showDialog(dialog, doc);
+        QCoreApplication::processEvents();
+
+        auto* taskWidget = findTaskWidget<PartGui::LoftWidget>(dialog);
+        QVERIFY(taskWidget != nullptr);
+        setCreateSolid(taskWidget, false);
+        moveAvailableObjectToSelected(taskWidget, blockingProfile->getNameInDocument());
+        moveAvailableObjectToSelected(taskWidget, secondProfile->getNameInDocument());
+
+        Part::BlockingLineTest::armBlocker();
+        blockingProfile->enforceRecompute();
+
+        acceptWithInlineProgress(doc, taskWidget, QStringLiteral("Computing loft"), []() {
+            Part::BlockingLineTest::releaseBlocker();
+        });
+
+        QTRY_VERIFY_WITH_TIMEOUT(guard.isNull(), 3000);
+        QCOMPARE(Gui::Control().activeDialog(doc), nullptr);
+        QVERIFY(!guiDoc->hasPendingCommand());
+        QCOMPARE(Part::BlockingLineTest::getExecutionCount(), 1);
+        QVERIFY(doc->getObject("Loft") != nullptr);
+    }
+
+    void sweepAcceptUsesInlineProgressInsteadOfModalDialog()  // NOLINT
+    {
+        auto* blockingProfile = doc->addObject<Part::BlockingLineTest>("BlockingSweepProfile");
+        QVERIFY(blockingProfile != nullptr);
+        setLine(blockingProfile, 0.0, 0.0, 0.0, 0.0, 2.0, 0.0);
+
+        auto* sweepPath = doc->addObject<Part::Line>("SweepPath");
+        QVERIFY(sweepPath != nullptr);
+        setLine(sweepPath, 0.0, 0.0, 0.0, 5.0, 0.0, 0.0);
+        doc->recompute();
+
+        auto* dialog = new PartGui::TaskSweep();
+        QPointer<PartGui::TaskSweep> guard(dialog);
+        Gui::Control().showDialog(dialog, doc);
+        QCoreApplication::processEvents();
+
+        auto* taskWidget = findTaskWidget<PartGui::SweepWidget>(dialog);
+        QVERIFY(taskWidget != nullptr);
+        setCreateSolid(taskWidget, false);
+        setFrenet(taskWidget, false);
+        moveAvailableObjectToSelected(taskWidget, blockingProfile->getNameInDocument());
+
+        Gui::Selection().clearSelection();
+        Gui::Selection().addSelection(docName.c_str(), sweepPath->getNameInDocument());
+        QCoreApplication::processEvents();
+
+        Part::BlockingLineTest::armBlocker();
+        blockingProfile->enforceRecompute();
+
+        acceptWithInlineProgress(doc, taskWidget, QStringLiteral("Computing sweep"), []() {
+            Part::BlockingLineTest::releaseBlocker();
+        });
+
+        QTRY_VERIFY_WITH_TIMEOUT(guard.isNull(), 3000);
+        QCOMPARE(Gui::Control().activeDialog(doc), nullptr);
+        QVERIFY(!guiDoc->hasPendingCommand());
+        QCOMPARE(Part::BlockingLineTest::getExecutionCount(), 1);
+        QVERIFY(doc->getObject("Sweep") != nullptr);
+    }
+
 private:
     Base::Reference<ParameterGrp> asyncParams;
     bool oldAsyncEnabled = true;
@@ -606,10 +815,10 @@ private:
     Part::BlockingThicknessTest* thickness = nullptr;
 };
 
-extern "C" Q_DECL_EXPORT int runTaskOffsetThicknessTest(int argc, char** argv)
+extern "C" Q_DECL_EXPORT int runTaskInlineAcceptProgressTest(int argc, char** argv)
 {
-    testTaskOffsetThickness test;
+    testTaskInlineAcceptProgress test;
     return QTest::qExec(&test, argc, argv);
 }
 
-#include "TaskOffsetThicknessTest.moc"
+#include "TaskInlineAcceptProgressTest.moc"
