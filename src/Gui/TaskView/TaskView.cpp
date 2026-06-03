23c39beea4819d04f01edf564faf41e30ddb603a
kevenwyld <keven@wyld.space>
2026-06-02 22:43:28 -0600

    use isHidden() which reflects only explicitly hidden widgets

--- src/Gui/TaskView/TaskView.cpp
+++ src/Gui/TaskView/TaskView.cpp
@@ -201,7 +201,7 @@ void TaskBox::hideGroupBox()
 
 bool TaskBox::isGroupVisible() const
 {
-    return myGroup->isVisible() || m_foldDirection == 1;
+    return !myGroup->isHidden() || m_foldDirection == 1;
 }
 
 void TaskBox::actionEvent(QActionEvent* e)
