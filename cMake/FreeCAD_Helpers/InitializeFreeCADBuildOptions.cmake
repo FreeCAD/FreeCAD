043d20ef2f17417c417fbb985bf6a1e91be52dc3
Louis Gombert <louisgombert@gmail.com>
2026-06-07 18:26:40 +0200

    Remove unused Cloud module

--- cMake/FreeCAD_Helpers/InitializeFreeCADBuildOptions.cmake
+++ cMake/FreeCAD_Helpers/InitializeFreeCADBuildOptions.cmake
@@ -177,7 +177,6 @@ macro(InitializeFreeCADBuildOptions)
     option(BUILD_WEB "Build the FreeCAD Web module" ON)
     option(BUILD_SURFACE "Build the FreeCAD surface module" ON)
     option(BUILD_VR "Build the FreeCAD Oculus Rift support (need Oculus SDK 4.x or higher)" OFF)
-    option(BUILD_CLOUD "Build the FreeCAD cloud module" OFF)
     option(ENABLE_DEVELOPER_TESTS "Build the FreeCAD unit tests suit" ON)
 
     if(MSVC OR APPLE)
