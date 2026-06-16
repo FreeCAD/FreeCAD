526fc8e68c352120fedc8380bdf0966d4cba54f3
Roy-043 <70520633+Roy-043@users.noreply.github.com>
2026-06-16 10:19:02 +0200

    Rename test file in TestDraftGui.py

--- src/Mod/Draft/TestDraftGui.py
+++ src/Mod/Draft/TestDraftGui.py
@@ -102,7 +102,7 @@ from drafttests.test_import_tools import DraftImportTools as DraftTestGui02
 from drafttests.test_pivy import DraftPivy as DraftTestGui03
 from drafttests.test_dimension_gui import DraftGuiDimension as DraftTestGui04
 from drafttests.test_manual_input_gui import DraftGuiManualInput as DraftTestGui05
-from drafttests.test_gui_lines import DraftGuiLines as DraftTestGui06
+from drafttests.test_lines_gui import DraftGuiLines as DraftTestGui06
 
 # Use the modules so that code checkers don't complain (flake8)
 True if DraftTestGui01 else False
