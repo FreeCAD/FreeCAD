5bf1d4051c8eefa243e6f94a1e654571cd7384f6
Florian Foinant-Willig <flachyjoe@gmail.com>
2026-06-27 12:13:53 +0200

    PartDesign:add result quality requirement

--- src/Mod/PartDesign/App/Feature.cpp
+++ src/Mod/PartDesign/App/Feature.cpp
@@ -23,6 +23,7 @@
  ***************************************************************************/
 
 
+#include <BRepAlgo.hxx>
 #include <BRep_Tool.hxx>
 #include <BRepBuilderAPI_MakeFace.hxx>
 #include <BRepCheck_Solid.hxx>
@@ -100,7 +101,13 @@ App::DocumentObjectExecReturn* Feature::recompute()
     }
 
     SuppressedShape.setValue(TopoShape());
-    return Part::Feature::recompute();
+    auto ret = Part::Feature::recompute();
+    if (!ret && !BRepAlgo::IsValid(Shape.getValue())) {
+        return new App::DocumentObjectExecReturn(
+            QT_TRANSLATE_NOOP("Exception", "Result shape is not valid")
+        );
+    }
+    return ret;
 }
 
 App::DocumentObjectExecReturn* Feature::recomputePreview()
