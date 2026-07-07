869d5755555d82c8b8f02477d2aee0fa91a31a58
Ladislav Michl <ladis@triops.cz>
2026-07-07 21:52:11 +0200

    Revert "Gui: Disable splashscreen messages by default"
    This reverts commit 5850c2e04b71 which is affordable since the printed
    messages do not introduce artificial delay anymore.

--- src/Gui/MainWindow.cpp
+++ src/Gui/MainWindow.cpp
@@ -2288,7 +2288,7 @@ void MainWindow::startSplasher()
         if (hGrp->GetBool("ShowSplasher", true)) {
             d->splashscreen = new SplashScreen(SplashScreen::splashImage());
 
-            if (!hGrp->GetBool("ShowSplasherMessages", false)) {
+            if (!hGrp->GetBool("ShowSplasherMessages", true)) {
                 d->splashscreen->setShowMessages(false);
             }
 
