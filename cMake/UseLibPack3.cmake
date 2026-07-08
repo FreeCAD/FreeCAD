5d741c12b7d4b673214284482f9f28a1eb513f21
Chris Hennes <chennes@pioneerlibrarysystem.org>
2026-07-07 21:05:31 -0500

    Build: Make sure that CMake can find executables in the LibPack's scripts directory

--- cMake/UseLibPack3.cmake
+++ cMake/UseLibPack3.cmake
@@ -1,6 +1,10 @@
 set(ENV{PATH} "${FREECAD_LIBPACK_DIR};$ENV{PATH}")
 list(PREPEND CMAKE_PREFIX_PATH "${FREECAD_LIBPACK_DIR}")
 
+# Python console-script entry points (e.g. mypy's stubgen) are installed under bin/Scripts, which is
+# not one of the directories find_program() searches by default. Add it so those tools are found.
+list(APPEND CMAKE_PROGRAM_PATH "${FREECAD_LIBPACK_DIR}/bin/Scripts")
+
 # Make really, really, REALLY sure that CMake doesn't do that thing where it decides that the LibPack's Python isn't
 # good enough, and just finds some other one to use. If there is a problem with the LibPack's Python, that is a fatal
 # error and should stop generation.
