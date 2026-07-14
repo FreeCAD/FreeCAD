FreeCAD/FreeCAD

commit f4665aa7b55f7f415d39ad71ee232a4dd8e01b30

Core: support multiple active transactions

src/Mod/TechDraw/Gui/TaskProjGroup.cpp
@@ -837,8 +837,8 @@ bool TaskProjGroup::reject()
     }
     else {
         //set the DPG and its views back to entry state.
-        if (Gui::Command::hasPendingCommand()) {
-            Gui::Command::abortCommand();
+        if (doc->hasPendingCommand()) {
+            doc->abortCommand();
         }
         // Restore views to initial spacing
         if (multiView) {
@@ -885,10 +885,10 @@ void TaskDlgProjGroup::open()
 {
     if (!widget->getCreateMode())  {    //this is an edit session, start a transaction
         if (dynamic_cast<TechDraw::DrawProjGroup*>(view)) {
-            App::GetApplication().setActiveTransaction("Edit Projection Group", true);
+            App::GetApplication().setActiveTransaction(App::TransactionName{.name="Edit Projection Group", .temporary=true});
         }
         else {
-            App::GetApplication().setActiveTransaction("Edit Part View", true);
+            App::GetApplication().setActiveTransaction(App::TransactionName{.name="Edit Part View", .temporary=true});
         }
     }
 }

commit 4be7dcab3b0b49f8aec69f6c1b603499e846a889

TD: Fix transaction status when creating ProjGroup
Commit f4665aa mistakenly left the trailing boolean parameter to
`setActiveTransaction` set to `true`, even though the sense of the
argument changed. This caused the bug reported in #30757.

Fixes #30757.

src/Mod/TechDraw/Gui/TaskProjGroup.cpp
@@ -885,10 +885,12 @@ void TaskDlgProjGroup::open()
 {
     if (!widget->getCreateMode())  {    //this is an edit session, start a transaction
         if (dynamic_cast<TechDraw::DrawProjGroup*>(view)) {
-            App::GetApplication().setActiveTransaction(App::TransactionName{.name="Edit Projection Group", .temporary=true});
+            App::GetApplication().setActiveTransaction(
+                App::TransactionName {.name = "Edit Projection Group", .temporary = false});
         }
         else {
-            App::GetApplication().setActiveTransaction(App::TransactionName{.name="Edit Part View", .temporary=true});
+            App::GetApplication().setActiveTransaction(
+                App::TransactionName {.name = "Edit Part View", .temporary = false});
         }
     }
 }