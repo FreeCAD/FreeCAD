d9a61ce06046828f866abfcff98507fe9d6cef46
wwmayer <wmayer@freecad.org>
2026-07-14 14:11:29 +0200

    CAM: Replace harmful use of eval()
    The method postProcessorBlacklist() reads the blacklisted processor
    types from the preferences that are stored as a string.
    To convert it back to a list of strings the function eval() is used
    which is considered harmful because it may lead to the execution of
    arbitrary code.
    
    To fix this vulnerability use the json module and its dumps() and
    loads() methods.

--- src/Mod/CAM/Path/Preferences.py
+++ src/Mod/CAM/Path/Preferences.py
@@ -25,6 +25,7 @@ import FreeCAD
 import Path
 import glob
 import importlib.util
+import json
 import os
 import pathlib
 from collections import defaultdict
@@ -470,16 +471,17 @@ def setJobDefaults(jobTemplate, geometryTolerance, curveAccuracy):
 def postProcessorBlacklist():
     pref = preferences()
     blacklist = pref.GetString(PostProcessorBlacklist, "")
-    if not blacklist:
+    try:
+        return json.loads(blacklist)
+    except Exception:
         return []
-    return eval(blacklist)
 
 
 def setPostProcessorDefaults(processor, args, blacklist):
     pref = preferences()
     pref.SetString(PostProcessorDefault, processor)
     pref.SetString(PostProcessorDefaultArgs, args)
-    pref.SetString(PostProcessorBlacklist, "%s" % (blacklist))
+    pref.SetString(PostProcessorBlacklist, json.dumps(blacklist))
 
 
 def setOutputFileDefaults(fileName, policy):
