3eaed6ad8a3ea65958167a9a65ee358e120e35e8
Chris Hennes <chennes@gmail.com>
2026-07-22 08:48:16 -0500

    Robot: Adapt escape for main repo
    (cherry picked from commit 0e0b70c5bc0cbc9b3ee0b38a6d8f204db7b29ffd)

--- src/Mod/Robot/Gui/CommandTrajectory.cpp
+++ src/Mod/Robot/Gui/CommandTrajectory.cpp
@@ -312,8 +312,8 @@ void CmdRobotSetDefaultValues::activated(int)
         Qt::MSWindowsFixedSizeDialogHint
     );
     if (ok && !text.isEmpty()) {
-        text = Base::Tools::escapeEncodeString(text);
-        doCommand(Doc, "_DefSpeed = '%s'", text.toUtf8().constData());
+        std::string encoded = Base::Tools::escapeEncodeString(text.toStdString());
+        doCommand(Doc, "_DefSpeed = '%s'", encoded.c_str());
     }
 
     QStringList items;
@@ -345,8 +345,8 @@ void CmdRobotSetDefaultValues::activated(int)
         Qt::MSWindowsFixedSizeDialogHint
     );
     if (ok && !text.isEmpty()) {
-        text = Base::Tools::escapeEncodeString(text);
-        doCommand(Doc, "_DefAcceleration = '%s'", text.toUtf8().constData());
+        std::string encoded = Base::Tools::escapeEncodeString(text.toStdString());
+        doCommand(Doc, "_DefAcceleration = '%s'", encoded.c_str());
     }
 
 
