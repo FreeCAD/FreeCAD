3f3220ffa40277674bf477f83ca20fbfedcd5873
paddle <pierrelouis.boyer@gmail.com>
2026-07-28 10:35:15 +0200

    TechDraw: Section view task rework

--- src/Mod/TechDraw/Gui/Widgets/CompassWidget.h
+++ src/Mod/TechDraw/Gui/Widgets/CompassWidget.h
@@ -30,7 +30,6 @@
 QT_BEGIN_NAMESPACE
 class QLabel;
 class QHBoxLayout;
-class QPushButton;
 class QVBoxLayout;
 QT_END_NAMESPACE
 
@@ -60,8 +59,6 @@ public:
     double value() const { return m_angle; }
     double positiveValue() { return m_angle < 0.0 ? m_angle + 360.0 : m_angle; }
     void setDialAngle(double newAngle);
-    void setAdvanceIncrement(double newIncrement);
-    double advanceIncrement() const { return m_advanceIncrement; }
 
 Q_SIGNALS:
     void angleChanged(double angle);
@@ -75,8 +72,7 @@ public Q_SLOTS:
     void setToNorth() { setDialAngle(90.0); }
     void setToWest() { setDialAngle(180.0); }
     void setToSouth() { setDialAngle(270.0); }
-    void slotCWAdvance();
-    void slotCCWAdvance();
+    void slotDialAngleSelected(double angle);
 
 protected:
     void paintEvent(QPaintEvent* event) override;
@@ -89,7 +85,6 @@ private:
     int m_minimumHeight;
     int m_defaultMargin;
     double m_angle;
-    double m_advanceIncrement;
 
     QVBoxLayout* compassLayout;
     QHBoxLayout* compassDialLayout;
@@ -99,8 +94,6 @@ private:
     //    DoubleSpinBoxNoEnter* dsbAngle;
     Gui::QuantitySpinBox* dsbAngle;
     QLabel* compassControlLabel;
-    QPushButton* pbCWAdvance;
-    QPushButton* pbCCWAdvance;
 };
 
 }//namespace TechDrawGui
