8ea1619a24960164156f64abf90b0d9fadd373f7
Joao Matos <joao@tritao.eu>
2026-08-04 16:50:18 +0100

    Gui: report invalid quantity edits

--- src/Gui/InputField.h
+++ src/Gui/InputField.h
@@ -161,8 +161,6 @@ public:
     /// set the number portion selected (use after setValue())
     void selectNumber();
     /// input validation
-    void fixup(QString& input) const;
-    /// input validation
     QValidator::State validate(QString& input, int& pos) const;
 
     /** @name history and default management */
@@ -223,6 +221,7 @@ private:
     QPixmap getValidationIcon(const char* name, const QSize& size) const;
     void updateText(const Base::Quantity&);
     void notifyValueChanged();
+    void commitInput();
 
 private:
     QByteArray m_sPrefGrp;
