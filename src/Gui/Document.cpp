da0c9e19c262c4db1104eabbb88c02d73b17e9d6
Krish Sharma <777krrish@gmail.com>
2026-08-05 11:25:57 +0530

    Update src/Gui/Document.cpp
    Co-authored-by: Chris Hennes <chennes@pioneerlibrarysystem.org>

--- src/Gui/Document.cpp
+++ src/Gui/Document.cpp
@@ -1707,7 +1707,7 @@ bool Document::save()
         }
         catch (const Base::FileException& e) {
             e.reportException();
-            return askIfSavingFailed(QString::fromUtf8(e.getTranslatedMessage().c_str()));
+            return askIfSavingFailed(QString::fromStdString(e.getTranslatedMessage()));
         }
         catch (const Base::Exception& e) {
             QMessageBox::critical(
