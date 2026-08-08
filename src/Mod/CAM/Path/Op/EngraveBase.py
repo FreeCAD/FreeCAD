c12f4d622e9f3053c22b58f28a83007a9ff3305c
tarman3 <joice501790018@tutamail.com>
2026-08-08 20:20:07 +0300

    CAM: Engrave - fix for wire with one edge

--- src/Mod/CAM/Path/Op/EngraveBase.py
+++ src/Mod/CAM/Path/Op/EngraveBase.py
@@ -203,13 +203,13 @@ class ObjectOp(PathOp.ObjectOp):
                         flip = reverseDir
                     elif Path.Geom.pointsCoincide(lastPoint, edge.valueAt(edge.FirstParameter)):
                         flip = False
-                        lastPoint = edge.valueAt(edge.LastParameter)
                     elif Path.Geom.pointsCoincide(lastPoint, edge.valueAt(edge.LastParameter)):
                         flip = True
-                        lastPoint = edge.valueAt(edge.FirstParameter)
                     else:
                         Path.Log.warning("Error while checks points coincide")
                         return
+
+                    lastPoint = edge.valueAt(edge.FirstParameter if flip else edge.LastParameter)
 
                     for cmd in Path.Geom.cmdsForEdge(edge, flip=flip, tol=tol):
                         # Add gcode for edge
