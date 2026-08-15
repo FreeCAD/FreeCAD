942ce72f7688fba5b6e4746e505de266ae9d91bc
shubhrai23 <shubhrai0205@gmail.com>
2026-08-16 03:55:59 +0530

    Fix empty tooltips in Material Editor Appearance

--- src/Mod/Material/Gui/MaterialsEditor.cpp
+++ src/Mod/Material/Gui/MaterialsEditor.cpp
@@ -367,6 +367,58 @@ void MaterialsEditor::propertyChange(const QString& property, const QVariant& va
     else if (_material->hasAppearanceProperty(property)) {
         _material->setAppearanceValue(property, value);
         updatePreview();
+
+        auto treeModel = qobject_cast<QStandardItemModel*>(ui->treeAppearance->model());
+        if (treeModel) {
+            auto models = _material->getAppearanceModels();
+            if (models) {
+                for (const QString& uuid : *models) {
+                    try {
+                        auto appModel = Materials::ModelManager::getManager().getModel(uuid);
+                        if (appModel) {
+                            for (auto itp = appModel->begin(); itp != appModel->end(); itp++) {
+                                if (itp->first == property) {
+                                    QString tooltip = itp->second.getDescription().trimmed();
+                                    QString valStr = _material->getAppearanceValueString(property);
+                                    
+                                    if (!valStr.trimmed().isEmpty()) {
+                                        if (itp->second.getPropertyType() == QStringLiteral("Color")) {
+                                            QString hexCode = getColorHash(valStr);
+                                            if (!tooltip.isEmpty()) {
+                                                tooltip += QStringLiteral("\n\n");
+                                            }
+                                            tooltip += QString::fromUtf8("Value: %1 %2").arg(hexCode).arg(valStr);
+                                        } else {
+                                            if (!tooltip.isEmpty()) {
+                                                tooltip += QStringLiteral("\n\n");
+                                            }
+                                            tooltip += QString::fromUtf8("Value: %1").arg(valStr);
+                                        }
+                                    }
+
+                                    for (int r = 0; r < treeModel->rowCount(); ++r) {
+                                        auto rootItem = treeModel->item(r);
+                                        if (rootItem) {
+                                            for (int c = 0; c < rootItem->rowCount(); ++c) {
+                                                auto propItem = rootItem->child(c, 0);
+                                                auto valItem = rootItem->child(c, 1);
+                                                if (propItem && valItem && propItem->data().toString() == property) {
+                                                    propItem->setToolTip(tooltip);
+                                                    valItem->setToolTip(tooltip);
+                                                    break;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    catch (Materials::ModelNotFound const&) {
+                    }
+                }
+            }
+        }
     }
     update();
 }
@@ -1117,12 +1169,23 @@ void MaterialsEditor::updateMaterialAppearance()
                     propertyItem->setData(key);
                     
                     QString valStr = _material->getAppearanceValueString(key);
-                    QString desc = itp->second.getDescription();
-                    QString tooltip = desc;
-                    if (tooltip.isEmpty()) {
-                        tooltip = QString::fromUtf8("Property: %1\nType: %2").arg(key).arg(itp->second.getPropertyType());
-                    }
+                    QString tooltip = itp->second.getDescription().trimmed();
                     
+                    if (!valStr.trimmed().isEmpty()) {
+                        if (itp->second.getPropertyType() == QStringLiteral("Color")) {
+                            QString hexCode = getColorHash(valStr);
+                            if (!tooltip.isEmpty()) {
+                                tooltip += QStringLiteral("\n\n");
+                            }
+                            tooltip += QString::fromUtf8("Value: %1 %2").arg(hexCode).arg(valStr);
+                        } else {
+                            if (!tooltip.isEmpty()) {
+                                tooltip += QStringLiteral("\n\n");
+                            }
+                            tooltip += QString::fromUtf8("Value: %1").arg(valStr);
+                        }
+                    }
+
                     propertyItem->setToolTip(tooltip);
                     items.append(propertyItem);
 
