c0143effc4dfea139808a2fa84f2224de65b3a69
marioalexis <mario.passaglia@gmail.com>
2026-08-18 03:47:09 -0300

    Fem: Add transform to control pipeline point scaling

--- src/Mod/Fem/App/FemPostObject.h
+++ src/Mod/Fem/App/FemPostObject.h
@@ -31,6 +31,7 @@
 
 #include <vtkBoundingBox.h>
 #include <vtkTransformFilter.h>
+#include <vtkTransform.h>
 #include <vtkSmartPointer.h>
 
 class vtkDataSet;
@@ -67,6 +68,7 @@ protected:
     // how this filter should be used to create data. This is to be implemented
     // by the derived classes.
     vtkSmartPointer<vtkTransformFilter> m_transform_filter;
+    vtkSmartPointer<vtkTransform> m_transform;
 
     void onChanged(const App::Property* prop) override;
 };
--- src/Mod/Fem/App/PropertyPostDataObject.h
+++ src/Mod/Fem/App/PropertyPostDataObject.h
@@ -47,8 +47,6 @@ public:
 
     /** @name Getter/setter */
     //@{
-    /// Scale the point coordinates of the data set with factor \a s
-    void scale(double s);
     /// set the dataset
     void setValue(const vtkSmartPointer<vtkDataObject>&);
     /// get the part shape
@@ -80,9 +78,6 @@ public:
 
     /// Get valid paths for this property; used by auto completer
     void getPaths(std::vector<App::ObjectIdentifier>& paths) const override;
-
-private:
-    static void scaleDataObject(vtkDataObject*, double s);
 
 protected:
     void createDataObjectByExternalType(vtkSmartPointer<vtkDataObject> ex);
