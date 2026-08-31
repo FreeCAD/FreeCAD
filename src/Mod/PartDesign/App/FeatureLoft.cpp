97dd02b8a7d453c3cc79e5ffd59fefcfad2d3deb
Peter Vonk <vonk@synthetik.ai>
2026-08-31 10:54:26 -0500

    change to ranges::stable_sort

--- src/Mod/PartDesign/App/FeatureLoft.cpp
+++ src/Mod/PartDesign/App/FeatureLoft.cpp
@@ -86,9 +86,7 @@ void sortWiresByNesting(std::vector<Part::TopoShape>& wires)
 
     // Keep the original ordering within the same depth (e.g., if multiple wires/loops exist within
     // an outer loop) because this doesn't determine correspondence between peers.
-    std::stable_sort(wireInfos.begin(), wireInfos.end(), [](const auto& left, const auto& right) {
-        return left.depth < right.depth;
-    });
+    std::ranges::stable_sort(wireInfos, {}, &WireInfo::depth);
     std::ranges::transform(wireInfos, wires.begin(), [](auto& info) { return std::move(info.wire); });
 }
 
