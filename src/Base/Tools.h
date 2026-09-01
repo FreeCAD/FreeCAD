a422fa6fe581e1d1bee499cdd457073a47084922
Céleste Wouters <foss@elementw.net>
2026-09-01 12:51:11 +0200

    Base: add std::unreachable polyfill

--- src/Base/Tools.h
+++ src/Base/Tools.h
@@ -492,4 +492,16 @@ constexpr auto to_underlying(E e) noexcept
     return static_cast<std::underlying_type_t<E>>(e);
 }
 
+#if MINIMUM_CPLUSPLUS_VERSION >= 202302L
+[[deprecated("Replace with std::unreachable() now that C++23 is required")]]
+#endif
+[[noreturn]] inline void unreachable()
+{
+#if defined(_MSC_VER) && !defined(__clang__)
+    __assume(false);
+#else
+    __builtin_unreachable();
+#endif
+}
+
 }  // namespace Base
