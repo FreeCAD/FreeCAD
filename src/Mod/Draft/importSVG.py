d142831ac0bfe633638cd016b47592a096fd26cb
Roy-043 <70520633+Roy-043@users.noreply.github.com>
2026-09-05 18:30:41 +0200

    Manual backport of Draft: flatten SVG faces (improved) (#32422)

--- src/Mod/Draft/importSVG.py
+++ src/Mod/Draft/importSVG.py
@@ -1013,6 +1013,9 @@ class svgHandler(xml.sax.ContentHandler):
                 sh = transformCopyShape(sh, self.transform)
             for transform in self.grouptransform[::-1]:
                 sh = transformCopyShape(sh, transform)
+            if sh.Faces:
+                # transformCopyShape results in a B-spline face, we want a planar face:
+                return Part.makeFace(sh.Wires, "Part::FaceMakerBullseye")
             return sh
         elif Draft.getType(sh) in ["Dimension", "LinearDimension"]:
             pts = []
