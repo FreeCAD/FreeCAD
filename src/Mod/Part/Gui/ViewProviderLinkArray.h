f11e7d4981c9d875fea5875b8cd7be227fc8aae6
paddle <pierrelouis.boyer@gmail.com>
2026-09-09 21:11:02 +0200

    PartGui: expose link array commands and editable view providers

---
+++ src/Mod/Part/Gui/ViewProviderLinkArray.h
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+/****************************************************************************
+ *   Copyright (c) 2026 Boyer Pierre-Louis <pierrelouis.boyer@gmail.com>    *
+ *                                                                          *
+ *   This file is part of FreeCAD.                                          *
+ *                                                                          *
+ *   FreeCAD is free software: you can redistribute it and/or modify it     *
+ *   under the terms of the GNU Lesser General Public License as            *
+ *   published by the Free Software Foundation, either version 2.1 of the   *
+ *   License, or (at your option) any later version.                        *
+ *                                                                          *
+ *   FreeCAD is distributed in the hope that it will be useful, but         *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU       *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU Lesser General Public       *
+ *   License along with FreeCAD. If not, see                                *
+ *   <https://www.gnu.org/licenses/>.                                       *
+ *                                                                          *
+ ***************************************************************************/
+
+#pragma once
+
+#include <QIcon>
+
+#include <Gui/ViewProviderLink.h>
+#include <Mod/Part/PartGlobal.h>
+
+namespace PartGui
+{
+
+class PartGuiExport ViewProviderLinkArray: public Gui::ViewProviderLink
+{
+    PROPERTY_HEADER_WITH_OVERRIDE(PartGui::ViewProviderLinkArray);
+
+public:
+    ViewProviderLinkArray();
+    ~ViewProviderLinkArray() override;
+
+    bool doubleClicked() override;
+    QIcon getIcon() const override;
+};
+
+}  // namespace PartGui
