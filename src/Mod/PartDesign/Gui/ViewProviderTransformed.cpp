6c9376fa0033e7a7f3b44c4ace94d79d80f4bf65
paddle <pierrelouis.boyer@gmail.com>
2026-09-09 21:07:36 +0200

    PartDesign: suppress individual transformed occurrences and previews

--- src/Mod/PartDesign/Gui/ViewProviderTransformed.cpp
+++ src/Mod/PartDesign/Gui/ViewProviderTransformed.cpp
@@ -118,7 +118,13 @@ void ViewProviderTransformed::updatePreview()
 
             Gui::coinRemoveAllChildren(pcPreviewRoot);
 
+            int index = 0;
             for (const auto& transform : transforms) {
+                if (feature->isTransformationSuppressed(index)) {
+                    ++index;
+                    continue;
+                }
+
                 Base::Matrix4D transformMatrix;
                 Part::TopoShape::convertToMatrix(transform, transformMatrix);
 
@@ -131,6 +137,7 @@ void ViewProviderTransformed::updatePreview()
                 sep->addChild(pcPreviewShape);
 
                 pcPreviewRoot->addChild(sep);
+                ++index;
             }
         }
     }
