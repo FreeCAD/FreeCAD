5e63db7d15e3812c752399eced5861cdd4905475
Chris Hennes <chennes@pioneerlibrarysystem.org>
2026-09-09 22:18:36 -0500

    FEM: SPDX cleanup

--- src/Mod/Fem/App/VTKExtensions/vtkSMPTools.h
+++ src/Mod/Fem/App/VTKExtensions/vtkSMPTools.h
@@ -1,19 +1,5 @@
+// SPDX-FileCopyrightText: Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
 // SPDX-License-Identifier: BSD-3-Clause
-
-/*=========================================================================
-
-  Program:   Visualization Toolkit
-  Module:    vtkSMPTools.h
-
-  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
-  All rights reserved.
-  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
-
-     This software is distributed WITHOUT ANY WARRANTY; without even
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-     PURPOSE.  See the above copyright notice for more information.
-
-=========================================================================*/
 /**
  * @class   vtkSMPTools
  * @brief   A set of parallel (multi-threaded) utility functions.
