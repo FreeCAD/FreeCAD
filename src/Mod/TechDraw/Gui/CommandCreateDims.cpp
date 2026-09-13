78e4038a564e4c8bfebb40119b41d67531232223
Niladri Bhowmik <bhowmikniladri414@gmail.com>
2026-09-13 17:43:34 +0530

    TechDraw: Fix incorrect ASME diameter dimension format (#32439) (#32492)

--- src/Mod/TechDraw/Gui/CommandCreateDims.cpp
+++ src/Mod/TechDraw/Gui/CommandCreateDims.cpp
@@ -2339,6 +2339,17 @@ DrawViewDimension* dimMaker(TechDraw::DrawViewPart* dvp, std::string dimType,
     dim->setReferences2d(references2d);
     dim->setReferences3d(references3d);
 
+    if (auto* vp = freecad_cast<ViewProviderDimension*>(Gui::Application::Instance->getViewProvider(dim))) {
+        if (dimType == "Diameter") {
+            if (vp->StandardAndStyle.getValue() == ViewProviderDimension::STD_STYLE_ASME_INLINED) {
+                vp->StandardAndStyle.setValue(ViewProviderDimension::STD_STYLE_ASME_REFERENCING);
+            }
+            if (vp->StandardAndStyle.getValue() == ViewProviderDimension::STD_STYLE_ASME_REFERENCING) {
+                vp->RenderingExtent.setValue(ViewProviderDimension::REND_EXTENT_REDUCED);
+            }
+        }
+    }
+
     Gui::Command::doCommand(Gui::Command::Doc,
                             "App.activeDocument().%s.addView(App.activeDocument().%s)",
                             PageName.c_str(),
