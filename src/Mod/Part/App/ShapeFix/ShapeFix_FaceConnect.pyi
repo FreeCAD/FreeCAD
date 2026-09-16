8b5b00408820b783acc837220e45e79d83a784cb
Frank Martinez <mnesarco@gmail.com>
2026-09-16 15:17:16 -0500

    [stubs] ShapeFix_FaceConnect: make FaceConnect Python stub signatures match the C++ binding

--- src/Mod/Part/App/ShapeFix/ShapeFix_FaceConnect.pyi
+++ src/Mod/Part/App/ShapeFix/ShapeFix_FaceConnect.pyi
@@ -4,6 +4,8 @@ from __future__ import annotations
 
 from Base.Metadata import export
 from Base.PyObjectBase import PyObjectBase
+from Part.TopoShapeFace import TopoShapeFace
+from Part.TopoShapeShell import TopoShapeShell
 
 @export(
     PythonName="Part.ShapeFix.FaceConnect",
@@ -19,13 +21,13 @@ class ShapeFix_FaceConnect(PyObjectBase):
     Licence: LGPL
     """
 
-    def add(self, face, /) -> None:
+    def add(self, face1: TopoShapeFace, face2: TopoShapeFace, /) -> None:
         """
-        add(face, face)
+        add(face1, face2)
         """
         ...
 
-    def build(self, shell, sewtolerance, fixtolerance, /) -> None:
+    def build(self, shell: TopoShapeShell, sewtolerance: float, fixtolerance: float, /) -> None:
         """
         build(shell, sewtolerance, fixtolerance)
         """
