FreeCAD/FreeCAD

commit d8cf415e4f6b2ed655af13a2f8cdfc00e2e463fb

Sketcher: replace solve() & friends' int return codes with enums
`GCS::SolveStatus` was already a thing for the actual solver engines'
main calls, but it was an unscoped enum and returned as `int`.
Sketch{,Object} either forwarded them, or sometimes introduced their own
set of negative integers with other meanings, making everything hard to
read and track.

Preserves the numerical values of the enum cases, as the Python API
returns them as a number. This ought to be revised one day but will mean
breaking the API.

src/Mod/Sketcher/App/Sketch.cpp
@@ -4955,7 +4955,7 @@ bool Sketch::updateNonDrivingConstraints()
 
 // solving ==========================================================
 
-int Sketch::solve()
+GCS::SolveStatus Sketch::solve()
 {
     captureGroupStates();
 
@@ -4977,50 +4977,50 @@ int Sketch::solve()
 
     SolveTime = Base::TimeElapsed::diffTimeF(start_time, end_time);
 
-    if (result == GCS::Success) {
+    if (result == GCS::SolveStatus::Success) {
         applyGroupTransformations();
     }
 
     return result;
 }
 
-int Sketch::internalSolve(std::string& solvername, int level)
+GCS::SolveStatus Sketch::internalSolve(std::string& solvername, int level)
 {
     if (!isInitMove) {  // make sure we are in single subsystem mode
         clearTemporaryConstraints();
         isFine = true;
     }
 
-    int ret = -1;
+    GCS::SolveStatus status;
     bool valid_solution;
     int defaultsoltype = -1;
 
     if (isInitMove) {
         solvername = "DogLeg";  // DogLeg is used for dragging (same as before)
-        ret = GCSsys.solve(isFine, GCS::DogLeg);
+        status = GCSsys.solve(isFine, GCS::DogLeg);
     }
     else {
         switch (defaultSolver) {
             case 0:
                 solvername = "BFGS";
-                ret = GCSsys.solve(isFine, GCS::BFGS);
+                status = GCSsys.solve(isFine, GCS::BFGS);
                 defaultsoltype = 2;
                 break;
             case 1:  // solving with the LevenbergMarquardt solver
                 solvername = "LevenbergMarquardt";
-                ret = GCSsys.solve(isFine, GCS::LevenbergMarquardt);
+                status = GCSsys.solve(isFine, GCS::LevenbergMarquardt);
                 defaultsoltype = 1;
                 break;
             case 2:  // solving with the BFGS solver
                 solvername = "DogLeg";
-                ret = GCSsys.solve(isFine, GCS::DogLeg);
+                status = GCSsys.solve(isFine, GCS::DogLeg);
                 defaultsoltype = 0;
                 break;
         }
     }
 
     // if successfully solved try to write the parameters back
-    if (ret == GCS::Success) {
+    if (status == GCS::SolveStatus::Success) {
         GCSsys.applySolution();
         valid_solution = updateGeometry();
         if (!valid_solution) {
@@ -5050,15 +5050,15 @@ int Sketch::internalSolve(std::string& solvername, int level)
             switch (soltype) {
                 case 0:
                     solvername = "DogLeg";
-                    ret = GCSsys.solve(isFine, GCS::DogLeg);
+                    status = GCSsys.solve(isFine, GCS::DogLeg);
                     break;
                 case 1:  // solving with the LevenbergMarquardt solver
                     solvername = "LevenbergMarquardt";
-                    ret = GCSsys.solve(isFine, GCS::LevenbergMarquardt);
+                    status = GCSsys.solve(isFine, GCS::LevenbergMarquardt);
                     break;
                 case 2:  // solving with the BFGS solver
                     solvername = "BFGS";
-                    ret = GCSsys.solve(isFine, GCS::BFGS);
+                    status = GCSsys.solve(isFine, GCS::BFGS);
                     break;
                 // last resort: augment the system with a second subsystem and use the SQP solver
                 case 3:
@@ -5074,19 +5074,19 @@ int Sketch::internalSolve(std::string& solvername, int level)
                         );
                     }
                     GCSsys.initSolution();
-                    ret = GCSsys.solve(isFine);
+                    status = GCSsys.solve(isFine);
                     break;
             }
 
             // if successfully solved try to write the parameters back
-            if (ret == GCS::Success) {
+            if (status == GCS::SolveStatus::Success) {
                 GCSsys.applySolution();
                 valid_solution = updateGeometry();
                 if (!valid_solution) {
                     GCSsys.undoSolution();
                     updateGeometry();
                     Base::Console().warning("Invalid solution from %s solver.\n", solvername.c_str());
-                    ret = GCS::SuccessfulSolutionInvalid;
+                    status = GCS::SolveStatus::SuccessfulSolutionInvalid;
                 }
                 else {
                     updateNonDrivingConstraints();
@@ -5136,11 +5136,11 @@ int Sketch::internalSolve(std::string& solvername, int level)
 
     // For OCCT reliant geometry that needs an extra solve() for example to update non-driving
     // constraints.
-    if (resolveAfterGeometryUpdated && ret == GCS::Success && level == 0) {
+    if (resolveAfterGeometryUpdated && status == GCS::SolveStatus::Success && level == 0) {
         return internalSolve(solvername, 1);
     }
 
-    return ret;
+    return status;
 }
 
 int Sketch::initMove(const std::vector<GeoElementId>& geoEltIds, bool fine)
@@ -5461,11 +5461,15 @@ int Sketch::initBSplinePieceMove(int geoId, PointPos pos, const Base::Vector3d& 
     return 0;
 }
 
-int Sketch::moveGeometries(const std::vector<GeoElementId>& geoEltIds, Base::Vector3d toPoint, bool relative)
+GCS::SolveStatus Sketch::moveGeometries(
+    const std::vector<GeoElementId>& geoEltIds,
+    Base::Vector3d toPoint,
+    bool relative
+)
 {
     if (hasConflicts()) {
         // don't try to move sketches that contain conflicting constraints
-        return -1;
+        return GCS::SolveStatus::Failed;
     }
 
     if (!isInitMove) {
@@ -5574,7 +5578,7 @@ int Sketch::moveGeometries(const std::vector<GeoElementId>& geoEltIds, Base::Vec
     return solve();
 }
 
-int Sketch::moveGeometry(int geoId, PointPos pos, Base::Vector3d toPoint, bool relative)
+GCS::SolveStatus Sketch::moveGeometry(int geoId, PointPos pos, Base::Vector3d toPoint, bool relative)
 {
     std::vector<GeoElementId> geoEltIds = {GeoElementId(geoId, pos)};
     return moveGeometries(geoEltIds, toPoint, relative);

src/Mod/Sketcher/App/Sketch.h
@@ -52,8 +52,10 @@ public:
     void Restore(Base::XMLReader& /*reader*/) override;
 
     /// solve the actual set up sketch
-    int solve();
-    /// resets the solver
+    GCS::SolveStatus solve();
+    /** Reset the solver.
+     * @return DoF degree count.
+     */
     int resetSolver();
     /// get standard (aka fine) solver precision
     double getSolverPrecision()
@@ -192,12 +194,12 @@ public:
      * a condition for satisfying the new point location!
      * The relative flag permits moving relatively to the current position
      */
-    int moveGeometries(
+    GCS::SolveStatus moveGeometries(
         const std::vector<GeoElementId>& geoEltIds,
         Base::Vector3d toPoint,
         bool relative = false
     );
-    int moveGeometry(int geoId, PointPos pos, Base::Vector3d toPoint, bool relative = false);
+    GCS::SolveStatus moveGeometry(int geoId, PointPos pos, Base::Vector3d toPoint, bool relative = false);
 
     /**
      * Sets whether the initial solution should be recalculated while dragging after a certain
@@ -810,7 +812,7 @@ private:
 
     void buildInternalAlignmentGeometryMap(const std::vector<Constraint*>& constraintList);
 
-    int internalSolve(std::string& solvername, int level = 0);
+    GCS::SolveStatus internalSolve(std::string& solvername, int level = 0);
 
     /// checks if the index bounds and converts negative indices to positive
     int checkGeoId(int geoId) const;

src/Mod/Sketcher/App/SketchAnalysis.cpp
@@ -618,17 +618,17 @@ Sketcher::Constraint* SketchAnalysis::create(const ConstraintIds& id)
 
 void SketchAnalysis::solveSketch(const char* errorText)
 {
-    int status {};
+    SketchSolveStatus status;
     int dofs {};
     solvesketch(status, dofs, true);
 
-    if (status == int(Solver::RedundantConstraints)) {
+    if (status == SketchSolveStatus::RedundantConstraints) {
         sketch->autoRemoveRedundants(DeleteOption::NoFlag);
 
         solvesketch(status, dofs, false);
     }
 
-    if (status) {
+    if (status != SketchSolveStatus::Success) {
         THROWMT(Base::RuntimeError, errorText);
     }
 }
@@ -832,7 +832,7 @@ void SketchAnalysis::makeMissingEqualityOneByOne()
     radiusequalityConstraints.clear();
 }
 
-void SketchAnalysis::solvesketch(int& status, int& dofs, bool updategeo)
+void SketchAnalysis::solvesketch(SketchSolveStatus& status, int& dofs, bool updategeo)
 {
     status = sketch->solve(updategeo);
 
@@ -844,14 +844,14 @@ void SketchAnalysis::solvesketch(int& status, int& dofs, bool updategeo)
     }
 
     if (sketch->getLastHasRedundancies()) {
-        status = int(Solver::RedundantConstraints);
+        status = SketchSolveStatus::RedundantConstraints;
     }
 
     if (dofs < 0) {
-        status = int(Solver::OverConstrained);
+        status = SketchSolveStatus::Overconstrained;
     }
     else if (sketch->getLastHasConflicts()) {
-        status = int(Solver::ConflictingConstraints);
+        status = SketchSolveStatus::ConflictingConstraints;
     }
 }
 

src/Mod/Sketcher/App/SketchAnalysis.h
@@ -25,7 +25,6 @@
 
 #pragma once
 
-#include <memory>
 #include <vector>
 
 #include <Precision.hxx>
@@ -41,11 +40,15 @@ namespace Sketcher
 class SketchObject;
 
 
-enum class Solver
+enum class SketchSolveStatus : int8_t
 {
-    RedundantConstraints = -2,
+    Success = 0,
+    Overconstrained = -4,
     ConflictingConstraints = -3,
-    OverConstrained = -4,
+    MalformedConstraints = -5,
+    SolverError = -1,
+    RedundantConstraints = -2,
+    InvalidGeometry = -6,
 };
 
 
@@ -182,7 +185,7 @@ public:
     /// solves the sketch and retrieves the error status, and the degrees of freedom.
     /// It enables to solve updating the geometry (so moving the geometry to match the constraints)
     /// or preserving the geometry.
-    void solvesketch(int& status, int& dofs, bool updategeo);
+    void solvesketch(SketchSolveStatus& status, int& dofs, bool updategeo);
 
     // third type of routines
     std::vector<Base::Vector3d> getOpenVertices() const;

src/Mod/Sketcher/App/SketchObject.cpp
@@ -141,7 +141,7 @@ SketchObject::SketchObject() : geoLastId(0)
     lastHasRedundancies = false;
     lastHasPartialRedundancies = false;
     lastHasMalformedConstraints = false;
-    lastSolverStatus = 0;
+    lastSolverStatus = GCS::SolveStatus::Success;
     lastSolveTime = 0;
 
     solverNeedsUpdate = false;
@@ -221,29 +221,29 @@ App::DocumentObjectExecReturn* SketchObject::execute()
 
     // This includes a regular solve including full geometry update, except when an error
     // ensues
-    int err = this->solve(true);
-
-    if (err == -4) {// over-constrained sketch
-        std::string msg = "Over-constrained sketch\n";
+    std::string msg;
+    switch (solve(true)) {
+    case SketchSolveStatus::Success:
+        break;
+    case SketchSolveStatus::Overconstrained:
+        msg = "Over-constrained sketch\n";
         appendConflictMsg(lastConflicting, msg);
-        return new App::DocumentObjectExecReturn(msg.c_str(), this);
-    }
-    else if (err == -3) {// conflicting constraints
-        std::string msg = "Sketch with conflicting constraints\n";
+        // TODO: std::move(msg) when DocumentObjectExecReturn takes string by value
+        return new App::DocumentObjectExecReturn(msg, this);
+    case SketchSolveStatus::ConflictingConstraints:
+        msg = "Sketch with conflicting constraints\n";
         appendConflictMsg(lastConflicting, msg);
-        return new App::DocumentObjectExecReturn(msg.c_str(), this);
-    }
-    else if (err == -2) {// redundant constraints
-        std::string msg = "Sketch with redundant constraints\n";
+        return new App::DocumentObjectExecReturn(msg, this);
+    case SketchSolveStatus::RedundantConstraints:
+        msg = "Sketch with redundant constraints\n";
         appendRedundantMsg(lastRedundant, msg);
-        return new App::DocumentObjectExecReturn(msg.c_str(), this);
-    }
-    else if (err == -5) {
-        std::string msg = "Sketch with malformed constraints\n";
+        return new App::DocumentObjectExecReturn(msg, this);
+    case SketchSolveStatus::MalformedConstraints:
+        msg = "Sketch with malformed constraints\n";
         appendMalformedConstraintsMsg(lastMalformedConstraints, msg);
-        return new App::DocumentObjectExecReturn(msg.c_str(), this);
-    }
-    else if (err == -1) {// Solver failed
+        return new App::DocumentObjectExecReturn(msg, this);
+    case SketchSolveStatus::SolverError:
+    case SketchSolveStatus::InvalidGeometry:
         return new App::DocumentObjectExecReturn("Solving the sketch failed", this);
     }
 
@@ -686,25 +686,30 @@ void SketchObject::generateId(const Part::Geometry* geo)
     FC_TRACE("found " << found.front());
     preReturn(found.front());
 }
-// clang-format off
 
-int SketchObject::setTextAndFont(int ConstrId, std::string& newText, std::string& newFont, bool isHeight, bool isConstruction)
+SketchSolveStatus SketchObject::setTextAndFont(
+    int ConstrId,
+    std::string& newText,
+    std::string& newFont,
+    bool isHeight,
+    bool isConstruction
+)
 {
-;    // no need to check input data validity as this is an sketchobject managed operation.
+    // no need to check input data validity as this is an sketchobject managed operation.
     Base::StateLocker lock(managedoperation, true);
 
     // set the changed value for the constraint
     if (this->Constraints.hasInvalidGeometry()) {
-        return -6;
+        return SketchSolveStatus::InvalidGeometry;
     }
     const std::vector<Constraint*>& vals = this->Constraints.getValues();
     if (ConstrId < 0 || ConstrId >= int(vals.size())) {
-        return -1;
+        return SketchSolveStatus::SolverError;
     }
 
     auto* constr = vals[ConstrId];
     if (constr->Type != Text || !constr->hasElement(0)) {
-        return -1;
+        return SketchSolveStatus::SolverError;
     }
 
     // First we replace the old geometries by the new text.
@@ -729,7 +734,7 @@ int SketchObject::setTextAndFont(int ConstrId, std::string& newText, std::string
             }
             geoIdsToDelete.push_back(constr->getGeoId(i));
             if (handleLast) {
-                --handleGeoId; // handle line is added after all text geos.
+                --handleGeoId;  // handle line is added after all text geos.
             }
         }
 
@@ -738,17 +743,19 @@ int SketchObject::setTextAndFont(int ConstrId, std::string& newText, std::string
 
     auto* line = dynamic_cast<const Part::GeomLineSegment*>(getGeometry(handleGeoId));
     if (!line) {
-        return -1;
+        return SketchSolveStatus::SolverError;
     }
 
     // Generate text geos based on new text/font :
     std::vector<std::unique_ptr<Part::Geometry>> newGeos;
     std::vector<TopoDS_Shape> shapes = Part::makeTextWires(newText, newFont);
-    Part::transformAndConvertToGeometry(newGeos,
-                                    shapes,
-                                    line->getStartPoint(),
-                                    line->getEndPoint(),
-                                    isHeight);
+    Part::transformAndConvertToGeometry(
+        newGeos,
+        shapes,
+        line->getStartPoint(),
+        line->getEndPoint(),
+        isHeight
+    );
 
     // Add the geometries to sketch
     int lastGeoid = getHighestCurveIndex();
@@ -775,7 +782,7 @@ int SketchObject::setTextAndFont(int ConstrId, std::string& newText, std::string
     if (hasExistingText) {
         constr = new Constraint();
         constr->Type = Text;
-        constr->truncateElements(0); // remove the First/Second/Third that are created automatically
+        constr->truncateElements(0);  // remove the First/Second/Third that are created automatically
         constr->addElement(GeoElementId(handleGeoId));
     }
     for (int i = lastGeoid + 1; i <= newLastGeoid; ++i) {
@@ -789,16 +796,17 @@ int SketchObject::setTextAndFont(int ConstrId, std::string& newText, std::string
         addConstraint(constr);
     }
 
-    int err = solve();
+    auto status = solve();
 
-    if (err) {
+    if (status == SketchSolveStatus::Success) {
         constr->setText(oldText);
         constr->setFont(oldFont);
         constr->setIsTextHeight(oldIsHeight);
     }
 
-    return err;
+    return status;
 }
+// clang-format off
 
 void SketchObject::acceptGeometry()
 {
@@ -1405,7 +1413,7 @@ void SketchObject::onSketchRestore()
         // this may happen when saving a sketch directly in edit mode
         // but never performed a recompute before
         if (Shape.getValue().IsNull() && hasConflicts() == 0) {
-            if (this->solve(true) == 0)
+            if (this->solve(true) == SketchSolveStatus::Success)
                 Shape.setValue(solvedSketch.toShape());
         }
 

src/Mod/Sketcher/App/SketchObject.h
@@ -340,25 +340,20 @@ public:
      * the sketch object */
     int diagnoseAdditionalConstraints(std::vector<Sketcher::Constraint*> additionalconstraints);
 
-    /** solves the sketch and updates the geometry, but not all the dependent features (does not
-       recompute) When a recompute is necessary, recompute triggers execute() which solves the
-       sketch and updates all dependent features When a solve only is necessary (e.g. DoF changed),
-       solve() solves the sketch and updates the geometry (if updateGeoAfterSolving==true), but does
-       not trigger any recompute.
-       @return 0 if no error, if error, the following codes in this order of priority:
-       -4 if overconstrained,
-       -3 if conflicting constraints,
-       -5 if malformed constraints,
-       -1 if solver error,
-       -2 if redundant constraints
-    */
-    int solve(bool updateGeoAfterSolving = true);
+    /** Solves the sketch and (optionally) updates the geometry.
+     * Does not trigger a recompute of any dependent features; when that is necessary, call
+     * execute() instead. A change in DoF does not require updating dependents, for instance.
+     * @return SketchSolveStatus::Success if no error, or else any of these in this order of
+     *         priority: Overconstrained, ConflictingConstraints, MalformedConstraints,
+     *         SolverError, RedundantConstraints.
+     */
+    SketchSolveStatus solve(bool updateGeoAfterSolving = true);
     /// set the datum of a Distance or Angle constraint and solve
-    int setDatum(int ConstrId, double Datum);
+    SketchSolveStatus setDatum(int ConstrId, double Datum);
     /// get the datum of a Distance or Angle constraint
     double getDatum(int ConstrId) const;
     /// set the text and font of a text constraint
-    int setTextAndFont(
+    SketchSolveStatus setTextAndFont(
         int ConstrId,
         std::string& newText,
         std::string& newFont,
@@ -422,13 +417,13 @@ public:
     /// set the visibility of a group of constraints at once
     int setVisibility(std::vector<int> constrIds, bool isVisible);
     /// move this point to a new location and solve
-    int moveGeometries(
+    SketchSolveStatus moveGeometries(
         const std::vector<GeoElementId>& geoEltIds,
         const Base::Vector3d& toPoint,
         bool relative = false,
         bool updateGeoBeforeMoving = false
     );
-    int moveGeometry(
+    SketchSolveStatus moveGeometry(
         int GeoId,
         PointPos PosId,
         const Base::Vector3d& toPoint,
@@ -481,9 +476,9 @@ public:
     );
 
     /// trim a curve
-    int trim(int geoId, const Base::Vector3d& point, bool includeSketchAxes = false);
+    SketchSolveStatus trim(int geoId, const Base::Vector3d& point, bool includeSketchAxes = false);
     /// extend a curve
-    int extend(int geoId, double increment, PointPos endPoint);
+    SketchSolveStatus extend(int geoId, double increment, PointPos endPoint);
     /// Once smaller pieces have been created from a larger curve (by split or trim, say), derive
     /// the constraint that will replace the given one (which is to be deleted). NOTE: Currently
     /// assuming all constraints on the end points of the old curve have been transferred or
@@ -746,7 +741,7 @@ public:
         return lastHasMalformedConstraints;
     }
     /// gets solver status of last solver execution
-    inline int getLastSolverStatus() const
+    inline GCS::SolveStatus getLastSolverStatus() const
     {
         return lastSolverStatus;
     }
@@ -809,12 +804,12 @@ public: /* Solver exposed interface */
      * state as a reference (enables dragging). NOTE: A temporary move operation must always be
      * preceded by a initTemporaryMove() operation.
      */
-    inline int moveGeometriesTemporary(
+    inline GCS::SolveStatus moveGeometriesTemporary(
         std::vector<GeoElementId> moved,
         Base::Vector3d toPoint,
         bool relative = false
     );
-    inline int moveGeometryTemporary(
+    inline GCS::SolveStatus moveGeometryTemporary(
         int geoId,
         PointPos pos,
         Base::Vector3d toPoint,
@@ -1194,7 +1189,7 @@ private:
     bool lastHasRedundancies;
     bool lastHasPartialRedundancies;
     bool lastHasMalformedConstraints;
-    int lastSolverStatus;
+    GCS::SolveStatus lastSolverStatus;
     float lastSolveTime;
 
     std::vector<int> lastConflicting;
@@ -1289,12 +1284,13 @@ inline int SketchObject::initTemporaryBSplinePieceMove(
     return solvedSketch.initBSplinePieceMove(geoId, pos, firstPoint, fine);
 }
 
-inline int SketchObject::
+inline GCS::SolveStatus SketchObject::
     moveGeometriesTemporary(std::vector<GeoElementId> geoEltIds, Base::Vector3d toPoint, bool relative /*=false*/)
 {
     return solvedSketch.moveGeometries(geoEltIds, toPoint, relative);
 }
-inline int SketchObject::moveGeometryTemporary(int geoId, PointPos pos, Base::Vector3d toPoint, bool relative /*=false*/)
+inline GCS::SolveStatus SketchObject::
+    moveGeometryTemporary(int geoId, PointPos pos, Base::Vector3d toPoint, bool relative /*=false*/)
 {
     std::vector<GeoElementId> moved = {GeoElementId(geoId, pos)};
     return moveGeometriesTemporary(moved, toPoint, relative);

src/Mod/Sketcher/App/SketchObjectConstraints.cpp
@@ -74,7 +74,7 @@ void SketchObject::retrieveSolverDiagnostics()
     lastMalformedConstraints = solvedSketch.getMalformedConstraints();
 }
 
-int SketchObject::solve(bool updateGeoAfterSolving /*=true*/)
+SketchSolveStatus SketchObject::solve(bool updateGeoAfterSolving /*=true*/)
 {
     // no need to check input data validity as this is an sketchobject managed operation.
     Base::StateLocker lock(managedoperation, true);
@@ -107,31 +107,31 @@ int SketchObject::solve(bool updateGeoAfterSolving /*=true*/)
     lastSolveTime = 0.0;
 
     // Failure is default for notifying the user unless otherwise proven
-    lastSolverStatus = GCS::Failed;
+    lastSolverStatus = GCS::SolveStatus::Failed;
 
-    int err = 0;
+    auto status = SketchSolveStatus::Success;
 
     // redundancy is a lower priority problem than conflict/over-constraint/solver error
     // we set it here because we are indeed going to solve, as we can. However, we still want to
     // provide the right error code.
     if (lastHasRedundancies) {// redundant constraints
-        err = -2;
+        status = SketchSolveStatus::RedundantConstraints;
     }
 
     if (lastDoF < 0) {// over-constrained sketch
-        err = -4;
+        status = SketchSolveStatus::Overconstrained;
     }
     else if (lastHasConflict) {// conflicting constraints
         // The situation is exactly the same as in the over-constrained situation.
-        err = -3;
+        status = SketchSolveStatus::ConflictingConstraints;
     }
     else if (lastHasMalformedConstraints) {
-        err = -5;
+        status = SketchSolveStatus::MalformedConstraints;
     }
     else {
         lastSolverStatus = solvedSketch.solve();
-        if (lastSolverStatus != 0) {// solving
-            err = -1;
+        if (lastSolverStatus != GCS::SolveStatus::Success) {// solving
+            status = SketchSolveStatus::SolverError;
         }
     }
 
@@ -165,7 +165,7 @@ int SketchObject::solve(bool updateGeoAfterSolving /*=true*/)
     // In uncommon situations, the analysis of QR decomposition leads to full rank, but the result
     // does not converge. We avoid marking a sketch as fully constrained when no convergence is
     // achieved.
-    if (err == 0) {
+    if (status == SketchSolveStatus::Success) {
         FullyConstrained.setValue(lastDoF == 0);
         if (updateGeoAfterSolving) {
             // set the newly solved geometry
@@ -178,30 +178,30 @@ int SketchObject::solve(bool updateGeoAfterSolving /*=true*/)
 
     signalSolverUpdate();
 
-    return err;
+    return status;
 }
 
-int SketchObject::setDatum(int ConstrId, double Datum)
+SketchSolveStatus SketchObject::setDatum(int ConstrId, double Datum)
 {
     // no need to check input data validity as this is an sketchobject managed operation.
     Base::StateLocker lock(managedoperation, true);
 
     // set the changed value for the constraint
     if (this->Constraints.hasInvalidGeometry())
-        return -6;
+        return SketchSolveStatus::InvalidGeometry;
     const std::vector<Constraint*>& vals = this->Constraints.getValues();
     if (ConstrId < 0 || ConstrId >= int(vals.size()))
-        return -1;
+        return SketchSolveStatus::SolverError;
     ConstraintType type = vals[ConstrId]->Type;
     // for tangent, value==0 is autodecide, value==Pi/2 is external and value==-Pi/2 is internal
     if (!vals[ConstrId]->isDimensional() && type != Tangent && type != Perpendicular)
-        return -1;
+        return SketchSolveStatus::SolverError;
 
     if ((type == Radius || type == Diameter || type == Weight) && Datum <= 0)
-        return (Datum == 0) ? -5 : -4;
+        return (Datum == 0) ? SketchSolveStatus::MalformedConstraints : SketchSolveStatus::Overconstrained;
 
     if (type == Distance && Datum == 0)
-            return -5;
+            return SketchSolveStatus::MalformedConstraints;
 
     // copy the list
     std::vector<Constraint*> newVals(vals);
@@ -211,12 +211,12 @@ int SketchObject::setDatum(int ConstrId, double Datum)
 
     this->Constraints.setValues(std::move(newVals));
 
-    int err = solve();
+    const auto status = solve();
 
-    if (err)
+    if (status != SketchSolveStatus::Success)
         this->Constraints.getValues()[ConstrId]->setValue(oldDatum);// newVals is a shell now
 
-    return err;
+    return status;
 }
 double SketchObject::getDatum(int ConstrId) const
 {

commit e23fdc7057ed948fdc1a07b9056d9b676edd0a9c

Sketcher: Fix uninitialized variable in internalSolve
Initialize status to SolveStatus::Failed, which routes an out-of-range
solver into the existing fallback instead of reporting success.

While there use enumerators fir the case labels.

Fixes: d8cf415e4f6b ("Sketcher: replace solve() & friends' int return codes with enums")

src/Mod/Sketcher/App/Sketch.cpp
@@ -4992,9 +4992,9 @@ GCS::SolveStatus Sketch::internalSolve(std::string& solvername, int level)
         clearTemporaryConstraints();
     }
 
-    GCS::SolveStatus status;
     bool valid_solution;
     int defaultsoltype = -1;
+    GCS::SolveStatus status = GCS::SolveStatus::Failed;
 
     if (isInitMove) {
         solvername = "DogLeg";  // DogLeg is used for dragging (same as before)
@@ -5002,17 +5002,17 @@ GCS::SolveStatus Sketch::internalSolve(std::string& solvername, int level)
     }
     else {
         switch (defaultSolver) {
-            case 0:
+            case GCS::BFGS:
                 solvername = "BFGS";
                 status = GCSsys.solve(GCS::BFGS);
                 defaultsoltype = 2;
                 break;
-            case 1:  // solving with the LevenbergMarquardt solver
+            case GCS::LevenbergMarquardt:
                 solvername = "LevenbergMarquardt";
                 status = GCSsys.solve(GCS::LevenbergMarquardt);
                 defaultsoltype = 1;
                 break;
-            case 2:  // solving with the BFGS solver
+            case GCS::DogLeg:
                 solvername = "DogLeg";
                 status = GCSsys.solve(GCS::DogLeg);
                 defaultsoltype = 0;