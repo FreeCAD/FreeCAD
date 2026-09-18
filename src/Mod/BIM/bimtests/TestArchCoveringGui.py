c3a9d0bfdf0195dac8221da38580532f3c83f1f4
ᴩʜᴏɴᴇᴅʀᴏɪᴅ <73050054+PhoneDroid@users.noreply.github.com>
2026-09-18 19:26:20 -0400

    [ BIM ]: Update SPDX / Copyright (#32770)

--- src/Mod/BIM/bimtests/TestArchCoveringGui.py
+++ src/Mod/BIM/bimtests/TestArchCoveringGui.py
@@ -1,9 +1,23 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
-#
-# Copyright (c) 2026 Furgo
-#
-# This file is part of the FreeCAD BIM workbench.
-# You can find the full license text in the LICENSE file in the root directory.
+# SPDX-FileCopyrightText: 2026 Furgo
+# SPDX-FileNotice: Part of the FreeCAD project.
+
+################################################################################
+#                                                                              #
+#   FreeCAD is free software: you can redistribute it and/or modify            #
+#   it under the terms of the GNU Lesser General Public License as             #
+#   published by the Free Software Foundation, either version 2.1              #
+#   of the License, or (at your option) any later version.                     #
+#                                                                              #
+#   FreeCAD is distributed in the hope that it will be useful,                 #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty                #
+#   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                    #
+#   See the GNU Lesser General Public License for more details.                #
+#                                                                              #
+#   You should have received a copy of the GNU Lesser General Public           #
+#   License along with FreeCAD. If not, see https://www.gnu.org/licenses       #
+#                                                                              #
+################################################################################
 
 import Arch
 import Draft
