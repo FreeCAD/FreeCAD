e1417fd7d1f106cde3ff6e10c9678d25772bec35
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2026-09-19 12:01:30 -0400

    [ BIM ]: Update SPDX / Copyright

--- src/Mod/BIM/bimcommands/BimCutPlane.py
+++ src/Mod/BIM/bimcommands/BimCutPlane.py
@@ -1,28 +1,25 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
+# SPDX-FileCopyrightText: 2014 Jonathan Wiedemann
+# SPDX-FileCopyrightText: 2019 Jerome Laverroux
+# SPDX-FileCopyrightText: 2023 FreeCAD Project Association
+# SPDX-FileNotice: Part of the FreeCAD project.
 
-# *****************************************************************************
-# *                                                                           *
-# *   Copyright (c) 2014 Jonathan Wiedemann <wood.galaxy@gmail.com> (cutplan) *
-# *   Copyright (c) 2019 Jerome Laverroux <jerome.laverroux@free.fr> (cutline)*
-# *   Copyright (c) 2023 FreeCAD Project Association                          *
-# *                                                                           *
-# *   This file is part of FreeCAD.                                           *
-# *                                                                           *
-# *   FreeCAD is free software: you can redistribute it and/or modify it      *
-# *   under the terms of the GNU Lesser General Public License as             *
-# *   published by the Free Software Foundation, either version 2.1 of the    *
-# *   License, or (at your option) any later version.                         *
-# *                                                                           *
-# *   FreeCAD is distributed in the hope that it will be useful, but          *
-# *   WITHOUT ANY WARRANTY; without even the implied warranty of              *
-# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU        *
-# *   Lesser General Public License for more details.                         *
-# *                                                                           *
-# *   You should have received a copy of the GNU Lesser General Public        *
-# *   License along with FreeCAD. If not, see                                 *
-# *   <https://www.gnu.org/licenses/>.                                        *
-# *                                                                           *
-# *****************************************************************************
+################################################################################
+#                                                                              #
+#   FreeCAD is free software: you can redistribute it and/or modify            #
+#   it under the terms of the GNU Lesser General Public License as             #
+#   published by the Free Software Foundation, either version 2.1              #
+#   of the License, or (at your option) any later version.                     #
+#                                                                              #
+#   FreeCAD is distributed in the hope that it will be useful,                 #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty                #
+#   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                    #
+#   See the GNU Lesser General Public License for more details.                #
+#                                                                              #
+#   You should have received a copy of the GNU Lesser General Public           #
+#   License along with FreeCAD. If not, see https://www.gnu.org/licenses       #
+#                                                                              #
+################################################################################
 
 """The Arch CutPlane command"""
 
