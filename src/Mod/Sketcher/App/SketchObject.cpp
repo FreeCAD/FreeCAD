FreeCAD/FreeCAD

commit 8a1bdba1dd2f15dc3bb774fbf97caa3e65f8ccd0

Base: Refactor logging printf-style to std::format
Signed-off-by: Slendi <slendi@socopon.com>
Signed-off-by: Ladislav Michl <ladis@triops.cz>

src/Mod/Sketcher/App/SketchObject.cpp
@@ -335,7 +335,7 @@ App::DocumentObjectExecReturn* SketchObject::execute()
     catch (const Base::Exception&) {
         // 9/16/24: We used to clear the constraints here, but we no longer want to do that
         // as missing reference geometry is not considered an error while we sort out sketcher UI.
-        // Base::Console().error("%s\nClear constraints to external geometry\n", e.what());
+        // Base::Console().error("{}\nClear constraints to external geometry\n", e.what());
         // we cannot trust the constraints of external geometries, so remove them
         //  delConstraintsToExternal();
     }
@@ -1111,10 +1111,14 @@ void SketchObject::onGeometryChanged()
         acceptGeometry();
     }
     else {
-        Base::Console().error(
+        Base::Console().send<
+            Base::LogStyle::Error,
+            Base::IntendedRecipient::All,
+            Base::ContentType::Untranslated>(
             this->getFullLabel() + " SketchObject::onChanged ",
+            "{}",
             QT_TRANSLATE_NOOP("Notifications", "Unmanaged change of Constraint "
-                              "Property results in invalid constraint indices") "\n");
+                "Property results in invalid constraint indices") "\n");
     }
     Base::StateLocker lock(internaltransaction, true);
     setUpSketch();
@@ -1155,8 +1159,12 @@ void SketchObject::onConstraintsChanged()
         }
     }
     else {
-        Base::Console().error(
+        Base::Console().send<
+            Base::LogStyle::Error,
+            Base::IntendedRecipient::All,
+            Base::ContentType::Untranslated>(
             this->getFullLabel() + " SketchObject::onChanged ",
+            "{}",
             QT_TRANSLATE_NOOP("Notifications", "Unmanaged change of Constraint "
                               "Property results in invalid constraint indices") "\n");
     }
@@ -1667,8 +1675,9 @@ void SketchObject::migrateSketch()
 
     Constraints.setValues(std::move(newConstraints));
 
-    Base::Console().critical(
+    Base::Console().send<Base::LogStyle::Critical>(
         this->getFullName(),
+        "{}",
         QT_TRANSLATE_NOOP(
             "Notifications",
             "Parabolas were migrated. Migrated files won't open in previous "

src/Mod/Sketcher/App/SketchObjectConstraints.cpp
@@ -137,8 +137,12 @@ SketchSolveStatus SketchObject::solve(bool updateGeoAfterSolving /*=true*/)
     }
 
     if (lastHasMalformedConstraints) {
-        Base::Console().error(
+        Base::Console().send<
+            Base::LogStyle::Error,
+            Base::IntendedRecipient::All,
+            Base::ContentType::Untranslated>(
             this->getFullLabel(),
+            "{}",
             QT_TRANSLATE_NOOP("Notifications", "The Sketch has malformed constraints!") "\n");
     }
 
@@ -158,7 +162,10 @@ SketchSolveStatus SketchObject::solve(bool updateGeoAfterSolving /*=true*/)
             "\"%1\" has partially redundant constraint(s)."
         ).arg(ref);
 
-        Base::Console().warning(this->getFullLabel(), "%s\n", msg.toUtf8().constData());
+        Base::Console().send<Base::LogStyle::Warning>(
+            this->getFullLabel(),
+            "{}\n",
+            msg.toStdString());
     }
 
     lastSolveTime = solvedSketch.getSolveTime();
@@ -2544,13 +2551,13 @@ int SketchObject::changeConstraintsLocking(bool bLock)
                 cntSuccess++;
 
             newVals[i] = constNew;
-            Base::Console().log("Constraint%i will be affected\n", i + 1);
+            Base::Console().log("Constraint{} will be affected\n", i + 1);
         }
     }
 
     this->Constraints.setValues(std::move(newVals));
 
-    Base::Console().log("ChangeConstraintsLocking: affected %i of %i tangent/perp constraints\n",
+    Base::Console().log("ChangeConstraintsLocking: affected {} of {} tangent/perp constraints\n",
                         cntSuccess,
                         cntToBeAffected);
 
@@ -2639,13 +2646,13 @@ int SketchObject::port_reversedExternalArcs(bool justAnalyze)
             if (!justAnalyze) {
                 newVals[ic] = constNew.release();
             }
-            Base::Console().log("Constraint%i will be affected\n", ic + 1);
+            Base::Console().log("Constraint{} will be affected\n", ic + 1);
         };
     }
 
     if (!justAnalyze) {
         this->Constraints.setValues(std::move(newVals));
-        Base::Console().log("Swapped start/end of reversed external arcs in %i constraints\n",
+        Base::Console().log("Swapped start/end of reversed external arcs in {} constraints\n",
                             cntToBeAffected);
     }
 
@@ -2750,7 +2757,7 @@ bool SketchObject::AutoLockTangencyAndPerpty(Constraint* cstr, bool bForce, bool
     }
     catch (Base::Exception& e) {
         // failure to determine tangency type is not a big deal, so a warning.
-        Base::Console().warning("Error in AutoLockTangency. %s \n", e.what());
+        Base::Console().warning("Error in AutoLockTangency. {} \n", e.what());
         return false;
     }
     return true;

src/Mod/Sketcher/Gui/DrawSketchHandlerBSpline.h
@@ -884,7 +884,10 @@ private:
                 // Since it happens very frequently that the interpolation fails
                 // it's sufficient to report this as log message to avoid to pollute
                 // the output window
-                Base::Console().log(std::string("drawBSplineToPosition"), "interpolation failed\n");
+                Base::Console().send<Base::LogStyle::Log>(
+                    "drawBSplineToPosition",
+                    "interpolation failed\n"
+                );
             }
         }
     }

commit 3e63665f5f29629527f0c1b0f53c04fd70b93626

Sketcher: Log through the named Console functions
Fixes: 8a1bdba1dd2f ("Base: Refactor logging printf-style to std::format")

src/Mod/Sketcher/App/SketchObject.cpp
@@ -1111,12 +1111,8 @@ void SketchObject::onGeometryChanged()
         acceptGeometry();
     }
     else {
-        Base::Console().send<
-            Base::LogStyle::Error,
-            Base::IntendedRecipient::All,
-            Base::ContentType::Untranslated>(
+        Base::Console().error(
             this->getFullLabel() + " SketchObject::onChanged ",
-            "{}",
             QT_TRANSLATE_NOOP("Notifications", "Unmanaged change of Constraint "
                 "Property results in invalid constraint indices") "\n");
     }
@@ -1159,12 +1155,8 @@ void SketchObject::onConstraintsChanged()
         }
     }
     else {
-        Base::Console().send<
-            Base::LogStyle::Error,
-            Base::IntendedRecipient::All,
-            Base::ContentType::Untranslated>(
+        Base::Console().error(
             this->getFullLabel() + " SketchObject::onChanged ",
-            "{}",
             QT_TRANSLATE_NOOP("Notifications", "Unmanaged change of Constraint "
                               "Property results in invalid constraint indices") "\n");
     }
@@ -1675,9 +1667,8 @@ void SketchObject::migrateSketch()
 
     Constraints.setValues(std::move(newConstraints));
 
-    Base::Console().send<Base::LogStyle::Critical>(
+    Base::Console().critical(
         this->getFullName(),
-        "{}",
         QT_TRANSLATE_NOOP(
             "Notifications",
             "Parabolas were migrated. Migrated files won't open in previous "

src/Mod/Sketcher/App/SketchObjectConstraints.cpp
@@ -137,12 +137,8 @@ SketchSolveStatus SketchObject::solve(bool updateGeoAfterSolving /*=true*/)
     }
 
     if (lastHasMalformedConstraints) {
-        Base::Console().send<
-            Base::LogStyle::Error,
-            Base::IntendedRecipient::All,
-            Base::ContentType::Untranslated>(
+        Base::Console().error(
             this->getFullLabel(),
-            "{}",
             QT_TRANSLATE_NOOP("Notifications", "The Sketch has malformed constraints!") "\n");
     }
 
@@ -162,10 +158,7 @@ SketchSolveStatus SketchObject::solve(bool updateGeoAfterSolving /*=true*/)
             "\"%1\" has partially redundant constraint(s)."
         ).arg(ref);
 
-        Base::Console().send<Base::LogStyle::Warning>(
-            this->getFullLabel(),
-            "{}\n",
-            msg.toStdString());
+        Base::Console().warning(this->getFullLabel(), "{}\n", msg.toStdString());
     }
 
     lastSolveTime = solvedSketch.getSolveTime();

src/Mod/Sketcher/Gui/DrawSketchHandlerBSpline.h
@@ -884,10 +884,7 @@ private:
                 // Since it happens very frequently that the interpolation fails
                 // it's sufficient to report this as log message to avoid to pollute
                 // the output window
-                Base::Console().send<Base::LogStyle::Log>(
-                    "drawBSplineToPosition",
-                    "interpolation failed\n"
-                );
+                Base::Console().log("drawBSplineToPosition", "interpolation failed\n");
             }
         }
     }