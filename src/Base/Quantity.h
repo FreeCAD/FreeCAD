9f9b829823f4bb9a7629620f25d3d6f48577ea18
marioalexis84 <53127171+marioalexis84@users.noreply.github.com>
2026-09-22 03:04:26 -0300

    Base: Make the FEM unit scheme usable
    * App: Add licence header in ExpressionParser script
    * Base: Add Newton-millimeter unit
    * Base: Make the FEM unit scheme usable

--- src/Base/Quantity.h
+++ src/Base/Quantity.h
@@ -337,6 +337,7 @@ public:
     static const Quantity MilliJoule;
     static const Quantity KiloJoule;
     static const Quantity NewtonMeter;
+    static const Quantity NewtonMilliMeter;
     static const Quantity VoltAmpereSecond;
     static const Quantity WattSecond;
     static const Quantity KiloWattHour;
