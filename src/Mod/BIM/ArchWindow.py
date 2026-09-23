02ec20d24b98d1005531a55fa271da78160085ae
Furgo <148809153+furgo16@users.noreply.github.com>
2026-09-23 19:57:34 +0200

    BIM: add percentage suffix for clarity

--- src/Mod/BIM/ArchWindow.py
+++ src/Mod/BIM/ArchWindow.py
@@ -1467,10 +1467,12 @@ class _ArchWindowTaskPanel:
                 )
             )
             # Opening is a scalar property, as opposed to a quantity property. It appears to have
-            # no "preferred unit" metadata. We cannot set the suffix manually either, but at least
-            # we can set some safe limits. These limits are hardcoded: the Property Editor clamps
-            # the property to these, so it must be reading them from metadata, but they might not
-            # be queryable via Python.
+            # no "preferred unit" metadata, but it is a percentage (App::PropertyPercent), so set
+            # the same " %" suffix the Property Editor uses for that property type. We can also set
+            # some safe limits. These limits are hardcoded: the Property Editor clamps the property
+            # to these, so it must be reading them from metadata, but they might not be queryable
+            # via Python.
+            self.openingWidget.setSuffix(" %")
             self.openingWidget.setProperty("minimum", 0)
             self.openingWidget.setProperty("maximum", 100)
             self.openingWidget.setProperty("value", self.obj.Opening)
