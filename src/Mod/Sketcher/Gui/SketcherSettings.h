0ffd12e033eea1bd95dfcba1c1830bff8caea0d0
chris jones @ipatch <chris.r.jones.1983@gmail.com>
2026-09-25 18:03:37 -0500

    rbme: update SketcherSettings.h

--- src/Mod/Sketcher/Gui/SketcherSettings.h
+++ src/Mod/Sketcher/Gui/SketcherSettings.h
@@ -50,18 +50,14 @@ public:
     void saveSettings() override;
     void loadSettings() override;
 
-
-    static void loadThemeDefaults();
-
 protected:
     void changeEvent(QEvent* e) override;
     void dimensioningModeChanged(int index);
     void checkForRestart();
 
 private:
-    void resetSettingsToDefaults() override;
-
     std::unique_ptr<Ui_SketcherSettings> ui;
+    void resetSettingsToDefaults() override;
 };
 
 /**
@@ -140,6 +136,7 @@ public:
 
     void saveSettings() override;
     void loadSettings() override;
+    static void loadThemeDefaults();
 
 protected:
     void changeEvent(QEvent* e) override;
@@ -152,14 +149,14 @@ private:
 // Mode for the sketch autoscale feature which scales
 // the geometry and zooms the camera when the first
 // scale defining constraint is set
-enum class AutoScaleMode : int
+enum class AutoScaleMode : u_int8_t
 {
     Always = 0,
     Never = 1,
 
     // Attempts to find scale reference objects int the viewport
     // (such as a 3d body) and disable the feature if it finds one
-    WhenNoScaleFeatureIsVisible = 2
+    WhenNoScaleFeatureIsVisible = 2,
 };
 
 }  // namespace SketcherGui
