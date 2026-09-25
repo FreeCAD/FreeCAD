bae48ab05e894b19df69b2f8542d08b756f76d76
John Lewin <lewin76@gmail.com>
2026-09-25 14:19:26 -0700

    Open spreadsheet after creation

--- src/Mod/Spreadsheet/Gui/Command.cpp
+++ src/Mod/Spreadsheet/Gui/Command.cpp
@@ -981,6 +981,7 @@ void CmdCreateSpreadsheet::activated(int iMsg)
     doCommand(Doc, "App.activeDocument().addObject('Spreadsheet::Sheet','%s\')", FeatName.c_str());
     doCommand(Gui, "Gui.Selection.clearSelection()\n");
     doCommand(Gui, "Gui.Selection.addSelection(App.activeDocument().Name,'%s\')", FeatName.c_str());
+    doCommand(Gui, "Gui.activeDocument().setEdit('%s')", FeatName.c_str());
     commitCommand();
 }
 
